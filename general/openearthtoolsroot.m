ed127f11683f57b40ca1f843eab34bd0ae11b395
heijer <heijer@localhost>
2008-11-12 12:01:30 +0000

    openearthtoolsroot.m added
    svn path=/trunk/matlab/; revision=36
    
    
    Former-commit-id: e3523acabe51d60b98dc4cec6a65e9e5faa0f61b

---
+++ general/openearthtoolsroot.m
@@ -0,0 +1,47 @@
+function S = openearthtoolsroot
+% OPENEARTHTOOLSROOT root directory of OpenEarthTools installation
+%
+% S = openearthtoolsroot returns a string that is the name of the directory
+% where the OpenEarthTools software is installed.
+%
+% see also: matlabroot
+
+%   --------------------------------------------------------------------
+%   Copyright (C) 2008 Deltares
+%       C.(Kees) den Heijer
+%
+%       Kees.denHeijer@deltares.nl	
+%
+%       Deltares
+%       P.O. Box 177
+%       2600 MH Delft
+%       The Netherlands
+%
+%   This library is free software; you can redistribute it and/or
+%   modify it under the terms of the GNU Lesser General Public
+%   License as published by the Free Software Foundation; either
+%   version 2.1 of the License, or (at your option) any later version.
+%
+%   This library is distributed in the hope that it will be useful,
+%   but WITHOUT ANY WARRANTY; without even the implied warranty of
+%   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+%   Lesser General Public License for more details.
+%
+%   You should have received a copy of the GNU Lesser General Public
+%   License along with this library; if not, write to the Free Software
+%   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+%   USA
+%   or http://www.gnu.org/licenses/licenses.html, http://www.gnu.org/, http://www.fsf.org/
+%   --------------------------------------------------------------------
+
+% $Id$ 
+% $Date$
+% $Author$
+% $Revision$
+
+%%
+S = which(mfilename); % fulle filename of current file
+
+ind = findstr(S, [filesep 'matlab' filesep 'general' filesep]);
+
+S = S(1:ind-1); % this is the openearthtoolsroot
