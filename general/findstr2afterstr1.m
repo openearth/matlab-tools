9f713dc64f223689e3d902221ab1033305eaa0e2
heijer <heijer@localhost>
2008-11-13 14:03:15 +0000

    LGPL block added
    svn path=/trunk/matlab/; revision=43
    
    
    Former-commit-id: 4c2c769ada7e8385054ab4dd2474da4b4e123ad0

--- general/findstr2afterstr1.m
+++ general/findstr2afterstr1.m
@@ -18,13 +18,40 @@ function [k] = findstr2afterstr1(str, str1, str2)
 %
 % See also: findstr
  
-%--------------------------------------------------------------------------------
-% Copyright(c) Deltares 2004 - 2008  FOR INTERNAL USE ONLY
-% Version:  Version 1.0, June 2008 (Version 1.0, June 2008)
-% By:      <C. (Kees) den Heijer (email:Kees.denHeijer@deltares.nl)>
-%--------------------------------------------------------------------------------
- 
- 
+%   --------------------------------------------------------------------
+%   Copyright (C) 2008 Deltares
+%       C.(Kees) den Heijer
+%
+%       Kees.denHeijer@deltares.nl	
+%
+%       Deltares
+%       P.O. Box 177
+%       2600 MH Delft
+%       The Netherlands
+%
+%   This library is free software; you can redistribute it and/or
+%   modify it under the terms of the GNU Lesser General Public
+%   License as published by the Free Software Foundation; either
+%   version 2.1 of the License, or (at your option) any later version.
+%
+%   This library is distributed in the hope that it will be useful,
+%   but WITHOUT ANY WARRANTY; without even the implied warranty of
+%   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+%   Lesser General Public License for more details.
+%
+%   You should have received a copy of the GNU Lesser General Public
+%   License along with this library; if not, write to the Free Software
+%   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+%   USA
+%   or http://www.gnu.org/licenses/licenses.html, http://www.gnu.org/, http://www.fsf.org/
+%   --------------------------------------------------------------------
+
+% $Id$ 
+% $Date$
+% $Author$
+% $Revision$
+
+%% 
 str1loc = min(findstr(str, str1));
 if isempty(str1loc)
     k = [];
