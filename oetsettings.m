48d81415712bd953869322a082cd0189b2a07b52
heijer <heijer@localhost>
2008-12-17 23:13:12 +0000

    function to automatically set the svn keywords for any new mfile added, and called from oetsettings.m
    svn path=/trunk/matlab/; revision=109
    
    
    Former-commit-id: 7cde430f69be0b1e15acb69ed9d35d13538fff11

--- oetsettings.m
+++ oetsettings.m
@@ -69,6 +69,9 @@ function oetsettings(varargin)
 
    clear state
 
+%% set svn:keywords automatically to any new m-file
+   autosetSVNkeywords
+   
 %% Report
 %% ---------------------
    help oetsettings
