b867e4f8cdf7265526577a2092566da11858d90f
boer_g <boer_g@localhost>
2009-01-22 13:54:53 +0000

    updated comments: tip on manual cticklabels
    svn path=/trunk/matlab/; revision=158
    
    
    Former-commit-id: 106aa7054d445fed24abf102ba0d986e6c7615ae

--- general/plot_fun/colorbarwithxlabel.m
+++ general/plot_fun/colorbarwithxlabel.m
@@ -10,10 +10,14 @@ function [varargout]=colorbarwithxlabel(colorbartxt,varargin)
 %   [h]=colorbarwithxlabel(colorbartxt) does the same
 %   and returns the handle h of the colorbar.
 %
-%   [ax, h]=colorbarwithxlabel(colorbartxt) 
+%   [ax, h]=colorbarwithxlabel(colorbartxt,<ctick>) 
 %   returns both the handle h of the colorbar and
 %   the handle ax of the axes. NOTE that ax is
 %   returned first to follow the syntax of ax = colorbar.
+%
+%   Example:  
+%   [ax, h]=colorbarwithxlabel('wind direction',[0:90:360]) 
+%   set(ax,'YTickLabel',{'E','N','W','S'})
 %
 %   colorbarwithxlabel(colorbartxt,arguments) or
 %   colorbarwithxlabel(colorbartxt,ctick,arguments) passes arguments
@@ -22,8 +26,33 @@ function [varargout]=colorbarwithxlabel(colorbartxt,varargin)
 %   See also: COLORBAR, SET(gca), GET(gca), COLORBARWITHTITLE,COLORBARWITHxlabel
 %             COLORBARWITHhTEXT, COLORBARWITHhTEXT
  
-%   G.J. de Boer, Delft Univeristy of Technology, WL | Delft Hydraulics
-%   Oct 2006, 1.1
+%   --------------------------------------------------------------------
+%   Copyright (C) 2006 Delft University of Technology
+%       Gerben J. de Boer
+%
+%       g.j.deboer@tudelft.nl	
+%
+%       Fluid Mechanics Section
+%       Faculty of Civil Engineering and Geosciences
+%       PO Box 5048
+%       2600 GA Delft
+%       The Netherlands
+%
+%   This library is free software; you can redistribute it and/or
+%   modify it under the terms of the GNU Lesser General Public
+%   License as published by the Free Software Foundation; either
+%   version 2.1 of the License, or (at your option) any later version.
+%
+%   This library is distributed in the hope that it will be useful,
+%   but WITHOUT ANY WARRANTY; without even the implied warranty of
+%   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+%   Lesser General Public License for more details.
+%
+%   You should have received a copy of the GNU Lesser General Public
+%   License along with this library; if not, write to the Free Software
+%   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+%   USA
+%   -------------------------------------------------------------------
 
     OPT.position = 'xlabel';
 
