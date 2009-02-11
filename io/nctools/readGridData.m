9428ceb879fff88ce12eeec63d3d478f1898d11d
baart_f <baart_f@localhost>
2009-02-11 12:16:32 +0000

    copied from mctools
    svn path=/trunk/matlab/; revision=197
    
    
    Former-commit-id: c932f6ac5cc57b0f520269b2d01bca94ad81319f

---
+++ io/nctools/readGridData.m
@@ -0,0 +1,64 @@
+function [d] = readGridData(datatype, name, year, soundingID)
+%READGRIDDATA   transforms processed data to proper meta data format
+%
+% input:
+%   datatype
+%   name
+%   year
+%   soundingID
+%
+%   See also readTransectData, readLineData, readPointData
+
+% -------------------------------------------------------------
+% Copyright (c) WL|Delft Hydraulics 2004-2008 FOR INTERNAL USE ONLY
+% Version:      Version 1.3, Februari 2008 (Version 1.0, February 2004)
+% By:           <M. van Koningsveld (email: mark.vankoningsveld@wldelft.nl>
+% -------------------------------------------------------------
+
+switch nargin
+    case 0
+        return
+    case 1
+        if ~isempty(str2num(datatype)) %#ok<ST2NM>
+            temp=DBGetTableEntry('grid','datatype',datatype);
+        else
+            temp=DBGetTableEntry('grid','datatypeinfo',datatype);
+        end
+    case 2
+        if ~isempty(str2num(datatype)) %#ok<ST2NM>
+            temp=DBGetTableEntry('grid','datatype',datatype,'name',name);
+        else
+            temp=DBGetTableEntry('grid','datatypeinfo',datatype,'name',name);
+        end
+    case 3
+        if ~isempty(str2num(datatype)) %#ok<ST2NM>
+            temp=DBGetTableEntry('grid','datatype',datatype,'name',name,'year',year);
+        else
+            temp=DBGetTableEntry('grid','datatypeinfo',datatype,'name',name,'year',year);
+        end
+    case 4
+        if ~isempty(str2num(datatype)) %#ok<ST2NM>
+            temp=DBGetTableEntry('grid','datatype',datatype,'name',name,'year',year,'soundingID',soundingID);
+        else
+            temp=DBGetTableEntry('grid','datatypeinfo',datatype,'name',name,'year',year,'soundingID',soundingID);
+        end
+end
+
+d=rearrangeData(temp);
+
+function d = rearrangeData(temp)
+d=temp;
+
+if size(d.fielddata.interpx,1)==1 & size(d.fielddata.interpx,2)~=1 %#ok<AND2>
+    d.fielddata.interpx = repmat(d.fielddata.interpx,size(d.fielddata.interpz,1),1);
+end
+if size(d.fielddata.interpy,2)==1 & size(d.fielddata.interpy,1)~=1 %#ok<AND2>
+    d.fielddata.interpy = repmat(d.fielddata.interpy,1,size(d.fielddata.interpz,2));
+end
+
+try d.X=[d.fielddata.interpx]; end
+try d.X(d.X==-999.99)=nan;     end
+try d.Y=[d.fielddata.interpy]; end
+try d.Y(d.Y==-999.99)=nan;     end
+try d.Z=[d.fielddata.interpz]; end
+try d.Z(d.Z==-999.99)=nan;     end
