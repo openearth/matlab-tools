931b5c4306b8150c1f6f946609eaf98ad5495373
sonnevi <sonnevi@localhost>
2009-02-13 13:19:27 +0000

    - UCIT-NetCDF Changed r29: if more datablocks are found with similar id take one with most data coverage. See e.g. Noord-Holland - 1985 - 00020
    svn path=/trunk/matlab/; revision=215
    
    
    Former-commit-id: cdbb2a8dda1ccfa7ef5e4191ca301848d45ad943

--- io/nctools/transect2netcdf.m
+++ io/nctools/transect2netcdf.m
@@ -1,38 +1,41 @@
 function transect2netcdf(filename, transectStruct)
-    % TODO: define the function header here ...
-    
+% TODO: define the function header here ...
+
 
 %% Lookup variables
-    % This assumes a grid already has been saved to the file
-    yearArray = nc_varget(filename, 'year');
-    transectIdArray = nc_varget(filename, 'id');
-    seawardDistanceArray = nc_varget(filename, 'seaward_distance');
-    missing = nc_attget(filename, 'height', '_FillValue');
+% This assumes a grid already has been saved to the file
+yearArray = nc_varget(filename, 'year');
+transectIdArray = nc_varget(filename, 'id');
+seawardDistanceArray = nc_varget(filename, 'seaward_distance');
+missing = nc_attget(filename, 'height', '_FillValue');
 %% Write data to file
-    % Loop over time first, this is most efficient if it's the slowest
-    % moving dimension.
-    for i = 1 : length(yearArray)
-        year = yearArray(i);
-        %block to store to netcdf. Storing more data at once is faster. But
-        %it will require more memory.
-        transectsForYearStruct = transectStruct([transectStruct.year] == year);
-        datablock = repmat(missing, length(transectIdArray), length(seawardDistanceArray));
-        for j = 1 : length(transectIdArray)
-            id = transectIdArray(j);
-            transect = transectsForYearStruct([transectsForYearStruct.id] == id);
-            if isempty(transect)
-                continue
-            elseif length(transect) > 1
-                transect = transect(length(transect)); %HACK: take the last one if we have more than 1. This should not happen if data is sound.
+% Loop over time first, this is most efficient if it's the slowest
+% moving dimension.
+for i = 1 : length(yearArray)
+    year = yearArray(i);
+    %block to store to netcdf. Storing more data at once is faster. But
+    %it will require more memory.
+    transectsForYearStruct = transectStruct([transectStruct.year] == year);
+    datablock = repmat(missing, length(transectIdArray), length(seawardDistanceArray));
+    for j = 1 : length(transectIdArray)
+        id = transectIdArray(j);
+        transect = transectsForYearStruct([transectsForYearStruct.id] == id);
+        if isempty(transect)
+            continue
+        elseif length(transect) > 1
+            for k = 1 : length(transect)
+                lengthOfData(k) = length(transect(k).height);
             end
+            transect = transect(find(lengthOfData==max(lengthOfData))); %HACK: take the longest. This should not happen if data is sound.
+
             [c, ia, ib] = intersect(seawardDistanceArray, transect.seawardDistance);
             datablock(j, ia) = transect.height(ib);
         end
         nc_varput(filename, 'height', datablock, [i-1, 0, 0], [1, size(datablock)]); % (/i-1, 0, 0/) -> in fortran
     end
-        
-        
-        
+
+
+
 
 
 end
