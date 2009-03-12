2a1c803fabb1399b8c8d5835661fcf95dffc609a
boer_g <boer_g@localhost>
2009-03-12 15:26:59 +0000

    Changed manual input arguments order to enforce specification of column names, and added option to use solely table field from SWAN_INPUT as input.
    svn path=/trunk/matlab/; revision=278
    
    
    Former-commit-id: 12921b520bfaec70af80339fa4ae1a1f84450191

--- applications/SWAN/swan_io_table.m
+++ applications/SWAN/swan_io_table.m
@@ -1,44 +1,57 @@
-function swan = swan_io_table(fname,varargin)
+function varargout = swan_io_table(varargin)
 %SWAN_IO_TABLE    read SWAN ASCII output table        (BETA VERSION).
 %
-% DATA = SWAN_IO_TABLE(fname)
-% DATA = SWAN_IO_TABLE(fname,mnmax)
-% DATA = SWAN_IO_TABLE(fname,[]   ,fieldcolumnnames)
-% DATA = SWAN_IO_TABLE(fname,mnmax,fieldcolumnnames)
+%   TAB = swan_io_table(fname)
+%   TAB = swan_io_table(fname,fieldcolumnnames)
+%   TAB = swan_io_table(fname,fieldcolumnnames,mxyc)
+%   TAB = swan_io_table(INP.table(i))
 %
-% where mnmax is a 2-element vector with the number of 
-%    SWAN !nodes ! , i.e. 1 more than the number of 
-%    SWAN !meshes! as given in the SWAN input file.
-%    Use swan_input to get to know this (in case of COMPGRID).
-%    If not specified, or is empty, 1D vectors are returned.
-%
+% where fname is the table file name (recommended *.crv)
 % where fieldcolumnnames is a cell array or a white space delimited char 
 %    with the field names for each column, which are by default 
 %    the small case SHORT names as defined for SWAN input file code:
 %    Use SWAN_INPUT to get to read these. Use SWAN_QUANTITY to
-%    get info regarding these.
-%    ------------------------------------------------------
-%    TABLE 'COMPGRID' NOHEAD 'SWANOUT'  _                                        
-%     HSIGN    DIR      TM01     DEPTH    VELOC    TRANSP _                          
-%     DSPR     DISSIP   LEAK     QB       XP       YP     _                          
-%     DIST     UBOT     STEEPW   WLENGTH  FORCES   RTP    _                          
-%     PDIR                                                                           
-%    ------------------------------------------------------
-% All fieldnames are turned into upper case.
+%    get info regarding these. All fieldnames are turned into upper case.
+%    If not specified, one raw output matrix is returned.
+% where mxyc is a 2-element vector with the number of 
+%    SWAN !nodes ! , i.e. 1 more than the number of 
+%    SWAN !meshes! as given in the SWAN input file.
+%    Use swan_input to get to know this (in case of COMPGRID).
+%    If not specified, or empty, 1D vectors are returned.
+% where INP.table is returned by INP = swan_io_input('INPUT')
+%    For multidimensional INP.table loads all tables.
 %
-% Example:
+% Example to load following SWAN table:
 %
-%   INP = swan_io_input('INPUT')
-%   for itab=1:length(INP.table)
-%      TAB(itab) = swan_io_table(INP.table(itab).fname ,...
-%                               [INP.table(itab).mxc+1  ...
-%                                INP.table(itab).myc+1],...
-%                                INP.table(itab).parameter.names)
-%   end
+%    ----------------------------------------------------
+%    TABLE 'COMPGRID' HEADER   'tst.crv'  XP YP DEP HSIGN 
+%    ----------------------------------------------------
+%
+%    TAB = swan_io_table('tst.crv','XP YP DEP HSIGN');
+%
+% Example to load SWAN table automatically using INPUT file-info:
+%
+%    INP = swan_io_input('INPUT')
+%
+%    for itab=1:length(INP.table)
+%       TAB(itab) = swan_io_table(INP.table(itab).fname ,...
+%                                 INP.table(itab).parameter.names,...
+%                                [INP.table(itab).mxc+1  ...
+%                                 INP.table(itab).myc+1]);
+%    end
+%
+%    % is same as
+%
+%    TAB = swan_io_table(INP.table)
 %
 % See also: SWAN_IO_SPECTRUM, SWAN_IO_INPUT, SWAN_IO_GRD, SWAN_IO_BOT
 
-% to do: add option to read based on solely table struct as read by SWAN_IO_INPUT
+% 2009 mar 12: added option to read based on solely table 
+%              struct as read by SWAN_IO_INPU. Removed 
+%              default parameter name list, only split into 
+%              fields when parameter names are specified, changed 
+%              order of input into (...,fieldcolumnnames,<mxyc>). [Gerben de Boer]
+%              
 
 %   --------------------------------------------------------------------
 %   Copyright (C) 2005 Delft University of Technology
@@ -69,123 +82,154 @@ function swan = swan_io_table(fname,varargin)
 %   http://www.gnu.org/licenses/licenses.html, http://www.gnu.org/, http://www.fsf.org/
 %   --------------------------------------------------------------------
 
-      mmax = [];
-      nmax = [];
-   if nargin>1
-      if ~isempty(varargin{1})
-         mmax = varargin{1}(1);
-         nmax = varargin{1}(2);
+%% Input
+%-------------------------
+
+   INP.table.mmax = [];
+   INP.table.nmax = [];
+   
+   if isstruct(varargin{1})
+      INP.table = varargin{1};
+      
+      %% Recursively call SWAN_IO_TABLE for multiple tables
+      %-------------------------
+      if length(INP.table(:)) > 1
+         disp(['swan_io_table: Loading multiple tables.'])
+         for itab=1:length(INP.table)      
+            TAB(itab) = swan_io_table(INP.table(itab));  
+            disp(['loaded table ',num2str(itab),': ',INP.table(itab).sname])
+         end
+         TAB = reshape(TAB,size(INP.table)); % for 2D TAB arrays
+         varargout = {TAB};
+         return
+      %% Proceed SWAN_IO_TABLE for single table
+      %-------------------------
+      else
+         INP.table.mmax = INP.table.mxc + 1;
+         INP.table.nmax = INP.table.myc + 1;
       end
+      
+   elseif ischar(varargin{1})
+      INP.table.fname = varargin{1};
+
+      %% column names
+      %-------------------------
+      if nargin>1
+   
+          %-% REMOVED DELFTD_WAVE DEFAULT:
+          %-% %% Define default (Delft3D) Column parameter names
+          %-% %-------------------------
+          %-% 
+          %-% INP.table.parameter.nfields     = ones(18,1);
+          %-% INP.table.parameter.nfields( 5) = 2;
+          %-% INP.table.parameter.nfields( 6) = 2;
+          %-% INP.table.parameter.nfields(17) = 2;
+          %-% 
+          %-%                                             %  parameter
+          %-%                                             %     columns
+          %-%                                             %          dimensions (vector vs. scalar)
+          %-%                                             % --------------------------------
+          %-% INP.table.parameter.names  = {'HS     ',... %  1  1    1
+          %-%                               'DIR    ',... %  2  2    1
+          %-%                               'TM01   ',... %  3  3    1
+          %-%                               'DEP    ',... %  4  4    1
+          %-%                               'VEL    ',... %  5  5- 6 2
+          %-%                               'TRA    ',... %  6  7- 8 2
+          %-%                               'DSPR   ',... %  7  9    1
+          %-%                               'DISS   ',... %  8 10    1
+          %-%                               'LEAK   ',... %  9 11    1
+          %-%                               'QB     ',... % 10 12    1
+          %-%                               'XP     ',... % 11 13    1
+          %-%                               'YP     ',... % 12 14    1
+          %-%                               'DIST   ',... % 13 15    1
+          %-%                               'UBOT   ',... % 14 16    1
+          %-%                               'STEEP  ',... % 15 17    1
+          %-%                               'WLEN   ',... % 16 18    1
+          %-%                               'FOR    ',... % 17 19-20 2
+          %-%                               'RTP    ',... % 18 21    1
+          %-%                               'PDIR   '};   % 19 22    1
+          
+         INP.table.parameter.names  = upper(varargin{2});
+         if ~iscell(INP.table.parameter.names)
+            INP.table.parameter.names = strtokens2cell(deblank(INP.table.parameter.names));
+         end
+         INP.table.parameter.nfields = ones(length(INP.table.parameter.names),1);
+         
+         %% for all vectors define 2 columns
+         %-------------------------
+         
+         for ifield=1:length(INP.table.parameter.nfields)
+            
+            fldname = char(deblank(INP.table.parameter.names{ifield}));
+            
+            if length(fldname) > 5
+               fldname = fldname(1:5);
+            end
+            
+            fldname = deblank(fldname);
+            
+            if strcmp(upper(fldname),'VEL') | ...
+               strcmp(upper(fldname),'TRA') | ...
+               strcmp(upper(fldname),'WIND' ) | ...
+               strcmp(upper(fldname),'FOR')
+      
+                INP.table.parameter.nfields(ifield) = 2;  
+      
+            end
+            
+         end % for ifield=1:length(shape.nfields)
+         
+      end % if  nargin>2 
+
+      %% reshape size
+      %-------------------------
+      if nargin>2
+         if ~isempty(varargin{1})
+            INP.table.mmax = varargin{1}(1);
+            INP.table.nmax = varargin{1}(2);
+         end
+      end
+      if nargin>3
+         error('syntax: dep = swan_TABLE(filename,<mxyc,fieldnames>)')
+      end
+      
    end
    
-   shape.nfields     = ones(18,1);
-   shape.nfields( 5) = 2;
-   shape.nfields( 6) = 2;
-   shape.nfields(17) = 2;
+%% Load full raw matrix
+%-------------------------
+
+   dat = load(INP.table.fname); %load(TAB.fname);
    
-                                 %  paramter
-                                 %     columns
-                                 %          dimensions (vector vs. scalar)
-                                 % --------------------------------
-   shape.names  = {'HS     ',... %  1  1    1
-                   'DIR    ',... %  2  2    1
-                   'TM01   ',... %  3  3    1
-                   'DEP    ',... %  4  4    1
-                   'VEL    ',... %  5  5- 6 2
-                   'TRA    ',... %  6  7- 8 2
-                   'DSPR   ',... %  7  9    1
-                   'DISS   ',... %  8 10    1
-                   'LEAK   ',... %  9 11    1
-                   'QB     ',... % 10 12    1
-                   'XP     ',... % 11 13    1
-                   'YP     ',... % 12 14    1
-                   'DIST   ',... % 13 15    1
-                   'UBOT   ',... % 14 16    1
-                   'STEEP  ',... % 15 17    1
-                   'WLEN   ',... % 16 18    1
-                   'FOR    ',... % 17 19-20 2
-                   'RTP    ',... % 18 21    1
-                   'PDIR   '};   % 19 22    1
-   
-   
-   if nargin>2
-      shape.names  = upper(varargin{2});
-      if ~iscell(shape.names)
-         shape.names = strtokens2cell(deblank(shape.names));
-      end
-      shape.nfields = ones(length(shape.names),1);
+%%  split into scalar/vector columns and give names
+%-------------------------
+
+   if isfield(INP.table,'parameter')
+   for ifield=1:length(INP.table.parameter.nfields)
+
+      ndata   = INP.table.parameter.nfields(ifield); % 1 or 2
+      fldname = char(deblank(INP.table.parameter.names{ifield}));
       
-      %% for all vectors define 2 columns
-      %% -----------------------
-      for ifield=1:length(shape.nfields)
-         
-         fldname = char(deblank(shape.names{ifield}));
-         
-         if length(fldname) > 5
-            fldname = fldname(1:5);
-         end
-         
-         fldname = deblank(fldname);
-         
-         if strcmp(upper(fldname),'VEL') | ...
-            strcmp(upper(fldname),'TRA') | ...
-            strcmp(upper(fldname),'WIND' ) | ...
-            strcmp(upper(fldname),'FOR')
+      % ['ifield:',num2str(ifield),' ndata:',num2str(ndata),' fldname:',fldname]
 
-             shape.nfields(ifield) = 2;  
+      for idata = 1:INP.table.parameter.nfields(ifield)
+         columns = sum(INP.table.parameter.nfields(1:ifield-1)) + idata;
+         data    = dat(:,columns);
 
-         end
+         %  reshape 1D column to proper 2D matrix (if mxc,myc provided)
+         %-------------------------
          
-      end % for ifield=1:length(shape.nfields)
-      
-   end % if  nargin>2 
-   
-   if nargin>3
-      error('syntax: dep = swan_TABLE(filename,<mnmax,fieldnames>)')
-   end
-
-   dat = load(fname); %load(fname);
-   
-   for ifield=1:length(shape.nfields)
-
-      ndata   = shape.nfields(ifield); % 1 or 2
-      fldname = char(deblank(shape.names{ifield}));
-
-      for idata = 1:shape.nfields(ifield)
-         columns = sum(shape.nfields(1:ifield-1)) + idata;
-         data                      = dat(:,columns);
-         if ~isempty(mmax)
-            swan.(fldname)(:,:,idata) = reshape(data,[mmax nmax]);
+         if ~isempty(INP.table.mmax) & ...
+            ~isempty(INP.table.nmax)
+            TAB.(fldname)(:,:,idata) = reshape(data,[INP.table.mmax INP.table.nmax]);
          else
-            swan.(fldname)(:,idata) = data;
+            TAB.(fldname)(:,idata  ) = data;
          end
       end
 
    end
-
-   % TABLE 'COMPGRID' NOHEAD 'SWANOUT'  _                                 
-   %  HSIGN    DIR      TM01     DEPTH    VELOC    TRANSP _               
-   %  DSPR     DISSIP   LEAK     QB       XP       YP     _               
-   %  DIST     UBOT     STEEPW   WLENGTH  FORCES   RTP    _               
-   
-   %HSIGN    >> (checked succesfully with wgxy)
-   %DIR      
-   %TM01     
-   %DEPTH    >> (checked succesfully)  
-   %VELOC    (vector)
-   %TRANSP   (vector)     
-   %DSPR   
-   %DISSIP   
-   %LEAK     
-   %QB       
-   %XP       
-   %YP                   
-   %DIST     
-   %UBOT     
-   %STEEPW   
-   %WLENGTH  >> (checked succesfully with wgxy)
-   %FORCES   (vector)
-   %RTP      >> (checked succesfully with wgxy)
-   %PDIR
+   varargout = {TAB};
+   else
+   varargout = {dat};
+   end % if isfield(INP.table,'parameter')
 
 %% EOF
