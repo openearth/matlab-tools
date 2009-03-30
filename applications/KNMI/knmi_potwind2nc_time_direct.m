38476bc86abda5c667082e99a69384a399ba1bf6
boer_g <boer_g@localhost>
2009-03-30 16:39:01 +0000

    loop over multiple files, added comment to standard names, put SVN keyword in nc history.
    svn path=/trunk/matlab/; revision=310
    
    
    Former-commit-id: 8afa6f353c4e08577739f2b8b3a27467b89fe705

--- applications/KNMI/knmi_potwind2nc_time_direct.m
+++ applications/KNMI/knmi_potwind2nc_time_direct.m
@@ -1,3 +1,4 @@
+% function knmi_potwind2nc_time_direct(varargin)
 %KNMI_POTWIND2NC_TIME_DIRECT  This is a first test to get wind timeseries into NetCDF
 %
 %  Timeseries data, see example:
@@ -12,16 +13,31 @@ try
    rmpath('Y:\app\matlab\toolbox\wl_mexnc\')
 end   
 
-OPT.fillvalue = 0; % NaNs do not work in netcdf API
+%% Initialize
+%------------------
+
+   OPT.fillvalue     = 0; % NaNs do not work in netcdf API
+   OPT.dump          = 0;
+   OPT.directory.raw = 'F:\checkouts\OpenEarthRawData\knmi\potwind\raw\';
+   OPT.directory.nc  = 'F:\checkouts\OpenEarthRawData\knmi\potwind\nc\';
+   OPT.files         = dir([OPT.directory.raw filesep 'potwind*']);
+   
+for ifile=1:length(OPT.files)  
+
+   disp(['Processing ',num2str(ifile),'/',num2str(length(OPT.files)  )])
+
+   OPT.filename = [OPT.directory.raw, filesep, OPT.files(ifile).name]; % e.g. 'potwind_210_1981'
 
 %% 0 Read raw data
 %------------------
-   D             = knmi_potwind('potwind_210_1981','variables',OPT.fillvalue);
+
+   D             = knmi_potwind(OPT.filename,'variables',OPT.fillvalue);
 
 %% 1a Create file
 %------------------
 
-   outputfile    = [filename(D.filename),'_time_direct.nc'];
+   outputfile    = [OPT.directory.nc filesep  filename(D.filename),'_time_direct.nc'];
+   
    nc_create_empty (outputfile)
 
    %% Add overall meta info
@@ -31,13 +47,13 @@ OPT.fillvalue = 0; % NaNs do not work in netcdf API
    nc_attput(outputfile, nc_global, 'title'        , '');
    nc_attput(outputfile, nc_global, 'institution'  , 'KNMI');
    nc_attput(outputfile, nc_global, 'source'       , 'surface observation');
-   nc_attput(outputfile, nc_global, 'history'      , ['Tranformed to NetCDF by G.J. de Boer <g.j.deboer@deltares.nl>, ',datestr(now,31),' by: ',mfilename]);
+   nc_attput(outputfile, nc_global, 'history'      , ['Original filename: ',D.filename,' version:',D.version,...
+                                                      ' tranformed to NetCDF by G.J. de Boer <g.j.deboer@deltares.nl>, ',datestr(now,31),' by: $HeadURL$ $Revision$ $Date$ $Author$']);
    nc_attput(outputfile, nc_global, 'references'   , '<http://www.knmi.nl/samenw/hydra>,<http://www.knmi.nl/klimatologie/onderzoeksgegevens/potentiele_wind/>');
    nc_attput(outputfile, nc_global, 'comment'      , '');
+   nc_attput(outputfile, nc_global, 'version'      , D.version);
 						   
    nc_attput(outputfile, nc_global, 'Conventions'  , 'CF-1.4');
-						   
-   nc_attput(outputfile, nc_global, 'history'      , ['original filename: ',D.filename,' version:',D.version]);
    
    nc_attput(outputfile, nc_global, 'stationnumber', D.stationnumber);
    nc_attput(outputfile, nc_global, 'stationname'  , D.stationname);
@@ -61,8 +77,9 @@ OPT.fillvalue = 0; % NaNs do not work in netcdf API
    %  http://cf-pcmdi.llnl.gov/documents/cf-standard-names/standard-name-table/current/standard-name-table
    %------------------
 
-  
+   %% Longitude
    % http://cf-pcmdi.llnl.gov/documents/cf-conventions/1.4/cf-conventions.html#longitude-coordinate
+   %------------------
    
    nc.lon = struct(...
    'Name'     , 'lon', ...
@@ -72,7 +89,9 @@ OPT.fillvalue = 0; % NaNs do not work in netcdf API
    nc.lon.Attribute(2) = struct('Name', 'units'          ,'Value', 'degrees_east');
    nc.lon.Attribute(3) = struct('Name', 'standard_name'  ,'Value', 'longitude');
     
+   %% Latitude
    % http://cf-pcmdi.llnl.gov/documents/cf-conventions/1.4/cf-conventions.html#latitude-coordinate
+   %------------------
    
    nc.lat = struct(...
    'Name'     , 'lat', ...
@@ -82,12 +101,14 @@ OPT.fillvalue = 0; % NaNs do not work in netcdf API
    nc.lat.Attribute(2) = struct('Name', 'units'          ,'Value', 'degrees_north');
    nc.lat.Attribute(3) = struct('Name', 'standard_name'  ,'Value', 'latitude');
 
+   %% Time
    % http://cf-pcmdi.llnl.gov/documents/cf-conventions/1.4/cf-conventions.html#time-coordinate
-   % time is a dimension, so there arw two options:
+   % time is a dimension, so there are two options:
    % * the variable name needs the same as the dimension
    %   http://cf-pcmdi.llnl.gov/documents/cf-conventions/1.4/cf-conventions.html#id2984551
    % * there needs to be an indirect mapping through the coordinates attribute
    %   http://cf-pcmdi.llnl.gov/documents/cf-conventions/1.4/cf-conventions.html#id2984605
+   %------------------
    
    nc.time = struct(...
        'Name'     , 'time', ...
@@ -97,6 +118,10 @@ OPT.fillvalue = 0; % NaNs do not work in netcdf API
    nc.time.Attribute(2) = struct('Name', 'units'          ,'Value', 'days since 0000-1-1 0:0:0'); % matlab datenumber convention
    nc.time.Attribute(3) = struct('Name', 'standard_name'  ,'Value', 'time');
    nc.time.Attribute(4) = struct('Name', '_FillValue'     ,'Value', OPT.fillvalue);
+   
+   %% Parameter
+   % * http://cf-pcmdi.llnl.gov/documents/cf-standard-names/standard-name-table/current/
+   %------------------
 
    nc.wind_speed = struct(...
        'Name'     , 'wind_speed', ...
@@ -107,7 +132,7 @@ OPT.fillvalue = 0; % NaNs do not work in netcdf API
    nc.wind_speed.Attribute(3) = struct('Name', 'standard_name'  ,'Value', 'wind_speed');
    nc.wind_speed.Attribute(4) = struct('Name', '_FillValue'     ,'Value', OPT.fillvalue);
 
-   nc.wind_to_direction = struct(...
+   nc.wind_to_direction = struct(... % wind_from_direction
        'Name'     , 'wind_to_direction', ...
        'Nctype'   , 'float', ...
        'Dimension', {{'time'}});
@@ -143,6 +168,10 @@ OPT.fillvalue = 0; % NaNs do not work in netcdf API
 %% 6 Check
 %------------------
 
+   if OPT.dump
    nc_dump(outputfile);
-
+   end
+   
+end %for ifile=1:length(OPT.files)   
+   
 %% EOF
