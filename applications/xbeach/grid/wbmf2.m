eddde1258311b2fb6749d48c30869ccbbed2459e
m.vankoningsveld@tudelft.nl <m.vankoningsveld@tudelft.nl@localhost>
2009-04-21 05:41:53 +0000

    Code commented and get(gcf,'UserData') replaced by global for speed
    svn path=/trunk/matlab/; revision=380
    
    
    Former-commit-id: ee448c584c7df45b81af4eda256fb9f51a9474c1

--- applications/xbeach/grid/wbmf2.m
+++ applications/xbeach/grid/wbmf2.m
@@ -1,23 +1,51 @@
 function wbmf2
-%window motion callback function
+% window motion callback function
+% - this function will be called each time the mouse moves
 
-utemp=get(gcf,'UserData')
-x1=utemp.p1(1);y1=utemp.p1(2);
-x2=utemp.p2(1);y2=utemp.p2(2);
-alfa=atan2(y2-y1,x2-x1);
-sina=sin(alfa);cosa=cos(alfa)
-p3=get(utemp.h,'CurrentPoint');
-x=p3(1,1);y=p3(1,2);
-yn=-sina*(x-x1)+cosa*(y-y1);
-x3=x2-yn*sina;
-y3=y2+yn*cosa;
-x4=x1-yn*sina;
-y4=y1+yn*cosa;
-xi=[x1,x2,x3,x,x4,x1];
-yi=[y1,y2,y3,y,y4,y1];
-set(utemp.lh,'XData',xi, ...
+% Created by Sandra Martinka March 2002
+% Modified by Dano Roelvink Feb 2009
+% Commented by Mark van Koningsveld April 2009
+
+% $Id$
+% $Date$
+% $Author$
+% $Revision$
+% $HeadURL$
+% $Keywords: $
+
+global udata
+
+%% get x1, y1 and x2, y2
+x1       = udata.p1(1);
+y1       = udata.p1(2);
+x2       = udata.p2(1);
+y2       = udata.p2(2);
+
+%% determine angle
+alfa     = atan2(y2-y1,x2-x1);
+sina     = sin(alfa);
+cosa     = cos(alfa);
+
+%%
+p3       = get(udata.h,'CurrentPoint');
+x        = p3(1,1);
+y        = p3(1,2);
+yn       = -sina*(x-x1)+cosa*(y-y1);
+
+%% get x3, y3 and x4, y4
+x3       = x2-yn*sina;
+y3       = y2+yn*cosa;
+x4       = x1-yn*sina;
+y4       = y1+yn*cosa;
+
+%% collect information in xi and yi
+xi       = [x1,x2,x3,x,x4,x1];
+yi       = [y1,y2,y3,y,y4,y1];
+
+%% set XData and YData on linehandle lh
+set(udata.lh,'XData',xi, ...
              'YData',yi);
-utemp.xi=xi;
-utemp.yi=yi;
-set(gcf,'UserData',utemp);
 
+%% store lineinfo in userdata of current figure
+udata.xi = xi;
+udata.yi = yi;
