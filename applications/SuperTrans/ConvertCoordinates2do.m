3c616faf194b5a6f7ef0176c947ad49c563d87a8
boer_g <boer_g@localhost>
2009-06-02 13:13:13 +0000

    added to do list
    svn path=/trunk/matlab/; revision=486
    
    
    Former-commit-id: d8422679c21b8e1ce95eccb5ee267e96ad604e93

---
+++ applications/SuperTrans/ConvertCoordinates2do.m
@@ -0,0 +1,7 @@
+% TO DO make scripts that transform svn files 2 mat files OK (i.e. batchable) to work with latest snv files from gdal
+% TO DO make script have option to choose sequence number of datumtransformation
+% TO DO think of a system (and make it if agreeable) to add user-defined coordinates
+% TO DO make groups of coordinates systems by region (with info from svn files)
+% TO DO make mex files for extra datumtransformatons
+% TO DO defines some aliases to have more easy access to epsg codes
+% TO DO find out whether everything can be in meters (no feet)
