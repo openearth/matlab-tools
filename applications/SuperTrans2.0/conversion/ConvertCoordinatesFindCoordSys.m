0bb06f0d9b50327762b83a3b5bc85b38ef5c8599
boer_g <boer_g@localhost>
2009-06-12 08:33:27 +0000

    added function headers
    svn path=/trunk/matlab/; revision=515
    
    
    Former-commit-id: 20ac1b5b27699ecac17b1de08a40fef28dc5de68

--- applications/SuperTrans2.0/conversion/ConvertCoordinatesFindCoordSys.m
+++ applications/SuperTrans2.0/conversion/ConvertCoordinatesFindCoordSys.m
@@ -1,4 +1,38 @@
 function CS = ConvertCoordinatesFindCoordSys(CS,STD)
+%CONVERTCOORDINATESFINDCOORDSYS .
+
+%   --------------------------------------------------------------------
+%   Copyright (C) 2009 Deltares for Building with Nature
+%       Thijs Damsma
+%
+%       Thijs.Damsma@deltares.nl	
+%
+%       Deltares
+%       P.O. Box 177
+%       2600 MH Delft
+%       The Netherlands
+%
+%   This library is free software: you can redistribute it and/or modify
+%   it under the terms of the GNU General Public License as published by
+%   the Free Software Foundation, either version 3 of the License, or
+%   (at your option) any later version.
+%
+%   This library is distributed in the hope that it will be useful,
+%   but WITHOUT ANY WARRANTY; without even the implied warranty of
+%   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%   GNU General Public License for more details.
+%
+%   You should have received a copy of the GNU General Public License
+%   along with this library.  If not, see <http://www.gnu.org/licenses/>.
+%   --------------------------------------------------------------------
+
+% $Id$
+% $Date$
+% $Author$
+% $Revision$
+% $HeadURL$
+% $Keywords: $
+
 ind1             = find(STD.coordinate_reference_system.coord_ref_sys_code == CS.code);
 CS.coordSys.code =      STD.coordinate_reference_system.coord_sys_code(ind1); %#ok<FNDSB>
 ind2             = find(STD.coordinate_system.coord_sys_code == CS.coordSys.code);
--- applications/SuperTrans2.0/conversion/ConvertCoordinatesFindDatumTransOpt.m
+++ applications/SuperTrans2.0/conversion/ConvertCoordinatesFindDatumTransOpt.m
@@ -1,4 +1,38 @@
 function [ OPT ] = ConvertCoordinatesFindDatumTransOpt(OPT,STD)
+%CONVERTCOORDINATESFINDDATUMTRANSOPT .
+
+%   --------------------------------------------------------------------
+%   Copyright (C) 2009 Deltares for Building with Nature
+%       Thijs Damsma
+%
+%       Thijs.Damsma@deltares.nl	
+%
+%       Deltares
+%       P.O. Box 177
+%       2600 MH Delft
+%       The Netherlands
+%
+%   This library is free software: you can redistribute it and/or modify
+%   it under the terms of the GNU General Public License as published by
+%   the Free Software Foundation, either version 3 of the License, or
+%   (at your option) any later version.
+%
+%   This library is distributed in the hope that it will be useful,
+%   but WITHOUT ANY WARRANTY; without even the implied warranty of
+%   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%   GNU General Public License for more details.
+%
+%   You should have received a copy of the GNU General Public License
+%   along with this library.  If not, see <http://www.gnu.org/licenses/>.
+%   --------------------------------------------------------------------
+
+% $Id$
+% $Date$
+% $Author$
+% $Revision$
+% $HeadURL$
+% $Keywords: $
+
 if OPT.CS1.geoRefSys.code == OPT.CS2.geoRefSys.code
     OPT.datum_trans1 = 'no datum transformation needed';
 else
