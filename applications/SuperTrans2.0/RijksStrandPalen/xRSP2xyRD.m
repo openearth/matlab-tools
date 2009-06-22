5689c263e85d673c3bb939f35fec43ac613dd3e6
damsma <damsma@localhost>
2009-06-22 12:17:40 +0000

    added functionality on request of PvG, now works both as old and new version.
    svn path=/trunk/matlab/; revision=571
    
    
    Former-commit-id: 9ccf7467892c9b095acf8eb3b553fcb6e901abf7

--- applications/SuperTrans2.0/RijksStrandPalen/xRSP2xyRD.m
+++ applications/SuperTrans2.0/RijksStrandPalen/xRSP2xyRD.m
@@ -1,12 +1,23 @@
-function [xRD,yRD] = xRSP2xyRD(xRSP,section,transectNr)
+function [xRD,yRD] = xRSP2xyRD(varargin)
 %XRSP2XYRD   transform RijksStrandPalen coordinates to RD coordinates
+%    
+% depending on inputs arguments, function will be:
+%    nargin = 3: [xRD,yRD] = xRSP2xyRD(xRSP,section,transectNr);
+%    nargin = 4: [xRD,yRD] = xRSP2xyRD(x0,y0,alpha,xRSP); 
 %
-%    [xRD,yRD] = xRSP2xyRD(xRSP,section,transectNr)
-%
+%   [xRD,yRD] = xRSP2xyRD(xRSP,section,transectNr)
 %   section and transectNr can be either single values, or arrays of the
-%   same length as xRD
+%   same length as xRSP
 %
-%See also: convertCoordinatesNew
+%   [xRD,yRD] = xRSP2xyRD(x0,y0,alpha,xRSP)
+%   x0/y0: zero point coordinates of transect; can be either single values,
+%          or arrays of the same length as xRSP.
+%   alpha: orientation of the transect (direction of positive xRSP). Can be
+%          either single values, or array of the same length as xRSP. 
+%          (!) Beware of certain old transects where alpha is based on a 
+%          (!) 400 degree system in stead of 'normal' 360 degrees
+%
+% See also: convertCoordinatesNew
 
 %   --------------------------------------------------------------------
 %   Copyright (C) 2009 Deltares for Building with Nature
@@ -40,25 +51,42 @@ function [xRD,yRD] = xRSP2xyRD(xRSP,section,transectNr)
 % $HeadURL$
 % $Keywords: $
 
-%% load raai data
-fid = fopen('raaien.txt', 'r');
-data = textscan(fid, '%n %n %n %n %n', 'headerlines', 1);
-fclose(fid);
+%% get data from database
+if nargin == 3;
+    
+    %assign varargin
+    xRSP        = varargin{1}; 
+    section     = varargin{2};
+    transectNr  = varargin{3};
+    
+    % load raai data
+    fid = fopen('raaien.txt', 'r');
+    data = textscan(fid, '%n %n %n %n %n', 'headerlines', 1);
+    fclose(fid);
 
-
-%% loop through data
-for ii = 1:length(transectNr)
-    try
-        ind(ii) = find(data{:,1}== section(ii)&data{:,2}==transectNr(ii)*10);
-    catch
-        error('could not convert section %d, transect number %d', section(ii), transectNr(ii))
+    % loop through data
+    for ii = 1:length(transectNr)
+        try
+            ind(ii) = find(data{:,1}== section(ii)&data{:,2}==transectNr(ii)*10);
+        catch
+            error('could not convert section %d, transect number %d', section(ii), transectNr(ii))
+        end
     end
+
+    % assign variables
+    alpha = data{5}(ind)/180*pi/100;
+    x0 = data{3}(ind)/100;
+    y0 = data{4}(ind)/100;
+else
+    %assign varargin
+    x0      = varargin{1}; 
+    y0      = varargin{2};
+    alpha   = varargin{3};
+    xRSP    = varargin{4};
 end
 
 %% convert coordinates
-alpha = data{5}(ind)/180*pi/100;
-x0 = data{3}(ind)/100;
-y0 = data{4}(ind)/100;
+
 xRD = x0 + xRSP.*sin(alpha);
 yRD = y0 + xRSP.*cos(alpha);
 end
