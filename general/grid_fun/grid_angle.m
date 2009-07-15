7996bfa1aea6b8540861c98050a842e999de3381
boer_g <boer_g@localhost>
2009-07-15 16:35:56 +0000

    Added corner option to grid_angle, adapted test case to check for deformation on all sides and rotation for all quadrants.
    svn path=/trunk/matlab/; revision=691
    
    
    Former-commit-id: 85b4dd7a5286c5b6aeeef55b78cb4450baf579c3

--- general/grid_fun/grid_angle.m
+++ general/grid_fun/grid_angle.m
@@ -1,25 +1,32 @@
 function TH = grid_angle(varargin)
-%GRID_ANGLE  get angle orientation at centers of grid
+%GRID_ANGLE  get local angle (orientation,slope) of all grid nodes
 %
 %    TH = grid_angle(x,y,<keyword,value>) 
 %
-% gives the grid cell orientation TH defined as the 
+% gives cell orientation TH for each grid cell defined as the 
 % angle in radians between the m axis (1st dimension, say x-ish) 
-% and the n-axis (2nd dimension, say y-ish) of the center points. 
+% and the n-axis (2nd dimension, say y-ish) of the grid points.
+%
+% TH is defined in radians (positive anti-clockwise, and 
+% zero for regular straight-up cube) [x,y]=meshgrid(0:1,0:1)
+%
+% TH is calculated as the average angle of the four lines that 
+% span a quadrangle around the grid centres (location='cen') or the
+% average angle of the 2 lines that cross a node (location='cor').
+% So there is no need for the grid(mesh) to be orthogonal.
 %
 % The following <keyword,value> pairs have been implemented.
 % * dim       which dimension to use as (1st dimension, say x-ish)
-%             default 1. Setting dim=2 does: x=x', y'y'.
+%             default 1 (appropriate for NDGRID). Setting dim=2 
+%             does: x=x', y'y' (i.e. appropriate for MESHGRID)
+% * location 'cen<ter>' (default) or 'cor<ner>' determines
+%             whether TH is defines at the cell centers  
+%             (one smaller in both dimension) or at the coners 
+%             (TH same size as x and y)
 %
-% Works well for grids created with NDGRID, not with MESH_GRID.
+% Works well for grids created with NDGRID, and not with MESH_GRID.
 %
 % See also: GRID_FUN, NDGRID
-
-% TO DO
-% * location 'cen<ter>' (default) or 'cor<ner>' determines
-%             whether TH is defines at teh cell centers (one 
-%             smaller in both dimension) or at the coners (same
-%             size as x and y)
 
 %% Copyright notice
 %   --------------------------------------------------------------------
@@ -49,9 +56,6 @@ function TH = grid_angle(varargin)
 %   --------------------------------------------------------------------
 
 %% Version <http://svnbook.red-bean.com/en/1.5/svn.advanced.props.special.keywords.html>
-% Created: 14 Jul 2009
-% Created with Matlab version: 7.5.0.342 (R2007b)
-
 % $Id$
 % $Date$
 % $Author$
@@ -76,7 +80,7 @@ function TH = grid_angle(varargin)
      cor.x = cor.x';
      cor.y = cor.y';
    end
-
+   
 %% Calculate
 
    if strcmpi(OPT.location(1:3),'cen')
@@ -91,43 +95,55 @@ function TH = grid_angle(varargin)
       
       TH = repmat(nan,[size(cor.x,1)-1,size(cor.x,2)-1,4]);
       
+      % left                                 top     LEFT             bottom  LEFT
       cen.dxdksi                  =  + cor.x(2:end  ,1:end-1) - cor.x(1:end-1,1:end-1);
       cen.dydksi                  =  + cor.y(2:end  ,1:end-1) - cor.y(1:end-1,1:end-1);
-      TH(:,:,1) = atan2(cen.dydksi, cen.dxdksi);
+      TH(:,:,1) =  atan2(cen.dydksi, cen.dxdksi);
       
+      % top                                  TOP     right            TOP     left
       cen.dxdksi                  =  + cor.y(2:end  ,2:end  ) - cor.y(2:end  ,1:end-1);
       cen.dydksi                  =  + cor.x(2:end  ,2:end  ) - cor.x(2:end  ,1:end-1);
       TH(:,:,2) = -atan2(cen.dydksi, cen.dxdksi);
       
+      % right                                top     RIGHT            bottom  RIGHT
       cen.dxdksi                  =  + cor.x(2:end  ,2:end  ) - cor.x(1:end-1,2:end  );
       cen.dydksi                  =  + cor.y(2:end  ,2:end  ) - cor.y(1:end-1,2:end  );
-      TH(:,:,3) = atan2(cen.dydksi, cen.dxdksi);
+      TH(:,:,3) =  atan2(cen.dydksi, cen.dxdksi);
       
+      % bottom                               BOTTOM  right            BOTTOM  left
       cen.dxdksi                  =  + cor.y(1:end-1,2:end  ) - cor.y(1:end-1,1:end-1);
       cen.dydksi                  =  + cor.x(1:end-1,2:end  ) - cor.x(1:end-1,1:end-1);
       TH(:,:,4) = -atan2(cen.dydksi, cen.dxdksi);
       
       if OPT.debug
-      rad2deg(TH(:))
+      rad2deg(TH)
       end
       
       TH = mean(TH,3);
 
    elseif strcmpi(OPT.location(1:3),'cor')
    
-   error('GRID_ANGLE not tested yet for corner')
-      
+      TH = repmat(nan,[size(cor.x,1),size(cor.x,2),2]);
+
       cor.dxdksi = repmat(nan,size(cor.x));
       cor.dydksi = repmat(nan,size(cor.x));
-      
-      cor.dxdksi(2:end-1,2:end-1) = (+ cor.x(3:end  ,2:end-1) - cor.x(1:end-2,2:end-1));
-      cor.dydksi(2:end-1,2:end-1) = (+ cor.y(3:end  ,2:end-1) - cor.y(1:end-2,2:end-1));
-      
-      cor.dxdksi(2:end-1,2:end-1) = (+ cor.x(3:end  ,2:end-1) - cor.x(1:end-2,2:end-1));
-      cor.dydksi(2:end-1,2:end-1) = (+ cor.y(3:end  ,2:end-1) - cor.y(1:end-2,2:end-1));
 
-      TH   = atan2(cor.dydksi, cor.dxdksi);
-   
+      % right-left                           right   center           left    center
+      cor.dxdksi(2:end-1,2:end-1) = (+ cor.x(3:end  ,2:end-1) - cor.x(1:end-2,2:end-1));
+      cor.dydksi(2:end-1,2:end-1) = (+ cor.y(3:end  ,2:end-1) - cor.y(1:end-2,2:end-1));
+      TH(:,:,1) =  atan2(cor.dydksi, cor.dxdksi);
+      
+      % top-bottom                           center  top              center  bottom
+      cor.dxdksi(2:end-1,2:end-1) = (+ cor.y(2:end-1,3:end  ) - cor.y(2:end-1,1:end-2));
+      cor.dydksi(2:end-1,2:end-1) = (+ cor.x(2:end-1,3:end  ) - cor.x(2:end-1,1:end-2));
+      TH(:,:,2) = -atan2(cor.dydksi, cor.dxdksi);
+
+      if OPT.debug
+      rad2deg(TH)
+      end
+      
+      TH = mean(TH,3);
+
    end
 
 %% EOF
--- general/grid_fun/grid_angle_test.m
+++ general/grid_fun/grid_angle_test.m
@@ -1,103 +1,142 @@
 function grid_angle_test(varargin)
 %GRID_ANGLE_TEST  test for GRID_ANGLE
+%
+% Checks correct rotation matrix:
+% for all quadrants
+% for centers and corners
+% for otrhogonal and deformed meshes
+%
+%See also: 
 
-   OPT.TH = deg2rad(30+90+90+90);
+%% Copyright notice
+%   --------------------------------------------------------------------
+%   Copyright (C) 2009 Deltares
+%       Gerben de Boer
+%
+%       gerben.deboer@deltares.nl	
+%
+%       Deltares
+%       P.O. Box 177
+%       2600 MH Delft
+%       The Netherlands
+%
+%   This library is free software: you can redistribute it and/or
+%   modify it under the terms of the GNU Lesser General Public
+%   License as published by the Free Software Foundation, either
+%   version 2.1 of the License, or (at your option) any later version.
+%
+%   This library is distributed in the hope that it will be useful,
+%   but WITHOUT ANY WARRANTY; without even the implied warranty of
+%   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+%   Lesser General Public License for more details.
+%
+%   You should have received a copy of the GNU Lesser General Public
+%   License along with this library. If not, see <http://www.gnu.org/licenses/>.
+%   --------------------------------------------------------------------
 
-%% 0 degree
+%% Version <http://svnbook.red-bean.com/en/1.5/svn.advanced.props.special.keywords.html>
+% $Id$
+% $Date$
+% $Author$
+% $Revision$
+% $HeadURL$
+% $Keywords: $
 
-  [cor(1).x,cor(1).y] = ndgrid([1:4]   ,[1:4]   );
-  [cen(1).x,cen(1).y] = ndgrid([1:3]+.5,[1:3]+.5);
+   OPT.TH  = 30;
+   OPT.THs = deg2rad(OPT.TH+[0 90 180 270 360]);
+
+%% ROTATION TESTS   
+
+clear cen cor
+
+   %% 0 degree
+
+   j = 1;
+  [cor(j).x,cor(j).y] = ndgrid([1:4]   ,[1:4]   );
+  [cen(j).x,cen(j).y] = ndgrid([1:3]+.5,[1:3]+.5);
   
-%% any degree
+   %% any degree
 
-   cor(2).x       = cos(OPT.TH)*cor(1).x - sin(OPT.TH)*cor(1).y;
-   cor(2).y       = sin(OPT.TH)*cor(1).x + cos(OPT.TH)*cor(1).y;
-
-   cen(2).x       = cos(OPT.TH)*cen(1).x - sin(OPT.TH)*cen(1).y;
-   cen(2).y       = sin(OPT.TH)*cen(1).x + cos(OPT.TH)*cen(1).y;
-
-%% block
+   for ii=1:length(OPT.THs)
    
-   cor(3).x = [1 1;2 2];
-   cor(3).y = [1 2;1 2];
-   cen(3).x = mean(cor(3).x(:));
-   cen(3).y = mean(cor(3).y(:));
+   OPT.TH = OPT.THs(ii);
 
-%% deformed block (south -45)
+   j = j+1;
+   cor(j).x       = cos(OPT.TH)*cor(1).x - sin(OPT.TH)*cor(1).y;
+   cor(j).y       = sin(OPT.TH)*cor(1).x + cos(OPT.TH)*cor(1).y;
+
+   cen(j).x       = cos(OPT.TH)*cen(1).x - sin(OPT.TH)*cen(1).y;
+   cen(j).y       = sin(OPT.TH)*cen(1).x + cos(OPT.TH)*cen(1).y;
+   end
    
-   j = 4;
-   cor(j).x = [1 1;2 2];
-   cor(j).y = [1 2;0 2];
-   cen(j).x = mean(cor(j).x(:));
-   cen(j).y = mean(cor(j).y(:));
+   %% loop cases
 
-%% deformed block (east -45))
+   for j=1:length(cor)
    
-   j = 5;
-   cor(j).x = [1 1;2 3];
-   cor(j).y = [1 2;1 2];
-   cen(j).x = mean(cor(j).x(:));
-   cen(j).y = mean(cor(j).y(:));
+      figure('name',['ROTATION #',num2str(j,'%0.3d')])
+      grid_angle_test_plot(cor(j),cen(j));
+      
+   end   
 
-%% deformed block (north -45)
+%% DEFORMATION TESTS   
+
+clear cen cor
+
+   %% deformed block (south -45)
    
-   j = 6;
-   cor(j).x = [1 1;2 2];
-   cor(j).y = [1 3;1 2];
-   cen(j).x = mean(cor(j).x(:));
-   cen(j).y = mean(cor(j).y(:));
+   j = 1;
+   cor(j).x = [0 0 0;1 1 1; 2 2 2];
+   cor(j).y = [1 2 2;0 1 2;-1 0 2];
+   cen(j).x = corner2center(cor(j).x);
+   cen(j).y = corner2center(cor(j).y);
 
-%% deformed block (west -45)
+   %% deformed block (east -45))
    
-   j = 7;
-   cor(j).x = [0 1;2 2];
-   cor(j).y = [1 2;1 2];
-   cen(j).x = mean(cor(j).x(:));
-   cen(j).y = mean(cor(j).y(:));
+   j = j+1;
+   cor(j).x = [1 1 1;1 2 3;2 3 4];
+   cor(j).y = [1 2 3;1 2 3;1 2 3];
+   cen(j).x = corner2center(cor(j).x);
+   cen(j).y = corner2center(cor(j).y);
 
-%% titled block (45 deg)
+   %% deformed block (north -45)
    
-   j = 8;
-   cor(j).x = [1 0;2 1];
-   cor(j).y = [1 2;2 3];
-   cen(j).x = mean(cor(j).x(:));
-   cen(j).y = mean(cor(j).y(:));
+   j = j+1;
+   cor(j).x = [1 1 1;2 2 2;3 3 3];
+   cor(j).y = [1 3 4;1 2 3;1 1 2];
+   cen(j).x = corner2center(cor(j).x);
+   cen(j).y = corner2center(cor(j).y);
 
-%% rotated block
+   %% deformed block (west -45)
    
-   j = 9;
-   cor0.x = [1 1;2 2];
-   cor0.y = [1 2;1 2];
-   cen0.x = mean(cor0.x(:));
-   cen0.y = mean(cor0.y(:));
+   j = j+1;
+   cor(j).x = [-1 0 1;0 1 2;2 2 2];
+   cor(j).y = [ 1 2 3;1 2 3;1 2 3];
+   cen(j).x = corner2center(cor(j).x);
+   cen(j).y = corner2center(cor(j).y);
 
-   cor(j).x       = cos(OPT.TH)*cor0.x - sin(OPT.TH)*cor0.y;
-   cor(j).y       = sin(OPT.TH)*cor0.x + cos(OPT.TH)*cor0.y;
+   %% loop cases
 
-   cen(j).x       = cos(OPT.TH)*cen0.x - sin(OPT.TH)*cen0.y;
-   cen(j).y       = sin(OPT.TH)*cen0.x + cos(OPT.TH)*cen0.y;
-
-%% loop cases
-
-   for j=4:length(cor)
+   for j=1:length(cor)
    
-      grid_angle_test_plot(cor(j),cen(j))
+      figure('name',['DEFORMATION #',num2str(j,'%0.3d')])
+      grid_angle_test_plot(cor(j),cen(j));
       
    end   
 
 %%    
-function grid_angle_test_plot(cor,cen)   
+function grid_angle_test_plot(cor,cen);
 
    cen.rad      = grid_angle(cor.x,cor.y);
-  %cor.rad      = grid_angle(cor.x,cor.y,'location','cor');
+   cor.rad      = grid_angle(cor.x,cor.y,'location','cor');
    cen.deg      = rad2deg(cen.rad);
-  %cor.deg      = rad2deg(cor.rad);
+   cor.deg      = rad2deg(cor.rad);
    
-   figure
    pcolorcorcen(cor.x,cor.y,mod(cen.deg,360),[.5 .5 .5])
    hold on
    caxis([0 360])
    colorbarwithtitle('\theta [\circ]',[0:90:360])
    text(cen.x(:),cen.y(:),num2str(cen.deg(:) ),'color','w')
-  %text(cor.x(:),cor.y(:),num2str(cor.deg(:) ),'color','k')
+   text(cor.x(:),cor.y(:),num2str(cor.deg(:) ),'color','k')
    axis equal
+
+%% EOF
