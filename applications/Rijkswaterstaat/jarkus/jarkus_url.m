3b5780e6871404de674494cdfa137f039679d113
damsma <damsma@localhost>
2009-08-04 15:56:34 +0000

    more jarkus functions. Use url = jarkus_url, instead of url = http:\\dtvirt5.openearth.mctools.opendap.deltares.nl:80808080\rijkswaterstaat\grids\jarkus\transects\netcdf\hyrax\thredds\vaklodingen\dodc\tranestc.nc, or something
    also uses local (p:\...) link to transects.nc, if available
    
    
    svn path=/trunk/matlab/; revision=759
    
    
    Former-commit-id: 67f404f497866eda8e91cc1e5676715e80033dc9

---
+++ applications/Rijkswaterstaat/jarkus/jarkus_url.m
@@ -0,0 +1,58 @@
+function url = jarkus_url
+% JARKUS_URL returns the link to the jarkus netCDF. 
+%
+% Returns the link to the Jarkus netCDF file. If the JarKus netCDT is 
+% available locally on the Deltares network, this is returned, otherwise 
+% the internet link is returned
+% 
+% See also: nc_dump,nc_varget
+
+%% Copyright notice
+%   --------------------------------------------------------------------
+%   Copyright (C) 2009 <Deltares>
+%       Thijs Damsma
+%
+%       <Thijs.Damsma@Deltares.nl>	
+%
+%       Deltares
+%       P.O. Box 177
+%       2600 MH Delft
+%       The Netherlands
+%
+%   This library is free software: you can redistribute it and/or
+%   modify it under the terms of the GNU Lesser General Public
+%   License as published by the Free Software Foundation, either
+%   version 2.1 of the License, or (at your option) any later version.
+%
+%   This library is distributed in the hope that it will be useful,
+%   but WITHOUT ANY WARRANTY; without even the implied warranty of
+%   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+%   Lesser General Public License for more details.
+%
+%   You should have received a copy of the GNU Lesser General Public
+%   License along with this library. If not, see <http://www.gnu.org/licenses/>.
+%   --------------------------------------------------------------------
+
+% This tools is part of <a href="http://OpenEarth.Deltares.nl">OpenEarthTools</a>.
+% OpenEarthTools is an online collaboration to share and manage data and 
+% programming tools in an open source, version controlled environment.
+% Sign up to recieve regular updates of this function, and to contribute 
+% your own tools.
+
+%% Version <http://svnbook.red-bean.com/en/1.5/svn.advanced.props.special.keywords.html>
+% Created: 04 Aug 2009
+% Created with Matlab version: 7.5.0.342 (R2007b)
+
+% $Id$
+% $Date$
+% $Author$
+% $Revision$
+% $HeadURL$
+% $Keywords: $
+
+if exist(fullfile('P:','mcdata','opendap','rijkswaterstaat','jarkus','profiles','transect.nc'),'file')
+	url = fullfile('P:','mcdata','opendap','rijkswaterstaat','jarkus','profiles','transect.nc');
+else
+	url = 'http://opendap.deltares.nl:8080/opendap/rijkswaterstaat/jarkus/profiles/transect.nc';
+end
+
