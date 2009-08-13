ec43cb4079a4093ca344b41db070d631f9a74cc6
boer_g <boer_g@localhost>
2009-08-13 08:53:59 +0000

    small updates to kml overview of opendap timeseries
    svn path=/trunk/matlab/; revision=814
    
    
    Former-commit-id: 103e73da545510d523caaf62207f2f30e366d862

--- io/nctools/nc_cf_stationtimeseries2kmloverview_test.m
+++ io/nctools/nc_cf_stationtimeseries2kmloverview_test.m
@@ -5,11 +5,27 @@ function nc_cf_stationtimeseries2kmloverview_test
 
 clear OPT
 
-OPT.fileName    = 'concentration_of_suspended_matter_in_sea_water.kml';
-OPT.kmlName     = 'waterbase concentration_of_suspended_matter_in_sea_water';
-OPT.THREDDSbase = 'http://opendap.deltares.nl:8080/thredds/dodsC/opendap/rijkswaterstaat/waterbase/concentration_of_suspended_matter_in_sea_water/';
-OPT.HYRAXbase   = 'http://opendap.deltares.nl:8080/opendap/rijkswaterstaat/waterbase/concentration_of_suspended_matter_in_sea_water/';
-OPT.ftpbase     = 'http://opendap.deltares.nl:8080/thredds/fileServer/opendap/rijkswaterstaat/waterbase/concentration_of_suspended_matter_in_sea_water/';
-OPT.description = 'source: <a href="http://www.waterbase.nl">Rijkswaterstaat</a>';
+subdirs = {'concentration_of_chlorophyll_in_sea_water',...
+           'concentration_of_suspended_matter_in_sea_water',...
+           'sea_surface_height',...
+           'sea_surface_salinity',...
+           'sea_surface_temperature',...
+           'sea_surface_wave_from_direction',...
+           'sea_surface_wave_significant_height',...
+           'sea_surface_wind_wave_mean_period_from_variance_spectral_density_second_frequency_moment'};
 
-nc_cf_stationtimeseries2kmloverview('concentration_of_suspended_matter_in_sea_water.xls',OPT)
+for ii=1:length(subdirs)
+
+disp(['Processing ',num2str(ii),' / ',num2str(length(subdirs)),': ',subdirs{ii}])
+
+OPT.fileName    = [subdirs{ii},'.kml'];
+OPT.kmlName     =  subdirs{ii};
+OPT.THREDDSbase = ['http://opendap.deltares.nl:8080/thredds/dodsC/opendap/rijkswaterstaat/waterbase/',subdirs{ii},'/'];
+OPT.HYRAXbase   = ['http://opendap.deltares.nl:8080/opendap/rijkswaterstaat/waterbase/',subdirs{ii},'/'];
+OPT.ftpbase     = ['http://opendap.deltares.nl:8080/thredds/fileServer/opendap/rijkswaterstaat/waterbase/',subdirs{ii},'/'];
+OPT.description = {['parameter: ',subdirs{ii}],...
+                   'source: <a href="http://www.waterbase.nl">Rijkswaterstaat</a>'};
+
+nc_cf_stationtimeseries2kmloverview([subdirs{ii},'.xls'],OPT)
+
+end
