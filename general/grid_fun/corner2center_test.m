bd8aa44929600740183def08c4cf6c96778baea4
geer <geer@localhost>
2009-08-14 10:14:19 +0000

    Adapted to new format
    svn path=/trunk/matlab/; revision=818
    
    
    Former-commit-id: d096b1b7a6e40f14a01296898b497f6e30ab3879

--- general/grid_fun/corner2center_test.m
+++ general/grid_fun/corner2center_test.m
@@ -1,21 +1,56 @@
-function unittest = center2corner_test
+%% CORNER2CENTER_TEST  Test for corner2center
+%
+% TestName: CORNER2CENTER_TEST
+%  
+% This tests has only one testcase in which the result of corner2center is evaluated.
+%
+%
+%   See also corner2center
+
+%% Version <http://svnbook.red-bean.com/en/1.5/svn.advanced.props.special.keywords.html>
+% Created: 14 Aug 2009
+% Created with Matlab version: 7.8.0.347 (R2009a)
+
+% $Id$
+% $Date$
+% $Author$
+% $Revision$
+% $HeadURL$
+% $Keywords: $
+
+
+%% #Case1 Description (CaseName = Unit test & IncludeCode = true & EvaluateCode = true)
+% First we create two 3x3 matrix:
+ 
+x1 = [1 2 3;4 5 6;7 8 9]
+x2 = [1 2 3;4 5 6;7 8 9]
+ 
+%%
+% Then we convert the coordinates of the corners with the help of corner2center. The inverse of this
+% function (center2corner) should give us the same coordinates. If so there are two possibilities:
+%
+% * Both functions are correct
+% * Both function contain the same mistake
+%
+
+%% #Case1 RunTest
+testresult = false;
 %CORNER2CENTER_TEST   regular grid test for center2corner & corner2center
 %
 %See also: CORNER2CENTER, CENTER2CORNER_TEST
 
- x1 = [1 2 3;4 5 6;7 8 9];
- y1 = corner2center(center2corner(x1));
-
- x2 = [1 2 3;4 5 6;7 8 9];
- y2 = center2corner(corner2center(x2));
+y1 = corner2center(center2corner(x1));
+y2 = center2corner(corner2center(x2));
 
 % non-regular matrices are not reversible
 %x3 = rand(3);
 %y3 = center2corner(corner2center(x2));
 
-if all(y1(:)==x1(:)) & all(y2(:)==x2(:))
-   unittest = 1;
-else
-   unittest = 0;
-end
+testresult = all(y1(:)==x1(:)) & all(y2(:)==x2(:));
+
+%% #Case1 TestResults (EvaluateCode = true)
+% The test result speaks for itself. It was:
+
+testresult
+
 
