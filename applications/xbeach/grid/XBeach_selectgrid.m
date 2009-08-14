e4b06c1167f549c5a4e228a89aa885e97de4ee31
roelvin <roelvin@localhost>
2009-08-14 12:42:43 +0000

    svn path=/trunk/matlab/; revision=825
    Former-commit-id: 581a14fedbb5a68e80cc0294f154fc21bfa458c3

--- applications/xbeach/grid/XBeach_selectgrid.m
+++ applications/xbeach/grid/XBeach_selectgrid.m
@@ -18,13 +18,13 @@ function XB = XBeach_selectgrid(X, Y, Z, varargin)
 %   Example
 %   XBeach_selectgrid
 %
-%   See also keyword_value, CreateEmptyXBeachVar
+%   See also
 
 %   --------------------------------------------------------------------
 %   Copyright (C) 2009 Deltares
 %       Dano Roelvink / Ap van Dongeren / C.(Kees) den Heijer
 %
-%       Kees.denHeijer@Deltares.nl	
+%       Kees.denHeijer@Deltares.nl
 %
 %       Deltares
 %       P.O. Box 177
@@ -60,7 +60,7 @@ function XB = XBeach_selectgrid(X, Y, Z, varargin)
 %% default Input section
 OPT = struct(...
     'manual', true,...
-    'plot', true,...
+    'struct',false,...
     'WL_t', 0,...
     'xori', 0,...
     'yori', 0,...
@@ -86,13 +86,13 @@ if ~isempty(XBid)
     XB = varargin{XBid};
     varargin(XBid) = [];
 end
-try
+try %#ok<TRYNC>
     % the functions keyword_value and CreateEmptyXBeachVar are available in OpenEarthTools
     OPT = setProperty(OPT, varargin{:});
     if ~exist('XB', 'var')
         XB = CreateEmptyXBeachVar;
     end
-catch %#ok<CTCH>
+catch
     if ~isempty(varargin)
         warning(['Properties ' sprintf('"%s" ', varargin{1:2:end}) 'have not been set']) %#ok<WNTAG>
     end
@@ -104,17 +104,16 @@ else
     [xi yi] = deal(OPT.polygon(1,:), OPT.polygon(2,:));
 end
 
-% modify deepval and dryval to make them correspond with posdwm (and so, with Z)
+% modify deepval and dryval to make them correspond with posdwm (and so,
+% with Z)
 OPT.deepval = OPT.posdwn * abs(OPT.deepval);
 OPT.dryval = -OPT.posdwn * abs(OPT.dryval);
 
+
+
 if OPT.manual
     figure;
-    if ~isempty(OPT.bathy{1})
-        scatter(OPT.bathy{1}, OPT.bathy{2}, 5, OPT.bathy{3}, 'filled');
-    else
-        pcolor(X, Y, Z); shading interp; view(2); axis equal
-    end
+    scatter(OPT.bathy{1}, OPT.bathy{2}, 5, OPT.bathy{3}, 'filled');
     colorbar;
     hold on
     xn = max(max(X));
@@ -126,69 +125,83 @@ if OPT.manual
     disp('Select polygon to include in bathy')
     disp('Left mouse button picks points.')
     disp('Right mouse button picks last point.')
-    [xi yi] = select_oblique_rectangle;
+    [xi yi]=select_polygon
 end
 
 % Interpolate to grid
 in = inpolygon(X, Y, xi, yi);
 Z(~in) = NaN;
 
-if OPT.plot
-    figure;
-    if ~isvector(X)
-        surf(X, Y, Z);
-        shading interp;
-        colorbar
-    else
-        plot(X, Z)
-    end
-end
+figure;
+surf(X, Y, Z);
+shading interp;
+colorbar
 
 % Extrapolate to sides
 [nX nY] = size(X);
-if any(any(isnan(Z)))
-    for i = 1:nX
-        for j = floor(nY/2):nY
-            %         if j == nY
-            %             dbstopcurrent
-            %         end
-            %         dbclear all
-            if isnan(Z(i,j))
-                Z(i,j) = Z(i,j-1);
-            end
-        end
-        for j = floor(nY/2)-1:-1:1
-            if isnan(Z(i,j))
-                Z(i,j) = Z(i,j+1);
-            end
+for i = 1:nX
+    for j = floor(nY/2):nY
+        %         if j == nY
+        %             dbstopcurrent
+        %         end
+        %         dbclear all
+        if isnan(Z(i,j))
+            Z(i,j) = Z(i,j-1);
         end
     end
-    for j = 1:nY
-        % Extrapolate to land
-        for i = floor(nX/2):nX
-            if isnan(Z(i,j));
-                Z(i,j) = OPT.posdwn * max(OPT.posdwn*Z(i-1,j)-OPT.maxslp*OPT.dx, OPT.posdwn*OPT.dryval);
-            end
-        end
-        % Extrapolate to sea
-        for i = floor(nX/2):-1:1
-            if isnan(Z(i,j));
-                Z(i,j) = OPT.posdwn * min(OPT.posdwn*Z(i+1,j)+OPT.seaslp*OPT.dx, OPT.posdwn*OPT.deepval);
-            end
+    for j = floor(nY/2)-1:-1:1
+        if isnan(Z(i,j))
+            Z(i,j) = Z(i,j+1);
         end
     end
+end
+for j = 1:nY
+    % Extrapolate to land
+    for i = floor(nX/2):nX
+        if isnan(Z(i,j));
+            Z(i,j) = OPT.posdwn * max(OPT.posdwn*Z(i-1,j)-OPT.maxslp*OPT.dx, OPT.posdwn*OPT.dryval);
+        end
+    end
+    % Extrapolate to sea
+    for i = floor(nX/2):-1:1
+        if isnan(Z(i,j));
+            Z(i,j) = OPT.posdwn * min(OPT.posdwn*Z(i+1,j)+OPT.seaslp*OPT.dx, OPT.posdwn*OPT.deepval);
+        end
+    end
+end
+figure;
+surf(X,Y,Z);
+shading interp;
+colorbar
+%
+% Include structures
+if OPT.struct
+    figure;
+    scatter(OPT.bathy{1}, OPT.bathy{2}, 5, OPT.bathy{3}, 'filled');
+    colorbar;
+    hold on
+    xn = max(max(X));
+    yn = max(max(Y));
+    plot([0 xn xn 0 0],[0 0 yn yn 0],'r-')
+    axis ([0 xn 0 yn]);axis equal
+    % Select polygon to define structure
+    % Loop, picking up the points.
+    disp('Select polygon to include in structure')
+    disp('Left mouse button picks points.')
+    disp('Right mouse button picks last point.')
+    [xi yi]=select_polygon
 end
 
-if OPT.plot
-    figure;
-    if ~isvector(X)
-        surf(X, Y, Z);
-        shading interp;
-        colorbar
-    else
-        plot(X, Z)
-    end
-end
+
+
+
+
+
+
+
+
+
+
 
 %% x-grid
 xnew = X(:,1);
@@ -199,7 +212,7 @@ while xnew(i)<X(end,1);
     % interpolate for each y the corresponding z with the newly
     % chosen x value
     znew = interp2(X', Y', Z', repmat(xnew(i), 1, nY), Y(1,:));
-    d = min(OPT.WL_t) + min(OPT.posdwn*znew);
+    d = min(OPT.WL_t) + mean(OPT.posdwn*znew);
     dxnew = max(OPT.dxmax * sqrt(max(d,.1)/d0), OPT.dxmin);
     i = i+1;
     xnew(i) = xnew(i-1)+dxnew;
@@ -243,6 +256,13 @@ Znew = interp2(X', Y', Z', Xnew, Ynew);
 Znew(end,:) = Znew(end-1,:);
 Znew(:,end) = Znew(:, end-1);
 
+if OPT.struct
+    in = inpolygon(Xnew, Ynew, xi, yi);
+    Z_hard=Znew-20;
+    Znew(in) = 5;
+    Z_hard(in)=5;
+end
+
 % figure
 % pcolor(Xnew,Ynew,Znew);axis equal
 
@@ -250,51 +270,68 @@ XB.Input.xInitial = Xnew;
 XB.Input.yInitial = Ynew;
 XB.Input.zInitial = Znew;
 
-XB.settings.Grid = mergestructs(XB.settings.Grid, struct(...
+XB.settings.Grid = struct(...
     'nx', nxnew-1,...
     'ny', nynew-1,...
     'vardx', 1,...
+    'depfile', '',...
+    'xfile', '',...
+    'yfile', '',...
     'dx', OPT.dx,...
     'dy', OPT.dy,...
     'xori', OPT.xori,...
     'yori', OPT.yori,...
     'alfa', OPT.alfa*180/pi,...
-    'posdwn', -1));
+    'posdwn', -1);
 
 
 
-% fi=fopen('bathy.dep','wt');
-% for j=1:nynew
-%     fprintf(fi,'%7.3f ',Znew(:,j));
-%     fprintf(fi,'\n');
-% end
-% fclose(fi);
-% 
-% fi=fopen('x.dep','wt');
-% for j=1:nynew
-%     fprintf(fi,'%7.3f ',Xnew(:,j));
-%     fprintf(fi,'\n');
-% end
-% fclose(fi);
-% 
-% fi=fopen('y.dep','wt');
-% for j=1:nynew
-%     fprintf(fi,'%7.3f ',Ynew(:,j));
-%     fprintf(fi,'\n');
-% end
-% fclose(fi);
-% 
-% fi=fopen('griddata.txt','wt');
-% fprintf(fi,'nx      = %3i \n',nxnew-1);
-% fprintf(fi,'ny      = %3i \n',nynew-1);
-% fprintf(fi,'dx      = %6.1f \n',OPT.dx);
-% fprintf(fi,'dy      = %6.1f \n',OPT.dy);
-% fprintf(fi,'xori    = %10.2f \n',xori);
-% fprintf(fi,'yori    = %10.2f \n',yori);
-% fprintf(fi,'alfa    = %10.2f \n',alfa*180/pi);
-% fprintf(fi,'depfile = bathy.dep \n');
-% fprintf(fi,'vardx   = 1 \n');
-% fprintf(fi,'xfile = x.dep \n');
-% fprintf(fi,'yfile = y.dep \n');
-% fprintf(fi,'posdwn  = -1 \n');
-% fclose(fi);
+fi=fopen('bathy.dep','wt');
+for j=1:nynew
+    fprintf(fi,'%7.3f ',Znew(:,j));
+    fprintf(fi,'\n');
+end
+fclose(fi);
+
+if OPT.struct
+    fi=fopen('hardlayer.dep','wt');
+    for j=1:nynew
+        fprintf(fi,'%7.3f ',Z_hard(:,j));
+        fprintf(fi,'\n');
+    end
+    fclose(fi);
+end
+
+fi=fopen('x.dep','wt');
+for j=1:nynew
+    fprintf(fi,'%7.3f ',Xnew(:,j));
+    fprintf(fi,'\n');
+end
+fclose(fi);
+
+fi=fopen('y.dep','wt');
+for j=1:nynew
+    fprintf(fi,'%7.3f ',Ynew(:,j));
+    fprintf(fi,'\n');
+end
+fclose(fi);
+%
+fi=fopen('griddata.txt','wt');
+fprintf(fi,'nx      = %3i \n',nxnew-1);
+fprintf(fi,'ny      = %3i \n',nynew-1);
+fprintf(fi,'dx      = %6.1f \n',OPT.dx);
+fprintf(fi,'dy      = %6.1f \n',OPT.dy);
+fprintf(fi,'xori    = %10.2f \n',OPT.xori);
+fprintf(fi,'yori    = %10.2f \n',OPT.yori);
+fprintf(fi,'alfa    = %10.2f \n',OPT.alfa*180/pi);
+fprintf(fi,'depfile = bathy.dep \n');
+fprintf(fi,'vardx   = 1 \n');
+fprintf(fi,'xfile = x.dep \n');
+fprintf(fi,'yfile = y.dep \n');
+fprintf(fi,'posdwn  = -1 \n');
+if OPT.struct
+    fprintf(fi,'struct = 1 \n');
+    fprintf(fi,'ne_layer  = hardlayer.dep \n');
+end
+
+fclose(fi);
