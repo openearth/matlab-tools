de3ec3eb8e76ae747f324e62320dbbd55b945273
huism_b <huism_b@localhost>
2009-08-21 13:49:25 +0000

    added/adjusted some parameters that are relevant for D++ This will not influence DUROS or D+!
    svn path=/trunk/matlab/; revision=1764
    
    
    Former-commit-id: f8267237fb9e10f04fc395173d4b2526f9d2a753

--- applications/DUROS/engines/DuneErosionSettings/getDefaultDESettings.m
+++ applications/DUROS/engines/DuneErosionSettings/getDefaultDESettings.m
@@ -1,75 +1,77 @@
 function DESettings = getDefaultDESettings
 
 DESettings = struct(...
-    'Flags',[],...          % for logical values that can be used to turn on / off various processes in the dune erosion calculation
-    'Constants',[],...      % for constants during the calculation
+    'Flags',[],...                                   % for logical values that can be used to turn on / off various processes in the dune erosion calculation
+    'Constants',[],...                               % for constants during the calculation
     'ParabolicProfile',[],...
-    'Other',struct ...      % for all other (temporary) variables that are needed during calculation
+    'Other',struct ...                               % for all other (temporary) variables that are needed during calculation
     );
 
 DESettings.Flags = struct(...
-    'AdditionalErosionMax',true,...     % can be used to turn on / off boundary limitation of the additional erosion to 15 meters
-    'ChannelSlopes',true,...            % can be used to turn on / off boundary limitation due to steep (channel) slopes
-    'PreventLandwardTransport',true,... % can be used to turn on / off volume correction for landward directed sediment transport
-    'AutoStrengthening',false,...       % can be used to turn on / off automated strenghtening of a landward profile part
-    'DuneBreachCalculation',true ...    % can be used to turn on / off secondary calculations after a first dune row has been breached
+    'AdditionalErosionMax',true,...                  % can be used to turn on / off boundary limitation of the additional erosion to 15 meters
+    'ChannelSlopes',true,...                         % can be used to turn on / off boundary limitation due to steep (channel) slopes
+    'PreventLandwardTransport',true,...              % can be used to turn on / off volume correction for landward directed sediment transport
+    'AutoStrengthening',false,...                    % can be used to turn on / off automated strenghtening of a landward profile part
+    'DuneBreachCalculation',true ...                 % can be used to turn on / off secondary calculations after a first dune row has been breached
     );
 
 DESettings.Constants = struct(...
-    'a',0.476,...       % first constant used in calculation of the fall velocity of sediment
-    'b',2.18,...        % second constant used in calculation of the fall velocity of sediment
-    'c',3.226,...       % third constant used in calculation of the fall velocity of sediment
-    'rho',1024,...      % specific gravity of salt water
-    'g',9.81...         % fall excelleration
+    'a',0.476,...                                    % first constant used in calculation of the fall velocity of sediment
+    'b',2.18,...                                     % second constant used in calculation of the fall velocity of sediment
+    'c',3.226,...                                    % third constant used in calculation of the fall velocity of sediment
+    'rho',1024,...                                   % specific gravity of salt water
+    'g',9.81...                                      % fall excelleration
     );
 
 DESettings.Other = struct(...
-    'AdditionalVolume','min([-20,.25*Volume])',...  % formulation to use to determine the target volume
-    'Plus','-plus',...                              % DUROS-plus method (including Tp, as opposed to DUROS)
-    'n_d',1, ...                                    % scale factor
-    'Bend',0, ...                                   % Coastal Bend
-    'ProfileFluct',0, ...                           % Profile fluctuation
-    'maxRetreat',15,...                             % Limitation of additional erosion length
-    'maxiter', 50, ...                              % Maximum number of iterations
-    'verbosemessages',[45,1,3,60,61,-4,-5,-6],...      % Messages that have to be displayed whem verbose is set to true
-    'FallVelocity', {{@getFallVelocity ...          % Fall velocity function
-        'a' 0.476 ...                               % Fall velocity input arguments; these arguments will be passed through to the fall velocity function together with the D50
+    'AdditionalVolume','min([-20,.25*Volume])',...   % formulation to use to determine the target volume
+    'Plus','-plus',...                               % DUROS-plus method (including Tp, as opposed to DUROS)
+    'n_d',1, ...                                     % scale factor
+    'Bend',0, ...                                    % Coastal Bend
+    'ProfileFluct',0, ...                            % Profile fluctuation
+    'maxRetreat',15,...                              % Limitation of additional erosion length
+    'maxiter', 50, ...                               % Maximum number of iterations
+    'verbosemessages',[45,1,3,60,61,-4,-5,-6],...    % Messages that have to be displayed whem verbose is set to true
+    'FallVelocity', {{@getFallVelocity ...           % Fall velocity function
+        'a' 0.476 ...                                % Fall velocity input arguments; these arguments will be passed through to the fall velocity function together with the D50
         'b' 2.18 ...
         'c' 3.226 ...
-        'D50'}}, ... % Since getFallVelocity uses propertyname propertyvalue pairs for all variables, the last arguments is therefore the propertyname (!) D50, since the property value of D50 is included during the process
-    'd',20 ...                                      % Gives the depth contour (relative to z=0) at which the hydraulic conditions are given.
+        'D50'}}, ...                                 % Since getFallVelocity uses propertyname propertyvalue pairs for all variables, the last arguments is therefore the propertyname (!) D50, since the property value of D50 is included during the process
+    'd',25 ...                                       % Gives the depth contour (relative to z=0) at which the hydraulic conditions are given.
     );
 
 DESettings.ParabolicProfile = struct(...
     'c_1',.4714,...
-    'c_2',nan,...                                   % Not in use at this moment (but maybe for later use)
+    'cp_c1',0,...                                    % coefficient used to include depth effects into c1 (no effect if cp_c1=0)
+    'c_2',18,...
+    'cp_c2',0,...                                    % coefficient used to include depth effects into c2 (no effect if cp_c2=0)
     'c_hs',7.6,...
     'cp_hs',1.28,...
-    'c_tp',12,...
-    'cp_tp',0.45,...
+    'c_tp',12,...                                    % Reference wave period for D+
+    'cp_tp',0.45,...                                 % power of wave period component in D+ equation...
     'c_w',0.0268,...
     'cp_w',0.56,...
-    'c_d',nan,...                                   % Not in use at this moment (but maybe for later use)
-    'cp_d',nan,...                                  % Not in use at this moment (but maybe for later use)
+    'c_d',25,...                                     % Reference depth for D++
+    'cp_d',-0.17,...                                 % power of depth component in D++ equation   <- !first estimate of this parameter used for test purposes!
     'ParabolicProfileFcn', @getParabolicProfile, ... % Parabolic profile function
     'ParabolicProfileFcnInput',{{...
         '#PARWL_T',...
-        '#PARHS_T',...                              % Input arguments for the parabolic profile function
-        '#PARTP_T',...                              % #PARHS_T #PARTP_T #PARW #PARX0 #PARX #PARD
+        '#PARHS_T',...                               % Input arguments for the parabolic profile function
+        '#PARTP_T',...                               % #PARHS_T #PARTP_T #PARW #PARX0 #PARX #PARD
         '#PARW',...                                 
         '#PARX0',...                                
         '#PARX'}},...                                
     'rcParabolicProfileFcn', @getRcParabolicProfile, ... % Function that calculates the rc of the parabolic profile excactly
     'rcParabolicProfileFcnInput',{{...
-        '#PARWL_T',...                              % Input arguments fot the rcDetermination:
-        '#PARHS_T',...                              %  #PARWL_T #PARHS_T #PARTP_T #PARW #PARZ #PARD
+        '#PARWL_T',...                               % Input arguments fot the rcDetermination:
+        '#PARHS_T',...                               %  #PARWL_T #PARHS_T #PARTP_T #PARW #PARZ #PARD
         '#PARTP_T',...
         '#PARW',...
         '#PARZ'}},...
      'invParabolicProfileFcn', @invParabolicProfile,...
      'invParabolicProfileFcnInput',{{...
-        '#PARWL_T',...                              % Input arguments fot the rcDetermination:
-        '#PARHS_T',...                              %  #PARWL_T #PARHS_T #PARTP_T #PARW #PARZ #PARD
+        '#PARWL_T',...                               % Input arguments fot the rcDetermination:
+        '#PARHS_T',...                               %  #PARWL_T #PARHS_T #PARTP_T #PARW #PARZ #PARD
         '#PARTP_T',...
         '#PARW',...
         '#PARZ'}}...
@@ -86,8 +88,8 @@ DESettings.ParabolicProfile = struct(...
 %       * #PARWL_T - Is replaced by the storm surge level
 
 DESettings.Actions = struct(...
-    'Verbose',true, ...             % Display important messages at the end of the calculation
-    'DUROS',true, ...               % can be used to turn on / off DUROS(-plus) calculation
-    'AdditionalErosion',true, ...   % can be used to turn on / off additional erosion calculation
-    'BoundaryProfile',true ...      % can be used to turn on / off boundary profile fitting
+    'Verbose',true, ...                              % Display important messages at the end of the calculation
+    'DUROS',true, ...                                % can be used to turn on / off DUROS(-plus) calculation
+    'AdditionalErosion',true, ...                    % can be used to turn on / off additional erosion calculation
+    'BoundaryProfile',true ...                       % can be used to turn on / off boundary profile fitting
     );
