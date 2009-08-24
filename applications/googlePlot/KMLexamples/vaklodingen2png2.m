8e98c2bb70cd502d8529c3ca0e0df11c8f6dfa77
damsma <damsma@localhost>
2009-08-24 07:28:14 +0000

    some updates, and a new 'vaklodingen_url' function (returns a structure with links to p:\...vaklodingen*.nc or http://..vaklodingen*.nc
    svn path=/trunk/matlab/; revision=866
    
    
    Former-commit-id: 801b9bbf0fd71fa3b7766985b6e00b85af370595

--- applications/googlePlot/KMLexamples/vaklodingen2png2.m
+++ applications/googlePlot/KMLexamples/vaklodingen2png2.m
@@ -2,19 +2,19 @@
 %
 %See also: jarkusgrids2png, vaklodingen2kml, vaklodingen_overview
 
-outputDir      = 'D:\Thijs\Desktop\vaklodingen2';
-url            = 'http://opendap.deltares.nl:8080/opendap/rijkswaterstaat/vaklodingen';
-contents       = opendap_folder_contents(url);
+outputDir      = 'D:\repositories\kml\vaklodingen';
+url            = vaklodingen_url;
 EPSG           = load('EPSG');
 
-colormap(colormapbathymetry(128));
-clim([-20 20]);
 
-for ii = length(contents):-1:1;
-    [path, fname] = fileparts(contents{ii});
-    x    = nc_varget(contents{ii},   'x');
-    y    = nc_varget(contents{ii},   'y');
-    time = nc_varget(contents{ii},'time');
+
+
+
+for ii = length(url):-1:1;
+    [path, fname] = fileparts(url{ii});
+    x    = nc_varget(url{ii},   'x');
+    y    = nc_varget(url{ii},   'y');
+    time = nc_varget(url{ii},'time');
 
     % expand x and y 10 m in each direction
     x = [x(1) + (x(1)-x(2))/2; x; x(end) + (x(end)-x(end-1))/2];
@@ -29,34 +29,33 @@ for ii = length(contents):-1:1;
     date = datestr(time,'yyyy-mm-dd');
     date(end+1,:) = datestr(now,'yyyy-mm-dd');
 
-    if size(time,1)>0
-        for jj = size(time,1);
-            if ~exist([outputDir filesep fname '_' date(jj,:) '.kml'],'file')
+    for jj = size(time,1):-1:size(time,1)+1 - min(size(time,1),2)   ;
+        if ~exist([outputDir filesep fname '_' date(jj,:) '.kml'],'file')
 
-                % display progress
-                disp([num2str(ii) '/' num2str(length(contents)) ' ' fname ' ' date(jj,:)]);
-                % load z data
-                z = nc_varget(contents{ii},'z',[jj-1,0,0],[1,-1,-1]);
+            % display progress
+            disp([num2str(ii) '/' num2str(length(url)) ' ' fname ' ' date(jj,:)]);
+            % load z data
+            z = nc_varget(url{ii},'z',[jj-1,0,0],[1,-1,-1]);
 
-                % expand z
-                z = z([1 1:end end],:);
-                z = z(:,[1 1:end end]);
-                z(z>500) = nan;
-                h = surf(lon,lat,-z);%camlight right;
-                camlight(-40,20,'infinite')
-                colormap(flipud(colormapbathymetry(17)));
-                clim([-20 20]);
-                shading interp;material([.7 .3 0.2]);lighting phong
-                axis off;axis tight;view(0,90);
-                KMLfig2png(h,'levels',5,'timeIn',date(jj,:),'timeOut',date(jj+1,:),...
-                    'fileName',[outputDir filesep fname '_' date(jj,:) '.kml'],...
-                    'drawOrder',str2double(datestr(time(jj),'yyyy'))*10,...
-                    'alpha',1,'maxLod',256,'minLod',64,'dim',128);
-            else
-                disp([num2str(ii) '/' num2str(length(contents)) ' ' fname ' ' date(jj,:) ' already created']);
-            end
+            % expand z
+            z = z([1 1:end end],:);
+            z = z(:,[1 1:end end]);
+            z(z>500) = nan;
+            h = surf(lon,lat,-z);%camlight right;
+            camlight(-40,20,'infinite')
+            shading interp;material([.7 .3 0.2]);lighting phong
+            axis off;axis tight;view(0,90);
+            colormap(flipud(colormapbathymetry(50)));
+            clim([-25 25]);
+            KMLfig2png(h,'levels',5,'timeIn',date(jj,:),'timeOut',date(jj+1,:),...
+                'fileName',[outputDir filesep fname '_' date(jj,:) '.kml'],...
+                'drawOrder',str2double(datestr(time(jj),'yyyy'))*10,...
+                'alpha',1,'maxLod',256,'minLod',64,'dim',128);
+        else
+            disp([num2str(ii) '/' num2str(length(url)) ' ' fname ' ' date(jj,:) ' already created']);
         end
     end
+
 end
 
 
