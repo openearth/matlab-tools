ff69934cf42cea7e702735e130ac7473bbd89186
geer <geer@localhost>
2009-09-09 13:16:56 +0000

    replaced createtutorialsummary with tutorials2html old one still available and it should work. New one better (and upgradet to a finction instead of a script).
    svn path=/trunk/matlab/; revision=981
    
    
    Former-commit-id: d6aeebdd4a9a58ee3e557efe5f2b5776a7cb8ee9

---
+++ maintenance/tutorialGeneration/tutorials2html.m
@@ -0,0 +1,303 @@
+function tutorials2html(varargin)
+%TUTORIALS2HTML  publishes all tutorials to html (including overview page).
+%
+%   This function finds all tutorials inside a toolbox and publishes the files to html. 
+%   It also creates an overview page. It fills a template (tutorial_summary.html.tpl by default) and
+%   publishes the results in an outputdir that can be specified. Default is the outputdir of the
+%   tutorials in you local copy of OpenEarth.
+%
+%   Syntax:
+%   tutorials2html(varargin)
+%
+%   Input:
+%   varargin  =
+%       TODO
+%
+%   Example
+%   tutorials2html;
+%
+%   See also publish
+
+%% Copyright notice
+%   --------------------------------------------------------------------
+%   Copyright (C) 2009 Deltares
+%       Pieter van Geer
+%
+%       pieter.vangeer@deltares.nl	
+%
+%       Rotterdamseweg 185
+%       2629 HD Delft
+%       P.O. 177
+%       2600 MH Delft
+%
+%   This library is free software: you can redistribute it and/or
+%   modify it under the terms of the GNU Lesser General Public
+%   License as published by the Free Software Foundation, either
+%   version 2.1 of the License, or (at your option) any later version.
+%
+%   This library is distributed in the hope that it will be useful,
+%   but WITHOUT ANY WARRANTY; without even the implied warranty of
+%   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+%   Lesser General Public License for more details.
+%
+%   You should have received a copy of the GNU Lesser General Public
+%   License along with this library. If not, see <http://www.gnu.org/licenses/>.
+%   --------------------------------------------------------------------
+
+% This tools is part of <a href="http://OpenEarth.Deltares.nl">OpenEarthTools</a>.
+% OpenEarthTools is an online collaboration to share and manage data and 
+% programming tools in an open source, version controlled environment.
+% Sign up to recieve regular updates of this function, and to contribute 
+% your own tools.
+
+%% Version <http://svnbook.red-bean.com/en/1.5/svn.advanced.props.special.keywords.html>
+% Created: 09 Sep 2009
+% Created with Matlab version: 7.8.0.347 (R2009a)
+
+% $Id$
+% $Date$
+% $Author$
+% $Revision$
+% $HeadURL$
+% $Keywords: $
+
+%% process input
+% maindir
+maindir = openearthtoolsroot;
+id = find(strcmpi(varargin,'maindir'));
+if ~isempty(id)
+    maindir = varargin{id+1};
+end
+
+% templatename
+publishtemplate = which('mxdom2tutorialhtmllocal.xsl');
+id = find(strcmpi(varargin,'publishtemplate'));
+if ~isempty(id)
+    publishtemplate = varargin{id+1};
+end
+
+summarytemplate = which('tutorial_summary.html.tpl');
+id = find(strcmpi(varargin,'summarytemplate'));
+if ~isempty(id)
+    summarytemplate = varargin{id+1};
+end
+
+outputdir = fullfile(openearthtoolsroot,'tutorials');
+id = find(strcmpi(varargin,'outputdir'));
+if ~isempty(id)
+    outputdir = varargin{id+1};
+end
+
+scriptdir = fullfile(fileparts(summarytemplate),'html','script');
+
+summaryonly = false;
+id = strcmpi(varargin,'nopublish');
+if any(id)
+    summaryonly = true;
+end
+
+show = false;
+if any(strcmpi(varargin,'show'))
+    show = true;
+end
+%% Gather all tutorial m-files
+% all directories
+alldirs = sort(strread(genpath(maindir),'%s',-1,'delimiter',';'));
+alldirs(~cellfun(@isempty,strfind(alldirs,'.svn')))=[];
+
+% loop dirs and find "_tutorial"
+tutorials = cell(size(alldirs,1),1);
+for idr = 1:length(alldirs)
+    fls = dir(fullfile(alldirs{idr},'*_tutorial*.m'));
+    tutorials{idr} = {fls.name}';
+end
+
+% remove dirs from list that did not have a tutorial
+id = ~cellfun(@isempty,tutorials);
+alldirs(~id)=[];
+tutorials(~id)=[];
+
+%% publish tutorials (if not already published)
+% target dirs
+outputhtmldir = fullfile(outputdir,'html');
+
+% publishopts
+publishopts = struct(...
+    'maxOutputLines',15,...
+    'format','html',...
+    'stylesheet',publishtemplate,...
+    'catchError',true,...
+    'outputDir',outputhtmldir,...
+    'useNewFigure',true,...
+    'codeToEvaluate',[]);
+
+% create outputdir
+if ~isdir(outputdir)
+    mkdir(outputdir);
+    mkdir(outputhtmldir);
+end
+
+% loop tutorials
+cdtemp = cd;
+htmlref = cell(size(alldirs));
+openfigs = findobj('Type','figure');
+for idr = 1:length(alldirs)
+    htmlref{idr} = cell(size(tutorials{idr}));
+    for itutorials = 1:length(tutorials{idr})
+        %% look for html files with the same name
+        [dum tutorialname] = fileparts(tutorials{idr}{itutorials});
+        htmlfilesthere = which([tutorialname '.html'],'-all');
+        if strcmp(tutorialname(1),'_')
+            htmlfilesthere = which([tutorialname(2:end) '.html'],'-all');
+        end
+        id = strncmp(htmlfilesthere,outputhtmldir,length(outputhtmldir));
+        if summaryonly
+            htmlref{idr}{itutorials} = htmlfilesthere{find(id,1,'first')};
+        else
+            if all(id)
+                %% publish file, it is not published yet
+                if strcmp(tutorialname(1),'_')
+                    %% rename the file, because a filename starting with _ won't work
+                    tutorialname = tutorialname(2:end);
+                    copyfile(which(tutorials{idr}{itutorials}),fullfile(tempdir,[tutorialname,'.m']));
+                    cd(tempdir);
+                end
+                publishopts.codeToEvaluate = ['evalinemptyworkspace(''' tutorialname ''');'];
+                %% save reference
+                
+                htmlref{idr}{itutorials} = publish(tutorialname,publishopts);
+                newfigs = findobj('type','figure');
+                close(newfigs(~ismember(newfigs,openfigs)));
+            else
+                %% copy html to outputhtmldir
+                copyfile(htmlfilesthere{find(~id,1,'first')},fullfile(outputhtmldir,[tutorialname,'.html']));
+                imagesindir = cat(1,...
+                    dir(fullfile(fileparts(htmlfilesthere{find(~id,1,'first')}),'*.png')),...
+                    dir(fullfile(fileparts(htmlfilesthere{find(~id,1,'first')}),'*.gif')),...
+                    dir(fullfile(fileparts(htmlfilesthere{find(~id,1,'first')}),'*.bmp')),...
+                    dir(fullfile(fileparts(htmlfilesthere{find(~id,1,'first')}),'*.tiff')));
+                for iim = 1:length(imagesindir)
+                    copyfile(fullfile(fileparts(htmlfilesthere{find(~id,1,'first')}),imagesindir(iim).name),...
+                        fullfile(outputhtmldir,imagesindir(iim).name));
+                end
+                %% save reference
+                htmlref{idr}{itutorials} = fullfile(outputhtmldir,[tutorialname,'.html']);
+            end
+        end
+    end
+end
+cd(cdtemp);
+
+%% Copy script files to html dir 
+tmpdir = tempname;
+copyfile(scriptdir,tmpdir,'f');
+drs = strread(genpath(tmpdir),'%s',-1,'delimiter',';');
+id = find(~cellfun(@isempty,strfind(drs,'.svn')));
+for idr = 1:length(id)
+    if isdir(drs{id(idr)})
+        rmdir(drs{id(idr)},'s');
+    end
+end
+copyfile(tmpdir,fullfile(outputhtmldir,'script'),'f');
+
+%% load template
+fid = fopen(summarytemplate,'r');
+str = fread(fid,'*char')';
+fclose(fid);
+
+%% Identify General and Application loops
+returnid = strfind(str,char(10));
+
+generalstr = str(min(returnid(returnid > strfind(str,'##BEGINGEN'))):...
+    max(returnid(returnid < strfind(str,'##ENDGEN'))));
+
+applicationstr = str(min(returnid(returnid > strfind(str,'##BEGINAPP'))):...
+    max(returnid(returnid < strfind(str,'##ENDAPP'))));
+
+%% identify general tutorials and application tutorials (m-files)
+[alldirsstripped sid] = sort(strrep(alldirs,openearthtoolsroot,''));
+htmlref = htmlref(sid);
+tutorials = tutorials(sid);
+
+id = strncmp(alldirsstripped,'applications',length('applications'));
+idgeneral = find(~id);
+idapplications = find(id);
+
+%% Fill template with general items
+% strings for each file / folder
+rtns = strfind(generalstr,char(10));
+filestr = generalstr(min(rtns(rtns > min(strfind(generalstr,'##BEGINFILE')))):...
+    max(rtns(rtns < min(strfind(generalstr,'##ENDFILE')))));
+folderstr = generalstr(min(rtns(rtns > min(strfind(generalstr,'##BEGINFOLDER')))):...
+    max(rtns(rtns < min(strfind(generalstr,'##ENDFOLDER')))));
+rtns2 = strfind(folderstr,char(10));
+fileinfolderstr = folderstr(min(rtns2(rtns2 > min(strfind(folderstr,'##BEGINFILE')))):...
+    max(rtns2(rtns2 < min(strfind(folderstr,'##ENDFILE')))));
+
+% loop strings and concatenate
+genstr = [];
+for idr = 1:length(idgeneral)
+    dirid = idgeneral(idr);
+    dirnames = strread(alldirsstripped{dirid},'%s',-1,'delimiter',[filesep filesep]);
+    if length(dirnames) == 1
+        filesstr = [];
+        for ifiles = 1:length(tutorials{dirid})
+            [dum name] = fileparts(htmlref{dirid}{ifiles});
+            filesstr = cat(2,filesstr,...
+                strrep(strrep(filestr,'#HTMLREF',['html/',name,'.html']),'#FILENAME',name));
+        end
+        genstr = cat(2,genstr,filesstr);
+    else
+        newfstr = strrep(folderstr,'#FOLDERNAME',dirnames{end});
+        newfilestr = [];
+        for ifiles = 1:length(tutorials{dirid})
+             [dum name] = fileparts(htmlref{dirid}{ifiles});
+            newfilestr = cat(2,newfilestr,...
+                strrep(strrep(fileinfolderstr,'#HTMLREF',['html/',name,'.html']),'#FILENAME',name));
+        end
+        newfstr = strrep(newfstr,fileinfolderstr,newfilestr);
+        genstr = cat(2,genstr,newfstr);
+    end
+end
+
+% replace general template string in template
+str = strrep(str,generalstr,genstr);
+
+%% Fill applications
+% identify folder and file template strings
+rtns = strfind(applicationstr,char(10));
+folderstr = applicationstr(min(rtns(rtns > min(strfind(applicationstr,'##BEGINFOLDER')))):...
+    max(rtns(rtns < min(strfind(applicationstr,'##ENDFOLDER')))));
+rtns2 = strfind(folderstr,char(10));
+fileinfolderstr = folderstr(min(rtns2(rtns2 > min(strfind(folderstr,'##BEGINFILE')))):...
+    max(rtns2(rtns2 < min(strfind(folderstr,'##ENDFILE')))));
+
+newappstr = [];
+for idr = 1:length(idapplications)
+    % fill application node
+    dirid = idapplications(idr);
+    dirnames = strread(alldirsstripped{dirid},'%s',-1,'delimiter',[filesep filesep]);
+
+    newfstr = strrep(folderstr,'#FOLDERNAME',dirnames{2});
+    newfilestr = [];
+    for ifiles = 1:length(tutorials{dirid})
+        [dum name] = fileparts(htmlref{dirid}{ifiles});
+        newfilestr = cat(2,newfilestr,...
+            strrep(strrep(fileinfolderstr,'#HTMLREF',['html/',name,'.html']),'#FILENAME',name));
+    end
+    newfstr = strrep(newfstr,fileinfolderstr,newfilestr);
+    newappstr = cat(2,newappstr,newfstr);
+end
+
+% replace application template string with the one we made above
+str = strrep(str,applicationstr,newappstr);
+
+%% write output file
+fid = fopen(fullfile(outputdir,'tutorial_summary.html'),'w');
+fprintf(fid,'%s',str);
+fclose(fid);
+
+%% show result
+if show
+    winopen(fullfile(outputdir,'tutorial_summary.html'));
+end
