abd863a13fd968ae20d7c68d473dff52d4308f64
geer <geer@localhost>
2009-09-22 12:53:02 +0000

    double file
    svn path=/trunk/matlab/; revision=1076
    
    
    Former-commit-id: e87770c7d0bbdb3995a1eeb747b449e053b81739

--- applications/googlePlot/KMLexamples/KMLline_test.m
+++
@@ -1,4 +0,0 @@
-[lat,lon] = meshgrid([51:54],[3:7]);
-
-kmlline(lat ,lon ,'fileName','KMLline_testh.kml','lineColor',jet(2),'kmlName','horizontal','text',{'1','2','3','4'});
-kmlline(lat',lon','fileName','KMLline_testv.kml','lineWidth',[1 3 3],'kmlName','vertical'  ,'text',{'a','b','c','d','e'});
