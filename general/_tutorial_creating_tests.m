a9f9d46e1377aa259456cc2be0e670490a3397ad
geer <geer@localhost>
2009-09-23 12:07:30 +0000

    relocated tutorials on testing
    svn path=/trunk/matlab/; revision=1093
    
    
    Former-commit-id: fd0242e62464c578f78e4990ab0f2806579f9530

--- general/_tutorial_creating_tests.m
+++
@@ -1,9 +0,0 @@
-%% 3. Creating tests
-% At this moment engines are being prepared to automatically run tests. In an ideal world one writes
-% a tests before writing the actual function. Tests also serve to judge whether changes you made to
-% a low level function influence the outcome of an application. Tutorials for writing and running
-% tests are being prepared. For now it is enough to know that (like creating a new function) there
-% is also a function to create a test template: oetnewtest. The help of this function shows you how
-% it can be used.
-
-help oetnewtest
