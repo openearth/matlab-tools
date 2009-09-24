0e12a8cb09e7f5ee1f1b5809300569d625556ce5
m.vankoningsveld@tudelft.nl <m.vankoningsveld@tudelft.nl@localhost>
2009-09-24 13:12:08 +0000

    Read routine added for Dynamic plume output
    svn path=/trunk/matlab/; revision=1104
    
    
    Former-commit-id: 2ee73ffbe7af71769fa22eb9a9a10536db0dc94c

---
+++ applications/TASS/TASS_readDynamic_res.m
@@ -0,0 +1,94 @@
+function [data, data_info, data_units] = TASS_readDynamic_res(varargin)
+% TASS_READDYNAMIC_RES  Routine to read the dynamic plume output file
+%
+%   Routine reads a dynamic plume output file (*.res) produced by TASS model.
+%   The routine takes a filename as an input file. Output produced is an
+%   array with data and a variable with column info and
+%   units.
+%
+%   Syntax:
+%       [data, data_info, data_units] = TASS_readDynamic_res(varargin)
+%
+%   Input:
+%   For the following keywords, values are accepted (values indicated are the current default settings):
+%       'filename', []                  = passive plume output filename
+%
+%   Output:
+%       data                            = 6 column array with output data
+%       data_info                       = cell array with column information
+%       data_units                      = cell array with column units
+%
+% See also TASS_readPassive_out
+
+%   --------------------------------------------------------------------
+%   Copyright (C) 2009 Delft University of Technology
+%       Mark van Koningsveld
+%
+%       m.vankoningsveld@tudelft.nl
+%
+%       Hydraulic Engineering Section
+%       Faculty of Civil Engineering and Geosciences
+%       Stevinweg 1
+%       2628CN Delft
+%       The Netherlands
+%
+%   This library is free software; you can redistribute it and/or
+%   modify it under the terms of the GNU Lesser General Public
+%   License as published by the Free Software Foundation; either
+%   version 2.1 of the License, or (at your option) any later version.
+%
+%   This library is distributed in the hope that it will be useful,
+%   but WITHOUT ANY WARRANTY; without even the implied warranty of
+%   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+%   Lesser General Public License for more details.
+%
+%   You should have received a copy of the GNU Lesser General Public
+%   License along with this library; if not, write to the Free Software
+%   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+%   USA
+%   or http://www.gnu.org/licenses/licenses.html, http://www.gnu.org/, http://www.fsf.org/
+%   --------------------------------------------------------------------
+
+% Created: 22 Feb 2009
+% Created with Matlab version: 7.7.0.471 (R2008b)
+
+% $Id$
+% $Date$
+% $Author$
+% $Revision$
+% $HeadURL$
+% $Keywords: 
+
+
+%% defaults
+OPT = struct( ...
+    'filename', 'd:\Documents and Settings\mrv\VanOord\Projecten\96.8015 TASS P15 Slibpluimmeting\Software\ExampleData\dynamic_test.res' ...
+    );
+
+%% overrule default settings by property pairs, given in varargin
+OPT = setProperty(OPT, varargin{:});
+
+if isempty(OPT.filename)
+    disp('Error: Input file needed')
+    return
+end
+
+disp(['Processing: ' OPT.filename])
+tic
+
+%% read some basic info from the output file
+% skip the first 22 lines
+fid = fopen(OPT.filename);
+for i = 1:5
+    fgetl(fid);
+end
+
+% read in the data
+data = fscanf(fid,'%f %f %f %f %f %f %f %f %f',[9 inf]);
+
+% flip the matrix to get nice column structure
+data = data';
+
+% provide info and units
+data_info  = {'Time', 'Total', 'Overflow susp.', 'Overflow bed', 'Surface', 'Detrain.', 'Bed plume width', 'Bed plume depth', 'Bed plume delay'};
+data_units = {'mins', 'kg/s', 'kg/s', 'kg/s', 'kg/s', 'kg/s', 'm', 'm', 's'};
