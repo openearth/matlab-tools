ae434294942f404f4cacda88157e9d33e8cad7fd
m.vankoningsveld@tudelft.nl <m.vankoningsveld@tudelft.nl@localhost>
2009-09-24 12:21:28 +0000

    Improve file for reading TASS input  - comments added  - help block added
    svn path=/trunk/matlab/; revision=1096
    
    
    Former-commit-id: b0375c9f13e29f40b0837430c8f0d30620b3b201

--- applications/TASS/TASS_readPassive_out.m
+++ applications/TASS/TASS_readPassive_out.m
@@ -1,10 +1,63 @@
-function [data] = TASS_readPassive_out(varargin)
+function [data, data_info, data_units] = TASS_readPassive_out(varargin)
 % TASS_READPASSIVE_OUT  Routine to read the passive plume output file
 %
-% Input
+%   Routine reads a passive plume output file (*.out) produced by TASS model.
+%   The routine takes a filename as an input file. Output produced is an
+%   array with data and a variable with column 
 %
+%   Syntax:
+%       [data, data_info, data_units] = TASS_readPassive_out(varargin)
+%
+%   Input:
+%   For the following keywords, values are accepted (values indicated are the current default settings):
+%       'filename', []                  = passive plume output filename
+%
+%   Output:
+%       data                            = 6 column array with output data
+%       data_info                       = cell array with column information
+%       data_units                      = cell array with column units
 %
 % See also
+
+%   --------------------------------------------------------------------
+%   Copyright (C) 2009 Delft University of Technology
+%       Mark van Koningsveld
+%
+%       m.vankoningsveld@tudelft.nl
+%
+%       Hydraulic Engineering Section
+%       Faculty of Civil Engineering and Geosciences
+%       Stevinweg 1
+%       2628CN Delft
+%       The Netherlands
+%
+%   This library is free software; you can redistribute it and/or
+%   modify it under the terms of the GNU Lesser General Public
+%   License as published by the Free Software Foundation; either
+%   version 2.1 of the License, or (at your option) any later version.
+%
+%   This library is distributed in the hope that it will be useful,
+%   but WITHOUT ANY WARRANTY; without even the implied warranty of
+%   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+%   Lesser General Public License for more details.
+%
+%   You should have received a copy of the GNU Lesser General Public
+%   License along with this library; if not, write to the Free Software
+%   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+%   USA
+%   or http://www.gnu.org/licenses/licenses.html, http://www.gnu.org/, http://www.fsf.org/
+%   --------------------------------------------------------------------
+
+% Created: 22 Feb 2009
+% Created with Matlab version: 7.7.0.471 (R2008b)
+
+% $Id$
+% $Date$
+% $Author$
+% $Revision$
+% $HeadURL$
+% $Keywords: 
+
 
 %% defaults
 OPT = struct( ...
@@ -22,10 +75,19 @@ end
 disp(['Processing: ' OPT.filename])
 tic
 
-%% read some basic info from the logfile header
+%% read some basic info from the output file
+% skip the first 22 lines
 fid = fopen(OPT.filename);
 for i = 1:22
     fgetl(fid);
 end
+
+% read in the data
 data = fscanf(fid,'%f %f %f %f %f %f',[6 inf]);
+
+% flip the matrix to get nice column structure
 data = data';
+
+% provide info and units
+data_info  = {'Run Time Concentration', 'Depth-averaged Concentration', 'Mid-depth Concentration', 'Concentration at 1m above bed', 'Dredger position x', 'Dredger position y'};
+data_units = {'Min', 'mg/l', 'mg/l', 'mg/l', 'm', 'm'};
