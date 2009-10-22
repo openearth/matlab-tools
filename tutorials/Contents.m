893fdae629ab0b7a5aabfa1cc4252fd2b09600c3
donchyt <donchyt@localhost>
2009-10-22 14:54:51 +0000

    contents.m
    svn path=/trunk/matlab/; revision=1836
    
    
    Former-commit-id: 246767ec0dfc51d32d233130c1753b026ebab682

---
+++ tutorials/Contents.m
@@ -0,0 +1,50 @@
+% Openearth tutorials:
+%
+%  General tutorials:
+%
+%     <a href="matlab:web(['jar:file:///' strrep(openearthtoolsroot,filesep,'/') 'docs/OpenEarthDocs/oethelpdocs/help.jar!/html/tutorial_initiate_toolbox.html'], '-helpbrowser');">1. Initializing the OpenEarthTools toolbox in matlab</a>
+%     <a href="matlab:web(['jar:file:///' strrep(openearthtoolsroot,filesep,'/') 'docs/OpenEarthDocs/oethelpdocs/help.jar!/html/tutorial_creating_new_functions.html'], '-helpbrowser');">2. Creating new functions</a>
+%     <a href="matlab:web(['jar:file:///' strrep(openearthtoolsroot,filesep,'/') 'docs/OpenEarthDocs/oethelpdocs/help.jar!/html/tutorial_automatedtesting.html'], '-helpbrowser');">3. Automated testing</a>
+%     <a href="matlab:web(['jar:file:///' strrep(openearthtoolsroot,filesep,'/') 'docs/OpenEarthDocs/oethelpdocs/help.jar!/html/tutorial_keyword_value_pairs.html'], '-helpbrowser');">4. Keyword Value pairs</a>
+%     <a href="matlab:web(['jar:file:///' strrep(openearthtoolsroot,filesep,'/') 'docs/OpenEarthDocs/oethelpdocs/help.jar!/html/tutorial_how_to_write_tutorials.html'], '-helpbrowser');">5. How to make a tutorial</a>
+%
+%     automatedTesting:
+%       <a href="matlab:web(['jar:file:///' strrep(openearthtoolsroot,filesep,'/') 'docs/OpenEarthDocs/oethelpdocs/help.jar!/html/testsInGeneral_tutorial.html'], '-helpbrowser');">1. General information on testing</a>
+%       <a href="matlab:web(['jar:file:///' strrep(openearthtoolsroot,filesep,'/') 'docs/OpenEarthDocs/oethelpdocs/help.jar!/html/testdefinitions_tutorial.html'], '-helpbrowser');">2. Creating a test</a>
+%       <a href="matlab:web(['jar:file:///' strrep(openearthtoolsroot,filesep,'/') 'docs/OpenEarthDocs/oethelpdocs/help.jar!/html/runningatest_tutorial.html'], '-helpbrowser');">3. Running individual tests</a>
+%       <a href="matlab:web(['jar:file:///' strrep(openearthtoolsroot,filesep,'/') 'docs/OpenEarthDocs/oethelpdocs/help.jar!/html/runtestswithmtestengine_tutorial.html'], '-helpbrowser');">4. Automated tesing with the mtest toolbox</a>
+%
+%     netCDF:
+%       <a href="matlab:web(['jar:file:///' strrep(openearthtoolsroot,filesep,'/') 'docs/OpenEarthDocs/oethelpdocs/help.jar!/html/tutorial_netCDF_create.html'], '-helpbrowser');">Creating a netCDF file</a>
+%       <a href="matlab:web(['jar:file:///' strrep(openearthtoolsroot,filesep,'/') 'docs/OpenEarthDocs/oethelpdocs/help.jar!/html/tutorial_netCDF_read_functions.html'], '-helpbrowser');">Reading a netCDF file</a>
+%
+%     print fun:
+%       <a href="matlab:web(['jar:file:///' strrep(openearthtoolsroot,filesep,'/') 'docs/OpenEarthDocs/oethelpdocs/help.jar!/html/printFigure_tutorial.html'], '-helpbrowser');">Convenient saving of figures</a>
+%
+%     tips and tricks:
+%       <a href="matlab:web(['jar:file:///' strrep(openearthtoolsroot,filesep,'/') 'docs/OpenEarthDocs/oethelpdocs/help.jar!/html/TODO_tutorial.html'], '-helpbrowser');">TODO function</a>
+%
+%  Application tutorials:
+%
+%     DUROS:
+%       <a href="matlab:web(['jar:file:///' strrep(openearthtoolsroot,filesep,'/') 'docs/OpenEarthDocs/oethelpdocs/help.jar!/html/findCrossings_tutorial.html'], '-helpbrowser');">Find crossings between arbitrary line segments</a>
+%
+%     Rijkswaterstaat:
+%       <a href="matlab:web(['jar:file:///' strrep(openearthtoolsroot,filesep,'/') 'docs/OpenEarthDocs/oethelpdocs/help.jar!/html/jarkus_tutorial_coordinates_RSP_2_RD.html'], '-helpbrowser');">Coordinate conversion from RSP to RD</a>
+%       <a href="matlab:web(['jar:file:///' strrep(openearthtoolsroot,filesep,'/') 'docs/OpenEarthDocs/oethelpdocs/help.jar!/html/jarkus_tutorial_reading_data_1.html'], '-helpbrowser');">Extracting JarKus data from netCDF (1)</a>
+%       <a href="matlab:web(['jar:file:///' strrep(openearthtoolsroot,filesep,'/') 'docs/OpenEarthDocs/oethelpdocs/help.jar!/html/jarkus_tutorial_reading_data_2.html'], '-helpbrowser');">Extracting JarKus data from netCDF (2)</a>
+%       <a href="matlab:web(['jar:file:///' strrep(openearthtoolsroot,filesep,'/') 'docs/OpenEarthDocs/oethelpdocs/help.jar!/html/jarkus_tutorial_calculate_MKL.html'], '-helpbrowser');">JarKus MKL calculations</a>
+%       <a href="matlab:web(['jar:file:///' strrep(openearthtoolsroot,filesep,'/') 'docs/OpenEarthDocs/oethelpdocs/help.jar!/html/jarkus_tutorial_calculate_volume.html'], '-helpbrowser');">JarKus volume calculations</a>
+%       <a href="matlab:web(['jar:file:///' strrep(openearthtoolsroot,filesep,'/') 'docs/OpenEarthDocs/oethelpdocs/help.jar!/html/jarkus_tutorial_plot_in_googleEarth.html'], '-helpbrowser');">Jarkus & Google Earth</a>
+%
+%     SuperTrans:
+%       <a href="matlab:web(['jar:file:///' strrep(openearthtoolsroot,filesep,'/') 'docs/OpenEarthDocs/oethelpdocs/help.jar!/html/convertCoordinates_tutorial.html'], '-helpbrowser');">Coordinate conversion</a>
+%
+%     googlePlot:
+%       <a href="matlab:web(['jar:file:///' strrep(openearthtoolsroot,filesep,'/') 'docs/OpenEarthDocs/oethelpdocs/help.jar!/html/KMLcurvedArrows_tutorial.html'], '-helpbrowser');">Animated arrows</a>
+%       <a href="matlab:web(['jar:file:///' strrep(openearthtoolsroot,filesep,'/') 'docs/OpenEarthDocs/oethelpdocs/help.jar!/html/KMLcontour3_tutorial.html'], '-helpbrowser');">KMLcontour3</a>
+%       <a href="matlab:web(['jar:file:///' strrep(openearthtoolsroot,filesep,'/') 'docs/OpenEarthDocs/oethelpdocs/help.jar!/html/KMLline_tutorial.html'], '-helpbrowser');">KMLline</a>
+%
+%     probabilistic:
+%       <a href="matlab:web(['jar:file:///' strrep(openearthtoolsroot,filesep,'/') 'docs/OpenEarthDocs/oethelpdocs/help.jar!/html/prob_calculation_tutorial.html'], '-helpbrowser');">setting up a probabilistic calculation</a>
+%
