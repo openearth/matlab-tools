feb3b9366c161d79ceebf70871c0bfee3e5b8bae
geer <geer@localhost>
2009-10-27 14:58:37 +0000

    moved tutorial on property value pairs
    svn path=/trunk/matlab/; revision=1856
    
    
    Former-commit-id: e7fc2cf7872ca77c9ee1169ae8135daeb823832d

--- general/_tutorial_how_to_write_tutorials.m
+++ general/_tutorial_how_to_write_tutorials.m
@@ -1,4 +1,68 @@
-%% 5. How to make a tutorial
-% This script is going to explain how to write a tutorial that is included in the overview at the
-% wiki.
-% We cannot do all things the same time, so the tutorial is coming soon..
+%% 4. How to make a tutorial
+% To document the various applications and engines in the OpenEarthTools toolbox users can create 
+% tutorials. Tutorials are documents describing the functionalities of applications and engines and
+% explain how these should be used. The following tutorials further explain how to write tutorials
+% and include them on the wiki of openearth and in the matlab help documentation.
+% 
+% <html>
+% <table border="0" cellpadding="5" width="100%">
+%   <tr valign="top">
+%       <td width="400">
+%           <a class="relref" href="tutorials_cell_formatting_tutorial.html" relhref="tutorials_cell_formatting_tutorial.html">How to write a tutorial</a>
+%       </td>
+%       <td width="600">
+%           General information about the use of cell formatting to publish an understandable tutorial.
+%       </td>
+%   </tr>
+%   <tr valign="top">
+%       <td width="400">
+%           <a class="relref" href="tutorials_publish_oet_style_tutorial.html" relhref="tutorials_publish_oet_style_tutorial.html">Publishing an individual tutorial</a>
+%       </td>
+%       <td width="600">
+%           Describes how to publish individual tutorials to examine the result.
+%       </td>
+%   </tr>
+%   <tr valign="top">
+%       <td width="400">
+%           <a class="relref" href="tutorials_publish_configurations_tutorial.html" relhref="tutorials_publish_configurations_tutorial.html">Publish settings</a>
+%       </td>
+%       <td width="600">
+%           Various options can be set while publishing a tutorial. Controlling for example the
+%           maximum number of output lines, or whether code needs to be executed at all is described
+%           in this tutorial.
+%       </td>
+%   </tr>
+%   <tr valign="top">
+%       <td width="400">
+%           <a class="relref" href="tutorials_include_tutorial.html" relhref="tutorials_include_tutorial.html">Including tutorials in the OET html documentation</a>
+%       </td>
+%       <td width="600">
+%           Tutorials can be published to html and included in the html tutorial pages on the
+%           openearthtools wiki and in the matlab help navigator. This tutorial describes how to
+%           include your own tutorial in the openearth tutorial pages.
+%       </td>
+%   </tr>
+% </table> 
+% </html>
+%
+% <html>
+% <style>
+% #notediv {
+%   background-color:   #D8E4F1;
+%   border:             solid; 
+%   border-width:       1px;  
+%   padding:            10px;
+%   }
+% </style>
+% <div id="notediv">
+%   <strong>
+%       <p>Tests are meant to check the basic functionalities of functions in the most simple form, so:</p> 
+%       <ul>
+%           <li>Test are not meant to show off, keep the tests as simple as possible!!!</li>
+%           <li>Do not write a test that needs user input</li>
+%           <li>Do not write a test that needs an internet connection to run</li>
+%       </ul>
+%       <p>(Tests that do need user input will be added to the ignore list for automatic testing of the toolbox)</p>
+%   </strong>
+% </div>
+% </html>
