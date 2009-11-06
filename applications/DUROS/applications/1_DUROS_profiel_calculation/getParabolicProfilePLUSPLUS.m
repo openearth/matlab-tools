daf6b56d0deffcb193da7eb8edb0fc2505fdc31c
huism_b <huism_b@localhost>
2009-11-06 16:07:11 +0000

    D++ update (geen invloed D+) - wave period geen invloed op locatie transitiepunt (xmax), zoals in D+
    svn path=/trunk/matlab/; revision=1903
    
    
    Former-commit-id: 58ebe239466f625b363f854a615e5ef016024262

--- applications/DUROS/applications/1_DUROS_profiel_calculation/getParabolicProfilePLUSPLUS.m
+++ applications/DUROS/applications/1_DUROS_profiel_calculation/getParabolicProfilePLUSPLUS.m
@@ -66,8 +66,8 @@ elseif strcmp(Plus,'-plus')
 %----------------------------------------------------------------------------------------------
 elseif strcmp(Plus,'-plusplus') & exist('x','var') %&& ~isempty(x)
     %Option to include depth contribution into the 'constants' C1, C2 and 'two'
-    c_1 = c_1*(Hsig_t/d_t)^cp_c1;
-    c_2 = c_2*(Hsig_t/d_t)^cp_c2;
+    %c_1 = c_1*(Hsig_t/d_t)^cp_c1;
+    %c_2 = c_2*(Hsig_t/d_t)^cp_c2;
     two = c_1*sqrt(c_2);
     %xmax = x0 + 250  *  (Hsig_t/c_hs)^cp_hs * (c_w/w)^cp_w * (c_d/d_t)^0.5;
     %xmax = x0 + 250  *  (Hsig_t/c_hs)^cp_hs * (Tp_t/c_tp)^cp_tp  * (c_w/w)^cp_w * (c_d/d_t)^0.5;
@@ -81,7 +81,8 @@ fallvelocitycmpt = (w/c_w)^cp_w;
 depthcmpt        = (d_t/c_d)^cp_d;    %Option to include depth contribution in the DUROS formulation (depthcmpt=1 for DUROS and D+)
 
 y      = (c_1*sqrt(waveheightcmpt*waveperiodcmpt*fallvelocitycmpt*depthcmpt*(x-x0)+c_2)-two) / (c_hs/Hsig_t);
-xmax   = x0 + xref  *  waveheightcmpt^-1*waveperiodcmpt^-1*fallvelocitycmpt^-1*depthcmpt^-1;
+%xmax   = x0 + xref  *  waveheightcmpt^-1*waveperiodcmpt^-1*fallvelocitycmpt^-1*depthcmpt^-1;
+xmax   = x0 + xref  *  waveheightcmpt^-1*fallvelocitycmpt^-1*depthcmpt^-1;
 
 % round to 8 decimal digits to prevent rounding problems later on
 y = roundoff(y, 8);
