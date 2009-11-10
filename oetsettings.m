80301cb7c60c038b0389f198b345ee86cb93c089
geer <geer@localhost>
2009-11-10 08:00:14 +0000

    removed catchment at try - catch statement for backwards compatibility
    svn path=/trunk/matlab/; revision=1911
    
    
    Former-commit-id: 63e3d68cdfef3131cc35bcc36ed031693d7923b0

--- oetsettings.m
+++ oetsettings.m
@@ -103,7 +103,7 @@ function oetsettings(varargin)
         if DocumentationExists && exist('builddocsearchdb','file')
             builddocsearchdb(fullfile(openearthtoolsroot,'docs','OpenEarthDocs','oethelpdocs'));
         end
-    catch me %#ok<NASGU>
+    catch %#ok<CTCH>
         warning('OET:NoSearchDB',['Could not build search database because ',openearthtoolsroot,' is read-only.', char(10),...
             'the OpenEarthTools help documentation is still avilable in the matlab help navigator.']);
     end
