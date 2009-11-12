15056fbec88af150f7e43bc0ef28702ff86b4c6c
boer_g <boer_g@localhost>
2009-11-12 14:09:36 +0000

    added nearest neighbor griddata for large matrices (optimized for memory instead of for speed)
    svn path=/trunk/matlab/; revision=1926
    
    
    Former-commit-id: 9a03298de2a9cbffa1ecbbd2e68db18b5a4d5267

---
+++ general/grid_fun/griddata_nearest.m
@@ -0,0 +1,88 @@
+function ZI = griddata_nearest(X,Y,Z,XI,YI)
+%GRIDDATA_NEAREST Data gridding and surface fitting.
+%
+%     ZI = GRIDDATA_NEAREST(X,Y,Z,XI,YI) 
+%
+%   fits a surface of the form Z = F(X,Y) to the
+%   data in the (usually) nonuniformly-spaced vectors (X,Y,Z). GRIDDATA_NEAREST
+%   interpolates this surface at the points specified by (XI,YI) to produce
+%   ZI.  The surface always goes through the data points. XI and YI are
+%   usually a uniform grid (as produced by MESHGRID) and is where GRIDDATA_NEAREST
+%   gets its name.
+%
+%   Unlike GRIDDATA, GRIDDATA_NEAREST does not connect 
+%   all points [X,Y] in a mesh (delaunay triangulation)  
+%   before interpolation. Instead GRIDDATA_NEAREST 
+%   finds the closest point in the random set of points 
+%   [X,Y] for all points [XI,YI]  one-at-a-time and 
+%   copies the associated value.
+%
+%   Note: GRIDDATA_NEAREST is much slower than GRIDDATA, but
+%   owes it existence to those cases where 
+%    i) GRIDDATA leads to gives DELAUNAY triangulation errors.
+%   ii) laaarge X and Y matrixes lead to MEMORY issues in GRIDDATA
+%
+%See also: GRIDDATA
+
+%% Copyright notice
+%   --------------------------------------------------------------------
+%   Copyright (C) 2009 Delft University of Technology
+%       Gerben J. de Boer
+%
+%       g.j.deboer@tudelft.nl	
+%
+%       Fluid Mechanics Section
+%       Faculty of Civil Engineering and Geosciences
+%       PO Box 5048
+%       2600 GA Delft
+%       The Netherlands
+%
+%   This library is free software: you can redistribute it and/or modify
+%   it under the terms of the GNU General Public License as published by
+%   the Free Software Foundation, either version 3 of the License, or
+%   (at your option) any later version.
+%
+%   This library is distributed in the hope that it will be useful,
+%   but WITHOUT ANY WARRANTY; without even the implied warranty of
+%   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%   GNU General Public License for more details.
+%
+%   You should have received a copy of the GNU General Public License
+%   along with this library.  If not, see <http://www.gnu.org/licenses/>.
+%   --------------------------------------------------------------------
+
+% This tool is part of <a href="http://OpenEarth.Deltares.nl">OpenEarthTools</a>.
+% OpenEarthTools is an online collaboration to share and manage data and 
+% programming tools in an open source, version controlled environment.
+% Sign up to recieve regular updates of this function, and to contribute 
+% your own tools.
+
+% TO DO: find closest of 3 points and interpolate value using
+%        analytical fit of linear surface (if inside the triangle), 
+%        or use inverse distance
+
+   OPT.ndisp = 100;
+
+   ZI   = repmat(NaN,(size(XI)));
+   R    = repmat(NaN,(size(X )));
+   npix = length(XI(:));
+
+   for ipix = 1:npix
+   
+      R = sqrt((XI(ipix) - X).^2 + ...
+               (YI(ipix) - Y).^2);
+               
+      % Wondering whether would it be faster to 
+      % * update R with the distance between  XI(ipix) and XI(ipix+1), or is min() the slowest process?
+      % * do not use pixel by pixel but use larger chunks
+      
+      [pix.distance,pix.index] = min(R(:));
+      ZI(ipix)                 = Z(pix.index);
+
+      if mod(ipix,floor(npix/OPT.ndisp))==0
+      disp(['processed ',num2str(100.*ipix./npix),' % in ',num2str(toc),' s'])
+      end
+
+   end
+
+%% EOF
