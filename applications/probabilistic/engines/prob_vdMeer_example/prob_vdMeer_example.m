1a93eb10f0e69114ae908a8f8c025b0fa6a83894
heijer <heijer@localhost>
2009-12-04 14:29:20 +0000

    probabilistic example with van der Meer formula replaced by one file with the Z-function as subfunction
    svn path=/trunk/matlab/; revision=2015
    
    
    Former-commit-id: db04fef9e6f219540f0cb456067d45acf941cef4

--- applications/probabilistic/engines/prob_vdMeer_example/prob_vdMeer_example.m
+++ applications/probabilistic/engines/prob_vdMeer_example/prob_vdMeer_example.m
@@ -1,10 +1,10 @@
-function stochast = prob_vdMeer_example_stochast(varargin)
-%PROB_VDMEER_EXAMPLE_STOCHAST  create stochast variable.
+function [resultMC resultFORM] = prob_vdMeer_example(varargin)
+%PROB_VDMEER_EXAMPLE  example of probabilistic calculation with van der Meer formula
 %
 %   More detailed description goes here.
 %
 %   Syntax:
-%   varargout = prob_vdMeer_example_stochast(varargin)
+%   varargout = prob_vdMeer_example(varargin)
 %
 %   Input:
 %   varargin  =
@@ -13,16 +13,16 @@ function stochast = prob_vdMeer_example_stochast(varargin)
 %   varargout =
 %
 %   Example
-%   prob_vdMeer_example_stochast
+%   prob_vdMeer_example
 %
-%   See also
+%   See also 
 
 %% Copyright notice
 %   --------------------------------------------------------------------
 %   Copyright (C) 2009 Delft University of Technology
-%       C.(Kees) den Heijer
+%       Kees den Heijer
 %
-%       C.denHeijer@TUDelft.nl
+%       C.denHeijer@TUDelft.nl	
 %
 %       Faculty of Civil Engineering and Geosciences
 %       P.O. Box 5048
@@ -43,15 +43,15 @@ function stochast = prob_vdMeer_example_stochast(varargin)
 %   License along with this library. If not, see <http://www.gnu.org/licenses/>.
 %   --------------------------------------------------------------------
 
-% This tools is part of <a href="http://OpenEarth.Deltares.nl">OpenEarthTools</a>.
-% OpenEarthTools is an online collaboration to share and manage data and
+% This tool is part of <a href="http://OpenEarth.nl">OpenEarthTools</a>.
+% OpenEarthTools is an online collaboration to share and manage data and 
 % programming tools in an open source, version controlled environment.
-% Sign up to recieve regular updates of this function, and to contribute
+% Sign up to recieve regular updates of this function, and to contribute 
 % your own tools.
 
 %% Version <http://svnbook.red-bean.com/en/1.5/svn.advanced.props.special.keywords.html>
-% Created: 03 Sep 2009
-% Created with Matlab version: 7.4.0.287 (R2007a)
+% Created: 04 Dec 2009
+% Created with Matlab version: 7.7.0.471 (R2008b)
 
 % $Id$
 % $Date$
@@ -60,7 +60,8 @@ function stochast = prob_vdMeer_example_stochast(varargin)
 % $HeadURL$
 % $Keywords: $
 
-%%
+%% define the stochasts
+% create a structure with fields 'Name', 'Distr' and 'Params'
 stochast = struct(...
     'Name', {
     'RhoS'...       % [kg/m3] RhoS density sediment
@@ -72,17 +73,17 @@ stochast = struct(...
     'N'...          % [-] N        number of waves
     'H'...          % [m] H        significant wave height
     'D'...          % [m] D        stone size
-    'Cpl'		% [-] Cpl      constant in vdMeer formula
+    'Cpl'		    % [-] Cpl      constant in vdMeer formula
     },...
     'Distr', {
     @norm_inv...       % [kg/m3] RhoS density sediment
     @norm_inv...       % [kg/m3] RhoW density water
     @norm_inv...       % [-] TanAlfa  slope of structure
     @norm_inv...       % [-] Steep    wave steepness
-    @logn_inv...        % [-] P        notional permeability
+    @logn_inv...       % [-] P        notional permeability
     @deterministic...  % [-] S        damage number
     @deterministic...  % [-] N        number of waves
-    @exp_inv...            % [m] H        significant wave height
+    @exp_inv...        % [m] H        significant wave height
     @norm_inv...       % [m] D        stone size
     @norm_inv...	   % [-] Cpl      constant in vdMeer formula
     },...
@@ -92,7 +93,7 @@ stochast = struct(...
     {0.25 0.0125}...% [-] TanAlfa  slope of structure
     {0.05 0.01}...  % [-] Steep    wave steepness
     {{@logn_moments2lambda 0.1  0.05} {@logn_moments2zeta 0.1  0.05}}...  % [-] P        notional permeability
-    {2  }...        % [-] S        damage number
+    {5  }...        % [-] S        damage number
     {7000}...       % [-] N        number of waves
     {1 3.83}...     % [m] H        significant wave height
     {0.6 0.05}...   % [m] D        stone size
@@ -100,12 +101,28 @@ stochast = struct(...
     } ...
     );
 
+%% main matter: running the calculation
+% run the calculation using Monte Carlo
+resultMC = MC(stochast,...
+    'NrSamples', 3e4,...
+    'x2zFunction', @prob_vdMeer_example_x2z);
+
+% run the calculation using FORM
+resultFORM = FORM(stochast,...
+    'x2zFunction', @prob_vdMeer_example_x2z);
+
+%% Z-function
+function z = prob_vdMeer_example_x2z(x, varnames, Resistance, varargin)
+
+%% retrieve calculation values
+for i = 1:size(x,2)
+    samples.(varnames{i}) = x(:,i);
+end
+
 %%
-OPT = struct(...
-    'active', true(size(stochast)));
-
-OPT = setProperty(OPT, varargin{:});
-
-for i = find(~OPT.active)
-    stochast(i).Distr = @deterministic;
+g = 9.81;                               %[m/s2]
+for i = 1:size(x,1)
+    Delta = (samples.RhoS(i) - samples.RhoW(i)) / samples.RhoW(i);    % [-] relative density
+    Ksi = samples.TanAlfa(i)/sqrt(samples.Steep(i));      % [-] Iribarren number
+    z(i,:) = samples.Cpl(i)*samples.P(i)^0.18*(samples.S(i)/sqrt(samples.N(i)))^0.2*Ksi^(-0.5)-samples.H(i)/Delta/samples.D(i); %[-] vdMeer
 end
