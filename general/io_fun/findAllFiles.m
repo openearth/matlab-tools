ea948841ad510b2c5896b375b0a076a5c75484e3
m.vankoningsveld@tudelft.nl <m.vankoningsveld@tudelft.nl@localhost>
2009-12-18 07:29:56 +0000

    Routine added to quickly find all files in a directory recursively (includes possibility to exclude files of a certain type)
    svn path=/trunk/matlab/; revision=2076
    
    
    Former-commit-id: 29f404c361b98e939058651f6770f4056341cb4a

---
+++ general/io_fun/findAllFiles.m
@@ -0,0 +1,94 @@
+function filenames = findAllFiles(varargin)
+%UNTITLED  One line description goes here.
+%
+%   More detailed description goes here.
+%
+%   Syntax:
+%   varargout = Untitled(varargin)
+%
+%   Input:
+%   varargin  =
+%
+%   Output:
+%   varargout =
+%
+%   Example
+%   Untitled
+%
+%   See also
+
+%% Copyright notice
+%   --------------------------------------------------------------------
+%   Copyright (C) 2009 Van Oord
+%       Mark van Koningsveld
+%
+%       mrv@vanoord.com
+%
+%       Watermanweg 64
+%       POBox 8574
+%       3009 AN Rotterdam
+%       Netherlands
+%
+%   This library is free software: you can redistribute it and/or
+%   modify it under the terms of the GNU Lesser General Public
+%   License as published by the Free Software Foundation, either
+%   version 2.1 of the License, or (at your option) any later version.
+%
+%   This library is distributed in the hope that it will be useful,
+%   but WITHOUT ANY WARRANTY; without even the implied warranty of
+%   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+%   Lesser General Public License for more details.
+%
+%   You should have received a copy of the GNU Lesser General Public
+%   License along with this library. If not, see <http://www.gnu.org/licenses/>.
+%   --------------------------------------------------------------------
+
+% This tools is part of VOTools which is the internal clone of <a href="http://OpenEarth.Deltares.nl">OpenEarthTools</a>.
+% OpenEarthTools is an online collaboration to share and manage data and
+% programming tools in an open source, version controlled environment.
+% Sign up to recieve regular updates of this function, and to contribute
+% your own tools.
+
+%% Version <http://svnbook.red-bean.com/en/1.5/svn.advanced.props.special.keywords.html>
+% Created: 08 Nov 2009
+% Created with Matlab version: 7.7.0.471 (R2008b)
+
+% $Id$
+% $Date$
+% $Author$
+% $Revision$
+% $HeadURL$
+% $Keywords: $
+
+%% settings
+% defaults
+OPT = struct(...
+    'pattern_excl', {{[filesep,'.svn']}}, ...                 % description of input argument 1
+    'pattern_incl', {'*.dat'}, ...                          % description of input argument 2
+    'basepath', 'D:\checkouts\VO-rawdata\waveclimates\' ... % description of input argument 3
+    );
+
+% overrule default settings by property pairs, given in varargin
+OPT = setProperty(OPT, varargin{:});
+
+%% Find all subdirs in basepath
+%---------------------------------------------
+
+if ispc
+    [a b] = system(['dir /b /a /s ' '"' OPT.basepath filesep OPT.pattern_incl '"']);
+else
+    disp('Not supported yet for this operating system')
+end
+
+%% Exclude the .svn directories from the path
+% read path as cell
+s = strread(b, '%s', 'delimiter', char(10));  
+
+% clear cells which contain OPT.pattern_excl
+for imask = 1:length(OPT.pattern_excl)
+    OPT.pattern = OPT.pattern_excl{imask};
+    s = s(cellfun('isempty', regexp(s, [OPT.pattern]))); % keep only paths not containing [filesep '.svn']
+end
+
+%% return cell with resulting files (including pathnames)
+filenames = s;
