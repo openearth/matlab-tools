a8d19125462a1036a120f305b97540fc07fe8252
winsemi <winsemi@localhost>
2010-01-18 17:02:01 +0000

    svn path=/trunk/matlab/; revision=2153
    Former-commit-id: d03e2fd6b5d3d4aafe5bfe6f2cc81099264c735e

---
+++ applications/FEWS-World/plot_test.m
@@ -0,0 +1,18 @@
+% Test for plotting in google earth
+
+% 
+lat = [0:100];
+lon = [0:100];
+[loni,lati] = meshgrid(lon,lat);
+dist = sqrt(loni.^2+lati.^2);
+sinx = sin(dist/20);
+
+KMLpcolor  (lati,loni,sinx,... % at corners for z !!
+                   'fileName','3D_test.kml',...
+                    'kmlName','depth [m]',...
+                  'lineAlpha',.6,...
+                       'cLim',[0 2],...
+                  'lineWidth',0,... % to prevent rastering of the pixels (WHY?)
+                'polyOutline',true,...
+                   'polyFill',true);
+
