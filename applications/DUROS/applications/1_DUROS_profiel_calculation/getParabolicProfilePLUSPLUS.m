91314c7d79fcb23a3fe8c3dad568fbb10ffd6488
huism_b <huism_b@localhost>
2010-01-21 16:33:13 +0000

    DRAFT version DuneErosionSettings('set','c_1',    0.6); DuneErosionSettings('set','c_2',    50); DuneErosionSettings('set','d',      depth); % (incl. opzet) DuneErosionSettings('set','xref',   270); DuneErosionSettings('set','Plus','-plusplus'); DuneErosionSettings('set','ParabolicProfileFcn',@getParabolicProfilePLUSPLUS); DuneErosionSettings('set','rcParabolicProfileFcn',@getRcParabolicProfilePLUSPLUS); DuneErosionSettings('set','invParabolicProfileFcn',@invParabolicProfilePLUSPLUS);
    svn path=/trunk/matlab/; revision=2179
    
    
    Former-commit-id: edd88454edc277eaa572ccfb28add2ef2cb21caa

--- applications/DUROS/applications/1_DUROS_profiel_calculation/getParabolicProfilePLUSPLUS.m
+++ applications/DUROS/applications/1_DUROS_profiel_calculation/getParabolicProfilePLUSPLUS.m
@@ -1,4 +1,4 @@
-function [xmax, z, Tp_t] = getParabolicProfile(WL_t, Hsig_t, Tp_t, w, x0, x)
+function [xmax, z, Tp_t] = getParabolicProfilePLUSPLUS2(WL_t, Hsig_t, Tp_t, w, x0, x)
 %GETPARABOLICPROFILE    routine to create the parabolic DUROS (-plus) profile 
 % 
 % This routine returns the most seaward x-coordinate of the parabolic DUROS
@@ -31,6 +31,8 @@ function [xmax, z, Tp_t] = getParabolicProfile(WL_t, Hsig_t, Tp_t, w, x0, x)
 Plus                                    = DuneErosionSettings('get','Plus');
 [c_hs c_tp c_w c_d c_1 c_2]             = DuneErosionSettings('get','c_hs','c_tp','c_w','c_d','c_1','c_2');
 [cp_hs cp_tp cp_w cp_d cp_c1 cp_c2]     = DuneErosionSettings('get','cp_hs','cp_tp','cp_w','cp_d','cp_c1','cp_c2');
+[predict_cpd]                           = DuneErosionSettings('get','predict_cpd');
+[slope]                                 = DuneErosionSettings('get','slope');
 [d_t]                                   = DuneErosionSettings('get','d');
 [xref]                                  = DuneErosionSettings('get','xref');
 [xmax,y,waveheightcmpt,waveperiodcmpt,fallvelocitycmpt,depthcmpt] = deal([]);
@@ -41,14 +43,13 @@ Plus                                    = DuneErosionSettings('get','Plus');
 %---------------------------------------------------------------------------------------------- 
 if strcmp(Plus,'')
     two = c_1*sqrt(c_2); % term in formulation which is 2 by approximation; by using this expression, the profile will exactly cross (x0,0)
-    Tp_t = 12;
+    %Tp_t = 12;
     c_tp = 12;
     cp_tp = 0;   %waveperiodcmpt = 1
     d_t = 25;
     c_d = 25;
     cp_d = 0;    %depthcmpt = 1
-    %xmax = x0 + 250*(Hsig_t/c_hs)^cp_hs*(c_w/w)^cp_w;
-
+    depthcmpt        = (d_t/c_d)^cp_d;
 
 %% -------------------------------------------------------------------------------------------- 
 %----------------------------------------DUROS plus-------------------------------------------- 
@@ -58,19 +59,23 @@ elseif strcmp(Plus,'-plus')
     d_t = 25;
     c_d = 25;
     cp_d = 0;    %depthcmpt = 1
-    %xmax = x0 + 250*(Hsig_t/c_hs)^cp_hs*(c_w/w)^cp_w;
-
+    depthcmpt        = (d_t/c_d)^cp_d;
 
 %% --------------------------------------------------------------------------------------------
 %-------------------------DUROS plusplus (for testing purposes only)---------------------------
 %----------------------------------------------------------------------------------------------
-elseif strcmp(Plus,'-plusplus') & exist('x','var') %&& ~isempty(x)
-    %Option to include depth contribution into the 'constants' C1, C2 and 'two'
-    %c_1 = c_1*(Hsig_t/d_t)^cp_c1;
-    %c_2 = c_2*(Hsig_t/d_t)^cp_c2;
-    two = c_1*sqrt(c_2);
-    %xmax = x0 + 250  *  (Hsig_t/c_hs)^cp_hs * (c_w/w)^cp_w * (c_d/d_t)^0.5;
-    %xmax = x0 + 250  *  (Hsig_t/c_hs)^cp_hs * (Tp_t/c_tp)^cp_tp  * (c_w/w)^cp_w * (c_d/d_t)^0.5;
+elseif strcmp(Plus,'-plusplus')
+    two              = c_1*sqrt(c_2);
+    depthcmpt        = 1;
+    HS_d             = (Hsig_t/d_t);
+    delta            = max(min((HS_d-0.40)/0.06,1),0);
+    xref2            = (1-delta)*xref + delta*((176*(c_d/d_t)+15));
+    xref             = max(xref,xref2);
+elseif strcmp(Plus,'-plusplus3')
+    two              = c_1*sqrt(c_2);
+    HS_d             = (Hsig_t/d_t);
+    depthcmpt        = 1;
+    xref             = max(xref,(5500*HS_d-2155));
 else
     error('Warning: variable "Plus" should be either '''' or ''-plus'' or ''-plusplus''')
 end
@@ -80,9 +85,8 @@ waveperiodcmpt   = (c_tp/Tp_t)^cp_tp;
 fallvelocitycmpt = (w/c_w)^cp_w;
 depthcmpt        = (d_t/c_d)^cp_d;    %Option to include depth contribution in the DUROS formulation (depthcmpt=1 for DUROS and D+)
 
+xmax   = x0 + xref  *  waveheightcmpt^-1*fallvelocitycmpt^-1*depthcmpt^-1; %xmax   = x0 + xref  *  waveheightcmpt^-1*waveperiodcmpt^-1*fallvelocitycmpt^-1*depthcmpt^-1;
 y      = (c_1*sqrt(waveheightcmpt*waveperiodcmpt*fallvelocitycmpt*depthcmpt*(x-x0)+c_2)-two) / (c_hs/Hsig_t);
-%xmax   = x0 + xref  *  waveheightcmpt^-1*waveperiodcmpt^-1*fallvelocitycmpt^-1*depthcmpt^-1;
-xmax   = x0 + xref  *  waveheightcmpt^-1*fallvelocitycmpt^-1*depthcmpt^-1;
 
 % round to 8 decimal digits to prevent rounding problems later on
 y = roundoff(y, 8);
