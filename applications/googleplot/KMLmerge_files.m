bdb63f75999e30f4d91ec4a553cf0f078791bc58
boer_g <boer_g@localhost>
2010-02-24 09:24:05 +0000

    optionally add folder names to each KML that is merged.
    svn path=/trunk/matlab/; revision=2274
    
    
    Former-commit-id: f937f21b44fc678d388d574c64ef4eba19f0deb3

--- applications/googleplot/KMLmerge_files.m
+++ applications/googleplot/KMLmerge_files.m
@@ -1,28 +1,34 @@
 function varargout = KMLmerge_files(varargin)
 %KMLMERGE_FILES   Merges all KML files in a certain directory
 %
-%   kmlmerge_files(<keyword,value>)
+%   KMLmerge_files(<keyword,value>)
 %
 % Merges all KML files in a certain directory.
+% For the <keyword,value> pairs and their defaults call
 %
-%See also: googlePLot
-
-OPT.fileName          = [];
-OPT.kmlName           = [];
-OPT.sourceFiles       = [];
-OPT.description       = '';
-OPT.deleteSourceFiles = false;
+%    OPT = KMLmerge_files()
+%
+%See also: googleplot
 
 %% set properties
-[OPT, Set, Default] = setProperty(OPT, varargin{:});
 
-if nargin==0 & nargout==1
-    varargout = {OPT};
-    return
-end
+   OPT.fileName          = [];
+   OPT.kmlName           = [];
+   OPT.sourceFiles       = [];
+   OPT.description       = '';
+   OPT.deleteSourceFiles = false;
+   OPT.foldernames       = {};
+
+   [OPT, Set, Default] = setProperty(OPT, varargin{:});
+   
+   if nargin==0 & nargout==1
+       varargout = {OPT};
+       return
+   end
 
 %% source files
-if isempty(OPT.sourceFiles)
+
+   if isempty(OPT.sourceFiles)
     [sourceName,sourcePath] = uigetfile('*.kml','Select KML files to merge','MultiSelect','on');
     if ischar(sourceName)
         OPT.sourceFiles{1} = fullfile(sourcePath,sourceName);
@@ -31,68 +37,82 @@ if isempty(OPT.sourceFiles)
             OPT.sourceFiles{ii,1} = fullfile(sourcePath,sourceName{ii});
         end
     end
-end
+   end
+   
+   if ~length(OPT.foldernames)==length(OPT.sourceFiles) & ~isempty(OPT.foldernames)
+      error('length of foldernames does ot match number of files.')
+   end
 
-%% filename
-% gui for filename, if not set yet
-if isempty(OPT.fileName)
-    [fileName, filePath] = uiputfile({'*.kml','KML file';'*.kmz','Zipped KML file'},'Save as','merged_files.kmz');
-    OPT.fileName = fullfile(filePath,fileName);
-end
-% set kmlName if it is not set yet
-if isempty(OPT.kmlName)
-    [ignore OPT.kmlName] = fileparts(OPT.fileName);
-end
+%% filename, gui for filename, if not set yet
+
+   if isempty(OPT.fileName)
+       [fileName, filePath] = uiputfile({'*.kml','KML file';'*.kmz','Zipped KML file'},'Save as','merged_files.kmz');
+       OPT.fileName = fullfile(filePath,fileName);
+   end
+
+%% set kmlName if it is not set yet
+
+   if isempty(OPT.kmlName)
+       [ignore OPT.kmlName] = fileparts(OPT.fileName);
+   end
 
 %% Write the new file
-fid0=fopen(OPT.fileName,'w');
-OPT_header = struct(...
-       'name',OPT.kmlName,...
-'description',OPT.description);
-fprintf(fid0,'%s',KML_header(OPT_header));
 
-for ii = 1:length(OPT.sourceFiles)
-if exist(OPT.sourceFiles{ii})
-    contents = textread(OPT.sourceFiles{ii},'%s','delimiter','\n','bufsize',1e6);
-    cutoff = strfind(contents,'Document');
- 
-    flag = true;
-    for jj = 1:length(contents)
-        other_flag = true;
-        if ~isempty(cutoff{jj})
-            contents{jj} = strrep(contents{jj},'<Document>','');
-            contents{jj} = strrep(contents{jj},'</Document>','');
-            flag = ~flag;
-            other_flag = false;
-        end
-        if flag&&other_flag
-            contents{jj} = [];
-        end  
-    end
+   fid0=fopen(OPT.fileName,'w');
+   OPT_header = struct(...
+          'name',OPT.kmlName,...
+   'description',OPT.description);
+   fprintf(fid0,'%s',KML_header(OPT_header));
+   
+   for ii = 1:length(OPT.sourceFiles)
+   if exist(OPT.sourceFiles{ii})
+       contents = textread(OPT.sourceFiles{ii},'%s','delimiter','\n','bufsize',1e6);
+       cutoff = strfind(contents,'Document');
     
-    fprintf(fid0,'%s','<Folder>');
-    fprintf(fid0,'   %s\n',contents{:});
-    fprintf(fid0,'%s','</Folder>');
-else
-   disp(['Does not exist:',OPT.sourceFiles{ii}])
-end
-end
+       flag = true;
+       for jj = 1:length(contents)
+           other_flag = true;
+           if ~isempty(cutoff{jj})
+               contents{jj} = strrep(contents{jj},'<Document>','');
+               contents{jj} = strrep(contents{jj},'</Document>','');
+               flag = ~flag;
+               other_flag = false;
+           end
+           if flag&&other_flag
+               contents{jj} = [];
+           end  
+       end
+       
+       fprintf(fid0,'%s','<Folder>');
+       if ~isempty(OPT.foldernames)
+       fprintf(fid0,'<name>%s</name>',OPT.foldernames{ii});
+       end
+       fprintf(fid0,'   %s\n',contents{:});
+       fprintf(fid0,'%s','</Folder>');
+   else
+      disp(['Does not exist:',OPT.sourceFiles{ii}])
+   end
+   end
 
-% FOOTER
-fprintf(fid0,'%s',KML_footer);
-% close KML
-fclose(fid0);
+%% close KML
+
+   fprintf(fid0,'%s',KML_footer);
+   fclose(fid0);% close KML
 
 %% delete old files?
-if OPT.deleteSourceFiles
+
+   if OPT.deleteSourceFiles
     delete(OPT.sourceFiles{:})
-end
+   end
 
 %% compress to kmz?
-if strcmpi   (OPT.fileName(end),'z')
-    movefile (OPT.fileName,[OPT.fileName(1:end-3) 'kml'])
-    zip      (OPT.fileName,[OPT.fileName(1:end-3) 'kml']);
-    movefile([OPT.fileName '.zip'],OPT.fileName)
-    delete  ([OPT.fileName(1:end-3) 'kml'])
-end
+
+   if strcmpi  ( OPT.fileName(end-2:end),'kmz')
+       movefile( OPT.fileName,[OPT.fileName(1:end-3) 'kml'])
+       zip     ( OPT.fileName,[OPT.fileName(1:end-3) 'kml']);
+       movefile([OPT.fileName '.zip'],OPT.fileName)
+       delete  ([OPT.fileName(1:end-3) 'kml'])
+   end
+   
+%% EOF   
 
