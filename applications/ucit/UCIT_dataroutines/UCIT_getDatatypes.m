99cf9f79a6912338500cd33a4202b56932201626
baart_f <baart_f@localhost>
2010-03-22 11:11:23 +0000

    set all ports to 8080
    svn path=/trunk/matlab/; revision=2328
    
    
    Former-commit-id: 75040513e687886b9a54bb271123321d34d97876

--- applications/ucit/UCIT_dataroutines/UCIT_getDatatypes.m
+++ applications/ucit/UCIT_dataroutines/UCIT_getDatatypes.m
@@ -52,7 +52,7 @@ function [datatypes] = UCIT_getDatatypes
    datatypes.transect.names  {1} =  'Jarkus Data';
    datatypes.transect.urls   {1} =  'http://dtvirt5.deltares.nl:8080/thredds/dodsC/opendap/rijkswaterstaat/jarkus/profiles/transect.nc';
    datatypes.transect.areas  {1} =  '';
-   datatypes.transect.catalog{1} =  'http://opendap.deltares.nl/thredds/catalog/opendap/rijkswaterstaat/jarkus/profiles/catalog.xml';
+   datatypes.transect.catalog{1} =  'http://opendap.deltares.nl:8080/thredds/catalog/opendap/rijkswaterstaat/jarkus/profiles/catalog.xml';
    datatypes.transect.ldbs{1}    =  'http://dtvirt5.deltares.nl:8080/thredds/dodsC/opendap/deltares/landboundaries/holland_fillable.nc';
    datatypes.transect.axes{1}    =  1E5*[-0.239487 2.901701 2.999500 6.787223];
    
@@ -77,7 +77,7 @@ function [datatypes] = UCIT_getDatatypes
 
    datatypes.grid.names{1}       =  'Jarkus';
    datatypes.grid.urls {1}       =  'http://opendap.deltares.nl:8080/thredds/dodsC/opendap/rijkswaterstaat/jarkus/grids/';
-   datatypes.grid.catalog{1}     =  'http://opendap.deltares.nl/thredds/catalog/opendap/rijkswaterstaat/jarkus/grids/catalog.xml';
+   datatypes.grid.catalog{1}     =  'http://opendap.deltares.nl:8080/thredds/catalog/opendap/rijkswaterstaat/jarkus/grids/catalog.xml';
    datatypes.grid.ldbs{1}        =  'http://dtvirt5.deltares.nl:8080/thredds/dodsC/opendap/deltares/landboundaries/holland_fillable.nc';
    datatypes.grid.axes{1}        =  1E5*[-0.239487 2.901701 2.999500 6.787223];
    datatypes.grid.cellsize{1}    =  20;
@@ -86,7 +86,7 @@ function [datatypes] = UCIT_getDatatypes
 
    datatypes.grid.names{2}       =  'Vaklodingen';
    datatypes.grid.urls {2}       =  'http://dtvirt5.deltares.nl:8080/thredds/dodsC/opendap/rijkswaterstaat/vaklodingen/';
-   datatypes.grid.catalog{2}     =  'http://dtvirt5.deltares.nl/thredds/catalog/opendap/rijkswaterstaat/vaklodingen/catalog.xml';
+   datatypes.grid.catalog{2}     =  'http://dtvirt5.deltares.nl:8080/thredds/catalog/opendap/rijkswaterstaat/vaklodingen/catalog.xml';
    datatypes.grid.ldbs{2}        =  'http://dtvirt5.deltares.nl:8080/thredds/dodsC/opendap/deltares/landboundaries/holland_fillable.nc';
    datatypes.grid.axes{2}        =  1E5*[-0.239487 2.901701 2.999500 6.787223];
    datatypes.grid.cellsize{2}    =  20;
@@ -95,7 +95,7 @@ function [datatypes] = UCIT_getDatatypes
 
    datatypes.grid.names{3}       =  'AHN100';
    datatypes.grid.urls {3}       =  'http://opendap.deltares.nl:8080/thredds/dodsC/opendap/tno/ahn100m/mv100.nc';
-   datatypes.grid.catalog{3}     =  'http://opendap.deltares.nl/thredds/catalog/opendap/tno/ahn100m/catalog.xml';
+   datatypes.grid.catalog{3}     =  'http://opendap.deltares.nl:8080/thredds/catalog/opendap/tno/ahn100m/catalog.xml';
    datatypes.grid.ldbs{3}        =  'http://dtvirt5.deltares.nl:8080/thredds/dodsC/opendap/deltares/landboundaries/holland_fillable.nc';
    datatypes.grid.axes{3}        =  1E5*[-0.239487 2.901701 2.999500 6.787223];
    datatypes.grid.cellsize{3}    =  100;
@@ -103,7 +103,7 @@ function [datatypes] = UCIT_getDatatypes
 
    datatypes.grid.names{4}       =  'AHN250'; % note 250 is in 100 directory on server
    datatypes.grid.urls {4}       =  'http://opendap.deltares.nl:8080/thredds/dodsC/opendap/tno/ahn100m/mv250.nc';
-   datatypes.grid.catalog{4}     =  'http://opendap.deltares.nl/thredds/catalog/opendap/tno/ahn100m/catalog.xml';
+   datatypes.grid.catalog{4}     =  'http://opendap.deltares.nl:8080/thredds/catalog/opendap/tno/ahn100m/catalog.xml';
    datatypes.grid.ldbs{4}        =  'http://dtvirt5.deltares.nl:8080/thredds/dodsC/opendap/deltares/landboundaries/holland_fillable.nc';
    datatypes.grid.axes{4}        =  1E5*[-0.239487 2.901701 2.999500 6.787223];
    datatypes.grid.cellsize{4}    =  250;
