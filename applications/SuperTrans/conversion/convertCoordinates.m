9a461979012a3d34470014d5630ea5e022154f76
boer_g <boer_g@localhost>
2010-04-19 09:47:07 +0000

    added small table of most used projections, replaced OPT by log in documentation to avoid conflicts with setProperty when cuttting/pasting code
    svn path=/trunk/matlab/; revision=2461
    
    
    Former-commit-id: 4b3b0d113ed4e3a0905fa696b2f358676dc39984

--- applications/SuperTrans/conversion/convertCoordinates.m
+++ applications/SuperTrans/conversion/convertCoordinates.m
@@ -1,7 +1,7 @@
 function [x2,y2,OPT]=convertCoordinates(x1,y1,varargin)
 %CONVERTCOORDINATES transformation between coordinate systems
 %
-% [x2,y2,<OPT>] = convertCoordinatesNew(x1,y1,'keyword','value')
+% [x2,y2,<log>] = convertCoordinatesNew(x1,y1,'keyword','value')
 %
 % Note 1: Beware of the Lon-Lat order of in- and output arguments!
 % Note 2: (x1,y1) can be vectors or matrices.
@@ -13,18 +13,18 @@ function [x2,y2,OPT]=convertCoordinates(x1,y1,varargin)
 %
 % x1,y1 : values of the coordinates to be transformed   , either X-Y or Lon-Lat.
 % x2,y2 : values of the coordinates after transformation, either X-Y or Lon-Lat.
-% OPT   : contains all conversion parameters that were used.
-%         To check this output, use 'var2evalstr(OPT)'.
+% log   : contains all conversion parameters that were used.
+%         To check this output, use 'var2evalstr(log)'.
 % 
 % Optionally the data structure with EPSG codes van be pre-loaded, this 
 % greatly speeds up the routine if many calls are made. The call is either 
 %
 %    EPSG        = load('EPSG');
-%    [x2,y2,OPT] = convertCoordinates(x1,y1,EPSG,'keyword','value')
+%    [x2,y2,log] = convertCoordinates(x1,y1,EPSG,'keyword','value')
 %                  
 %    or:
 %
-%    [x2,y2,OPT] = convertCoordinates(x1,y1,     'keyword','value')
+%    [x2,y2,log] = convertCoordinates(x1,y1,     'keyword','value')
 %
 % The most important keyword value pairs are the identifiers for the
 % coordinate systems:
@@ -48,24 +48,30 @@ function [x2,y2,OPT]=convertCoordinates(x1,y1,varargin)
 %
 % Example 1: 4 different notations of 1 single transformation case:
 %
-%    [x,y,OPT]=convertCoordinates(5,52,'CS1.name','WGS 84','CS1.type','geo','CS2.name','WGS 84 / UTM zone 31N','CS2.type','xy')
-%    [x,y,OPT]=convertCoordinates(5,52,'CS1.code',4326                     ,'CS2.name','WGS 84 / UTM zone 31N')
+%    [x,y,log]=convertCoordinates(5,52,'CS1.name','WGS 84','CS1.type','geo','CS2.name','WGS 84 / UTM zone 31N','CS2.type','xy')
+%    [x,y,log]=convertCoordinates(5,52,'CS1.code',4326                     ,'CS2.name','WGS 84 / UTM zone 31N')
 %
 %    ESPG = load('EPSG')
 %
-%    [x,y,OPT]=convertCoordinates(52,5,EPSG,'CS1.name','WGS 84','CS1.type','geo','CS2.code',32631)
-%    [x,y,OPT]=convertCoordinates(52,5,EPSG,'CS1.code',4326                     ,'CS2.code',32631)
+%    [x,y,log]=convertCoordinates(52,5,EPSG,'CS1.name','WGS 84','CS1.type','geo','CS2.code',32631)
+%    [x,y,log]=convertCoordinates(52,5,EPSG,'CS1.code',4326                     ,'CS2.code',32631)
 %
 % Example 2: Rijksdriehoek to WGS 84:
 %
-%   [lon,lat,OPT]=convertCoordinates(xRD,yRD,'CS1.code',28992,'CS2.code',4326)
+%   [lon,lat,log]=convertCoordinates(xRD,yRD,'CS1.code',28992,'CS2.code',4326)
 %
 % Example 3: decimal degree to sexagesimal DMS conversion:
 %
-%   [lon,lat,OPT]=convertCoordinates(52,5.5,'CS1.code',4326,'CS2.code',4326,'CS2.UoM.name','sexagesimal DMS')
+%   [lon,lat,log]=convertCoordinates(52,5.5,'CS1.code',4326,'CS2.code',4326,'CS2.UoM.name','sexagesimal DMS')
 %
-% To find specifications of coordinate systems (name <=> code):
-% <a href="http://www.epsg-registry.org">www.epsg-registry.org</a>.
+% +-------+-------------------------+------------------+
+% | code  |  name                   |  type            |
+% +-------+-------------------------+------------------+
+% |  4326 | 'WGS 84'                | 'geographic 2D'  |  To find specifications of 
+% | 28992 | 'Amersfoort / RD New'   | 'projected'      |  more coordinate systems 
+% | 32631 | 'WGS 84 / UTM zone 31N' | 'projected'      |  (name <=> code):
+% | 23031 | 'ED50 / UTM zone 31N'   | 'projected'      |  <a href="http://www.epsg-registry.org">www.epsg-registry.org</a>
+% +-------+-------------------------+------------------+
 %
 % See also: SuperTrans, EPSG.mat
 
@@ -105,33 +111,36 @@ function [x2,y2,OPT]=convertCoordinates(x1,y1,varargin)
 % $Keywords: $
 
 %% version check
-if datenum(version('-date'))<datenum(2007,1,1)
-    warning('the matlab version your using might be to old for this function'); %#ok<WNTAG>
-end
+
+   if datenum(version('-date'))<datenum(2007,1,1)
+       warning('the matlab version your using might be to old for this function');
+   end
 
 %% check if EPSG codes are given
 
-if odd(length(varargin))
-    STD = varargin{1};
-    varargin(1)=[];
-else
-    STD = load('EPSG');
-end
+   if odd(length(varargin))
+       STD = varargin{1};
+       varargin(1)=[];
+   else
+       STD = load('EPSG');
+   end
 
 %% get and set keyword value parameters
-OPT=[];
-OPT=FindCSOptions(OPT,STD,varargin);
+
+   OPT = [];
+   OPT = FindCSOptions(OPT,STD,varargin);
 
 %% Transform input coordinates to geographic 2D radians
-switch OPT.CS1.type
-    case 'projected' % convert projection to geographic radians
-%         x1 = convertUnits(x1,OPT.CS1.UoM.name,'metre',STD);
-%         y1 = convertUnits(y1,OPT.CS1.UoM.name,'metre',STD);
-        [lat1,lon1] = ConvertCoordinatesProjectionConvert(x1,y1,OPT.CS1,OPT.proj_conv1,'xy2geo',STD);
-    case 'geographic 2D' % do nothing, except for a unit conversion
-        lon1 = convertUnits(x1,OPT.CS1.UoM.name,'radian',STD);
-        lat1 = convertUnits(y1,OPT.CS1.UoM.name,'radian',STD);
-end
+
+   switch OPT.CS1.type
+       case 'projected' % convert projection to geographic radians
+            %x1 = convertUnits(x1,OPT.CS1.UoM.name,'metre',STD);
+            %y1 = convertUnits(y1,OPT.CS1.UoM.name,'metre',STD);
+           [lat1,lon1] = ConvertCoordinatesProjectionConvert(x1,y1,OPT.CS1,OPT.proj_conv1,'xy2geo',STD);
+       case 'geographic 2D' % do nothing, except for a unit conversion
+           lon1 = convertUnits(x1,OPT.CS1.UoM.name,'radian',STD);
+           lat1 = convertUnits(y1,OPT.CS1.UoM.name,'radian',STD);
+   end
 
 %% find datum transformation options
 % check if geogcrs_code1 and geogcrs_code2 are different
@@ -141,38 +150,42 @@ end
 % 
 % * if multiple options found, use the newest unless user has defined something else
 % * if no direct transformation exists, convert via WGS 84
-OPT = ConvertCoordinatesFindDatumTransOpt(OPT,STD);
+
+   OPT = ConvertCoordinatesFindDatumTransOpt(OPT,STD);
 
 %% do datum transformation
 
-if ischar(OPT.datum_trans)
-    if strcmpi(OPT.datum_trans,'no transformation available')
-        error('No transformation methods available ...');
-    end
-end
+   if ischar(OPT.datum_trans)
+       if strcmpi(OPT.datum_trans,'no transformation available')
+           error('No transformation methods available ...');
+       end
+   end
+   
+   if ischar(OPT.datum_trans)
+       % no transformation required
+       lat2 = lat1;
+       lon2 = lon1;
+   else
+       if ~isfield(OPT,'datum_trans_from_WGS84') %only exists when tranforming via WGS 84
+           [lat2,lon2] = ConvertCoordinatesDatumTransform(lat1,lon1,OPT,'datum_trans'          ,STD);
+       else
+           [lat2,lon2] = ConvertCoordinatesDatumTransform(lat1,lon1,OPT,'datum_trans_to_WGS84'  ,STD);
+           [lat2,lon2] = ConvertCoordinatesDatumTransform(lat2,lon2,OPT,'datum_trans_from_WGS84',STD);
+       end
+   end
 
-if ischar(OPT.datum_trans)
-    % no transformation required
-    lat2 = lat1;
-    lon2 = lon1;
-else
-    if ~isfield(OPT,'datum_trans_from_WGS84') %only exists when tranforming via WGS 84
-        [lat2,lon2] = ConvertCoordinatesDatumTransform(lat1,lon1,OPT,'datum_trans',STD);
-    else
-        [lat2,lon2] = ConvertCoordinatesDatumTransform(lat1,lon1,OPT,'datum_trans_to_WGS84',STD);
-        [lat2,lon2] = ConvertCoordinatesDatumTransform(lat2,lon2,OPT,'datum_trans_from_WGS84',STD);
-    end
-end
+%% Transform geographic 2D radians to output coordinates
 
-%% Transform geographic 2D radians to output coordinates 
-switch OPT.CS2.type
-    case 'projected' % convert projection to geographic radians
-        [y2,x2] = ConvertCoordinatesProjectionConvert(lon2,lat2,OPT.CS2,OPT.proj_conv2,'geo2xy',STD);
-        x2 = convertUnits(x2,OPT.CS2.UoM.name,'metre',STD);
-        y2 = convertUnits(y2,OPT.CS2.UoM.name,'metre',STD);
-    case 'geographic 2D' 
-        x2 = convertUnits(lon2,'radian',OPT.CS2.UoM.name,STD);
-        y2 = convertUnits(lat2,'radian',OPT.CS2.UoM.name,STD);
-end
+   switch OPT.CS2.type
+       case 'projected' % convert projection to geographic radians
+           [y2,x2] = ConvertCoordinatesProjectionConvert(lon2,lat2,OPT.CS2,OPT.proj_conv2,'geo2xy',STD);
+           x2 = convertUnits(x2,OPT.CS2.UoM.name,'metre',STD);
+           y2 = convertUnits(y2,OPT.CS2.UoM.name,'metre',STD);
+       case 'geographic 2D' 
+           x2 = convertUnits(lon2,'radian',OPT.CS2.UoM.name,STD);
+           y2 = convertUnits(lat2,'radian',OPT.CS2.UoM.name,STD);
+   end
+   
+   end
+
 %% EOF
-end
