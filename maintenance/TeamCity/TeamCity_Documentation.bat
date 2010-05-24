24b3e846be8ac97fd68b376f60dc3278eee41c87
geer <geer@localhost>
2010-05-24 11:33:44 +0000

    moved documentation generation to TeamCity dir
    svn path=/trunk/matlab/; revision=2591
    
    
    Former-commit-id: 821d062e20d26a117c172728ecefc7291f2bb5e5

---
+++ maintenance/TeamCity/TeamCity_Documentation.bat
@@ -0,0 +1,41 @@
+echo off
+rem Build script to test OpenEarthTools matlab scripts
+rem with TeamCity Distributed Build Management and Continuous Integration Server
+rem <http://www.jetbrains.com/teamcity/>
+
+rem TO DO: put entire mapping command in environment variable %openearth_map%
+
+rem Map drive with matlab
+rem -----------------------------------
+net use y: /delete
+net use y: \\wlhost\library %openearth_password% /USER:%openearth_user% 
+
+:oet_perform
+rem Call matlab
+rem http://www.mathworks.com/support/solutions/data/1-16B8X.html
+rem -----------------------------------
+rem Create a temp file
+echo Matlab is running > matlabruns.busy
+rem now run matlab
+Y:\app\MATLAB2010a\bin\matlab.exe -nosplash -nodesktop -minimize -r "TeamCity_makedocumentation(%BUILD_VCS_NUMBER%);" -logfile mlogfile.log -sd "%teamcity.build.workingDir%"
+
+rem hold reporting until matlab status file has been deleted
+
+:loopmatlabbusy
+if exist teamcitymessage.matlab goto echo_teamcity_message
+:continueloobmatlabbusy
+If exist matlabruns.busy goto loopmatlabbusy
+
+echo 'teamcity OK'
+
+rem Remove drive with matlab
+rem -----------------------------------
+net use y: /delete
+goto end
+
+:echo_teamcity_message
+type teamcitymessage.matlab
+del teamcitymessage.matlab
+goto continueloobmatlabbusy
+
+:end
---
+++ maintenance/TeamCity/TeamCity_makedocumentation.m
@@ -0,0 +1,103 @@
+function TeamCity_makedocumentation(varargin)
+%TEAMCITY_MAKEDOCUMENTATION  Function that creates all tutorials in the OpenEarthTools repository.
+%
+%   This function gathers and creates all tutorials in the OpenEarthTools repository.
+%
+%   Syntax:
+%   TeamCity_makedocumentation;
+%
+%   See also mtestengine mtest oetpublish
+
+%% Copyright notice
+%   --------------------------------------------------------------------
+%   Copyright (C) 2009 Deltares
+%       Pieter van Geer
+%
+%       pieter.vangeer@deltares.nl
+%
+%       Rotterdamseweg 185
+%       2629 HD Delft
+%       P.O. 177
+%       2600 MH Delft
+%
+%   This library is free software: you can redistribute it and/or
+%   modify it under the terms of the GNU Lesser General Public
+%   License as published by the Free Software Foundation, either
+%   version 2.1 of the License, or (at your option) any later version.
+%
+%   This library is distributed in the hope that it will be useful,
+%   but WITHOUT ANY WARRANTY; without even the implied warranty of
+%   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+%   Lesser General Public License for more details.
+%
+%   You should have received a copy of the GNU Lesser General Public
+%   License along with this library. If not, see <http://www.gnu.org/licenses/>.
+%   --------------------------------------------------------------------
+
+% This tools is part of <a href="http://OpenEarth.Deltares.nl">OpenEarthTools</a>.
+% OpenEarthTools is an online collaboration to share and manage data and
+% programming tools in an open source, version controlled environment.
+% Sign up to recieve regular updates of this function, and to contribute
+% your own tools.
+
+%% Version <http://svnbook.red-bean.com/en/1.5/svn.advanced.props.special.keywords.html>
+% Created: 15 Sep 2009
+% Created with Matlab version: 7.8.0.347 (R2009a)
+
+% $Id$
+% $Date$
+% $Author$
+% $Revision$
+% $HeadURL$
+% $Keywords: $
+
+%% First load oetsettings
+try
+    %% temp remove targetdir from repos checkout
+    matlabdir = strrep(fileparts(mfilename('fullpath')),'maintenance\TeamCity','');
+    if isdir(fullfile(matlabdir,'tutorials'))
+        rmdir(fullfile(matlabdir,'tutorials'),'s');
+    end
+    if isdir(fullfile(matlabdir,'docs'))
+        rmdir(fullfile(matlabdir,'docs'),'s');
+    end
+
+    %% load oetsettings
+    addpath(matlabdir);
+    addpath(genpath(fullfile(matlabdir,'maintenance')));
+    postmessage('progressStart',true,'Running oetsettings.');
+    oetsettings;
+    postmessage('progressFinish',true,'Oetsettings enabled.');
+catch me
+    postmessage('message', true, 'text', 'Matlab was unable to run oetsettings.',...
+        'errorDetails',me.message,...
+        'status','ERROR');
+    %% Remove template files
+    delete('matlabruns.busy');
+    exit;
+end
+
+try
+
+    %% start documenting
+    tutorials2html(varargin{:},'teamcity');
+    
+    %% zip result
+    delete('htmldocumentation.zip');
+    delete('matlabtocfiles.zip');
+    zip('htmldocumentation',{fullfile(oetroot,'tutorials','*.*')});
+    zip('matlabtocfiles',{fullfile(oetroot,'docs','OpenEarthDocs','*.*')});
+    
+    %% remove targetdir
+    rmdir(fullfile(oetroot,'tutorials'),'s');
+    rmdir(fullfile(oetroot,'docs'),'s');
+catch me
+    postmessage('message', true, 'text', 'Something went wrong while running the tests.',...
+        'errorDetails',me.message,...
+        'status','ERROR');
+    delete('matlabruns.busy');
+    exit;
+end
+
+delete('matlabruns.busy');
+exit;
