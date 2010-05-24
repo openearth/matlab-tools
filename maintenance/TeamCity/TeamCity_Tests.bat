32dd12b55d47e0f81509c8546b4278fec2c2073e
geer <geer@localhost>
2010-05-24 08:46:31 +0000

    - changed name of mapped drive from y: to m: (TeamCity_Tests.bat) - tried to restore search path when it is deleted while running a test (MTest.m) - moved some tests to ignore instead of exclude them entirely
    svn path=/trunk/matlab/; revision=2586
    
    
    Former-commit-id: d3263d7f7c13b0708dde52f34a467a826f4d550f

--- maintenance/TeamCity/TeamCity_Tests.bat
+++ maintenance/TeamCity/TeamCity_Tests.bat
@@ -7,8 +7,8 @@ rem TO DO: put entire mapping command in environment variable %openearth_map%
 
 rem Map drive with matlab
 rem -----------------------------------
-net use y: /delete
-net use y: \\wlhost\library %openearth_password% /USER:%openearth_user% 
+net use m: /delete
+net use m: \\wlhost\library %openearth_password% /USER:%openearth_user% 
 
 :oet_perform
 rem Call matlab
@@ -18,7 +18,7 @@ rem Create a temp file
 echo Matlab is running > matlabruns.busy
 rem now run matlab
 
-Y:\app\MATLAB2010a\bin\matlab.exe -nosplash -nodesktop -minimize -r "TeamCity_runtests;" -logfile mlogfile.log -sd "%teamcity.build.workingDir%"
+M:\app\MATLAB2010a\bin\matlab.exe -nosplash -nodesktop -minimize -r "TeamCity_runtests;" -logfile mlogfile.log -sd "%teamcity.build.workingDir%"
 rem hold reporting until matlab status file has been deleted
 
 :loopmatlabbusy
@@ -30,7 +30,7 @@ echo 'teamcity OK'
 
 rem Remove drive with matlab
 rem -----------------------------------
-net use y: /delete
+net use m: /delete
 goto end
 
 :echo_teamcity_message
