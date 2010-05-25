2f136ea661f52f4daa4e2f25ab1d6df05428a06b
ormondt <ormondt@localhost>
2010-05-25 12:19:24 +0000

    SuperTrans removed
    svn path=/trunk/matlab/; revision=2609
    
    
    Former-commit-id: 6afae86ddd5b3003ca9a6bd7c991fc39a2dfb54b

--- applications/DelftDashBoard/utils/SuperTrans/conversion_dlls/Helmert3.m
+++
@@ -1,5 +0,0 @@
-function [x1,y1,z1]=Helmert3(x0,y0,z0,dx,dy,dz)
-      
-x1 = x0 + dx;
-y1 = y0 + dy;
-z1 = z0 + dz;
--- applications/DelftDashBoard/utils/SuperTrans/conversion_dlls/Helmert7.m
+++
@@ -1,7 +0,0 @@
-function [x1,y1,z1]=Helmert7(x0,y0,z0,dx,dy,dz,rx,ry,rz,ds)
-      
-m=1.0 + ds*0.000001;
-      
-x1 = m*(    x0 - rz*y0 + ry*z0) + dx;
-y1 = m*( rz*x0 +    y0 - rx*z0) + dy;
-z1 = m*(-ry*x0 + rx*y0 +    z0) + dz;
