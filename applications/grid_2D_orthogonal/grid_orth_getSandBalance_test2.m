daa19d2500ed407ee78bd9f133f65bb5d0861cf0
m.vankoningsveld@tudelft.nl <m.vankoningsveld@tudelft.nl@localhost>
2010-05-31 08:47:52 +0000

    Change in coverage percentages in test
    svn path=/trunk/matlab/; revision=2633
    
    
    Former-commit-id: 3b8f1dcdbce8f243c3d7a8700823b81a15884675

--- applications/grid_2D_orthogonal/grid_orth_getSandBalance_test2.m
+++ applications/grid_2D_orthogonal/grid_orth_getSandBalance_test2.m
@@ -9,7 +9,7 @@ OPT.ldburl          = 'http://opendap.deltares.nl/thredds/dodsC/opendap/deltares
 OPT.workdir         = 'D:\checkouts\VO-rawdata\projects\151027_maasvlakte_2\scripts\sedbudget\mv\';
 OPT.polygondir      = 'D:\checkouts\VO-rawdata\projects\151027_maasvlakte_2\scripts\sedbudget\mv\polygons\';
 OPT.searchinterval  = 0;                                % acceptable interval to include data from (in days)
-OPT.min_coverage    = [75 90];                          % coverage percentage (can be several, e.g. [50 75 90]
+OPT.min_coverage    = [50 90];                          % coverage percentage (can be several, e.g. [50 75 90]
 
 if 1
     try rmdir(fullfile(OPT.workdir, 'coverage'),  's'); end
