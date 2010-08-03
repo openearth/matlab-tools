fa82f960e0e0f7fe4338438340deabdcc1c412d2
tda@vanoord.com <tda@vanoord.com@localhost>
2010-08-03 16:09:24 +0000

    update of multibeam data processing scripts
    svn path=/trunk/matlab/; revision=2905
    
    
    Former-commit-id: 7b55c8c48c180b7cf3dca01b177aef7c87dc4c77

--- applications/nc_processing/nc_multibeam/nc_multibeam_from_xyz.m
+++ applications/nc_processing/nc_multibeam/nc_multibeam_from_xyz.m
@@ -300,7 +300,7 @@ if OPT.copy2server
     end
     nc_cf_opendap2catalog('urlPathFcn',@(s) strrep (strrep (s,OPT.basepath_local,OPT.basepath_opendap),filesep,'/'),...
         'base',fullfile(OPT.basepath_local,OPT.netcdf_path),...
-        'save',true);
+        'save',true,'quiet',true);
 else
     if OPT.make
         if exist(fullfile(OPT.basepath_local,OPT.netcdf_path,'catalog.nc'),'file')
@@ -308,7 +308,7 @@ else
         end
         nc_cf_opendap2catalog(...
             'base',fullfile(OPT.basepath_local,OPT.netcdf_path),...
-            'save',true);
+            'save',true,'quiet',true);
         multiWaitbar('Raw data to NetCDF',1,'label','Generating NC files')
     end
 end
