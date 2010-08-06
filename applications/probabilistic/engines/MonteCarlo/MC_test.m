859db4e9ab18b5b96cb2162ebcb45e622b24f6ce
heijer <heijer@localhost>
2010-08-06 14:29:10 +0000

    Monte Carlo test function added
    svn path=/trunk/matlab/; revision=2926
    
    
    Former-commit-id: 2c3f05a4298fb7ac0846b9edb897e55cef95f6da

---
+++ applications/probabilistic/engines/MonteCarlo/MC_test.m
@@ -0,0 +1,120 @@
+function testresult = MC_test()
+% MC_TEST  Test routine for Monte Carlo function
+%  
+% More detailed description of the test goes here.
+%
+%
+%   See also 
+
+%% Copyright notice
+%   --------------------------------------------------------------------
+%   Copyright (C) 2010 Delft University of Technology
+%       Kees den Heijer
+%
+%       C.denHeijer@TUDelft.nl	
+%
+%       Faculty of Civil Engineering and Geosciences
+%       P.O. Box 5048
+%       2600 GA Delft
+%       The Netherlands
+%
+%   This library is free software: you can redistribute it and/or
+%   modify it under the terms of the GNU Lesser General Public
+%   License as published by the Free Software Foundation, either
+%   version 2.1 of the License, or (at your option) any later version.
+%
+%   This library is distributed in the hope that it will be useful,
+%   but WITHOUT ANY WARRANTY; without even the implied warranty of
+%   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+%   Lesser General Public License for more details.
+%
+%   You should have received a copy of the GNU Lesser General Public
+%   License along with this library. If not, see <http://www.gnu.org/licenses/>.
+%   --------------------------------------------------------------------
+
+% This tools is part of <a href="http://OpenEarth.Deltares.nl">OpenEarthTools</a>.
+% OpenEarthTools is an online collaboration to share and manage data and 
+% programming tools in an open source, version controlled environment.
+% Sign up to recieve regular updates of this function, and to contribute 
+% your own tools.
+
+%% Version <http://svnbook.red-bean.com/en/1.5/svn.advanced.props.special.keywords.html>
+% Created: 06 Aug 2010
+% Created with Matlab version: 7.10.0.499 (R2010a)
+
+% $Id$
+% $Date$
+% $Author$
+% $Revision$
+% $HeadURL$
+% $Keywords: $
+
+MTest.category('UnCategorized');
+
+%% define stochastic variables
+stochast = struct(...
+    'Name', {
+    'R'...
+    'S'...
+    },...
+    'Distr', {
+    @norm_inv...
+    @norm_inv...
+    },...
+    'Params', {
+    {9 2}...
+    {6 3}...
+    } ...
+    );
+
+testresult = [];
+
+%% test 1
+resultCMC = MC(...
+    'stochast', stochast,...
+    'x2zFunction', @x2z_testfunction,...
+    'seed', 0);
+
+% Probability of failure should be always the same for a fixed seed
+testresult(end+1) = resultCMC.Output.P_f == 0.19;
+
+%% test 2
+resultSISMC = MC(...
+    'stochast', stochast,...
+    'x2zFunction', @x2z_testfunction,...
+    'ISvariable', 'S',...
+    'W', 10,...
+    'seed', 0);
+% Probability of failure should be always the same for a fixed seed
+testresult(end+1) = resultSISMC.Output.P_f == 0.083;
+
+%% test 3
+resultAISMC = MC(...
+    'stochast', stochast,...
+    'x2zFunction', @x2z_testfunction,...
+    'ISvariable', 'S',...
+    'f1', 1,...
+    'f2', 1e-6,...
+    'seed', 0);
+% Probability of failure should be always the same for a fixed seed
+testresult(end+1) = roundoff(resultAISMC.Output.P_f, 4) == 0.2089;
+
+%% test 2
+resultCMC_resistance = MC(...
+    'stochast', stochast,...
+    'x2zFunction', @x2z_testfunction,...
+    'Resistance', 0:4,...
+    'seed', 0);
+
+% Probability of failure for a vector with resistances
+testresult(end+1) = all(resultCMC_resistance.Output.P_f == [0.19 0.15 0.12 0.05 0.03]);
+
+%% combine all testresults in one boolean
+testresult = all(testresult);
+
+function z = x2z_testfunction(samples, Resistance, varargin)
+
+variables = fieldnames(samples);
+nsamples = length(samples.(variables{1}));
+nResistance = length(Resistance);
+z = repmat(Resistance, nsamples, 1) + repmat(samples.R - samples.S, 1, nResistance);
