dbb92732e4895dcc22c9c65611c18220597677b6
geer <geer@localhost>
2010-08-23 10:41:21 +0000

    echo difference if it is too large
    svn path=/trunk/matlab/; revision=2982
    
    
    Former-commit-id: ca4a919e9abf3b821483a6cd1e95616a6bb1740c

--- applications/oceancolor/seawifs_datenum_test.m
+++ applications/oceancolor/seawifs_datenum_test.m
@@ -8,7 +8,7 @@ num = ceil(now*24*60)./24/60; % on minutes
 
 answ = seawifs_datenum(seawifs_datenum(num));
 OK(1) = abs(num - answ) < 1e-6; % floats are never exactly identical
-assert(OK(1),['answ should be input (' num2str(num) '), but was ' num2str(answ) ' instead.']);
+assert(OK(1),['answ should be input (' num2str(num) '), but was ' num2str(answ) ' instead.', char(10), 'Difference was: ' num2str(abs(num - answ),'%e')]);
 
 OK(2) = strcmpi(str,seawifs_datenum(seawifs_datenum(str)));
 assert(OK(2),'String should be returned');
