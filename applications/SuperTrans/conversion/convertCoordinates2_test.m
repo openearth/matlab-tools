896c4d557f31c292ec214a8d01cb5f67bdbbf003
mol <mol@localhost>
2010-09-02 15:15:28 +0000

    2nd test of convertCoordinates; it tests conversions of all systems (codes) to wgs84 and back.
    svn path=/trunk/matlab/; revision=3024
    
    
    Former-commit-id: cddaae49babea7f5dc93b580ac1688f8d17c7600

---
+++ applications/SuperTrans/conversion/convertCoordinates2_test.m
@@ -0,0 +1,52 @@
+function OK = convertCoordinates2_test
+%CONVERTCOORDINATES_TEST   test convertCoordinates with kadaster data pointy
+%
+%See also: CONVERTCOORDINATES
+
+% load coordinate systems
+load('EPSG.mat');
+codes = coordinate_reference_system.coord_ref_sys_code;
+kinds = coordinate_reference_system.coord_ref_sys_kind;
+
+geo_wgs84_code = 4326;
+
+% test coordinates for geographic systems
+lon = 05+24/60+02.5391/3600;
+lat = 53+25/60+13.2124/3600;
+
+% test coordinates for projected systems
+x = 100000;
+y = 100000;
+
+% create file for printing error messages
+fid = fopen('convertCoordinate_errorMessages.txt','w');
+
+% start loop over all systems
+for s = 1:length(codes)
+% check what kind of system this is
+    switch kinds{s}
+        case 'geographic 2d'
+        try
+            [lon2,lat2] = convertcoordinates(lon ,lat ,'CS1.code',codes(s),'CS2.code', 4326);
+            [lon3,lat3] = convertcoordinates(lon2 ,lat2 ,'CS1.code', 4326,'CS2.code',codes(s));
+            testresult(s) = abs(lon - lon3) < 0.0001 & abs(lat -lat3) < 0.0001;
+        catch
+            testresult(s) = nan;
+            fprintf(fid,'%s\n',['Error using system ' coordinate_reference_system.coord_ref_sys_name{s} ', with code ' num2str(codes(s)) ':']);
+            fprintf(fid,'%s\n\n',lasterr);
+        end
+        case 'projected'
+        try
+            [lon2,lat2] = convertcoordinates(x ,y ,'CS1.code',codes(s),'CS2.code', 4326);
+            [x3,y3]     = convertcoordinates(lon2 ,lat2 ,'CS1.code', 4326,'CS2.code',codes(s));
+            testresult(s) = abs(x - x3) < 10 & abs(y -y3) < 10;
+        catch
+            testresult(s) = nan;
+            fprintf(fid,'%s\n',['Error using system' coordinate_reference_system.coord_ref_sys_name{s} ', with code ' num2str(codes(s)) ':']);
+            fprintf(fid,'%s\n\n',lasterr);
+        end
+    end
+end
+
+fclose(fid);
+OK = all(testresult);
