477cc0fa03aead69ae5293dd1b10308f63498a5e
mol <mol@localhost>
2010-09-14 07:49:42 +0000

    Added OET default description and copyright block
    svn path=/trunk/matlab/; revision=3051
    
    
    Former-commit-id: 75750ad19a9d1e4009eff03d7697dcd2aa82c6d9

--- applications/DelftDashBoard/main/operations/DelftDashBoard.m
+++ applications/DelftDashBoard/main/operations/DelftDashBoard.m
@@ -1,6 +1,54 @@
 function DelftDashBoard
+%DELFTDASHBOARD.
+%
+%   Compile with ddcompile
+%
+%   See also MUPPET, DETRAN
 
-% Compile with ddcompile
+%% Copyright notice
+%   --------------------------------------------------------------------
+%   Copyright (C) 2010 <COMPANY>
+%       Maarten van Ormondt
+%
+%       maarten.vanormondt@deltares.nl
+%
+%       P.O. Box 177
+%       2600 MH Delft
+%       The Netherlands
+%
+%   This library is free software: you can redistribute it and/or
+%   modify it under the terms of the GNU Lesser General Public
+%   License as published by the Free Software Foundation, either
+%   version 2.1 of the License, or (at your option) any later version.
+%
+%   This library is distributed in the hope that it will be useful,
+%   but WITHOUT ANY WARRANTY; without even the implied warranty of
+%   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+%   Lesser General Public License for more details.
+%
+%   You should have received a copy of the GNU Lesser General Public
+%   License along with this library. If not, see <http://www.gnu.org/licenses/>.
+%   --------------------------------------------------------------------
+
+% This tool is part of <a href="http://OpenEarth.nl">OpenEarthTools</a>.
+% OpenEarthTools is an online collaboration to share and manage data and 
+% programming tools in an open source, version controlled environment.
+% Sign up to recieve regular updates of this function, and to contribute 
+% your own tools.
+
+%% Version <http://svnbook.red-bean.com/en/1.5/svn.advanced.props.special.keywords.html>
+% Created: 14 Sep 2010
+% Created with Matlab version: 7.9.0.529 (R2009b)
+
+% $Id$
+% $Date$
+% $Author$
+% $Revision$
+% $HeadURL$
+% $Keywords: $
+
+%%
+
 
 handles.DelftDashBoardVersion='1.0';
 handles.MatlabVersion=version;
