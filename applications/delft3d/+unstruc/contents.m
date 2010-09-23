ec0daee2b5bc3755135e73b91d67d02b15c3c16d
melger <melger@localhost>
2010-09-23 08:41:45 +0000

    Updated contact info
    svn path=/trunk/matlab/; revision=3085
    
    
    Former-commit-id: 9c37814c8616149f24cdcd710d03647121f64c18

--- applications/delft3d/+unstruc/contents.m
+++ applications/delft3d/+unstruc/contents.m
@@ -1,15 +1,16 @@
 function G = readNet(varargin)
 %UNSTRUC   toolbox for handling Deltares unstruc netCDF files
 %
-% UNSTRUC is the new (1D, 2D, 3D) unstructured hydrodynamic flow 
-% model solver of Deltares. UNSTRUC is currently in development phase,
+% D-Flow is the new (1D, 2D, 3D) unstructured hydrodynamic flow 
+% model solver of Deltares, developed within the UNSTRUC project. 
+% D-Flow is currently in development phase,
 % and was as presented at the JONSMOD 2010 conference.
-% It can currently solve 1D flow systems a la Sobek, and 2D flow systems
+% It can currently solve 1D flow systems a la SOBEK, and 2D flow systems
 % a la Delft3D-FLOW/SIMONA-WAQUA/TRIWAQ. A 3D extension is foreseen. 
 % Beta test versions are available for Deltares partners, please contact the 
-% Deltares Software Centre (DSC) at www.delft3d.nl or www.sobek.nl.
+% Deltares Software Centre (DSC) at delft3d.sales@deltares.nl or sobek.sales@deltares.nl.
 %
-% The UNSTRUC netCDF output specification has been published at
+% The D-Flow netCDF output specification has been published at
 % the Deltares wiki: http://public.deltares.nl/display/NETCDF/netCDF.
 %
 % Read/plot grid
