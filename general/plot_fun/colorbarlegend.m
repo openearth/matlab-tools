0bbdcc0c1df47980144d871a7d9ddb97ff8c935c
boer_g <boer_g@localhost>
2010-10-12 12:08:27 +0000

    revised
    svn path=/trunk/matlab/; revision=3134
    
    
    Former-commit-id: 248ec911ab269363eb5a2e4c7e9afbaae5c88c0b

--- general/plot_fun/colorbarlegend.m
+++ general/plot_fun/colorbarlegend.m
@@ -76,36 +76,6 @@ function varargout = colorbarlegend(varargin)
 %   USA
 %   --------------------------------------------------------------------
 
-zposition   = -1e3;         % vertical psoition colorbar patch within axes
-xlims       = [];
-ylims       = [];
-
-if ishandle(varargin{1})
-   AX          = varargin{1};
-   if isnumeric(varargin{4})
-      xlims    = varargin{2};
-      ylims    = varargin{3};
-      clims    = varargin{4};
-      argstart = 5;
-   else
-      xlims    = [];
-      ylims    = [];
-      clims    = varargin{2};
-      argstart = 3;
-   end
-else
-   if isnumeric(varargin{3})
-      xlims    = varargin{1};
-      ylims    = varargin{2};
-      clims    = varargin{3};
-   else
-      xlims    = [];
-      ylims    = [];
-      clims    = varargin{1};
-   end
-   argstart = 4;
-end
-
 OPT.units          = 'normalized';
 OPT.reference      = 'gca';
 OPT.ontop          = 0;
@@ -115,117 +85,148 @@ OPT.titleposition  = 'title';
 OPT.titlecolor     = 'k';
 OPT.orientation    = 'horizontal'; % 'vertical';
 OPT.ctick          = nan; % nan = auto, [] = clims, values = values
+OPT.zposition      = -1e3;         % vertical psoition colorbar patch within axes
 
-OPT.axes           = gca;
-OPT.figure         = gcf;
+OPT.xlims          = [];
+OPT.ylims          = [];
+OPT.clims          = [];
 
-OPT.figure0        = gcf;
-OPT.axes0          = gca;
+if nargin==0
+   varargout = {OPT};
+   return
+end
+
+if ishandle(varargin{1})
+   AX          = varargin{1};
+   if isnumeric(varargin{4})
+      OPT.xlims    = varargin{2};
+      OPT.ylims    = varargin{3};
+      OPT.clims    = varargin{4};
+      argstart = 5;
+   else
+      OPT.xlims    = [];
+      OPT.ylims    = [];
+      OPT.clims    = varargin{2};
+      argstart = 3;
+   end
+else
+   if isnumeric(varargin{3})
+      OPT.xlims    = varargin{1};
+      OPT.ylims    = varargin{2};
+      OPT.clims    = varargin{3};
+   else
+      OPT.xlims    = [];
+      OPT.ylims    = [];
+      OPT.clims    = varargin{1};
+   end
+   argstart = 4;
+end
 
 OPT = setproperty(OPT,varargin{argstart:end});
    
 %% Make colorbar axes active
 
-   if ~isempty(xlims)
-   AX        = axesontop(xlims,ylims,'units',OPT.units,...
+   if ~isempty(OPT.xlims)
+   AX        = axesontop(OPT.xlims,OPT.ylims,'units',OPT.units,...
                                  'reference',OPT.reference,...
                                      'ontop',OPT.ontop);
    end
    
 %% Calculate position
 
-   if isempty(xlims)
-      xlims   = get(AX  ,'xlim');
+   if isempty(OPT.xlims)
+      OPT.xlims   = get(AX  ,'xlim');
    else
-      set(AX  ,'xlim',[xlims]);
+      set(AX  ,'xlim',[OPT.xlims]);
    end
-   if isempty(ylims)
-      ylims   = get(AX  ,'ylim');
+   if isempty(OPT.ylims)
+      OPT.ylims   = get(AX  ,'ylim');
    else
-      set(AX  ,'ylim',[ylims]);
+      set(AX  ,'ylim',[OPT.ylims]);
    end
 
    clrmap  = get(gcf,'colormap');
    
    L       = size(clrmap,1);
 
-   x       = linspace(clims(1),clims(2),L+1);
+   x       = linspace(OPT.clims(1),OPT.clims(2),L+1);
    x       = [x;x];
 
    y       = zeros(size(x));
-   y(1,:)  = ylims(1);
-   y(2,:)  = ylims(2);
+   y(1,:)  = OPT.ylims(1);
+   y(2,:)  = OPT.ylims(2);
 
    x       = x';
    y       = y';
 
-   c       = [linspace(clims(1),clims(2),L) nan];
+   c       = [linspace(OPT.clims(1),OPT.clims(2),L) nan];
    c       = [c;c]';
-   z       = zeros(size(c)) + zposition;
+   z       = zeros(size(c)) + OPT.zposition;
 
 %% Old code
 
-   %image([xlims],[ylims],clrmap)
+   %image([OPT.xlims],[OPT.ylims],clrmap)
    %clrbar      = colorbar(A);
    %clrbarchild = get(clrbar,'Children');
    %clrbarchild = findobj(clrbarchild,'Tag','TMW_COLORBAR')
    %Xdata       = get(clrbarchild,'Xdata')
    %Ydata       = get(clrbarchild,'Ydata')
-   %Xdata(1)    = xlims(1);
-   %Xdata(2)    = ylims(2);
-   %Ydata(1)    = xlims(1);
-   %Ydata(2)    = ylims(2);
+   %Xdata(1)    = OPT.xlims(1);
+   %Xdata(2)    = OPT.ylims(2);
+   %Ydata(1)    = OPT.xlims(1);
+   %Ydata(2)    = OPT.ylims(2);
    %set(clrbarchild,'Xdata',Xdata);
    %set(clrbarchild,'Ydata',Ydata);
 
 %% Fix color limits
 
-   caxis(clims);
+   caxis(OPT.clims);
    
 %% Draw colorbar patch
 
    if strcmp(lower(OPT.orientation(1)),'h')
 
-      P  = surf(x,y,z,c);
+      P  = surf(AX,x,y,z,c);
 
-      xlim(clims);
-      ylim([min(y(:)) max(y(:))]);%ylim([0 1]);
+      set(AX,'xlim',OPT.clims);
+      set(AX,'ylim',[min(y(:)) max(y(:))]);
 
-         set(gca,'ytick',[])
+         set(AX,'ytick',[])
       if isempty(OPT.ctick)
-         set(gca,'xtick',clims)
+         set(AX,'xtick',OPT.clims)
       elseif isnan(OPT.ctick)
       else
-         set(gca,'xtick',OPT.ctick)
+         set(AX,'xtick',OPT.ctick)
       end
 
       %set(A,'xtick'     ,[1 L+1]);
-      %set(A,'xticklabel',{num2str(clims(1)),num2str(clims(2))});
+      %set(A,'xticklabel',{num2str(OPT.clims(1)),num2str(OPT.clims(2))});
       
    elseif strcmp(lower(OPT.orientation(1)),'v')
 
-      P  = surf(y,x,z,c);
+      P  = surf(AX,y,x,z,c);
       
-      xlim([min(y(:)) max(y(:))]);%xlim([0 1]);
-      ylim(clims);
+      set(AX,'xlim',[min(y(:)) max(y(:))]);
+      set(AX,'ylim',OPT.clims);
 
-         set(gca,'xtick',[])
+         set(AX,'xtick',[])
       if isempty(OPT.ctick)
-         set(gca,'ytick',clims)
+         set(AX,'ytick',OPT.clims)
       elseif isnan(OPT.ctick)
       else
-         set(gca,'ytick',OPT.ctick)
+         set(AX,'ytick',OPT.ctick)
       end
 
       %set(AX,'xtick'     ,[1 L+1]);
-      %set(AX,'xticklabel',{num2str(clims(1)),num2str(clims(2))});
+      %set(AX,'xticklabel',{num2str(OPT.clims(1)),num2str(OPT.clims(2))});
       
    end
-   view(0,90)
-   set(P,'FaceColor','flat');
-   set(P,'EdgeColor','flat');
-   grid off
-   axis tight
+
+   view(AX,0,90)
+   set (P,'FaceColor','flat');
+   set (P,'EdgeColor','flat');
+   grid(AX,'off')
+   axis(AX,'tight')
    
 %% Add title
 
@@ -247,7 +248,7 @@ OPT = setproperty(OPT,varargin{argstart:end});
 %% Restore previous figure and axes
 
    if    OPT.ontop
-   axes  (OPT.axes);
+   axes  (AX);
    end
 
 if nargout==1
