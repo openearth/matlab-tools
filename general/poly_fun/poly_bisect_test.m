7e2744a05aa0fe8bdf97a8caf539fa0d29d45d4b
thijs@damsma.net <thijs@damsma.net@localhost>
2010-11-03 16:05:01 +0000

    added polygon function poly_simplify and poly_bisect
    svn path=/trunk/matlab/; revision=3228
    
    
    Former-commit-id: d62e0b914044801c5d44c002fbaa421e8c60da48

---
+++ general/poly_fun/poly_bisect_test.m
@@ -0,0 +1,63 @@
+function testResult = poly_bisect_test
+% POLY_BISECT_TEST  One line description goes here
+%  
+% More detailed description of the test goes here.
+%
+%
+%   See also 
+
+%% Copyright notice
+%   --------------------------------------------------------------------
+%   Copyright (C) 2010 Van Oord Dredging and Marine Contractors BV
+%       Thijs Damsma
+%
+%       tda@vanoord.com	
+%
+%       Watermanweg 64%       3067 GG%       Rotterdam%       The Netherlands
+%
+%   This library is free software: you can redistribute it and/or
+%   modify it under the terms of the GNU Lesser General Public
+%   License as published by the Free Software Foundation, either
+%   version 2.1 of the License, or (at your option) any later version.
+%
+%   This library is distributed in the hope that it will be useful,
+%   but WITHOUT ANY WARRANTY; without even the implied warranty of
+%   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+%   Lesser General Public License for more details.
+%
+%   You should have received a copy of the GNU Lesser General Public
+%   License along with this library. If not, see <http://www.gnu.org/licenses/>.
+%   --------------------------------------------------------------------
+
+% This tools is part of OpenEarthTools.
+% OpenEarthTools is an online collaboration to share and manage data and 
+% programming tools in an open source, version controlled environment.
+% Sign up to recieve regular updates of this function, and to contribute 
+% your own tools.
+
+%% Version <http://svnbook.red-bean.com/en/1.5/svn.advanced.props.special.keywords.html>
+% Created: 03 Nov 2010
+% Created with Matlab version: 7.11.0.584 (R2010b)
+
+% $Id$
+% $Date$
+% $Author$
+% $Revision$
+% $HeadURL$
+% $Keywords: $
+
+MTest.category('UnCategorized');
+
+x           = [0 1 2 3 4   5 6 7 nan 9];
+y           = [0 1 1 0 1 nan 1 1   0 0];
+theta       = poly_bisect(x,y);
+theta_check = [1/4 1/8 -1/8 0 1/4 nan 0 0 nan nan]*pi;
+
+testResult = false;
+if isequal(isnan(theta),isnan(theta_check));
+    if max(theta(~isnan(theta))-theta_check(~isnan(theta_check)))<eps(10)
+        testResult = true;
+    end
+end
+
+
