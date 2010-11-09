78675fcfde6faa32379cb29bf662cbbda2266c02
boer_we <boer_we@localhost>
2010-11-09 13:31:16 +0000

    svn path=/trunk/matlab/; revision=3255
    Former-commit-id: a670d7925e154ebd677b08267a93c3481687e892

---
+++ applications/DelftDashBoard/models/Delft3DFLOW/fileio/ddb_initializeLTR.m
@@ -0,0 +1,11 @@
+function LTR=ddb_initializeLTR
+
+LTR.NumberofClimates = 0;
+LTR.ORKST= '';
+LTR.PROFH = '';
+LTR.PRO = '';
+LTR.CFS = '';
+LTR.CFE = '';
+LTR.SCO = '';
+LTR.RAY = '';
+LTR.MDA = '';
