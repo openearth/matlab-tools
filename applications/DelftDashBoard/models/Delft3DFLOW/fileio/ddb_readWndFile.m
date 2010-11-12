64673c0f3a6c1b3da01fdbfe954f43fc74578c9d
boer_we <boer_we@localhost>
2010-11-12 10:31:29 +0000

    svn path=/trunk/matlab/; revision=3269
    Former-commit-id: a04985c96ed5801716be86682b4b0f590546c131

--- applications/DelftDashBoard/models/Delft3DFLOW/fileio/ddb_readWndFile.m
+++
@@ -1,4 +0,0 @@
-function handles=ddb_readWndFile(handles,id)
-
-fname=handles.Model(md).Input(id).WndFile;
-handles.Model(md).Input(id).WindData=load(fname);
--- applications/DelftDashBoard/models/Delft3DFLOW/fileio/ddb_saveWndFile.m
+++
@@ -1,8 +0,0 @@
-function ddb_saveWndFile(handles,id)
-
-fname=handles.Model(md).Input(id).WndFile;
-
-fid=fopen(fname,'w');
-fprintf(fid,'%16.7e %16.7e %16.7e\n',handles.Model(md).Input(id).WindData');
-fclose(fid);
-
