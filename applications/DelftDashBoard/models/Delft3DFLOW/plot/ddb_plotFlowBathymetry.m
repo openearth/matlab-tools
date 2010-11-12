eecaf04d74f3f14ee751cd5f224095095f3cf0e6
mol <mol@localhost>
2010-11-12 13:46:46 +0000

    Restored deleted files (2)
    svn path=/trunk/matlab/; revision=3272
    
    
    Former-commit-id: c93971f67978d8049abf8e2b4b5392d71b88edf2

---
+++ applications/DelftDashBoard/models/Delft3DFLOW/plot/ddb_plotFlowBathymetry.m
@@ -0,0 +1,53 @@
+function ddb_plotFlowBathymetry(handles,opt,id)
+
+switch lower(opt)
+
+    case{'plot'}
+
+        h=findobj(gca,'Tag','FlowBathymetry','UserData',id);
+        delete(h);
+
+        if size(handles.Model(md).Input(ad).Depth,1)>0
+
+            clims=get(gca,'CLim');
+            zmin=clims(1);
+            zmax=clims(2);
+            
+            colormap(ddb_getColors(handles.mapData.colorMaps.earth,64)*255);
+            
+            caxis([zmin zmax]);
+            
+            x=handles.Model(md).Input(id).GridX;
+            y=handles.Model(md).Input(id).GridY;
+            z=handles.Model(md).Input(id).Depth;
+
+            z0=zeros(size(z));
+            bathy=surface(x,y,z);
+            set(bathy,'FaceColor','flat');
+            set(bathy,'HitTest','off');
+            set(bathy,'Tag','FlowBathymetry','UserData',id);
+            set(bathy,'EdgeColor','none');
+            set(bathy,'ZData',z0);
+            if strcmp(get(findobj((get(handles.GUIHandles.Menu.View.Model,'Children')),'Label','Bathymetry'),'Checked'),'off')
+                set(bathy,'Visible','off');
+            end
+        end
+
+    case{'delete'}
+        h=findobj(gca,'Tag','FlowBathymetry','UserData',id);
+        delete(h);
+
+    case{'activate'}
+        h=findobj(gca,'Tag','Bathymetry','UserData',id);
+        if ~isempty(h)
+            set(h,'Visible','on');
+        end
+
+    case{'deactivate'}
+        h=findobj(gca,'Tag','Bathymetry','UserData',id);
+        if ~isempty(h)
+            set(h,'Visible','off');
+        end
+
+end
+
