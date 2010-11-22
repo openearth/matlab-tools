970235959e741c4086d2d6923bc0dcf096df0e68
boer_g <boer_g@localhost>
2010-11-22 13:46:05 +0000

    added xbeach contents
    svn path=/trunk/matlab/; revision=3363
    
    
    Former-commit-id: 1bc7885570934ce29008ad5315adb703a5f0c8bc

---
+++ applications/xbeach/contents.m
@@ -0,0 +1,14 @@
+% Generic Matlab tools for XBeach.
+%
+% For more info in XBeach please refer to our <a href="http://public.deltares.nl/display/XBEACH/">wiki</a> & <a href="http://www.xbeach.org/">mailinglist</a>.
+%
+% Tools:
+%   XB_readpars        - read input parameters
+%   XB_updateParams    - read input parameters
+%
+% Toolboxes:
+%   xb_analysis        - perform analysis on xbeach results
+%   xb_io              - read xbeach input files
+%   xb_modelsetup      - facilitate model set-up
+%   xb_readoutput      - read xbeach input files
+%   xb_visualise       - facilitate model visualisation
