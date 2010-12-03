ae8ec953be80cefae2ab1a5aa30828717cde0dfe
geer <geer@localhost>
2010-12-03 12:48:04 +0000

    fix test
    svn path=/trunk/matlab/; revision=3517
    
    
    Former-commit-id: 4624b7db8b6bb8f17de5b2e301d9c88cd927943a

--- maintenance/TeamCity/TeamCity_runtests.m
+++ maintenance/TeamCity/TeamCity_runtests.m
@@ -107,6 +107,10 @@ try %#ok<TRYNC>
         TeamCity.postmessage('progressStart','Prepare MTestRunner');
         %% initiate variables:
         maindir = oetroot;
+        targetdir = fullfile(oetroot,'teamcitytesthtml');
+        if isdir(targetdir)
+            rmdir(targetdir,'s');
+        end
 
         exclusions = {...
             '.svn',...
