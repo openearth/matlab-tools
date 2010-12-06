fe3c5588a9cb576e3340ccb74d15df6b1fd89a6a
geer <geer@localhost>
2010-12-06 10:36:01 +0000

    Give more feedback while running
    svn path=/trunk/matlab/; revision=3535
    
    
    Former-commit-id: 7f08b49c1fd1a0e81f170ec542f12b6ac6cc9bf0

--- maintenance/TeamCity/TeamCity_Matlab_Runner.bat
+++ maintenance/TeamCity/TeamCity_Matlab_Runner.bat
@@ -6,7 +6,10 @@ rem <http://www.jetbrains.com/teamcity/>
 rem Start matlab
 rem -----------------------------------
 
+echo 'map matlab drive'
 %map_matlab%
+
+echo 'map network drive'
 %map_network_drive%
 
 %matlab_path%matlab.exe -nosplash -nodesktop -minimize -r "%matlab_command%" -logfile mlogfile.log -sd "%teamcity.build.workingDir%"
@@ -19,9 +22,12 @@ rem check whether matlab.exe is still running
 tasklist /FI "IMAGENAME eq MATLAB.exe" 2>NUL | find /I /N "MATLAB.exe">NUL
 if "%ERRORLEVEL%"=="0" goto loopmatlabbusy
 
-echo 'teamcity OK'
+echo 'Matlab stopped'
 
+echo 'Unmap matlab drive'
 %unmap_matlab%
+
+echo 'Unmap network drive'
 %unmap_network_drive%
 
 goto end
