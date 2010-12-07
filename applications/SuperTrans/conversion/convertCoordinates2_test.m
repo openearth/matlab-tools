a3c967e4d65bf16222116b9553f47f84f031f8c5
thijs@damsma.net <thijs@damsma.net@localhost>
2010-12-07 17:24:11 +0000

    updated convertCoordinates test, * considerably shortened run time * improved error logging output formatting * improved test cases
    svn path=/trunk/matlab/; revision=3573
    
    
    Former-commit-id: 106096a749f47e259d9d764a72f5722e2b204f64

--- applications/SuperTrans/conversion/convertCoordinates2_test.m
+++ applications/SuperTrans/conversion/convertCoordinates2_test.m
@@ -7,29 +7,23 @@ function OK = convertCoordinates2_test
 %See also: CONVERTCOORDINATES, CONVERTCOORDINATES_TEST
 
 MTestCategory.DataAccess;
-if TeamCity.running
-    TeamCity.ignore('Test takes very long');
-    OK = 1;
-    return;
-end
+
+% Test is shortened to run in 1 minute
+% if TeamCity.running
+%     TeamCity.ignore('Test takes very long');
+%     OK = 1;
+%     return;
+% end
 
 % load coordinate systems
-load('EPSG_v7_5');
-codes = coordinate_reference_system.coord_ref_sys_code;
-kinds = coordinate_reference_system.coord_ref_sys_kind;
+EPSG = load('EPSG');
+codes = EPSG.coordinate_reference_system.coord_ref_sys_code;
+kinds = EPSG.coordinate_reference_system.coord_ref_sys_kind;
 
-geo_wgs84_code = 4326;
-
-% test coordinates for geographic systems
-lon = 05+24/60+02.5391/3600;
-lat = 53+25/60+13.2124/3600;
-
-% test coordinates for projected systems
-x = 100000;
-y = 100000;
+geo_code = 4326; % 4326 = WGS 84
 
 % create file for printing error messages
-pat = fileparts(which('convertCoordinates2_test'))
+pat = fileparts(which('convertCoordinates2_test'));
 fid = fopen([pat filesep 'convertCoordinate_errorMessages.txt'],'w');
 
 % preacclocate testresult
@@ -37,41 +31,40 @@ testresult = nan(size(codes));
 
 % start loop over all systems
 for s = 1:length(codes)
-% check what kind of system this is
+    % check what kind of system this is
     switch kinds{s}
-        case 'geographic 2d'
-        try
-            [lon2,lat2] = convertcoordinates(lon ,lat ,'CS1.code',codes(s),'CS2.code', 4326);
-            [lon3,lat3] = convertcoordinates(lon2 ,lat2 ,'CS1.code', 4326,'CS2.code',codes(s));
-            testresult(s) = abs(lon - lon3) < 0.0001 & abs(lat -lat3) < 0.0001;
-            fprintf(fid,'%s\n',['Converting using system ' coordinate_reference_system.coord_ref_sys_name{s} ', with code ' num2str(codes(s)) ':']);
-            if testresult(s)
-                fprintf(fid,'%s\n\n','Test result is OK!');
-            else
-                fprintf(fid,'%s\n\n',['Test result is not OK: diff(lon) = ' num2str(abs(lon-lon3)) ' deg and diff(lat) = ' num2str(abs(lat-lat3)) ' deg']);
+        case {'geographic 2D', 'projected'}
+            fprintf(fid,'Code :%-9.0f| Name:%-68s| ',codes(s), EPSG.coordinate_reference_system.coord_ref_sys_name{s});
+            try
+                % try conversion at the center and the limits of the
+                % area_of_use (area where coordinate system should be valid) 
+                
+                ind           = find(EPSG.coordinate_reference_system.coord_ref_sys_code == codes(s),1,'first');
+                ind           = find(EPSG.area.area_code == EPSG.coordinate_reference_system.area_of_use_code(ind),1,'first');
+                
+                [lat,lon]     = meshgrid(...
+                    linspace(EPSG.area.area_north_bound_lat(ind),EPSG.area.area_south_bound_lat(ind),3),...
+                    linspace(EPSG.area.area_east_bound_lon(ind) ,EPSG.area.area_west_bound_lon(ind) ,3));
+                lat = lat([5 1 3 7 9]); % center and corners
+                lon = lon([5 1 3 7 9]); % center and corners
+                                
+                [x   ,   y]   = convertCoordinates(lon ,lat ,EPSG,'CS1.code', geo_code,'CS2.code', codes(s));
+                [lon2,lat2]   = convertCoordinates(x   ,y   ,EPSG,'CS2.code', geo_code,'CS1.code', codes(s));
+                testresult(s) = all(abs(lon - lon2) < 0.0001 & abs(lat -lat2) < 0.0001);
+                if testresult(s)
+                    fprintf(fid,'Test result is OK!\n');
+                else
+                    % check if correct at the center
+                    if abs(lon(1) - lon2(1)) < 0.0001 && lat(1) -lat2(1) < 0.0001
+                        fprintf(fid,'Test result is OK in center, but not in corners: diff(lon) =%s | diff(lat) =%s \n',sprintf('% 10.4f',lon(:)-lon2(:)), sprintf('% 10.4f',lat(:)-lat2(:)));
+                    else
+                        fprintf(fid,'Test result is not OK: diff(lon) =%s | diff(lat) =%s \n',sprintf('% 10.4f',lon(:)-lon2(:)), sprintf('% 10.4f',lat(:)-lat2(:)));
+                    end
+                end
+            catch
+                testresult(s) = nan;
+                fprintf(fid,'%s\n',strrep(lasterr,char(10),' '));
             end
-        catch
-            testresult(s) = nan;
-            fprintf(fid,'%s\n',['Error using system ' coordinate_reference_system.coord_ref_sys_name{s} ', with code ' num2str(codes(s)) ':']);
-            fprintf(fid,'%s\n\n',lasterr);
-        end
-        case 'projected'
-        try
-            [lon2,lat2] = convertcoordinates(x ,y ,'CS1.code',codes(s),'CS2.code', 4326);
-            [x3,y3]     = convertcoordinates(lon2 ,lat2 ,'CS1.code', 4326,'CS2.code',codes(s));
-            testresult(s) = abs(x - x3) < 10 & abs(y -y3) < 10;
-            fprintf(fid,'%s\n',['Converting using system ' coordinate_reference_system.coord_ref_sys_name{s} ', with code ' num2str(codes(s)) ':']);
-            if testresult(s)
-                fprintf(fid,'%s\n\n','Test result is OK!');
-            else
-                fprintf(fid,'%s\n\n',['Test result is not OK: diff(x) = ' num2str(abs(x-x3)) ' m and diff(y) = ' num2str(abs(y-y3)) ' m']);
-            end
-
-        catch
-            testresult(s) = nan;
-            fprintf(fid,'%s\n',['Error using system ' coordinate_reference_system.coord_ref_sys_name{s} ', with code ' num2str(codes(s)) ':']);
-            fprintf(fid,'%s\n\n',lasterr);
-        end
     end
 end
 
