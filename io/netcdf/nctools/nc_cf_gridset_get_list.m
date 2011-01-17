e8824293671bce666c1cad24e936cdcefab60eed
boer_g <boer_g@localhost>
2011-01-17 14:09:37 +0000

    added some default datasets to test OPeNDAP CF gridsets
    svn path=/trunk/matlab/; revision=3890
    
    
    Former-commit-id: 30c26479b8846a815740f606589f72d632afd45b

--- io/netcdf/nctools/nc_cf_gridset_get_list.m
+++ io/netcdf/nctools/nc_cf_gridset_get_list.m
@@ -1,5 +1,5 @@
-function nc_cf_gridset_get_list(url,varargin)
-%nc_cf_gridset_get_list  list of all tiles and their times from opendap server
+function nc_cf_gridset_get_list(varargin)
+%NC_CF_GRIDSET_GET_LIST  list of all tiles and their times from opendap server
 %
 %    nc_cf_gridset_get_list(url,<keyword,value>)
 %
@@ -16,77 +16,100 @@ function nc_cf_gridset_get_list(url,varargin)
 %    nc_cf_gridset_get_list('http://dtvirt5.deltares.nl:8080/thredds/catalog/opendap/rijkswaterstaat/kustlidar/catalog.html')
 %    nc_cf_gridset_get_list('http://opendap.deltares.nl/thredds/catalog/opendap/rijkswaterstaat/kustlidar/catalog.html')
 %
+% NC_CF_GRIDSET_GET_LIST test all 5 above gridsets.
+%
 % A local netCDF gridset:
 %    nc_cf_gridset_get_list(pwd)
 %
 %See also: grid_2D_orthogonal, opendap_catalog, rijkswaterstaat
 
    if ~odd(nargin)
-   url      = 'http://opendap.deltares.nl/thredds/catalog/opendap/rijkswaterstaat/vaklodingen/catalog.xml';
-   firstarg = 2;
+   % jarkus grid test + production
+    urls{1}   = 'http://dtvirt5.deltares.nl:8080/thredds/catalog/opendap/rijkswaterstaat/jarkus/grids/catalog.html';
+    urls{2}   = 'http://opendap.deltares.nl/thredds/catalog/opendap/rijkswaterstaat/jarkus/grids/catalog.html';
+   
+   % vaklodingen grid test + production
+    urls{3}   = 'http://dtvirt5.deltares.nl:8080/thredds/catalog/opendap/rijkswaterstaat/vaklodingen/catalog.xml';
+    urls{4}   = 'http://opendap.deltares.nl/thredds/catalog/opendap/rijkswaterstaat/vaklodingen/catalog.xml';
+   
+   % kustlidar grid test + production
+    urls{5}   = 'http://dtvirt5.deltares.nl:8080/thredds/catalog/opendap/rijkswaterstaat/kustlidar/catalog.html';
+    urls{6}   = 'http://opendap.deltares.nl/thredds/catalog/opendap/rijkswaterstaat/kustlidar/catalog.html';
+    firstarg  = 1;
    else
-   firstarg = 1;
+   urls     = varargin{1};
+   firstarg = 2;
    end
    
   %OPT.xlsname = [mfilename('fullpath'),'.xls'];
    OPT.xlsname = ['nc_cf_gridset_get_list.xls'];
    
    OPT = setProperty(OPT,varargin{firstarg:end});
-
-%% get available files
-
-   L = opendap_catalog(url);
-   L = sort(L);
-
-%% get meta-info
-%  TO DO: get bounding box too
-
-   disp('    name                 : first date - last date  n                                                            ')
-   for i=1:length(L)
-       t{i} = nc_cf_time(L{i},'time');
-      t0(i) = t{i}(1);
-      t1(i) = t{i}(end);
-      nt(i) = length(t{i});
-     %disp([num2str(i,'%0.3d'),'/',num2str(length(L),'%0.3d')])
-      disp([num2str(i,'%0.3d'),' ',pad(filename(L{i}),' ',-21),': ',datestr(t0(i),29),' - ',datestr(t1(i),29),' ',num2str(nt(i),'%0.2d')])
-   end
-
-%% get all available dates
-
-   allt = [];
-   for i=1:length(L)
-   allt = [allt t{i}'];
-   end
-
-%% make ascii table with available years
-
-   years = unique(year(allt));
-   array =repmat('~',[length(L) length(years)]);
-   mask  =repmat(0  ,[length(L) length(years)]);
-   disp([url,' ', datestr(now)]);
-   disp(addrowcol(num2str(years')',0,-54,' '));
-   disp('#   name                 : first date - last date  n                                                            ')
-   for i=1:length(L)
-      [dummy,dummy,ind]=intersect(year(t{i}),years);
-      array(i,ind)='#';
-      mask (i,ind)=1;
-      disp([num2str(i,'%0.3d'),' ',pad(filename(L{i}),' ',-21),': ',datestr(t0(i),29),' - ',datestr(t1(i),29),' ',num2str(nt(i),'%0.2d'),' ',array(i,:)])
-   end
    
-%% make xls table with available years
+   if ischar(urls); urls = cellstr(urls);end
    
-   S.volgnummer = 1:length(L);
-   S.name       = char(L);
-   S.start      = datestr(t0,'yyyy-mm-dd HH:MM:SS');
-   S.end        = datestr(t1,'yyyy-mm-dd HH:MM:SS');
-   S.aantal     = nt;
-%%
-   for i=1:length(years)
+for i=1:length(urls)   
+
+   url = urls{i};
    
-   varname = ['year_',(num2str(years(i)))];
+   disp(['Testing: ',url])
+
+   %% get available files
    
-   S.(varname) = mask(:,i);
+      L = opendap_catalog(url);
+      L = sort(L);
    
-   end
+   %% get meta-info
+   %  TO DO: get bounding box too
    
-   struct2xls(OPT.xlsname,S,'header',{url,datestr(now),'Created with $nc_cf_gridset_get_list.m$'})
+      disp('    name                 : first date - last date  n  urlPath                                                   ')
+      for i=1:length(L)
+          t{i} = nc_cf_time(L{i},'time');
+         t0(i) = t{i}(1);
+         t1(i) = t{i}(end);
+         nt(i) = length(t{i});
+        %disp([num2str(i,'%0.3d'),'/',num2str(length(L),'%0.3d')])
+         disp([num2str(i,'%0.3d'),' ',pad(filename(L{i}),' ',-21),': ',datestr(t0(i),29),' - ',datestr(t1(i),29),' ',num2str(nt(i),'%0.2d'),' ',L{i}])
+      end
+   
+   %% get all available dates
+   
+      allt = [];
+      for i=1:length(L)
+      allt = [allt t{i}'];
+      end
+   
+   %% make ascii table with available years
+   
+      years = unique(year(allt));
+      array =repmat('~',[length(L) length(years)]);
+      mask  =repmat(0  ,[length(L) length(years)]);
+      disp([url,' ', datestr(now)]);
+      disp(addrowcol(num2str(years')',0,-54,' '));
+      disp('#   name                 : first date - last date  n                                                            ')
+      for i=1:length(L)
+         [dummy,dummy,ind]=intersect(year(t{i}),years);
+         array(i,ind)='#';
+         mask (i,ind)=1;
+         disp([num2str(i,'%0.3d'),' ',pad(filename(L{i}),' ',-21),': ',datestr(t0(i),29),' - ',datestr(t1(i),29),' ',num2str(nt(i),'%0.2d'),' ',array(i,:)])
+      end
+      
+   %% make xls table with available years
+      
+      S.volgnummer = 1:length(L);
+      S.name       = char(L);
+      S.start      = datestr(t0,'yyyy-mm-dd HH:MM:SS');
+      S.end        = datestr(t1,'yyyy-mm-dd HH:MM:SS');
+      S.aantal     = nt;
+   %%
+      for i=1:length(years)
+      
+      varname = ['year_',(num2str(years(i)))];
+      
+      S.(varname) = mask(:,i);
+      
+      end
+      
+      struct2xls(OPT.xlsname,S,'header',{url,datestr(now),'Created with $nc_cf_gridset_get_list.m$'})
+
+end
