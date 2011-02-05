dee6a887a79977ad96ac6b9a02067c2fe4d76923
ormondt <ormondt@localhost>
2011-02-05 14:18:20 +0000

    uipolyline added
    svn path=/trunk/matlab/; revision=3985
    
    
    Former-commit-id: e3310e1024e7b2e4c8e58f80b3283a9558a3363a

---
+++ applications/DelftDashBoard/general/plot/UIPolyline.m
@@ -0,0 +1,354 @@
+function [x,y,h]=UIPolyline(h,opt,varargin)
+
+if strcmpi(get(h,'Type'),'axes')
+    ax=h;
+end
+
+% Default values
+lineColor='g';
+lineWidth=1.5;
+marker='';
+markerEdgeColor='r';
+markerFaceColor='r';
+markerSize=4;
+maxPoints=10000;
+txt=[];
+callback=[];
+closed=0;
+
+% Not generic yet! DDB specific.
+windowbuttonupdownfcn=@ddb_setWindowButtonUpDownFcn;
+windowbuttonmotionfcn=@ddb_setWindowButtonMotionFcn;
+
+for i=1:length(varargin)
+    if ischar(varargin{i})
+        switch lower(varargin{i})
+            case{'linecolor','color'}
+                lineColor=varargin{i+1};
+            case{'linewidth','width'}
+                lineWidth=varargin{i+1};
+            case{'marker'}
+                marker=varargin{i+1};
+            case{'markeredgecolor'}
+                markerEdgeColor=varargin{i+1};
+            case{'markerfacecolor'}
+                markerFaceColor=varargin{i+1};
+            case{'markerfacesize'}
+                markerSize=varargin{i+1};
+            case{'max'}
+                maxPoints=varargin{i+1};
+            case{'text'}
+                txt=varargin{i+1};
+            case{'tag'}
+                tag=varargin{i+1};
+            case{'callback'}
+                callback=varargin{i+1};
+            case{'x'}
+                x=varargin{i+1};
+            case{'y'}
+                y=varargin{i+1};
+            case{'closed'}
+                closed=varargin{i+1};
+            case{'windowbuttonupdownfcn'}
+                windowbuttonupdownfcn=varargin{i+1};
+            case{'windowbuttonmotionfcn'}
+                windowbuttonmotionfcn=varargin{i+1};
+        end
+    end
+end
+
+switch lower(opt)
+    case{'draw'}
+       
+        % Plot first (invisible) point
+        
+        x=0;
+        y=0;       
+        h=plot3(x,y,9000);
+        set(h,'Visible','off');
+
+        set(h,'Tag',tag);
+        set(h,'Color',lineColor);
+        set(h,'LineWidth',lineWidth);
+        
+        if ~isempty(marker)
+            set(h,'Marker',marker);
+            set(h,'MarkerEdgeColor',markerEdgeColor);
+            set(h,'MarkerFaceColor',markerFaceColor);
+            set(h,'MarkerSize',markerSize);
+        end
+        
+        setappdata(h,'x',[]);
+        setappdata(h,'y',[]);
+        setappdata(h,'axes',ax);
+        setappdata(h,'closed',closed);
+        setappdata(h,'callback',callback);
+        setappdata(h,'tag',tag);        
+        setappdata(h,'color',lineColor);
+        setappdata(h,'width',lineWidth);
+        setappdata(h,'marker',marker);
+        setappdata(h,'markeredgecolor',markerEdgeColor);
+        setappdata(h,'markerfacecolor',markerFaceColor);
+        setappdata(h,'markersize',markerSize);
+        setappdata(h,'maxpoints',maxPoints);
+        setappdata(h,'text',txt);
+        setappdata(h,'closed',closed);
+        setappdata(h,'windowbuttonupdownfcn',windowbuttonupdownfcn);
+        setappdata(h,'windowbuttonmotionfcn',windowbuttonmotionfcn);
+
+        set(gcf, 'windowbuttondownfcn',   {@clickNextPoint,h});
+        set(gcf, 'windowbuttonmotionfcn', {@moveMouse,h});
+        
+    case{'plot'}
+        setappdata(h,'callback',callback);
+        setappdata(h,'tag',tag);        
+        setappdata(h,'color',lineColor);
+        setappdata(h,'width',lineWidth);
+        setappdata(h,'marker',marker);
+        setappdata(h,'markeredgecolor',markerEdgeColor);
+        setappdata(h,'markerfacecolor',markerFaceColor);
+        setappdata(h,'markersize',markerSize);
+        setappdata(h,'maxpoints',maxPoints);
+        setappdata(h,'text',txt);
+        setappdata(h,'closed',closed);
+        setappdata(h,'windowbuttonupdownfcn',windowbuttonupdownfcn);
+        setappdata(h,'windowbuttonmotionfcn',windowbuttonmotionfcn);        
+        drawPolyline(h);
+
+    case{'delete'}
+        ch=getappdata(h,'children');
+        delete(h);
+        delete(ch);
+
+end
+
+%%
+function drawPolyline(h)
+
+x=getappdata(h,'x');
+y=getappdata(h,'y');
+z=zeros(size(x))+100;
+tag=getappdata(h,'tag');
+lineColor=getappdata(h,'color');
+lineWidth=getappdata(h,'width');
+marker=getappdata(h,'marker');
+markerEdgeColor=getappdata(h,'markeredgecolor');
+markerFaceColor=getappdata(h,'markerfacecolor');
+markerSize=getappdata(h,'markersize');
+maxPoints=getappdata(h,'maxpoints');
+txt=getappdata(h,'text');
+callback=getappdata(h,'callback');
+ax=getappdata(h,'axes');
+closed=getappdata(h,'closed');
+windowbuttonupdownfcn=getappdata(h,'windowbuttonupdownfcn');
+windowbuttonmotionfcn=getappdata(h,'windowbuttonmotionfcn');
+
+ch=getappdata(h,'children');
+delete(h);
+delete(ch);
+
+if ~isempty(x)
+    h=plot3(x,y,z,'g');
+    set(h,'Tag',tag);
+    set(h,'Color',lineColor);
+    set(h,'LineWidth',lineWidth);
+    set(h,'HitTest','off');
+    
+    setappdata(h,'color',lineColor);
+    setappdata(h,'width',lineWidth);
+    setappdata(h,'marker',marker);
+    setappdata(h,'markeredgecolor',markerEdgeColor);
+    setappdata(h,'markerfacecolor',markerFaceColor);
+    setappdata(h,'markersize',markerSize);
+    setappdata(h,'maxpoints',maxPoints);
+    setappdata(h,'text',txt);
+    setappdata(h,'callback',callback);
+    setappdata(h,'x',x);
+    setappdata(h,'y',y);
+    setappdata(h,'tag',tag);
+    setappdata(h,'axes',ax);
+    setappdata(h,'closed',closed);
+    setappdata(h,'windowbuttonupdownfcn',windowbuttonupdownfcn);
+    setappdata(h,'windowbuttonmotionfcn',windowbuttonmotionfcn);
+    
+    for i=1:length(x)
+        mh(i)=plot3(x(i),y(i),200,['r' marker]);
+        set(mh(i),'MarkerEdgeColor',markerEdgeColor,'MarkerFaceColor',markerFaceColor,'MarkerSize',markerSize);
+        set(mh(i),'ButtonDownFcn',{@moveVertex});
+        set(mh(i),'Tag',tag);
+        setappdata(mh(i),'parent',h);
+        setappdata(mh(i),'number',i);
+    end
+    setappdata(h,'children',mh);
+    
+    if ~isempty(callback)
+        feval(callback,x,y);
+    end
+end
+        
+%%
+function clickNextPoint(imagefig, varargins,h)
+
+mouseclick=get(gcf,'SelectionType');
+
+ax=getappdata(h,'axes');
+
+buttonUpDownFcn=getappdata(h,'windowbuttonupdownfcn');
+buttonMotionFcn=getappdata(h,'windowbuttonmotionfcn');
+
+if strcmp(mouseclick,'normal')
+
+    pos=get(ax, 'CurrentPoint');
+    posx=pos(1,1);
+    posy=pos(1,2);
+    
+    xl=get(ax,'XLim');
+    yl=get(ax,'YLim');
+
+    if posx>=xl(1) && posx<=xl(2) && posy>=yl(1) && posy<=yl(2)
+        
+        x=getappdata(h,'x');
+        y=getappdata(h,'y');
+        maxPoints=getappdata(h,'maxpoints');
+        
+        np=length(x);
+        np=np+1;
+        
+        x=[x posx];
+        y=[y posy];
+        z=zeros(size(x))+9000;
+        
+        setappdata(h,'x',x);
+        setappdata(h,'y',y);
+        
+        set(h,'XData',x,'YData',y,'ZData',z);
+        set(h,'Visible','on');
+
+        if np==maxPoints
+            feval(buttonUpDownFcn);
+            feval(buttonMotionFcn);
+            drawPolyline(h);
+        end
+    end
+    
+else
+    closed=getappdata(h,'closed');
+    if closed
+        % Add last point
+        x=getappdata(h,'x');
+        y=getappdata(h,'y');
+        if ~isempty(x)
+            x(end+1)=x(1);
+            y(end+1)=y(1);
+            setappdata(h,'x',x);
+            setappdata(h,'y',y);
+        end
+    end
+    feval(buttonUpDownFcn);
+    feval(buttonMotionFcn);
+    drawPolyline(h);
+end
+
+%%
+function moveMouse(imagefig, varargins, h)
+
+x=getappdata(h,'x');
+y=getappdata(h,'y');
+ax=getappdata(h,'axes');
+
+np=length(x);
+
+pos=get(ax, 'CurrentPoint');
+posx=pos(1,1);
+posy=pos(1,2);
+
+xl=get(ax,'XLim');
+yl=get(ax,'YLim');
+
+if posx>=xl(1) && posx<=xl(2) && posy>=yl(1) && posy<=yl(2)
+    if np>0
+        x(np+1)=posx;
+        y(np+1)=posy;
+        z=zeros(size(x))+9000;
+        set(h,'XData',x,'YData',y,'ZData',z);
+    end
+end
+ddb_updateCoordinateText('crosshair');
+
+%%
+function moveVertex(imagefig, varargins)
+set(gcf, 'windowbuttonmotionfcn', {@followTrack});
+set(gcf, 'windowbuttonupfcn',     {@stopTrack});
+
+%%
+function followTrack(imagefig, varargins)
+h=get(gcf,'CurrentObject');
+p=getappdata(h,'parent');
+x=getappdata(p,'x');
+y=getappdata(p,'y');
+ch=getappdata(p,'children');
+nr=getappdata(h,'number');
+pos = get(gca, 'CurrentPoint');
+xi=pos(1,1);
+yi=pos(1,2);
+x(nr)=xi;
+y(nr)=yi;
+closed=getappdata(p,'closed');
+if closed
+    if nr==1
+        x(end)=x(1);
+        y(end)=y(1);
+    elseif nr==length(x)
+        x(1)=x(end);
+        y(1)=y(end);
+    end
+end
+setappdata(p,'x',x);
+setappdata(p,'y',y);
+set(p,'XData',x,'YData',y);
+if closed
+    if nr==1
+        set(ch(1),'XData',x(nr),'YData',y(nr));
+        set(ch(end),'XData',x(nr),'YData',y(nr));
+    elseif nr==length(x)
+        set(ch(1),'XData',x(nr),'YData',y(nr));
+        set(ch(end),'XData',x(nr),'YData',y(nr));
+    else
+        set(ch(nr),'XData',x(nr),'YData',y(nr));
+    end
+else
+    set(ch(nr),'XData',x(nr),'YData',y(nr));
+end
+ddb_updateCoordinateText('arrow');
+
+%%
+function stopTrack(imagefig, varargins)
+
+h=get(gcf,'CurrentObject');
+p=getappdata(h,'parent');
+buttonUpDownFcn=getappdata(p,'windowbuttonupdownfcn');
+buttonMotionFcn=getappdata(p,'windowbuttonmotionfcn');
+x=getappdata(p,'x');
+y=getappdata(p,'y');
+ch=getappdata(p,'children');
+nr=getappdata(h,'number');
+pos = get(gca, 'CurrentPoint');
+xi=pos(1,1);
+yi=pos(1,2);
+
+x(nr)=xi;
+y(nr)=yi;
+
+setappdata(p,'x',x);
+setappdata(p,'y',y);
+
+set(p,'XData',x,'YData',y);
+set(ch(nr),'XData',x(nr),'YData',y(nr));
+
+callback=getappdata(p,'callback');
+if ~isempty(callback)
+    feval(callback,x,y);
+end
+feval(buttonUpDownFcn);
+feval(buttonMotionFcn);
+
