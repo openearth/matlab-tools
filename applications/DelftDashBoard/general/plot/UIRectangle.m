84c31dc634176f2ed5910dfaf0b0eb98032950d8
ormondt <ormondt@localhost>
2011-03-04 16:47:13 +0000

    UIRectangle update
    svn path=/trunk/matlab/; revision=4173
    
    
    Former-commit-id: bc5a8d528b557c9b3bf217f5c6096a167b26c18f

--- applications/DelftDashBoard/general/plot/UIRectangle.m
+++ applications/DelftDashBoard/general/plot/UIRectangle.m
@@ -19,6 +19,7 @@ userdata=[];
 
 rotation=0;
 rotate=0;
+movable=1;
 
 tag='';
 
@@ -61,6 +62,8 @@ for i=1:length(varargin)
                 rotation=varargin{i+1};
             case{'rotate'}
                 rotate=varargin{i+1};
+            case{'movable'}
+                movable=varargin{i+1};
             case{'windowbuttonupdownfcn'}
                 windowbuttonupdownfcn=varargin{i+1};
             case{'windowbuttonmotionfcn'}
@@ -76,20 +79,13 @@ switch lower(opt)
         
         x=0;
         y=0;       
-        h=plot3(x,y,9000);
+        h=plot(x,y);
         set(h,'Visible','off');
 
         set(h,'Tag',tag);
         set(h,'Color',lineColor);
         set(h,'LineWidth',lineWidth);
-        
-        if ~isempty(marker)
-            set(h,'Marker',marker);
-            set(h,'MarkerEdgeColor',markerEdgeColor);
-            set(h,'MarkerFaceColor',markerFaceColor);
-            set(h,'MarkerSize',markerSize);
-        end
-        
+                
         setappdata(h,'x0',[]);
         setappdata(h,'y0',[]);
         setappdata(h,'dx',[]);
@@ -107,13 +103,15 @@ switch lower(opt)
         setappdata(h,'markersize',markerSize);
         setappdata(h,'text',txt);
         setappdata(h,'rotate',rotate);
+        setappdata(h,'movable',movable);
         setappdata(h,'windowbuttonupdownfcn',windowbuttonupdownfcn);
         setappdata(h,'windowbuttonmotionfcn',windowbuttonmotionfcn);
 
         set(gcf, 'windowbuttondownfcn',   {@startRectangle,h});
+        set(gcf, 'windowbuttonmotionfcn', {@dragRectangle,h});
         
     case{'plot'}
-        h=plot3(0,0,9000);
+        h=plot(0,0);
         setappdata(h,'callback',callback);
         set(h,'userdata',userdata);
         setappdata(h,'tag',tag);        
@@ -179,10 +177,8 @@ if ~isempty(x0)
     % Compute coordinates of corner points
     
     [x,y]=computeCoordinates(x0,y0,dx,dy,rotation);
-
-    z=zeros(size(x))+100;
     
-    h=plot3(x,y,z,'g');
+    h=plot(x,y,'g');
     set(h,'Tag',tag);
     set(h,'Color',lineColor);
     set(h,'LineWidth',lineWidth);
@@ -217,7 +213,7 @@ if ~isempty(x0)
             mh(i)=plot3(x(i),y(i),200,['r' marker]);
             set(mh(i),'MarkerEdgeColor',markerEdgeColor,'MarkerFaceColor',markerFaceColor,'MarkerSize',markerSize);
         end
-        set(mh(i),'ButtonDownFcn',{@moveVertex});
+        set(mh(i),'ButtonDownFcn',{@moveCornerPoint});
         set(mh(i),'Tag',tag);
         setappdata(mh(i),'parent',h);
         setappdata(mh(i),'number',i);
@@ -254,24 +250,28 @@ if posx>=xl(1) && posx<=xl(2) && posy>=yl(1) && posy<=yl(2)
     markerFaceColor=getappdata(h,'markerfacecolor');
     markerSize=getappdata(h,'markersize');
     tag=get(h,'Tag');
+    movable=getappdata(h,'movable');
     
     x=[x0 x0 x0 x0 x0];
     y=[y0 y0 y0 y0 y0];
-    z=zeros(size(x))+100;
     
-    set(h,'XData',x,'YData',y,'ZData',z);
+    set(h,'XData',x,'YData',y);
     set(h,'Visible','on');
     
     for i=1:4
         if i==1
             % Origin
-            mh(i)=plot3(x(i),y(i),200,['r' marker]);
-            set(mh(i),'MarkerEdgeColor',markerEdgeColor,'MarkerFaceColor',markerFaceColor,'MarkerSize',markerSize);
+            mh(i)=plot(x(i),y(i),['r' marker]);
+            set(mh(i),'MarkerEdgeColor',markerEdgeColor,'MarkerFaceColor','y','MarkerSize',markerSize+2);
         else
-            mh(i)=plot3(x(i),y(i),200,['y' marker]);
+            mh(i)=plot(x(i),y(i),['y' marker]);
             set(mh(i),'MarkerEdgeColor',markerEdgeColor,'MarkerFaceColor',markerFaceColor,'MarkerSize',markerSize);
         end
-        set(mh(i),'ButtonDownFcn',{@moveVertex});
+        if movable
+            set(mh(i),'ButtonDownFcn',{@changeRectangle,h,i});
+        else
+            set(mh(i),'HitTest','off');
+        end
         set(mh(i),'Tag',tag);
         setappdata(mh(i),'parent',h);
         setappdata(mh(i),'number',i);
@@ -279,12 +279,12 @@ if posx>=xl(1) && posx<=xl(2) && posy>=yl(1) && posy<=yl(2)
     setappdata(h,'children',mh);
     
     set(gcf, 'windowbuttonupfcn',     {@finishRectangle,h});
-    set(gcf, 'windowbuttonmotionfcn', {@moveMouse,h});
+    set(gcf, 'windowbuttonmotionfcn', {@dragRectangle,h});
 
 end
 
 %%
-function moveMouse(imagefig, varargins, h)
+function dragRectangle(imagefig, varargins, h)
 
 ax=getappdata(h,'axes');
 
@@ -299,70 +299,32 @@ if posx>=xl(1) && posx<=xl(2) && posy>=yl(1) && posy<=yl(2)
     
     x0=getappdata(h,'x0');
     y0=getappdata(h,'y0');
-    dx=posx-x0;
-    dy=posy-y0;
-    setappdata(h,'dx',dx);
-    setappdata(h,'dy',dy);
-    rotation=0;
-    ch=getappdata(h,'children');
-
-    [x,y]=computeCoordinates(x0,y0,dx,dy,rotation);
-
-    set(h,'XData',x,'YData',y);
     
-    for i=1:4
-        set(ch(i),'XData',x(i),'YData',y(i));
-    end
+    if ~isempty(x0)
 
+        dx=posx-x0;
+        dy=posy-y0;
+        setappdata(h,'dx',dx);
+        setappdata(h,'dy',dy);
+        rotation=0;
+        ch=getappdata(h,'children');
+        
+        [x,y]=computeCoordinates(x0,y0,dx,dy,rotation);
+        
+        setappdata(h,'x',x);
+        setappdata(h,'y',y);
+        
+        set(h,'XData',x,'YData',y);
+        
+        for i=1:4
+            set(ch(i),'XData',x(i),'YData',y(i));
+        end
+        
+    end
+    
 end
 
 ddb_updateCoordinateText('crosshair');
-
-%%
-function moveVertex(imagefig, varargins)
-set(gcf, 'windowbuttonmotionfcn', {@followTrack});
-set(gcf, 'windowbuttonupfcn',     {@stopTrack});
-
-%%
-function followTrack(imagefig, varargins)
-h=get(gcf,'CurrentObject');
-p=getappdata(h,'parent');
-x=getappdata(p,'x');
-y=getappdata(p,'y');
-ch=getappdata(p,'children');
-nr=getappdata(h,'number');
-pos = get(gca, 'CurrentPoint');
-xi=pos(1,1);
-yi=pos(1,2);
-x(nr)=xi;
-y(nr)=yi;
-closed=getappdata(p,'closed');
-if closed
-    if nr==1
-        x(end)=x(1);
-        y(end)=y(1);
-    elseif nr==length(x)
-        x(1)=x(end);
-        y(1)=y(end);
-    end
-end
-setappdata(p,'x',x);
-setappdata(p,'y',y);
-set(p,'XData',x,'YData',y);
-if closed
-    if nr==1
-        set(ch(1),'XData',x(nr),'YData',y(nr));
-        set(ch(end),'XData',x(nr),'YData',y(nr));
-    elseif nr==length(x)
-        set(ch(1),'XData',x(nr),'YData',y(nr));
-        set(ch(end),'XData',x(nr),'YData',y(nr));
-    else
-        set(ch(nr),'XData',x(nr),'YData',y(nr));
-    end
-else
-    set(ch(nr),'XData',x(nr),'YData',y(nr));
-end
-ddb_updateCoordinateText('arrow');
 
 %%
 function finishRectangle(imagefig, varargins,h)
@@ -388,6 +350,10 @@ rotation=0;
 
 [x,y]=computeCoordinates(x0,y0,dx,dy,rotation);
 
+setappdata(h,'x',x);
+setappdata(h,'y',y);
+setappdata(h,'rotation',rotation);
+
 set(h,'XData',x,'YData',y);
 for i=1:4
     set(ch(i),'XData',x(i),'YData',y(i));
@@ -397,6 +363,95 @@ callback=getappdata(h,'callback');
 if ~isempty(callback)
     feval(callback,x,y,h);
 end
+
+
+
+
+
+%%
+function changeRectangle(imagefig, varargins,h,i)
+
+switch get(gcf,'SelectionType')
+    case{'normal'}
+        % Move corner point
+        set(gcf, 'windowbuttonmotionfcn', {@moveCornerPoint,h,i,'move'});
+        set(gcf, 'windowbuttonupfcn',     {@moveCornerPoint,h,i,'finish'});
+    case{'alt'}
+        if i==1
+            % Move rectangle
+            set(gcf, 'windowbuttonmotionfcn', {@moveRectangle,h,i,'move'});
+            set(gcf, 'windowbuttonupfcn', {@moveRectangle,h,i,'finish'});
+        else
+            rotate=getappdata(h,'rotate');
+            if rotate
+                % Rotate rectangle
+                set(gcf, 'windowbuttonmotionfcn', {@rotateRectangle,h,i,'move'});
+                set(gcf, 'windowbuttonupfcn', {@rotateRectangle,h,i,'finish'});
+            end
+        end
+end
+
+%%
+function moveCornerPoint(imagefig, varargins,h,i,opt)
+
+ax=getappdata(h,'axes');
+
+pos = get(ax, 'CurrentPoint');
+posx=pos(1,1);
+posy=pos(1,2);
+
+x0=getappdata(h,'x0');
+y0=getappdata(h,'y0');
+dx=getappdata(h,'dx');
+dy=getappdata(h,'dy');
+rotation=getappdata(h,'rotation');
+x=getappdata(h,'x');
+y=getappdata(h,'y');
+
+[x0,y0,dx,dy,rotation]=computeDxDy(x0,y0,dx,dy,rotation,posx,posy,x,y,i,'move');
+
+[x,y]=computeCoordinates(x0,y0,dx,dy,rotation);
+
+setappdata(h,'x0',x0);
+setappdata(h,'y0',y0);
+setappdata(h,'dx',dx);
+setappdata(h,'dy',dy);
+setappdata(h,'x',x);
+setappdata(h,'y',y);
+
+set(h,'XData',x,'YData',y);
+
+ch=getappdata(h,'children');
+for i=1:4
+    set(ch(i),'XData',x(i),'YData',y(i));
+end
+
+ddb_updateCoordinateText('arrow');
+
+switch opt
+    case{'finish'}
+        buttonUpDownFcn=getappdata(h,'windowbuttonupdownfcn');
+        buttonMotionFcn=getappdata(h,'windowbuttonmotionfcn');
+        feval(buttonUpDownFcn);
+        feval(buttonMotionFcn);
+        callback=getappdata(h,'callback');
+        if ~isempty(callback)
+            feval(callback,x,y,h);
+        end
+end
+
+%%
+function moveRectangle(imagefig, varargins,h,i)
+
+%%
+function rotateRectangle(imagefig, varargins,h,i)
+
+%%
+function finishChangingRectangle(imagefig, varargins,h)
+
+
+
+
 
 %%
 function [x,y]=computeCoordinates(x0,y0,dx,dy,rotation)
@@ -410,3 +465,54 @@ x(4)=x(3)+dx*cos(pi*(rotation+180)/180);
 y(4)=y(3)+dx*sin(pi*(rotation+180)/180);
 x(5)=x0;
 y(5)=y0;
+
+
+
+
+%%
+function [x0,y0,dx,dy,rotation]=computeDxDy(x0,y0,dx,dy,rotation,posx,posy,x,y,i,opt)
+
+switch opt
+    case{'move'}
+        switch i
+            case 1
+                
+                x00=[posx posy];
+                
+                x1=[x(3) y(3)];
+                x2=[x(2) y(2)];
+                pt=sqrt((x2(1)-x1(1))^2  + (x2(2)-x1(2))^2);
+                dx=det([x2-x1 ; x1-x00])/pt;
+                
+                x1=[x(4) y(4)];
+                x2=[x(3) y(3)];
+                pt=sqrt((x2(1)-x1(1))^2  + (x2(2)-x1(2))^2);
+                dy=det([x2-x1 ; x1-x00])/pt;
+                
+                x0=posx;
+                y0=posy;
+                
+            case 2
+                dx=sqrt((posx-x0)^2 + (posy-y0)^2);
+                
+            case 3
+                
+                x00=[posx posy];
+                
+                x1=[x(1) y(1)];
+                x2=[x(4) y(4)];
+                pt=sqrt((x2(1)-x1(1))^2  + (x2(2)-x1(2))^2);
+                dx=det([x2-x1 ; x1-x00])/pt;
+                
+                x1=[x(2) y(2)];
+                x2=[x(1) y(1)];
+                pt=sqrt((x2(1)-x1(1))^2  + (x2(2)-x1(2))^2);
+                dy=det([x2-x1 ; x1-x00])/pt;
+                
+            case 4
+                dy=sqrt((posx-x0)^2 + (posx-x0)^2);
+                
+        end
+    case{'rotate'}
+end
+
