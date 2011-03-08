0600f64fb58bcb565bb1e8d719c455f97671b2ab
ormondt <ormondt@localhost>
2011-03-08 16:05:53 +0000

    further work tide station toolbox
    svn path=/trunk/matlab/; revision=4202
    
    
    Former-commit-id: 9bbfcb50b8b20bb76b47fe1c87b2e47c38506f8c

---
+++ applications/DelftDashBoard/models/Delft3DFLOW/toolbox/TideStations/ddb_Delft3DFLOW_addTideStations.m
@@ -0,0 +1,84 @@
+function ddb_Delft3DFLOW_addTideStations
+
+handles=getHandles;
+
+posx=[];
+
+iac=handles.Toolbox(tb).Input.activeDatabase;
+names=handles.Toolbox(tb).Input.database(iac).stationShortNames;
+
+xg=handles.Model(md).Input(ad).gridX;
+yg=handles.Model(md).Input(ad).gridY;
+
+xmin=min(min(xg));
+xmax=max(max(xg));
+ymin=min(min(yg));
+ymax=max(max(yg));
+
+ns=length(handles.Toolbox(tb).Input.database(iac).xLoc);
+n=0;
+
+x=handles.Toolbox(tb).Input.database(iac).xLoc;
+y=handles.Toolbox(tb).Input.database(iac).yLoc;
+
+% First find points within grid bounding box
+for i=1:ns
+    if x(i)>xmin && x(i)<xmax && ...
+       y(i)>ymin && y(i)<ymax
+       n=n+1;
+       posx(n)=x(i);
+       posy(n)=y(i);
+       name{n}=names{i};
+       istat(n)=i;
+    end
+end
+
+% Find stations within grid
+nrp=0;
+if ~isempty(posx)
+    [m,n]=findGridCell(posx,posy,xg,yg);
+    [m,n]=CheckDepth(m,n,handles.Model(md).Input(ad).depthZ);
+    for i=1:length(m)
+        if m(i)>0
+            nrp=nrp+1;
+            istation(nrp)=istat(i);
+            mm(nrp)=m(i);
+            nn(nrp)=n(i);
+            posx2(nrp)=posx(i);
+            posy2(nrp)=posy(i);
+        end
+    end
+end
+
+for i=1:nrp
+
+    k=istation(i);
+    
+    shortName=handles.Toolbox(tb).Input.database(iac).stationShortNames{k};
+    nobs=handles.Model(md).Input(ad).nrObservationPoints;
+    Names{1}='';
+    for k=1:nobs
+        Names{k}=handles.Model(md).Input(ad).observationPoints(k).name;
+    end
+
+    if isempty(strmatch(shortName,Names,'exact'))
+        nobs=nobs+1;
+        handles.Model(md).Input(ad).observationPoints(nobs).M=mm(i);
+        handles.Model(md).Input(ad).observationPoints(nobs).N=nn(i);
+        handles.Model(md).Input(ad).observationPoints(nobs).x=posx2(i);
+        handles.Model(md).Input(ad).observationPoints(nobs).y=posy2(i);
+        handles.Model(md).Input(ad).observationPoints(nobs).name=shortName;
+        handles.Model(md).Input(ad).observationPointNames{nobs}=shortName;
+        Names{nobs}=shortName;
+    end
+
+    handles.Model(md).Input(ad).nrObservationPoints=nobs;
+
+end
+
+if nrp>0
+    handles=ddb_Delft3DFLOW_plotAttributes(handles,'plot','observationpoints','domain',ad,'visible',1,'active',0);
+end
+
+setHandles(handles);
+
---
+++ applications/DelftDashBoard/models/Delft3DFLOW/toolbox/TideStations/ddb_Delft3DFLOW_exportTideSignals.m
@@ -0,0 +1,101 @@
+function ddb_Delft3DFLOW_exportTideSignals
+handles=getHandles;
+
+posx=[];
+
+iac=handles.Toolbox(tb).Input.activeDatabase;
+names=handles.Toolbox(tb).Input.database(iac).stationShortNames;
+
+xg=handles.Model(md).Input(ad).gridX;
+yg=handles.Model(md).Input(ad).gridY;
+
+xmin=min(min(xg));
+xmax=max(max(xg));
+ymin=min(min(yg));
+ymax=max(max(yg));
+
+ns=length(handles.Toolbox(tb).Input.database(iac).xLoc);
+n=0;
+
+x=handles.Toolbox(tb).Input.database(iac).xLoc;
+y=handles.Toolbox(tb).Input.database(iac).yLoc;
+
+wb = awaitbar(0,'Finding stations...');
+
+% First find points within grid bounding box
+for i=1:ns
+    if x(i)>xmin && x(i)<xmax && ...
+       y(i)>ymin && y(i)<ymax
+       n=n+1;
+       posx(n)=x(i);
+       posy(n)=y(i);
+       name{n}=names{i};
+       istat(n)=i;
+    end
+end
+
+% Find stations within grid
+nrp=0;
+if ~isempty(posx)
+    [m,n]=findGridCell(posx,posy,xg,yg);
+    for i=1:length(m)
+        if m(i)>0
+            nrp=nrp+1;
+            istation(nrp)=istat(i);
+        end
+    end
+end
+
+for i=1:nrp
+
+    k=istation(i);
+
+    stationName=handles.Toolbox(tb).Input.database(iac).stationList{k};
+
+    str=['Station ' stationName ' - ' num2str(i) ' of ' num2str(nrp) ' ...'];
+    [hh,abort2]=awaitbar(i/(nrp),wb,str);
+
+    if abort2 % Abort the process by clicking abort button
+        break;
+    end;
+    if isempty(hh); % Break the process when closing the figure
+        break;
+    end;
+    
+    t0=handles.Toolbox(tb).Input.startTime;
+    t1=handles.Toolbox(tb).Input.stopTime;
+    dt=handles.Toolbox(tb).Input.timeStep/1440;
+    tim=t0:dt:t1;
+    
+    % Read data from nc file
+    fname=[handles.toolBoxDir 'tidestations\' handles.Toolbox(tb).Input.database(iac).shortName '.nc'];
+    ncomp=length(handles.Toolbox(tb).Input.database(iac).components);
+    amp00=nc_varget(fname,'amplitude',[0 k],[ncomp 1]);
+    phi00=nc_varget(fname,'phase',[0 k],[ncomp 1]);
+    
+    components=[];
+    amplitudes=[];
+    phases=[];
+    
+    % Find non-zero amplitudes
+    ii=find(amp00~=0);
+    for j=1:length(ii)
+        ik=ii(j);
+        components{j}=handles.Toolbox(tb).Input.database(iac).components{ik};
+        amplitudes(j)=amp00(ik);
+        phases(j)=phi00(ik);
+    end
+    
+    latitude=handles.Toolbox(tb).Input.database(iac).y(k);
+    
+    wl=makeTidePrediction(tim,components,amplitudes,phases,latitude);
+    
+    shortName=handles.Toolbox(tb).Input.database(iac).stationShortNames{k};
+    fname=[shortName '.tek'];
+    exportTEK(wl',tim',fname,stationName);
+
+end
+
+try
+    close(wb);
+end
