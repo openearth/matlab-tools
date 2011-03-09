ae3acbbd516111102d3fee62cf5cdcc2eeed7e37
ormondt <ormondt@localhost>
2011-03-09 10:31:03 +0000

    select working directory fixed
    svn path=/trunk/matlab/; revision=4222
    
    
    Former-commit-id: b59e431cc8033a6c86a61b8539790c5e3f5f4c00

--- applications/DelftDashBoard/main/operations/ddb_selectWorkingDirectory.m
+++ applications/DelftDashBoard/main/operations/ddb_selectWorkingDirectory.m
@@ -2,13 +2,13 @@ function ddb_selectWorkingDirectory
 
 handles=getHandles;
 
-directoryname = uigetdir(handles.WorkingDirectory, 'Select working directory');
+directoryname = uigetdir(handles.workingDirectory, 'Select working directory');
 
 if directoryname==0
     return
 end
 
 cd(directoryname)
-handles.WorkingDirectory=directoryname;
+handles.workingDirectory=directoryname;
 
 setHandles(handles);
