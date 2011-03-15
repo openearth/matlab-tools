5726b7b1f736b86b84459e4c2cde0f4eabaa14d8
ormondt <ormondt@localhost>
2011-03-15 00:40:30 +0000

    matroos stuff added
    svn path=/trunk/matlab/; revision=4257
    
    
    Former-commit-id: 225594bbe8fee54dd583f66198262cf5299bd1c8

---
+++ applications/DelftDashBoard/toolboxes/ObservationStations/matroos_user_password.m
@@ -0,0 +1,5 @@
+function [user, passwd, url]=matroos_user_password
+%MATROOS_USER_PASSWORD  returns my matroos username and password
+user   = 'deltares';
+passwd = 'M@TR00$';
+url    = 'matroos.deltares.nl';
