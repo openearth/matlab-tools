987f6aa7943366c6b70c28c3a9b4845dd46a2092
ormondt <ormondt@localhost>
2011-03-19 11:29:12 +0000

    background in edit box made white
    svn path=/trunk/matlab/; revision=4303
    
    
    Former-commit-id: 3740c66d015937ae60f967b4a2b3b830885ecc65

--- general/gui_fun/GetUIString.m
+++ general/gui_fun/GetUIString.m
@@ -20,7 +20,7 @@ end
 handles.StrOut=[];
 
 handles.Text       = uicontrol(gcf,'Style','text',      'String',strin,   'Position',[ 30  85 240 15],'Tag','UIControl');
-handles.EditStrOut = uicontrol(gcf,'Style','edit',      'String','',      'Position',[ 30  60 240 20],'HorizontalAlignment','left');
+handles.EditStrOut = uicontrol(gcf,'Style','edit',      'String','',      'Position',[ 30  60 240 20],'HorizontalAlignment','left','BackgroundColor',[1 1 1]);
 handles.PushOK     = uicontrol(gcf,'Style','pushbutton','String','OK',    'Position',[200  30  70 20]);
 handles.PushCancel = uicontrol(gcf,'Style','pushbutton','String','Cancel','Position',[120  30  70 20]);
 
