dfb38256d715b660801630cf0b2a5a1f5c67137f
thiel <thiel@localhost>
2011-03-21 10:18:22 +0000

    - fixed fix nx and ny
    svn path=/trunk/matlab/; revision=4310
    
    
    Former-commit-id: be399aed3447daac2cc0f9748cc35e695e2a667b

--- applications/xbeach/xb_io/xb_dat/xb_dat_dims.m
+++ applications/xbeach/xb_io/xb_dat/xb_dat_dims.m
@@ -148,8 +148,8 @@ elseif regexp(fname, '^(drifter)\d+$')
 else
 
     % determine space dimensions
-    nx = d.globalx-1;
-    ny = d.globaly-1;
+    nx = d.globalx;
+    ny = d.globaly;
 
     % determine time dimension
     if regexp(fname, '_(mean|max|min|var)$')
