7d69aa07e57123ee8a396feb19893abb3aaaae73
boer_g <boer_g@localhost>
2011-04-13 07:03:07 +0000

    added option to save ascii file to txt file instead
    svn path=/trunk/matlab/; revision=4451
    
    
    Former-commit-id: 80b74e1c083bac3630e9aa6455438b3fdc892e61

--- io/netcdf/nctools/nc_cf_gridset_get_list.m
+++ io/netcdf/nctools/nc_cf_gridset_get_list.m
@@ -3,6 +3,9 @@ function nc_cf_gridset_get_list(varargin)
 %
 %    nc_cf_gridset_get_list(url,<keyword,value>)
 %
+% saves overview of grid tiles to *.xls, *.mat and *.nc file, 
+% choose custom names with keywords 'xlsname', 'matname', 'ncname' 
+%
 % Example: for OpenEarth test and production server
 %
 % RWS Jarkus Grids: production
@@ -20,6 +23,9 @@ function nc_cf_gridset_get_list(varargin)
 %
 % A local netCDF gridset:
 %    nc_cf_gridset_get_list(pwd)
+%
+% By default screen overview appears, save it to file with keyword 'fid' (default 1=to screen):
+%    nc_cf_gridset_get_list(pwd,'fid','nc_cf_gridset_get_list.txt')
 %
 %See also: grid_2D_orthogonal, opendap_catalog, rijkswaterstaat
 
@@ -43,8 +49,15 @@ function nc_cf_gridset_get_list(varargin)
    
   %OPT.xlsname = [mfilename('fullpath'),'.xls'];
    OPT.xlsname = ['nc_cf_gridset_get_list.xls'];
+   OPT.matname = ['nc_cf_gridset_get_list.mat'];
+   OPT. ncname = ['nc_cf_gridset_get_list.nc'];   
+   OPT.fid     = 1;
    
    OPT = setProperty(OPT,varargin{firstarg:end});
+   
+   if ischar(OPT.fid)
+       OPT.fid = fopen(OPT.fid,'w');
+   end
    
    if ischar(urls); urls = cellstr(urls);end
    
@@ -84,14 +97,21 @@ for i=1:length(urls)
       years = unique(year(allt));
       array =repmat('~',[length(L) length(years)]);
       mask  =repmat(0  ,[length(L) length(years)]);
-      disp([url,' ', datestr(now)]);
-      disp(addrowcol(num2str(years')',0,-54,' '));
-      disp('#   name                 : first date - last date  n                                                            ')
+      dprintf(OPT.fid,[url,' ', datestr(now),'\n']);
+      yearheader = [addrowcol(num2str(years')',0,-54,' ')];
+      for iy=1:size(yearheader,1)
+      dprintf(OPT.fid,[yearheader(iy,:),'\n']);
+      end
+      dprintf(OPT.fid,'#   name                 : first date - last date  n                                                            \n')
       for i=1:length(L)
          [dummy,dummy,ind]=intersect(year(t{i}),years);
          array(i,ind)='#';
          mask (i,ind)=1;
-         disp([num2str(i,'%0.3d'),' ',pad(filename(L{i}),' ',-21),': ',datestr(t0(i),29),' - ',datestr(t1(i),29),' ',num2str(nt(i),'%0.2d'),' ',array(i,:)])
+         dprintf(OPT.fid,[num2str(i,'%0.3d'),' ',pad(filename(L{i}),' ',-21),': ',datestr(t0(i),29),' - ',datestr(t1(i),29),' ',num2str(nt(i),'%0.2d'),' ',array(i,:),'\n'])
+      end
+      
+      if OPT.fid > 1
+          fclose(OPT.fid)
       end
       
    %% make xls table with available years
@@ -111,5 +131,9 @@ for i=1:length(urls)
       end
       
       struct2xls(OPT.xlsname,S,'header',{url,datestr(now),'Created with $nc_cf_gridset_get_list.m$'})
+      % in case there is no Excel installed make other formats too
+      struct2nc (OPT. ncname,S,'header',{url,datestr(now),'Created with $nc_cf_gridset_get_list.m$'})
+      save      (OPT.matname,'-struct','S')
+      
 
 end
