7b9626b9683f4aaa96b207568ed741e04ab09e9d
ormondt <ormondt@localhost>
2011-04-18 10:09:29 +0000

    Moved some files around
    svn path=/trunk/matlab/; revision=4482
    
    
    Former-commit-id: 79541e1a9f9bb29a0f24db5f5097f4c4ac3ab558

--- applications/DelftDashBoard/toolboxes/Nesting/nesthd2/nestsdo.m
+++
@@ -1,8 +0,0 @@
-inpdir='F:\USGS_Tijuana\sandiego\newgrid\newt8_05\';
-runid1='sdo';
-datadir='F:\USGS_Tijuana\southerncalifornia\coarse\sigma_tan\socaldd\nodens_saltemp\';
-runid2='sdodd';
-nestadm='F:\USGS_Tijuana\southerncalifornia\nesting2\nest_sdodd.adm';
-z0=0;
-opt='both'; % Can also be hydro or transport
-nesthd2(inpdir,runid1,datadir,runid2,nestadm,z0,opt);
