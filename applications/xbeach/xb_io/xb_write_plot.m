ff2e3c6ab5e1dc5c97a4a6c0eec7b5c3cc9f6016
hoonhout <hoonhout@localhost>
2011-04-20 15:35:04 +0000

    added zbuffer as default renderer
    svn path=/trunk/matlab/; revision=4505
    
    
    Former-commit-id: ccdec25253fe55b6f66521300c4c4808ec125c91

--- applications/xbeach/xb_io/xb_write_plot.m
+++ applications/xbeach/xb_io/xb_write_plot.m
@@ -1,6 +1,8 @@
 function xb_write_plot(fh, dir, name)
 %SAVEPLOT: provide figure handle, output directory and filename (without extension)
 
+set(fh, 'Renderer', 'ZBuffer');
+
 pname = fullfile(dir, name);
 
 hgsave(fh, [pname '.fig']);
