48c2780f1cc9f08a36c9d7ca019405f53eb03cc9
thijs@damsma.net <thijs@damsma.net@localhost>
2011-06-28 13:54:12 +0000

    improved surfer read so it can accept binary surfer files form version 6 and 7  and ascii grd files
    svn path=/trunk/matlab/; revision=4731
    
    
    Former-commit-id: 4d859df27db7425c08a844a5a1b7e47fa575ad19

--- general/io_fun/surfer_read.m
+++ general/io_fun/surfer_read.m
@@ -15,6 +15,10 @@ function [x y data OPT] = surfer_read(filename)
 %
 %   Example
 %   [x y data OPT] = surfer_read(filename.grd)
+%
+%   http://www.risoe.dk/vea/projects/nimo/WEngHelp/FileFormatofGRD.htm
+%   http://www.geospatialdesigns.com/surfer6_format.htm
+%   http://www.geospatialdesigns.com/surfer7_format.htm
 
 %% Copyright notice
 %   --------------------------------------------------------------------
@@ -61,29 +65,98 @@ function [x y data OPT] = surfer_read(filename)
 
 %% open grid file
 fid         = fopen(filename);
-
 %% read the limited meta data in the surfer grid file
-OPT.id      = char(fread(fid,4,'char')');
-OPT.size_x  =      fread(fid,1,'uint16');
-OPT.size_y  =      fread(fid,1,'uint16');
-OPT.min_x   =      fread(fid,1,'double');
-OPT.max_x   =      fread(fid,1,'double');
-OPT.min_y   =      fread(fid,1,'double');
-OPT.max_y   =      fread(fid,1,'double');
-OPT.min_val =      fread(fid,1,'double');
-OPT.max_val =      fread(fid,1,'double');
+OPT.id      = fread(fid,4,'*char' )';
 
-%% read the contents of the file 
-data        = nan(OPT.size_x,OPT.size_y);
-data(1:end) = fread(fid,numel(data),'single');
-data        = data';
+switch OPT.id
+    case 'DSAA' % Surfer ascii file format
+         %% read the meta data in the surfer grid file
+        OPT.size_x  = fscanf(fid,'%d',1); 
+        OPT.size_y  = fscanf(fid,'%d',1); 
+        OPT.min_x   = fscanf(fid,'%f',1);
+        OPT.max_x   = fscanf(fid,'%f',1);
+        OPT.min_y   = fscanf(fid,'%f',1);
+        OPT.max_y   = fscanf(fid,'%f',1);
+        OPT.min_val = fscanf(fid,'%f',1);
+        OPT.max_val = fscanf(fid,'%f',1);
+        
+        %% read the contents of the file
+        data        = nan(OPT.size_x,OPT.size_y);
+        data(1:end) = fscanf(fid,'%f');
+        data        = data';
+                
+        %% close the file
+        fclose(fid);
+        
+        %% set highest values to nan
+        data(data > 1e+037) = nan;
+        
+        %% calculate x and y
+        x = linspace(OPT.min_x,OPT.max_x,OPT.size_x);
+        y = linspace(OPT.min_y,OPT.max_y,OPT.size_y);
+    case 'DBSS' % Surfer 6 file format
+        
+        %% read the meta data in the surfer grid file
+        OPT.size_x  =      fread(fid,1,'uint16');
+        OPT.size_y  =      fread(fid,1,'uint16');
+        OPT.min_x   =      fread(fid,1,'double');
+        OPT.max_x   =      fread(fid,1,'double');
+        OPT.min_y   =      fread(fid,1,'double');
+        OPT.max_y   =      fread(fid,1,'double');
+        OPT.min_val =      fread(fid,1,'double');
+        OPT.max_val =      fread(fid,1,'double');
+        
+        %% read the contents of the file
+        data        = nan(OPT.size_x,OPT.size_y);
+        data(1:end) = fread(fid,numel(data),'single');
+        data        = data';
+        
+        %% close the file
+        fclose(fid);
+        
+        %% set highest values to nan
+        data(data > 1e+037) = nan;
+        
+        %% calculate x and y
+        x = linspace(OPT.min_x,OPT.max_x,OPT.size_x);
+        y = linspace(OPT.min_y,OPT.max_y,OPT.size_y);
+    case 'DSRB' % Surfer 7 file format
+        unknown1    = fread(fid,1,'uint32');
+        unknown2    = fread(fid,1,'uint32');
+        grid_keyw   = fread(fid,4,'*char' )';
+        grid_bytes  = fread(fid,1,'uint32');
+        OPT.size_y  = fread(fid,1,'uint32');
+        OPT.size_x  = fread(fid,1,'uint32');
+        OPT.min_x   = fread(fid,1,'double');
+        OPT.min_y   = fread(fid,1,'double');
+        OPT.dx      = fread(fid,1,'double');
+        OPT.dy      = fread(fid,1,'double');
+        OPT.min_val = fread(fid,1,'double');
+        OPT.max_val = fread(fid,1,'double');
+        unknown5    = fread(fid,1,'double');
+        fillvalue   = fread(fid,1,'double');
+        data_keyw   = fread(fid,4,'*char' )';
+        data_bytes  = fread(fid,1,'uint32');
+        %% errorchecking
+        if OPT.size_y*OPT.size_x ~= data_bytes/8
+            error('error')
+        end
 
-%% close the file
-fclose(fid);
+        %% read the contents of the file 
+        data        = nan(OPT.size_x,OPT.size_y);
+        data(1:end) = fread(fid,numel(data),'double');
+        data        = data';
 
-%% set highest values to nan
-data(data > 1e+037) = nan;
+        %% close the file
+        fclose(fid);
 
-%% calculate x and y
-x = linspace(OPT.min_x,OPT.max_x,OPT.size_x);
-y = linspace(OPT.min_y,OPT.max_y,OPT.size_y);
+        %% set highest values to nan
+        data(data==fillvalue) = nan;
+
+        %% calculate x and y
+
+        x = OPT.min_x + [0:OPT.size_x-1]*OPT.dx;
+        y = OPT.min_y + [0:OPT.size_y-1]*OPT.dy;
+    otherwise
+        warning('file format not surrported')
+end
