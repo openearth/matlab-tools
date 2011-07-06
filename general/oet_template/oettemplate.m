76566dcbd29332e69a8ec70ebc83978307a1437e
boer_g <boer_g@localhost>
2011-07-06 14:56:19 +0000

    Now Delft3D is open source, change OpenEarth template license to GNU GPL license (instead of GNU Lesser General Public license)
    svn path=/trunk/matlab/; revision=4771
    
    
    Former-commit-id: e109e90ff3edc13ca1913499b91e2dd2ba889755

--- general/oet_template/oettemplate.m
+++ general/oet_template/oettemplate.m
@@ -26,21 +26,21 @@ function $output = $filename($input)
 %
 %       $address
 %
-%   This library is free software: you can redistribute it and/or
-%   modify it under the terms of the GNU Lesser General Public
-%   License as published by the Free Software Foundation, either
-%   version 2.1 of the License, or (at your option) any later version.
+%   This library is free software: you can redistribute it and/or modify
+%   it under the terms of the GNU General Public License as published by
+%   the Free Software Foundation, either version 3 of the License, or
+%   (at your option) any later version.
 %
 %   This library is distributed in the hope that it will be useful,
 %   but WITHOUT ANY WARRANTY; without even the implied warranty of
-%   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-%   Lesser General Public License for more details.
+%   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%   GNU General Public License for more details.
 %
-%   You should have received a copy of the GNU Lesser General Public
-%   License along with this library. If not, see <http://www.gnu.org/licenses/>.
+%   You should have received a copy of the GNU General Public License
+%   along with this library.  If not, see <http://www.gnu.org/licenses/>.
 %   --------------------------------------------------------------------
 
-% This tool is part of <a href="http://OpenEarth.nl">OpenEarthTools</a>.
+% This tool is part of <a href="http://www.OpenEarth.eu">OpenEarthTools</a>.
 % OpenEarthTools is an online collaboration to share and manage data and 
 % programming tools in an open source, version controlled environment.
 % Sign up to recieve regular updates of this function, and to contribute 
