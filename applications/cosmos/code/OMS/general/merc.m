bb2a1a4a80fdf40b9fccc9a1801c7add16afa9bf
ormondt <ormondt@localhost>
2011-07-07 15:45:25 +0000

    cosmos removed
    svn path=/trunk/matlab/; revision=4777
    
    
    Former-commit-id: 78cdfb6221d16311a460113736fbcc615f9808f8

--- applications/cosmos/code/OMS/general/merc.m
+++
@@ -1,3 +0,0 @@
-function y=merc(y)
-
-y=180*log(tan(pi*y/180)+sec(pi*y/180))/pi;
--- applications/cosmos/code/OMS/tstzip.m
+++
@@ -1,6 +0,0 @@
-tic
-system(['F:\OperationalModelSystem\SoCalCoastalHazards\exe\zip.exe -q -j F:\OperationalModelSystem\SoCalCoastalHazards\temp\2098\sp2.zip F:\OperationalModelSystem\SoCalCoastalHazards\temp\2098\*.sp2']);
-toc
-tic
-system(['F:\OperationalModelSystem\SoCalCoastalHazards\exe\zip.exe -q -j -1 F:\OperationalModelSystem\SoCalCoastalHazards\temp\2098\sp2.zip F:\OperationalModelSystem\SoCalCoastalHazards\temp\2098\*.sp2']);
-toc
