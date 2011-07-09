59c9aafa4f33227988ae19023a218ad18e202f24
ormondt <ormondt@localhost>
2011-07-09 16:18:39 +0000

    mapplot resolution improved
    svn path=/trunk/matlab/; revision=4796
    
    
    Former-commit-id: 32be0b978ca46b9b67cf8a31859bc17a4df91a4e

--- applications/cosmos/code/OMSRunner/postproc/figures/cosmos_mapPlot.m
+++ applications/cosmos/code/OMSRunner/postproc/figures/cosmos_mapPlot.m
@@ -17,9 +17,6 @@ for i=1:length(varargin)
     end
 end
 
-% data.y=merc(data.y);
-% ylim=merc(ylim);
-
 szx=8;
 szy=szx*(ylim(2)-ylim(1))/(xlim(2)-xlim(1));
 
@@ -29,21 +26,34 @@ h=figure('Visible','off');
 
 set(h,'Units','centimeters');
 set(h,'Position',[0 0 pps]);
+set(h,'PaperUnits','centimeters');
+set(h,'PaperSize',pps);
+set(h,'PaperPosition',[0 0 pps]);
+set(h,'PaperPositionMode','manual');
 
 ax=axes;
 set(ax,'Units','centimeters');
 
-pcolor(data.x,data.y,data.z);caxis(clim);shading flat;
+pcolor(data.x,data.y,data.z);caxis(clim);
 colormap(clmap);
+shading flat;
+
+bgc=[0.5 0.5 0.5];
 
 set(ax,'XLim',xlim,'YLim',ylim);
 set(ax,'Box','off');
-set(ax,'XTick',-1e6,'YTick',-1e6);
-set(ax,'Position',[-0.02 -0.02 szx+0.04 szy+0.04]);
-set(h,'Color','none');
-set(ax,'Color','none');
+set(ax,'XTick',[],'YTick',[]);
+set(ax,'Position',[-0.00 -0.00 szx+0.00 szy+0.00]);
+set(h,'Color',bgc);
+set(ax,'Color',bgc);
+set(h,'InvertHardCopy', 'off');
 
-export_fig(h,fname,'-nocrop','-a1');
+print('-dpng','-r200','-painters','-loose',fname);
+
+uniquebgcolor=[127 127 127]; % <- select a color that does not exist in your image!!!
+im = imread(fname,'BackgroundColor',bgc);
+mask = bsxfun(@eq,im,reshape(uniquebgcolor,1,1,3));
+imwrite(im,fname,'png','Alpha',1-double(all(mask,3)));
 
 close(h);
 
