c542dfcd969f72b1f2c3dffa7d661e16a97e4402
ormondt <ormondt@localhost>
2011-07-26 13:46:05 +0000

    struct2xml added
    svn path=/trunk/matlab/; revision=4910
    
    
    Former-commit-id: 5c1367983338807de1a35dd55d74e97a6351ca30

---
+++ applications/DelftDashBoard/general/fileio/struct2xml.m
@@ -0,0 +1,67 @@
+function struct2xml(filename,s)
+% Writes Matlab structure s to xml file
+%
+% e.g.
+%
+% s.name.value='csm';
+% s.name.type='char';
+% s.size.value=4;
+% s.size.type='int';
+% s.stations(1).station.name.value='hallo!';
+% s.stations(1).station.name.type='char';
+% s.stations(1).station.lon.value=58;
+% s.stations(1).station.lon.type='real';
+% s.stations(2).station.name.value='haha';
+% s.stations(2).station.name.type='char';
+% s.stations(2).station.lon.value=53;
+% s.stations(2).station.lon.type='real';
+% struct2xml('test.xml',s);
+
+fid=fopen(filename,'wt');
+fprintf(fid,'%s\n','<?xml version="1.0"?>');
+fprintf(fid,'%s\n','<root>');
+splitstruct(fid,s,1,2);
+fprintf(fid,'%s\n','</root>');
+fclose(fid);
+
+%%
+function splitstruct(fid,s,ilev,nindent)
+fnames=fieldnames(s);
+for k=1:length(fnames)
+    if isfield(s.(fnames{k}),'value')
+        write2xml(fid,s,fnames{k},ilev,nindent)
+    else
+        fprintf(fid,'%s\n',[repmat(' ',1,ilev*nindent) '<' fnames{k} '>']);
+        ilev=ilev+1;
+        for j=1:length(s.(fnames{k}))
+            splitstruct(fid,s.(fnames{k})(j),ilev,nindent);
+        end
+        ilev=ilev-1;
+        fprintf(fid,'%s\n',[repmat(' ',1,ilev*nindent) '</' fnames{k} '>']);
+    end
+end
+
+%%
+function write2xml(fid,s,fldname,ilev,nindent)
+v=s.(fldname).value;
+fldnames=fieldnames(s.(fldname));
+attstr='';
+for j=1:length(fldnames)
+    if ~strcmpi(fldnames{j},'value')
+        attstr=[attstr ' ' fldnames{j} '="' s.(fldname).(fldnames{j}) '"'];        
+    end
+end
+tp=s.(fldname).type;
+str1=[repmat(' ',1,ilev*nindent) '<' fldname attstr '>'];
+switch lower(tp)
+    case{'char'}
+        str2=v;
+    case{'int'}
+        str2=num2str(v);
+    case{'real'}
+        str2=num2str(v);
+    case{'date'}
+        str2=datestr(v,'yyyymmdd HHMMSS');
+end
+str3=['</' fldname '>'];
+fprintf(fid,'%s%s%s\n',str1,str2,str3);
