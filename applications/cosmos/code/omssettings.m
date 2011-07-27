2882960143ec7eaeabf7a153481aac258778bab2
ormondt <ormondt@localhost>
2011-07-27 09:48:23 +0000

    omssettings updated
    svn path=/trunk/matlab/; revision=4922
    
    
    Former-commit-id: 68219b61f56ef96d3cbbc0640a3d11fac2e30769

--- applications/cosmos/code/omssettings.m
+++ applications/cosmos/code/omssettings.m
@@ -15,3 +15,10 @@ wlsettings;
 rmpath y:\app\matlab\toolbox\wl_snctools;
 rmpath y:\app\matlab\toolbox\wl_netcdf;
 rmpath y:\app\matlab\toolbox\wl_mexnc;
+
+rmpath 	Y:\app\MATLAB2009b\toolbox\images\colorspaces;
+rmpath 	Y:\app\MATLAB2009b\toolbox\images\images;
+rmpath 	Y:\app\MATLAB2009b\toolbox\images\imdemos;
+rmpath 	Y:\app\MATLAB2009b\toolbox\images\imuitools;
+rmpath 	Y:\app\MATLAB2009b\toolbox\images\iptformats;
+rmpath 	Y:\app\MATLAB2009b\toolbox\images\iptutils;
