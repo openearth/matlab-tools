1f284da9acab18b34c4346b051e0d5b6fc1d842d
b.t.grasmeijer@arcadis.nl <b.t.grasmeijer@arcadis.nl@localhost>
2011-08-12 08:04:20 +0000

    reclass reclasses matrix to plot pcolor or surf with non-equidistant colors
    svn path=/trunk/matlab/; revision=5038
    
    
    Former-commit-id: 450c3a92cc90ab9dd66a5c295f4433b70e0115e0

---
+++ general/el_mat/reclass.m
@@ -0,0 +1,86 @@
+function mtrxr = reclass(mtrx,cls,varargin)
+%RECLASS reclasses matrix to plot pcolor or surf with non-equidistant colors
+%
+%   Syntax:
+%   mtrxr = reclass(mtrx,cls,varargin)
+%
+%   Input:
+%   mtrx  = matrix of Z values
+%
+%   Output:
+%   mtrxr = matrix with reclassed Z values
+%
+%   Example
+%       figure;
+%       mylevels = [-10 -5 0 .6 .8 1.0 1.25 1.5 1.75 2.0 2.5 3.0 3.5 10.0];
+%       mybed = reclass(Z,mylevels);
+%       pcolor(X,Y,mybed);
+%       shading flat;
+%       mymap = colormap(jet(13));
+%       clim([1:14]);
+%       axis equal; axis tight;
+%       hold on;
+%       colorbardiscrete('test',[1:13],'fixed',true,'reallevels',mylevels);
+%
+%   See also colorbardiscrete
+
+%% Copyright notice
+%   --------------------------------------------------------------------
+%   Copyright (C) 2011 ARCADIS
+%       ademaj
+%
+%       jeroen.adema@arcadis.nl
+%
+%       <ADDRESS>
+%
+%   This library is free software: you can redistribute it and/or modify
+%   it under the terms of the GNU General Public License as published by
+%   the Free Software Foundation, either version 3 of the License, or
+%   (at your option) any later version.
+%
+%   This library is distributed in the hope that it will be useful,
+%   but WITHOUT ANY WARRANTY; without even the implied warranty of
+%   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%   GNU General Public License for more details.
+%
+%   You should have received a copy of the GNU General Public License
+%   along with this library.  If not, see <http://www.gnu.org/licenses/>.
+%   --------------------------------------------------------------------
+
+% This tool is part of <a href="http://www.OpenEarth.eu">OpenEarthTools</a>.
+% OpenEarthTools is an online collaboration to share and manage data and
+% programming tools in an open source, version controlled environment.
+% Sign up to recieve regular updates of this function, and to contribute
+% your own tools.
+
+%% Version <http://svnbook.red-bean.com/en/1.5/svn.advanced.props.special.keywords.html>
+% Created: 12 Aug 2011
+% Created with Matlab version: 7.11.0.584 (R2010b)
+
+% $Id$
+% $Date$
+% $Author$
+% $Revision$
+% $HeadURL$
+% $Keywords: $
+
+%%
+OPT.interp=true;
+OPT = setproperty(OPT,varargin{:});
+
+if nargin==0;
+    varargout = OPT;
+    return;
+end
+%% code
+[mmax nmax] = size(mtrx);
+mtrxr = NaN(mmax,nmax);
+
+for k = 2:length(cls)
+    i = find(mtrx >= cls(k-1) & mtrx <= cls(k));
+    if OPT.interp
+    mtrxr(i) = k-1 + (mtrx(i)-cls(k-1))/(cls(k)-cls(k-1));
+    else
+        mtrxr(i) = k-1+0.5;
+    end
+end
