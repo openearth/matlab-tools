653e7c9fb0149fccb36e5cb8e65ce87520249497
boer_g <boer_g@localhost>
2011-08-19 10:30:24 +0000

    svn path=/trunk/matlab/; revision=5088
    Former-commit-id: 6ad6dc565e459849304d614fe37aa3cac69e8c46

--- applications/delft3d/+dflowfm/contents.m
+++ applications/delft3d/+dflowfm/contents.m
@@ -16,24 +16,26 @@ function G = readNet(varargin)
 % POST-PROCESSING
 %
 % Read/plot grid (no time)
-%  dflowfm.readNet          - Reads network data of unstructured net.
-%  dflowfm.plotNet          - Plot an unstructured grid.
-%  dflowfm.plotNetkml       - Plot an unstructured grid as Google Earth kml file (beta).
-%  dflowfm.peri2cell        - turn perimeter matrix into cell
-%  dflowfm.readNet2tri      - Reads network and triangulates quadrilaterals and pentagons
+%  dflowfm.readNet            - Reads network data of unstructured net.
+%  dflowfm.plotNet            - Plot an unstructured grid.
+%  dflowfm.plotNetkml         - Plot an unstructured grid as Google Earth kml file (beta).
+%  dflowfm.peri2cell          - turn perimeter matrix into cell
+%  dflowfm.readNet2tri        - Reads network and triangulates quadrilaterals and pentagons
 %
 % Read/plot map (per timestep)
-%  dflowfm.readMap          - Reads solution data on an unstructured net.
-%  dflowfm.plotMap          - Plot an unstructured map.
-%  dflowfm.plotMapkml       - Plot an unstructured map as Google Earth kml file (beta).
+%  dflowfm.readMap            - Reads solution data on an unstructured net.
+%  dflowfm.plotMap            - Plot an unstructured map.
+%  dflowfm.plotMapkml         - Plot an unstructured map as Google Earth kml file (beta).
+%
+%  dflowfm.add_CF_coordinates - appends CF coordinates to ncfile
 %
 % PRE-PROCESSING
 %
 % Convert delft3d-flow model to  Delft3D-FM
-%  dflowfm.mdf2mdu          - convert Delft3D-flow model input to D-Flow FM model input
-%  dflowfm.opendap2obs      - get list of observation points from netCDF (OPeNDAP) time series collection
-%  dflowfm.analyseHis       - validate water levels with netCDF (OPeNDAP) time series collection for time series & t_tide
-%  dflowfm.indexHis         - show overview of locations incl. coordinates
-%  dflowfm.fillDep          - fill depth values from OPeNDAP data source (single grid or gridset of tiles)
+%  dflowfm.mdf2mdu     - convert Delft3D-flow model input to D-Flow FM model input
+%  dflowfm.opendap2obs - get list of observation points from netCDF (OPeNDAP) time series collection
+%  dflowfm.analyseHis  - validate water levels with netCDF (OPeNDAP) time series collection for time series & t_tide
+%  dflowfm.indexHis    - show overview of locations incl. coordinates
+%  dflowfm.fillDep     - fill depth values from OPeNDAP data source (single grid or gridset of tiles)
 %
 % See also: delft3d
