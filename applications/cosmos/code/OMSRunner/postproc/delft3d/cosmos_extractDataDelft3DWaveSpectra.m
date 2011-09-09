5640a42c0b19d6e40bff3274254345e1afa06f88
ormondt <ormondt@localhost>
2011-09-09 08:21:35 +0000

    little fix in extract wave spectra delft3d
    svn path=/trunk/matlab/; revision=5214
    
    
    Former-commit-id: 67d9068084ca14e509df289bc5cef2d88acfea58

--- applications/cosmos/code/OMSRunner/postproc/delft3d/cosmos_extractDataDelft3DWaveSpectra.m
+++ applications/cosmos/code/OMSRunner/postproc/delft3d/cosmos_extractDataDelft3DWaveSpectra.m
@@ -6,25 +6,20 @@ outdir=[dr 'lastrun' filesep 'output' filesep];
 archdir = model.archiveDir;
 mkdir([archdir hm.cycStr filesep 'sp2']); %% sp2 Folder at line 24 cannot be created straight away from that line, dont know how to do it
 %% Read and store the spectra
-for i=1:model.nrTimeSeriesDatasets
-
-    stName=model.timeSeriesDatasets(i).station;
-%    stLongName=model.stations(i).longName;
-    tstart=max(model.tWaveOkay,model.tFlowOkay);
-
-    try
-%         model.stations(i).sp2id=lower(model.stations(i).sP2id);
-        sp2list=dir([outdir '/' model.timeSeriesDatasets(i).sp2id '.*.sp2']);
-
-        for j=1:length(sp2list)
-            [SP2Data.time(j).Separation,SP2Data.time(j).spec,SP2Data.time(j).Bulk]=...
-                getWaveSeparationFromSP2(([outdir sp2list(j).name]));
-%            SP2Data.Station=stLongName;
-            SP2Data.Station=stName;
+for i=1:model.nrTimeSeriesDatasets    
+    if strcmpi(model.timeSeriesDatasets(i).parameter,'sp2')
+        try
+            stName=model.timeSeriesDatasets(i).station;        
+            sp2list=dir([outdir '/' model.timeSeriesDatasets(i).sp2id '.*.sp2']);            
+            for j=1:length(sp2list)
+                [SP2Data.time(j).Separation,SP2Data.time(j).spec,SP2Data.time(j).Bulk]=...
+                    getWaveSeparationFromSP2(([outdir sp2list(j).name]));
+                SP2Data.Station=stName;
+            end
+            fname=[archdir hm.cycStr filesep 'sp2' filesep 'sp2.' stName '.mat'];
+            save(fname,'SP2Data');
+        catch
+            WriteErrorLogFile(hm,['Something went wrong reading SP2 data - ' hm.models(m).name]);
         end
-        fname=[archdir hm.cycStr filesep 'sp2' filesep 'sp2.' stName '.mat'];
-        save(fname,'SP2Data');
-    catch
-        WriteErrorLogFile(hm,['Something went wrong reading SP2 data - ' hm.models(m).name]);
-    end
+    end 
 end
