84a75fda09d1a5fa17ba2b4811d3e08c94dd2ae8
ormondt <ormondt@localhost>
2011-10-30 00:42:55 +0000

    getDirectories updated
    svn path=/trunk/matlab/; revision=5397
    
    
    Former-commit-id: c206f0f6c5c785e85d20f156fe70a49088177432

--- applications/DelftDashBoard/main/initialize/ddb_getDirectories.m
+++ applications/DelftDashBoard/main/initialize/ddb_getDirectories.m
@@ -26,22 +26,23 @@ else
         txt='Select folder (preferably named "delftdashboard") for data storage (e.g. d:\delftdashboard). You may need to create a new folder. Folder must be outside OET repository!';
         dirname = uigetdir(inipath,txt);
         
-        if dirname==0 || isempty(dirname)
+        if isnumeric(dirname)
+            dirname='';
+        end
+        
+        if isempty(dirname)
             error('Local data directory not found, check reference in ini-file!');
         end
         
         datadir=[dirname filesep 'data'];
 
-        if ~isdir(datadir)
-%             % Folder is empty, create new folder
-%             datadir=
-        end
-        disp('Making delftdashboard.ini file. Probably the first time DDB was started.');
-        fid=fopen([inipath 'DelftDashBoard.ini'],'wt');
+        disp('Making delftdashboard.ini file ...');
         
+        fid=fopen([inipath 'delftdashboard.ini'],'wt');        
         fprintf(fid,'%s\n','% Data directories');
-        fprintf(fid,'%s\n',['DataDir=' inipath 'data' filesep]);
+        fprintf(fid,'%s\n',['DataDir=' datadir filesep]);
         fclose(fid);
+
     end
     
     handles.settingsDir=[inipath 'settings' filesep];
@@ -51,13 +52,64 @@ else
     elseif exist([fileparts(inifile) filesep ddbdir])==7 % relative path
         ddbdir = [cd(cd([fileparts(inifile) filesep ddbdir])) filesep];
     else
-        error(['Local data directory ''' ddbdir ''' not found, check reference in ini-file!']);
+%         error(['Local data directory ''' ddbdir ''' not found, check reference in ini-file!']);
     end
     
     try
         additionalToolboxDir=getINIValue(inifile,'AdditionalToolboxDir');
     catch
         additionalToolboxDir=[];
+    end
+
+    if ~isdir(ddbdir)
+        
+        % Usually done the first time ddb is run. Files are copied from
+        % repository to DDB data folder
+        
+        % Create new folder
+        disp('Copying data file from repository ...');
+        mkdir(ddbdir);
+        mkdir([ddbdir 'bathymetry']);
+        copyfiles([inipath 'data' filesep 'bathymetry'],[ddbdir 'bathymetry']);
+        mkdir([ddbdir 'imagery']);
+        mkdir([ddbdir 'shorelines']);
+        copyfiles([inipath 'data' filesep 'shorelines'],[ddbdir 'shorelines']);
+        mkdir([ddbdir 'tidemodels']);
+        copyfiles([inipath 'data' filesep 'tidemodels'],[ddbdir 'tidemodels']);
+        mkdir([ddbdir 'toolboxes']);
+        
+        % Find toolboxes and copy all files in data folders
+        flist=dir([inipath 'toolboxes']);
+        for i=1:length(flist)
+            if isdir([inipath 'toolboxes' filesep flist(i).name])
+                switch lower(flist(i).name)
+                    case{'.','..','.svn'}
+                    otherwise
+                        if isdir([inipath 'toolboxes' filesep flist(i).name filesep 'data'])
+                            mkdir([ddbdir 'toolboxes' filesep flist(i).name]);
+                            copyfiles([inipath 'toolboxes' filesep flist(i).name filesep 'data'],[ddbdir 'toolboxes' filesep flist(i).name]);
+                        end
+                end
+            end
+        end
+        
+        % Find ADDITIONAL toolboxes and copy all files in data folders
+        if ~isempty(additionalToolboxDir)
+            flist=dir(additionalToolboxDir);
+            for i=1:length(flist)
+                if isdir([additionalToolboxDir filesep flist(i).name])
+                    switch lower(flist(i).name)
+                        case{'.','..','.svn'}
+                        otherwise
+                            if isdir([additionalToolboxDir filesep flist(i).name filesep 'data'])
+                                mkdir([ddbdir 'toolboxes' filesep flist(i).name]);
+                                copyfiles([additionalToolboxDir filesep flist(i).name filesep 'data'],[ddbdir 'toolboxes' filesep flist(i).name]);
+                            end
+                    end
+                end
+            end
+        end
+        
     end
     
 end
@@ -74,4 +126,13 @@ if isdeployed
 else
     dr=fileparts(which('EPSG.mat'));
     handles.superTransDir=[dr filesep];
+end
+
+function copyfiles(inpdir,outdir)
+% Copies all files (and not the directories!) to new folder
+flist=dir([inpdir filesep '*']);
+for i=1:length(flist)
+    if ~isdir([inpdir filesep flist(i).name])
+        copyfile([inpdir filesep flist(i).name],outdir);
+    end
 end
