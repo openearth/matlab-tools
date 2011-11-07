315ace501a565118a8fcfdcfcb2b6af4976fece1
ormondt <ormondt@localhost>
2011-11-07 19:05:49 +0000

    work on export function in ddb_ObservationStationsToolbox.m
    svn path=/trunk/matlab/; revision=5446
    
    
    Former-commit-id: 5dc5bc5b2d0aad85bf6df86e565a35b4c2d7fce6

--- applications/DelftDashBoard/toolboxes/ObservationStations/ddb_ObservationStationsToolbox.m
+++ applications/DelftDashBoard/toolboxes/ObservationStations/ddb_ObservationStationsToolbox.m
@@ -59,30 +59,35 @@ url=[handles.Toolbox(tb).Input.database(iac).URL idcode '/' idcode 'h9999.nc'];
 time=[];
 data=[];
 
-% wb = waitbox('Downloading data ...');
-% 
-% try
+wb = waitbox('Downloading data ...');
+
+try
+    
+    [time,data]=getTimeSeriesFromNDBC(handles.Toolbox(tb).Input.database(iac).URL,t0,t1,idcode,'wave_height');
+    
 %     time = nc_varget(url,'time');
 %     time=double(time);
 %     time=datenum(1970,1,1)+time/86400;
 %     it1=find(time<=t0,1,'last');
 %     it2=find(time>=t1,1,'first');
 %     j=handles.Toolbox(tb).Input.activeParameter;
-%     data = nc_varget(url,handles.Toolbox(tb).Input.database(iac).parameters(ii).Name{j},[it1-1 0 0],[it2-it1+1 1 1]);
+%     par='wave_height';
+% %    data = nc_varget(url,handles.Toolbox(tb).Input.database(iac).parameters(ii).Name{j},[it1-1 0 0],[it2-it1+1 1 1]);
+%     data = nc_varget(url,par,[it1-1 0 0],[it2-it1+1 1 1]);
 %     time=time(it1:it2);
-%     close(wb);
-%     if ~isempty(time) && ~isempty(data)
-%         stationName=handles.Toolbox(tb).Input.database(iac).stationNames{ii};
-%         ddb_plotTimeSeries(time,data,stationName);
-%     end
-%     
-%     close(wb);
-%     
-% catch
-%     
-%     close(wb);
-%     
-% end
+    close(wb);
+    if ~isempty(time) && ~isempty(data)
+        stationName=handles.Toolbox(tb).Input.database(iac).stationNames{ii};
+        ddb_plotTimeSeries(time,data,stationName);
+    end
+    
+    close(wb);
+    
+catch
+    
+    close(wb);
+    
+end
 
 
 %%
@@ -94,31 +99,41 @@ t1=handles.Toolbox(tb).Input.stopTime;
 iac=handles.Toolbox(tb).Input.activeDatabase;
 ii=handles.Toolbox(tb).Input.activeObservationStation;
 idcode=handles.Toolbox(tb).Input.database(iac).idCodes{ii};
-url=[handles.Toolbox(tb).Input.database(iac).URL idcode '/' idcode 't9999.nc'];
 
 time=[];
 data=[];
 
-% wb = waitbox('Downloading data ...');
-% 
-% try
+wb = waitbox('Downloading data ...');
+
+try
+
+    switch lower(handles.Toolbox(tb).Input.database(iac).shortName)
+        case{'ndbc'}
+            [time,data]=getTimeSeriesFromNDBC(handles.Toolbox(tb).Input.database(iac).URL,t0,t1,idcode,'wave_height');
+        case{'co-ops'}
+            [time,data]=getWLFromCoops(idcode,t0,t1);
+    end
+    
 %     time = nc_varget(url,'time');
 %     time=double(time);
 %     time=datenum(1970,1,1)+time/86400;
 %     it1=find(time<=t0,1,'last');
 %     it2=find(time>=t1,1,'first');
-%     data = nc_varget(url,handles.Toolbox(tb).Input.database(iac).parameters(ii).Name{1},[it1-1 0 0],[it2-it1+1 1 1]);
+%     par='wave_height';
+% %    data = nc_varget(url,handles.Toolbox(tb).Input.database(iac).parameters(ii).Name{1},[it1-1 0 0],[it2-it1+1 1 1]);
+%     data = nc_varget(url,par,[it1-1 0 0],[it2-it1+1 1 1]);
 %     time=time(it1:it2);
-% end
-% 
-% close(wb);
-% 
-% if ~isempty(time) && ~isempty(data)
-%     stationName=handles.Toolbox(tb).Input.database(iac).stationNames{ii};
-%     shortName=idcode;
-%     fname=[shortName '.tek'];
-%     exportTEK(data,time,fname,stationName);
-% end
+    close(wb);
+catch
+    close(wb);
+end
+
+if ~isempty(time) && ~isempty(data)
+    stationName=handles.Toolbox(tb).Input.database(iac).stationNames{ii};
+    shortName=idcode;
+    fname=[shortName '.tek'];
+    exportTEK(data,time,fname,stationName);
+end
 
 %%
 function selectObservationStationFromMap(imagefig, varargins)
