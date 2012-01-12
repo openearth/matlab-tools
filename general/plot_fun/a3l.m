8d4cce4a1bafae2ebc19bcc2ca575cac50bb8bb8
morelis <morelis@localhost>
2012-01-12 09:52:09 +0000

    svn path=/trunk/matlab/; revision=5696
    Former-commit-id: a0273cf43ac194c961dbac6cb1905302ef89621f

---
+++ general/plot_fun/a3l.m
@@ -0,0 +1,5 @@
+function a3l
+
+%Sets current papersize and position to a3 landscape and fills page
+
+set(gcf,'papero','l','papertype','a3','paperpos',[0 0 42 29.7]);
---
+++ general/plot_fun/a4l.m
@@ -0,0 +1,5 @@
+function a4l
+
+%Sets current papersize and position to a4 landscape and fills page
+
+set(gcf,'papero','l','papertype','a4','paperpos',[0 0 29.7 21]);
