54aceea209e1ce097ebf54cb384fd7cf8fb65ba1
boer_g <boer_g@localhost>
2012-02-16 12:08:33 +0000

    Matlab datestr crashes on NaN
    svn path=/trunk/matlab/; revision=5777
    
    
    Former-commit-id: 5cd809a5ce03f0f85122e324e295fe059a90bbec

---
+++ general/time_fun/datestrnan.m
@@ -0,0 +1,57 @@
+function S = datestrnan(varargin)
+%DATESTRNAN   datestr that doe snot crash on NaNs
+%
+%    S = DATESTRNAN(...,FillSymbol) where the optional
+%    FillSymbol is the symbol returned in case of NaNs,
+%    (default one space ' ').
+%
+%    Example
+%    S = datestrnan([nan now],'yyyy','*')
+%
+% See also: DATESTR
+
+%% Copyright notice
+%   --------------------------------------------------------------------
+%   Copyright (C) 2011 Deltares for NMDC.eu
+%       Gerben J. de Boer
+%
+%       gerben.deboer@Deltares.nl
+%
+%       Deltares
+%       P.O. Box 177
+%       2600 MH Delft
+%       The Netherlands
+%
+%   This library is free software: you can redistribute it and/or
+%   modify it under the terms of the GNU Lesser General Public
+%   License as published by the Free Software Foundation, either
+%   version 2.1 of the License, or (at your option) any later version.
+%
+%   This library is distributed in the hope that it will be useful,
+%   but WITHOUT ANY WARRANTY; without even the implied warranty of
+%   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+%   Lesser General Public License for more details.
+%
+%   You should have received a copy of the GNU Lesser General Public
+%   License along with this library. If not, see <http://www.gnu.org/licenses/>.
+%   --------------------------------------------------------------------
+
+%% Version <http://svnbook.red-bean.com/en/1.5/svn.advanced.props.special.keywords.html>
+% $Id$
+% $Date$
+% $Author$
+% $Revision$
+% $HeadURL$
+% $Keywords: $
+
+   V          = varargin{1};
+   FillValue  = varargin{end};
+   mask       = ~isnan(V);
+   S2         = datestr(V(mask),varargin{2:end-1});
+   sz         = size(S2); sz(1) = length(V);
+   S          = repmat(FillValue,sz);
+   index      = find(mask);
+   S(index,:) = S2;
+   
+   
+   
