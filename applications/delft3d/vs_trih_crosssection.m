13dc73d17d21660e86f9dcc86514be371d79c815
bartgrasmeijer.x <bartgrasmeijer.x@localhost>
2012-03-12 10:35:58 +0000

    vs_trih_crosssection.m fix: if no constituents then do not read advective and dispersive transport
    svn path=/trunk/matlab/; revision=5842
    
    
    Former-commit-id: 41570d9577352a987f38f24ab012bc679a5ff656

--- applications/delft3d/vs_trih_crosssection.m
+++ applications/delft3d/vs_trih_crosssection.m
@@ -14,14 +14,14 @@ function varargout = vs_trih_crosssection(trih,crosssection_id)
 %  - CTR  Monumentary discharge
 %  - 'substance_advective' Cumulative advective transport
 %  - 'substance_dispersive' Cumulative dispersive transport
-% 
+%
 % where crosssection_id can be :
-% - the index(es) in the trih file 
+% - the index(es) in the trih file
 % - crosssection name(s) as a multidimensional characters array.
 %   where crosssections are counted in the first dimension.
 % - a cell array of crosssection name(s)
-% 
-% Examples: 
+%
+% Examples:
 %
 % st = vs_trih_crosssection(trihfile(i),{'coast05','coast06'});
 % st = vs_trih_crosssection(trihfile(i),['coast05';'coast06']);
@@ -39,7 +39,7 @@ function varargout = vs_trih_crosssection(trih,crosssection_id)
 %   Copyright (C) 2005 Delft University of Technology
 %       Gerben J. de Boer
 %
-%       g.j.deboer@tudelft.nl	
+%       g.j.deboer@tudelft.nl
 %
 %       Fluid Mechanics Section
 %       Faculty of Civil Engineering and Geosciences
@@ -69,104 +69,105 @@ function varargout = vs_trih_crosssection(trih,crosssection_id)
 % $Revision$
 % $HeadURL$
 
-   iostat =  1;
+iostat =  1;
 
-   %% Get names from indices of vv.
-   %% ------------------------------
+%% Get names from indices of vv.
+%% ------------------------------
 
-   if iscell(crosssection_id)
-      crosssection_id = char(crosssection_id);
-   end
-   
-   if ischar(crosssection_id)
-      ST.name  = crosssection_id;
-      ST.index = vs_trih_crosssection_index(trih,crosssection_id);
-   elseif isnumeric(crosssection_id)
-      ST.index = crosssection_id;
-      ST.name  = permute(vs_let(trih,'his-const','NAMTRA',{ST.index}),[2 3 1]);
-   end
-   
-   if ~(length(ST.index)==size(ST.name,1))
-      disp (addrowcol(crosssection_id,0,[-1 1],''''))
-      disp(['Not all of above crosssections have match in : ',trih.FileName]);
-      iostat = -1;
-   end
+if iscell(crosssection_id)
+    crosssection_id = char(crosssection_id);
+end
 
-   %% Get data
-   %% ------------------------------
+if ischar(crosssection_id)
+    ST.name  = crosssection_id;
+    ST.index = vs_trih_crosssection_index(trih,crosssection_id);
+elseif isnumeric(crosssection_id)
+    ST.index = crosssection_id;
+    ST.name  = permute(vs_let(trih,'his-const','NAMTRA',{ST.index}),[2 3 1]);
+end
 
-   if iostat==1
-   
-      if ~(size(ST.index,1)==0)
-      
-         ST.m         = permute(vs_let(trih,'his-const','MNTRA',{[1 3],ST.index}),[1 2]);
-         ST.n         = permute(vs_let(trih,'his-const','MNTRA',{[2 4],ST.index}),[1 2]);
-      
-         ST.x         = permute(vs_let(trih,'his-const','XYTRA',{[1 3],ST.index}),[1 2]);
-         ST.y         = permute(vs_let(trih,'his-const','XYTRA',{[2 4],ST.index}),[1 2]);
-         
-         ST.kmax      = permute(vs_let(trih,'his-const','KMAX'                  ),[1 2]);
-         
-         ST.datenum   = vs_time(trih,0,1);
-         
-         if ~isempty(vs_get_elm_def(trih,'FLTR'))
-         ST.FLTR      = permute(vs_let(trih,'his-series','FLTR',{ST.index      }),[1 2]);
-         ST.CTR       = permute(vs_let(trih,'his-series','CTR' ,{ST.index      }),[1 2]);
-         ATR       = permute(vs_let(trih,'his-series','ATR' ,{ST.index,0    }),[1 3 2]);
-         DTR       = permute(vs_let(trih,'his-series','DTR' ,{ST.index,0    }),[1 3 2]);
-         
-         flds     = vs_get_constituent_index(trih);
-         fldnames = fieldnames(flds);
-         for ifld=1:length(fldnames)
-            fldname = fldnames{ifld};
-            if strcmpi(flds.(fldname).elementname,'GRO')
-               ST.([fldname,'_advective'             ]) = ATR(:,flds.(fldname).index);
-               ST.([fldname,'_dispersive'            ]) = DTR(:,flds.(fldname).index);
-               ST.([fldname,'_advective_description' ]) = 'Cumulative Advective transport of ',fldname,' through cross section (velocity points)';
-               ST.([fldname,'_dispersive_description']) = 'Cumulative Dispersive transport of ',fldname,' through cross section (velocity points)';
-               ST.([fldname,'_advective_units'       ]) = '-';
-               ST.([fldname,'_dispersive_units'      ]) = '-';
+if ~(length(ST.index)==size(ST.name,1))
+    disp (addrowcol(crosssection_id,0,[-1 1],''''))
+    disp(['Not all of above crosssections have match in : ',trih.FileName]);
+    iostat = -1;
+end
+
+%% Get data
+%% ------------------------------
+
+if iostat==1
+    
+    if ~(size(ST.index,1)==0)
+        
+        ST.m         = permute(vs_let(trih,'his-const','MNTRA',{[1 3],ST.index}),[1 2]);
+        ST.n         = permute(vs_let(trih,'his-const','MNTRA',{[2 4],ST.index}),[1 2]);
+        
+        ST.x         = permute(vs_let(trih,'his-const','XYTRA',{[1 3],ST.index}),[1 2]);
+        ST.y         = permute(vs_let(trih,'his-const','XYTRA',{[2 4],ST.index}),[1 2]);
+        
+        ST.kmax      = permute(vs_let(trih,'his-const','KMAX'                  ),[1 2]);
+        
+        ST.datenum   = vs_time(trih,0,1);
+        
+        if ~isempty(vs_get_elm_def(trih,'FLTR'))
+            ST.FLTR      = permute(vs_let(trih,'his-series','FLTR',{ST.index      }),[1 2]);
+            ST.CTR       = permute(vs_let(trih,'his-series','CTR' ,{ST.index      }),[1 2]);
+            LSTCI = vs_let(trih,'his-const','LSTCI');
+            if LSTCI > 0
+                ATR       = permute(vs_let(trih,'his-series','ATR' ,{ST.index,0    }),[1 3 2]);
+                DTR       = permute(vs_let(trih,'his-series','DTR' ,{ST.index,0    }),[1 3 2]);
+                flds     = vs_get_constituent_index(trih);
+                fldnames = fieldnames(flds);
+                for ifld=1:length(fldnames)
+                    fldname = fldnames{ifld};
+                    if strcmpi(flds.(fldname).elementname,'GRO')
+                        ST.([fldname,'_advective'             ]) = ATR(:,flds.(fldname).index);
+                        ST.([fldname,'_dispersive'            ]) = DTR(:,flds.(fldname).index);
+                        ST.([fldname,'_advective_description' ]) = 'Cumulative Advective transport of ',fldname,' through cross section (velocity points)';
+                        ST.([fldname,'_dispersive_description']) = 'Cumulative Dispersive transport of ',fldname,' through cross section (velocity points)';
+                        ST.([fldname,'_advective_units'       ]) = '-';
+                        ST.([fldname,'_dispersive_units'      ]) = '-';
+                    end
+                end
             end
-         end
-         
-         ST.FLTR_description = 'Total discharge through cross section (velocity points)';
-         ST.CTR_description  = 'Monumentary discharge through cross section (velocity points)';
+            ST.FLTR_description = 'Total discharge through cross section (velocity points)';
+            ST.CTR_description  = 'Monumentary discharge through cross section (velocity points)';
+            
+            ST.FLTR_units       = 'm3';
+            ST.CTR_units        = 'm3/s';
+        end
+        
+    else
+        
+        ST.m      = [];
+        ST.n      = [];
+        
+        ST.x      = [];
+        ST.y      = [];
+        
+        ST.angle  = [];
+        
+    end
+    
+    ST.FileName        = trih.FileName;
+    ST.Description     = 'Delft3d-FLOW monitoring point (*.obs) time serie.';
+    ST.extracted_at    = datestr(now,31);
+    ST.extracted_with  = 'vs_trih_crosssection.m  of G.J. de Boer (gerben.deboer@wldelft.nl)';
+    
+end
 
-         ST.FLTR_units       = 'm3';
-         ST.CTR_units        = 'm3/s';
-         end
+%% Output
+%% ------------------------------
 
-      else
-      
-         ST.m      = [];
-         ST.n      = [];
-      
-         ST.x      = [];
-         ST.y      = [];
-      
-         ST.angle  = [];
-
-      end   
-      
-      ST.FileName        = trih.FileName;
-      ST.Description     = 'Delft3d-FLOW monitoring point (*.obs) time serie.';
-      ST.extracted_at    = datestr(now,31);
-      ST.extracted_with  = 'vs_trih_crosssection.m  of G.J. de Boer (gerben.deboer@wldelft.nl)';
-      
-   end
-   
-   %% Output
-   %% ------------------------------
-
-   if     nargout==1
-     if iostat==1
+if     nargout==1
+    if iostat==1
         varargout = {ST};
-     else
+    else
         error(' ');
         varargout = {iostat};
-     end
-   elseif nargout==2
-     varargout = {ST,iostat};
-   end
-   
-%% EOF   
+    end
+elseif nargout==2
+    varargout = {ST,iostat};
+end
+
+%% EOF
