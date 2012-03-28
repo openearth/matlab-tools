7ae62416f3b3be8995dcf50c1a7e8aa5186e63bc
ormondt <ormondt@localhost>
2012-03-28 03:26:33 +0000

    svn path=/trunk/matlab/; revision=5895
    Former-commit-id: a57576a159bb477dca41a1faf25557d5f3d81d02

--- applications/DelftDashBoard/models/Delft3DFLOW/fileio/ddb_readBcaFile.m
+++ applications/DelftDashBoard/models/Delft3DFLOW/fileio/ddb_readBcaFile.m
@@ -63,6 +63,9 @@ function handles = ddb_readBcaFile(handles, id)
 %% Reads bca file and stores component sets in DDB structure
 
 fname=handles.Model(md).Input(id).bcaFile;
-handles.Model(md).Input(id).astronomicComponentSets=delft3dflow_readBcaFile(fname);
-handles.Model(md).Input(id).nrAstronomicComponentSets=length(handles.Model(md).Input(id).astronomicComponentSets);
-
+try
+    handles.Model(md).Input(id).astronomicComponentSets=delft3dflow_readBcaFile(fname);
+    handles.Model(md).Input(id).nrAstronomicComponentSets=length(handles.Model(md).Input(id).astronomicComponentSets);
+catch
+    disp('An error occured while reading bca file!');
+end
