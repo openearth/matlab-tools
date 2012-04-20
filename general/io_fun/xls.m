9e2a656a68c950fda54931b69932cc3b1667b208
tda.x <tda.x@localhost>
2012-04-20 13:29:21 +0000

    many changes to object oriented xls function. % xls is much faster than matlab native xls read when multiple calls are % made, because the refrence to the activex excel server is stored in the % object and reused. Similarly, the reference to the open workbook (xls % file) is reused and only updated when a differen workbook is opened.
    Changes:
    * changed to a handle object to allow a desctructor method to be defined
    * added destructor function to always close activex server when the object is obsolete (e.g. after a crash)
    * many other improvements
    
    svn path=/trunk/matlab/; revision=6027
    
    
    Former-commit-id: 023768e165e1aa20dc36057789e112b54a45a468

--- general/io_fun/xls.m
+++ general/io_fun/xls.m
@@ -1,118 +1,216 @@
-classdef xls
-    %XLS  fast, object oriented, reading and writing of xls files
-    %
-    %   Still in beta
-    %   absolute filename must be used
-    %
-    %   Methods for class xls:
-    %   close  open   read   write  
-    %
-    %   Example
-    %   xls
-    %
-    %   See also
+classdef xls < handle
+%XLS  fast, object oriented, reading and writing of xls files
+%
+% xls is much faster than matlab native xls read when multiple calls are
+% made, because the refrence to the activex excel server is stored in the
+% object and reused. Similarly, the reference to the open workbook (xls
+% file) is reused and only updated when a differen workbook is opened.
+%
+%   Methods for class xls:
+%   open  read  write  save  getWorksheets  getUsedRange
+%
+%   Example
+%     % open instance of xls
+%     excelsheet = xls.('mysheet.xls')
+%
+%     % read data from sheet1, range 'A1:B4'
+%     data = excelsheet.read('sheet1','A1:B4')
+%
+%     % read from another file. the xls server is reused
+%     excelsheet.open('another_file.xls')
+%
+%     % call read with no arguments: the first sheet is read over the used range
+%     data = excelsheet.read
+%
+%     % clearing the sheet also closes the xls server
+%     clear excelsheet
+%
+%   See also: xls read
     
-    %% Copyright notice
-    %   --------------------------------------------------------------------
-    %   Copyright (C) 2011 Van Oord Dredging and Marine Contractors BV
-    %       Thijs Damsma
-    %
-    %       tda@vanoord.com
-    %
-    %       Watermanweg 64
-    %       3067 GG
-    %       Rotterdam
-    %       The Netherlands
-    %
-    %   This library is free software: you can redistribute it and/or modify
-    %   it under the terms of the GNU General Public License as published by
-    %   the Free Software Foundation, either version 3 of the License, or
-    %   (at your option) any later version.
-    %
-    %   This library is distributed in the hope that it will be useful,
-    %   but WITHOUT ANY WARRANTY; without even the implied warranty of
-    %   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    %   GNU General Public License for more details.
-    %
-    %   You should have received a copy of the GNU General Public License
-    %   along with this library.  If not, see <http://www.gnu.org/licenses/>.
-    %   --------------------------------------------------------------------
+%% Copyright notice
+%   --------------------------------------------------------------------
+%   Copyright (C) 2011 Van Oord Dredging and Marine Contractors BV
+%       Thijs Damsma
+%
+%       tda@vanoord.com
+%
+%       Watermanweg 64
+%       3067 GG
+%       Rotterdam
+%       The Netherlands
+%
+%   This library is free software: you can redistribute it and/or modify
+%   it under the terms of the GNU General Public License as published by
+%   the Free Software Foundation, either version 3 of the License, or
+%   (at your option) any later version.
+%
+%   This library is distributed in the hope that it will be useful,
+%   but WITHOUT ANY WARRANTY; without even the implied warranty of
+%   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%   GNU General Public License for more details.
+%
+%   You should have received a copy of the GNU General Public License
+%   along with this library.  If not, see <http://www.gnu.org/licenses/>.
+%   --------------------------------------------------------------------
+
+% This tool is part of <a href="http://www.OpenEarth.eu">OpenEarthTools</a>.
+% OpenEarthTools is an online collaboration to share and manage data and
+% programming tools in an open source, version controlled environment.
+% Sign up to recieve regular updates of this function, and to contribute
+% your own tools.
+
+%% Version <http://svnbook.red-bean.com/en/1.5/svn.advanced.props.special.keywords.html>
+% Created: 26 Aug 2011
+% Created with Matlab version: 7.12.0.62 (R2011a)
+
+% $Id$
+% $Date$
+% $Author$
+% $Revision$
+% $HeadURL$
+% $Keywords: $
     
-    % This tool is part of <a href="http://www.OpenEarth.eu">OpenEarthTools</a>.
-    % OpenEarthTools is an online collaboration to share and manage data and
-    % programming tools in an open source, version controlled environment.
-    % Sign up to recieve regular updates of this function, and to contribute
-    % your own tools.
-    
-    %% Version <http://svnbook.red-bean.com/en/1.5/svn.advanced.props.special.keywords.html>
-    % Created: 26 Aug 2011
-    % Created with Matlab version: 7.12.0.62 (R2011a)
-    
-    % $Id$
-    % $Date$
-    % $Author$
-    % $Revision$
-    % $HeadURL$
-    % $Keywords: $
-    
+%% start of code
     properties(GetAccess = 'public', SetAccess = 'private')
         % define the properties of the class here, (like fields of a struct)
-        excel;
         filename;
-        workbook;
         data_written;
+    end
+    properties(GetAccess = 'private', SetAccess = 'private')
+        % define the properties of the class here, (like fields of a struct)
+        excel;
+        workbook;
     end
     methods
         % methods, including the constructor are defined in this block
+        
         function obj = xls(filename)
-            if nargin ~= 1
+            if nargin == 1
+                obj.setFile(filename);
+            end
+            
+            obj.data_written = false;
+            obj.open;
+        end
+        
+        function delete(obj)
+            obj.close
+        end
+        
+        function setFile(obj,filename)
+            if nargin ~= 2
                 error('only input filename')
             end
-            % class constructor
             if ~exist(filename,'file')
                 error('file could not be found')
             end
-            fid = fopen(filename);
-            if fid <2
-                error('file could not be opened')
+            fns = dir2(filename);
+            filename = [fns.pathname fns.name];
+            if strcmpi(filename,obj.filename)
+                % same file, do nothing
+            else
+                % check new workbook
+                fid = fopen(filename);
+                assert(fid>=2,'file could not be opened');
+                fclose(fid);
+                
+                % clear open workbook if any
+                if ~isempty(obj.workbook)
+                    obj.closeWorkbook
+                end
+                
+                % reset filename, and open it
+                obj.filename = filename;
+                obj.open;
             end
-            fclose(fid);
-%             % use the absolute path for filename
-%             if ~isempty(strfind(filename,'.'))
-%                 obj.filename     = which(filename);
-%             else % append a '.' to make which look for a file is there is no '.' in the filename
-%                 obj.filename     = which([filename '.']);
-%             end
-            obj.filename     = filename;
-            obj.data_written = false;
         end
-        function obj = open(obj)
-            obj.excel = actxserver('excel.application');
-            % open workbook
-            obj.excel.DisplayAlerts = 0;
-            obj.workbook = obj.excel.workbooks.Open(obj.filename);
+        
+        function open(obj,filename)
+            % only open excel object if it is not available already
+            if isempty(obj.excel)
+                obj.excel = actxserver('excel.application');
+                obj.excel.DisplayAlerts = 0;
+            end
+            
+            if nargin == 2
+                % change filename if that is passed as argument
+                obj.setFile(filename);
+            elseif isempty(obj.workbook)
+                if isempty(obj.filename)
+                    warning('XLS:noFileSpecified','Call <obj>.open with an argument to specify which workbook to open');
+                    return
+                end
+                % only open workbook if there is not one open already;
+                obj.workbook = obj.excel.workbooks.Open(obj.filename);
+            end
         end
-        function obj = close(obj)
+        
+        function worksheets = getWorksheets(obj)
+            obj.open;
+            num_of_worksheets = obj.workbook.Worksheets.count;
+            for ii = num_of_worksheets:-1:1
+                names{ii} = obj.workbook.Worksheets.Item(ii).Name;
+            end
+            worksheets = names;
+        end
+        
+        function usedRange = getUsedRange(obj,sheet)
+            if nargin < 2
+                sheet = 1;
+            end
+            usedRange = obj.workbook.Worksheets.Item(sheet).UsedRange.Address;
+        end
+        
+        function save(obj)
             if obj.data_written
                 obj.workbook.Save;
+                obj.data_written = false;
             end
-            obj.excel.Quit;
-            obj.excel.delete;
         end
+        
+        function close(obj)
+            obj.closeWorkbook;
+            if ~isempty(obj.excel)
+                obj.excel.Quit;
+                obj.excel.delete;
+                obj.excel = [];
+            end
+        end
+        
+        function closeWorkbook(obj)
+            if ~isempty(obj.workbook)
+                if obj.data_written
+                    warning('XLS:dataDiscarded','Data was not saved in %s, call <obj>.save before closing',obj.filename);
+                end
+                obj.excel.workbooks.Close;
+                obj.workbook = [];
+            end
+        end
+        
         function data = read(obj,sheet,range)
+            obj.open;
+            if nargin < 2
+                sheet = 1;
+            end
             TargetSheet = get(obj.excel.sheets,'item',sheet);
             %Activate silently fails if the sheet is hidden
             set(TargetSheet, 'Visible','xlSheetVisible');
             % activate worksheet
             Activate(TargetSheet);
+            if nargin < 3
+                range = obj.getUsedRange(sheet);
+            end
             Select(Range(obj.excel,range));
             DataRange = get(obj.excel,'Selection');
             data = DataRange.Value;
         end
-        function obj = write(obj,data,sheet,range)
+        
+        function write(obj,data,sheet,range)
+            obj.open;
             if obj.workbook.ReadOnly ~= 0
                 %This means the file is probably open in another process.
-                error('MATLAB:xlswrite:LockedFile', 'The file %s is not writable.  It may be locked by another process.', file);
+                warning('XLS:LockedFile', 'The file %s is not writable.  It may be locked by another process.',obj.filename);
+                return
             end
             TargetSheet = get(obj.excel.sheets,'item',sheet);
             %Activate silently fails if the sheet is hidden
@@ -130,5 +228,6 @@ classdef xls
             set(obj.excel.selection,'Value',data);
             obj.data_written = true;
         end
+        % write(obj,data,sheet,range)
     end
 end
