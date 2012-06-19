afd768a8a821d671cf29be44d8c5ddf5f497c891
huism_b <huism_b@localhost>
2012-06-19 15:23:25 +0000

    update
    svn path=/trunk/matlab/; revision=6454
    
    
    Former-commit-id: 488bcea58f25e2e84461dc3bcb1c710cab7e744d

--- applications/ITHK/Matlab/run/ITHK_runUB.m
+++ applications/ITHK/Matlab/run/ITHK_runUB.m
@@ -3,13 +3,11 @@ function result=ITHK_runUB(outputDir)
 % Leon de Jongste, Witteveen+Bos
 % last edited: 15-03-2011
 
-%% run Unibest-CL
-% % method 1
-% [status,result] = dos('COMPUTECLR.bat','-echo');
 global S
 
-% method 2
+%% run Unibest-CL
+curdir=cd;
 cd(S.settings.outputdir);
 batchfileName='computeClrIT.bat';
 [status,result] = system(batchfileName,'-echo');
-cd(S.settings.basedir)
+cd(curdir);
