8677658b52cabb4f3fdceb762a11268cc1e1523c
huism_b <huism_b@localhost>
2012-06-19 15:19:33 +0000

    renamed ITHK_postprocessing2.m to ITHK_postprocessing.m
    svn path=/trunk/matlab/; revision=6452
    
    
    Former-commit-id: 2452a9528a877d8183f46de24927cfce440046c4

--- applications/ITHK/Matlab/postprocessing/ITHK_postprocessing.m
+++ applications/ITHK/Matlab/postprocessing/ITHK_postprocessing.m
@@ -1,25 +1,66 @@
-function ITHK_postprocessing(sens)
-
-global S
+function ITHK_postprocessing2(sens)
 
 %% Postprocessing Unibest Interactive Tool
+global S
 
-%% Prepare plotting in GE
-ITHK_mapUBtoGE(sens);
+%% General PP settings
+% Extract MDAdata for original and updated coastline
+[S.PP.settings.MDAdata_ORIG_OLD]=ITHK_readMDA('BASIS_ORIG_OLD.MDA');
+[S.PP.settings.MDAdata_ORIG]=ITHK_readMDA('BASIS_ORIG.MDA');
+[S.PP.settings.MDAdata_NEW]=ITHK_readMDA('BASIS.MDA');
+
+% time settings
+S.PP.settings.tvec = S.UB(sens).results.PRNdata.year;
+S.PP.settings.t0 = 2005;
+
+% settings
+S.PP.settings.dsRough         = str2double(S.settings.plotting.barplot.dsrough); % grid size rough grid
+S.PP.settings.dsFine          = str2double(S.settings.plotting.barplot.dsfine);  % grid size fine grid
+S.PP.settings.dxFine          = str2double(S.settings.plotting.barplot.widthfine); % distance from suppletion location where fine grid is used
+S.PP.settings.sVectorLength   = str2double(S.settings.plotting.barplot.barscalevector);   % scaling factor for vector length
+
+% initial coast line
+S.PP.settings.x0              = S.PP.settings.MDAdata_ORIG.Xcoast; 
+S.PP.settings.y0              = S.PP.settings.MDAdata_ORIG.Ycoast; 
+S.PP.settings.s0              = distXY(S.PP.settings.MDAdata_ORIG.Xcoast,S.PP.settings.MDAdata_ORIG.Ycoast);
+
+% Rough & fine grids
+S.PP.settings.sgridRough      = S.PP.settings.s0(1):S.PP.settings.dsRough:S.PP.settings.s0(end);
+S.PP.settings.sgridFine       = S.PP.settings.s0(1):S.PP.settings.dsFine:S.PP.settings.s0(end);
+S.PP.settings.idplotrough     = ones(length(S.PP.settings.sgridRough),1);
+S.PP.settings.widthRough      = max(mean(diff(S.UB(sens).results.PRNdata.xdist)),S.PP.settings.dsRough/2);
+S.PP.settings.widthFine       = max(mean(diff(S.UB(sens).results.PRNdata.xdist)),S.PP.settings.dsFine/2);
+
+% Find ids of fine grid corresponding to rough grid
+for ii=1:length(S.PP.settings.sgridRough)
+    distFR{ii} = abs(S.PP.settings.sgridFine-S.PP.settings.sgridRough(ii));
+    S.PP.settings.idFR(ii) = find(distFR{ii} == min(distFR{ii}),1,'first');
+end
 
 %% Make KML
 S.PP.output.kmlFileName     = [S.settings.outputdir filesep 'output' filesep S.userinput.name filesep S.userinput.name,'.kml'];
-%S.PP.output.kmlFileName     = [S.settings.outputdir S.userinput.name,'.kml'];
 KMLmapName      = S.userinput.name;
 nam             = S.userinput.name;
 
-% KML header
+%% KML header
 fid     = fopen(S.PP.output.kmlFileName,'w');
 kml  = [];
 S.PP.output.kml  = [kml KML_header('kmlName',nam)];
 
-% Add measures
-%NGRO =0;
+%% Add effect SLR to PRN info
+if S.userinput.indicators.slr == 1
+    ITHK_add_SLR(sens);
+end
+
+%% Map UB results to GE
+ITHK_mapUBtoGE2(sens);
+
+% % Add PRN info
+% if S.userinput.indicators.coast == 1
+%     ITHK_PRN_to_kml(sens);
+% end
+
+%% Add measures
 for jj = 1:length(S.userinput.phases)
     if ~strcmp(lower(strtok(S.userinput.phase(jj).GROfile,'.')),'basis')
         for ii = 1:length(S.userinput.phase(jj).groids)
@@ -39,20 +80,11 @@ for jj = 1:length(S.userinput.phases)
         end
     end
 end
-
-% Add effect SLR to PRN info
-if S.userinput.indicators.slr == 1
-    ITHK_add_SLR(sens);
-end
-
-% Add PRN info
-if S.userinput.indicators.coast == 1
-    ITHK_PRN_to_kml(sens);
-end
  
-% Add indicators
+%% Add indicators
 if S.userinput.indicators.dunes == 1
-    ITHK_dunerules(sens);                 %dunes
+    ITHK_dunerules2(sens);                 %dunes
+    ITHK_dunes_habitatrichness;
 end
 % if str2double(S.settings.dunes.on)==1
 %     settings.CSTorient = 'BASIS_ORIG.MDA'; 
@@ -61,10 +93,16 @@ end
 % end
 %S=ITHK_calculate_costs(S);    %budget
 if S.userinput.indicators.eco == 1
-   ITHK_ecorules2;
+   eval(S.settings.indicators.eco.function);%ITHK_ecorules2;
 end
 
-% KML footer
+%% Add disclaimer
+if isfield(S.settings.postprocessing,'disclaimer') 
+    disclaimer = ITHK_kmldisclaimer;
+    S.PP.output.kml = [S.PP.output.kml disclaimer];
+end
+
+%% KML footer
 S.PP.output.kml = [S.PP.output.kml KML_footer];
 fprintf(fid,S.PP.output.kml);
 fclose(fid);
