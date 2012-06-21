89066e06f0a86106ac16bfb88914ceb2cd88f52a
swinkel <swinkel@localhost>
2012-06-21 14:27:41 +0000

    svn path=/trunk/matlab/; revision=6530
    Former-commit-id: 9d374c004273956de77ce73a855aa4c31593fb17

---
+++ applications/cosmos/code/OMSRunner/postproc/scp/cosmos_postFiguresForecast.m
@@ -0,0 +1,31 @@
+function cosmos_postFigures(hm,m)
+% Post model output (kmz, png) to web server
+
+model=hm.models(m);
+
+locdir=[hm.webDir 'zandmotor' filesep 'forecast' filesep 'results' filesep hm.models(m).forecastplot.name];
+
+% Uploading figures
+
+dr='zandmotor';
+
+fid=fopen('scp.txt','wt');
+fprintf(fid,'%s\n','option batch on');
+fprintf(fid,'%s\n','option confirm off');
+fprintf(fid,'%s\n','option echo on');
+fprintf(fid,'%s\n','open cosmos:c0sm0sw3bs1t3@cosmos.deltares.nl -timeout=15 -hostkey="ssh-rsa 1024 cc:17:70:a2:d1:1e:ed:86:09:23:ea:2e:1c:3e:66:5e"');
+fprintf(fid,'%s\n',['cd ' dr]);
+fprintf(fid,'%s\n',['mkdir forecast']);
+fprintf(fid,'%s\n',['cd forecast']);
+fprintf(fid,'%s\n',['mkdir results']);
+fprintf(fid,'%s\n',['cd results']);
+fprintf(fid,'%s\n',['mkdir ' hm.models(m).forecastplot.name]);
+fprintf(fid,'%s\n',['cd ' hm.models(m).forecastplot.name]);
+fprintf(fid,'%s\n','rm *.png');
+fprintf(fid,'%s\n','rm *.xml');
+fprintf(fid,'%s\n',['put ' locdir filesep '*.*']);
+fprintf(fid,'%s\n','close');
+fprintf(fid,'%s\n','exit');
+fclose(fid);
+system([hm.exeDir 'winscp.exe /console /script=scp.txt']);
+delete('scp.txt');
