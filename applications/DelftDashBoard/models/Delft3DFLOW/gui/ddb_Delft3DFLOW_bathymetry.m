f63a35206a860b27adfb9ab110876e4945f01d1f
elias <elias@localhost>
2012-06-22 13:31:03 +0000

    getDepthZ iso GetDepthZ
    svn path=/trunk/matlab/; revision=6577
    
    
    Former-commit-id: 22490e11a172266478bfe177332f8ff838ce00c1

--- applications/DelftDashBoard/models/Delft3DFLOW/gui/ddb_Delft3DFLOW_bathymetry.m
+++ applications/DelftDashBoard/models/Delft3DFLOW/gui/ddb_Delft3DFLOW_bathymetry.m
@@ -79,7 +79,7 @@ filename=handles.Model(md).Input(ad).depFile;
 dp=ddb_wldep('read',filename,[handles.Model(md).Input(ad).MMax,handles.Model(md).Input(ad).NMax]);
 handles.Model(md).Input(ad).depth=-dp(1:end-1,1:end-1);
 handles.Model(md).Input(ad).depth(handles.Model(md).Input(ad).depth==999.999)=NaN;
-handles.Model(md).Input(ad).depthZ=GetDepthZ(handles.Model(md).Input(ad).depth,handles.Model(md).Input(ad).dpsOpt);
+handles.Model(md).Input(ad).depthZ=getDepthZ(handles.Model(md).Input(ad).depth,handles.Model(md).Input(ad).dpsOpt);
 handles=ddb_Delft3DFLOW_plotBathy(handles,'plot','domain',ad);
 setHandles(handles);
 %ddb_plotFlowBathymetry(handles,'plot',ad);
