cc1c2a31330e56eed00081b93c27e2cec03768d2
ormondt <ormondt@localhost>
2012-06-28 16:20:11 +0000

    delft3dwave gui update
    svn path=/trunk/matlab/; revision=6698
    
    
    Former-commit-id: 501ef9db06d2f5e0e7a3f3b19fb5512538445804

--- applications/DelftDashBoard/models/Delft3DWAVE/gui/ddb_Delft3DWAVE_boundaries.m
+++ applications/DelftDashBoard/models/Delft3DWAVE/gui/ddb_Delft3DWAVE_boundaries.m
@@ -10,6 +10,8 @@ else
             addBoundary;
         case{'editboundaryconditions'}
             editBoundaryConditions;
+        case{'drawxyboundary'}
+            drawXYBoundary;
     end
 end
 
@@ -64,4 +66,40 @@ if ok
     handles=h;
     setHandles(handles);
 end
+
+%%
+function drawXYBoundary
+ddb_zoomOff;
+handles=getHandles;
+xg=handles.Model(md).Input.domains(1).gridx;
+yg=handles.Model(md).Input.domains(1).gridy;
+gui_dragLine('callback',@addXYBoundary,'method','alonggridline','gridx',xg,'gridy',yg);
+
+%%
+function addXYBoundary(x,y,m,n)
+handles=getHandles;
+h=gui_polyline('plot','x',x,'y',y,'tag','delft3dwaveboundary','Marker','o','changecallback',@changeXYBoundary,'closed',0, ...
+    'color','r','markeredgecolor','r','markerfacecolor','r');
+iac=handles.Model(md).Input.activeboundary;
+if isfield(handles.Model(md).Input.boundaries(iac),'plothandle')
+    if ~isempty(handles.Model(md).Input.boundaries(iac).plothandle)
+        try
+            delete(handles.Model(md).Input.boundaries(iac).plothandle);
+        end
+    end
+end
+
+handles.Model(md).Input.boundaries(iac).plothandle=h;
+handles.Model(md).Input.boundaries(iac).startcoordx=x(1);
+handles.Model(md).Input.boundaries(iac).endcoordx=x(2);
+handles.Model(md).Input.boundaries(iac).startcoordy=y(1);
+handles.Model(md).Input.boundaries(iac).endcoordy=y(2);
+setHandles(handles);
+
+gui_updateActiveTab;
+
+%%
+function changeXYBoundary(h,x,y,nr)
+x
+y
 
