76959c2c5b74d0477b6567f4a03811e8cff5762f
stengs <stengs@localhost>
2012-07-04 09:29:17 +0000

    clear bug from copying zandmotor.kmz to wrong file
    svn path=/trunk/matlab/; revision=6772
    
    
    Former-commit-id: dab99f2a284d4b4ea968f9aa71dce057defc9006

--- applications/cosmos/code/OMSRunner/postproc/website/cosmos_copyFiguresToLocalWebsite.m
+++ applications/cosmos/code/OMSRunner/postproc/website/cosmos_copyFiguresToLocalWebsite.m
@@ -19,13 +19,13 @@ end
 
 if model.forecastplot.plot
     try
-        dir1=[dr 'lastrun' filesep 'figures' filesep 'forecast' filesep '*.*'];
+        dir3=[dr 'lastrun' filesep 'figures' filesep 'forecast' filesep '*.*'];
         MakeDir([hm.webDir 'zandmotor' filesep 'forecast' filesep],'results');
         MakeDir([hm.webDir 'zandmotor' filesep 'forecast' filesep],'results',hm.models(m).forecastplot.name);
-        dir2=[hm.webDir 'zandmotor' filesep 'forecast' filesep 'results' filesep hm.models(m).forecastplot.name];
-        delete([dir2 filesep '*.xml']);
-        delete([dir2 filesep '*.png']);
-        [status,message,messageid]=copyfile(dir1,dir2,'f');
+        dir4=[hm.webDir 'zandmotor' filesep 'forecast' filesep 'results' filesep hm.models(m).forecastplot.name];
+        delete([dir4 filesep '*.xml']);
+        delete([dir4 filesep '*.png']);
+        [status,message,messageid]=copyfile(dir3,dir4,'f');
     catch
         disp(['Something went wrong with copying forecast plots to local website - ' hm.models(m).name]);
     end
