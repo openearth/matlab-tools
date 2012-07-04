060fd6c57912e66fad11a9cf4e11e2134ee1e15d
boer_we <boer_we@localhost>
2012-07-04 11:31:14 +0000

    Enabled multiselection in grid tab
    svn path=/trunk/matlab/; revision=6775
    
    
    Former-commit-id: 9eb85397d8d7702ff9502f4e79b6597e04105daf

--- applications/DelftDashBoard/models/Delft3DWAVE/gui/ddb_Delft3DWAVE_output.m
+++ applications/DelftDashBoard/models/Delft3DWAVE/gui/ddb_Delft3DWAVE_output.m
@@ -3,4 +3,30 @@ function ddb_Delft3DWAVE_output(varargin)
 if isempty(varargin)
     ddb_zoomOff;
     ddb_refreshScreen;
+else
+    opt=varargin{1};
+    switch lower(opt)
+        case{'selectgrid'}
+            selectGrid;
+        case{'checkoutput'}
+            checkOutputGrid;
+    end
 end
+
+%%
+function selectGrid
+handles = getHandles;
+handles = ddb_Delft3DWAVE_setNestGrids(handles);
+setHandles(handles);
+ddb_plotDelft3DWAVE('update','wavedomain',0,'active',1);
+
+%%
+function checkOutputGrid
+handles=getHandles;
+val=handles.Model(md).Input.domains(awg).output;
+iac=handles.Model(md).Input.activegrids;
+for ii=1:length(iac)
+    n=iac(ii);
+    handles.Model(md).Input.domains(n).output=val;
+end
+setHandles(handles);
