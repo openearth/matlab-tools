821fecd4dd2cf52b80c002f7cc1b184963478642
boer_we <boer_we@localhost>
2012-07-04 14:18:21 +0000

    Updates hydrodynamics tab WAV GUI
    svn path=/trunk/matlab/; revision=6783
    
    
    Former-commit-id: 9fd9a0dac53227f994b0c3d80f7ab6a0dc3fa31a

--- applications/DelftDashBoard/models/Delft3DWAVE/gui/ddb_Delft3DWAVE_hydrodynamics.m
+++ applications/DelftDashBoard/models/Delft3DWAVE/gui/ddb_Delft3DWAVE_hydrodynamics.m
@@ -6,6 +6,8 @@ if isempty(varargin)
 else
     opt=varargin{1};
     switch lower(opt)
+        case{'sethydrodynamics'}
+            setHydrodynamics;
         case{'checkwaterlevel'}
             checkWaterLevel;
         case{'checkbedlevel'}
@@ -16,6 +18,55 @@ else
             checkWind;
     end
 end
+
+%%
+function setHydrodynamics
+
+handles=getHandles;
+
+ButtonName = questdlg('Couple with Delft3D-FLOW model?', ...
+    'Couple with flow', ...
+    'Cancel', 'No', 'Yes', 'Yes');
+switch ButtonName,
+    case 'Cancel',
+        return;
+    case 'No',
+        couplewithflow=0;
+    case 'Yes',
+        couplewithflow=1;
+end
+
+if couplewithflow == 1
+   if handles.Model(1).Input(1).comInterval==0 || handles.Model(1).Input(1).comStartTime==handles.Model(1).Input(1).comStopTime
+      ddb_giveWarning('text','Please make sure to set the communication file times in Delft3D-FLOW model!'); 
+   end
+   handles.Model(md).Input.coupledwithflow=1;
+   handles.Model(1).Input(1).waves=1;
+   handles.Model(1).Input(1).onlineWave=1;
+end
+
+% TODO : change coordsyst
+if strcmp(handles.Model(md).Input.flowtype,'ddb')
+    handles.Model(md).Input.referencedate=handles.Model(1).Input(1).itDate;
+    handles.Model(md).Input.mapwriteinterval=handles.Model(1).Input(1).mapInterval;
+    handles.Model(md).Input.comwriteinterval=handles.Model(1).Input(1).comInterval;
+    handles.Model(md).Input.writecom=1;
+    handles.Model(md).Input.mdffile=handles.Model(1).Input(1).mdfFile;
+else
+    MDF=ddb_readMDFText(handles.Model(md).Input.mdffile);
+    handles.Model(md).Input.referencedate=datenum(MDF.itdate,'yyyy-mm-dd');
+    handles.Model(md).Input.mapwriteinterval=MDF.flmap(2);
+    handles.Model(md).Input.comwriteinterval=MDF.flpp(2);
+    handles.Model(md).Input.writecom=1;
+end
+
+%startTime=handles.Model(md).Input.referencedate+MDF.tstart/1440.0;
+%stopTime=handles.Model(md).Input.referencedate+MDF.tstop/1440.0;
+%timeStep=MDF.dt;
+%timeZone=MDF.tzone;
+
+setHandles(handles);
+
 
 %%
 function checkWaterLevel
