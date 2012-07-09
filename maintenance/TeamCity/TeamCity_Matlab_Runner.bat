37442fb1300979f6d28d3feb9de166c91b50e887
baart_f <baart_f@localhost>
2012-07-09 14:56:37 +0000

    where did matlab go
    svn path=/trunk/matlab/; revision=6822
    
    
    Former-commit-id: 4deb980f16397936f8188977750527e51687330c

--- maintenance/TeamCity/TeamCity_Matlab_Runner.bat
+++ maintenance/TeamCity/TeamCity_Matlab_Runner.bat
@@ -18,6 +18,7 @@ echo Starting matlab in path: %matlab_path%
 echo With command: %matlab_command%
 echo In working dir %teamcity.build.workingDir%
 echo Listing directory %matlab_path%
+echo Listing directory Y:\app\ma*
 dir %matlab_path%
 
 %matlab_path%matlab -nosplash -nodesktop -minimize -r "%matlab_command%" -logfile mlogfile.log -sd "%teamcity.build.workingDir%"
