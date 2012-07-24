294cc9f737eb0912a309956c9ec319cde1e64565
baart_f <baart_f@localhost>
2012-07-24 11:06:15 +0000

    waterbase automation
    svn path=/trunk/matlab/; revision=7004
    
    
    Former-commit-id: c60a9c88b7068243c976e4cbb419575d1a21c25b

--- maintenance/TeamCity/TeamCity_waterbase.m
+++ maintenance/TeamCity/TeamCity_waterbase.m
@@ -1,4 +1,4 @@
-function TeamCity_runtests(varargin)
+function TeamCity_waterbase(varargin)
 %TEAMCITYRUNOETTESTS  Function that runs all tests available in the OpenEarthTools repository.
 %
 %   This function gathers and runs all tests in the OpenEarthTools repository.
@@ -56,13 +56,6 @@ try %#ok<TRYNC>
 
     %% First load oetsettings
     try
-        if exist('OetTestResult.zip','file')
-            delete('OetTestResult.zip');
-        end
-        if exist('OetTestResult.mat','file')
-            delete('OetTestResult.mat');
-        end
-
         TeamCity_initialize;
 
     catch me
@@ -77,146 +70,26 @@ try %#ok<TRYNC>
          exit;
     end
 
-    OPT = struct(...
-        'TestsMainDir',[],...
-        'Category','all',...
-        'PublishCoverage',false,...
-        'RunDir',cd,...
-        'RevisionNumber',NaN);
-
-    if nargin > 0
-        try
-            OPT = setproperty(OPT,varargin);
-            if ~isempty(OPT.TestsMainDir)
-                TeamCity.postmessage('message', 'text', ['Add tests directory:',char(10),OPT.TestsMainDir]);
-                tempcd = cd;
-                cd(OPT.TestsMainDir);
-                oettestsettings;
-                cd(tempcd);
-                TeamCity.postmessage('message', 'text', 'Finished adding tests');
-            end
-        catch me
-            TeamCity.postmessage('message', 'text', 'Matlab was unable to set options or add the main dir of the test data.',...
-                'errorDetails',me.getReport,...
-                'status','ERROR');
-            TeamCity.postmessage('buildStatus',...
-                'status','FAILURE',...
-                'text', 'FAILURE: Matlab was unable to set options or add the main dir of the test data.');
-        end
-    end
     try
-        TeamCity.postmessage('progressStart','Prepare MTestRunner');
-        %% initiate variables:
-        maindir = oettestroot;
-        targetdir = fullfile(oetroot,'teamcitytesthtml');
-        if isdir(targetdir)
-            rmdir(targetdir,'s');
+        OPT = setproperty(OPT,varargin);
+        if ~isempty(OPT.TestsMainDir)
+            TeamCity.postmessage('message', 'text', ['Run ' ...
+                                'waterbase']);
+            rws_waterbase_all('download', 1, 'make_nc', 1, ...
+                              'make_catalog', 1, 'make_kml', 1)
+            TeamCity.postmessage('message', 'text', ['Finished ' ...
+                                'running waterbase '])
         end
-
-        %% Create testengine
-        mtr = MTestRunner(...
-            'MainDir'       ,maindir,...
-            'Recursive'     ,true,...
-            'Verbose'       ,true,...
-            'IncludeCoverage',OPT.PublishCoverage);
-
-        TeamCity.postmessage('progressFinish','Prepare MTestRunner');
-
-        %% Collect tests that need to be run
-        TeamCity.postmessage('progressStart','Collect Tests');
-        mtr.gathertests;
-        collectedTestCategories = [mtr.Tests.Category]';
-
-        % Check which tests we have to run
-        if strcmp(OPT.Category,'all')
-            id = true(size(collectedTestCategories)) & ...
-            collectedTestCategories ~= MTestCategory.UserInput &...
-            collectedTestCategories ~= MTestCategory.WorkInProgress;
-        else
-            id = collectedTestCategories  == str2category(OPT.Category);
-        end
-
-        mtr.Tests(~id)=[];
-        if isempty(mtr.Tests)
-            % exit because we do not have any test in this category
-            TeamCity.postmessage('progressMessage', 'No tests were found under this category.');
-            TeamCity.postmessage('progressFinish','Collect Tests');
-            exit
-        end
-
-        TeamCity.postmessage('progressMessage', ['Identified ' num2str(length(mtr.Tests)) ' tests within the specified category ("' OPT.Category '")']);
-        TeamCity.postmessage('progressFinish','Collect Tests');
-
-        %% Run tests
-        TeamCity.postmessage('progressStart','Run Tests');
-        mtr.run;
-        TeamCity.postmessage('progressFinish','Run Tests');
-
-        %% Remove template files
-        if exist(fullfile(targetdir,'mxdom2defaulthtml.xsl'),'file')
-            delete(fullfile(targetdir,'mxdom2defaulthtml.xsl'));
-        end
-
-        if any(~[mtr.Tests.Ignore]) && OPT.PublishCoverage
-            TeamCity.postmessage('progressStart','Publish coverage');
-
-            TeamCity.postmessage('progressMessage', 'Remove coverage target dir.');
-            targetDir = fullfile(OPT.RunDir,'OetTestCoverage');
-            if isdir(targetDir)
-                rmdir(targetDir,'s');
-            end
-
-            TeamCity.postmessage('progressMessage', 'Calculate and publish coverage.');
-            mtp = MTestPublisher(...
-                'Publish',true,...
-                'Verbose',true,...
-                'TargetDir',targetDir,...
-                'OutputDir',targetDir);
-            mtp.publishcoverage(mtr.ProfileInfo);
-
-            TeamCity.postmessage('progressFinish','Publish coverage');
-        end
-
-        %% save test info
-        TeamCity.postmessage('progressMessage', 'Save result.');
-        OetTestResult = struct(...
-            'Revisionnumber',OPT.RevisionNumber,...
-            'Date',datestr(now),...
-            'TestRunner',mtr,...
-            'TeamCity',TeamCity,...
-            'OPT',OPT);
-        save('OetTestResult.mat','OetTestResult');
-
     catch me
-        try %#ok<TRYNC>
-            TeamCity.postmessage('message',...
-                'text','error detailes:',...
+        TeamCity.postmessage('message', 'text', 'Matlab was unable to run waterbase.',...
                 'errorDetails',me.getReport,...
-                'status','ERROR');
-            TeamCity.postmessage('buildStatus',...
-                'status','FAILURE',...
-                'text', 'FAILURE: Something went wrong while running the tests.');
-        end
-%            rethrow(me);
-         exit;
+                             'status','ERROR');
+        TeamCity.postmessage('buildStatus',...
+                             'status','FAILURE',...
+                             'text', ['FAILURE: Matlab was unable ' ...
+                            'to create the waterbase dataset.']);
     end
-end
-exit;
+    exit;
 end
 
-function cat = str2category(str)
-switch str
-    case 'Integration'
-        cat = MTestCategory.Integration;
-    case 'DataAccess'
-        cat = MTestCategory.DataAccess;
-    case 'Performance'
-        cat = MTestCategory.Performance;
-    case 'Unit'
-        cat = MTestCategory.Unit;
-    case 'UserInput'
-        cat = MTestCategory.UserInput;
-    case 'WorkInProgress'
-        cat = MTestCategory.WorkInProgress;
-end
 end
