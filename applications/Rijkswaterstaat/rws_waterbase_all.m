6b11fbd62523cfdd7af1f2a440f8faa445ba6b1d
baart_f <baart_f@localhost>
2012-07-24 13:30:14 +0000

    default download everything
    svn path=/trunk/matlab/; revision=7007
    
    
    Former-commit-id: 3b098d774105e8f3539319f5ebac5bc4f3ef6550

--- applications/Rijkswaterstaat/rws_waterbase_all.m
+++ applications/Rijkswaterstaat/rws_waterbase_all.m
@@ -11,7 +11,7 @@ function rws_waterbase_all
 
 %% Initialize
 
-   OPT.download       = 0; % get fresh downloads from rws and move exisitng to sub dir old
+   OPT.download       = 1; % get fresh downloads from rws and move exisitng to sub dir old
    OPT.make_nc        = 1; % makes also temporary mat files, moves exisiting nc to old subdir
    OPT.make_catalog   = 1; % otherwise load existing one
    OPT.make_kml       = 1; % processing all kml only takas about 4 hours
