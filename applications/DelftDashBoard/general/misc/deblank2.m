d7488b273015263f01c0be48b17e35551c589a5a
ormondt <ormondt@localhost>
2012-09-05 09:49:34 +0000

    deblank2 update
    svn path=/trunk/matlab/; revision=7187
    
    
    Former-commit-id: 903471598758e57749ff0e3462eeb089ea09abd2

--- applications/DelftDashBoard/general/misc/deblank2.m
+++ applications/DelftDashBoard/general/misc/deblank2.m
@@ -61,16 +61,18 @@ function str = deblank2(str)
 
 %% Removes starting and trailing spaces from string
 
-spaces=str==' ';
-ifirstchar=[];
-for i=1:length(spaces)
-    if ~spaces(i)
-        ifirstchar=i;
-        break
+if ischar(str)
+    spaces=str==' ';
+    ifirstchar=[];
+    for i=1:length(spaces)
+        if ~spaces(i)
+            ifirstchar=i;
+            break
+        end
     end
+    if ~isempty(ifirstchar)
+        str=str(ifirstchar:end);
+    end
+    str=deblank(str);
 end
-if ~isempty(ifirstchar)
-    str=str(ifirstchar:end);
-end
-str=deblank(str);
 
