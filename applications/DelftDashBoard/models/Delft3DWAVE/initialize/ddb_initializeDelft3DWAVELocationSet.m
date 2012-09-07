26ce1d18dd66c149133f43b57016ac8ce22c9a8c
ormondt <ormondt@localhost>
2012-09-07 11:00:24 +0000

    xml files update
    svn path=/trunk/matlab/; revision=7211
    
    
    Former-commit-id: 39c6f43c3b827454bdcd48e15fdd561425736377

--- applications/DelftDashBoard/models/Delft3DWAVE/initialize/ddb_initializeDelft3DWAVELocationSet.m
+++ applications/DelftDashBoard/models/Delft3DWAVE/initialize/ddb_initializeDelft3DWAVELocationSet.m
@@ -1,7 +1,7 @@
 function locationsets=ddb_initializeDelft3DWAVELocationSet(locationsets,iac)
 
-locationsets(iac).x=[];
-locationsets(iac).y=[];
+locationsets(iac).x=0;
+locationsets(iac).y=0;
 locationsets(iac).handle=[];
 locationsets(iac).activepoint=1;
 locationsets(iac).nrpoints=0;
