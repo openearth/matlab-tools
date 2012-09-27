01337c7f8af435f00590529072b172f68db07bbd
boer_we <boer_we@localhost>
2012-09-27 07:30:50 +0000

    Added functions for communication with server based on xml
    svn path=/trunk/matlab/; revision=7321
    
    
    Former-commit-id: 15d068dd9a2ddf9ced98acee403f99e1a71fd3c9

---
+++ applications/ITHK/Matlab/ITHK_xml.m
@@ -0,0 +1,227 @@
+function ITHK_xml(h,xml)
+
+% function outputKML =  UnibestInteractiveTool(lat,lon,mag,time,name,measure,implementation)
+%UNIBESTINTERACTIVETOOL  Backbone function of Unibest Interactive Tool for the Holland Coast
+%
+%   The Unibest Interactive Tool for the Holland Coast is developed in the Building with Nature program
+%   by the working packages HK4.1 and DM1.3. This function is the backbone of the tool in which simulates 
+%   the effects of a number of (pre-defined) coastal measures on the coastline development of the Holland
+%   Coast by means of a UNIBEST model. The Unibest Interactive Tool can only run, when the user has a
+%   license of the UNIBEST software. The tool can be called either direclty from Matlab or from a web
+%   application.
+%
+%   Syntax:
+%   outputKML = UnibestInteractiveTool(lat,lon,mag,time,name,measure,implementation)
+%
+%   Input:
+%   measure       = code indicating the type of coastal intervention:
+%                   [0] Continuous triangular nourishment (from year of implementation to end)
+%                   [1] Single triangular nourishment (only in year of implementation) 
+%                   [2] Groyne
+%                   [3] Revetment
+%				    [4] Evenly distributed nourishment (from year of implementation to end)
+%   lat           = 1xN array with the latitudes of coastal interventions
+%   lon           = 1xN array with the longitudes of coastal interventions
+%   impl          = 1xN array with the years of implementation of coastal intervention (measured from base year)
+%   len           = 1xN array with the (spreading) length of coastal interventions (0.5*len = radius around lat,lon)
+%   vol           = 1xN array with the nourishment volumes
+%   fill          = 1xN array with the fillup behind revetments (0=no fillup, 1=fillup)
+%   tin           = string specifying the timespan for the Unibest calculations in years
+%   varNameIn     = string specifying the name of the scenario
+%   coast         = string specifying whether coastline output will be generated
+%   eco           = string specifying whether ecology output will be generated
+%   dunes         = string specifying whether dunes output will be generated
+%   slr           = string specifying whether sea level rise will be taken into account
+%
+%   Output:
+%   outputKML = contents of the KML-file (filename = varNameIn.kml) containing the model results of the Unibest calculations
+%
+%   Example
+%   measure = [1, 2, 2, 3, 4, 3];
+%   lat = [52.0295, 52.0716, 52.1167, 52.2049, 52.1078, 52.2444];
+%   lon = [4.1588, 4.2209, 4.2824, 4.3912, 4.2693, 4.4269];
+%   impl = [10, 15, 5, 1, 5, 15];
+%   len = [2500, 500, 500, 2500, 1000, 2500];
+%   vol = [10000000, 0, 0, 0, 1000000, 0];
+%   fill = [0, 0, 0, 0, 0, 0];
+%   tin = '20';
+%   varNameIn = 'example';
+%   coast = '1';
+%   eco = '0';
+%   dunes = '0';
+%   slr = '1';
+%   outputKML = UnibestInteractiveTool(measure,lat,lon,impl,len,vol,fill,tin,varNameIn,coast,eco,dunes,slr)
+
+%   See also
+
+%% Copyright notice
+%   --------------------------------------------------------------------
+%   Copyright (C) 2011 Deltares
+%       Wiebe de Boer
+%
+%       wiebe.deboer@deltares.nl
+%
+%       Deltares
+%       Rotterdamseweg 185
+%       PO Box Postbus 177
+%       2600MH Delft
+%       The Netherlands
+%
+%   This library is free software: you can redistribute it and/or modify
+%   it under the terms of the GNU General Public License as published by
+%   the Free Software Foundation, either version 3 of the License, or
+%   (at your option) any later version.
+%
+%   This library is distributed in the hope that it will be useful,
+%   but WITHOUT ANY WARRANTY; without even the implied warranty of
+%   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%   GNU General Public License for more details.
+%
+%   You should have received a copy of the GNU General Public License
+%   along with this library.  If not, see <http://www.gnu.org/licenses/>.
+%   --------------------------------------------------------------------
+
+% This tool is part of <a href="http://www.OpenEarth.eu">OpenEarthTools</a>.
+% OpenEarthTools is an online collaboration to share and manage data and
+% programming tools in an open source, version controlled environment.
+% Sign up to recieve regular updates of this function, and to contribute
+% your own tools.
+
+%% Version <http://svnbook.red-bean.com/en/1.5/svn.advanced.props.special.keywords.html>
+% Created: 22 Sep 2011
+% Created with Matlab version: 7.11.0.584 (R2010b)
+
+% $Id$
+% $Date$
+% $Author$
+% $Revision$
+% $HeadURL$
+% $Keywords: $
+
+%% Unibest Interactive Tool
+% find Tooldir
+[toolpath] = fileparts(which('ITHK_xml'));
+cd(toolpath);
+ss = strfind(toolpath,'\');
+baseDir = toolpath(1:ss(end));
+web = 1; % for now
+
+% create global struct
+global S
+S = struct;
+S.EPSG = load(which('EPSG.mat'));
+disp(['Running web-based test function - ' datestr(now)]);
+
+S.h = h;
+S.statusFilename=['status_' xml.uniqueID '.xml'];
+
+%Go up one dir
+cd(S.h,'..');
+
+%Go into status dir
+cd(S.h,'status');
+
+%Write status xml file
+status='Matlab called by Interactive Tool';
+ITHK_update_status(status);
+%disp('Matlab called by Interactive Tool')
+
+%% Process input
+%S.userinput = process_webinput(lat,lon,mag,time,name,measure,implementation);
+%xml = xml_read('xml_Xuan.xml');
+S.userinput = ITHK_process_webinput_xml(xml,1);
+
+%Read settings
+if web ==1
+    S.settings = xml_load(which('ITHK_settings_web.xml'));
+else
+    S.settings = xml_load(which('ITHK_settings.xml'));%d:\2011\InteractiveTool_Kustatelier\Matlab\settings2.xml
+end
+S.settings.basedir = baseDir;
+
+% subdirectories
+S.settings.inputdir            = [baseDir 'Matlab\preprocessing\input\'];
+S.settings.rundir              = [baseDir 'UB model\input\'];
+S.settings.outputdir           = [baseDir 'UB model\output\' S.userinput.name filesep]; 
+if ~isdir(S.settings.outputdir)
+    mkdir(S.settings.outputdir);
+elseif ~isempty(dir(S.settings.outputdir))
+    flist = dir(S.settings.outputdir);
+    for ii=1:length(flist)
+        switch lower(flist(ii).name)
+            case{'.','..','.svn'}
+            otherwise
+                delete([S.settings.outputdir flist(ii).name]);
+        end
+    end
+end
+copyfile([S.settings.rundir],S.settings.outputdir);
+%S.settings.outputdir           = [baseDir 'UB model\'];
+
+%% Preprocessing Unibest Interactive Tool
+for ii=1:1%length(sensitivities)
+    ITHK_preprocessing(ii);
+    disp('preprocessing Unibest Interactive Tool completed')
+
+    %% Running Unibest Interactive Tool
+    ITHK_runUB;
+    disp('running Unibest completed');
+    
+%     %% Create output dir
+%     if ~isdir([S.settings.outputdir 'output' filesep S.userinput.name])
+%        mkdir([S.settings.outputdir 'output' filesep S.userinput.name]);
+%     end 
+
+    %% Extract UB (PRN) results for current & reference scenario
+    PRNfileName = [S.userinput.name,'.PRN']; 
+    S.UB(ii).results.PRNdata = ITHK_io_readPRN([S.settings.outputdir PRNfileName]);
+    S.UB(ii).data_ref.PRNdata = ITHK_io_readPRN([S.settings.outputdir 'Natural_development.PRN']);%REFERENCE_IT.PRN
+
+    %% Postprocessing Unibest Interactive Tool
+    ITHK_postprocessing(ii);
+%     ITHK_cleanup(ii);
+end
+save([S.settings.outputdir filesep S.userinput.name,'.mat'],'-struct','S')
+outputKML=fileread(S.PP.output.kmlFileName);
+disp('postprocessing Unibest Interactive Tool completed');
+
+%% Output result to server
+%Make output xml
+outputFilename=['output_' xml.uniqueID '.xml'];
+
+root.item.kmlTitle='Test result';
+root.item.kmlFile=[S.userinput.name '.kml'];
+
+xml_write(outputFilename,root);
+
+%First copy KML
+%Go up one dir
+cd(S.h,'..');
+%Go into kml dir
+cd(S.h,'kml');
+
+mput(S.h,S.PP.output.kmlFileName);
+delete(root.item.kmlFile);
+
+%Then write output xml to server
+%Go up one dir
+cd(h,'..');
+%Go into output dir
+cd(h,'output');
+
+mput(h,outputFilename);
+%Check if it is really there
+d=dir(h,outputFilename);
+if isempty(d)
+    warning(['OUTPUT XML FILE: ' outputFilename ' could not be copied to FTP!']);
+else
+    delete(outputFilename);
+end
+
+%Then go back to the input dir for listener
+%Go up one dir
+cd(h,'..');
+%Go into output dir
+cd(h,'input');
+
+%}
---
+++ applications/ITHK/Matlab/userinput/ITHK_process_webinput_xml.m
@@ -0,0 +1,200 @@
+function S = ITHK_process_webinput_xml(xml,web)
+
+%% Process input from Viewer
+if isfield(xml.data,'features')
+    features = xml.data.features;
+    for ii = 1:size(xml.data.features,1)
+        if isfield(features(ii),'properties');
+            measure{ii} = features(ii).properties.name;
+            if isfield(features(ii).properties,'type')
+               type(ii) = features(ii).properties.type;
+            else
+               type(ii) = NaN;
+            end
+            lon(ii) = features(ii).properties.lon;
+            lat(ii) = features(ii).properties.lat;
+            S.implementation(ii) = features(ii).properties.year;
+            len(ii) = features(ii).properties.length;
+            volume(ii) = features(ii).properties.volume*1e6;
+            fillup(ii) = features(ii).properties.fillup;
+        end
+    end
+end
+
+%% Scenario information
+S.name = [xml.data.scenarioname '_' xml.uniqueID];
+S.duration = min(95,xml.data.duration);
+S.Nmeasures = size(measure,2);
+
+%% Measures information
+id_supp = find(strcmp(measure,'Nourishment'));
+id_rev = find(strcmp(measure,'Revetment'));
+id_gro = find(strcmp(measure,'Groyne'));
+
+% id_lat = find(lat==0);
+% id_contsupp = find(measure==0);
+% id_singlesupp = find(measure==1);
+% id_gro = find(measure==2);
+% id_rev = find(measure==3);
+% id_distrsupp = find(measure==4);
+
+su=0;gr=0;rev=0;
+if ~isempty(measure)
+    for ii = 1:size(measure,2)
+        if ismember(ii,id_supp)
+            su = su+1;
+            S.nourishment(su).start = S.implementation(ii);
+            S.nourishment(su).volume = volume(ii);
+            S.nourishment(su).width = len(ii);
+            S.nourishment(su).volperm = volume(ii)/len(ii);
+            if type(ii) == 0;
+                S.nourishment(su).category = 'cont';
+                S.nourishment(su).stop = S.duration;
+            elseif type(ii) == 1;
+                S.nourishment(su).category = 'single';
+                S.nourishment(su).stop = S.implementation(ii)+1;
+            elseif type(ii) == 4;
+                S.nourishment(su).category = 'distr';
+                S.nourishment(su).stop = S.duration;
+            elseif type(ii) == 5;
+                S.nourishment(su).category = 'distrsupp_single';
+                S.nourishment(su).stop = S.implementation(ii)+1;
+            end
+            S.nourishment(su).lat = lat(ii);
+            S.nourishment(su).lon = lon(ii);
+        end
+        if ismember(ii,id_gro)
+            gr=gr+1;
+            S.groyne(gr).start = S.implementation(ii);
+            S.groyne(gr).stop = S.duration;
+            S.groyne(gr).length = len(ii);
+            S.groyne(gr).lat = lat(ii);
+            S.groyne(gr).lon = lon(ii);
+        end
+        if ismember(ii,id_rev)
+            rev=rev+1;
+            S.revetment(rev).start = S.implementation(ii);
+            S.revetment(rev).stop = S.duration;
+            S.revetment(rev).length = len(ii);
+            S.revetment(rev).fill = fillup(ii);
+            S.revetment(rev).lat = lat(ii);
+            S.revetment(rev).lon = lon(ii);
+        end
+    end
+end
+
+%% Number of phases
+% Number of phases is based on implementation of measures and type of
+% nourishments
+yrstop = [];
+if  isfield(S,'nourishment')
+    for jj = 1:size(S.nourishment,2)
+        if  strcmp(S.nourishment(jj).category,'single')==1 || strcmp(S.nourishment(jj).category,'distrsupp_single')==1
+            yrstop = [yrstop S.nourishment(jj).stop];
+        end
+    end
+end
+if ~isempty(S.implementation)
+    if size(S.implementation,1)>1
+         S.implementation =  S.implementation';
+    end
+    S.phases = unique([0 S.implementation yrstop]);
+else
+    S.phases = 0;
+end
+
+%% Add measures per phase
+% Get start times
+supstart = []; revstart = []; grostart = [];
+if  isfield(S,'nourishment')
+    for ii=1:size(S.nourishment,2)
+        supstart(ii) = S.nourishment(ii).start; 
+    end
+end
+if  isfield(S,'groyne')
+    for ii=1:size(S.groyne,2)
+        grostart(ii) = S.groyne(ii).start; 
+    end
+end
+if  isfield(S,'revetment')
+    for ii=1:size(S.revetment,2)
+        revstart(ii) = S.revetment(ii).start; 
+    end
+end
+idssup = [];idsgro = [];idsrev = [];
+for ii = 1:size(S.phases,2)
+    S.phase(ii).SOSfile = '';S.phase(ii).GROfile = '';S.phase(ii).REVfile = '';
+end
+
+% Add measures to phases based on start times
+for ii = 1:size(S.phases,2)
+    idssup = find(ismember(supstart,S.phases(ii)));
+    idsgro = find(ismember(grostart,S.phases(ii)));
+    idsrev = find(ismember(revstart,S.phases(ii)));
+    if ~isempty(idssup)
+        for jj=1:length(idssup)
+            if strcmp(S.nourishment(idssup(jj)).category,'single')                
+                S.phase(ii).supcat{jj} = 'single';
+            elseif strcmp(S.nourishment(idssup(jj)).category,'distrsupp_single')                
+                S.phase(ii).supcat{jj} = 'distrsupp_single';                
+            elseif strcmp(S.nourishment(idssup(jj)).category,'distr')
+                % If nourishment is continuous, nourishment should be taken
+                % into account in every phase after implementation
+                for kk=ii:length(S.phases)
+                    S.phase(kk).SOSfilecont = '1HOTSPOTSIT_cont.sos';
+                end
+                S.phase(ii).supcat{jj} = 'distr';
+            else
+                % If nourishment is continuous, nourishment should be taken
+                % into account in every phase after implementation
+                for kk=ii:length(S.phases)
+                    S.phase(kk).SOSfilecont = '1HOTSPOTSIT_cont.sos';
+                end
+                S.phase(ii).supcat{jj} = 'cont';
+            end 
+            S.phase(ii).SOSfile = ['1HOTSPOTS',num2str(ii),'IT.sos'];
+            S.phase(ii).supids = idssup;
+            %S.nourishment(idssup(jj)).filename = ['1HOTSPOTS',num2str(ii),'IT.sos'];
+        end
+    elseif isfield(S.phase(ii),'SOSfilecont')
+           S.phase(ii).SOSfile ='1HOTSPOTSIT_cont.sos';
+    else
+        S.phase(ii).SOSfile = 'BASIS.sos';    
+    end
+    if ~isempty(idsgro)
+        for kk=ii:size(S.phases,2)
+            S.phase(kk).GROfile = ['BRIJN90A' num2str(ii) '.GRO'];
+            S.phase(kk).groids = idsgro;
+        end
+        for jj=1:length(idsgro)
+            S.groyne(idsgro(jj)).filename = ['BRIJN90A' num2str(ii) '.GRO'];
+        end
+    elseif isempty(S.phase(ii).GROfile)
+        S.phase(ii).GROfile = ['BASIS.GRO'];
+    end
+    if ~isempty(idsrev)
+        for kk=ii:size(S.phases,2)
+            S.phase(kk).REVfile = ['HOLLANDCOAST' num2str(ii) '.REV'];
+            S.phase(kk).revids = idsrev;
+        end
+        for jj=1:length(idsrev)
+            S.revetment(idsrev(jj)).filename = ['HOLLANDCOAST' num2str(ii) '.REV'];
+        end
+    elseif isempty(S.phase(ii).REVfile)
+        S.phase(ii).REVfile = ['BASIS.REV'];
+    end
+end
+
+%% Process indicators
+if isfield(xml.data,'settings')
+    S.indicators.coast = xml.data.settings.Coastline;
+    S.indicators.dunes = xml.data.settings.Dunes;
+    S.indicators.eco = xml.data.settings.Ecology;
+    S.indicators.costs = xml.data.settings.Costs;
+    S.indicators.economy = xml.data.settings.Economy;
+    S.indicators.safety = xml.data.settings.Safety;
+    S.indicators.recreation = xml.data.settings.Recreation;
+    S.indicators.residency = xml.data.settings.Residency;
+    S.indicators.slr = xml.data.settings.Sea_level_rise; 
+end
+S.web = web;
