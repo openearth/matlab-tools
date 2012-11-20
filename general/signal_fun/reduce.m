e4705089792428dc2c3877aaa258fd41ff9d2957
tda.x <tda.x@localhost>
2012-11-20 09:11:24 +0000

    added function reduce, useful for subsampling timeseries (for data reduction)
    svn path=/trunk/matlab/; revision=7704
    
    
    Former-commit-id: 58be078283c6fa0ee8dc0706b3e54cd6859cbc85

---
+++ general/signal_fun/reduce.m
@@ -0,0 +1,139 @@
+function varargout = reduce(x,y,tolerance,maximum_nr_of_points)
+%REDUCE  Iteratively reduce density of points in vector data
+%
+% Reduce a set of x,y points by linearizing over a subset,
+% and iteratively adding the point with largest error to
+% the subset, as in the Ramer–Douglas–Peucker algorithm
+% but with only y distance as critrion
+% 
+% Routine continues until either tolerance criterion
+% or maximum_nr_of_points criterion is met
+% 
+% The calling syntax is either:
+%
+%		[indices,max_error]                     = reduce(x,y,tolerance,max nr of points)
+%       [reduced_x,reduced_y,indices,max_error] = reduce(x,y,tolerance,max nr of points)
+%
+%   Input:
+%       x           	  =  sorted vector
+%       y                 =  vector of size(x)
+%       tolerance         =  scalar
+%       max nr of points  =  scalar
+%
+%   Output:
+%       indices           = indices of the points in y and y needed to
+%                           fullfill the maximum_nr_of_points and/or tolerance criterion
+%
+%
+%
+%   Example
+%{
+
+% generate some data
+x = (1:5000)';
+y = 2*sind(x) + 3*sind(x/2) + 4*sind(x/3);
+
+subplot(3,2,1)
+max_nr_of_points = 10;
+[ind,max_error]= reduce(x,y,0,max_nr_of_points);
+plot(x,y,x(ind),y(ind),x(ind),[y(ind)+max_error y(ind)-max_error],'r:')
+title(sprintf('max points set at %d ==> max error %.1f',length(ind),max_error))
+
+subplot(3,2,3)
+max_nr_of_points = 15;
+[ind,max_error]= reduce(x,y,0,max_nr_of_points);
+plot(x,y,x(ind),y(ind),x(ind),[y(ind)+max_error y(ind)-max_error],'r:')
+title(sprintf('max points set at %d ==> max error %.1f',length(ind),max_error))
+
+subplot(3,2,5)
+max_nr_of_points = 250;
+[ind,max_error]= reduce(x,y,0,max_nr_of_points);
+plot(x,y,x(ind),y(ind),x(ind),[y(ind)+max_error y(ind)-max_error],'r:')
+title(sprintf('max points set at %d ==> max error %.1f',length(ind),max_error))
+
+subplot(3,2,2)
+tolerance = 5;
+[ind,max_error]= reduce(x,y,tolerance);
+plot(x,y,x(ind),y(ind),x(ind),[y(ind)+max_error y(ind)-max_error],'r:')
+title(sprintf('tolerance set at %.1f ==> %d points needed',tolerance,length(ind)))
+
+subplot(3,2,4)
+tolerance = 3;
+[ind,max_error]= reduce(x,y,tolerance);
+plot(x,y,x(ind),y(ind),x(ind),[y(ind)+max_error y(ind)-max_error],'r:')
+title(sprintf('tolerance set at %.1f ==> %d points needed',tolerance,length(ind)))
+
+subplot(3,2,6)
+tolerance = 0.1;
+[ind,max_error]= reduce(x,y,tolerance);
+plot(x,y,x(ind),y(ind),x(ind),[y(ind)+max_error y(ind)-max_error],'r:')
+title(sprintf('tolerance set at %.1f ==> %d points needed',tolerance,length(ind)))
+
+%}
+%
+%   See also: running_median_filter
+
+%% Copyright notice
+%   --------------------------------------------------------------------
+%   Copyright (C) 2012 Van Oord
+%       Thijs Damsma
+%
+%       tda@vanoord.com
+%
+%       Watermanweg 64
+%       3067 GG
+%       Rotterdam
+%       Netherlands
+%
+%   This library is free software: you can redistribute it and/or modify
+%   it under the terms of the GNU General Public License as published by
+%   the Free Software Foundation, either version 3 of the License, or
+%   (at your option) any later version.
+%
+%   This library is distributed in the hope that it will be useful,
+%   but WITHOUT ANY WARRANTY; without even the implied warranty of
+%   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%   GNU General Public License for more details.
+%
+%   You should have received a copy of the GNU General Public License
+%   along with this library.  If not, see <http://www.gnu.org/licenses/>.
+%   --------------------------------------------------------------------
+
+% This tool is part of <a href="http://www.OpenEarth.eu">OpenEarthTools</a>.
+% OpenEarthTools is an online collaboration to share and manage data and
+% programming tools in an open source, version controlled environment.
+% Sign up to recieve regular updates of this function, and to contribute
+% your own tools.
+
+%% Version <http://svnbook.red-bean.com/en/1.5/svn.advanced.props.special.keywords.html>
+% Created: 19 Nov 2012
+% Created with Matlab version: 8.0.0.783 (R2012b)
+
+% $Id$
+% $Date$
+% $Author$
+% $Revision$
+% $HeadURL$
+% $Keywords: $
+
+%% input check
+assert(isvector(x));
+assert(isequal(size(x),size(y)));
+assert(issorted(x));
+narginchk(3,4);
+assert(isscalar(tolerance));
+% also allow three inputs
+if nargin==3
+    maximum_nr_of_points = length(x);
+end
+assert(isscalar(maximum_nr_of_points));
+
+%% call mex function
+[indices,max_error,starts,ends,max_err_locs,max_err_vals] = reduce(double(x),double(y),double(tolerance),uint32(maximum_nr_of_points)); %#ok<ASGLU,NASGU>
+
+%% prepare output
+if nargout == 2
+    varargout = {indices,max_error};
+else
+   varargout = {x(indices),y(indices),indices,max_error}; 
+end
