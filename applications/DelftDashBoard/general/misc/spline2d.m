0cf40fd01b3845f6cde87e735b597a4dd285ec16
ormondt <ormondt@localhost>
2013-02-01 23:24:49 +0000

    spline2d added
    svn path=/trunk/matlab/; revision=8012
    
    
    Former-commit-id: 1a085b15f9464bf7d3adfc98e83f9b4c599a846a

---
+++ applications/DelftDashBoard/general/misc/spline2d.m
@@ -0,0 +1,20 @@
+function [xs2,ys2]=spline2d(xp,yp)
+% Generates spline in xy spaces with equidistant points
+
+xy=[xp';yp'];
+
+np=5;
+n=length(xp);
+
+t=1:n;
+ts=1:(1/np):n;
+xys=spline(t,xy,ts);
+
+xs=xys(1,:);
+ys=xys(2,:);
+
+pd0=pathdistance(xs,ys);
+pd=0:(pd0(end)/(np*n)):pd0(end);
+
+xs2=interp1(pd0,xs,pd);
+ys2=interp1(pd0,ys,pd);
