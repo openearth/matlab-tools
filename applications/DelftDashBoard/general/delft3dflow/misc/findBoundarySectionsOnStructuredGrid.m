bf56d085d88d6ec2780abb015707ea7b12faec16
ormondt <ormondt@localhost>
2013-02-06 15:45:33 +0000

    findBoundarySectionsOnStructuredGrid.m update (now also works for dpsopt=dp) makeModelSeries.m added
    svn path=/trunk/matlab/; revision=8040
    
    
    Former-commit-id: fe7dfe2039db4224525ea220fb6efeeb25043152

--- applications/DelftDashBoard/general/delft3dflow/misc/findBoundarySectionsOnStructuredGrid.m
+++ applications/DelftDashBoard/general/delft3dflow/misc/findBoundarySectionsOnStructuredGrid.m
@@ -1,8 +1,17 @@
 function openBoundaries=findBoundarySectionsOnStructuredGrid(openBoundaries,depth,zmax,d,varargin)
 
-opt=1;
-if ~isempty(varargin)
-    opt=varargin{1};
+namingoption=1;
+dpsopt='max';
+
+for ii=1:length(varargin)
+    if ischar(varargin{ii})
+        switch lower(varargin{ii})
+            case{'dpsopt'}
+                dpsopt=lower(varargin{ii+1});
+            case{'namingoption'}
+                namingoption=varargin{ii+1};
+        end
+    end
 end
 
 mmax=size(depth,1);
@@ -11,17 +20,22 @@ nmax=size(depth,2);
 % Boundary locations
 
 % North and South
-switch opt
+switch namingoption
     case 1
         dir={'North','South'};
     case 2
         dir={'Upper','Lower'};
 end
 
-n=[nmax 1];
+switch dpsopt
+    case{'dp'}
+        n=[nmax 2];
+    otherwise
+        n=[nmax 1];
+end
 n2=[nmax+1 1];
 nb=0;
-
+            
 for j=1:2
     
     nd=0;
@@ -32,23 +46,45 @@ for j=1:2
         % New boundary section
         while m<=mmax
             % Find start point of boundary section
-            if ~isnan(depth(m,n(j))) && ~isnan(depth(m-1,n(j))) && ...
-                    depth(m,n(j))<zmax
-                mstart=m;
-                break
-            else
-                m=m+1;
+            switch dpsopt
+                case{'dp'}
+                        if ~isnan(depth(m,n(j))) && ...
+                                depth(m,n(j))<zmax
+                            mstart=m;
+                            break
+                        else
+                            m=m+1;
+                        end
+                otherwise
+                    if ~isnan(depth(m,n(j))) && ~isnan(depth(m-1,n(j))) && ...
+                            depth(m,n(j))<zmax
+                        mstart=m;
+                        break
+                    else
+                        m=m+1;
+                    end
             end
         end
         mend=0;
         while m<=mstart+d-1 && m<=mmax
             % Find end point of boundary section
-            if ~isnan(depth(m,n(j))) && ~isnan(depth(m-1,n(j))) && ...
-                    depth(m,n(j))<zmax
-                mend=m;
-                m=m+1;
-            else
-                break
+            switch dpsopt
+                case{'dp'}
+                    if ~isnan(depth(m,n(j))) && ...
+                            depth(m,n(j))<zmax
+                        mend=m;
+                        m=m+1;
+                    else
+                        break
+                    end
+                otherwise
+                    if ~isnan(depth(m,n(j))) && ~isnan(depth(m-1,n(j))) && ...
+                            depth(m,n(j))<zmax
+                        mend=m;
+                        m=m+1;
+                    else
+                        break
+                    end
             end
         end
         if mstart>0 && mend>0
@@ -67,14 +103,19 @@ for j=1:2
 end
 
 % West and East
-switch opt
+switch namingoption
     case 1
         dir={'West','East'};
     case 2
         dir={'Left','Right'};
 end
 
-m=[1 mmax];
+switch dpsopt
+    case{'dp'}
+        m=[2 mmax];
+    otherwise
+        m=[1 mmax];
+end
 m2=[1 mmax+1];
 
 for j=1:2
@@ -86,23 +127,45 @@ for j=1:2
     while n<=nmax
         while n<=nmax
             % Find start point
-            if ~isnan(depth(m(j),n)) && ~isnan(depth(m(j),n-1)) && ...
-                    depth(m(j),n)<zmax
-                nstart=n;
-                break
-            else
-                n=n+1;
+            switch dpsopt
+                case{'dp'}
+                    if ~isnan(depth(m(j),n)) && ~isnan(depth(m(j),n-1)) && ...
+                            depth(m(j),n)<zmax
+                        nstart=n;
+                        break
+                    else
+                        n=n+1;
+                    end
+                otherwise
+                    if ~isnan(depth(m(j),n)) && ...
+                            depth(m(j),n)<zmax
+                        nstart=n;
+                        break
+                    else
+                        n=n+1;
+                    end                    
             end
         end
         nend=0;
         while n<=nstart+d-1 && n<=nmax
             % Find end point
-            if ~isnan(depth(m(j),n)) && ~isnan(depth(m(j),n-1)) && ...
-                    depth(m(j),n)<zmax
-                nend=n;
-                n=n+1;
-            else
-                break
+            switch dpsopt
+                case{'dp'}
+                    if ~isnan(depth(m(j),n)) && ...
+                            depth(m(j),n)<zmax
+                        nend=n;
+                        n=n+1;
+                    else
+                        break
+                    end
+                otherwise
+                    if ~isnan(depth(m(j),n)) && ~isnan(depth(m(j),n-1)) && ...
+                            depth(m(j),n)<zmax
+                        nend=n;
+                        n=n+1;
+                    else
+                        break
+                    end
             end
         end
         %                n=n+1;
