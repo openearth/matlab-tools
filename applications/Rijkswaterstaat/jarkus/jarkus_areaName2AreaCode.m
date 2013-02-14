2ce8e3c1cebeec104f4e7636d480bf1e8055291f
santinel <santinel@localhost>
2013-02-14 09:26:11 +0000

    small improvement to allow for cellstr
    svn path=/trunk/matlab/; revision=8082
    
    
    Former-commit-id: b11ea7d945430aeb1c3ded094ff0866dae306414

--- applications/Rijkswaterstaat/jarkus/jarkus_areaName2AreaCode.m
+++ applications/Rijkswaterstaat/jarkus/jarkus_areaName2AreaCode.m
@@ -2,8 +2,13 @@ function areacode = jarkus_areaName2AreaCode(areaname)
 %JARKUS_AREANAME2AREACODE  returns jarkus area code of selected jarkus area name
 
 url = jarkus_url;
-id = nc_varget(url,'id');
 areanames = nc_varget(url, 'areaname');
 areacodes = nc_varget(url, 'areacode');
-ids = strcmp(areaname,cellstr(areanames));
-areacode = areacodes(find(ids > 0, 1, 'first'));
+if size(areaname,1) == 1
+    ids = strcmp(areaname,cellstr(areanames));
+    areacode = areacodes(find(ids > 0, 1, 'first'));
+else
+    [dum idcell] = ismember(areaname,cellstr(areanames));
+    areacode = areacodes(idcell(idcell>0));
+end
+
