c8db4454ba4825d8f15438df5ee80530d5a4c43b
ormondt <ormondt@localhost>
2013-02-18 00:14:05 +0000

    comments removed from ddb_Delft3DFLOW_processes.m
    svn path=/trunk/matlab/; revision=8098
    
    
    Former-commit-id: af170e15a6350ee8e4664e6f0e47398f006370d0

--- applications/DelftDashBoard/models/Delft3DFLOW/gui/ddb_Delft3DFLOW_processes.m
+++ applications/DelftDashBoard/models/Delft3DFLOW/gui/ddb_Delft3DFLOW_processes.m
@@ -63,7 +63,6 @@ function ddb_Delft3DFLOW_processes(varargin)
 if isempty(varargin)
     ddb_zoomOff;
     ddb_refreshScreen;
-    % setUIElements('delft3dflow.processes');
 else
     
     opt=varargin{1};
@@ -71,67 +70,24 @@ else
     switch(lower(opt))
         
         case{'edittracers'}
-            ddb_Delft3DFLOW_editTracers;
-            % setUIElement('delft3dflow.processes.checktracers');
-            % setUIElement('delft3dflow.processes.pushedittracers');
-            
+            ddb_Delft3DFLOW_editTracers;            
             
         case{'editsediments'}
             ddb_Delft3DFLOW_editSediments;
-            % setUIElement('delft3dflow.processes.checksediments');
-            % setUIElement('delft3dflow.processes.pusheditsediments');
             
         case{'checkconstituents'}
             
         case{'checksediments'}
-            %             handles=getHandles;
-            %             if handles.Model(md).Input(ad).sediments.include
-            %                 enableTab(handles.GUIHandles.mainWindow,'delft3dflow.physicalparameters.physicalparameterspanel','sediments');
-            %                 enableTab(handles.GUIHandles.mainWindow,'delft3dflow.physicalparameters.physicalparameterspanel','morphology');
-            %             else
-            %                 disableTab(handles.GUIHandles.mainWindow,'delft3dflow.physicalparameters.physicalparameterspanel','sediments');
-            %                 disableTab(handles.GUIHandles.mainWindow,'delft3dflow.physicalparameters.physicalparameterspanel','morphology');
-            %             end
             
         case{'checktemperature'}
-            %             handles=getHandles;
-            %             if handles.Model(md).Input(ad).temperature.include
-            %                 enableTab(handles.GUIHandles.mainWindow,'delft3dflow.physicalparameters.physicalparameterspanel','heatflux');
-            %             else
-            %                 disableTab(handles.GUIHandles.mainWindow,'delft3dflow.physicalparameters.physicalparameterspanel','heatflux');
-            %             end
             
         case{'checkwind'}
-            %             handles=getHandles;
-            %             if handles.Model(md).Input(ad).wind
-            %                 enableTab(handles.GUIHandles.mainWindow,'delft3dflow.physicalparameters.physicalparameterspanel','wind');
-            %             else
-            %                 disableTab(handles.GUIHandles.mainWindow,'delft3dflow.physicalparameters.physicalparameterspanel','wind');
-            %             end
             
         case{'checkroller'}
-            %             handles=getHandles;
-            %             if handles.Model(md).Input(ad).roller.include
-            %                 enableTab(handles.GUIHandles.mainWindow,'delft3dflow.physicalparameters.physicalparameterspanel','roller');
-            %             else
-            %                 disableTab(handles.GUIHandles.mainWindow,'delft3dflow.physicalparameters.physicalparameterspanel','roller');
-            %             end
             
         case{'checktidalforces'}
-            %             handles=getHandles;
-            %             if handles.Model(md).Input(ad).tidalForces
-            %                 enableTab(handles.GUIHandles.mainWindow,'delft3dflow.physicalparameters.physicalparameterspanel','tidalforces');
-            %             else
-            %                 disableTab(handles.GUIHandles.mainWindow,'delft3dflow.physicalparameters.physicalparameterspanel','tidalforces');
-            %             end
             
         case{'checkdredging'}
-            %             handles=getHandles;
-            %             if handles.Model(md).Input(ad).dredging
-            %                 enableTab(handles.GUIHandles.mainWindow,'delft3dflow.physicalparameters.physicalparameterspanel','dredging');
-            %             else
-            %                 disableTab(handles.GUIHandles.mainWindow,'delft3dflow.physicalparameters.physicalparameterspanel','dredging');
-            %             end
             
     end
     
