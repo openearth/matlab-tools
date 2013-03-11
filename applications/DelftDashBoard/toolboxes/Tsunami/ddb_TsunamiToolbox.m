970d307351ddb8388934f9b6014ce507035632dc
ormondt <ormondt@localhost>
2013-03-11 16:21:20 +0000

    bug fix tsunamitoolbox with adjust depth files
    svn path=/trunk/matlab/; revision=8307
    
    
    Former-commit-id: ea35ac6e92a7af0fabf840a924f9e675a64fa4c3

--- applications/DelftDashBoard/toolboxes/Tsunami/ddb_TsunamiToolbox.m
+++ applications/DelftDashBoard/toolboxes/Tsunami/ddb_TsunamiToolbox.m
@@ -443,6 +443,8 @@ if isempty(handles.Model(md).Input(ad).gridX)
     return
 end
 
+depfiles=[];
+
 for id=1:handles.Model(md).nrDomains
     [filename, pathname, filterindex] = uiputfile('*.ini', ['Select initial conditions file for domain ' upper(handles.Model(md).Input(id).runid)],'');
     filenames{id}=filename;
