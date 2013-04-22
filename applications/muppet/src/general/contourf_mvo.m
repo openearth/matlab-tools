b33f97b664378b63989404d7800243bb62496d40
ormondt <ormondt@localhost>
2013-04-22 13:43:34 +0000

    contourf_mvo update
    svn path=/trunk/matlab/; revision=8485
    
    
    Former-commit-id: 38465610378fd1510878fc5a93ad5e7d89022072

--- applications/muppet/src/general/contourf_mvo.m
+++ applications/muppet/src/general/contourf_mvo.m
@@ -1,57 +1,13 @@
-function [c0,h,wp]=contourf_mvo(x0,y0,z0,col,clmap)
+function [c0,h,wp]=contourf_mvo(x0,y0,z0,col)
 
-c0=[];
-h=[];
-wp=[];
-
-colormap(clmap);
-
-nocol=size(col,2)-1;
- 
-z0max=max(max(z0));
-z0min=min(min(z0));
- 
-if z0max==z0min
-    for i=1:nocol
-        if z0max>=col(i) && z0max<col(i+1)
-            unicol=i;
-        end
-    end
-    if z0max<=col(1)
-        unicol=1;
-    end
-    if z0max>=col(end)
-        unicol=nocol;
-    end
-    for i=1:size(z0,1);
-        for j=1:size(z0,2);
-            r(i,j)=col(1)+(col(end)-col(1))*(i/size(z0,1));
-        end
-    end
-    z0=z0+0.01*r;
-end
-
-%[c0,h]=mp_contourfcorr(x0,y0,z0,col);hold on;
 [c0,h]=contourf(x0,y0,z0,col);hold on;
-cvbb=get(h);
-set(h,'EdgeColor','none');
-% sz=size(cvbb,1);
-% for i=1:sz
-%     set(h(i),'EdgeColor','none');
-%     val(i)=get(h(i),'CData');
-%     for j=1:nocol
-%         if val(i)==col(j)
-%              set(h(i),'FaceColor',clmap(max(1,j),:));
-%         end
-%     end
-% end
 
-clear x1;
-clear y1;
+set(h,'EdgeColor','none');
 
 jj=1;
 wp=[];
 
+% Add white triangles
 mmax=size(x0,1);
 nmax=size(x0,2);
 for i=2:mmax-1
@@ -65,10 +21,9 @@ for i=2:mmax-1
             k(6)=isfinite(z0(i  ,j-1));
             k(7)=isfinite(z0(i-1,j-1));
             k(8)=isfinite(z0(i-1,j  ));
-            ktot=sum(k);
             fac=0.2;
             z1=zeros(5);
-            if k<8;
+            if sum(k)<8;
                 if k(8) && k(2) && ~k(1)
                     x1(1)=x0(i-1,j  );
                     x1(3)=x0(i  ,j+1);
@@ -137,4 +92,3 @@ for i=2:mmax-1
         end
     end
 end
- 
