f94f12c41fde586cb4fb65fe3827959752d05efc
stengs <stengs@localhost>
2013-04-23 08:47:09 +0000

    Get all relevant SWAN parameters for a D-Flow FM computation
    svn path=/trunk/matlab/; revision=8493
    
    
    Former-commit-id: 575b4bf0f7d9b69e70e521841f45b244b18b6ea4

--- applications/bmi/bmi_demo_swan_bas.m
+++ applications/bmi/bmi_demo_swan_bas.m
@@ -12,18 +12,16 @@ if ini ~= 1
     config_file = 'd:\Checkouts\swan\branches\feature\esmf\esmf40.91\tests\DMrecSWAN02\swan.inp';
     
     %% Load the library
+    tic
     [bmidll] = bmi_new(dll);
     bmi_initialize(bmidll, config_file);
-    tic
     bmi_update(bmidll,3600);
-    toc
     
     %% Get variables
-    tic
     voq = bmi_var_get(bmidll, 'VOQ');
-    toc
-    
-    voqids    = bmi_var_get(bmidll, 'VOQR');    
+    voqids    = bmi_var_get(bmidll, 'VOQR');  
+    voq2      = bmi_var_get(bmidll, 'VOQ2');
+    voq2ids   = bmi_var_get(bmidll, 'VOQR2');
     s.XCGRID  = bmi_var_get(bmidll, 'XCGRID');
     s.YCGRID  = bmi_var_get(bmidll, 'YCGRID');
     s.dp2     = bmi_var_get(bmidll, 'DP2');
@@ -42,26 +40,46 @@ end
 %% Process data
 
 mip = 676;
-% Check swmod1.for MODULE SWCOMM1 for administration of index. 
+% Check swmod1.for MODULE SWCOMM1 and swanmain.for SUBROUTINE SWINIT for administration of index. 
 % Set in swanout1.for SUBROUTINE SWOEXA 
-ids.XP = 1;
-ids.YP = 2;
-ids.Hs = 10; % should be 10 (*) 
+ids.XP    = 1;
+ids.YP    = 2;
 ids.depth = 4;
+ids.Hs    = 10; 
+ids.tps   = 53;  % 'Relative peak period (smooth)'
+ids.phi   = 13;  % 'Average wave direction'
+ids.uorb  = 6;   % mind! ~= urms
+ids.F     = 20;  % 'Wave driven force per unit surface'
 
-s.XP = voq((mip*(voqids(ids.XP)-1)+1):(mip*(voqids(ids.XP))));
-s.YP = voq((mip*(voqids(ids.YP)-1)+1):(mip*(voqids(ids.YP))));
-s.twoD.XP = reshape(s.XP,fliplr(size(s.XCGRID)));
-s.twoD.YP = reshape(s.YP,fliplr(size(s.XCGRID)));
-
-s.Hs = voq((mip*(voqids(ids.Hs)-1)+1):(mip*(voqids(ids.Hs))));
-s.twoD.Hs = reshape(s.Hs,fliplr(size(s.XCGRID)));
-s.twoD.Hs(s.twoD.Hs==-999)=NaN;
-
+% retrieve from VOQ
+s.XP    = voq((mip*(voqids(ids.XP)-1)+1):(mip*(voqids(ids.XP))));
+s.YP    = voq((mip*(voqids(ids.YP)-1)+1):(mip*(voqids(ids.YP))));
 s.depth = voq((mip*(voqids(ids.depth)-1)+1):(mip*(voqids(ids.depth))));
-s.twoD.depth = reshape(s.depth,fliplr(size(s.XCGRID)));
-s.twoD.depth(s.twoD.depth==-999)=NaN;
+s.Hs    = voq((mip*(voqids(ids.Hs)-1)+1):(mip*(voqids(ids.Hs))));
+s.tps   = voq2((mip*(voq2ids(ids.tps)-1)+1):(mip*(voq2ids(ids.tps))));
+s.phi   = voq((mip*(voqids(ids.phi)-1)+1):(mip*(voqids(ids.phi))));
+s.uorb  = voq((mip*(voqids(ids.uorb)-1)+1):(mip*(voqids(ids.uorb))));
+s.F     = voq((mip*(voqids(ids.F)-1)+1):(mip*(voqids(ids.F))));
 
+% give 2D shape
+s.twoD.XP    = reshape(s.XP,fliplr(size(s.XCGRID)));
+s.twoD.YP    = reshape(s.YP,fliplr(size(s.XCGRID)));
+s.twoD.depth = reshape(s.depth,fliplr(size(s.XCGRID)));
+s.twoD.Hs    = reshape(s.Hs,fliplr(size(s.XCGRID)));
+s.twoD.tps   = reshape(s.tps,fliplr(size(s.XCGRID)));
+s.twoD.phi   = reshape(s.phi,fliplr(size(s.XCGRID)));
+s.twoD.uorb  = reshape(s.uorb,fliplr(size(s.XCGRID)));
+s.twoD.F     = reshape(s.F,fliplr(size(s.XCGRID)));
+
+% remove -999
+s.twoD.depth(s.twoD.depth==-999)=NaN;
+s.twoD.Hs(s.twoD.Hs==-999)=NaN;
+s.twoD.tps(s.twoD.tps==-999)=NaN;
+s.twoD.phi(s.twoD.phi==-999)=NaN;
+s.twoD.uorb(s.twoD.uorb==-999)=NaN;
+s.twoD.F(s.twoD.F==-999)=NaN;
+
+% old
 s.dp2 = s.dp2(1:end-1);
 s.twoD.dp2 = reshape(s.dp2,fliplr(size(s.XCGRID)));
 
@@ -77,18 +95,40 @@ s.twoD.dp2 = reshape(s.dp2,fliplr(size(s.XCGRID)));
 % figure
 % surfc(double(s.XCGRID), double(s.YCGRID), double(s.twoD.dp2)); colorbar
 
-figure
-surfc(double(s.twoD.XP), double(s.twoD.YP), double(s.twoD.Hs))
-colorbar
-
 
 figure
 % plot3(double(s.XP), double(s.YP), double(s.depth))
 surfc(double(s.twoD.XP), double(s.twoD.YP), double(s.twoD.depth))
+title('depth')
 xlabel('x')
 ylabel('y')
 colorbar
 
+figure
+surfc(double(s.twoD.XP), double(s.twoD.YP), double(s.twoD.Hs))
+title('Hs')
+colorbar
 
+figure
+surfc(double(s.twoD.XP), double(s.twoD.YP), double(s.twoD.tps))
+title('tps')
+colorbar
+
+figure
+surfc(double(s.twoD.XP), double(s.twoD.YP), double(s.twoD.phi))
+title('phi')
+colorbar
+
+figure
+surfc(double(s.twoD.XP), double(s.twoD.YP), double(s.twoD.uorb))
+title('uorb')
+colorbar
+
+figure
+surfc(double(s.twoD.XP), double(s.twoD.YP), double(s.twoD.F))
+title('wave force')
+colorbar
+
+toc
 %% cleanup
 bmi_finalize(bmidll);
