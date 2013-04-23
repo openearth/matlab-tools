d694ed5e59d53d5c2efe75a150449f3c9463de52
verploe <verploe@localhost>
2013-04-23 14:21:40 +0000

    svn path=/trunk/matlab/; revision=8494
    Former-commit-id: e82ef9f283fde60cdc4b9e31a8c25dd3c530db5f

--- applications/DelftDashBoard/general/fileio/ReadTextFile.m
+++ applications/DelftDashBoard/general/fileio/ReadTextFile.m
@@ -62,14 +62,15 @@ function txt = ReadTextFile(FileName)
 %%
 fid=fopen(FileName);
 
-k=0;
-for i=1:1000
+k = 0;
+while ~feof(fid)
     tx0=fgets(fid);
     if and(ischar(tx0), size(tx0>0))
         v0=strread(tx0,'%q');
     else
         v0='';
     end
+
     if size(v0,1)>0
         if strcmp(tx0(1),'#')==0
             v=strread(tx0,'%q');
@@ -81,6 +82,7 @@ for i=1:1000
             clear v;
         end
     end
+
 end
 
 fclose(fid);
