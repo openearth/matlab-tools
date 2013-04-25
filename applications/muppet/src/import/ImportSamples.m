51d7f34b0c226a445a88f38ddcb63f60bb9520d4
verploe <verploe@localhost>
2013-04-25 11:23:50 +0000

    possibility of reading another column added.
    svn path=/trunk/matlab/; revision=8512
    
    
    Former-commit-id: bd954fac703b8f596001f9ca97bd425045fc68aa

--- applications/muppet/src/import/ImportSamples.m
+++ applications/muppet/src/import/ImportSamples.m
@@ -4,7 +4,8 @@ a=load([DataProperties(i).PathName DataProperties(i).FileName]);
  
 DataProperties(i).x=a(:,1);
 DataProperties(i).y=a(:,2);
-DataProperties(i).z=a(:,3);
+kol = DataProperties(i).Column;
+DataProperties(i).z=a(:,kol);
 
 DataProperties(i).Type = 'Samples';
 DataProperties(i).TC='c';
