f3bfc23476b97db6068865518102803bec65689f
hengst <hengst@localhost>
2013-04-29 14:59:13 +0000

    Added an optional location parameter
    svn path=/trunk/matlab/; revision=8539
    
    
    Former-commit-id: 2b0205a865314bf5a3475a2166ae6102665966a2

--- applications/probabilistic/distributions/logn_inv.m
+++ applications/probabilistic/distributions/logn_inv.m
@@ -1,19 +1,20 @@
-function X = logn_inv(P, mu, sigma)
+function X = logn_inv(P, mu, sigma, varargin)
 %LOGN_INV  inverse of the lognormal cumulative distribution function (cdf)
 %
 %   This function returns the inverse cdf of the lognormal distribution,
-%   evaluated at the values in P.
+%   evaluated at the values in P. The last optional argument 
 %
 %   Syntax:
 %   X = logn_inv(P, mu, sigma)
 %
 %   Input:
-%   X     = variable values
-%
-%   Output:
 %   P     = cdf
 %   mu    = mean value
 %   sigma = standard deviation
+%   varargin may contain an optional location parameter to shift the distribution
+%
+%   Output:
+%   X     = variable values
 %
 %   Example
 %   logn_inv
@@ -55,10 +56,19 @@ function X = logn_inv(P, mu, sigma)
 % $HeadURL$
 % $Keywords: $
 
-%%
+%% Code
+
+% Assign a value to the location parameter xloc, the variable which shifts the distribution
+if isempty(varargin)
+    x_loc = 0;
+else
+    x_loc = cell2mat(varargin(1));
+end
+
 % Return NaN for out of range parameters or probabilities.
 sigma(sigma <= 0) = NaN;
 P(P < 0 | 1 < P) = NaN;
 
+% Apply lognormal distribution
 logx0 = -sqrt(2).*erfcinv(2*P);
-X = exp(sigma.*logx0 + mu);
+X = exp(sigma.*logx0 + mu) + x_loc;
