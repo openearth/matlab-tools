8c3a3a077c58426599375663c11c84f91840d7c6
kaaij <kaaij@localhost>
2013-04-29 14:57:55 +0000

    svn path=/trunk/matlab/; revision=8538
    Former-commit-id: a62c42ddb904d3149c960ba513a1a3a253eef396

--- applications/delft3d/nest_matlab/run.bat
+++ applications/delft3d/nest_matlab/run.bat
@@ -1,8 +1,8 @@
-nesthd test\shenzen\shenzen_hd1.ini
-nesthd test\shenzen\shenzen_hd2.ini
-nesthd test\tidal_flume_triwaq\triwaq_coarse\flume_hd1.ini
-nesthd test\tidal_flume_triwaq\triwaq_coarse\flume_hd2.ini
-nesthd test\milford\milford_hd1.ini
-nesthd test\milford\milford_hd1.ini
-nesthd test\zeedelta\zeedelta_hd1.ini
-nesthd test\zeedelta\zeedelta_hd2.ini
+nesthd d:\projects\nesthd_matlab\test\shenzen\shenzen_hd1.ini
+nesthd d:\projects\nesthd_matlab\test\shenzen\shenzen_hd2.ini
+nesthd d:\projects\nesthd_matlab\test\tidal_flume_triwaq\triwaq_coarse\flume_hd1.ini
+nesthd d:\projects\nesthd_matlab\test\tidal_flume_triwaq\triwaq_coarse\flume_hd2.ini
+nesthd d:\projects\nesthd_matlab\test\milford\milford_hd1.ini
+nesthd d:\projects\nesthd_matlab\test\milford\milford_hd1.ini
+nesthd d:\projects\nesthd_matlab\test\zeedelta\zeedelta_hd1.ini
+nesthd d:\projects\nesthd_matlab\test\zeedelta\zeedelta_hd2.ini
