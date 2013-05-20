8864a94686f581bed30fc3df2ac5f5ad70231382
heijer <heijer@localhost>
2013-05-20 13:30:15 +0000

    read and write functions added for UNIBEST-TC bottom files
    svn path=/trunk/matlab/; revision=8652
    
    
    Former-commit-id: 4247beb005f58f2b87c0386d781cd7368bc840ab

---
+++ applications/unibest/fileio/readBOT.m
@@ -0,0 +1,86 @@
+function varargout = readBOT(fname, varargin)
+%READBOT  Read bottom file.
+%
+%   More detailed description goes here.
+%
+%   Syntax:
+%   varargout = readBOT(varargin)
+%
+%   Input: For <keyword,value> pairs call readBOT() without arguments.
+%   varargin  =
+%
+%   Output:
+%   varargout =
+%
+%   Example
+%   readBOT
+%
+%   See also
+
+%% Copyright notice
+%   --------------------------------------------------------------------
+%   Copyright (C) 2013 Deltares
+%       Kees den Heijer
+%
+%       kees.denheijer@deltares.nl
+%
+%       P.O. Box 177
+%       2600 MH  DELFT
+%       The Netherlands
+%       Rotterdamseweg 185
+%       2629 HD  DELFT
+%
+%   This library is free software: you can redistribute it and/or modify
+%   it under the terms of the GNU General Public License as published by
+%   the Free Software Foundation, either version 3 of the License, or
+%   (at your option) any later version.
+%
+%   This library is distributed in the hope that it will be useful,
+%   but WITHOUT ANY WARRANTY; without even the implied warranty of
+%   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%   GNU General Public License for more details.
+%
+%   You should have received a copy of the GNU General Public License
+%   along with this library.  If not, see <http://www.gnu.org/licenses/>.
+%   --------------------------------------------------------------------
+
+% This tool is part of <a href="http://www.OpenEarth.eu">OpenEarthTools</a>.
+% OpenEarthTools is an online collaboration to share and manage data and
+% programming tools in an open source, version controlled environment.
+% Sign up to recieve regular updates of this function, and to contribute
+% your own tools.
+
+%% Version <http://svnbook.red-bean.com/en/1.5/svn.advanced.props.special.keywords.html>
+% Created: 20 May 2013
+% Created with Matlab version: 8.1.0.604 (R2013a)
+
+% $Id$
+% $Date$
+% $Author$
+% $Revision$
+% $HeadURL$
+% $Keywords: $
+
+%%
+OPT.Nr_commentlines=4;
+% return defaults (aka introspection)
+if nargin==0;
+    varargout = {OPT};
+    %return
+end
+% overwrite defaults with user arguments
+OPT = setproperty(OPT, varargin);
+
+%% code
+fname = 'd:\heijer\Work\Deltares\Projects\KPP\TvdK\nourishment\iniProf.bot';
+fid = fopen(fname);
+txt = cell(OPT.Nr_commentlines);
+for i = 1:OPT.Nr_commentlines
+    txt{i} = fgetl(fid);
+end
+data = fscanf(fid,'%f',[2 inf]);
+fclose(fid);
+
+x = data(:,1);
+z = data(:,2);
+varargout = {x, z, txt};
