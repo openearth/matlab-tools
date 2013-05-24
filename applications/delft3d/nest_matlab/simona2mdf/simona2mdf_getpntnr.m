cbf51ecdb54bf1b845200e3c9155617a497c34d1
kaaij <kaaij@localhost>
2013-05-24 08:23:35 +0000

    svn path=/trunk/matlab/; revision=8669
    Former-commit-id: 81fbc5b4ec674e1c12a7a1fba8b0f77d2b8a45ac

--- applications/delft3d/nest_matlab/simona2mdf/simona2mdf_getpntnr.m
+++ applications/delft3d/nest_matlab/simona2mdf/simona2mdf_getpntnr.m
@@ -2,6 +2,8 @@ function pnt = simona2mdf_area(list,nr)
 
 % simona2mdf_getpntnr : gets the point (squence) number out of a list of points/curves
 
+pnt = [];
+
 for ipnt = 1: length(list)
     if list(ipnt).SEQNR == nr
         pnt = ipnt;
