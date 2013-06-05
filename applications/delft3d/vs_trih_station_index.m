be0b01b858c817f93f7bfc6a62a0c12d21750041
bartgrasmeijer.x <bartgrasmeijer.x@localhost>
2013-06-05 13:38:20 +0000

    vs_trih_station_index.m minor fix
    svn path=/trunk/matlab/; revision=8777
    
    
    Former-commit-id: 13effa58607fe624581017823b8ba50c253b130c

--- applications/delft3d/vs_trih_station_index.m
+++ applications/delft3d/vs_trih_station_index.m
@@ -68,126 +68,125 @@ function indices = vs_trih_station_index(trih,varargin)
 % 2009 sep 28: added implementation of WAQ hda files [Yann Friocourt]
 
 if ~isstruct(trih)
-   if strcmpi(fileext(trih),'.nc')
-   ncfile = trih;
-   trih.SubType   = 'netCDF';
-   ST.Description = 'DFLOW monitoring point (*.obs) time serie.';
-   error('This is a netCDD file: use dflow.indexHis instead')
-   else
-   trih = vs_use(trih,'quiet');
-   end
+    if strcmpi(fileext(trih),'.nc')
+        ncfile = trih;
+        trih.SubType   = 'netCDF';
+        ST.Description = 'DFLOW monitoring point (*.obs) time serie.';
+        error('This is a netCDD file: use dflow.indexHis instead')
+    else
+        trih = vs_use(trih,'quiet');
+    end
 end
 
-   method = 'strcmp';
-   method = 'strmatch';
+method = 'strcmp';
+method = 'strmatch';
 
 if nargin==1
-   method = 'list';
+    method = 'list';
 end
 
 if nargin > 1
-   stationname = varargin{1};
+    stationname = varargin{1};
 end
 
 if nargin > 2
-   method = varargin{2};
+    method = varargin{2};
 end
 
 %% Do we work with a FLOW or WAQ file?
 
-   if strcmp(trih.SubType,'Delft3D-trih')
-       OPT.GrpName    = 'his-const';
-       OPT.ElmName    = 'NAMST';
-       ST.Description = 'Delft3d-FLOW monitoring point (*.obs) time serie.';
-   elseif strcmp(trih.SubType,'Delft3D-waq-history')
-       OPT.GrpName    = 'DELWAQ_PARAMS';
-       OPT.ElmName    = 'LOCATION_NAMES';
-       ST.Description = 'Delft3d-WAQ monitoring point (*.obs) time serie.';
-   end
+if strcmp(trih.SubType,'Delft3D-trih')
+    OPT.GrpName    = 'his-const';
+    OPT.ElmName    = 'NAMST';
+    ST.Description = 'Delft3d-FLOW monitoring point (*.obs) time serie.';
+elseif strcmp(trih.SubType,'Delft3D-waq-history')
+    OPT.GrpName    = 'DELWAQ_PARAMS';
+    OPT.ElmName    = 'LOCATION_NAMES';
+    ST.Description = 'Delft3d-WAQ monitoring point (*.obs) time serie.';
+end
 
 %% Load all station names
 
-   namst = squeeze(vs_let(trih(1),OPT.GrpName,OPT.ElmName,'quiet'));
-   
-   nstat = size(namst,1);
+namst = squeeze(vs_let(trih(1),OPT.GrpName,OPT.ElmName,'quiet'));
+
+nstat = size(namst,1);
 
 %% Cycle all stations and quit immediatlety
 %  when a match has been found
 
 switch method
-case 'list' %this one should be first in case
-
-    if strcmp(trih.SubType,'Delft3D-trih')
-
-        D = vs_trih_station(trih(1));
-
-        if nargout==0
-
-            disp('+------------------------------------------------------------------------->')
-            disp(['| ',trih(1).FileName])
-            disp('| index         name         m    n     angle  x and y')
-            disp('+-----+--------------------+-----+-----+-----+---------------------------->')
-
-            for istat=1:nstat
-
-                disp([' ',...
-                    pad(num2str(      istat  ) ,-5,' ') ,' ',...
-                    pad(D.name(istat,:) ,20,' ') ,' ',...
-                    pad(num2str(D.m    (istat)),-5,' ') ,' ',...
-                    pad(num2str(D.n    (istat)),-5,' ') ,' ',...
-                    pad(num2str(D.angle(istat  ),'%+3.1f'),-5,' ') ,' ',...
-                    pad(num2str(D.x    (istat),'%+16.6f'),-14,' '),' ',...
-                    pad(num2str(D.y    (istat),'%+16.6f'),-14,' ')]);
-
+    case 'list' %this one should be first in case
+        
+        if strcmp(trih.SubType,'Delft3D-trih')
+            
+            D = vs_trih_station(trih(1));
+            
+            if nargout==0
+                
+                disp('+------------------------------------------------------------------------->')
+                disp(['| ',trih(1).FileName])
+                disp('| index         name         m    n     angle  x and y')
+                disp('+-----+--------------------+-----+-----+-----+---------------------------->')
+                
+                for istat=1:nstat
+                    
+                    disp([' ',...
+                        pad(num2str(      istat  ) ,-5,' ') ,' ',...
+                        pad(D.name(istat,:) ,20,' ') ,' ',...
+                        pad(num2str(D.m    (istat)),-5,' ') ,' ',...
+                        pad(num2str(D.n    (istat)),-5,' ') ,' ',...
+                        pad(num2str(D.angle(istat  ),'%+3.1f'),-5,' ') ,' ',...
+                        pad(num2str(D.x    (istat),'%+16.6f'),-14,' '),' ',...
+                        pad(num2str(D.y    (istat),'%+16.6f'),-14,' ')]);
+                    
+                end
+                
+                istat = nan;
+                
+            elseif nargout==1
+                indices.namst = namst;
+                indices.m     = D.m   ;
+                indices.n     = D.n   ;
+                indices.ang   = D.angle  ;
+                indices.x     = D.x   ;
+                indices.y     = D.y   ;
+                
             end
-
-            istat = nan;
-
-        elseif nargout==1
-
-            indices.namst = namst;
-            indices.mn    = mn   ;
-            indices.mn    = mn   ;
-            indices.ang   = ang  ;
-            indices.xy    = xy   ;
-            indices.xy    = xy   ;
-
+            
         end
-
-    end
-
-case 'strcmp'
-
-   indices = [];
-
-   for i=1:size(stationname,1)
-
-      for istat=1:nstat
-
-         if strcmp(strtrim(stationname(i,:)),...
-                   strtrim(namst(istat,:)))
-
+        
+    case 'strcmp'
+        
+        indices = [];
+        
+        for i=1:size(stationname,1)
+            
+            for istat=1:nstat
+                
+                if strcmp(strtrim(stationname(i,:)),...
+                        strtrim(namst(istat,:)))
+                    
+                    indices = [indices istat];
+                    
+                end
+                
+            end
+            
+        end
+        
+        
+    case 'strmatch'
+        
+        indices = [];
+        
+        for i=1:size(stationname,1)
+            
+            istat = strmatch(stationname(i,:),namst); % ,'exact'
+            
             indices = [indices istat];
-
-         end
-
-      end
-
-   end
-
-
-case 'strmatch'
-
-   indices = [];
-
-   for i=1:size(stationname,1)
-
-      istat = strmatch(stationname(i,:),namst); % ,'exact'
-
-      indices = [indices istat];
-
-   end
-
+            
+        end
+        
 end
 
 %% EOF
