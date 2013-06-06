45837fbe105ffb135831017adf0075bf4d1c60ab
rcmartyr <rcmartyr@localhost>
2013-06-06 23:06:58 +0000

    Updated NetCDF variable reading commands Removed legacy reading code in conflict with updated NetCDF reading
    svn path=/trunk/matlab/; revision=8785
    
    
    Former-commit-id: ec84c856b66105e0199f726ce541ed474e821d8d

--- applications/delft3d/+dflowfm/readHis.m
+++ applications/delft3d/+dflowfm/readHis.m
@@ -15,42 +15,42 @@ else
     statname = [];
 end
 
-ihisfile = netcdf.open(filename, 'NOWRITE');
+
 
 hisdata=struct();
-hisdata.time       = nc_getVarByName(ihisfile, 'time');
+hisdata.time       = nc_varget(filename, 'time');
 
-station_name       = nc_getVarByName(ihisfile, 'station_name');
-station_x_coord    = nc_getVarByName(ihisfile, 'station_x_coordinate');
-station_y_coord    = nc_getVarByName(ihisfile, 'station_y_coordinate');
+station_name       = nc_varget(filename, 'station_name');
+station_x_coord    = nc_varget(filename, 'station_x_coordinate');
+station_y_coord    = nc_varget(filename, 'station_y_coordinate');
 
 if isempty(statname)
     hisdata.station_name    = station_name;
     hisdata.station_x_coord = station_x_coord;
     hisdata.station_y_coord = station_y_coord;
-	hisdata.waterlevel      = nc_getVarByName(ihisfile, 'waterlevel');
-	hisdata.x_velocity      = nc_getVarByName(ihisfile, 'x_velocity');
-	hisdata.y_velocity      = nc_getVarByName(ihisfile, 'y_velocity');
-    hisdata.cross_section_discharge = nc_getVarByName(ihisfile, 'cross_section_discharge');
-    hisdata.cross_section_area      = nc_getVarByName(ihisfile, 'cross_section_area');
-    hisdata.cross_section_velocity  = nc_getVarByName(ihisfile, 'cross_section_velocity');
+	hisdata.waterlevel      = nc_varget(filename, 'waterlevel');
+	hisdata.x_velocity      = nc_varget(filename, 'x_velocity');
+	hisdata.y_velocity      = nc_varget(filename, 'y_velocity');
+    hisdata.cross_section_discharge = nc_varget(filename, 'cross_section_discharge');
+    hisdata.cross_section_area      = nc_varget(filename, 'cross_section_area');
+    hisdata.cross_section_velocity  = nc_varget(filename, 'cross_section_velocity');
 else
     idx = [];
-    for i=1:size(station_name,2)
-        if strcmpi(deblank(station_name(:,i)'), statname)
+    for i=1:size(station_name,1)
+        if strcmpi(deblank(station_name(i,:)), statname)
             idx = i;
         end
     end
     if isempty(idx)
         warning('Station ''%s'' not found.', statname);
     else
-        waterlevel              = nc_getVarByName(ihisfile, 'waterlevel');
-        hisdata.station_name    = deblank(station_name(:,idx)');
+        waterlevel              = nc_varget(filename, 'waterlevel');
+        hisdata.station_name    = deblank(station_name(idx,:));
         hisdata.station_x_coord = station_x_coord(idx);
         hisdata.station_y_coord = station_y_coord(idx);
-        hisdata.waterlevel = waterlevel(idx,:);
+        hisdata.waterlevel = waterlevel(:,idx);
     end
 end
-netcdf.close(ihisfile);
+
 
 end
