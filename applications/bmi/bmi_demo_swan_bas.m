eb8f24e1889af3748bb2bbcce554610de3eedbae
stengs <stengs@localhost>
2013-06-06 08:37:31 +0000

    plot adjustments
    svn path=/trunk/matlab/; revision=8779
    
    
    Former-commit-id: 0cc29f60a3ab3a2607fe717e7c4764fb27c3a2fd

--- applications/bmi/bmi_demo_swan_bas.m
+++ applications/bmi/bmi_demo_swan_bas.m
@@ -19,17 +19,17 @@ if ini ~= 1
     
     %% Get variables
     voq = bmi_var_get(bmidll, 'VOQ');
-    voqids    = bmi_var_get(bmidll, 'VOQR');  
+    voqids    = bmi_var_get(bmidll, 'VOQR');
     voq2      = bmi_var_get(bmidll, 'VOQ2');
     voq2ids   = bmi_var_get(bmidll, 'VOQR2');
     s.XCGRID  = bmi_var_get(bmidll, 'XCGRID');
     s.YCGRID  = bmi_var_get(bmidll, 'YCGRID');
     s.dp2     = bmi_var_get(bmidll, 'DP2');
     
-%     s.ac2     = bmi_var_get(bmidll, 'AC2');
-%     s.spcsig  = bmi_var_get(bmidll, 'SPCSIG');
-%     s.ddir    = bmi_var_get(bmidll, 'DDIR');
-%     s.pwtail  = bmi_var_get(bmidll, 'PWTAIL');
+    %     s.ac2     = bmi_var_get(bmidll, 'AC2');
+    %     s.spcsig  = bmi_var_get(bmidll, 'SPCSIG');
+    %     s.ddir    = bmi_var_get(bmidll, 'DDIR');
+    %     s.pwtail  = bmi_var_get(bmidll, 'PWTAIL');
     %% (*) (still to) get variables
     % frintf  = bmi_var_get(bmidll, 'FRINTF');
     % fx      = bmi_var_get(bmidll, 'fx');          & dS/dx ??
@@ -40,12 +40,12 @@ end
 %% Process data
 
 mip = 676;
-% Check swmod1.for MODULE SWCOMM1 and swanmain.for SUBROUTINE SWINIT for administration of index. 
-% Set in swanout1.for SUBROUTINE SWOEXA 
+% Check swmod1.for MODULE SWCOMM1 and swanmain.for SUBROUTINE SWINIT for administration of index.
+% Set in swanout1.for SUBROUTINE SWOEXA
 ids.XP    = 1;
 ids.YP    = 2;
 ids.depth = 4;
-ids.Hs    = 10; 
+ids.Hs    = 10;
 ids.tps   = 53;  % 'Relative peak period (smooth)'
 ids.phi   = 13;  % 'Average wave direction'
 ids.uorb  = 6;   % mind! ~= urms
@@ -88,47 +88,105 @@ s.twoD.dp2 = reshape(s.dp2,fliplr(size(s.XCGRID)));
 % out.hs_2D = reshape(out.hs,fliplr(size(s.XCGRID)));
 
 
-%% Print
+%% Print individual parameters
+
 
 % figure
 % surfc(double(s.XCGRID), double(s.YCGRID), flipud(double(out.hs_2D)))
 % figure
 % surfc(double(s.XCGRID), double(s.YCGRID), double(s.twoD.dp2)); colorbar
 
+pl.indivP = 0;
+if pl.indivP
+    figure
+    % plot3(double(s.XP), double(s.YP), double(s.depth))
+    surfc(double(s.twoD.XP), double(s.twoD.YP), double(s.twoD.depth))
+    title('depth')
+    xlabel('x')
+    ylabel('y')
+    colorbar
+    
+    figure
+    surfc(double(s.twoD.XP), double(s.twoD.YP), double(s.twoD.Hs))
+    title('Hs')
+    colorbar
+    
+    figure
+    surfc(double(s.twoD.XP), double(s.twoD.YP), double(s.twoD.tps))
+    title('tps')
+    colorbar
+    
+    figure
+    surfc(double(s.twoD.XP), double(s.twoD.YP), double(s.twoD.phi))
+    title('phi')
+    colorbar
+    
+    figure
+    surfc(double(s.twoD.XP), double(s.twoD.YP), double(s.twoD.F))
+    title('wave force')
+    colorbar
+    
+    figure
+    surfc(double(s.twoD.XP), double(s.twoD.YP), double(s.twoD.uorb))
+    title('uorb')
+    colorbar
+    
+end
 
-figure
-% plot3(double(s.XP), double(s.YP), double(s.depth))
-surfc(double(s.twoD.XP), double(s.twoD.YP), double(s.twoD.depth))
-title('depth')
-xlabel('x')
-ylabel('y')
-colorbar
 
-figure
-surfc(double(s.twoD.XP), double(s.twoD.YP), double(s.twoD.Hs))
-title('Hs')
-colorbar
 
-figure
-surfc(double(s.twoD.XP), double(s.twoD.YP), double(s.twoD.tps))
-title('tps')
-colorbar
+%% combine parameters in one figure
 
-figure
-surfc(double(s.twoD.XP), double(s.twoD.YP), double(s.twoD.phi))
-title('phi')
-colorbar
+pl.combP = 1;
+if pl.combP
+    scrsz= get(0, 'ScreenSize');
+    figure('Position', scrsz, 'Visible','on');
+    set(gcf,'defaultaxesfontsize',10)
+    
+    subplot(321)
+    surfc(double(s.twoD.XP), double(s.twoD.YP), -double(s.twoD.depth))
+    title('depth [m]')
+    colorbar
+    view(142.5, 30)
+    
+    
+    
+    subplot(322)
+    surfc(double(s.twoD.XP), double(s.twoD.YP), double(s.twoD.Hs))
+    title('Hs [m]')
+    colorbar
+    view(142.5, 30)
+    
+    subplot(323)
+    surfc(double(s.twoD.XP), double(s.twoD.YP), double(s.twoD.tps))
+    title('tps [s]')
+    colorbar
+    view(142.5, 30)
+    
+    subplot(324)
+    surfc(double(s.twoD.XP), double(s.twoD.YP), double(s.twoD.phi))
+    title('phi [deg]')
+    colorbar
+    view(142.5, 30)
+    
+    subplot(325)
+    surfc(double(s.twoD.XP), double(s.twoD.YP), double(s.twoD.F))
+    title('wave force [N/m^2]')
+    xlabel('x')
+    ylabel('y')
+    colorbar
+    view(142.5, 30)
+    
+    subplot(326)
+    surfc(double(s.twoD.XP), double(s.twoD.YP), double(s.twoD.uorb))
+    title('uorb [m/s]')
+    xlabel('x')
+    ylabel('y')
+    colorbar
+    view(142.5, 30)
+end
 
-figure
-surfc(double(s.twoD.XP), double(s.twoD.YP), double(s.twoD.uorb))
-title('uorb')
-colorbar
 
-figure
-surfc(double(s.twoD.XP), double(s.twoD.YP), double(s.twoD.F))
-title('wave force')
-colorbar
-
-toc
 %% cleanup
 bmi_finalize(bmidll);
+toc
