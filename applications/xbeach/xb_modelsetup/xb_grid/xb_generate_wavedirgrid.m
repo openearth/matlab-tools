e3813afb2460eb6a05b27d3f6dc079c06b78736f
robbin.vansanten.x <robbin.vansanten.x@localhost>
2013-07-04 15:21:52 +0000

    svn path=/trunk/matlab/; revision=8885
    Former-commit-id: 4da7a7d12ba366e2783d21ff8333a3f4810e6239

--- applications/xbeach/xb_modelsetup/xb_grid/xb_generate_wavedirgrid.m
+++ applications/xbeach/xb_modelsetup/xb_grid/xb_generate_wavedirgrid.m
@@ -16,7 +16,7 @@ function xb = xb_generate_wavedirgrid(xb,varargin)
 %   xb          = XBeach structure array (with adapted theta
 %
 %   Example
-%   % 
+%   %
 %   waves = xb_generate_waves
 %   xb = xs_join(xb, waves);
 %   exmaple 1
@@ -26,14 +26,14 @@ function xb = xb_generate_wavedirgrid(xb,varargin)
 %   example 3
 %   xb = xs_empty(); xb = xs_set(xb,'alpha',-33,'dir',[231 241 258 273],'s',[2 10 2 5]); xb = xb_generate_wavedirgrid(xb);
 %
-%   See also 
+%   See also
 
 %% Copyright notice
 %   --------------------------------------------------------------------
 %   Copyright (C) 2010 <COMPANY>
 %       Jaap van Thiel de Vries
 %
-%       <EMAIL>	
+%       <EMAIL>
 %
 %       <ADDRESS>
 %
@@ -52,9 +52,9 @@ function xb = xb_generate_wavedirgrid(xb,varargin)
 %   --------------------------------------------------------------------
 
 % This tool is part of <a href="http://OpenEarth.nl">OpenEarthTools</a>.
-% OpenEarthTools is an online collaboration to share and manage data and 
+% OpenEarthTools is an online collaboration to share and manage data and
 % programming tools in an open source, version controlled environment.
-% Sign up to recieve regular updates of this function, and to contribute 
+% Sign up to recieve regular updates of this function, and to contribute
 % your own tools.
 
 %% Version <http://svnbook.red-bean.com/en/1.5/svn.advanced.props.special.keywords.html>
@@ -68,7 +68,7 @@ function xb = xb_generate_wavedirgrid(xb,varargin)
 % $HeadURL$
 % $Keywords: $
 
-%% 
+%%
 
 OPT = struct( ...
             'varthr', 0.05, ...
@@ -77,10 +77,6 @@ OPT = struct( ...
             'plot', false ...
             );
 
-% if OPT.plot == 1
-%     close(figure(33))
-% end
-        
 OPT = setproperty(OPT, varargin{:});
 
 if ismember(xs_get(xb, 'bcfile.type'), {'jonswap' 'jonswap_mtx'})
@@ -90,21 +86,23 @@ if ismember(xs_get(xb, 'bcfile.type'), {'jonswap' 'jonswap_mtx'})
 
     % find mean wave direction, minimum wave direction and maximum wave
     % direction in wave bc time series
-    
+
     % bas: this should be alfa instead of alpha, right?
     %      this parameter is not available at this location
     %      is it necessary anyway? thetabins should be defined in world coordinates
     alpha = 0; %xs_get(xb,'alpha');             % get coastline / grid orientation alpha
-    
+
     phi0t = xs_get(xb,'bcfile.mainang');    % get wave directions (nautical)
     phi0 = mean(phi0t);                     % compute mean wave direction
-    theta_min = phi0;           
+    theta_min = phi0;
     theta_max = phi0;
     st = xs_get(xb,'bcfile.s');             % use max s to setup wavedir grid
 
     % make directional distribution as proposed by Longuet_Higgins et al.
     % (1963)
-    % figure(33);
+    if OPT.plot
+        figure(333);hold on;grid on;box on
+    end
     for i = 1:length(st)
         m = 2*st(i);
         phi = phi0t(i)-180:1:phi0t(i)+180;
@@ -120,9 +118,11 @@ if ismember(xs_get(xb, 'bcfile.type'), {'jonswap' 'jonswap_mtx'})
         % directional spreading as defined by Kuik et al, 1988
         sig(i) = sqrt(2/(st(i)+1))/2/pi*360;
 
-    %     plot(phi,p,'b'); hold on;
-    %     plot(phi(indmin),p(indmin),'r*');
-    %     plot(phi(indmax),p(indmax),'r*');
+        if OPT.plot
+            plot(phi,p,'b'); hold on;
+            plot(phi(indmin),p(indmin),'r*');
+            plot(phi(indmax),p(indmax),'r*');
+        end
 
     end
 
@@ -138,8 +138,10 @@ if ismember(xs_get(xb, 'bcfile.type'), {'jonswap' 'jonswap_mtx'})
         thata_max = max(theta_max, OPT.normal-alpha+safety);
     end
 
-    % plot([theta_min theta_min; theta_max theta_max]',[0 1; 0 1;]','r--o','LineWidth',1.5,'MarkerSize',8);
-    % plot([270-alpha 270-alpha],[0 1],'g--o','LineWidth',1.5,'MarkerSize',8); grid on;
+    if OPT.plot
+        plot([theta_min theta_min; theta_max theta_max]',[0 1; 0 1;]','r--o','LineWidth',1.5,'MarkerSize',8);
+        plot([270-alpha 270-alpha],[0 1],'g--o','LineWidth',1.5,'MarkerSize',8); grid on;
+    end
 
     % combine range p> OPT.varthr and directional range from wave bc time series
     dthetasum = theta_max-theta_min;
@@ -156,7 +158,9 @@ if ismember(xs_get(xb, 'bcfile.type'), {'jonswap' 'jonswap_mtx'})
     % get what we need as output
     xb = xs_set(xb,'thetamin',theta_min,'thetamax',theta_max','dtheta',dtheta,'thetanaut',1);
 
-    % plot(thetagr,zeros(1,length(thetagr)),'g-s'); grid on;
+    if OPT.plot
+        plot(thetagr,zeros(1,length(thetagr)),'g-s'); grid on;
+    end
 
     xb_verbose(1,'Settings');
     xb_verbose(2,{'thetamin','thetamax','dtheta','thetanaut'},{theta_min,theta_max',dtheta,1});
