de8ab17458aa20beb1f072ea9d4910f77c22b3d1
pijl <pijl@localhost>
2013-08-19 07:14:59 +0000

    *matlab/dflowfm/interpolate_on_polygon: fix
    svn path=/trunk/matlab/; revision=9076
    
    
    Former-commit-id: 407e61ef959a1731ef64b76e330a60b1ba1a3cca

--- applications/delft3d/+dflowfm/interpolate_on_polygon.m
+++ applications/delft3d/+dflowfm/interpolate_on_polygon.m
@@ -2,7 +2,7 @@ function polout = interpolate_on_polygon(Gin,Din,pol,idmn)
 % interpolate flow data on polygon
 
 if ( nargin<4)
-    idmn=-1
+    idmn=-1;
 end
 
 pol.x = reshape(pol.x,1,[]);
@@ -32,8 +32,10 @@ for idmnp1=1:length(Gin)    % idmn + 1
     idxAtoL=G.cen.Link(1,:)<=G.cen.n & G.cen.Link(2,:)<=G.cen.n;
     idxAtoL=find(idxAtoL);
     if ( isfield(G.cen,'idmn') & idmn>-1 )
-        idxAtoL=idxAtoL((G.cen.idmn(G.cen.Link(1,idxAtoL))==idmn) &    ...
-                        (G.cen.idmn(G.cen.Link(2,idxAtoL))==idmn));
+%         idxAtoL=idxAtoL(min(G.cen.idmn(G.cen.Link(1,idxAtoL)),    ...
+%                             G.cen.idmn(G.cen.Link(2,idxAtoL)))==idmn);
+        idxAtoL=idxAtoL(G.cen.idmn(G.cen.Link(1,idxAtoL)) == idmn |   ...
+                        G.cen.idmn(G.cen.Link(2,idxAtoL)) == idmn);
     end
 
 
