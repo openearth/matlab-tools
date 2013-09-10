02f3e7e2ee0e45c3f37b6bba14d7579d63ae66c2
kaaij <kaaij@localhost>
2013-09-10 18:23:56 +0000

    svn path=/trunk/matlab/; revision=9208
    Former-commit-id: af5ea0faba2795ace8c49c28100a92a7330f0aa0

--- applications/delft3d/nest_matlab/simona2mdu/simona2mdu_thd.m
+++ applications/delft3d/nest_matlab/simona2mdu/simona2mdu_thd.m
@@ -1,4 +1,21 @@
-function simona2mdu (mdf)
+function simona2mdu_thd(mdf)
+
+% siminp2mdu_thd : Writes drypoints and thin dams to unstruc input files
+
+filgrd = [mdf.pathd3d filesep mdf.filcco];
+fildry = [mdf.pathd3d filesep mdf.fildry];
+filthd = [mdf.pathd3d filesep mdf.filthd];
+
+% Open and read the files
+
+grid = delft3d_io_grd('open',filgrd);
+xcoor = grid.cor.x;
+ycoor = grid.cor.y;
+
+MNdry = delft3d_io_grd('open',fildry);
+
+MNthd = delft3d_io_thd('open',filthd);
+
 
 
 
