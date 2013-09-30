0a0a788bcfac19202ef7870c60bed54b96b99519
ormondt <ormondt@localhost>
2013-09-30 14:29:34 +0000

    dflowfm nesting update
    svn path=/trunk/matlab/; revision=9298
    
    
    Former-commit-id: 98f3e687667872b357ba53916a7bcdb862dfc362

---
+++ applications/DelftDashBoard/toolboxes/Nesting/ddb_NestingToolbox_DFlowFM_nest1.m
@@ -0,0 +1,204 @@
+function ddb_NestingToolbox_DFlowFM_nestHD1(varargin)
+%DDB_NESTINGTOOLBOX_NESTHD1  One line description goes here.
+%
+%   More detailed description goes here.
+%
+%   Syntax:
+%   ddb_NestingToolbox_nestHD1(varargin)
+%
+%   Input:
+%   varargin =
+%
+%
+%
+%
+%   Example
+%   ddb_NestingToolbox_nestHD1
+%
+%   See also
+
+%% Copyright notice
+%   --------------------------------------------------------------------
+%   Copyright (C) 2011 Deltares
+%       Maarten van Ormondt
+%
+%       Maarten.vanOrmondt@deltares.nl
+%
+%       P.O. Box 177
+%       2600 MH Delft
+%       The Netherlands
+%
+%   This library is free software: you can redistribute it and/or modify
+%   it under the terms of the GNU General Public License as published by
+%   the Free Software Foundation, either version 3 of the License, or
+%   (at your option) any later version.
+%
+%   This library is distributed in the hope that it will be useful,
+%   but WITHOUT ANY WARRANTY; without even the implied warranty of
+%   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%   GNU General Public License for more details.
+%
+%   You should have received a copy of the GNU General Public License
+%   along with this library.  If not, see <http://www.gnu.org/licenses/>.
+%   --------------------------------------------------------------------
+
+% This tool is part of <a href="http://www.OpenEarth.eu">OpenEarthTools</a>.
+% OpenEarthTools is an online collaboration to share and manage data and
+% programming tools in an open source, version controlled environment.
+% Sign up to recieve regular updates of this function, and to contribute
+% your own tools.
+
+%% Version <http://svnbook.red-bean.com/en/1.5/svn.advanced.props.special.keywords.html>
+% Created: 02 Dec 2011
+% Created with Matlab version: 7.11.0.584 (R2010b)
+
+% $Id$
+% $Date$
+% $Author$
+% $Revision$
+% $HeadURL$
+% $Keywords: $
+
+%%
+if isempty(varargin)
+    % New tab selected
+    ddb_zoomOff;
+    ddb_refreshScreen;
+    setInstructions({'','Click Make Observation Points in order to generate observation points in the overall grid', ...
+                'The overall model domain must be selected!'});
+else
+    %Options selected
+    opt=lower(varargin{1});
+    switch opt
+        case{'nesthd1'}
+            nestHD1;
+        case{'selectcs'}
+            selectCS;
+    end
+end
+
+%%
+function nestHD1
+
+handles=getHandles;
+
+switch handles.Toolbox(tb).Input.detailmodeltype
+
+    case{'delft3dflow'}
+        
+        if isempty(handles.Toolbox(tb).Input.grdFile)
+            ddb_giveWarning('text','Please first load grid file of nested model!');
+            return
+        end
+        
+        if isempty(handles.Toolbox(tb).Input.encFile)
+            ddb_giveWarning('text','Please first load enclosure file of nested model!');
+            return
+        end
+        
+        if isempty(handles.Toolbox(tb).Input.bndFile)
+            ddb_giveWarning('text','Please first load boundary file of nested model!');
+            return
+        end
+        
+        if isempty(handles.Model(md).Input(ad).gridX)
+            ddb_giveWarning('text','Please first load or create model grid!');
+            return
+        end
+        
+        fid=fopen('nesthd1.inp','wt');
+        fprintf(fid,'%s\n',handles.Model(md).Input(ad).grdFile);
+        fprintf(fid,'%s\n',handles.Model(md).Input(ad).encFile);
+        fprintf(fid,'%s\n',handles.Toolbox(tb).Input.grdFile);
+        fprintf(fid,'%s\n',handles.Toolbox(tb).Input.encFile);
+        fprintf(fid,'%s\n',handles.Toolbox(tb).Input.bndFile);
+        fprintf(fid,'%s\n',handles.Toolbox(tb).Input.admFile);
+        fprintf(fid,'%s\n','ddtemp.obs');
+        fclose(fid);
+        
+        system(['"' handles.Toolbox(tb).dataDir 'nesthd1" < nesthd1.inp']);
+        
+        [name,m,n] = textread('ddtemp.obs','%21c%f%f');
+        
+        k=handles.Model(md).Input(ad).nrObservationPoints;
+        for i=1:length(m)
+            % Check if observation point already exists
+            nm=deblank(name(i,:));
+            ii=strmatch(nm,handles.Model(md).Input(ad).observationPointNames,'exact');
+            if isempty(ii)
+                % Observation point does not yet exist
+                k=k+1;
+                handles.Model(md).Input(ad).observationPoints(k).name=nm;
+                handles.Model(md).Input(ad).observationPoints(k).M=m(i);
+                handles.Model(md).Input(ad).observationPoints(k).N=n(i);
+                handles.Model(md).Input(ad).observationPoints(k).x=handles.Model(md).Input(ad).gridXZ(m(i),n(i));
+                handles.Model(md).Input(ad).observationPoints(k).y=handles.Model(md).Input(ad).gridYZ(m(i),n(i));
+                handles.Model(md).Input(ad).observationPointNames{k}=handles.Model(md).Input(ad).observationPoints(k).name;
+            end
+        end
+        delete('nesthd1.inp');
+        try
+            delete('ddtemp.obs');
+        end
+        handles.Model(md).Input(ad).nrObservationPoints=length(handles.Model(md).Input(ad).observationPoints);
+        
+        handles=ddb_Delft3DFLOW_plotAttributes(handles,'plot','observationpoints','domain',ad,'visible',1,'active',0);
+        
+        setHandles(handles);
+        
+    case{'dflowfm'}
+
+        if isempty(handles.Toolbox(tb).Input.extfile)
+            ddb_giveWarning('text','Please first load external forcing file of nested model!');
+            return
+        end
+        
+        cs.name=handles.Toolbox(tb).Input.detailmodelcsname;
+        cs.type=handles.Toolbox(tb).Input.detailmodelcstype;
+
+        newpoints=ddb_nesthd1_dflowfm_in_delft3dflow('admfile',handles.Toolbox(tb).Input.admFile,'extfile',handles.Toolbox(tb).Input.extfile, ...
+            'grdfile',handles.Model(md).Input(ad).grdFile,'encfile',handles.Model(md).Input(ad).encFile, ...
+            'csoverall',handles.screenParameters.coordinateSystem,'csdetail',cs);        
+        
+        if ~isempty(newpoints)            
+            k=handles.Model(md).Input(ad).nrObservationPoints;
+            for ip=1:length(newpoints)
+                % Check if observation point already exists
+                nm=deblank(newpoints(ip).name);
+                ii=strmatch(nm,handles.Model(md).Input(ad).observationPointNames,'exact');
+                if isempty(ii)
+                    % Observation point does not yet exist
+                    k=k+1;
+                    handles.Model(md).Input(ad).observationPoints(k).name=nm;
+                    handles.Model(md).Input(ad).observationPoints(k).M=newpoints(ip).m;
+                    handles.Model(md).Input(ad).observationPoints(k).N=newpoints(ip).n;
+                    handles.Model(md).Input(ad).observationPoints(k).x=handles.Model(md).Input(ad).gridXZ(newpoints(ip).m,newpoints(ip).n);
+                    handles.Model(md).Input(ad).observationPoints(k).y=handles.Model(md).Input(ad).gridYZ(newpoints(ip).m,newpoints(ip).n);
+                    handles.Model(md).Input(ad).observationPointNames{k}=handles.Model(md).Input(ad).observationPoints(k).name;
+                end
+            end
+            handles.Model(md).Input(ad).activeObservationPoint=1;
+            handles.Model(md).Input(ad).nrObservationPoints=k;
+            handles=ddb_Delft3DFLOW_plotAttributes(handles,'plot','observationpoints','domain',ad,'visible',1,'active',0);        
+            setHandles(handles);
+        end
+        
+end
+
+%%
+function selectCS
+
+handles=getHandles;
+
+% Open GUI to select data set
+
+[cs,type,nr,ok]=ddb_selectCoordinateSystem(handles.coordinateData,handles.EPSG,'default','WGS 84','type','both','defaulttype','geographic');
+
+if ok
+    handles.Toolbox(tb).Input.detailmodelcsname=cs;
+    handles.Toolbox(tb).Input.detailmodelcstype=type;    
+    setHandles(handles);
+end
+
+gui_updateActiveTab;
+
---
+++ applications/DelftDashBoard/toolboxes/Nesting/ddb_NestingToolbox_DFlowFM_nest2.m
@@ -0,0 +1,257 @@
+function ddb_NestingToolbox_DFlowFM_nest2(varargin)
+%DDB_NESTINGTOOLBOX_NESTHD2  One line description goes here.
+%
+%   More detailed description goes here.
+%
+%   Syntax:
+%   ddb_NestingToolbox_nestHD2(varargin)
+%
+%   Input:
+%   varargin =
+%
+%
+%
+%
+%   Example
+%   ddb_NestingToolbox_nestHD2
+%
+%   See also
+
+%% Copyright notice
+%   --------------------------------------------------------------------
+%   Copyright (C) 2011 Deltares
+%       Maarten van Ormondt
+%
+%       Maarten.vanOrmondt@deltares.nl
+%
+%       P.O. Box 177
+%       2600 MH Delft
+%       The Netherlands
+%
+%   This library is free software: you can redistribute it and/or modify
+%   it under the terms of the GNU General Public License as published by
+%   the Free Software Foundation, either version 3 of the License, or
+%   (at your option) any later version.
+%
+%   This library is distributed in the hope that it will be useful,
+%   but WITHOUT ANY WARRANTY; without even the implied warranty of
+%   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%   GNU General Public License for more details.
+%
+%   You should have received a copy of the GNU General Public License
+%   along with this library.  If not, see <http://www.gnu.org/licenses/>.
+%   --------------------------------------------------------------------
+
+% This tool is part of <a href="http://www.OpenEarth.eu">OpenEarthTools</a>.
+% OpenEarthTools is an online collaboration to share and manage data and
+% programming tools in an open source, version controlled environment.
+% Sign up to recieve regular updates of this function, and to contribute
+% your own tools.
+
+%% Version <http://svnbook.red-bean.com/en/1.5/svn.advanced.props.special.keywords.html>
+% Created: 02 Dec 2011
+% Created with Matlab version: 7.11.0.584 (R2010b)
+
+% $Id$
+% $Date$
+% $Author$
+% $Revision$
+% $HeadURL$
+% $Keywords: $
+
+%%
+if isempty(varargin)
+    % New tab selected
+    ddb_zoomOff;
+    ddb_refreshScreen;
+    setInstructions({'Click Run Nesting in order to generate boundary conditions for the nested model', ...
+        'The overall model simulation must be finished and a history file must be present','The nested model domain must be selected!'});
+else
+    %Options selected
+    opt=lower(varargin{1});
+    switch opt
+        case{'nesthd2'}
+            nestHD2;
+    end
+end
+
+%%
+function nestHD2
+
+handles=getHandles;
+
+if isempty(handles.Toolbox(tb).Input.trihFile)
+    ddb_giveWarning('text','Please first load history file of overall model!');
+    return
+end
+
+hisfile=handles.Toolbox(tb).Input.trihFile;
+nestadm=handles.Toolbox(tb).Input.admFile;
+z0=handles.Toolbox(tb).Input.zCor;
+opt='';
+if handles.Toolbox(tb).Input.nestHydro && handles.Toolbox(tb).Input.nestTransport
+    opt='both';
+elseif handles.Toolbox(tb).Input.nestHydro
+    opt='hydro';
+elseif handles.Toolbox(tb).Input.nestTransport
+    opt='transport';
+end
+stride=1;
+
+if ~isempty(opt)
+    
+    % Make structure info for nesthd2
+%    bnd=handles.Model(md).Input(ad).openBoundaries;
+    bnd=handles.Model(md).Input(ad).boundaries;
+    
+    % Vertical grid info
+%     vertGrid.KMax=handles.Model(md).Input(ad).KMax;
+%     vertGrid.layerType=handles.Model(md).Input(ad).layerType;
+%     vertGrid.thick=handles.Model(md).Input(ad).thick;
+%     vertGrid.zTop=handles.Model(md).Input(ad).zTop;
+%     vertGrid.zBot=handles.Model(md).Input(ad).zBot;
+
+    vertGrid=[];
+
+    % Run Nesthd2
+    cs=handles.screenParameters.coordinateSystem.type;
+    bnd=nesthd2_new('input',handles.Model(md).Input,'openboundaries',bnd,'vertgrid',vertGrid,'hisfile',hisfile, ...
+        'admfile',nestadm,'zcor',z0,'stride',stride,'opt',opt,'coordinatesystem',cs,'save','n');
+    
+    handles.Model(md).Input(ad).boundaries=bnd;
+
+%     zersunif=zeros(2,1);
+%     
+%     for i=1:length(bnd)
+%         
+%         if strcmpi(bnd(i).forcing,'T')
+%             
+%             if handles.Toolbox(tb).Input.nestHydro
+%                 % Copy boundary data
+%                 % Hydrodynamics
+%                 handles.Model(md).Input(ad).openBoundaries(i).nrTimeSeries=length(bnd(i).timeSeriesT);
+%                 handles.Model(md).Input(ad).openBoundaries(i).timeSeriesT=bnd(i).timeSeriesT;
+%                 handles.Model(md).Input(ad).openBoundaries(i).timeSeriesA=bnd(i).timeSeriesA;
+%                 handles.Model(md).Input(ad).openBoundaries(i).timeSeriesB=bnd(i).timeSeriesB;
+%                 handles.Model(md).Input(ad).openBoundaries(i).timeSeriesAV=bnd(i).timeSeriesAV;
+%                 handles.Model(md).Input(ad).openBoundaries(i).timeSeriesBV=bnd(i).timeSeriesBV;
+%                 handles.Model(md).Input(ad).openBoundaries(i).profile=bnd(i).profile;
+%             end
+%             
+%             if handles.Toolbox(tb).Input.nestTransport
+%                 % Transport
+%                 
+%                 % Salinity
+%                 
+%                 handles.Model(md).Input(ad).openBoundaries(i).salinity.nrTimeSeries=2;
+%                 handles.Model(md).Input(ad).openBoundaries(i).salinity.timeSeriesT=[handles.Model(md).Input(ad).startTime handles.Model(md).Input(ad).stopTime];
+%                 
+%                 handles.Model(md).Input(ad).openBoundaries(i).salinity.profile='uniform';
+%                 handles.Model(md).Input(ad).openBoundaries(i).salinity.timeSeriesA=zersunif+handles.Model(md).Input(ad).salinity.ICConst;
+%                 handles.Model(md).Input(ad).openBoundaries(i).salinity.timeSeriesB=zersunif+handles.Model(md).Input(ad).salinity.ICConst;
+%                 
+%                 if handles.Model(md).Input(ad).salinity.include
+%                     if isfield(bnd(i),'salinity')
+%                         handles.Model(md).Input(ad).openBoundaries(i).salinity.nrTimeSeries=length(bnd(i).salinity.timeSeriesT);
+%                         handles.Model(md).Input(ad).openBoundaries(i).salinity.timeSeriesT=bnd(i).salinity.timeSeriesT;
+%                         handles.Model(md).Input(ad).openBoundaries(i).salinity.timeSeriesA=bnd(i).salinity.timeSeriesA;
+%                         handles.Model(md).Input(ad).openBoundaries(i).salinity.timeSeriesB=bnd(i).salinity.timeSeriesB;
+%                         handles.Model(md).Input(ad).openBoundaries(i).salinity.profile=bnd(i).salinity.profile;
+%                     end
+%                 end
+%                 
+%                 % Temperature
+%                 handles.Model(md).Input(ad).openBoundaries(i).temperature.nrTimeSeries=2;
+%                 handles.Model(md).Input(ad).openBoundaries(i).temperature.timeSeriesT=[handles.Model(md).Input(ad).startTime handles.Model(md).Input(ad).stopTime];
+%                 
+%                 handles.Model(md).Input(ad).openBoundaries(i).temperature.profile='uniform';
+%                 handles.Model(md).Input(ad).openBoundaries(i).temperature.timeSeriesA=zersunif+handles.Model(md).Input(ad).temperature.ICConst;
+%                 handles.Model(md).Input(ad).openBoundaries(i).temperature.timeSeriesB=zersunif+handles.Model(md).Input(ad).temperature.ICConst;
+%                 
+%                 if handles.Model(md).Input(ad).temperature.include
+%                     if isfield(bnd(i),'temperature')
+%                         handles.Model(md).Input(ad).openBoundaries(i).temperature.nrTimeSeries=length(bnd(i).temperature.timeSeriesT);
+%                         handles.Model(md).Input(ad).openBoundaries(i).temperature.timeSeriesT=bnd(i).temperature.timeSeriesT;
+%                         handles.Model(md).Input(ad).openBoundaries(i).temperature.timeSeriesA=bnd(i).temperature.timeSeriesA;
+%                         handles.Model(md).Input(ad).openBoundaries(i).temperature.timeSeriesB=bnd(i).temperature.timeSeriesB;
+%                         handles.Model(md).Input(ad).openBoundaries(i).temperature.profile=bnd(i).temperature.profile;
+%                     end
+%                 end
+%                 
+%                 % Tracers
+%                 for j=1:handles.Model(md).Input(ad).nrTracers
+%                     handles.Model(md).Input(ad).openBoundaries(i).tracer(j).nrTimeSeries=2;
+%                     handles.Model(md).Input(ad).openBoundaries(i).tracer(j).timeSeriesT=[handles.Model(md).Input(ad).startTime handles.Model(md).Input(ad).stopTime];
+%                     
+%                     handles.Model(md).Input(ad).openBoundaries(i).tracer(j).profile='uniform';
+%                     handles.Model(md).Input(ad).openBoundaries(i).tracer(j).timeSeriesA=zersunif;
+%                     handles.Model(md).Input(ad).openBoundaries(i).tracer(j).timeSeriesB=zersunif;
+%                     
+%                     if isfield(bnd(i),'tracer')
+%                         if length(bnd(i).tracer)<=j
+%                             handles.Model(md).Input(ad).openBoundaries(i).tracer(j).nrTimeSeries=length(bnd(i).tracer(j).timeSeriesT);
+%                             handles.Model(md).Input(ad).openBoundaries(i).tracer(j).timeSeriesT=bnd(i).tracer(j).timeSeriesT;
+%                             handles.Model(md).Input(ad).openBoundaries(i).tracer(j).timeSeriesA=bnd(i).tracer(j).timeSeriesA;
+%                             handles.Model(md).Input(ad).openBoundaries(i).tracer(j).timeSeriesB=bnd(i).tracer(j).timeSeriesB;
+%                             handles.Model(md).Input(ad).openBoundaries(i).tracer(j).profile=bnd(i).tracer(j).profile;
+%                         end
+%                     end
+%                 end
+%                 
+%                 % Sediments
+%                 for j=1:handles.Model(md).Input(ad).nrSediments
+%                     
+%                     handles.Model(md).Input(ad).openBoundaries(i).sediment(j).nrTimeSeries=2;
+%                     handles.Model(md).Input(ad).openBoundaries(i).sediment(j).timeSeriesT=[handles.Model(md).Input(ad).startTime handles.Model(md).Input(ad).stopTime];
+%                     
+%                     handles.Model(md).Input(ad).openBoundaries(i).sediment(j).profile='uniform';
+%                     handles.Model(md).Input(ad).openBoundaries(i).sediment(j).timeSeriesA=zersunif;
+%                     handles.Model(md).Input(ad).openBoundaries(i).sediment(j).timeSeriesB=zersunif;
+%                     
+%                     if isfield(bnd(i),'sediment')
+%                         if length(bnd(i).sediment)<=j
+%                             handles.Model(md).Input(ad).openBoundaries(i).sediment(j).nrTimeSeries=length(bnd(i).sediment(j).timeSeriesT);
+%                             handles.Model(md).Input(ad).openBoundaries(i).sediment(j).timeSeriesT=bnd(i).sediment(j).timeSeriesT;
+%                             handles.Model(md).Input(ad).openBoundaries(i).sediment(j).timeSeriesA=bnd(i).sediment(j).timeSeriesA;
+%                             handles.Model(md).Input(ad).openBoundaries(i).sediment(j).timeSeriesB=bnd(i).sediment(j).timeSeriesB;
+%                             handles.Model(md).Input(ad).openBoundaries(i).sediment(j).profile=bnd(i).sediment(j).profile;
+%                         end
+%                     end
+%                 end
+%             end
+%             
+%         end
+%         
+%     end
+    
+    
+%     if handles.Toolbox(tb).Input.nestHydro
+%         [filename, pathname, filterindex] = uiputfile('*.bct','Select Timeseries Conditions File');
+%         if pathname~=0
+%             curdir=[lower(cd) '\'];
+%             if ~strcmpi(curdir,pathname)
+%                 filename=[pathname filename];
+%             end
+%             handles.Model(md).Input(ad).bctFile=filename;
+%             ddb_saveBctFile(handles,ad);
+%         end
+%     end
+%     
+%     
+%     if handles.Toolbox(tb).Input.nestTransport
+%         [filename, pathname, filterindex] = uiputfile('*.bcc','Select Transport Conditions File');
+%         if pathname~=0
+%             curdir=[lower(cd) '\'];
+%             if ~strcmpi(curdir,pathname)
+%                 filename=[pathname filename];
+%             end
+%             handles.Model(md).Input(ad).bccFile=filename;
+%             ddb_saveBccFile(handles,ad);
+%         end
+%     end
+    
+    
+end
+
+setHandles(handles);
+
