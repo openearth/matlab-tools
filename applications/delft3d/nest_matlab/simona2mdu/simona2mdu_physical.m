64e264406f18c99bfbc2299b22bc2c0ee54b3f11
kaaij <kaaij@localhost>
2013-10-06 17:39:53 +0000

    svn path=/trunk/matlab/; revision=9341
    Former-commit-id: e78a1bc5159c333a216d64e44a1a937376453fe3

--- applications/delft3d/nest_matlab/simona2mdu/simona2mdu_physical.m
+++
@@ -1,7 +0,0 @@
-function mdu = simona2mdu_physical(mdf, mdu, ~)
-
-% siminp2mdu_physical : Get physical information out of the mdf structure and set in the mdu structure
-
-mdu.physics.Ag      = mdf.ag;
-mdu.physics.Rhomean = mdf.rhow;
-if strcmpi(mdf.sub1(1),'S') mdu.physics.Salinity = true; end
