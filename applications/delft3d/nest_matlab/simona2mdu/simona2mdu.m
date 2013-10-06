e8187adbf356828f5845c5b13b2aa38df3ae8c03
kaaij <kaaij@localhost>
2013-10-06 17:35:28 +0000

    svn path=/trunk/matlab/; revision=9340
    Former-commit-id: 028834c74efce29816fca2d80ebdc32a5e3587d6

--- applications/delft3d/nest_matlab/simona2mdu/simona2mdu.m
+++ applications/delft3d/nest_matlab/simona2mdu/simona2mdu.m
@@ -14,6 +14,11 @@ Gen_inf    = {'This tool converts a SIMONA siminp file into an Unstruc mdu file'
               'This tool does a basic first conversion but please check carefully'                                 ;
               '(USE AT OWN RISK)'                                                                                  ;
               ' '                                                                                                  ;
+              'Firstly, the SIMINP file is converted to a temporary Delft3D-Flow mdf file'                         ;
+              'Secondly, the mdf file is converted into an unstruc mdu file  '                                     ;
+              'Finally, the  temporary mdf file is removed'                                                        ;
+              'If you want to keep the mdf file, use SIMONA2MDF and MDF2MDU seperately'                            ;
+              ' '                                                                                                  ;
               'If you encounter problems, please do not hesitate to contact me'                                    ;                                                                                   ;
               'Theo.vanderkaaij@deltares.nl'                                                                      };
 
@@ -48,79 +53,17 @@ name_mdu = [path_mdu filesep name_mdu];
 
 path_mdf = [path_mdu filesep 'TMP'];
 if ~isdir(path_mdf); mkdir(path_mdf);end
-name_mdf = [path_mdf filesep 'tmp'];
+name_mdf = [path_mdf filesep 'tmp.mdf'];
 
 %% Display the general information
 
 logo = imread([getenv('nesthd_path') filesep 'bin' filesep 'dflowfm.jpg']);
-simona2mdf_message(Gen_inf,'Logo',logo,'n_sec',5,'Window','SIMONA2MDU Message','Close',true);
+simona2mdf_message(Gen_inf,'Logo',logo,'n_sec',10,'Window','SIMONA2MDU Message','Close',true);
 
 %% Convert the Simona siminp file to a temporary mdf file
 
-%simona2mdf (filwaq,[name_mdf '.mdf']);
+%simona2mdf (filwaq,name_mdf,'DispGen',false);
 
-%% Start with creating empty mdu template
+%% Convert the Delft3D-Flow mdf file to an unstruc mdu file
 
-mdu = unstruc_io_mdu('new',[getenv('nesthd_path') filesep 'bin' filesep 'dflowfm-properties.csv']);
-
-%% Read the temporary mdf file, add the path of the d3d files to allow for reading later
-
-tmp            = delft3d_io_mdf('read',[name_mdf '.mdf']);
-mdf            = tmp.keywords;
-mdf.pathd3d    = path_mdf;
-
-%% Generate the net file from the area information
-
-simona2mdf_message('Generating the Net file'                ,'Logo',logo,'Window','SIMONA2MDU Message');
-simona2mdu_grd2net([path_mdf filesep mdf.filcco],[path_mdf filesep mdf.fildep],name_mdu);
-mdu.geometry.NetFile = [name_mdu '_net.nc'];
-mdu.geometry.NetFile = simona2mdf_rmpath(mdu.geometry.NetFile);
-
-%% Generate unstruc additional files and fill the mdu structure
-simona2mdf_message('Generating UNSTRUC Area              information','Logo',logo,'Window','SIMONA2MDU Message');
-mdu = simona2mdu_area   (mdf,mdu,name_mdu);
-
-simona2mdf_message('Generating UNSTRUC Thin Dam          information','Logo',logo,'Window','SIMONA2MDU Message');
-mdu = simona2mdu_thd    (mdf,mdu,name_mdu);
-
-simona2mdf_message('Generating UNSTRUC TIMES             information','Logo',logo,'Window','SIMONA2MDU Message');
-mdu = simona2mdu_times    (mdf,mdu,name_mdu);
-
-simona2mdf_message('Generating UNSTRUC PHYSICAL          information','Logo',logo,'Window','SIMONA2MDU Message');
-mdu = simona2mdu_physical (mdf,mdu,name_mdu);
-
-simona2mdf_message('Generating UNSTRUC NUMERICAL         information','Logo',logo,'Window','SIMONA2MDU Message');
-mdu = simona2mdu_numerical(mdf,mdu,name_mdu);
-
-simona2mdf_message('Generating UNSTRUC Boundary definition           ','Logo',logo,'Window','SIMONA2MDU Message');
-mdu.Filbnd = simona2mdu_bnd2pli([path_mdf filesep mdf.filcco],[path_mdf filesep mdf.filbnd],name_mdu);
-
-if mdu.physics.Salinity    ; % Salinity, write _sal pli files
-    tmp = simona2mdu_bnd2pli([path_mdf filesep mdf.filcco],[path_mdf filesep mdf.filbnd],name_mdu,'Salinity',true);
-    mdu.Filbnd = [mdu.Filbnd tmp];
-end
-
-simona2mdf_message('Generating UNSTRUC Initial Condition information','Logo',logo,'Window','SIMONA2MDU Message');
-mdu = simona2mdu_initial  (mdf,mdu,name_mdu);
-
-simona2mdf_message('Generating UNSTRUC Friction          information','Logo',logo,'Window','SIMONA2MDU Message');
-mdu = simona2mdu_friction (mdf,mdu,name_mdu);
-
-simona2mdf_message('Generating UNSTRUC Viscosity/diff.   information','Logo',logo,'Window','SIMONA2MDU Message');
-mdu = simona2mdu_viscosity(mdf,mdu,name_mdu);
-
-simona2mdf_message('Generating External forcing file                ','Logo',logo,'Window','SIMONA2MDU Message');
-mdu = simona2mdu_genext   (name_mdu,'mdu',mdu,'Filbnd' ,mdu.Filbnd ,'Filini' ,mdu.Filini ,'Filrgh',mdu.Filrgh  ,  ...
-                                              'Filvico',mdu.Filvico,'Fildico',mdu.Fildico                      );
-simona2mdf_message('Generating UNSTRUC STATION           information','Logo',logo,'Window','SIMONA2MDU Message');
-mdu = simona2mdu_obs      (mdf,mdu,name_mdu);
-
-simona2mdf_message('Generating UNSTRUC CROSS-SECTION     information','Logo',logo,'Window','SIMONA2MDU Message');
-mdu = simona2mdu_crs      (mdf,mdu,name_mdu);
-
-%% Finally,  write the mdu file and close everything
-
-unstruc_io_mdu('write',[name_mdu '.mdu'],mdu);
-
-simona2mdf_message('','Window','SIMONA2MDU Message','Close',true,'n_sec',0);
-
+d3d2mdu (name_mdf,name_mdu,'DispGen',false);
