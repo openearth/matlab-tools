f599a260b457d26ee65ed2dd1a0e9f0911813fe3
kaaij <kaaij@localhost>
2013-10-15 08:04:26 +0000

    svn path=/trunk/matlab/; revision=9409
    Former-commit-id: 4efbf6aaeb4b338f812d9522fc2ea06d712e12e8

--- applications/salinity_conversion/compile.bat
+++ applications/salinity_conversion/compile.bat
@@ -1,5 +1,2 @@
 del saco*.exe
-n:\Applications\Matlab\MATLAB2012a\bin\matlab    -nosplash -nodesktop -wait -r compile_saco
-ren saco.exe saco_w32.exe
-n:\Applications\Matlab\MATLAB2012a_64\bin\matlab -nosplash -nodesktop -wait -r compile_saco
-ren saco.exe saco_w64.exe
+"c:\Program Files (x86)\MATLAB\MATLAB2012b\bin\matlab"   -nosplash -nodesktop -wait -r compile_saco
