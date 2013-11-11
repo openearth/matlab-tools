293614e669bcef902f483b624ab294aa8c212c78
kaaij <kaaij@localhost>
2013-11-11 16:27:26 +0000

    svn path=/trunk/matlab/; revision=9647
    Former-commit-id: 9bf5224c9894a94ef8924d79bed253a2a750630b

--- applications/delft3d/nest_matlab/d3d2dflowfm/d3d2dflowfm_viscosity.m
+++ applications/delft3d/nest_matlab/d3d2dflowfm/d3d2dflowfm_viscosity.m
@@ -2,56 +2,25 @@ function mdu = d3d2dflowfm_viscosity(mdf,mdu,name_mdu)
 
 % d3d2dflowfm_viscosity : Writes viscosity/diffusvity information to D-Flow FM input files
 
-filgrd = [mdf.pathd3d filesep mdf.filcco];
 [~,nameshort,~] = fileparts(name_mdu);
 mdu.Filvico     = '';
 mdu.Fildico     = '';
 
 %% If space varying:
 if simona2mdf_fieldandvalue(mdf,'filedy')
-    filedy                   = [mdf.pathd3d filesep mdf.filedy];
     mdu.physics.Vicouv       = -999.999;
     mdu.physics.Dicouv       = -999.999;
     mdu.Filvico              = [nameshort '_vico.xyz'];
-    grid                     = delft3d_io_grd('read',filgrd);
-    mmax                     = grid.mmax;
-    nmax                     = grid.nmax;
-    xcoor_u                  = grid.u_full.x';
-    ycoor_u                  = grid.u_full.y';
-    xcoor_v                  = grid.v_full.x';
-    ycoor_v                  = grid.v_full.y';
+    filgrd                   = [mdf.pathd3d filesep mdf.filcco];
+    filedy                   = [mdf.pathd3d filesep mdf.filedy];
 
-    % read the viscosity/diffusivity values
-    edy        = wldep('read',filedy,[mmax nmax],'multiple');
-
-    % Fill LINE struct with viscosity and diffusivity values
-    no_edy = 1 ;
-    if mdu.physics.Salinity
-        no_edy       = 2;
+    % Derive and write values to file
+    if ~mdu.physics.Salinity
+        unstruc_io_viscosity('write',filgrd,filedy,[name_mdu '_vico.xyz']);
+    else
         mdu.Fildico  = [nameshort '_dico.xyz'];
+        unstruc_io_viscosity('write',filgrd,filedy,[name_mdu '_vico.xyz'],[name_mdu '_dico.xyz']);
     end
-
-    for i_edy = 1: no_edy
-        tmp(i_edy,:,1) = reshape(xcoor_u',mmax*nmax,1);
-        tmp(i_edy,:,2) = reshape(ycoor_u',mmax*nmax,1);
-        tmp(i_edy,:,3) = reshape(edy(i_edy).Data',mmax*nmax,1);
-
-        tmp(i_edy,mmax*nmax+1:2*mmax*nmax,1) = reshape(xcoor_v',mmax*nmax,1);
-        tmp(i_edy,mmax*nmax+1:2*mmax*nmax,2) = reshape(ycoor_v',mmax*nmax,1);
-        tmp(i_edy,mmax*nmax+1:2*mmax*nmax,3) = reshape(edy(i_edy).Data',mmax*nmax,1);
-    end
-
-    nonan = ~isnan(tmp(1,:,1));
-
-    for i_edy = 1: no_edy
-        LINE(i_edy).DATA = num2cell(squeeze(tmp(i_edy,nonan,:)));
-    end
-
-    % write viscosity to file
-    dflowfm_io_xydata('write',[name_mdu '_vico.xyz'],LINE(1));
-
-    % write diffusivity to file
-    if mdu.physics.Salinity dflowfm_io_xydata('write',[name_mdu '_dico.xyz'],LINE(2));end
 else
 
     % Constant values from mdf file
@@ -60,9 +29,9 @@ else
 end
 
 %
-% Fill additonal paameters releated to viscosity
+% Fill additonal parameters releated to viscosity
 %
 mdu.physics.Smagorinsky = 0.0;
 mdu.physics.Elder       = 0;
 mdu.physics.irov        = 0;
-mdu.physics.wall_ks      = -999.999; % not used so make clear in the input
+mdu.physics.wall_ks     = -999.999; % not used so make clear in the input
