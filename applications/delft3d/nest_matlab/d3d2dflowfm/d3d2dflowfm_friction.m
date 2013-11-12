f58d50b14e7a6da8cdc3ca74f5d54e6507ac3a83
kaaij <kaaij@localhost>
2013-11-12 07:39:45 +0000

    svn path=/trunk/matlab/; revision=9655
    Former-commit-id: 7329fbd2e5d9d45966adc36672ff98d41dd4f5cb

--- applications/delft3d/nest_matlab/d3d2dflowfm/d3d2dflowfm_friction.m
+++ applications/delft3d/nest_matlab/d3d2dflowfm/d3d2dflowfm_friction.m
@@ -2,8 +2,6 @@ function mdu = d3d2dflowfm_friction(mdf,mdu, name_mdu)
 
 % d3d2dflwfm_friction: Writes friction information to D-Flow FM input files
 
-filgrd = [mdf.pathd3d filesep mdf.filcco];
-filrgh = [mdf.pathd3d filesep mdf.filrgh];
 [~,nameshort,~] = fileparts(name_mdu);
 mdu.Filrgh      = '';
 
@@ -14,36 +12,13 @@ if strcmpi(mdf.roumet,'w') mdu.physics.UnifFrictType = 2;end
 if strcmpi(mdf.roumet,'z') mdu.physics.UnifFrictType = 3;end
 
 %% Reads roughness values from file
+filgrd = [mdf.pathd3d filesep mdf.filcco];
+filrgh = [mdf.pathd3d filesep mdf.filrgh];
 if ~isempty(filrgh)
     mdu.physics.UnifFrictCoef = -999.999;
     mdu.Filrgh               = [nameshort '_rgh.xyz'];
-    grid                     = delft3d_io_grd('read',filgrd);
-    mmax                     = grid.mmax;
-    nmax                     = grid.nmax;
-    xcoor_u                  = grid.u_full.x';
-    ycoor_u                  = grid.u_full.y';
-    xcoor_v                  = grid.v_full.x';
-    ycoor_v                  = grid.v_full.y';
 
-    % read the roughness values
-    rgh        = wldep('read',filrgh,[mmax nmax],'multiple');
-
-    % Fill LINE struct with roughness values
-    tmp(:,1) = reshape(xcoor_u',mmax*nmax,1);
-    tmp(:,2) = reshape(ycoor_u',mmax*nmax,1);
-    tmp(:,3) = reshape(rgh(1).Data',mmax*nmax,1);
-
-    no_val = size(tmp,1);
-
-    tmp(no_val+1:no_val+mmax*nmax,1) = reshape(xcoor_v',mmax*nmax,1);
-    tmp(no_val+1:no_val+mmax*nmax,2) = reshape(ycoor_v',mmax*nmax,1);
-    tmp(no_val+1:no_val+mmax*nmax,3) = reshape(rgh(2).Data',mmax*nmax,1);
-
-    nonan = ~isnan(tmp(:,1));
-
-    LINE.DATA = num2cell(tmp(nonan,:));
-
-    dflowfm_io_xydata('write',[name_mdu '_rgh.xyz'],LINE);
+    dflowfm_io_roughness('write',filgrd,filrgh,[name_mdu '_rgh.xyz']);
 else
 
     % Constant values from mdf file
