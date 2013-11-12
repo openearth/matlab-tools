2f93433608c99a416ce392b4202ab0ea8bba76c1
kaaij <kaaij@localhost>
2013-11-12 17:43:50 +0000

    svn path=/trunk/matlab/; revision=9665
    Former-commit-id: 5c53d9fe900437c9df0cca73e15dec858ff35640

--- applications/delft3d/nest_matlab/general/nesthd_cmpfiles.m
+++ applications/delft3d/nest_matlab/general/nesthd_cmpfiles.m
@@ -1,4 +1,13 @@
-function nesthd_cmpfiles(files)
+function nesthd_cmpfiles(files,varargin)
+
+opt.Filename = '';
+if ~isempty(varargin)
+    opt = setproperty(opt,varargin);
+end
+
+if ~isempty(opt,Filename)
+    fid = fopen(opt.Filename,'a');
+end
 
 % cmpfiles : compare file in cell array files with original fles (extension .org)
 
@@ -28,7 +37,15 @@ for itest = 1:length(files)
            string = ['New testcase  : ' files{itest}];
            movefile(files{itest},[files{itest} '.org']);
         end
-        disp(string);
+
+        if ~isempty(opt.Filename)
+            fprintf(fid,'%s',string);
+        else
+            disp(string);
+        end
     end
 end
 
+if ~isempty(opt.Filename)
+    fclose (fid);
+end
