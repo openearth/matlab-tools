0a63920a1c2f520fa13146b7119ce18f9536b8ef
baart_f <baart_f@localhost>
2013-11-28 14:20:25 +0000

    WPS Parser
    svn path=/trunk/matlab/; revision=9789
    
    
    Former-commit-id: faaeb58f4c980d7fc0b3fd0c48fb1f6cb718f0bd

--- applications/wps/runner/parseoetwps.m
+++ applications/wps/runner/parseoetwps.m
@@ -1,6 +1,25 @@
-function [match] = parseoetwps(filename)
-    fid = fopen(filename);
-    text = fgetl(fid);
-    match = regexp(text, '(?<function>function)\s*\[\s*(\w+,?\s*)*\]\s*=\s*(?<name>\w+)\s*\(\s*((?<arg>\w+),?\s*)*\)', 'names')
+function [wpsstruct] = parseoetwps(filename)
+    [argout, argin, fcn] = parse_function_call(filename)
     
+    txt = fileread(filename);
+    inputs = struct();
+    for i=1:length(argin)
+        % scan for type of input arguments
+        arg = argin{i};
+        match = regexp(txt, [arg, '\s*\:\s*(?<type>\w+(/\w+)?)'], 'names');
+        inputs.(arg) = match;
+    end
+    outputs = struct();
+    for i=1:length(argout)
+        % scan for type of output arguments
+        arg = argout{i};
+        match = regexp(txt, [arg, '\s*\:\s*(?<type>\w+(/\w+)?)'], 'names');
+        outputs.(arg) = match;
+    end
+    
+    wpsstruct = struct(...
+        'identifier', fcn, ...
+        'inputs', inputs,...
+        'outputs', outputs ...
+    );
 end 
