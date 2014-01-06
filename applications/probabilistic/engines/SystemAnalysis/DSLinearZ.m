db0bedecf283269f28101b6fc583f43f560393e5
dierman <dierman@localhost>
2014-01-06 09:28:02 +0000

    added code for combining linear Z-functions. Method is based on directional sampling.
    svn path=/trunk/matlab/; revision=9937
    
    
    Former-commit-id: 12721a9c1b4144d4b1fe0a350ccf848103579c44

---
+++ applications/probabilistic/engines/SystemAnalysis/DSLinearZ.m
@@ -0,0 +1,82 @@
+function [beta, Pf] = DSLinearZ(alphas, betas, N)
+% function for combining m linear Z-functions, that are functions of n
+% variables u1.. un. Variables are the same for each Z-function, i.e.
+% Zi = betai+alpha_i1*u_1+...+alpha_in*u_n
+% This means correlation between variable ui of function Zj and variable ui of function Zk =1
+% The combining method is directional sampling with an exact (analytical) search
+% function for crossings with Z=0
+
+% assumed form of z-function: Zi = betai+alphai1*u1+...+alphain*un
+% dimensions: m Z-functions and n random variables
+
+% Input
+%   alphas:  alphas of all z-functions (dimension: m * n)
+%   betas:   corresponding betas (dimension: m*1)
+%   N:       number of samples 
+
+% Output
+%   reliability (beta) and failure probability (Pf) of the system
+
+%   --------------------------------------------------------------------
+%   Copyright (C) 2014 Deltares 
+%       F.L.M. Diermanse
+%
+%       Fedrinand.diermanse@Deltares.nl	
+%
+%   This library is free software; you can redistribute it and/or
+%   modify it under the terms of the GNU Lesser General Public
+%   License as published by the Free Software Foundation; either
+%   version 2.1 of the License, or (at your option) any later version.
+%
+%   This library is distributed in the hope that it will be useful,
+%   but WITHOUT ANY WARRANTY; without even the implied warranty of
+%   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+%   Lesser General Public License for more details.
+%
+%   You should have received a copy of the GNU Lesser General Public
+%   License along with this library; if not, write to the Free Software
+%   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+%   USA
+%   or http://www.gnu.org/licenses/licenses.html, http://www.gnu.org/, http://www.fsf.org/
+%   --------------------------------------------------------------------
+
+
+%% sample directions
+% dimensions of alpha
+[~,n]=size(alphas);
+
+% sample N vectors of u-variables;
+U = randn(N,n);
+
+% normalise the N vectors
+% each normalised vector represents a sampled direction
+Norm = sqrt(sum(U.*U,2));
+Unorm = U./repmat(Norm,1,n);
+
+%% compute distance to failure area for each sampled direction
+dist2Fail = -repmat(betas',N,1)./(Unorm*alphas'); % distance per Z-function
+dist2Fail(dist2Fail<0)=inf;  % negative distance means: no failure in current direction
+mindist2Fail = min(dist2Fail,[],2); % minimum over all Z-functions
+
+%% compute failure probability
+Pfdir = 1-chi2_cdf(mindist2Fail.^2,n);  % P(failure | sampled direction)
+Pf = sum(Pfdir)/N;           % mean failure probability over all directions 
+beta = -wnorminv(Pf);        % corresponding beta
+
+% %% compare with MC
+% % compute Crude MC beta
+% Z = repmat(betas',N,1)+(U*alphas');  % Z-values for each u-vector
+% fail = any(Z<0,2);       % for each u-vector: check if Z<0 for any Z-fucntion 
+% PfMC = sum(fail)/N;      % percentage of u-vectors in failure area
+% betaMC = -wnorminv(PfMC);  % corresponding beta
+% 
+% % convergence plot
+% figure; hold on; grid on;
+% plot(1:N,-wnorminv(cumsum(fail')./(1:N)),'r-');
+% plot(1:N,-wnorminv(cumsum(Pfdir')./(1:N)),'b-');
+% legend('crude MC', 'DS');
+% ylabel('beta');
+% xlabel('nr. of samples');
+% set(gca,'xscale','log');
+
+
