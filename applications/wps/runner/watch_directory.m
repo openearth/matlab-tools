5dc7b79073da22a6f74f258cc52fe228ac6222ed
baart_f <baart_f@localhost>
2014-01-11 11:54:19 +0000

    Event generator for directory changes Add delete event
    svn path=/trunk/matlab/; revision=9997
    
    
    Former-commit-id: 22ddc4951c5c8bb8205f006b0ad105db776433c2

--- applications/wps/runner/watch_directory.m
+++ applications/wps/runner/watch_directory.m
@@ -1,23 +1,36 @@
 % keep track of changes to a directory
 function watch_directory(dirname, handle)
     import java.nio.file.*
-    
+
     import java.nio.file.StandardWatchEventKinds.*
     filesystem = FileSystems.getDefault();
     % hack to get path
     dir = filesystem.getPath(dirname, {''});
- 
+
     if ~dir.isAbsolute() 
         warning(['Dirname should be absolute.']);
         warning(['Got: ', dirname, ' which refers to', char(dir.toAbsolutePath())]);
         return
     end
     watcher = filesystem.newWatchService();
-    events = [java.nio.file.StandardWatchEventKinds.ENTRY_CREATE, java.nio.file.StandardWatchEventKinds.ENTRY_MODIFY];
+    events = [...
+            java.nio.file.StandardWatchEventKinds.ENTRY_CREATE,  ...
+            java.nio.file.StandardWatchEventKinds.ENTRY_MODIFY,  ...
+            java.nio.file.StandardWatchEventKinds.ENTRY_DELETE,  ...
+            ];
     key = dir.register(watcher, events);
-    events = watcher.take();
     
-    handle(events);
+    % blocks, add poll with a timeout and timeunits if required
+    keyframe = watcher.take();
 
-    
+    events = keyframe.pollEvents();
+    matevents = struct('kind',[], 'context',[]);
+    for i=1:events.size
+        event = events.get(i-1)
+        matevents(i) = struct('kind', char(event.kind), 'context', char(event.context));
+    end
+
+    handle(matevents);
+
+
 end
