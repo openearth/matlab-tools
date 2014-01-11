8f3ae4a3fde89fc37dad44106418fe6e7887df98
baart_f <baart_f@localhost>
2014-01-11 12:13:04 +0000

    Code documentation
    svn path=/trunk/matlab/; revision=9999
    
    
    Former-commit-id: a536f56891f172343dcd95eccc67746127d9d624

--- applications/wps/runner/watch_directory.m
+++ applications/wps/runner/watch_directory.m
@@ -23,37 +23,53 @@
 % See also: fullfile, ls
 
 function [events] = watch_directory(dirname)
-    import java.nio.file.*
 
+    % Import java classes that we need
+    import java.nio.file.*
     import java.nio.file.StandardWatchEventKinds.*
+    
+    % Filesystem is OS specific
     filesystem = FileSystems.getDefault();
-    % hack to get path
+    
+    % hack to get path (expects 2 arguments, due to java ellipsis)
     dir = filesystem.getPath(dirname, {''});
 
+    % Java does not know about the current dir in matlab, so we always want
+    % an absolute path
     if ~dir.isAbsolute() 
         warning(['Dirname should be absolute.']);
         warning(['Got: ', dirname, ' which refers to', char(dir.toAbsolutePath())]);
         return
     end
+    % Start a watchservice, which can be used for listening
     watcher = filesystem.newWatchService();
+    % subscribe to these events
     events = [...
             java.nio.file.StandardWatchEventKinds.ENTRY_CREATE,  ...
             java.nio.file.StandardWatchEventKinds.ENTRY_MODIFY,  ...
             java.nio.file.StandardWatchEventKinds.ENTRY_DELETE,  ...
             ];
+    % start watching
     key = dir.register(watcher, events);
     
     % blocks, add poll with a timeout and timeunits if required
     keyframe = watcher.take();
 
+    % get the array of events
     events = keyframe.pollEvents();
+    % put them in a matlab struct, can this be done easier???
     matevents = struct('kind',[], 'context',[]);
+    % loop manually
     for i=1:events.size
+        % java is 0 based
         event = events.get(i-1);
+        % and matlab is 1 based, explicitly convert to char 
         matevents(i) = struct('kind', char(event.kind), 'context', char(event.context));
     end
-    
+    % stop listening otherwise the computer slows down (MS virus checker goes
+    % crazy)
     watcher.close();
+    % return the events
     events = matevents;
     
 
