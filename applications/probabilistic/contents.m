f4be9614c309db33a58dfefb924b16f43ecd6a4e
heijer <heijer@localhost>
2014-01-16 08:55:50 +0000

    contents.m added to probabilistic
    svn path=/trunk/matlab/; revision=10019
    
    
    Former-commit-id: 60325c04d2838c559938227d8507d2043c65d039

---
+++ applications/probabilistic/contents.m
@@ -0,0 +1,10 @@
+% Probabilistic tools in <a href="http://www.OpenEarth.eu">OpenEarthTools</a>
+%
+% Available methods
+%   FORM                    - First Order Reliability Method
+%   MC                      - Monte Carlo
+%   DirectionalSampling     - Directional Sampling (DS)
+%   AdaptiveResponseSurface - Adaptive Response Surface (ARS)
+%   Subset                  - Subset simulation
+%   HohenBichler            - Hohen Bichler
+%   SystemAnalysis          - System Analysis
