7d8e238d94957c0440063b636d0a1496239ea6ba
boer_g <boer_g@localhost>
2014-01-17 10:21:43 +0000

    cosmetic changes
    svn path=/trunk/matlab/; revision=10033
    
    
    Former-commit-id: 3c7528b86a4f94c775f1739531f4e7c7ec6041bb

--- applications/wps/runner/parse_oet_wps.m
+++ applications/wps/runner/parse_oet_wps.m
@@ -1,30 +1,38 @@
 % PARSE_OET_WPS  Parse a function that adheres to the OETWPS convention
+%
 %    [WPSSTRUCT] = PARSE_OET_WPS(FILENAME) parses the specified
 %    file and returns a struct that contains all the metadata for the 
-%    WPS server
- 
+%    WPS server. FILEName should include the extension .m.
+%
+%See also: runner
+
 function [wpsstruct] = parse_oet_wps(filename)
-    [argout, argin, fcn] = parse_function_call(filename);
+
+    [argout, argin, fcn] = parse_function_call(filename); % [argout, argin, fcn] = m.parse.call()
     
-    txt = fileread(filename);
+    txt = fileread(filename); % txt = m.parse.header()
+    
+    % obtain argin, argout description from comment block
+    
     inputs = struct();
     for i=1:length(argin)
         % scan for type of input arguments
-        arg = argin{i};
+        arg   = argin{i};
         match = regexp(txt, [arg, '\s*\:\s*(?<type>\w+(/\w+)?)'], 'names');
         inputs.(arg) = match;
     end
+    
     outputs = struct();
     for i=1:length(argout)
         % scan for type of output arguments
-        arg = argout{i};
+        arg   = argout{i};
         match = regexp(txt, [arg, '\s*\:\s*(?<type>\w+(/\w+)?)'], 'names');
         outputs.(arg) = match;
     end
     
     wpsstruct = struct(...
         'identifier', fcn, ...
-        'inputs', inputs,...
-        'outputs', outputs ...
+        'inputs'    , inputs,...
+        'outputs'   , outputs ...
     );
 end 
