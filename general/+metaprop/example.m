f2f910e5fa32525c3254c8343cefbe6cf546090e
tda.x <tda.x@localhost>
2014-02-24 11:32:31 +0000

    Added svn and license blocks
    svn path=/trunk/matlab/; revision=10272
    
    
    Former-commit-id: b60c765c8600c65d87da01a84124d1965d3b7a56

--- general/+metaprop/example.m
+++ general/+metaprop/example.m
@@ -2,6 +2,49 @@
 %
 % See also: metaprop.example_classdef
 
+%% Copyright notice
+%   --------------------------------------------------------------------
+%   Copyright (C) 2014 Van Oord
+%       Thijs Damsma
+%
+%       Thijs.Damsma@VanOord.com
+%
+%       Schaardijk 211
+%       3063 NH
+%       Rotterdam
+%       Netherlands
+%
+%   This library is free software: you can redistribute it and/or modify
+%   it under the terms of the GNU General Public License as published by
+%   the Free Software Foundation, either version 3 of the License, or
+%   (at your option) any later version.
+%
+%   This library is distributed in the hope that it will be useful,
+%   but WITHOUT ANY WARRANTY; without even the implied warranty of
+%   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%   GNU General Public License for more details.
+%
+%   You should have received a copy of the GNU General Public License
+%   along with this library.  If not, see <http://www.gnu.org/licenses/>.
+%   --------------------------------------------------------------------
+
+% This tool is part of <a href="http://www.OpenEarth.eu">OpenEarthTools</a>.
+% OpenEarthTools is an online collaboration to share and manage data and
+% programming tools in an open source, version controlled environment.
+% Sign up to recieve regular updates of this function, and to contribute
+% your own tools.
+
+%% Version <http://svnbook.red-bean.com/en/1.5/svn.advanced.props.special.keywords.html>
+% Created: 21 Feb 2014
+% Created with Matlab version: 8.3.0.73043 (R2014a)
+
+% $Id$
+% $Date$
+% $Author$
+% $Revision$
+% $HeadURL$
+% $Keywords: $
+
 %% instantiate a class
 % an example class
 obj = metaprop.example_classdef;
