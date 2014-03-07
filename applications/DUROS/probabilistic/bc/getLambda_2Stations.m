03c6996a8ad7ec7351b455f836b6a9b9510b3fb0
bieman <bieman@localhost>
2014-03-07 12:43:33 +0000

    From WIP to working version
    svn path=/trunk/matlab/; revision=10345
    
    
    Former-commit-id: cb5564f10d0845aeb9f2afd08e9f52f61bc6dc79

--- applications/DUROS/probabilistic/bc/getLambda_2Stations.m
+++ applications/DUROS/probabilistic/bc/getLambda_2Stations.m
@@ -1,4 +1,4 @@
-function [lambda1, lambda2, station1, station2] = getLambda_2Stations(varargin)
+function [Lambda1, Lambda2] = getLambda_2Stations(varargin)
 %GETLAMBDA_2STATIONS  Calculate lambda for profiles that are in between 2
 %stations
 %
@@ -67,9 +67,10 @@ OPT = struct(...
     'Station1',     '',         ...
     'Station2',     '',         ...
     'JarkusId',     [],         ...
-    'JarkusXRD',    [],         ...
-    'JarkusYRD',    [],         ... 
-    'JarkusAngle',  [],         ...
+    'Distance',     40000,      ...
+    'RSPX',         [],         ...
+    'RSPY',         [],         ... 
+    'Angle',        [],         ...
     'JarkusURL',    jarkus_url  ...
     );
 
@@ -78,12 +79,66 @@ OPT = setproperty(OPT, varargin{:});
 %% Determine Jarkus location
 
 if ~isempty(OPT.JarkusId)
-    
-elseif ~isempty(OPT.JarkusYRD) && ~isempty(OPT.JarkusYRD) && ~isempty(OPT.JarkusAngle)
+    % Get Jarkus info from NetCDF file
+    IDs         = nc_varget(OPT.JarkusURL,'id');
+    iLocation   = find(IDs == OPT.JarkusId)-1; % Target file is 0 based, where Matlab is 1 based
+    RSPX        = nc_varget(OPT.JarkusURL,'rsp_x',iLocation,1);
+    RSPY        = nc_varget(OPT.JarkusURL,'rsp_y',iLocation,1);
+    Angle       = nc_varget(OPT.JarkusURL,'angle',iLocation,1);
+    ExtendedX   = RSPX + OPT.Distance*cosd(90-Angle);
+    ExtendedY   = RSPY + OPT.Distance*sind(90-Angle);
+elseif ~isempty(OPT.RSPX) && ~isempty(OPT.RSPY) && ~isempty(OPT.Angle)
+    % Use given Jarkus info
+    RSPX        = OPT.RSPX;
+    RSPY        = OPT.RSPY;
+    ExtendedX   = RSPX + OPT.Distance*cosd(90-OPT.Angle);
+    ExtendedY   = RSPY + OPT.Distance*sind(90-OPT.Angle);
 else
     error('You need to specify either a Jarkus ID or a location (in RD coordinates) with an angle!')
 end
 
 %% Determine the two stations
 
+% Station information obtained from "Resultaten analyse HR2006 duinen" HKV
+% & WL|Delft Hydraulics, 2006
+StationInfo = {
+    'Hoek van Holland',     58748,  450830;
+    'IJmuiden',             79249,  501800;
+    'Den Helder',           99703,  552650;
+    'Eierlandse Gat',       106514, 587985;
+    'Steunpunt Waddenzee',  150000, 621230;
+    'Borkum',               221990, 621330
+    };
+
+Station1X   = []; 
+Station1Y   = [];
+Station2X   = []; 
+Station2Y   = [];
+
+for iStation = 1:size(StationInfo,1)
+    if strcmpi(StationInfo{iStation,1}, OPT.Station1)
+        Station1X   = StationInfo{iStation,2}; 
+        Station1Y   = StationInfo{iStation,3};
+    elseif strcmpi(StationInfo{iStation,1}, OPT.Station2)
+        Station2X   = StationInfo{iStation,2}; 
+        Station2Y   = StationInfo{iStation,3};
+    end
+end
+
+if isempty(Station1X) || isempty(Station1Y) || isempty(Station2X) || isempty(Station2Y)
+    error('Please specify valid station names!')
+end
+
 %% Calculate Lambda
+
+% Interpolate both to the same grid
+XDummy      = linspace(RSPX, ExtendedX, 1000);
+JarkusLine  = interp1([RSPX ExtendedX], [RSPY ExtendedY], XDummy);
+StationLine = interp1([Station1X Station2X], [Station1Y Station2Y], XDummy);
+
+% Find intersection
+[XIntersection, YIntersection]  = intersection(XDummy, JarkusLine, StationLine);
+
+% Calculate both Lambdas
+Lambda1     = distance([Station1X XIntersection],[Station1Y YIntersection])/distance([Station1X Station2X],[Station1Y Station2Y]);
+Lambda2     = distance([Station2X XIntersection],[Station2Y YIntersection])/distance([Station1X Station2X],[Station1Y Station2Y]);
