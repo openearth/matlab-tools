dc89081cb79046fe946439f3a896dbc44176e377
scheel <scheel@localhost>
2014-04-17 15:53:01 +0000

    End points are now consistently kept in the ldb when shortening.
    e.g: cutting up an ldb part of 190 mtr. in 100 mtr sections would result in 1 section of 100 mtr., now it results 100 and 90 mtr.
    
    Future: make a uniform cut-up with max. value dx, so for the above example result in 95 and 95 mtr. parts.
    
    help description added along with simple UI when not specifying input variables..
    
    svn path=/trunk/matlab/; revision=10568
    
    
    Former-commit-id: 80eb2565549927fa85e3273c5418b69cca6729c7

--- applications/DelftDashBoard/models/UnibestCL/gui/add_equidist_points.m
+++ applications/DelftDashBoard/models/UnibestCL/gui/add_equidist_points.m
@@ -1,16 +1,80 @@
-function outLDB=addEquidistantPointsBetweenSupportingLDBPoints(dx,ldb)
-
-% addEquidistantPointsBetweenSupportingLDBPoints - does what it says
+function outLDB=add_equidist_points(dx,ldb)
+%=Overview=================================================================
 %
-% out=addEquidistantPointsBetweenSupportingLDBPoints(in,dx)
+% add_equidist_points - cuts up ldb's in dx sections (only finer, not coarser)
+%
+% Syntax:
+%
+% ldb_out=add_equidist_points            <-- 2 dialog prompts triggered (ldb & dx)
+% ldb_out=add_equidist_points(dx)        <-- 1 dialog prompt triggered (ldb)
+% ldb_out=add_equidist_points(dx,ldb_in) <-- scripting (no dialogs)
+%
+%=Scematic overview of functionality=======================================
+%
+% X = original ldb points
+% # = new points in between ldb points (X's) using dx
+%
+%(1)(start)            (2)                                    (3)
+% :                     :                                      :
+% X_____________________X______________________________________X
+%                                                               \        
+%                                                                \       
+%                                                                 \      
+%                                                                  \     
+%                                                                   \    
+%                                                                    \   
+%                                                                     \  
+%                                                                      \ 
+%         RETURNS:                                                      \
+%                                                                        X
+%                                                                        :
+%(1)(start)            (2)                                    (3)       (4)
+% :          1a         :          2a         2b         2c    :        end                                        
+% X__________#__________X__________#__________#__________#_____X
+%                                   <-- dx -->              ^   \
+%                                                           :    \
+%                                                           :     \ dx
+%                                                           :      \
+%                                                           :       \
+%                                                           :        #
+%                                                           :         \
+% !Note that 'remainder' parts may be smaller than dx! <....:.........>\
+%                                                                       \
+%                                                                        X
+%                                                                        :
+%                                                                       (4)
+%                                                                       end
+%
+%==========================================================================
+% see also: landboundary ldbTool tekal disassembleLdb rebuildLdb
 
+%
+% Small additions 2014 (UI, endpoints check, help): Freek Scheel
+% freek.scheel@deltares.nl
+%
 outLDB=[];
 
-if nargin==0
+if nargin<2 
     if isempty(which('landboundary.m'))
         wlsettings;
     end
     ldb=landboundary('read');
+    if isempty(ldb)
+        disp('No ldb chosen, aborting...');
+        return
+    end
+end
+
+if nargin==0
+    dx_t = inputdlg('Specify a value for dx','dx?',1,{'1'});
+    if isempty(dx_t)
+        disp('No dx chosen, aborting...');
+        return
+    end
+    dx = str2num(char(dx_t));
+    if isempty(dx)
+        error('Please specify a numeric number for dx');
+    end
 end
 
 if isempty(ldb)
@@ -39,9 +103,10 @@ for cc=1:length(ldbCell)
         out=[out ; [ox oy]];
     end
     outCell{cc}=out;
+    if (outCell{cc}(end,1) ~= ldbEnd(cc,1)) | (outCell{cc}(end,2) ~= ldbEnd(cc,2))
+        outCell{cc}(end+1,1:2) = ldbEnd(cc,:);
+    end
 end
-
-out=rebuildLdb(outCell);
 
 if ~isstruct(ldb)
     outLDB=rebuildLdb(outCell);
