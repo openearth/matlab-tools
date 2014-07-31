dcdd920912a59020157840eadc05e57d7d2ab26f
huism_b <huism_b@localhost>
2014-07-31 12:24:41 +0000

    fix bug for situation where S=Phi curve can not be parameterized
    svn path=/trunk/matlab/; revision=11015
    
    
    Former-commit-id: 3b8f9b1c74c434a1b9a4a1c2e4620e93bc0b8177

--- applications/unibest/fileio/readGLO.m
+++ applications/unibest/fileio/readGLO.m
@@ -143,14 +143,18 @@ function [Cequi]=computeEQUI(equi,c1,c2,hoek,QSoffset)
     Cangle    = [(hoek-equi)-70:0.1:(hoek-equi)+70];
     phi_r     = Cangle-(hoek-equi);
     QS        = -c1.*phi_r.*exp(-((c2.*phi_r).^2)) +QSoffset/1000;
-    if max(QS)>=0 && min(QS)<=0
-        id        = find(abs(QS)==min(abs(QS)));
-    elseif max(QS)<0
-        id        = find(QS==max(QS));
-    elseif min(QS)>0
-        id        = find(QS==min(QS));
+    if ~isnan(c1);
+        if max(QS)>=0 && min(QS)<=0
+            id        = find(abs(QS)==min(abs(QS)));
+        elseif max(QS)<0
+            id        = find(QS==max(QS));
+        elseif min(QS)>0
+            id        = find(QS==min(QS));
+        end
+        Cequi     = Cangle(id(1));
+    else
+        Cequi     = nan;
     end
-    Cequi     = Cangle(id(1));
 end
 % 
 % %% SUBFUNCTION computeEQUI2
