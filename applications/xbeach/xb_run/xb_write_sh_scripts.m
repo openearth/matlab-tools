b2a3ac4c34d0efbecff9584dd9bd996d62cb450d
bieman <bieman@localhost>
2014-11-13 12:45:52 +0000

    - Finished submit script for multiple different runs on 1 node (increasing cluster efficiency) - Cleaner spacing for shell scripts (still containing some superfluous output for debugging purposes)
    svn path=/trunk/matlab/; revision=11367
    
    
    Former-commit-id: 22de499aa8cae496e4274960b6f947c26352797f

--- applications/xbeach/xb_run/xb_write_sh_scripts.m
+++ applications/xbeach/xb_run/xb_write_sh_scripts.m
@@ -115,8 +115,9 @@ switch upper(OPT.mpitype)
             fprintf(fid,'#$ -pe distrib %d\n', OPT.nodes);
         end
         
-        fprintf(fid,'. /opt/ge/InitSGE\n');
+        fprintf(fid,'. /opt/ge/InitSGE\n\n');
         fprintf(fid,'export NSLOTS=`expr $NHOSTS \\* 4`\n');
+        fprintf(fid,'export DELTAQ_NumSlots=`expr $DELTAQ_NumNodes \\* 4`\n\n');
         
         switch OPT.cluster
             case 'h5'
@@ -124,13 +125,13 @@ switch upper(OPT.mpitype)
                     % Define seperate cases for all different available versions
                     case 1.21
                         fprintf(fid,'module load mpich2-x86_64\n');
-                        fprintf(fid,'module load xbeach/xbeach121-gcc44-netcdf41-mpi10\n');
+                        fprintf(fid,'module load xbeach/xbeach121-gcc44-netcdf41-mpi10\n\n');
                     case 'wtisettings'
                         fprintf(fid,'module load gcc/4.9.1\n');
                         fprintf(fid,'module load hdf5/1.8.13_gcc_4.9.1\n');
                         fprintf(fid,'module load netcdf/v4.3.2_v4.4.0_gcc_4.9.1\n');
                         fprintf(fid,'module load /opt/xbeach/openmpi/1.8.1_gcc_4.9.1\n');
-                        fprintf(fid,'module load /u/bieman/privatemodules/xbeach-wtisettings_gcc_4.9.1_1.8.1_HEAD\n');
+                        fprintf(fid,'module load /u/bieman/privatemodules/xbeach-wtisettings_gcc_4.9.1_1.8.1_HEAD\n\n');
                     otherwise
                         % assume that OPT.version contains the complete
                         % module name
@@ -138,10 +139,10 @@ switch upper(OPT.mpitype)
                         fprintf(fid,'module load hdf5/1.8.13_gcc_4.9.1\n');
                         fprintf(fid,'module load netcdf/v4.3.2_v4.4.0_gcc_4.9.1\n');
                         fprintf(fid,'module load /opt/xbeach/openmpi/1.8.1_gcc_4.9.1\n');
-                        fprintf(fid,'module load %s\n',OPT.version);
+                        fprintf(fid,'module load %s\n\n',OPT.version);
                 end
                 fprintf(fid,'module list\n');
-                fprintf(fid,'env\n');
+                fprintf(fid,'env\n\n');
                 fprintf(fid,'awk ''{print $1":"1}'' $PE_HOSTFILE > $(pwd)/machinefile\n');
                 fprintf(fid,'mpdboot -n $NHOSTS -f $(pwd)/machinefile\n');
                 fprintf(fid,'mpdtrace\n');
@@ -154,8 +155,9 @@ switch upper(OPT.mpitype)
         fprintf(fid,'#$ -N %s\n', OPT.name);
         fprintf(fid,'#$ -m ea\n');
         fprintf(fid,'#$ -q %s\n', OPT.queuetype);
-        fprintf(fid,'#$ -pe distrib %d\n', OPT.nodes);
+        fprintf(fid,'#$ -pe distrib %d\n\n', OPT.nodes);
         fprintf(fid,'export NSLOTS=`expr $NHOSTS \\* 4`');
+        fprintf(fid,'export DELTAQ_NumSlots=`expr $DELTAQ_NumNodes \\* 4`\n\n');
         
         switch OPT.cluster
             case 'h5'
@@ -163,22 +165,22 @@ switch upper(OPT.mpitype)
                     % Define seperate cases for all different available versions
                     case 1.21
                         fprintf(fid,'module load mpich2-x86_64\n');
-                        fprintf(fid,'module load xbeach/xbeach121-gcc44-netcdf41-mpi10\n');
+                        fprintf(fid,'module load xbeach/xbeach121-gcc44-netcdf41-mpi10\n\n');
                     case 'wtisettings'
                         fprintf(fid,'module load gcc/4.9.1\n');
                         fprintf(fid,'module load hdf5/1.8.13_gcc_4.9.1\n');
                         fprintf(fid,'module load netcdf/v4.3.2_v4.4.0_gcc_4.9.1\n');
-                        fprintf(fid,'module load /u/bieman/privatemodules/xbeach-wtisettings_gcc_4.9.1_1.8.1_HEAD\n');
+                        fprintf(fid,'module load /u/bieman/privatemodules/xbeach-wtisettings_gcc_4.9.1_1.8.1_HEAD\n\n');
                     otherwise
                         % assume that OPT.version contains the complete
                         % module name
                         fprintf(fid,'module load gcc/4.9.1\n');
                         fprintf(fid,'module load hdf5/1.8.13_gcc_4.9.1\n');
                         fprintf(fid,'module load netcdf/v4.3.2_v4.4.0_gcc_4.9.1\n');
-                        fprintf(fid,'module load %s\n',OPT.version);
+                        fprintf(fid,'module load %s\n\n',OPT.version);
                 end
-                fprintf(fid,'module list\n');
-                fprintf(fid,'. /opt/ge/InitSGE\n');
+                fprintf(fid,'module list\n\n');
+                fprintf(fid,'. /opt/ge/InitSGE\n\n');
                 fprintf(fid,'awk ''{print $1":"1}'' $PE_HOSTFILE > $(pwd)/machinefile\n');
                 fprintf(fid,'mpdboot -n $NHOSTS -f $(pwd)/machinefile\n');
                 fprintf(fid,'mpirun -n $NSLOTS xbeach\n');
