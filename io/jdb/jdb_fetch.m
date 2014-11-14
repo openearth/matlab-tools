ec074b7c2fa26913ab31eba15febfe07186b18da
tda.x <tda.x@localhost>
2014-11-14 15:33:56 +0000

    re-added support for oracle.sql.TIMESTAMP datatype
    svn path=/trunk/matlab/; revision=11381
    
    
    Former-commit-id: 88d6ee831cf0bf8340bf2ce2c84161b51267dc62

--- io/jdb/jdb_fetch.m
+++ io/jdb/jdb_fetch.m
@@ -59,6 +59,15 @@ else
             row=row+1;
             for col = 1:numberOfColumns
                 jtype = char(rsMetaData.getColumnClassName(col));
+                
+                % not for editing:
+                % If you are getting a JDB:DATA_FETCH_ERROR, then the
+                % datatype you get is not implemented. There are many
+                % datatypes, which are easy to implement as long as you
+                % have test data. 
+                % 
+                % Please do not remove existing cases, just add more for
+                % more cases
                 switch jtype
                     case 'java.lang.String'
                         data{row,col} = char(rs.getString(col));  %#ok<*AGROW>
@@ -66,7 +75,7 @@ else
                         data{row,col} = rs.getDouble(col);
                     case 'java.lang.Int'
                         data{row,col} = rs.getInt(col);
-                    case 'java.sql.Timestamp'                       %'oracle.sql.TIMESTAMP'
+                    case {'java.sql.Timestamp','oracle.sql.TIMESTAMP'}
                         data{row,col} = datenum(1970,0,0) + rs.getTimestamp(col).getTime()/1000/3600/24;
                     otherwise
                         warning('JDB:DATA_FETCH_ERROR:TYPE_NOT_IMPLEMENTED:datatype %s implemented',jtype)
