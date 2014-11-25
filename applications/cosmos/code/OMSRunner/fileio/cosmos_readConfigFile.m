0a967a8a41c3c7513ca91c332552ccfb5b6944e0
ormondt <ormondt@localhost>
2014-11-25 09:52:46 +0000

    cosmos.ini moved to main folder
    svn path=/trunk/matlab/; revision=11432
    
    
    Former-commit-id: 3bf33333e8b2b8df5dc6b772515b216323489e8d

--- applications/cosmos/code/OMSRunner/fileio/cosmos_readConfigFile.m
+++ applications/cosmos/code/OMSRunner/fileio/cosmos_readConfigFile.m
@@ -2,51 +2,51 @@ function hm=cosmos_readConfigFile
 
 hm.eMailOnError.send=0;
 
-fname='cosmos.ini';
+fname=['..' filesep '..' filesep 'cosmos.ini'];
 [pathstr,name,ext] = fileparts(pwd);
 hm.scenario=name;
 
-if ~exist(fname,'file')
-
-    %% Make cosmos.ini
-    rundir = uigetdir(pwd, 'Select Run Directory');
-    hm.runDir=[rundir filesep];
-    
-%     [filename,pathname,filterindex] = uigetfile([hm.runDir 'scenarios\*.xml'],'Select scenario');
-%     hm.scenario = filename(1:end-4);
-
-    datadir = uigetdir(pwd, 'Select Data Directory');
-    hm.dataDir=[datadir filesep];
-
-    jobdir = uigetdir(pwd, 'Select Job Directory');
-    hm.jobDir=[jobdir filesep];
-    
-    webdir = uigetdir(pwd, 'Select Website Directory');
-    hm.webDir=[webdir filesep];
-
-    hm.nrProfilesPerJob=50;
-    hm.eMailOnError.send=0;
-    hm.eMailOnError.adress='Maarten.vanOrmondt@deltares.nl';
-    hm.h4.userName='ormondt';
-    hm.h4.password='0rm0ndt';
-    hm.h4.path=strrep(['/' hm.jobDir],'\','/');
-    hm.h4.path=strrep(hm.h4.path,':','');
-
-    fid=fopen(fname,'wt');
-    fprintf(fid,'%s\n',['RunDirectory     ' hm.runDir]);
-    fprintf(fid,'%s\n',['DataDirectory    ' hm.dataDir]);
-    fprintf(fid,'%s\n',['JobDirectory     ' hm.jobDir]);
-    fprintf(fid,'%s\n',['WebDirectory     ' hm.webDir]);
-    fprintf(fid,'%s\n',['H4Directory      ' hm.h4.path]);
-    fprintf(fid,'%s\n',['H4UserName       ' hm.h4.userName]);
-    fprintf(fid,'%s\n',['H4Password       ' hm.h4.password]);
-%    fprintf(fid,'%s\n',['Scenario         ' hm.scenario]);
-    fprintf(fid,'%s\n',['eMailOnError     0']);
-    fprintf(fid,'%s\n',['eMailAdress      ' hm.eMailOnError.adress]);
-    fprintf(fid,'%s\n',['nrProfilesPerJob ' num2str(hm.nrProfilesPerJob)]);
-    fclose(fid);
-
-end
+% if ~exist(fname,'file')
+% 
+%     %% Make cosmos.ini
+%     rundir = uigetdir(pwd, 'Select Run Directory');
+%     hm.runDir=[rundir filesep];
+%     
+% %     [filename,pathname,filterindex] = uigetfile([hm.runDir 'scenarios\*.xml'],'Select scenario');
+% %     hm.scenario = filename(1:end-4);
+% 
+%     datadir = uigetdir(pwd, 'Select Data Directory');
+%     hm.dataDir=[datadir filesep];
+% 
+%     jobdir = uigetdir(pwd, 'Select Job Directory');
+%     hm.jobDir=[jobdir filesep];
+%     
+%     webdir = uigetdir(pwd, 'Select Website Directory');
+%     hm.webDir=[webdir filesep];
+% 
+%     hm.nrProfilesPerJob=50;
+%     hm.eMailOnError.send=0;
+%     hm.eMailOnError.adress='Maarten.vanOrmondt@deltares.nl';
+%     hm.h4.userName='ormondt';
+%     hm.h4.password='0rm0ndt';
+%     hm.h4.path=strrep(['/' hm.jobDir],'\','/');
+%     hm.h4.path=strrep(hm.h4.path,':','');
+% 
+%     fid=fopen(fname,'wt');
+%     fprintf(fid,'%s\n',['RunDirectory     ' hm.runDir]);
+%     fprintf(fid,'%s\n',['DataDirectory    ' hm.dataDir]);
+%     fprintf(fid,'%s\n',['JobDirectory     ' hm.jobDir]);
+%     fprintf(fid,'%s\n',['WebDirectory     ' hm.webDir]);
+%     fprintf(fid,'%s\n',['H4Directory      ' hm.h4.path]);
+%     fprintf(fid,'%s\n',['H4UserName       ' hm.h4.userName]);
+%     fprintf(fid,'%s\n',['H4Password       ' hm.h4.password]);
+% %    fprintf(fid,'%s\n',['Scenario         ' hm.scenario]);
+%     fprintf(fid,'%s\n',['eMailOnError     0']);
+%     fprintf(fid,'%s\n',['eMailAdress      ' hm.eMailOnError.adress]);
+%     fprintf(fid,'%s\n',['nrProfilesPerJob ' num2str(hm.nrProfilesPerJob)]);
+%     fclose(fid);
+% 
+% end
 
 txt=ReadTextFile(fname);
 
