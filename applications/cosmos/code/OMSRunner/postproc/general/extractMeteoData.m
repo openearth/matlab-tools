368816ff05d47ff610713d8cc6dfad3d6a713601
ormondt <ormondt@localhost>
2015-02-19 15:06:33 +0000

    cosmos: extractMeteoData.m update
    svn path=/trunk/matlab/; revision=11727
    
    
    Former-commit-id: 780fcfe8a0b20f18783b233dc4623ffd31ebf985

--- applications/cosmos/code/OMSRunner/postproc/general/extractMeteoData.m
+++ applications/cosmos/code/OMSRunner/postproc/general/extractMeteoData.m
@@ -15,91 +15,43 @@ if ~strcmpi(coordsystype,'geographic')
     dx=model.dXMeteo;
     dy=model.dXMeteo;
 else
-    dx=0;
-    dy=0;
+    if ~isempty(model.meteospw)
+        dx=0.1;
+        dy=0.1;
+    else        
+    dx=[];
+    dy=[];
+    end
 end
 
 tstart=model.tFlowStart;
 tstop=model.tStop;
 
-dt=dt/24;
+dt=dt*60; % minutes
 
-nt=(tstop-tstart)/dt+1;
+tmpdir=[];
+fname=[];
 
-xlimg=xlim;
-ylimg=ylim;
-
-xlim(2)=max(xlim(2),xlim(1)+dx);
-ylim(2)=max(ylim(2),ylim(1)+dy);
-
-if ~strcmpi(coordsystype,'geographic')
-    [xg,yg]=meshgrid(xlim(1):dx:xlim(2),ylim(1):dy:ylim(2));
-    [xgeo,ygeo]=convertCoordinates(xg,yg,'persistent','CS1.name',coordsys,'CS1.type',coordsystype,'CS2.name','WGS 84','CS2.type','geographic');
-    xlimg(1)=min(min(xgeo));
-    xlimg(2)=max(max(xgeo));
-    ylimg(1)=min(min(ygeo));
-    ylimg(2)=max(max(ygeo));
-    unit='m';
-else
-    unit='degree';
-end
-
-for it=1:nt
-
-    t=tstart+(it-1)*dt;
-    tstr=datestr(t,'yyyymmddHHMMSS');
-    fstru=[meteodir meteowind '.u.' tstr '.mat'];
-    fstrv=[meteodir meteowind '.v.' tstr '.mat'];
-%    fstrp=[meteodir meteoname '.p.' tstr '.mat'];
-    if exist(fstru,'file')
-        su=load(fstru);
-        sv=load(fstrv);
-%        sp=load(fstrp);
-    else
-        % find first available file
-        for n=1:1000
-            t0=t+n*dt;
-            tstr=datestr(t0,'yyyymmddHHMMSS');
-            fstru=[meteodir meteowind '.u.' tstr '.mat'];
-            fstrv=[meteodir meteowind '.v.' tstr '.mat'];
-%            fstrp=[meteodir meteoname '.p.' tstr '.mat'];
-            if exist(fstru,'file')
-                su=load(fstru);
-                sv=load(fstrv);
-%                sp=load(fstrp);
-                break
-            end
-        end
-    end
-
-    [u,lon,lat]=getMeteoMatrix(su.u,su.lon,su.lat,xlimg,ylimg);
-    [v,lon,lat]=getMeteoMatrix(sv.v,sv.lon,sv.lat,xlimg,ylimg);
-%    [p,lon,lat]=getMeteoMatrix(sp.p,sp.lon,sp.lat,xlimg,ylimg);
+switch lower(par)
     
-    if ~strcmpi(coordsystype,'geographic')
-        u=interp2(lon,lat,u,xgeo,ygeo);
-        v=interp2(lon,lat,v,xgeo,ygeo);
-%        p=interp2(lon,lat,p,xgeo,ygeo);
-    end
+    case{'windvel'}
+        
+        parameter={'u','v'};
 
-    s2.Parameter=par;   
-
-    s2.Time(it)=t;
-
-    if ~strcmpi(coordsystype,'geographic')
-        s2.X=xlim(1):dx:xlim(2);
-        s2.Y=ylim(1):dy:ylim(2);
-    else
-        s2.X=lon;
-        s2.Y=lat;
-    end
-
-    switch(par)
-        case{'windvel'}
-            s2.XComp(it,:,:)=u;
-            s2.YComp(it,:,:)=v;
-        case{'airp'}
-            s2.Val(it,:,:)=p;
-    end
-
+        if ~isempty(model.meteospw)        
+            spwfile=[meteodir 'spiderwebs' filesep model.meteospw];
+        else
+            spwfile=[];
+        end
+        
+        s=write_meteo_file([meteodir meteowind filesep], meteowind, parameter, tmpdir, fname, xlim, ylim, tstart, tstop, 'dx',dx,'dy',dy,'dt',dt,'spwfile',spwfile,'model','none');
+        
+        s2.Parameter=par;
+        s2.Time=s.parameter(1).time;
+        s2.X=s.parameter(1).x;
+        s2.Y=s.parameter(1).y;
+        s2.XComp=s.parameter(1).val;
+        s2.YComp=s.parameter(2).val;
+        
 end
+
