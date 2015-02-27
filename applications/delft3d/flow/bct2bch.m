1a4de948e35b6d96676b4308aa6ad5aa881e1f91
ormondt <ormondt@localhost>
2015-02-27 21:18:45 +0000

    bct2bch header info added
    svn path=/trunk/matlab/; revision=11753
    
    
    Former-commit-id: b2ce152811f30796a97d91acdb09ca7c0242f110

--- applications/delft3d/flow/bct2bch.m
+++ applications/delft3d/flow/bct2bch.m
@@ -1,7 +1,48 @@
 function bct2bch(bctfile,bchfile,tstart,tstop,ncomp,varargin)
-
-% Create bch file from bct file
+% Creates bch file from bct file (using 'good old' TYD2HAR.EXE)
+% bct2bch(bctfile,bchfile,tstart,tstop,ncomp,varargin)
+%
 % e.g. bct2bch('DD_GOM2_4KM.bct','test.bch',datenum(2011,1,5),datenum(2011,1,6),4,'plot')
+%
+% By default, the A0 component is ignored. If A0 needs to be included in
+% BCH file, use input argument INCLUDEA0.
+% 
+% e.g. bct2bch('DD_GOM2_4KM.bct','test.bch',datenum(2011,1,5),datenum(2011,1,6),4,'includea0')
+
+%% Copyright notice
+%   --------------------------------------------------------------------
+%   Copyright (C) 2015 Deltares
+%       Maarten van Ormondt
+%
+%       Maarten.vanOrmondt@deltares.nl
+%
+%       P.O. Box 177
+%       2600 MH Delft
+%       The Netherlands
+%
+%   This library is free software: you can redistribute it and/or modify
+%   it under the terms of the GNU General Public License as published by
+%   the Free Software Foundation, either version 3 of the License, or
+%   (at your option) any later version.
+%
+%   This library is distributed in the hope that it will be useful,
+%   but WITHOUT ANY WARRANTY; without even the implied warranty of
+%   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%   GNU General Public License for more details.
+%
+%   You should have received a copy of the GNU General Public License
+%   along with this library.  If not, see <http://www.gnu.org/licenses/>.
+%   --------------------------------------------------------------------
+
+% This tool is part of <a href="http://www.OpenEarth.eu">OpenEarthTools</a>.
+% OpenEarthTools is an online collaboration to share and manage data and
+% programming tools in an open source, version controlled environment.
+% Sign up to recieve regular updates of this function, and to contribute
+% your own tools.
+
+%% Version <http://svnbook.red-bean.com/en/1.5/svn.advanced.props.special.keywords.html>
+% Created: 29 Nov 2011
+% Created with Matlab version: 7.11.0.584 (R2010b)
 
 t2hfolder=fileparts(which('tyd2har.exe'));
 
