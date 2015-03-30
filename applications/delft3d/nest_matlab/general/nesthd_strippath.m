518eb5d2c81df792c04809e5b838df3ba31eca62
kaaij <kaaij@localhost>
2015-03-30 15:47:58 +0000

    svn path=/trunk/matlab/; revision=11841
    Former-commit-id: 29efeb15ac534ee090d78bb1492cdf94d17aceae

---
+++ applications/delft3d/nest_matlab/general/nesthd_strippath.m
@@ -0,0 +1,9 @@
+function name = nesthd_strippath(fullname)
+
+% gives back just the filename from the entire, fullpath, name
+
+name = '';
+if ~isempty(fullname)
+    [~,fname,fext] = fileparts(fullname);
+    name = [fname fext];
+end
