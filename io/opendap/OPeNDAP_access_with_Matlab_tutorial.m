6f164bd0ea27eb61b62bcf74c0ec873cdc9b4ace
gerben.deboer.x <gerben.deboer.x@localhost>
2015-06-19 13:08:12 +0000

    fixed for change nc_varget to ncread
    svn path=/trunk/matlab/; revision=12019
    
    
    Former-commit-id: 3b4611dbc95450ec269bd873e82e7f56d427169e

--- io/opendap/OPeNDAP_access_with_Matlab_tutorial.m
+++ io/opendap/OPeNDAP_access_with_Matlab_tutorial.m
@@ -14,28 +14,28 @@
 % This document is also posted on a wiki: http://public.deltares.nl/display/OET/OPeNDAP+access+with+matlab
 
 %%
-run('...\openearthtools\matlab\oetsettings.m')
+%run('..\..\oetsettings.m')
 
 %% Read data from an opendap server
 url_grid = 'http://opendap.deltares.nl/thredds/dodsC/opendap/rijkswaterstaat/vaklodingen/vaklodingenKB116_4544.nc'
-url_time = 'http://opendap.deltares.nl/thredds/dodsC/opendap/rijkswaterstaat/waterbase/concentration_of_suspended_matter_in_sea_water/id410-DELFZBTHVN.nc'
+url_time = 'http://opendap.deltares.nl/thredds/dodsC/opendap/rijkswaterstaat/waterbase/concentration_of_suspended_matter_in_water/id410-DELFZBTHVN.nc'
 
 %%
-nc_dump(url_grid)
-nc_dump(url_time)
+ncdisp(url_grid)
+ncdisp(url_time)
 
 %% Get grid data 
-G.x = nc_varget(url_grid,'x');
-G.y = nc_varget(url_grid,'y');
-G.z = nc_varget(url_grid,'z',[0 0 0],[1 -1 -1]);% get only one timestep (here: 1st), but all x and y, note: nc_varget is zero based.
+G.x = ncread(url_grid,'x');
+G.y = ncread(url_grid,'y');
+G.z = ncread(url_grid,'z',[1 1 1],[Inf Inf 1]); % get only one timestep (here: 1st), but all x and y, note: nc_varget is zero based.
 
 %% Get time series data
 figure
-T.t   = nc_cf_time(url_time,'time'); % adapt reference date of 1970 in netCDF to Matlab reference date of 0000
-T.eta = nc_varget (url_time,'concentration_of_suspended_matter_in_sea_water');
+T.t   = ncread_cf_time(url_time,'time'); % adapt reference date of 1970 in netCDF to Matlab reference date of 0000
+T.eta = ncread(url_time,'concentration_of_suspended_matter_in_water');
 
 %% plot grid data
-pcolorcorcen(G.x,G.y,G.z)
+pcolorcorcen(G.x,G.y,G.z')
 axis equal
 axis tight
 tickmap('xy')
