d5a6c37ec855a12478279a53d140f42f104c94ca
verploe <verploe@localhost>
2015-07-06 08:45:30 +0000

    svn path=/trunk/matlab/; revision=12067
    Former-commit-id: e1f8bb4b57198ab722a6476b1a468920e01c21e3

--- applications/delft3d/nest_matlab/general/nesthd_detlength.m
+++ applications/delft3d/nest_matlab/general/nesthd_detlength.m
@@ -2,7 +2,7 @@ function [dist] = nesthd_detlength(x_from,y_from,x_to,y_to,varargin)
 
 %   nesthd_detlength, computes lengths from "from" to "to"
 %   from might be a vector
-%   
+%
 %   Optional <keyword>,<value> pair
 %            'Spherical' which is either false (default) or true
 
@@ -10,15 +10,19 @@ OPT.Spherical = false;
 OPT           = setproperty(OPT,varargin);
 
 if ~OPT.Spherical
-    
+
     %% Cartesian coordinates
     dist = sqrt((x_from - x_to).^2 + (y_from - y_to).^2 );
 else
-    
+
     %% Spherical coordinates, use native matlab functions
-    no_pnt        = length(x_from);
-    for i_pnt = 1: no_pnt
-        dist(i_pnt) = deg2km(distance(x_from(i_pnt),y_from(i_pnt),x_to,y_to))/1000.;
-    end
+    pnt_1 = [x_from ; y_from];
+    pnt_2 = [x_to   ; y_to];
+    dist  = deg2km(distance(pnt_1,pnt_2))/1000.;
+
+ %   no_pnt        = length(x_from);
+ %   for i_pnt = 1: no_pnt
+ %       dist(i_pnt) = deg2km(distance(x_from(i_pnt),y_from(i_pnt),x_to,y_to))/1000.;
+ %   end
 end
 
