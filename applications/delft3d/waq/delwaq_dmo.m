3fec848d10ec7067cf5326ab6ccb464fd23e4adf
gaytan_sa <gaytan_sa@localhost>
2015-07-09 18:38:05 +0000

    svn path=/trunk/matlab/; revision=12079
    Former-commit-id: e7a074237c9d4f1445367e66ce7f70a63ae9ee97

--- applications/delft3d/waq/delwaq_dmo.m
+++ applications/delft3d/waq/delwaq_dmo.m
@@ -2,30 +2,51 @@
 %
 %   [name segments] = DELWAQ_OBS(dmoFile) Read the observation file
 %   <obsFile>  and gives back x,y, layer and the id.
-%   
+%
 %   NOTE: <obsFile> has to be a delwaq *.obs format
-%   
-%   
-%   See also: 
+%
+%
+%   See also:
 
 %   Copyright 2011 Deltares, the Netherlands
 %   http://www.delftsoftware.com
 %   2011-Jul-12 Created by Gaytan-Aguilar
 %   email: sandra.gaytan@deltares.com
 %--------------------------------------------------------------------------
-function [name segments] = delwaq_dmo(dmoFile)
+function S = delwaq_dmo(type,dmoFile,S)
 
-fid = fopen(dmoFile);
-
-n = cell2mat(textscan(fid, '%f'));
-
-for i = 1:n
-    aname = textscan(fid, '%s',2);
-    aname = aname{1};
-    aname = char(aname{1});
-    name{i} = aname(2:end); %#ok<*AGROW>
-    ns{i} = cell2mat(textscan(fid, '%f',1));
-    segments{i} = cell2mat(textscan(fid, '%f',ns));
+if nargin ==2
+    S = [];
 end
 
-fclose(fid);
+switch type
+    case 'read'
+        fid = fopen(dmoFile);
+        
+        n = cell2mat(textscan(fid, '%f'));
+        
+        for i = 1:n
+            disp(['Reading area: ' num2str(i) '/' num2str(n)])
+            localname = textscan(fid, '%s',1);
+            localname = localname{1};
+            localname = char(localname{1});
+            S.name{i} = localname(2:end-1); %#ok<*AGROW>
+            ns = cell2mat(textscan(fid, '%f',1));
+            S.segments{i} = cell2mat(textscan(fid, '%f',ns));
+        end
+        
+        fclose(fid);
+    case 'write'
+        
+        fid = fopen(dmoFile,'w+');
+        
+        n = length(S.name);
+        fprintf(fid,'%s\n',num2str(n));
+        for i = 1:n
+            fprintf(fid,'%s\t %s\n',['''' S.name{i} ''''], num2str(length(S.segments{i})));
+            fprintf(fid,'%0.0f\n',S.segments{i});
+        end
+        
+        fclose(fid);
+        
+end
