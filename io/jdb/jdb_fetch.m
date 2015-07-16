5c5c20cedfd7a6fbbfa5b2c1b5274cec19a2aa62
rho.x <rho.x@localhost>
2015-07-16 15:33:35 +0000

    Improvement increased by checking the jtype on the first row only and increasing the fetch size.
    svn path=/trunk/matlab/; revision=12121
    
    
    Former-commit-id: 6e594ce1c26143be2aef077da10103f242700387

--- io/jdb/jdb_fetch.m
+++ io/jdb/jdb_fetch.m
@@ -1,10 +1,11 @@
 function varargout = jdb_fetch(conn, sql, varargin)
 % Executes a SQL query and imports database data into matlab
 %
-%   Executes a SQL query with the (i) licensed Mathworks database
-%   toolbox or otherwise with (ii) the JDBC driver directly
-%   fetches the result and checks the result for
-%   errors. Returns the resulting data in a cell array or matrix.
+%   Executes a SQL query with the: 
+%       (i)  licensed Mathworks database toolbox (with no checks etc.). 
+%       (ii) the JDBC driver directly and fetches the result and checks 
+%            the result for errors. 
+%   Returns the resulting data in a cell array or matrix.
 %
 %   Syntax:
 %   rs = jdb_fetch(conn, sql, varargin)
@@ -16,6 +17,10 @@ function varargout = jdb_fetch(conn, sql, varargin)
 %
 %   Output:
 %   rs        = Fetched data from result set from SQL query
+%
+%   Remarks
+%       The performance is improved by testing the jtype only on the first
+%       row and increasing the fetch size.
 %
 %   Example
 %   conn = jdb_connectdb('someDatabase');
@@ -39,77 +44,82 @@ end
 OPT = setproperty(OPT, varargin);
 
 if OPT.database_toolbox
-    error('Database toolbox is not supported')
+    warning('Database toolbox is not fully implemented')
     data = fetch(conn, sql);
+    header = {''};
     % does not parse time automatically to datenum
-end
+else
 
-% http://docs.oracle.com/javase/7/docs/api/java/sql/PreparedStatement.html
+    % http://docs.oracle.com/javase/7/docs/api/java/sql/PreparedStatement.html
 
-pstat = conn.prepareStatement(sql);
-rs = pstat.executeQuery();
+    pstat = conn.prepareStatement(sql);
+    rs = pstat.executeQuery();
 
-rsMetaData = rs.getMetaData();
-numberOfColumns = rsMetaData.getColumnCount();
+    rsMetaData = rs.getMetaData();
+    numberOfColumns = rsMetaData.getColumnCount();
 
-% a large fetch size greatly improves performance of large queries
-rs.setFetchSize(10000)
+    % a large fetch size greatly improves performance of large queries
+    rs.setFetchSize(20000)      %Increased from 10000 -> better performance, 50000 gave errors
 
-row = 0;
-data = {};
-while 1
-    try
-        if ~rs.next()
+    row = 0;
+    data = {};
+    jtypes = cell(numberOfColumns,1);
+    while 1
+        try
+            if ~rs.next()
+                break
+            end
+            row = row+1;
+            for col = 1:numberOfColumns
+                if row==1
+                    jtypes{col} = char(rsMetaData.getColumnClassName(col));     % Only needed for first row (less calculations)
+                end
+    %                 jtype = char(rsMetaData.getColumnClassName(col));
+
+                % not for editing:
+                % If you are getting a JDB:DATA_FETCH_ERROR, then the
+                % datatype you get is not implemented. There are many
+                % datatypes, which are easy to implement as long as you
+                % have test data. 
+                % 
+                % Please do not remove existing cases, just add more for
+                % more cases
+                switch jtypes{col} %jtype
+                    case {'java.lang.String'}
+                        data{row,col} = char(rs.getString(col));  %#ok<*AGROW>
+                    case {'java.lang.Float','java.lang.Double','java.math.BigDecimal'}
+                        data{row,col} = rs.getDouble(col);
+                    case {'java.lang.Short','java.lang.Int','java.lang.Integer','java.lang.Long'}
+                        data{row,col} = rs.getInt(col);
+                    case {'java.sql.Timestamp','oracle.sql.TIMESTAMP'}
+                        jt = rs.getTimestamp(col);
+                        mils_since_epoch = jt.getTime() - (jt.getTimezoneOffset * 60 * 1000);
+                        epoch = datenum(1970,1,1);
+                        data{row,col} = epoch + mils_since_epoch/1000/3600/24;
+                    case {'java.lang.Boolean'}
+                        data{row,col} = logical(rs.getBoolean(col));
+                    case 'org.postgresql.util.PGInterval'
+                        jt = rs.getTimestamp(col);
+                        mils_since_epoch = jt.getTime() - (jt.getTimezoneOffset * 60 * 1000);
+                        data{row,col} = mils_since_epoch * datenum(0,0,0,0,0,1/1000);
+                    otherwise
+                        warning('JDB:DATA_FETCH_ERROR:TYPE_NOT_IMPLEMENTED:datatype %s implemented',jtype)
+                end
+            end
+        catch ME
+            warning('JDB:DATA_FETCH_ERROR',ME.getReport())
             break
         end
-        row=row+1;
-        for col = 1:numberOfColumns
-            jtype = char(rsMetaData.getColumnClassName(col));
-
-            % not for editing:
-            % If you are getting a JDB:DATA_FETCH_ERROR, then the
-            % datatype you get is not implemented. There are many
-            % datatypes, which are easy to implement as long as you
-            % have test data. 
-            % 
-            % Please do not remove existing cases, just add more for
-            % more cases
-            switch jtype
-                case {'java.lang.String'}
-                    data{row,col} = char(rs.getString(col));  %#ok<*AGROW>
-                case {'java.lang.Float','java.lang.Double','java.math.BigDecimal'}
-                    data{row,col} = rs.getDouble(col);
-                case {'java.lang.Short','java.lang.Int','java.lang.Integer','java.lang.Long'}
-                    data{row,col} = rs.getInt(col);
-                case {'java.sql.Timestamp','oracle.sql.TIMESTAMP'}
-                    jt = rs.getTimestamp(col);
-                    mils_since_epoch = jt.getTime() - (jt.getTimezoneOffset * 60 * 1000);
-                    epoch = datenum(1970,1,1);
-                    data{row,col} = epoch + mils_since_epoch/1000/3600/24;
-                case {'java.lang.Boolean'}
-                    data{row,col} = logical(rs.getBoolean(col));
-                case 'org.postgresql.util.PGInterval'
-                    jt = rs.getTimestamp(col);
-                    mils_since_epoch = jt.getTime() - (jt.getTimezoneOffset * 60 * 1000);
-                    data{row,col} = mils_since_epoch * datenum(0,0,0,0,0,1/1000);
-                otherwise
-                    warning('JDB:DATA_FETCH_ERROR:TYPE_NOT_IMPLEMENTED:datatype %s implemented',jtype)
-            end
-        end
-    catch ME
-        warning('JDB:DATA_FETCH_ERROR',ME.getReport())
-        break
     end
+
+    header = arrayfun(...
+        @(x) char(rsMetaData.getColumnName(x)),...
+        (1:numberOfColumns),...
+        'UniformOutput',false);
+
+    pstat.close();
+    rs.close();
 end
-
-
-header = arrayfun(...
-    @(x) char(rsMetaData.getColumnName(x)),...
-    (1:numberOfColumns),...
-    'UniformOutput',false);
-
-pstat.close();
-rs.close();
 
 %% convert to desired output format
 switch OPT.format
