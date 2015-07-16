792ab91a5273574ae3d8097c13691d8759c1b229
ormondt <ormondt@localhost>
2015-07-16 07:57:20 +0000

    ddb ww3_define_nesting_sections.m update
    svn path=/trunk/matlab/; revision=12113
    
    
    Former-commit-id: 5bae886dbce2f48cd1983da4fe3486e4a0b3f1e4

--- applications/DelftDashBoard/general/wavewatch3/nesting/ww3_define_nesting_sections.m
+++ applications/DelftDashBoard/general/wavewatch3/nesting/ww3_define_nesting_sections.m
@@ -1,11 +1,13 @@
-function [x0,y0,dx,dy,np]=ww3_define_nesting_sections(ww3_grid_file)
+function [x0out,y0out,dxout,dyout,npout,ixdout,iydout]=ww3_define_nesting_sections(ww3_grid_file)
 % Creates nesting sections for ww3 in ww3 nesting
 
 % Read detailed model data
 grd=ww3_read_grid_inp(ww3_grid_file);
-dr=fileparts(handles.toolbox.nesting.ww3_grid_file);
-bot=ww3_read_bottom_depth_file([dr filesep grd.bottom_depth_filename],grd.nx,grd.ny);
+dr=fileparts(ww3_grid_file);
+bot=ww3_read_bottom_depth_file([dr filesep grd.bottom_depth_filename],grd.nx,grd.ny,grd.bottom_depth_scaling_factor);
 [xg,yg]=meshgrid(grd.x0:grd.dx:grd.x0+(grd.nx-1)*grd.dx,grd.y0:grd.dy:grd.y0+(grd.ny-1)*grd.dy);            
+
+thresh=-10;
 
 % Bottom row
 ix=0;
@@ -19,7 +21,7 @@ while 1
     end
     if newsection
         % Looking for a new section
-        if bot(iy,ix)>10
+        if bot(iy,ix)<thresh
             % Start of new section found
             newsection=0;
             nsec=nsec+1;
@@ -28,13 +30,22 @@ while 1
             y0(nsec)=yg(iy,ix); 
             dx(nsec)=grd.dx;
             dy(nsec)=0;
+            % Also define detailed model boundary points
+            ixd(nsec,1)=ix;
+            iyd(nsec,1)=iy;
+            ixd(nsec,2)=ix;
+            iyd(nsec,2)=iy;
         end
     else
-        if bot(iy,ix)<10
-            % Continue along this section
+        if bot(iy,ix)>thresh
+            % End of section found
             newsection=1;
         else
+            % Continue along this section
             np(nsec)=np(nsec)+1;            
+            % Also define detailed model boundary points
+            ixd(nsec,2)=ix;
+            iyd(nsec,2)=iy;
         end
     end
 end
@@ -50,7 +61,7 @@ while 1
     end
     if newsection
         % Looking for a new section
-        if bot(iy,ix)>10
+        if bot(iy,ix)<thresh
             % Start of new section found
             newsection=0;
             nsec=nsec+1;
@@ -59,13 +70,22 @@ while 1
             y0(nsec)=yg(iy,ix); 
             dx(nsec)=grd.dx;
             dy(nsec)=0;
+            % Also define detailed model boundary points
+            ixd(nsec,1)=ix;
+            iyd(nsec,1)=iy;
+            ixd(nsec,2)=ix;
+            iyd(nsec,2)=iy;
         end
     else
-        if bot(iy,ix)<10
-            % Continue along this section
+        if bot(iy,ix)>thresh
+            % End of section found
             newsection=1;
         else
+            % Continue along this section
             np(nsec)=np(nsec)+1;            
+            % Also define detailed model boundary points
+            ixd(nsec,2)=ix;
+            iyd(nsec,2)=iy;
         end
     end
 end
@@ -81,7 +101,7 @@ while 1
     end
     if newsection
         % Looking for a new section
-        if bot(iy,ix)>10
+        if bot(iy,ix)<thresh
             % Start of new section found
             newsection=0;
             nsec=nsec+1;
@@ -90,13 +110,22 @@ while 1
             y0(nsec)=yg(iy,ix); 
             dx(nsec)=0;
             dy(nsec)=grd.dx;
+            % Also define detailed model boundary points
+            ixd(nsec,1)=ix;
+            iyd(nsec,1)=iy;
+            ixd(nsec,2)=ix;
+            iyd(nsec,2)=iy;
         end
     else
-        if bot(iy,ix)<10
-            % Continue along this section
+        if bot(iy,ix)>thresh
+            % End of section found
             newsection=1;
         else
+            % Continue along this section
             np(nsec)=np(nsec)+1;            
+            % Also define detailed model boundary points
+            ixd(nsec,2)=ix;
+            iyd(nsec,2)=iy;
         end
     end
 end
@@ -112,7 +141,7 @@ while 1
     end
     if newsection
         % Looking for a new section
-        if bot(iy,ix)>10
+        if bot(iy,ix)<thresh
             % Start of new section found
             newsection=0;
             nsec=nsec+1;
@@ -121,13 +150,41 @@ while 1
             y0(nsec)=yg(iy,ix); 
             dx(nsec)=0;
             dy(nsec)=grd.dx;
+            % Also define detailed model boundary points
+            ixd(nsec,1)=ix;
+            iyd(nsec,1)=iy;
+            ixd(nsec,2)=ix;
+            iyd(nsec,2)=iy;
         end
     else
-        if bot(iy,ix)<10
-            % Continue along this section
+        if bot(iy,ix)>thresh
+            % End of section found
             newsection=1;
         else
+            % Continue along this section
             np(nsec)=np(nsec)+1;            
+            % Also define detailed model boundary points
+            ixd(nsec,2)=ix;
+            iyd(nsec,2)=iy;
         end
+    end
+end
+
+% Get rid of boundary sections that cover just one point
+nsec=0;
+for j=1:length(np)
+    if ixd(j,1)==ixd(j,2) && iyd(j,1)==iyd(j,2)
+        % Skip this section
+    else
+        nsec=nsec+1;
+        npout(nsec)=np(j);
+        x0out(nsec)=x0(j);
+        y0out(nsec)=y0(j);
+        dxout(nsec)=dx(j);
+        dyout(nsec)=dy(j);
+        ixdout(nsec,1)=ixd(j,1);
+        ixdout(nsec,2)=ixd(j,2);
+        iydout(nsec,1)=iyd(j,1);
+        iydout(nsec,2)=iyd(j,2);
     end
 end
