c17d321ea8c6b802f8b60f0eee9b4e826c47c9f3
bart.roest.x <bart.roest.x@localhost>
2015-09-11 07:29:29 +0000

    Correction of help text
    svn path=/trunk/matlab/; revision=12235
    
    
    Former-commit-id: 7237fcf29e3a85f2d78f5c653ca84c5c079721ab

--- general/el_fun/deg2rad.m
+++ general/el_fun/deg2rad.m
@@ -1,7 +1,7 @@
 function rad = deg2rad(deg)
 %DEG2RAD   conversion of degrees to radians
 %
-% Note the officla Mathworks DEG2RAD is part of the mapping toolbox.
+% Note the official Mathworks DEG2RAD is part of the mapping toolbox.
 %
 %See also: RAD2DEG, ANGLE2DOMAIN, MAP
 
--- general/el_fun/rad2deg.m
+++ general/el_fun/rad2deg.m
@@ -1,7 +1,7 @@
 function deg = rad2deg(rad)
-%RAD2DEG   conversion of degrees to radians
+%RAD2DEG   conversion of radians to degrees
 %
-% Note the officla Mathworks RAD2DEG is part of the mapping toolbox.
+% Note the official Mathworks RAD2DEG is part of the mapping toolbox.
 %
 %See also: DEG2RAD, ANGLE2DOMAIN, MAP
 
