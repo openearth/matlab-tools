923b863a31386362fba1958c76b7e17c066a6262
ormondt <ormondt@localhost>
2015-10-11 18:26:01 +0000

    tsunami toolbox dynamic fault rupture updates
    svn path=/trunk/matlab/; revision=12288
    
    
    Former-commit-id: 1a01545bef6d17682f9ddf345d28973c71509833

--- applications/DelftDashBoard/toolboxes/Tsunami/subfaults/example/dynamic_fault_rupture_test.m
+++ applications/DelftDashBoard/toolboxes/Tsunami/subfaults/example/dynamic_fault_rupture_test.m
@@ -1,12 +1,20 @@
 clear variables;close all;
 
+% Limits
 xlower = 135;
 xupper = 150;
 ylower = 30;
 yupper = 45;
-refdate=datenum(2015,10,9);
+
+dx=180; % horizontal resolution in seconds
+dt=6;   % time step in sdu file in seconds
+grdfile='japan.grd'; % Delft3D grid file
+sdufile='japan01.sdu'; % Delft3D sdu file
+inifile='japan01.ini';
+
+refdate=datenum(2015,10,9); % reference data in sdu file
 
 subfaultfile='ucsb_subfault_2011_03_11_v3.cfg';
 
-dynamic_fault_rupture('subfaultfile',subfaultfile,'xlim',[xlower xupper],'ylim',[ylower yupper],'dx',180,'dt',6,'grdfile','japan.grd','sdufile','japan01.sdu','refdate',refdate,'inifile','japan01.ini');
+dynamic_fault_rupture('subfaultfile',subfaultfile,'xlim',[xlower xupper],'ylim',[ylower yupper],'dx',dx,'dt',dt,'grdfile',grdfile,'sdufile',sdufile,'refdate',refdate,'inifile',inifile);
 
