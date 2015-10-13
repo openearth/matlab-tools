9c369e89c6a5097a56294e53cbcd535953d4714d
kaaij <kaaij@localhost>
2015-10-13 15:47:22 +0000

    svn path=/trunk/matlab/; revision=12299
    Former-commit-id: b6fd81cbfef71154edf1c7a737835d1f0f944ba4

--- applications/delft3d/nest_matlab/nesthd1/nesthd_detnst_dflowfm.m
+++ applications/delft3d/nest_matlab/nesthd1/nesthd_detnst_dflowfm.m
@@ -1,10 +1,9 @@
-      function [names,weight,x_nes,y_nes] = nesthd_detnst  (x,y,name_coarse,xbnd,ybnd,sphere)
+      function [names,weight,x_nes,y_nes] = nesthd_detnst  (x,y,name_coarse,xbnd,ybnd,sphere,i_type)
 
       % detnst_dflowfm determines coordinates nest stations and belonging weight factors
-      % x,y, are cell centres (water level points, flow nodes)
+      %
 
       %% initialise
-
       no_pnt                = size(xbnd,2);
       x_nes (1:no_pnt,4  ) = 0 ;
       y_nes (1:no_pnt,4  ) = 0 ;
@@ -14,63 +13,47 @@
               names {i_pnt,i_nes} = '';
           end
       end
-      
-      
+
       %% Create triangulation netwerk through flow nodes (cell centres)
       tri = delaunay(x,y);
 
       %% For all of the boundary support points
       for i_pnt = 1: no_pnt
-          toc ; display (i_pnt) ;tic ;
-          waitbar(i_pnt/no_pnt);
+
+          waitbar(((i_type-1)*no_pnt + i_pnt)/(3*no_pnt));
           xbsp = xbnd  (i_pnt);
           ybsp = ybnd  (i_pnt);
 
           %% find surrounding flow nodes
           if ~isnan(xbsp)
 
-              inside        = false;
-              dist          = nesthd_detlength(x,y,xbsp,ybsp,'Spherical',sphere);
-              [~,idualface] = min(dist);
-              %% First search neighbouring points
-              % create selection of neighbouring flow nodes
-              neighbours    = [find(tri(:,1) == idualface); find(tri(:,2) == idualface); find(tri(:,3) == idualface)];
-
-              % see if point can be found within neighbouring point
-              for i_neighbour = 1: length(neighbours)
-                  inside = insidepoly(xbsp, ybsp, x(tri(neighbours(i_neighbour),1:3)), y(tri(neighbours(i_neighbour),1:3)));
-                  if inside
-                      idualface = neighbours(i_neighbour) ;
-                      break;
+              inside = false;
+              if i_pnt > 1
+                  %% First search neighbouring points
+                  % create selection of neighbouring nodes
+                  neighbours = [];
+                  points    = idualface;
+                  for i_depth = 1: 3
+                      for i_corner_1 =  1: 3
+                          for i_corner_2 = 1: 3
+                              for ii_pnt = 1: length(points)
+                                  [tmp]      = find(tri(:,i_corner_2) == tri(points(ii_pnt),i_corner_1));
+                                  neighbours = [neighbours; tmp];
+                              end
+                          end
+                      end
+                      neighbours  = unique(neighbours);
+                      points      = neighbours;
+                  end
+                  % see if point can be found within neighbouring point
+                  for i_neighbour = 1: length(neighbours)
+                      inside = insidepoly(xbsp, ybsp, x(tri(neighbours(i_neighbour),1:3)), y(tri(neighbours(i_neighbour),1:3)));
+                      if inside
+                          idualface = neighbours(i_neighbour);
+                          break;
+                      end
                   end
               end
-%               if i_pnt > 1
-%                   
-%                   %% First search neighbouring points
-%                   % create selection of neighbouring nodes
-%                   neighbours = [];
-%                   points    = idualface;
-%                   for i_depth = 1: 3
-%                       for i_corner_1 =  1: 3
-%                           for i_corner_2 = 1: 3
-%                               for i = 1: length(points)
-%                                   [tmp]      = find(tri(:,i_corner_2) == tri(points(i),i_corner_1));
-%                                   neighbours = [neighbours; tmp];
-%                               end
-%                           end
-%                       end
-%                       neighbours  = unique(neighbours);
-%                       points      = neighbours;
-%                   end
-%                   % see if point can be found within neighbouring point
-%                   for i_neighbour = 1: length(neighbours)
-%                       inside = insidepoly(xbsp, ybsp, x(tri(neighbours(i_neighbour),1:3)), y(tri(neighbours(i_neighbour),1:3)));
-%                       if inside
-%                           idualface = neighbours(i_neighbour) ;
-%                           break;
-%                       end
-%                   end
-%               end
 
               %% not found within neigbouring points
               if inside ~= 1
@@ -104,4 +87,3 @@
               end
           end
       end
- 
--- applications/delft3d/nest_matlab/nesthd1/nesthd_wrista_2.m
+++ applications/delft3d/nest_matlab/nesthd1/nesthd_wrista_2.m
@@ -26,8 +26,8 @@ switch filetype
         for i_pnt = 1: no_pnt
             for iwght = 1: 4
                 itel            = itel + 1;
-%                 name_hulp(itel) = string_mnnes{i_pnt,iwght};
-                name_hulp(itel) = string_mnnes(i_pnt,iwght);
+                name_hulp(itel) = string_mnnes{i_pnt,iwght};
+%                name_hulp(itel) = string_mnnes(i_pnt,iwght);
             end
         end
         %
@@ -37,9 +37,8 @@ switch filetype
         switch filetype
             case {'Delft3D','SIMONA'}
                 % Remove first point if m = 0
-%                 m = str2num(name_hulp{1}(10:13));
-                m = cellfun(@isempty,name_hulp(1));
-                if m == 1
+                m = str2num(name_hulp{1}(10:13));
+                if m == 0
                     name_hulp = name_hulp(2:end);
                 end
         end
@@ -107,7 +106,7 @@ switch filetype
         for i_pnt = 1: size(string_mnnes,1)
             for i_nes = 1: 4
                 if ~isempty(string_mnnes{i_pnt,i_nes})
-                    fprintf(fid,'%14.9e %14.9e %s \n',x_nest(i_pnt,i_nes),y_nest(i_pnt,i_nes),cell2str(string_mnnes{i_pnt,i_nes}));
+                    fprintf(fid,'%14.9e %14.9e %s \n',x_nest(i_pnt,i_nes),y_nest(i_pnt,i_nes),(string_mnnes{i_pnt,i_nes}));
                 end
             end
         end
