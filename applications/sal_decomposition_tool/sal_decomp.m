af552c96559f7b3cbe9686edc2427caf19b75f75
kaaij <kaaij@localhost>
2016-01-05 16:45:24 +0000

    Slightly different method of computing the individual terms. Just use the weighed velocities (weighed with the relative cell size). ==> Perfect closure: check = sum(terms(1:16)) Computation of dispersion coefficient added!
    svn path=/trunk/matlab/; revision=12479
    
    
    Former-commit-id: bf4cb839d49ecb28110dd3e7d2433b0281ed2190

--- applications/sal_decomposition_tool/sal_decomp.m
+++ applications/sal_decomposition_tool/sal_decomp.m
@@ -22,10 +22,13 @@ function sal_decomp(varargin)
 % ana_start   = '20510101  091000'
 % ana_stop    = '20511130  220000'
 % -----------------------------------------------------------------
-%%
-%   Output: tekal-file with total and decomposed fluxes.
 %
-
+%%  If the history file doe not exist, a list of stations needed for the
+%   decomposition is generated
+%
+%%  Output: tekal-file with total and decomposed fluxes.
+%           tekalmap-file with salinties for plotting
+%
 %% Copyright notice
 %   --------------------------------------------------------------------
 %   Copyright (C) 2015 <Deltares>
@@ -90,16 +93,15 @@ for i_sim = 1: length(OPT.Filinp)
     %% Construct filenames
     myrun    = ['trih-' run] ;                                       % run id
     mymap    = ['trim-' run] ;
-    
+
     %% Check if trih file exist, if not, generate a list of stations needed for decomposition
     action = 'decomp';
     if ~exist([mydir filesep myrun '.dat'],'file') action = 'initial';end;
-    
+
     %% Read cross section
     crs    = delft3d_io_crs('read',crsfil) ;
     no_crs = crs.NTables ;
     
-    %% Determine type of cross section (u or v)
     for i_crs = 1: no_crs
         if crs.DATA(i_crs).mn(1) == crs.DATA(i_crs).mn(3)
             type{i_crs} = 'u';
@@ -107,11 +109,12 @@ for i_sim = 1: length(OPT.Filinp)
             type{i_crs} = 'v';
         end
     end
-    
+
+
     %% decomposition of fluxes
     switch action
         case 'decomp'
-            
+
             %% Read history data
             %  times
             display ('Read data history file ')
@@ -119,13 +122,13 @@ for i_sim = 1: length(OPT.Filinp)
             T        = vs_time(myfile) ;                                                      % read timesteps
             it_start = find( T.datenum >= datenum(start(2:end-1),'yyyymmdd HHMMSS'),1,'first' ) ;
             it_stop  = find( T.datenum >= datenum(stop (2:end-1),'yyyymmdd HHMMSS'),1,'first' ) ;
-            
+
             %  stations
             trih     = vs_use(myfile);                                                        % open trih file
             stat     = vs_trih_station(trih);                                                 % read stations
             namst    = cellstr(stat.name);                                                    % names monitoring points
             no_stat  = size(namst,1);                                                         % total number monitoring points
-            
+
             %  series
             sal      = vs_let(trih,'his-series',{it_start:it_stop},'GRO',{0 0 1});            % salinity
             zcuru    = vs_let(trih,'his-series',{it_start:it_stop},'ZCURU',{0 0});            % u velocity
@@ -133,34 +136,36 @@ for i_sim = 1: length(OPT.Filinp)
             zwl      = vs_let(trih,'his-series',{it_start:it_stop},'ZWL',{0});                % water level
             dps      = vs_let(trih,'his-series',{it_start:it_stop},'DPS',{0});                % depth (cell centres)
             catos    = vs_let(trih,'his-series',{it_start:it_stop},'ATR',{0 0});              % cumulated advective transport
-            
+
             %  names an coordinates
             mnstat   = squeeze(vs_let(trih,'his-const','MNSTAT')) ;                           % m,n stations
             mntra    = squeeze(vs_let(trih,'his-const','MNTRA')) ;                            % m,n crs-sections
             namtra   = squeeze(vs_let(trih,'his-const','NAMTRA')) ;                           % name crs-sections
-            
-            
+
+
             %% Read const data map file
             display ('Read const data map file ')
             myfile   = [mydir filesep mymap '.dat'];                                          % data file
             trim     = vs_use(myfile);                                                        % open trim file
-            
+
             xcor     = vs_let(trim,'map-const','XCOR');                                       % X-coordinates grid points
             ycor     = vs_let(trim,'map-const','YCOR');                                       % Y-coordinates grid points
             thick    = vs_let(trim,'map-const','THICK');                                      % Thickness layers
-            
+
             %% retrieve data grid data
             G                            = vs_meshgrid2dcorcen(trim);
             guu(2:G.nmax-1,2:G.mmax-1)   = G.cen.guu ;
             gvv(2:G.nmax-1,2:G.mmax-1)   = G.cen.gvv ;
-            
+            gvu(2:G.nmax-1,1:G.mmax-1)   = G.u.gvu   ;
+            guv(1:G.nmax-1,2:G.mmax-1)   = G.v.guv   ; 
+
             %% Set constants
             kmax     = length(thick)    ;
             mmax     = G.mmax ;
             nmax     = G.nmax ;
             times    = 1:size((sal),1)  ;
             no_times = length(times) ;
-            
+
             %% Calculate areas per timestep per obs-point
             display ('calculate areas per timestep per obs-point (to use in eq.8 of doc. of Yoeri)')
             crsu_area(1:no_times,1:no_stat) = 0;
@@ -176,27 +181,37 @@ for i_sim = 1: length(OPT.Filinp)
                     crsv_area(:,istat)   =crsv_area(:,istat) + gvv(n,m) * thick(k) * wd;         %% calculated areas
                 end
             end
-            
+
             %% Mask transects
             display ('Mask transects')
-            mymask = false(no_stat,no_crs);
+            mymask  = false(no_stat,no_crs);
+            mymask2 = false(no_stat,no_crs);
             for i_crs =1:no_crs
                 for i_pnt =1:length(crs.DATA(i_crs).m)
                     m = crs.DATA(i_crs).m(i_pnt);
                     n = crs.DATA(i_crs).n(i_pnt);
                     i_stat = find(mnstat(1,:) == m & mnstat(2,:) == n) ;
                     mymask(i_stat,i_crs) = true;
+                    if strcmp(type{i_crs},'u')
+                        i_stat = find(mnstat(1,:) == m + 1 & mnstat(2,:) == n); 
+                    elseif strcmp(type{i_crs},'v')
+                        i_stat = find(mnstat(1,:) == m     & mnstat(2,:) == n + 1);
+                    end
+                    if ~isempty (i_stat)
+                        mymask2(i_stat,i_crs) = true;
+                    else
+                        mymask2 = mymask;
+                    end
                 end
                 no_crs_tra(i_crs) = find(strcmp(strtrim(crs.DATA(i_crs).name),char2cell(namtra)) ==1) ;
             end
-            
+
             %% Decomposition Yoeri (Loop over transects)
             no_terms = 16;
             terms    = nan(no_crs,no_terms);
             for i_crs=1 :no_crs
-                
+
                 %% check transport
-                no_pts = length(crs.DATA(i_crs).m);
                 no_pts = length(find(mymask(:,i_crs)==1)) ; %Number of stations buiding the crs section derived from the history stations
                 if strcmp(type{i_crs},'u')
                     % u section
@@ -210,7 +225,7 @@ for i_sim = 1: length(OPT.Filinp)
                 ss        =sal  (times,mymask(:,i_crs),:);
                 ff        =uu.*ss.*aa;
                 fff(i_crs)=mean(sum(sum(ff,3),2));
-                
+
                 %% Cross-sections and weights
                 if strcmp(type{i_crs},'u')
                     % u section
@@ -225,46 +240,84 @@ for i_sim = 1: length(OPT.Filinp)
                     Avar    = repmat(sum(crsv_area(:,mymask(:,i_crs)),2),[1,no_pts,kmax]);
                     weights = crs2_v(:,mymask(:,i_crs),:)./Avar;
                 end
+
+                %%  create weighed time series of velocity 
+                weights = weights*no_pts*kmax;
+                uu      = uu.*weights;
                 
-                %%  velocities uu already determined (zcuru or zcurv)
-                ua=CsumTavg(uu.*weights);
-                ub=Csum(uu.*weights)-repmat(ua,[no_times,1]);
+                %% decompose velocity
+                ua=1/A*CTavg(uu.*Avar);
+                ub=1/A*Cavg(uu.*Avar)-repmat(ua,[no_times,1]);
                 uc=Tavg(uu)-repmat(ua,[1, no_pts,kmax]);
                 ud=uu-repmat(ub,[1,no_pts,kmax])- ...
-                    repmat(uc,[no_times,1,1]) - ...
-                    repmat(ua,[no_times,no_pts,kmax]);
+                      repmat(uc,[no_times,1,1]) - ...
+                      repmat(ua,[no_times,no_pts,kmax]);
                 
-                %%  salinities ss already determined
-                sa=CsumTavg(ss.*weights);
-                sb=Csum(ss.*weights)-repmat(sa,[no_times,1]);
+                %% decompose salinity
+                sa=1/A*CTavg(ss.*Avar);
+                sb=1/A*Cavg(ss.*Avar)-repmat(sa,[no_times,1]);
                 sc=Tavg(ss)-repmat(sa,[1, no_pts,kmax]);
                 sd=ss-repmat(sb,[1,no_pts,kmax])- ...
-                    repmat(sc,[no_times,1,1]) - ...
-                    repmat(sa,[no_times,no_pts,kmax]);
+                      repmat(sc,[no_times,1,1]) - ...
+                      repmat(sa,[no_times,no_pts,kmax]);
                 
                 %% Construct the terms
-                term(1) =sa*ua*A;
-                term(2) =sa*Tavg(ub.*aap);
-                term(3) =sa*CsumTavg(repmat(uc,[no_times,1,1]).*weights.*Avar);
-                term(4) =sa*CsumTavg(ud.*weights.*Avar);
+                term(1)=sa*ua*A;
+                term(2)=sa*Tavg(ub.*aap);
+                term(3)=sa*CTavg(repmat(uc,[no_times,1,1]).*Avar);
+                term(4)=sa*CTavg(ud.*Avar);
                 
-                term(5) =ua*Tavg(sb.*aap);
-                term(6) =Tavg(ub.*sb.*(A+aap));
-                term(7) =Tavg(sb.*Csum(uc.*Tavg(weights)).*aap);
-                term(8) =Tavg(sb.*Csum(ud.*weights).*(A+aap));
+                term(5)=ua*Tavg(sb.*aap);
+                term(6)=Tavg(ub.*sb.*(A+aap));
+                term(7)=Tavg(sb.*Cavg(uc).*aap);
+                term(8)=Tavg(sb.*Cavg(ud).*(A+aap));
                 
-                term(9) =ua*CsumTavg(repmat(sc,[no_times,1,1]).*Avar.*weights);
-                term(10)=Tavg(ub.*Csum(sc.*Tavg(weights)).*aap);
-                term(11)=Csum(Tavg(weights).*uc.*sc*A);
-                term(12)=Csum(Tavg(weights).*sc.*Tavg(ud.*Avar));
+                term(9)=ua*CTavg(repmat(sc,[no_times,1,1]).*Avar);
+                term(10)=Tavg(ub.*Cavg(sc).*aap);
+                term(11)=Cavg(uc.*sc*A);
+                term(12)=Cavg(sc.*Tavg(ud.*Avar));
                 
-                term(13)=ua*CsumTavg(sd.*Avar.*weights);
-                term(14)=Tavg(ub.*Csum(sd.*weights).*(A+aap));
-                term(15)=Csum(uc.*Tavg(sd.*Avar).*Tavg(weights));
-                term(16)=CsumTavg(sd.*ud.*Avar.*weights);
+                term(13)=ua*CTavg(sd.*Avar);
+                term(14)=Tavg(ub.*Cavg(sd).*(A+aap));
+                term(15)=Cavg(uc.*Tavg(sd.*Avar));
+                term(16)=CTavg(sd.*ud.*Avar);
                 
                 %% Store
                 terms(i_crs,1:length(term))=term;
+            end
+            
+            %% Combine the terms
+            for i_crs = 1: no_crs
+                mtx(i_crs)      = sum(terms(i_crs,1:end));
+                river(i_crs)    =     terms(i_crs,1    )     ;
+                tide(i_crs)     =     terms(i_crs,2    )  + sum(terms(i_crs,5: 6));
+                variation(i_crs)= sum(terms(i_crs,3:4  )) + sum(terms(i_crs,7:16));
+            end
+            
+            %% Compute dispersion coeffcient, start with concentration gradient
+            for i_crs = 1: no_crs
+                
+                s1       = CTavg(sal  (times,mymask (:,i_crs),:));
+                s2       = CTavg(sal  (times,mymask2(:,i_crs),:));
+                dist     = 0.;
+                if strcmp(type{i_crs},'u')
+                    m  = crs.DATA(i_crs).mn(1);
+                    n(1) = crs.DATA(i_crs).mn(2);
+                    n(2) = crs.DATA(i_crs).mn(4);
+                    n    = sort(n);
+                    dist = mean(gvu(n(1):n(2),m));
+                elseif strcmp(type{i_crs},'v')
+                    n  = crs.DATA(i_crs).mn(2);
+                    m(1) = crs.DATA(i_crs).mn(1);
+                    m(2) = crs.DATA(i_crs).mn(3);
+                    m    = sort(m);
+                    dist = mean(guv(n,m(1):m(2)));
+                end
+                dcdx(i_crs) = (s2-s1)/dist;
+                D   (i_crs) = 0.;
+                if dcdx(i_crs) > 1e-20
+                    D   (i_crs) = -1.*variation(i_crs)/(A*dcdx(i_crs));
+                end
             end
             
             %% Print to output file
@@ -281,45 +334,40 @@ for i_sim = 1: length(OPT.Filinp)
             fprintf (fid,'%s \n','* tide      = term2 + term5 + term6') ;
             fprintf (fid,'%s \n','* variation = term3 + term4 + term7 + term8 + (term9 until 16)') ;
             fprintf (fid,'%s \n','* ') ;
-            
+
             %   1234567890112345678901123456789011234567890112345678901123456789011234567890112345678901123456789011234567890112345678901 12345678901
-            fprintf (fid,'%s%s%s \n','* cs       check  check_atr        mtx      river       tide  variation     dummy      ',...
-                'term1      term2      term3      term4      term5      term6      term7      term8      ',... ;
-                'term9     term10     term11     term12     term13     term14     term15     term16') ;
-            
+            fprintf (fid,'%s%s%s%s \n','* cs       check  check_atr        mtx      river       tide  variation     dummy      ',...
+                 'term1      term2      term3      term4      term5      term6      term7      term8      ',... ;
+                 'term9     term10     term11     term12     term13     term14     term15     term16      ',...
+                 ' dcdx          k');
+
             fprintf (fid,'%s \n',   'BL01') ;
-            fprintf (fid,'%4i %i \n',no_crs, 24) ;
-            
+            fprintf (fid,'%4i %i \n',no_crs, 26) ;
+
             %% Print the results (the decomposed fluxes)
             for i_crs = 1: no_crs
-                %% Combine
-                mtx      = sum(terms(i_crs,1:end));
-                river    =     terms(i_crs,1    )     ;
-                tide     =     terms(i_crs,2    )  + sum(terms(i_crs,5: 6));
-                variation= sum(terms(i_crs,3:4  )) + sum(terms(i_crs,7:16));
                 dummy    = 999.999;
-                
-                %% print to file
-                fprintf (fid,['%4i  ' repmat('%10.3f ',1,no_terms + 7) ' \n'], i_crs, fff(i_crs), catos(end,no_crs_tra(i_crs))/(T.t(end)-T.t(1)),mtx, ...
-                    river,tide,variation,dummy,terms(i_crs,:));
+                fprintf (fid,['%4i  ' repmat('%10.3f ',1,no_terms + 7) '%12.6f ' '%10.0f \n'],             ... 
+                    i_crs         , fff (i_crs), catos(end,no_crs_tra(i_crs))/(T.t(end)-T.t(1)),mtx(i_crs), ...
+                    river(i_crs)  , tide(i_crs), variation(i_crs),dummy,                                    ...
+                    terms(i_crs,:),dcdx(i_crs) ,D(i_crs)               );
             end
-            
             fclose(fid);
-            
+
             %% A second file with matrix of mean max and min salinities
             point   = strfind(outfil,'.');
             outfil = [outfil(1:point(end)-1) '.map'];
             fid     = fopen (outfil,'w');
-            
+
             for i_crs = 1: no_crs
-                
+
                 % averaging, maximum and minimum
                 dp_mean(i_crs)  = -1.0*mean(mean(dps(:,mymask(:,i_crs))));
                 s1_mean(i_crs)  =      mean(mean(zwl(:,mymask(:,i_crs))));
                 sal_mean(i_crs,:) =      mean(mean(sal(:,mymask(:,i_crs),:),2),1);
                 sal_max (i_crs,:) =      max (mean(sal(:,mymask(:,i_crs),:),2),[],1);
                 sal_min (i_crs,:) =      min (mean(sal(:,mymask(:,i_crs),:),2),[],1);
-                
+
                 % positions
                 x(i_crs,1) = i_crs;
                 y(i_crs,1) = s1_mean(i_crs) - 0.5*thick(1)*(s1_mean(i_crs) - dp_mean(i_crs));
@@ -328,7 +376,7 @@ for i_sim = 1: length(OPT.Filinp)
                     y(i_crs,k) = y(i_crs,k-1) - thick(k)*(s1_mean(i_crs) - dp_mean(i_crs));
                 end
             end
-            
+
             % write to output file
             fprintf(fid,'* Salinities \n');
             fprintf(fid,'* Column 1: x-coordinate (cross section number) \n');
@@ -345,7 +393,7 @@ for i_sim = 1: length(OPT.Filinp)
                         sal_min (i_crs,k)                 );
                 end
             end
-            
+
             %% Close tekal file
             fclose(fid) ;
         case 'initial'
@@ -383,12 +431,12 @@ for i_sim = 1: length(OPT.Filinp)
     end
     clearvars -except OPT;
 end
-
 end
-%% Csum cross-section-summation
-function out=Csum(in)
 
-out=sum(sum(in,3),2);
+%% Cave cross-section-averaging
+function out=Cavg(in)
+
+out=mean(mean(in,3),2);
 
 end
 
@@ -399,9 +447,12 @@ out=mean(in,1);
 
 end
 
-%% CsumTavg cross-section-summation and time-average function
-function out=CsumTavg(in)
+%% CTavg cross-section and time averaging function
+function out=CTavg(in)
 
-out=mean(sum(sum(in,3),2),1);
+out=mean(mean(mean(in,3),2),1);
 
 end
+
+
+
