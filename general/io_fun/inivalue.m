d56b53f018528feb6f741466b1cdc84e73ad8ea9
vroom_ja <vroom_ja@localhost>
2016-01-14 13:56:55 +0000

    svn path=/trunk/matlab/; revision=12487
    Former-commit-id: d38d3c4a4e33d801de7e07049a0bf38f81c5caee

--- general/io_fun/inivalue.m
+++ general/io_fun/inivalue.m
@@ -44,32 +44,33 @@ function varargout=inivalue(fileName,varargin)
 OPT.commentchar = '';
 
 fid = 0;
+jv = -1;
 
 sectionName = [];
 keyName     = [];
 
 if nargin > 1; 
-if isstruct(varargin{1})
-   OPT = setproperty(OPT,varargin{1});
-else
-  sectionName = varargin{1};
-end
+    if isstruct(varargin{1})
+       OPT = setproperty(OPT,varargin{1});
+    else
+      sectionName = varargin{1};
+    end
 end
 
 if nargin > 2; 
-if isstruct(varargin{2})
-   OPT = setproperty(OPT,varargin{2});
-else
-  keyName = varargin{2};
-end
+    if isstruct(varargin{2})
+       OPT = setproperty(OPT,varargin{2});
+    else
+      keyName = varargin{2};
+    end
 end
 
 if nargin > 3; 
-if isstruct(varargin{3})
-   OPT = setproperty(OPT,varargin{3});
-else
-  error('');
-end
+    if isstruct(varargin{3})
+       OPT = setproperty(OPT,varargin{3});
+    else
+       error('');
+    end
 end
 
 if exist(fileName) ~= 2 
@@ -82,114 +83,117 @@ if fid<=0
     floce(fid)
 end;
    
-   if ~isnan(sectionName)
-   sectionString = ['[' sectionName ']'];
-   else
-   sectionString = ['['  ']'];
-   end
-   sectionFound  = 0;
-   rec           = fgetl_no_comment_line(fid,OPT.commentchar);
-   
-   while sectionFound~=1
-   
-      if isempty(rec)
-         rec = fgetl_no_comment_line(fid,OPT.commentchar);
-         continue;
-      elseif rec==-1
-         break;
-      end;
+if ~isnan(sectionName)
+sectionString = ['[' sectionName ']'];
+else
+sectionString = ['['  ']'];
+end
+sectionFound  = 0;
+rec           = fgetl_no_comment_line(fid,OPT.commentchar);
 
-      if isempty(sectionName)| isnan(sectionName) | strcmp(strtrim(rec), sectionString) > 0 % allow leading spaces
-      
-          if isempty(sectionName)
-             i0=find(rec=='[');
-             i1=find(rec==']');
-             section = rec(i0+1:i1-1);
-             rec          = fgetl_no_comment_line(fid,OPT.commentchar);
-          elseif isnan(sectionName)
-             section = '';
-          else
-             section = sectionName;
-             rec          = fgetl_no_comment_line(fid,OPT.commentchar);
-          end
-          
-       %% look for the key
-          
-          sectionFound = 1;
-          keyFound     = 0;
-          
-          while keyFound==0
+while sectionFound~=1
 
-             if isempty(rec)==1
-                  rec = fgetl_no_comment_line(fid,OPT.commentchar);
-                  continue;
-              end;
-              if rec==-1 % EOF
-                  break;
-              end;
-              if isempty(strtrim(rec)) == 0 
-                  rec = strtrim(sscanf(rec, '%c')); % keep all whitespaces except leading and trailing
-                  if rec(1)=='[' % next section                   
-                      break;
-                  end;
-                  
-              %% look for the value
-              
-                  eq_idx=find(rec=='=');
-                  if ~isempty(eq_idx) %~=1 & eq_idx(1)>1 
-                      key=strtrim(rec(1:eq_idx(1)-1));
-                      if isempty(keyName) | strcmp(key, keyName)>0 
-                          [rs, cs]=size(rec);
-                          if eq_idx(1)>=cs
-                              keyValue = '';
-                          else
-                              keyValue=strtrim(rec(eq_idx(1)+1:end));
-                          end;
-                          if strcmp(key, keyName)>0 
-                             keyFound = 1;
-                             out      = keyValue;
-                             break;
-                          else
-                             if isempty(sectionName)
-                             out.(mkvar(section)).(mkvar(key)) = keyValue; % in case section name has spaces etc. use mkvar()
-                             else
-                             out.(mkvar(key)) = keyValue;
-                             end
-                          end
+    if isempty(rec)
+        rec = fgetl_no_comment_line(fid,OPT.commentchar);
+        continue;
+    elseif rec==-1
+        break;
+    end;
 
-                      end;
-                  end;               
-              end;
-              
-              rec = fgetl_no_comment_line(fid,OPT.commentchar);
-              if rec==-1
-                  break
-              end
-              
-              rec = strtrim(sscanf(rec, '%c')); % keep all whitespaces except leading and trailing
+    if isempty(sectionName)| isnan(sectionName) | strcmp(strtrim(rec), sectionString) > 0 % allow leading spaces
 
-	      if ~isempty(rec)
-	      if rec(1)=='[' % next section                   
+        if isempty(sectionName)
+            i0=find(rec=='[');
+            i1=find(rec==']');
+            section = rec(i0+1:i1-1);
+            rec          = fgetl_no_comment_line(fid,OPT.commentchar);
+        elseif isnan(sectionName)
+            section = '';
+        else
+            section = sectionName;
+            rec          = fgetl_no_comment_line(fid,OPT.commentchar);
+        end
+        
+        %% look for the key
 
-                 if isempty(sectionName)
-                    i0=find(rec=='[');
-                    i1=find(rec==']');
-                    section = rec(i0+1:i1-1);
-                 else
-                    section = sectionName;
-                 end
+        sectionFound = 1;
+        keyFound     = 0;
 
-                 rec = fgetl_no_comment_line(fid,OPT.commentchar);
+        while keyFound==0
 
-	      end
-	      else
-	         break
-	      end
-	      
-          end; % keyFound
-          break;
-      end; % sectionString
-      rec = fgetl_no_comment_line(fid,OPT.commentchar);
-   end; % sectionFound
-   fclose(fid);
-   varargout = {out};
+            if isempty(rec)==1
+                rec = fgetl_no_comment_line(fid,OPT.commentchar);
+                continue;
+            end;
+            if rec==-1 % EOF
+                break;
+            end;
+            if isempty(strtrim(rec)) == 0 
+                rec = strtrim(sscanf(rec, '%c')); % keep all whitespaces except leading and trailing
+                if rec(1)=='[' % next section                   
+                    break;
+                end;
+
+                %% look for the value
+
+                eq_idx=find(rec=='=');
+                if ~isempty(eq_idx) %~=1 & eq_idx(1)>1 
+                    key=strtrim(rec(1:eq_idx(1)-1));
+                    if isempty(keyName) | strcmp(key, keyName)>0 
+                        [rs, cs]=size(rec);
+                        if eq_idx(1)>=cs
+                            keyValue = '';
+                        else
+                            keyValue=strtrim(rec(eq_idx(1)+1:end));
+                        end;
+                        if strcmp(key, keyName)>0 
+                            keyFound = 1;
+                            out      = keyValue;
+                            break;
+                        else
+                            if isempty(sectionName)
+                                out.(mkvar(section)).(mkvar(key)) = keyValue; % in case section name has spaces etc. use mkvar()
+                            else
+                                out.(mkvar(key)) = keyValue;
+                            end
+                        end
+
+                    end;
+                end;               
+            end;
+
+            rec = fgetl_no_comment_line(fid,OPT.commentchar);
+            if rec==-1
+                break
+            end
+
+            rec = strtrim(sscanf(rec, '%c')); % keep all whitespaces except leading and trailing
+
+            if ~isempty(rec)
+                if rec(1)=='[' % next section                   
+
+                    if isempty(sectionName)
+                        i0=find(rec=='[');
+                        i1=find(rec==']');
+                        section = rec(i0+1:i1-1);
+                    else
+                        section = sectionName;
+                    end
+                    
+                    jv = jv+1;
+                    section = [section num2str(jv)];
+
+                    rec = fgetl_no_comment_line(fid,OPT.commentchar);
+
+                end
+            else
+                break
+            end
+
+        end; % keyFound
+        break;
+    end; % sectionString
+    rec = fgetl_no_comment_line(fid,OPT.commentchar);
+end; % sectionFound
+fclose(fid);
+varargout = {out};
