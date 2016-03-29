5860234eaf56c5777014471772501169d15b4cf2
huism_b <huism_b@localhost>
2016-03-29 14:23:39 +0000

    extended file IO of RAY and SCO files
    svn path=/trunk/matlab/; revision=12645
    
    
    Former-commit-id: 14c2b7201ddbdbc68f4cb24fd1bf0d79af3bfe96

--- applications/unibest/fileio/readSCO.m
+++ applications/unibest/fileio/readSCO.m
@@ -177,6 +177,15 @@ elseif numFields==8 && SCOdata.isTimeseries
     end
 end
 
+
+% read tide table (not yet implemented)
+%if ~SCOdata.isTimeseries
+%    for mm=1:length()
+%        SCOdata.Ptide 
+%    end
+%end
+
+
 if nargout==1
     varargout{1}=SCOdata;
 else
