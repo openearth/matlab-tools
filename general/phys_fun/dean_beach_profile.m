4b92bbfbac11680b53a34b52ff52930d42247c9e
ormondt <ormondt@localhost>
2016-04-01 09:44:03 +0000

    dean beach profile added
    svn path=/trunk/matlab/; revision=12659
    
    
    Former-commit-id: 801040307c73a11f6b18ddc9b689dddf37cb8e2f

---
+++ general/phys_fun/dean_beach_profile.m
@@ -0,0 +1,79 @@
+function [x,z,a,m]=dean_beach_profile(dx,zmin,zmax,beta_dry,varargin)
+% Computes Dean beach profile z=Ax^m
+%
+% Inputs:
+%   dx       : grid spacing
+%   zmin     : minimum elevation, e.g. -20 m (below sea level)
+%   zmax     : maximum elevation, e.g. +5 m (above sea level)
+%   beta_dry : dry beach slope 
+%
+% Optional inputs:
+%   d50  : sediment diameter (m)
+%   A    : profile scale parameter
+%   m    : profile exponent (default 2/3)
+% Either A or d50 needs to be supplied as input
+%
+% Example:
+% [x,z,a,m]=dean_beach_profile(1.0,-20,10,0.01,'d50',0.0002);
+
+
+m=2/3;
+a=[];
+D=[];
+
+for ii=1:length(varargin)
+    if ischar(varargin{ii})
+        switch lower(varargin{ii})
+            case{'a'}
+                a=varargin{ii+1};
+            case{'m'}
+                m=varargin{ii+1};
+            case{'d50'}
+                D=varargin{ii+1};
+        end
+    end
+end
+
+if isempty(a)
+    if isempty(D)
+        error('Please supply either a or D50!');
+    end    
+    % Compute a
+    vfall=fall_velocity_vanrijn2007(D,15);
+    a = 0.51 * vfall ^ 0.44;
+end
+
+x=-10000:dx:10000;
+
+z=-a*x.^(m);
+z2=-beta_dry*x;
+z(x<0)=z2(x<0);
+
+ifirst=find(z>=zmax,1,'last');
+ilast=find(z<=zmin,1,'first');
+x=x(ifirst:ilast);
+z=z(ifirst:ilast);
+
+%%
+function ws=fall_velocity_vanrijn2007(dss,temp)
+
+% Fall velocity according to Van Rijn 2007
+rhoint=1024;
+rhosol=2650;
+dsand=0.000064;
+dgravel=0.002;
+ag=9.81;
+s = rhosol / rhoint;
+vcmol = 4.0e-5 / (20.0 + temp);
+if dss < 1.5*dsand
+    ws = (s-1.0) * ag * dss^2/(18.0*vcmol);
+elseif dss < 0.5*dgravel
+    if dss < 2.0*dsand
+        coefw = (-2.9912/dsand) * dss + 15.9824;
+    else
+        coefw = 10.0;
+    end
+    ws = coefw * vcmol / dss * (sqrt(1.0 + (s-1.0)*ag*dss^3 / (100.0*vcmol^2)) - 1.0);
+else
+    ws = 1.1 * sqrt((s-1.0)*ag*dss);
+end
