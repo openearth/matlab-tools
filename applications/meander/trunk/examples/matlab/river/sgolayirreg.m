e229328e2b171ac90144f977848b8e96d4674e36
ottevan <ottevan@localhost>
2016-04-22 14:04:24 +0000

    update
    svn path=/trunk/matlab/; revision=12703
    
    
    Former-commit-id: 31fbcf5ed75ed02aeb69dcd84e39eb8db685f397

--- applications/meander/trunk/examples/matlab/river/sgolayirreg.m
+++ applications/meander/trunk/examples/matlab/river/sgolayirreg.m
@@ -1,20 +1,45 @@
-function [y0,y1,y2,y0m] = sgolayirreg(x,y,degree,halfwin);
-% x=[0:0.05:2]+0.025*rand(size([0:0.05:2]));
+function [y0,y1,y2,y0m] = sgolayirreg(x,y,degree,halfwindowsize);
+% [y0,y1,y2,y0m] = SGOLAYIRREG(x,y,degree,halfwin);
+% Perform Savitsky-Golay filtering for irregularly spaced data
+%
+% Example usage:
+% x=[0:0.01:2]+0.025*rand(size([0:0.05:2]));
 % y=sin(pi*[0:0.05:2])+0.025*rand(size([0:0.05:2]));
+%
+% [y0,y1,y2,y0m] = sgolayirreg(x,y,3,7);
+% plot(x,y,'r.',x,y0)
+%
+% Output: 0-th, 1-st and 2nd derivative of y with respect to x. 
+% 
+% Copyright (C) 2016 Willem Ottevanger
+%
+% This program is free software; you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation; either version 3 of the License, or
+% (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 nmax =length(x);
-xe = [x(1)+[-halfwin:-1]*(x(2)-x(1)),x,x(end)+[1:halfwin]*(x(end)-x(end-1))];
-ye = [y(1)+[-halfwin:-1]*(y(2)-y(1)),y,y(end)+[1:halfwin]*(y(end)-y(end-1))];
-for k = 1+halfwin:nmax+halfwin;
-    nl = k-halfwin;%max(1,k-halfwin);
-    nr = k+halfwin;%min(nmax+halfwin,k+halfwin);
-p = polyfit(xe(nl:nr),ye(nl:nr),degree);
-p1 = polyder(p);
-p2 = polyder(p1);
-y0(k-halfwin) = polyval(p,xe(k));
-y1(k-halfwin) = polyval(p1,xe(k));
-y2(k-halfwin) = polyval(p2,xe(k));
-y0L(k-halfwin) = polyval(p,xe(k)-0.5*(xe(k)-xe(k-1)));
-y0R(k-halfwin) = polyval(p,xe(k)+0.5*(xe(k+1)-xe(k)));
+xe = [x(1)+[-halfwindowsize:-1]*(x(2)-x(1)),x,x(end)+[1:halfwindowsize]*(x(end)-x(end-1))];
+ye = [y(1)+[-halfwindowsize:-1]*(y(2)-y(1)),y,y(end)+[1:halfwindowsize]*(y(end)-y(end-1))];
+for k = 1+halfwindowsize:nmax+halfwindowsize;
+    nl = k-halfwindowsize;%max(1,k-halfwin);
+    nr = k+halfwindowsize;%min(nmax+halfwin,k+halfwin);
+    p = polyfit(xe(nl:nr),ye(nl:nr),degree);
+    y0(k-halfwindowsize) = polyval(p,xe(k));
+    p1 = polyder(p);
+    p2 = polyder(p1);
+    y1(k-halfwindowsize) = polyval(p1,xe(k));
+    y2(k-halfwindowsize) = polyval(p2,xe(k));
+    y0L(k-halfwindowsize) = polyval(p,xe(k)-0.5*(xe(k)-xe(k-1)));
+    y0R(k-halfwindowsize) = polyval(p,xe(k)+0.5*(xe(k+1)-xe(k)));
 end
 y0m=0.5*(y0L(2:nmax)+y0R(1:nmax-1));
 % figure(1);
