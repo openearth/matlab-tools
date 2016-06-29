7c1b2252e8af848e201a9ac3b429f6e423a485a3
nederhof <nederhof@localhost>
2016-06-29 12:09:17 +0000

    Add function for plotting a georeference image
    svn path=/trunk/matlab/; revision=12789
    
    
    Former-commit-id: def271490773e208eee2dbca6e62db11beb40d7b

---
+++ applications/DelftDashBoard/toolboxes/GeoImage/georefence_image.m
@@ -0,0 +1,80 @@
+function [x,y,z,col] = georefence_image(filename)
+% v1.0 ??????? Ormondt
+% v1.1 June-16 Nederhoff
+
+% Get full filename
+filename1 = [filename, '.jpg'];
+filename2 = [filename, '.jgw'];
+[jpgcol X] = imread(filename1); 
+
+% Other
+sz = size(jpgcol);
+
+step=1;
+jpgcol=jpgcol(1:step:sz(1),1:step:sz(2),:);
+col=double(jpgcol)/255;
+
+[x,y]=meshgrid(1:step:sz(2),1:step:sz(1));
+
+% Read text
+fid=fopen(filename2);
+eof=0;
+k=0;
+
+while ~eof
+
+    tx0=fgets(fid);
+    
+    if tx0==-1
+        eof=1;
+        break;
+    end
+    
+    if and(ischar(tx0), size(tx0>0))
+        v0=strread(tx0,'%q');
+    else
+        v0='';
+    end
+    if size(v0,1)>0
+        if strcmp(tx0(1),'#')==0
+            v=strread(tx0,'%q');
+            nowords=size(v,1);
+            for j=1:nowords
+                k=k+1;
+                txt{k}=v{j};
+            end
+            clear v;
+        end
+    end
+end
+fclose(fid);
+
+k=1;
+dx=str2double(txt{k});
+k=k+1;
+roty=str2double(txt{k});
+k=k+1;
+rotx=str2double(txt{k});
+k=k+1;
+dy=str2double(txt{k});
+k=k+1;
+x0=str2double(txt{k});
+k=k+1;
+y0=str2double(txt{k});
+
+a=dx*step;
+d=roty;
+b=rotx;
+e=dy*step;
+c=x0;
+f=y0;
+
+x0=x;
+y0=y;
+
+x=a*x0+b*y0+c;
+y=d*x0+e*y0+f;
+
+z=zeros(size(x));
+
+end
