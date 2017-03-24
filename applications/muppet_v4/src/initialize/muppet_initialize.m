bc640d3d542894adbfea1beb4da9a358593dd702
ormondt <ormondt@localhost>
2017-03-24 11:51:45 +0000

    muppet4 xml files added to settings folder
    svn path=/trunk/matlab/; revision=13222
    
    
    Former-commit-id: 4de5d610ae2200bbe0713f725fb94fcaca77cb17

--- applications/muppet_v4/src/initialize/muppet_initialize.m
+++ applications/muppet_v4/src/initialize/muppet_initialize.m
@@ -2,6 +2,6 @@ function handles=muppet_initialize(handles)
 
 handles=muppet_getCoordinateSystems(handles);
 handles=muppet_initializeAnimationSettings(handles);
-handles.dateformats=muppet_readDateFormats([handles.settingsdir 'defaults' filesep 'dateformats.def']);
+handles.dateformats=muppet_readDateFormats([handles.settingsdir 'dateformats' filesep 'dateformats.def']);
 handles.colormaps=muppet_importColorMaps([handles.settingsdir 'colormaps' filesep]);
 handles=muppet_readXmlFiles(handles);
