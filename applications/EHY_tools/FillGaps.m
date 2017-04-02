e86d808d3e97a53847ba670b909a44cad42f5fe6
kaaij <kaaij@localhost>
2017-04-02 16:48:10 +0000

    svn path=/trunk/matlab/; revision=13244
    Former-commit-id: d66a287ee7e0f48da6cca3258fd498c8c1e1a6f4

--- applications/EHY_tools/FillGaps.m
+++ applications/EHY_tools/FillGaps.m
@@ -12,18 +12,22 @@ values_new = values;
 max_diff   = 365;
 
 %% As long as there are time differences larger than interval
-while max_diff > OPT.interval
-    time_diff = times(2:end) - times(1:end-1);
-    index     = find(time_diff > OPT.interval + eps,1,'first');
-    if max(time_diff) > OPT.interval
+time_diff = times(2:end) - times(1:end-1);
+max_diff  = max(time_diff);
+while max_diff > OPT.interval + eps
+    index     = find(time_diff > OPT.interval + eps,1,'first')
+    if ~isempty(index)
         %% add NaN in between
         times_new  = vertcat(times (1:index), times(index) + OPT.interval, times (index + 1:end));
         values_new = vertcat(values(1:index), NaN                        , values(index + 1:end));
         times      = times_new;
         values     = values_new;
     end
+    time_diff = times(2:end) - times(1:end-1);
     max_diff = max(time_diff);
 end
+
+disp(max_diff)
     
 
 
