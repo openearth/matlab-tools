39a615ca0213166fed110adc744c17a8d9de0563
groenenb <groenenb@localhost>
2017-05-05 10:29:08 +0000

    svn path=/trunk/matlab/; revision=13320
    Former-commit-id: 8d81c85ba2f67b10173edd496ce27ff4e4b178ed

--- applications/EHY_tools/EHY_dflowfm_status.m
+++ applications/EHY_tools/EHY_dflowfm_status.m
@@ -2,16 +2,25 @@ function EHY_dflowfm_status(varargin)
 %% EHY_dflowfm_status(varargin)
 %
 % Example1: EHY_dflowfm_status
-% Example2: EHY_dflowfm_status('D:\model.mdu')
+% Example2: EHY_dflowfm_status('D:\run1\')
+% Example3: EHY_dflowfm_status('D:\run1\model.mdu')
 %
 % created by Julien Groenenboom, May 2017
 
 %%
-if nargin>0
-    mdFile=varargin{1};
-else
+if nargin==0
     [filename, pathname]=uigetfile('*.mdu','Open a .mdu file');
     mdFile=[pathname filename];
+elseif nargin==1
+    [pathstr, name, ext] = fileparts(varargin{1});
+    if strcmp(ext,'.mdu')
+        mdFile=varargin{1};
+    else
+        D=dir([pathstr '\*.mdu']);
+        mdFile=[D(1).folder filesep D(1).name];
+    end   
+else
+    error('Too many input arguments')
 end
 
 [pathstr, name, ext] = fileparts(mdFile);
@@ -31,6 +40,6 @@ line=strsplit(line);
 runPeriod_S=str2num(line{2});
 runPeriod_D=runPeriod_S/3600/24;
 
-disp(['Status: ' num2str(runPeriod_D) '/' num2str(simPeriod_D) ' days - ',...
+disp(['Status of ' name ext ': ' num2str(runPeriod_D) '/' num2str(simPeriod_D) ' days - ',...
     sprintf('%0.1f',runPeriod_S/simPeriod_S*100) '%']);
 
