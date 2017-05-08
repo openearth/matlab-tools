bde75d7578bd180ecfb7ec17b2e072f17fd0eec9
groenenb <groenenb@localhost>
2017-05-08 11:52:12 +0000

    svn path=/trunk/matlab/; revision=13328
    Former-commit-id: c21227818ef20e4b5b8bc2dcc72654c05fe519f3

---
+++ applications/EHY_tools/support_functions/getTimeInfoFromMdFile.m
@@ -0,0 +1,49 @@
+function [refdate,tunit,tstart,tstop]=getTimeInfoFromMdFile(mdFile)
+
+modelType=nesthd_det_filetype(mdFile);
+
+switch modelType
+    case 'mdu'
+        mdu=dflowfm_io_mdu('read',mdFile);
+        refdate=datenum(num2str(mdu.time.RefDate),'yyyymmdd');
+        tunit=mdu.time.Tunit;
+        tstart=mdu.time.TStart;
+        tstop=mdu.time.TStop;
+    case 'mdf'
+        mdf=delft3d_io_mdf('read',mdFile);
+        refdate=datenum(mdf.keywords.itdate,'yyyy-mm-dd');
+        tunit=mdf.keywords.tunit;
+        tstart=mdf.keywords.tstart;
+        tstop=mdf.keywords.tstop;
+    case 'siminp'
+        [pathstr,name,ext]=fileparts(mdFile);
+        siminp=readsiminp(pathstr,[name ext]);
+        try % if DATE, TSTART and TSTOP are on separate lines
+            ind=strmatch('DATE',siminp.File);
+            [~,refdate]=strtok(siminp.File{ind},'''');
+            % lets see if it TSTART,was on same line as DATE
+            if ~isempty(findstr('tstart',lower(refdate)))
+                error;
+            end
+            tunit='M';
+            ind=strmatch('TSTART',siminp.File);
+            [~,tstart]=strtok(siminp.File{ind},' ');
+            tstart=str2double(tstart); %deal with .00
+            ind=strmatch('TSTOP',siminp.File);
+            [~,tstop]=strtok(siminp.File{ind},' ');
+            tstop=str2double(tstop); %deal with .00
+        catch % if DATE, TSTART and TSTOP are NOT on separate lines
+            ind=strmatch('DATE',siminp.File);
+            split=strsplit(siminp.File{ind});
+            indDate=strmatch('date',lower(split),'exact');
+            indStart=strmatch('tstart',lower(split),'exact');
+            indStop=strmatch('tstop',lower(split),'exact');
+            refdate=datestr(datenum(lower(strjoin(split(indDate+1:indStart-1)))));
+            tunit='M';
+            tstart=str2double(split{indStart+1}); %deal with .00
+            tstop=str2double(split{indStop+1}); %deal with .00
+        end
+end
+
+
+
---
+++ applications/EHY_tools/support_functions/timeFactor.m
@@ -0,0 +1,19 @@
+function factor=timeFactor(fromTunit,toTunit)
+
+tunits={'S','M','H','D','Y'};
+factors=[60 60 24 365.25];
+
+if ~ismember(fromTunit,tunits) || ~ismember(toTunit,tunits)
+   error(['fromTunit and toTunit have to be: ' strjoin(tunits(1:end-1) ) ' or ' tunits{end}]) 
+end
+
+ind1=strmatch(lower(fromTunit),lower(tunits));
+ind2=strmatch(lower(toTunit),lower(tunits));
+
+if ind1>ind2
+    factor=1/prod(factors(ind2:ind1-1));
+elseif ind1<ind2
+    factor=prod(factors(ind1:ind2-1));
+elseif ind1==ind2
+    factor=1;
+end
