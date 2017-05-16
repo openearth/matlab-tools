408c1e1373044390f0faa75eeea213a252974dd4
groenenb <groenenb@localhost>
2017-05-16 09:52:28 +0000

    svn path=/trunk/matlab/; revision=13352
    Former-commit-id: bc8510457daa50f3079681a92a3ead5f3c3a146d

--- applications/EHY_tools/support_functions/timeFactor.m
+++ applications/EHY_tools/support_functions/timeFactor.m
@@ -11,9 +11,9 @@ ind1=strmatch(lower(fromTunit),lower(tunits));
 ind2=strmatch(lower(toTunit),lower(tunits));
 
 if ind1<ind2
-    factor=1/prod(factors(ind2:ind1-1));
+    factor=1/prod(factors(ind1:ind2-1));
 elseif ind1>ind2
-    factor=prod(factors(ind1:ind2-1));
+    factor=prod(factors(ind2:ind1-1));
 elseif ind1==ind2
     factor=1;
 end
