8f543049c23bd1ab318543e5d1f27275683617a3
groenenb <groenenb@localhost>
2017-06-01 08:11:59 +0000

    svn path=/trunk/matlab/; revision=13379
    Former-commit-id: 55aa4dda1edda67b3d3e5bab896b3e58f2f2daba

--- applications/EHY_tools/support_functions/getTimeInfoFromMdFile.m
+++ applications/EHY_tools/support_functions/getTimeInfoFromMdFile.m
@@ -18,33 +18,20 @@ switch modelType
     case 'siminp'
         [pathstr,name,ext]=fileparts(mdFile);
         siminp=readsiminp(pathstr,[name ext]);
-        try % if DATE, TSTART and TSTOP are on separate lines
-            ind=strmatch('DATE',siminp.File);
-            [~,refdate]=strtok(siminp.File{ind},'''');
-            refdate=datenum(refdate);
-            % lets see if it TSTART,was on same line as DATE
-            if ~isempty(findstr('tstart',lower(refdate)))
-                error;
+        
+        tunit='M';
+        
+        for var={'date','tstart','tstop'}
+            ind1=find(~cellfun(@isempty,strfind(lower(siminp.File),var{1})));
+            ind2=regexp(lower(siminp.File{ind1}),var{1})+length(var{1});
+            if strcmp(var{1},'date')
+                refdate=datenum(strtrim(siminp.File{ind1}(ind2:end)));
+            else
+                eval([var{1} '=str2double(siminp.File{ind1}(ind2:end));'])
             end
-            tunit='M';
-            ind=strmatch('TSTART',siminp.File);
-            [~,tstart]=strtok(siminp.File{ind},' ');
-            tstart=str2double(tstart); %deal with .00
-            ind=strmatch('TSTOP',siminp.File);
-            [~,tstop]=strtok(siminp.File{ind},' ');
-            tstop=str2double(tstop); %deal with .00
-        catch % if DATE, TSTART and TSTOP are NOT on separate lines
-            ind=strmatch('DATE',siminp.File);
-            split=strsplit(siminp.File{ind});
-            indDate=strmatch('date',lower(split),'exact');
-            indStart=strmatch('tstart',lower(split),'exact');
-            indStop=strmatch('tstop',lower(split),'exact');
-            refdate=datenum(lower(strjoin(split(indDate+1:indStart-1))));
-            tunit='M';
-            tstart=str2double(split{indStart+1}); %deal with .00
-            tstop=str2double(split{indStop+1}); %deal with .00
         end
 end
+
 
 
 
