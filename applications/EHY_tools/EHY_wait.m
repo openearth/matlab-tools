da42b9c21141880ae798167231a51000ec008c98
groenenb <groenenb@localhost>
2017-06-23 12:48:17 +0000

    svn path=/trunk/matlab/; revision=13403
    Former-commit-id: d116c9e18066433e9382e468f22913f7e332cf9a

--- applications/EHY_tools/EHY_wait.m
+++ applications/EHY_tools/EHY_wait.m
@@ -10,14 +10,16 @@ if ~isnumeric(time)
     time=datenum(time);
 end
 
+[pathstr, name, ext] = fileparts(path);
+
 keepLooping=1;
 while keepLooping
     if now > time
         run(path)
         keepLooping=0;
     else
-        disp(['Waiting untill ' datestr(time) ' - time now: ' datestr(now)]);
-         pause(10)
+        disp(['Waiting untill ' datestr(time) ' to run the script ''' name ext ''' - time now: ' datestr(now)]);
+         pause(5)
     end
 end
 
