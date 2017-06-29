dcae7936ce61de4401981f9ebf172685ed0c84f8
verploe <verploe@localhost>
2017-06-29 11:03:57 +0000

    nu met runnen .bash_profile
    svn path=/trunk/matlab/; revision=13421
    
    
    Former-commit-id: 81d9a324cbe0600b7bff5b272ccc814314c59783

--- applications/EHY_tools/PuTTY/OpenPuttyH6inCurrentFolder.bat
+++ applications/EHY_tools/PuTTY/OpenPuttyH6inCurrentFolder.bat
@@ -9,10 +9,12 @@ set DRIVE=%CURDIR:~0,1%
 set SCRIPTDIR=%~dp0%
 set OUTPUTFILE="n:\My Documents\unix-h6\run_ext.sh"
 
-ECHO cd "%GODIR%" > %OUTPUTFILE%
-ECHO clear >> %OUTPUTFILE%
-ECHO . /etc/bashrc >> %OUTPUTFILE%
-ECHO /bin/bash >> %OUTPUTFILE%
+ECHO . .bash_profile  > %OUTPUTFILE%
+ECHO cd "%GODIR%"    >> %OUTPUTFILE%
+ECHO clear           >> %OUTPUTFILE%
+ECHO . /etc/bashrc   >> %OUTPUTFILE%
+ECHO /bin/bash       >> %OUTPUTFILE%
+
 
 
 start "" "C:\Program Files (x86)\Putty\putty.exe" -ssh USERNAME@h6.directory.intra -pw PASSWORD  -m %OUTPUTFILE% -t
