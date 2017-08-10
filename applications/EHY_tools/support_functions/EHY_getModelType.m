bedc1fcf874792515711266a02b0a03f4eceb401
groenenb <groenenb@localhost>
2017-08-10 07:20:27 +0000

    svn path=/trunk/matlab/; revision=13554
    Former-commit-id: 53d75fcd921f43922aaa444ecfb281063d6d0f85

---
+++ applications/EHY_tools/support_functions/EHY_getModelType.m
@@ -0,0 +1,6 @@
+function modelType=EHY_getModelType(path)
+    path=[path filesep];
+    mdFiles=[dir([path '*.mdu']); dir([path '*.mdf']); dir([path '*siminp*']); dir([path '*waqpro*'])];
+    mdFile=[mdFiles(1).folder filesep mdFiles(1).name];
+    modelType=nesthd_det_filetype(mdFile);
+
