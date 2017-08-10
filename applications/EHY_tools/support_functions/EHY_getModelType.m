38d140adb4ac27f17fc0ae3c951cfdf63c2d98e5
groenenb <groenenb@localhost>
2017-08-10 07:24:29 +0000

    svn path=/trunk/matlab/; revision=13555
    Former-commit-id: 235703a0f8dc99cbac6cf80396e8f7c7a0eb2e1e

--- applications/EHY_tools/support_functions/EHY_getModelType.m
+++ applications/EHY_tools/support_functions/EHY_getModelType.m
@@ -1,4 +1,4 @@
-function modelType=EHY_getModelType(path)
+function [modelType,mdFile]=EHY_getModelType(path)
     path=[path filesep];
     mdFiles=[dir([path '*.mdu']); dir([path '*.mdf']); dir([path '*siminp*']); dir([path '*waqpro*'])];
     mdFile=[mdFiles(1).folder filesep mdFiles(1).name];
