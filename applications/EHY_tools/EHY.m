0a03bc7c3050222cfb1e0eb772acbb6f2fdecbea
groenenb <groenenb@localhost>
2017-08-11 12:54:30 +0000

    svn path=/trunk/matlab/; revision=13571
    Former-commit-id: cceb8034ff59c7d46ec6af2d83721b3d1bfa4ea4

---
+++ applications/EHY_tools/EHY.m
@@ -0,0 +1,29 @@
+function EHY
+[pathstr ~] = fileparts(which('EHY'));
+D=dir([pathstr filesep '*.m']);
+
+NoInputNeeded={};
+for iD=1:length(D)
+    fid=fopen([pathstr filesep D(iD).name],'r');
+    line=fgetl(fid);
+    fclose(fid);
+    if ~isempty(strfind(line,'(varargin)'))
+        NoInputNeeded{end+1,1}=D(iD).name;
+    end
+end
+
+% delete certain scripts
+NoInputNeeded(strmatch('EHY_opendap',NoInputNeeded))=[];
+NoInputNeeded(strmatch('EHY_plot_google_map',NoInputNeeded))=[];
+
+% select which function to use
+selection=  listdlg('PromptString',['Which function would you like to use:'],...
+    'SelectionMode','single',...
+    'ListString',strrep(NoInputNeeded,'.m',''),...
+    'ListSize',[300 200]);
+% run selection
+if ~isempty(selection)
+    run(NoInputNeeded{selection})
+else
+    disp('No function was selected.')
+end
