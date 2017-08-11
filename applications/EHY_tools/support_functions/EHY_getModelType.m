567cb7f4a4789d51a877493f778a68b754c772f6
groenenb <groenenb@localhost>
2017-08-11 09:42:36 +0000

    svn path=/trunk/matlab/; revision=13568
    Former-commit-id: dc35f375699586feea5981c01c5e07aa0a0685eb

--- applications/EHY_tools/support_functions/EHY_getModelType.m
+++ applications/EHY_tools/support_functions/EHY_getModelType.m
@@ -1,11 +1,11 @@
 function [modelType,mdFile]=EHY_getModelType(path)
 [pathstr, name, ext] = fileparts(path);
-if ~isempty(ext) % a file was given, but not a mdf,mdu or siminp. Let's search in the folder
+if isempty(ext) % a file was given, but not a mdf,mdu or siminp. Let's search in the folder
     path=pathstr;
 end
 
 path=[path filesep];
-mdFiles=[dir([path '*.mdu']); dir([path '*.mdf']); dir([path '*siminp*']); dir([path '*waqpro*'])];
+mdFiles=[dir([path '*.mdu']); dir([path '*.mdf']); dir([path '*siminp*'])];
 [~,order] = sort([mdFiles.datenum]);
 mdFile=[mdFiles(order(end)).folder filesep mdFiles(order(end)).name];
 modelType=nesthd_det_filetype(mdFile);
