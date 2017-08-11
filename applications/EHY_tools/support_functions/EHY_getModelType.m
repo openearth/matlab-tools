d403ef88a1d714114f5f86d692c48c28298fbe54
groenenb <groenenb@localhost>
2017-08-11 13:11:46 +0000

    svn path=/trunk/matlab/; revision=13573
    Former-commit-id: d32535c06db8e7ce650d5ac1ed95f034fa51c8fa

--- applications/EHY_tools/support_functions/EHY_getModelType.m
+++ applications/EHY_tools/support_functions/EHY_getModelType.m
@@ -1,18 +1,15 @@
 function [modelType,mdFile]=EHY_getModelType(path)
 
-% [modelType,mdFile]=EHY_getModelType('p:\1230071.011-g6-dhydro\DHYDRO_2017_q1\Runs\F04_2_dtmax100')
+% [modelType,mdFile]=EHY_getModelType('p:\1230071.011-g6-dhydro\DHYDRO_2017_q1\Runs\F04_2_dtmax100\unstruc.dia')
 
-
-mdFiles=[dir([path '*.mdu']); dir([path '*.mdf']); dir([path '*siminp*'])];
+mdFiles=[dir([path filesep '*.mdu']); dir([path filesep '*.mdf']); dir([path filesep '*siminp*'])];
 if ~isempty(mdFiles)
-[~,order] = sort([mdFiles.datenum]);
-mdFile=[mdFiles(order(end)).folder filesep mdFiles(order(end)).name];
-modelType=nesthd_det_filetype(mdFile);
+    [~,order] = sort([mdFiles.datenum]);
+    mdFile=[mdFiles(order(end)).folder filesep mdFiles(order(end)).name];
+    modelType=nesthd_det_filetype(mdFile);
 else % a file in the run directory was given
-    
-    
-   [pathstr, name, ext] = fileparts(path);
-   [modelType,mdFile]=EHY_getModelType([pathstr filesep]);
+    [pathstr, name, ext] = fileparts(path);
+    [modelType,mdFile]=EHY_getModelType([pathstr filesep]);
 end
 
 
