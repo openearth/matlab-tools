790b336b94f50fbcaf65a17495fa40f13f1cd4b1
groenenb <groenenb@localhost>
2017-08-15 06:44:10 +0000

    svn path=/trunk/matlab/; revision=13584
    Former-commit-id: 0f274aefbab1fb26039fe34b8f97f58bd56944ce

--- applications/EHY_tools/support_functions/EHY_getModelType.m
+++ applications/EHY_tools/support_functions/EHY_getModelType.m
@@ -3,7 +3,7 @@ function [modelType,mdFile]=EHY_getModelType(path)
 mdFiles=[dir([path filesep '*.mdu']); dir([path filesep '*.mdf']); dir([path filesep '*siminp*'])];
 if ~isempty(mdFiles)
     [~,order] = sort([mdFiles.datenum]);
-    mdFile=[mdFiles(order(end)).folder filesep mdFiles(order(end)).name];
+    mdFile=[mdFiles(order(1)).folder filesep mdFiles(order(1)).name];
     modelType=nesthd_det_filetype(mdFile);
 else % a file in the run directory was given
     [pathstr, name, ext] = fileparts(path);
