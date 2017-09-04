2e0334a68f385b9940d8a3fb5904133b9861cfb6
groenenb <groenenb@localhost>
2017-09-04 09:59:44 +0000

    svn path=/trunk/matlab/; revision=13638
    Former-commit-id: 6beaf1f8a5ef506ec664c12e35c0e0cb43c4b813

---
+++ applications/EHY_tools/EHY_xy2mn.m
@@ -0,0 +1,28 @@
+function [m,n]=EHY_xy2mn(x,y,grdFile)
+% [m,n]=EHY_xy2mn(x,y,grdFile)
+%
+% Find the m,n-location(s) of your x,y-location(s) in a structured
+% Delft3D/SIMONA grid.
+% 
+% Example1: [m,n]=EHY_xy2mn(70000,445000,'D:\Nederland.grd')
+% Example2: [m,n]=EHY_xy2mn([70000 80000],[445000 455000],'D:\Nederland.grd')
+% 
+% created by Julien Groenenboom, May 2017
+
+tempGrdFile=[tempdir 'tmp.grd'];
+copyfile(grdFile,tempGrdFile);
+grd=wlgrid('read',tempGrdFile);
+delete(tempGrdFile);
+
+grd.Xcen=corner2centernan(grd.X);
+grd.Ycen=corner2centernan(grd.Y);
+
+if length(x)~=length(y); error('x and y should have the same length'); end
+
+for ixy=1:length(x)
+dist=sqrt( (grd.Xcen-x(ixy)).^2 + (grd.Ycen-y(ixy)).^2 );
+[mm,nn]=find(dist==min(min(dist)));
+m(ixy)=mm(1)+1;
+n(ixy)=nn(1)+1;
+end
+
