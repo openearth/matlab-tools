9760820cafb196d94d7fd003983355cec1cacabf
ormondt <ormondt@localhost>
2017-09-06 12:11:27 +0000

    ddb update
    svn path=/trunk/matlab/; revision=13667
    
    
    Former-commit-id: 86c7bd6a0f5a37d500a42dfb9fb3b91332d183fa

--- applications/DelftDashBoard/general/tropicalcyclones/fileio/tc_read_jtwc_best_track.m
+++ applications/DelftDashBoard/general/tropicalcyclones/fileio/tc_read_jtwc_best_track.m
@@ -68,12 +68,14 @@ tc.name='not_known';
 
 fid=fopen(fname,'r');
 while 1
-    s=fgetl(fid);
-    if s==-1
+    s0=fgetl(fid);
+    if s0==-1
         break
     end
-    s=textscan(s,'%s%f%f%s%s%f%s%s%f%f%s%f%s%f%f%f%f%f%f%f%f%f%s%f%s%f%f%s%s','delimiter',',');
-    tc.basin=s{1}{1};
+    s=textscan(s0,'%s','delimiter',',');
+    s=s{1};
+%    s=textscan(s0,'%s%f%f%s%s%f%s%s%f%f%s%f%s%f%f%f%f%f%f%f%f%f%s%f%s%f%f%s%s','delimiter',',');
+    tc.basin=s{1};
     tc.storm_number=s{2};
     % Read time
     tstr=s{3};tstr=num2str(tstr);
@@ -83,77 +85,77 @@ while 1
         tstr = tstr(1,:);    
         newtime=datenum(tstr,'yyyymmddHH');
     end
-    
+    hrs=str2double(s{6});
+    newtime=newtime+hrs/24;    
     if newtime>lasttime+1/86400
         % New time point found
         it=it+1;
         lasttime=newtime;
         tc=set_default_values(tc,it);
     end
-    if it==11
-        datestr(newtime);
-    end
     tc.time(it)=newtime;
     % Latitude
-    if strcmpi(s{7}{1}(end),'n')
-        tc.y(it)=0.1*str2double(s{7}{1}(1:end-1));
+    if strcmpi(s{7}(end),'n')
+        tc.y(it)=0.1*str2double(s{7}(1:end-1));
     else
-        tc.y(it)=-0.1*str2double(s{7}{1}(1:end-1));
+        tc.y(it)=-0.1*str2double(s{7}(1:end-1));
     end
     % Longitude
-    if strcmpi(s{8}{1}(end),'e')
-        tc.x(it)=0.1*str2double(s{8}{1}(1:end-1));
+    if strcmpi(s{8}(end),'e')
+        tc.x(it)=0.1*str2double(s{8}(1:end-1));
     else
-        tc.x(it)=-0.1*str2double(s{8}{1}(1:end-1));
+        tc.x(it)=-0.1*str2double(s{8}(1:end-1));
     end
-    if s{9}>0
-        tc.vmax(it)=s{9};
+    if str2double(s{9})>0
+        tc.vmax(it)=str2double(s{9});
     end
-    if s{10}>0
-        tc.pc(it)=s{10};
+    if str2double(s{10})>0
+        tc.pc(it)=str2double(s{10});
     end
     tc.cat=s{11};
     % Radii
-    r=s{12};
+    r=str2double(s{12});
     if ~isempty(r)
         switch r
             case{34,35}
-                tc.r35ne(it)=s{14};
-                tc.r35se(it)=s{15};
-                tc.r35sw(it)=s{16};
-                tc.r35nw(it)=s{17};
+                tc.r35ne(it)=str2double(s{14});
+                tc.r35se(it)=str2double(s{15});
+                tc.r35sw(it)=str2double(s{16});
+                tc.r35nw(it)=str2double(s{17});
             case 50
-                tc.r50ne(it)=s{14};
-                tc.r50se(it)=s{15};
-                tc.r50sw(it)=s{16};
-                tc.r50nw(it)=s{17};
+                tc.r50ne(it)=str2double(s{14});
+                tc.r50se(it)=str2double(s{15});
+                tc.r50sw(it)=str2double(s{16});
+                tc.r50nw(it)=str2double(s{17});
             case{64,65}
-                tc.r65ne(it)=s{14};
-                tc.r65se(it)=s{15};
-                tc.r65sw(it)=s{16};
-                tc.r65nw(it)=s{17};
+                tc.r65ne(it)=str2double(s{14});
+                tc.r65se(it)=str2double(s{15});
+                tc.r65sw(it)=str2double(s{16});
+                tc.r65nw(it)=str2double(s{17});
             case 100
-                tc.r100ne(it)=s{14};
-                tc.r100se(it)=s{15};
-                tc.r100sw(it)=s{16};
-                tc.r100nw(it)=s{17};
+                tc.r100ne(it)=str2double(s{14});
+                tc.r100se(it)=str2double(s{15});
+                tc.r100sw(it)=str2double(s{16});
+                tc.r100nw(it)=str2double(s{17});
         end
     end
     
     
     if ~isempty(s{18})
-        tc.pressure_last_closed_isobar(it)=s{18};
+        tc.pressure_last_closed_isobar(it)=str2double(s{18});
     end
     if ~isempty(s{19})
-        tc.radius_last_closed_isobar(it)=s{19};
+        tc.radius_last_closed_isobar(it)=str2double(s{19});
     end
     if ~isempty(s{20})
-        if s{20}>0
-            tc.rmax(it)=s{20};
+        if str2double(s{20})>0
+            tc.rmax(it)=str2double(s{20});
         end
     end
-    if ~isempty(s{28});
-        tc.name=s{28}{1};
+    if length(s)>=28
+    if ~isempty(s{28})
+        tc.name=s{28};
+    end
     end
 end
 fclose(fid);
