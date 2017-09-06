e1f8d63324f92bae1e03fe0fb925659161b55a5e
groenenb <groenenb@localhost>
2017-09-06 09:30:29 +0000

    svn path=/trunk/matlab/; revision=13655
    Former-commit-id: 8c0e1eeb0c78a9444ee1e8e1a23b582f39cc1409

--- applications/EHY_tools/support_functions/MATLAB_functions/iscellstr.m
+++ applications/EHY_tools/support_functions/MATLAB_functions/iscellstr.m
@@ -1,10 +1,17 @@
-%ISCELLSTR Determine whether input is cell array of character vectors
-%   ISCELLSTR(S) returns 1 if S is a cell array of character vectors and 0
-%   otherwise.  A cell array of character vectors is a cell array where 
-%   every element is a character vector.
+function tf = iscellstr(s)
+%ISCELLSTR True for cell array of strings.
+%   ISCELLSTR(S) returns 1 if S is a cell array of strings and 0
+%   otherwise.  A cell array of strings is a cell array where 
+%   every element is a character array.
 %
-%   See also CELLSTR, ISCELL, CHAR, ISCHAR, STRING, ISSTRING.
+%   See also CELLSTR, ISCELL, CHAR, ISCHAR.
 
-%   Copyright 1984-2016 The MathWorks, Inc.
-%   Built-in function.
+%   Copyright 1984-2003 The MathWorks, Inc.
+%   $Revision$  $Date$
 
+if isa(s,'cell'), 
+  res = cellfun('isclass',s,'char'); 
+  tf = all(res(:)); 
+else 
+  tf = false; 
+end 
