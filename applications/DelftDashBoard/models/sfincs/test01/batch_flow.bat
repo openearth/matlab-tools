f14ff543a6f79961caa9c397113b6c425658fb62
ormondt <ormondt@localhost>
2017-09-06 14:53:36 +0000

    ddb update
    svn path=/trunk/matlab/; revision=13682
    
    
    Former-commit-id: b8c713064872b6aefac386614f16fa8993aeed38

--- applications/DelftDashBoard/models/sfincs/test01/batch_flow.bat
+++
@@ -1,5 +0,0 @@
-@ echo off
-set argfile=config_d_hydro.xml
-set flowexedir="d:\checkouts\opendelft3d_002\bin\win32\flow2d3d\bin\"
-set PATH=%flowexedir%;%waveexedir%;%PATH%
-%flowexedir%\d_hydro.exe %argfile%
