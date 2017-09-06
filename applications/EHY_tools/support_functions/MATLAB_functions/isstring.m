f1f20ef9f89f98f2bcebafc619ead6ff25919de0
groenenb <groenenb@localhost>
2017-09-06 09:34:33 +0000

    svn path=/trunk/matlab/; revision=13656
    Former-commit-id: c095d0891b88a3f32593664e68dfa872033504e5

--- applications/EHY_tools/support_functions/MATLAB_functions/isstring.m
+++ applications/EHY_tools/support_functions/MATLAB_functions/isstring.m
@@ -1,15 +1,6 @@
-%ISSTRING Determine whether input is string array
-%   ISSTRING(S) returns true if S is a string array and 0 otherwise.
-%
-%   Examples:
-%       S = string({'Smith','Burns','Jones'});
-%       isstring(S)                             % returns 1
-%
-%       S = 'Mary Jones';
-%       isstring(S)                             % returns 0
-%
-%   See also STRING, ISCHAR, ISCELLSTR.
-
-%   Copyright 2016 The MathWorks, Inc.
-
-%   Built-in function.
+function tf = isstring(s)
+if isa(s,'char'), 
+  tf = true; 
+else 
+  tf = false; 
+end 
