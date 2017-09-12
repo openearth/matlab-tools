8282df361c315ed7da1370073b87f5fb3847e363
groenenb <groenenb@localhost>
2017-09-12 14:06:26 +0000

    svn path=/trunk/matlab/; revision=13701
    Former-commit-id: d5512473a3ee31e309e61768da23c1443af31355

--- applications/EHY_tools/EHY_model2GoogleEarth.m
+++ applications/EHY_tools/EHY_model2GoogleEarth.m
@@ -31,45 +31,57 @@ if ~exist(outputDir); mkdir(outputDir); end
 OPT.saveoutputFile=1;
 switch modelType
     case 'mdu'
+        mdu=delft3d_io_mdu('read',mdFile);
         
     case 'mdf'
          mdf=delft3d_io_mdf('read',mdFile);
          % .grd
+         if isfield(mdf.keywords,'filcco') && ~isempty(mdf.keywords.filcco)
          OPT.grdFile=[runDir filesep mdf.keywords.filcco];
          d3dfile=mdf.keywords.filcco;
          inputFile=[runDir filesep d3dfile];
-         outputFile=[outputDir strrep(d3dfile,'.grd','.kml')];
+         outputFile=[outputDir strrep(d3dfile,'.grd','_grd.kml')];
          inputFile=[runDir filesep d3dfile];
          [~,OPT]=EHY_convert(inputFile,'kml','outputFile',outputFile,'lineColor',[1 0 0],OPT);
-        
+         end
+         
          % .dry
+         if isfield(mdf.keywords,'fildry') && ~isempty(mdf.keywords.fildry)
          d3dfile=mdf.keywords.fildry;
-         outputFile=[outputDir strrep(d3dfile,'.dry','.kml')];
+         outputFile=[outputDir strrep(d3dfile,'.dry','_dry.kml')];
          inputFile=[runDir filesep d3dfile];
          [~,OPT]=EHY_convert(inputFile,'kml','outputFile',outputFile,'lineColor',[0 1 0],OPT);
+         end
          
          % .thd
+         if isfield(mdf.keywords,'filtd') && ~isempty(mdf.keywords.filthd)
          d3dfile=mdf.keywords.filtd;
-         outputFile=[outputDir strrep(d3dfile,'.thd','.kml')];
+         outputFile=[outputDir strrep(d3dfile,'.thd','_thd.kml')];
          inputFile=[runDir filesep d3dfile];
          [~,OPT]=EHY_convert(inputFile,'kml','outputFile',outputFile,'lineColor',[0 1 0],OPT);
+         end
          
          % .crs
+         if isfield(mdf.keywords,'filcrs') && ~isempty(mdf.keywords.filcrs)
          d3dfile=mdf.keywords.filcrs;
-         outputFile=[outputDir strrep(d3dfile,'.crs','.kml')];
+         outputFile=[outputDir strrep(d3dfile,'.crs','_crs.kml')];
          inputFile=[runDir filesep d3dfile];
          [~,OPT]=EHY_convert(inputFile,'kml','outputFile',outputFile,'lineColor',[0 0 1],OPT);
+         end
          
          % .obs
+         if isfield(mdf.keywords,'filsta') && ~isempty(mdf.keywords.filsta)
          d3dfile=mdf.keywords.filsta;
-         outputFile=[outputDir strrep(d3dfile,'.obs','.kml')];
+         outputFile=[outputDir strrep(d3dfile,'.obs','_obs.kml')];
          inputFile=[runDir filesep d3dfile];
          [~,OPT]=EHY_convert(inputFile,'kml','outputFile',outputFile,OPT);
+         end
          
          % .src
+         if isfield(mdf.keywords,'filsrc') && ~isempty(mdf.keywords.filsrc)
          d3dfile=mdf.keywords.filsrc;
-         outputFile=[outputDir strrep(d3dfile,'.src','.kml')];
+         outputFile=[outputDir strrep(d3dfile,'.src','_src.kml')];
          inputFile=[runDir filesep d3dfile];
          [~,OPT]=EHY_convert(inputFile,'kml','outputFile',outputFile,'lineColor',[0 1 0],OPT);
-         
+         end
 end
