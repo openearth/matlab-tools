158dce7a25d2a6b593322abb111a15a21d92afdf
groenenb <groenenb@localhost>
2017-11-07 08:59:15 +0000

    svn path=/trunk/matlab/; revision=13892
    Former-commit-id: 3c5dbaad086da3ec319ffb01d6fc83c9406a4631

--- applications/EHY_tools/support_functions/EHYs.m
+++ applications/EHY_tools/support_functions/EHYs.m
@@ -1,6 +1,6 @@
 function EHY_userStats(mfilename)
 try
-    filename=['n:\Deltabox\Bulletin\groenenb\OET_EHY\stats\' getenv('username') '_' mfilename '_' datestr(now,'yyyymmddHHMMSS')];
+    filename=['n:\Deltabox\Bulletin\groenenb\OET_EHY\stats\' getenv('username') '__' mfilename '__' datestr(now,'yyyymmddHHMMSS') '.ehy'];
     fid=fopen(filename,'w');
     fclose(fid);
 end
