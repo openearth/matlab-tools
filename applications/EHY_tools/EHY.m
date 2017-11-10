9094d46d338a98aea585783e5baf67a97f650a15
groenenb <groenenb@localhost>
2017-11-10 16:52:04 +0000

    new 'GUI' for EHY
    svn path=/trunk/matlab/; revision=13922
    
    
    Former-commit-id: a0156cad827d9b4a4230f0d284c3cad8f6ea9d01

--- applications/EHY_tools/EHY.m
+++ applications/EHY_tools/EHY.m
@@ -1,30 +1,33 @@
 function EHY
-EHYs(mfilename);
-[pathstr ~] = fileparts(which('EHY'));
-D=dir([pathstr filesep '*.m']);
 
-NoInputNeeded={};
-for iD=1:length(D)
-    fid=fopen([pathstr filesep D(iD).name],'r');
-    line=fgetl(fid);
-    fclose(fid);
-    if ~isempty(strfind(line,'(varargin)'))
-        NoInputNeeded{end+1,1}=D(iD).name;
-    end
+EHYs(mfilename);
+
+functions{1,1}='EHY_convert';
+functions{1,2}='Conversion from and to model input files. Including coordinate conversion.';
+functions{2,1}='EHY_simulationStatus';
+functions{2,2}='Check the status of a simulation.';
+functions{3,1}='EHY_runTimeInfo';
+functions{3,2}='Check the simulation period, run time, number of partitions, etc.';
+functions{4,1}='EHY_opendap';
+functions{4,2}='Retrieve data from Rijkswaterstaats waterbase';
+functions{5,1}='EHY_simulationInputTimes';
+functions{5,2}='A tool to help with using the corrent model input times';
+functions{6,1}='EHY_model2GoogleEarth';
+functions{6,2}='Visualize your model in GoogleEarth';
+functions{7,1}='EHY_crop';
+functions{7,2}='This function crops the surrounding area of a figure based on the background color.';
+
+figure('units','normalized','position',[0.4922 0.0583 0.5000 0.3000],'name','EHY_TOOLS')
+for iF=1:length(functions)
+    button=uicontrol('Style', 'pushbutton', 'String',functions{iF,1},...
+        'Position', [20 240-iF*30 200 20],...
+        'Callback', @runEHYscript);
+      uicontrol('Style','text',...
+        'Position',[240 240-iF*30-3 1000 20],...
+        'String',functions{iF,2},'horizontalalignment','left');
+end
 end
 
-% delete certain scripts
-NoInputNeeded(strmatch('EHY_plot_google_map',NoInputNeeded))=[];
-
-% select which function to use
-selection=  listdlg('PromptString',['Which function would you like to use:'],...
-    'SelectionMode','single',...
-    'ListString',strrep(NoInputNeeded,'.m',''),...
-    'ListSize',[300 200]);
-
-% run selection
-if ~isempty(selection)
-    run(NoInputNeeded{selection}(1:end-2))
-else
-    disp('No function was selected.')
+function runEHYscript(hobject,event)
+run(event.Source.String);
 end
