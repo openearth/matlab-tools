00736b1ddcbf5133fc08dfeff0d0f50adcf66ff7
groenenb <groenenb@localhost>
2017-11-13 16:23:26 +0000

    close button
    svn path=/trunk/matlab/; revision=13925
    
    
    Former-commit-id: 7685e95745af93adb9c60a2ec3c68bb3c0b74836

--- applications/EHY_tools/EHY.m
+++ applications/EHY_tools/EHY.m
@@ -11,23 +11,31 @@ functions{3,2}='Check the simulation period, run time, number of partitions, etc
 functions{4,1}='EHY_opendap';
 functions{4,2}='Retrieve data from Rijkswaterstaats waterbase';
 functions{5,1}='EHY_simulationInputTimes';
-functions{5,2}='A tool to help with using the corrent model input times';
+functions{5,2}='A tool to help using the corrent model input times';
 functions{6,1}='EHY_model2GoogleEarth';
 functions{6,2}='Visualize your model in GoogleEarth';
 functions{7,1}='EHY_crop';
 functions{7,2}='This function crops the surrounding area of a figure based on the background color.';
 
-figure('units','normalized','position',[0.4922 0.0583 0.5000 0.3000],'name','EHY_TOOLS')
+EHYfig=figure('units','normalized','position',[0.4922 0.0583 0.5000 0.3000],'name','EHY_TOOLS');
 for iF=1:length(functions)
     button=uicontrol('Style', 'pushbutton', 'String',functions{iF,1},...
-        'Position', [20 240-iF*30 200 20],...
+        'Position', [20 255-iF*30 200 20],...
         'Callback', @runEHYscript);
       uicontrol('Style','text',...
-        'Position',[240 240-iF*30-3 1000 20],...
+        'Position',[240 255-iF*30-3 1000 20],...
         'String',functions{iF,2},'horizontalalignment','left');
 end
+%close button
+    button=uicontrol('Style', 'pushbutton', 'String','Close',...
+        'Position', [20 255-(iF+1)*30 200 20],...
+        'Callback', @closeFig);
 end
 
-function runEHYscript(hobject,event)
+function runEHYscript(hObject,event)
 run(event.Source.String);
+end
+
+function closeFig(hObject,event)
+close(get(hObject,'Parent'))
 end
