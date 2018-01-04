08da4e15f8e445360554161214b2a28cc177801d
groenenb <groenenb@localhost>
2018-01-04 11:01:45 +0000

    Can now also be used without input arguments
    svn path=/trunk/matlab/; revision=14064
    
    
    Former-commit-id: f22937d5f8cb4ce69291509785a579a80a354ec0

--- applications/EHY_tools/EHY_wait.m
+++ applications/EHY_tools/EHY_wait.m
@@ -1,12 +1,27 @@
-function EHY_wait(time,path)
+function EHY_wait(varargin)
+% EHY_wait(varargin)
 % EHY_wait(time,path)
 % Onces TIME is reached, this function runs the PATH
 % time can be either a datenum or datestr
-% 
-% Example: EHY_wait('23-Mar-2017 12:00','D:\script.m')
+%
+% Example1: EHY_wait
+% Example2: EHY_wait('23-Mar-2018 12:00','D:\script.m')
 %
 % created by Julien Groenenboom, March 2017
 EHYs(mfilename);
+
+if nargin==0
+    disp('Select MATLAB-script to run')
+    path=uigetfile('*.m','Select MATLAB-script to run');
+    if isnumeric(path); disp('EHY_wait was stopped by user'); return; end
+    disp('Select MATLAB-script to run')
+    time=inputdlg('After what time would you like to execute this script? [dd-mm-yyyy HH:MM]','EHY_wait',1,{datestr(now,'dd-mm-yyyy HH:MM')});
+    if isempty(time); disp('EHY_wait was stopped by user'); return; end
+    time=datenum(time,'dd-mm-yyyy HH:MM');
+elseif narargin==2
+    time=varargin{1};
+    path=varargin{1};
+end
 
 if ~isnumeric(time)
     time=datenum(time);
@@ -20,8 +35,8 @@ while keepLooping
         run(path)
         keepLooping=0;
     else
-        disp(['Waiting untill ' datestr(time) ' to run the script ''' name ext ''' - time now: ' datestr(now)]);
-         pause(5)
+        disp(['Waiting until ' datestr(time) ' to run the script ''' name ext ''' - time now: ' datestr(now)]);
+        pause(5)
     end
 end
 
