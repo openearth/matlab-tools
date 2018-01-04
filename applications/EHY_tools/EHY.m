360bc6d0ff8b1af7705a1c2e408753a11aee0575
groenenb <groenenb@localhost>
2018-01-04 11:02:14 +0000

    two new functions added
    svn path=/trunk/matlab/; revision=14065
    
    
    Former-commit-id: 425fd30f8e866a10573c63657f73cda4f9fcad1b

--- applications/EHY_tools/EHY.m
+++ applications/EHY_tools/EHY.m
@@ -2,20 +2,25 @@ function EHY
 
 EHYs(mfilename);
 
-functions{1,1}='EHY_convert';
-functions{1,2}='Conversion from and to model input files. Including coordinate conversion.';
-functions{2,1}='EHY_simulationStatus';
-functions{2,2}='Check the status of a simulation.';
-functions{3,1}='EHY_runTimeInfo';
-functions{3,2}='Check the simulation period, run time, number of partitions, etc.';
-functions{4,1}='EHY_opendap';
-functions{4,2}='Retrieve data from Rijkswaterstaats waterbase';
-functions{5,1}='EHY_simulationInputTimes';
-functions{5,2}='A tool to help using the corrent model input times';
-functions{6,1}='EHY_model2GoogleEarth';
-functions{6,2}='Visualize your model in GoogleEarth';
-functions{7,1}='EHY_crop';
-functions{7,2}='This function crops the surrounding area of a figure based on the background color.';
+functions={};
+functions{end+1,1}='EHY_convert';
+functions{end  ,2}='Conversion from and to model input files. Including coordinate conversion';
+functions{end+1,1}='EHY_getmodeldata';
+functions{end  ,2}='Interactive retrieval of model data using EHY_getmodeldata';
+functions{end+1,1}='EHY_simulationStatus';
+functions{end  ,2}='Check the status of a simulation';
+functions{end+1,1}='EHY_runTimeInfo';
+functions{end  ,2}='Check the simulation period, run time, number of partitions, etc.';
+functions{end+1,1}='EHY_opendap';
+functions{end  ,2}='Retrieve data from Rijkswaterstaats waterbase';
+functions{end+1,1}='EHY_simulationInputTimes';
+functions{end  ,2}='A tool to help using the correct model input times';
+functions{end+1,1}='EHY_model2GoogleEarth';
+functions{end  ,2}='Visualize your model in GoogleEarth';
+functions{end+1,1}='EHY_crop';
+functions{end  ,2}='This function crops the surrounding area of a figure based on the background color';
+functions{end+1,1}='EHY_wait';
+functions{end  ,2}='Run a selected MATLAB-script once a certain date and time is reached';
 
 h=findall(0,'type','figure','name','EHY_TOOLS');
 if ~isempty(h)
@@ -23,24 +28,27 @@ if ~isempty(h)
     figure(h);
     disp('The EHY_TOOLS GUI was already open')
 else
-    EHYfig=figure('units','normalized','position',[0.4922 0.0583 0.5000 0.3000],'name','EHY_TOOLS');
+    EHYfig=figure('units','normalized','position',[0.2698 0.3 0.4089 0.3463],'name','EHY_TOOLS');
 end
+height=305;
 for iF=1:length(functions)
     button=uicontrol('Style', 'pushbutton', 'String',functions{iF,1},...
-        'Position', [20 255-iF*30 200 20],...
+        'Position', [20 height-iF*30 200 20],...
         'Callback', @runEHYscript);
       uicontrol('Style','text',...
-        'Position',[240 255-iF*30-3 1000 20],...
+        'Position',[240 height-iF*30-3 1000 20],...
         'String',functions{iF,2},'horizontalalignment','left');
 end
 %close button
     button=uicontrol('Style', 'pushbutton', 'String','Close',...
-        'Position', [20 255-(iF+1)*30 200 20],...
+        'Position', [20 height-(iF+1)*30 200 20],...
         'Callback', @closeFig);
 end
 
 function runEHYscript(hObject,event)
+disp([char(10) 'EHY_tools: Start of running the function ''' get(hObject,'String') ''' - BUSY (even though MATLAB says it''s not)' char(10)])
 run(get(hObject,'String'))
+disp([char(10) 'EHY_tools: Finished running the ' get(hObject,'String') '-function  ' char(10)])
 end
 
 function closeFig(hObject,event)
