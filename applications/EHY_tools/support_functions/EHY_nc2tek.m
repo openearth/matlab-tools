d421ddd27d604f2b9a0a1ed8ceadb7b33d3c3497
kaaij <kaaij@localhost>
2018-03-01 15:38:23 +0000

    svn path=/trunk/matlab/; revision=14200
    Former-commit-id: 0e4780d769f71231e843c35392848ae4cc95588e

--- applications/EHY_tools/support_functions/EHY_nc2tek.m
+++ applications/EHY_tools/support_functions/EHY_nc2tek.m
@@ -8,13 +8,29 @@ function EHY_nc2tek(filename,varargin)
 % 
 % EHY_nc2tek('c05r_his.nc','stationlist',stationlist, ...
 %                          'crslist'    ,crslist    , ...
-%                          'layers'     ,[1 10]     , ... 
-%                          'waterlevel' ,true       , ...
+%                          'layers'     ,[1 10]     , ...  
+%                          'waterlevel' ,true       , ... % Hydrodynamics, station information
 %                          'salinity'   ,true       , ...
 %                          'temperature',true       , ...
 %                          'density'    ,true       , ...
-%                          'discharge'  ,true       , ...
-%                          'cummdis'    ,true       );
+%                          'discharge'  ,true       , ... % Hydrodynamics, cross section information
+%                          'cummdis'    ,true       , ...
+%                          'crs_area'   ,true       , ...
+%                          'crs_vel'    ,true       , ...
+%                          'crs_salt'   ,true       , ...
+%                          'crs_temp'   ,true       , ...
+%                          'wind'       ,true       , ... % Meteorology/temperature, station information
+%                          'patm'       ,true       , ... 
+%                          'Tair'       ,true       , ...
+%                          'rhum'       ,true       , ...
+%                          'clou'       ,true       , ...
+%                          'Qsun'       ,true       , ...
+%                          'Qeva'       ,true       , ...
+%                          'Qcon'       ,true       , ...
+%                          'Qlong'      ,true       , ...
+%                          'Qfreva'     ,true       , ...
+%                          'Qfrcon'     ,true       , ...
+%                          'Qtot'       ,true       ) ;
 %
 %  Initialise
 OPT.stationlist = {};
@@ -26,9 +42,34 @@ OPT.temperature = false;
 OPT.density     = false;
 OPT.discharge   = false;
 OPT.cummdis     = false;
+OPT.crs_area    = false;
+OPT.crs_vel     = false;
+OPT.crs_salt    = false;
+OPT.crs_temp    = false;
+OPT.wind        = false;
+OPT.patm        = false;
+OPT.Tair        = false;
+OPT.rhum        = false;
+OPT.clou        = false;
+OPT.Qsun        = false;
+OPT.Qeva        = false;
+OPT.Qcon        = false;
+OPT.Qlong       = false;
+OPT.Qfreva      = false;
+OPT.Qfrcon      = false;
+OPT.Qtot        = false;
 OPT             = setproperty(OPT,varargin);
 tmp             = strfind(filename,'_');
 sim             = filename(1:tmp(1) - 1);
+
+%  General: Dimensions
+Info        = ncinfo(filename);
+nr_field    = find(~cellfun(@isempty,strfind({Info.Dimensions.Name},'laydim'))==1,1);
+laydim      = Info.Dimensions(nr_field).Length;
+nr_field    = find(~cellfun(@isempty,strfind({Info.Dimensions.Name},'stations'))==1,1);
+no_stations = Info.Dimensions(nr_field).Length;
+nr_field    = find(~cellfun(@isempty,strfind({Info.Dimensions.Name},'cross_section'))==1,1);
+no_crs      = Info.Dimensions(nr_field).Length;
 
 %  General: times
 times     = ncread    (filename,'time');
@@ -44,16 +85,9 @@ station_names = deblank(char2cell(tmp'));
 tmp                 = ncread    (filename,'cross_section_name');
 cross_section_names = deblank(char2cell(tmp'));
 
-%  General: number of layers (not sure if this parameter exist in 2D simulation
-try
-    laydim = length(ncread    (filename,'zcoordinate_c',[1 1 1],[inf 1 1],[1 1 1]));
-catch
-    laydim = 1;
-end
-
 %% Get nr of the stations and/or cross sections to extract
 if isempty(OPT.stationlist)
-    station_nr = 1:1:length(station_names);
+    station_nr = 1:1:no_stations;
 else
     station_nr = [];
     for i_stat = 1: length(OPT.stationlist)
@@ -63,7 +97,7 @@ else
 end
 
 if isempty(OPT.crslist)
-    crs_nr = 1:1:length(cross_section_names);
+    crs_nr = 1:1:no_crs;
 else
     crs_nr = [];
     for i_stat = 1: length(OPT.crslist)
@@ -82,12 +116,32 @@ end
 %% Create list of parameters to extract
 param_list    = {};
 threeD        = [];
+% Hydrodynamics station information
 if OPT.waterlevel  param_list{end + 1} = 'waterlevel'                        ; threeD(end + 1) = false; end
 if OPT.salinity    param_list{end + 1} = 'salinity'                          ; threeD(end + 1) = true ; end
 if OPT.temperature param_list{end + 1} = 'temperature'                       ; threeD(end + 1) = true ; end
 if OPT.density     param_list{end + 1} = 'density'                           ; threeD(end + 1) = true ; end
+% Hydrodynamics cross section information
 if OPT.discharge   param_list{end + 1} = 'cross_section_discharge'           ; threeD(end + 1) = false; end
 if OPT.cummdis     param_list{end + 1} = 'cross_section_cumulative_discharge'; threeD(end + 1) = false; end
+if OPT.crs_area    param_list{end + 1} = 'cross_section_area'                ; threeD(end + 1) = false; end
+if OPT.crs_vel     param_list{end + 1} = 'cross_section_velocity'            ; threeD(end + 1) = false; end
+if OPT.crs_salt    param_list{end + 1} = 'cross_section_salt'                ; threeD(end + 1) = false; end
+if OPT.crs_temp    param_list{end + 1} = 'cross_section_temperature'         ; threeD(end + 1) = false; end
+% Meteorology/temperature at stations
+if OPT.wind        param_list{end + 1} = 'wind'                              ; threeD(end + 1) = false; end
+if OPT.patm        param_list{end + 1} = 'patm'                              ; threeD(end + 1) = false; end
+if OPT.Tair        param_list{end + 1} = 'Tair'                              ; threeD(end + 1) = false; end
+if OPT.rhum        param_list{end + 1} = 'rhum'                              ; threeD(end + 1) = false; end
+if OPT.clou        param_list{end + 1} = 'clou'                              ; threeD(end + 1) = false; end
+if OPT.Qsun        param_list{end + 1} = 'Qsun'                              ; threeD(end + 1) = false; end
+if OPT.Qeva        param_list{end + 1} = 'Qeva'                              ; threeD(end + 1) = false; end
+if OPT.Qcon        param_list{end + 1} = 'Qcon'                              ; threeD(end + 1) = false; end
+if OPT.Qlong       param_list{end + 1} = 'Qlong'                             ; threeD(end + 1) = false; end
+if OPT.Qfreva      param_list{end + 1} = 'Qfreva'                            ; threeD(end + 1) = false; end
+if OPT.Qfrcon      param_list{end + 1} = 'Qfrcon'                            ; threeD(end + 1) = false; end
+if OPT.Qtot        param_list{end + 1} = 'Qtot'                              ; threeD(end + 1) = false; end
+
 
 %% Get data and write
 for i_param = 1: length(param_list)
