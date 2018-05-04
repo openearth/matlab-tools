feba82761b0f2c701b4d0ccb27e24180bdcd5f41
torresdu <torresdu@localhost>
2018-05-04 15:52:31 +0000

    Added option to exclude polygon
    svn path=/trunk/matlab/; revision=14323
    
    
    Former-commit-id: 290c3831ca51051f80cba3f7638bf525ddafdd8a

--- applications/DelftDashBoard/general/sfincs/fileio/sfincs_make_mask.m
+++ applications/DelftDashBoard/general/sfincs/fileio/sfincs_make_mask.m
@@ -1,5 +1,5 @@
 function msk=sfincs_make_mask(x,y,z,zlev,varargin)
-
+% Leijnse april 18: included option to exclude points via polygon input
 xy=[];
 
 for ii=1:length(varargin)
@@ -7,6 +7,9 @@ for ii=1:length(varargin)
         switch lower(varargin{ii})
             case{'includepolygon'}
                 xy=varargin{ii+1};
+            case{'excludepolygon'}
+                xy_ex=varargin{ii+1};
+                
         end
     end
 end
@@ -19,6 +22,14 @@ if ~isempty(xy)
     yp=xy(:,2);
     inp=inpolygon(x,y,xp,yp);
     z(inp)=max(z(inp),zlev(1)+0.01);
+end
+
+if ~isempty(xy_ex)
+    % Throw away points within polygon
+    xp_ex=xy_ex(:,1);
+    yp_ex=xy_ex(:,2);
+    inp_ex=inpolygon(x,y,xp_ex,yp_ex);
+    z(inp_ex)=NaN; %max(z(inp),zlev(1)+0.01);
 end
 
 z(z<zlev(1))=NaN;
