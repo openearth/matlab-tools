308a80936f78b2746a92f690a26455b426fa6cc4
groenenb <groenenb@localhost>
2018-05-14 09:05:36 +0000

    svn path=/trunk/matlab/; revision=14330
    Former-commit-id: e17784ed238c3172c8c6dc93000e5bd6e244043c

--- applications/EHY_tools/support_functions/getTimeInfoFromMdFile.m
+++ applications/EHY_tools/support_functions/getTimeInfoFromMdFile.m
@@ -1,4 +1,15 @@
-function [refdate,tunit,tstart,tstop]=getTimeInfoFromMdFile(mdFile)
+function [refdate,tunit,tstart,tstop,hisstart,hisstop,mapstart,mapstop]=getTimeInfoFromMdFile(mdFile)
+% refdate       : Reference date in MATLAB's datenum
+% tunit         : Time unit of tstart and tstop (e.g. 'S' , 'M')
+% tstart        : Start time of simulation w.r.t. refdate (in tunit)
+% tstop         : Stop time of simulation w.r.t. refdate (in tunit)
+% hisstart      : Start time of writing history output (in minutes)
+% hisstop       : Stop time of writing history output (in minutes)
+% mapstart      : Start time of writing map output (in minutes)
+% mapstop       : Stop time of writing map output (in minutes)
+% mdFile        : Master definition file (*.mdf, *.mdu, *siminp*)
+%
+% support function of the EHY_tools, Julien.Groenenboom@deltares.nl
 
 [modelType,mdFile]=EHY_getModelType(mdFile);
 
@@ -9,12 +20,30 @@ switch modelType
         tunit=mdu.time.Tunit;
         tstart=mdu.time.TStart;
         tstop=mdu.time.TStop;
+        if length(mdu.output.HisInterval)==1 % only interval of his file
+            hisstart=tstart;
+            hisstop=tstop;
+        else % his start stop, all in seconds
+            hisstart=mdu.output.HisInterval(2)*timeFactor('S','M');
+            hisstop=mdu.output.HisInterval(3)*timeFactor('S','M');
+        end
+        if length(mdu.output.MapInterval)==1 % only interval of his file
+            mapstart=tstart;
+            mapstop=tstop;
+        else % his start stop, all in seconds
+            mapstart=mdu.output.MapInterval(2)*timeFactor('S','M');
+            mapstop=mdu.output.MapInterval(3)*timeFactor('S','M');
+        end
     case 'mdf'
         mdf=delft3d_io_mdf('read',mdFile);
         refdate=datenum(mdf.keywords.itdate,'yyyy-mm-dd');
         tunit=mdf.keywords.tunit;
         tstart=mdf.keywords.tstart;
         tstop=mdf.keywords.tstop;
+        hisstart=mdf.keywords.flhis(1);
+        hisstop=mdf.keywords.flhis(3);
+        mapstart=mdf.keywords.flmap(1);
+        mapstop=mdf.keywords.flmap(3);
     case 'siminp'
         [pathstr,name,ext]=fileparts(mdFile);
         siminp=readsiminp(pathstr,[name ext]);
@@ -29,6 +58,19 @@ switch modelType
                 refdate=datenum(strtrim(sprintf('%s ',dmy{2:4})));
             else
                 eval([var{1} '=str2double(dmy(2));'])
+            end
+        end
+        
+        vars={'tfhis'   ,'tlhis'  ,'tfmap'   ,'tlmap';,...
+            'hisstart','hisstop','mapstart','mapstop'};
+        for iVar=1:length(vars)
+            ind1=find(~cellfun(@isempty,strfind(lower(siminp.File),vars{1,iVar})));
+            if ~isempty(ind1)
+                ind2=regexp(lower(siminp.File{ind1}),vars{1,iVar})+length(vars{1,iVar});
+                dmy=regexp(siminp.File{ind1}(ind2:end),'\s+','split');
+                eval([vars{2,iVar} '=str2double(dmy(2));'])
+            else
+                eval([vars{2,iVar} '=t' vars{2,iVar}(4:end) ';'])
             end
         end
 end
--- applications/EHY_tools/support_functions/timeFactor.m
+++ applications/EHY_tools/support_functions/timeFactor.m
@@ -1,4 +1,14 @@
 function factor=timeFactor(fromTunit,toTunit)
+%% factor=timeFactor(fromTunit,toTunit)
+%
+% This function calculates the factor that you can apply in a multiplication
+% to get from 'fromTunit' to 'toTunit'
+%
+% Example1: factor = timeFactor('M','S') [=60]
+% Example2: factor = timeFactor('D','M') [=1440]
+% Example3: factor = timeFactor('S','M') [=1/60=0.0166..]
+%
+% support function of the EHY_tools, Julien.Groenenboom@deltares.nl
 
 tunits={'S','M','H','D','Y'};
 factors=[60 60 24 365.25];
