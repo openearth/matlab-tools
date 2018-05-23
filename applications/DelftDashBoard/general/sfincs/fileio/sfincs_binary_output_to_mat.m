2f0f7f38edff7ddf84999d3caea07cd7d14bdd9a
leijnse <leijnse@localhost>
2018-05-23 09:45:57 +0000

    Some alterations grid output + vmax output + added option to use MAT-file 7.3 (keyword version=7.3) for variables>2GB
    svn path=/trunk/matlab/; revision=14363
    
    
    Former-commit-id: d8235744c0599ca76277014fc480675e19623ece

--- applications/DelftDashBoard/general/sfincs/fileio/sfincs_binary_output_to_mat.m
+++ applications/DelftDashBoard/general/sfincs/fileio/sfincs_binary_output_to_mat.m
@@ -1,4 +1,5 @@
-function sfincs_binary_output_to_mat(matfile,inpfile,tref)
+function sfincs_binary_output_to_mat(matfile,infile,tref)
+function sfincs_binary_output_to_mat(matfile,inpfile,tref,version)
 
 [folder,name,ext] = fileparts(inpfile);
 if ~isempty(folder)
@@ -8,9 +9,8 @@ end
 inp=sfincs_initialize_input;
 inp=sfincs_read_input(inpfile,inp);
 
+[xg,yg]=meshgrid(0:inp.dx:(inp.mmax)*inp.dx,0:inp.dy:(inp.nmax)*inp.dy);
 tstart=datenum(inp.tstart,'yyyymmdd HHMMSS');
-
-[xg,yg]=meshgrid(0:inp.dx:(inp.mmax-1)*inp.dx,0:inp.dy:(inp.nmax-1)*inp.dy);
 rot=inp.rotation*pi/180;
 x=inp.x0+cos(rot)*xg-sin(rot)*yg;
 y=inp.y0+sin(rot)*xg+cos(rot)*yg;
@@ -25,7 +25,8 @@ fclose(fid);
 fid=fopen([folder inp.depfile],'r');
 zbv=fread(fid,np,'real*4');
 fclose(fid);
-zb=zeros(size(x));
+
+zb=zeros(inp.nmax,inp.mmax);
 zb(zb==0)=NaN;
 zb(indices)=zbv;
 
@@ -40,7 +41,7 @@ while 1
     it=it+1;
     zsv=fread(fid,np,'real*4');
     idummy=fread(fid,1,'integer*4');
-    zs0=zeros(size(x));
+    zs0=zeros(inp.nmax,inp.mmax);
     zs0(zs0==0)=NaN;
     zs0(indices)=zsv;
 %    zs0(zs0-zb<0.05)=NaN;
@@ -61,7 +62,7 @@ if exist([folder 'cumprcp.dat'],'file')
         it=it+1;
         zsv=fread(fid,np,'real*4');
         idummy=fread(fid,1,'integer*4');
-        zs0=zeros(size(x));
+        zs0=zeros(inp.nmax,inp.mmax);
         zs0(zs0==0)=NaN;
         zs0(indices)=zsv;
         %    zs0(zs0-zb<0.05)=NaN;
@@ -70,78 +71,124 @@ if exist([folder 'cumprcp.dat'],'file')
     fclose(fid);
 end
 
-if ~isempty(inp.hmaxfile)
-    % Read Hmax file
-    fid=fopen([folder inp.hmaxfile],'r');
-    idummy=fread(fid,1,'integer*4');
-    hmaxv=fread(fid,np,'real*4');
-    idummy=fread(fid,1,'integer*4');
-    fclose(fid);
-    hmax=zeros(size(x));
-    hmax(hmax==0)=NaN;
-    hmax(indices)=hmaxv;
-end
 
-if ~isempty(inp.vmaxfile)
-    % Read vmax file
-    fid=fopen([folder inp.vmaxfile],'r');
-    idummy=fread(fid,1,'integer*4');
-    vmaxv=fread(fid,np,'real*4');
-    idummy=fread(fid,1,'integer*4');
-    fclose(fid);
-    vmax=zeros(size(x));
-    vmax(vmax==0)=NaN;
-    vmax(indices)=vmaxv;
-end
+% Read Hmax file
+fid=fopen([folder inp.hmaxfile],'r');
+idummy=fread(fid,1,'integer*4');
+hmaxv=fread(fid,np,'real*4');
+idummy=fread(fid,1,'integer*4');
+fclose(fid);
+hmax=zeros(inp.nmax,inp.mmax);
+hmax(hmax==0)=NaN;
+hmax(indices)=hmaxv;
+
+
+% Read vmaxfile
+inp.vmaxfile = 'vmax.dat';
+inp.vmaxgeofile = 'vmaxgeo.dat';
+
+fid=fopen([folder inp.vmaxfile],'r');
+idummy=fread(fid,1,'integer*4');
+vmaxv=fread(fid,np,'real*4');
+idummy=fread(fid,1,'integer*4');
+fclose(fid);
+vmax=zeros(inp.nmax,inp.mmax);
+vmax(vmax==0)=NaN;
+vmax(indices)=vmaxv;
+
+% % Read vmaxgeo file
+% fid=fopen([folder vmaxgeo.dat],'r');
+% idummy=fread(fid,1,'integer*4');
+% vmaxv=fread(fid,np,'real*4');
+% idummy=fread(fid,1,'integer*4');
+% fclose(fid);
+% vmax=zeros(size(x));
+% vmax(vmax==0)=NaN;
+% vmax(indices)=vmaxv;
+
+% % Data file
+% fid=fopen(datafile,'r');
+% fread(fid,1,'int64'); % dummy line
+% zv=fread(fid,np,'real*4');
+% fclose(fid);
+
+
+valout=zeros(size(val,1),inp.nmax+1,inp.mmax+1);
+zbout=zeros(inp.nmax+1,inp.mmax+1);
+valout(valout==0)=NaN;
+zbout(zbout==0)=NaN;
+hmaxout=zbout;
+vmaxout=zbout;
+cumprcpout=valout;
+
 
 n=0;
 
+valout(:,1:end-1,1:end-1)=val;
 n=n+1;
 s.parameters(n).parameter.name='water level';
+
 s.parameters(n).parameter.time=tstart+t/86400;
 s.parameters(n).parameter.x=x;
 s.parameters(n).parameter.y=y;
-s.parameters(n).parameter.val=val;
+s.parameters(n).parameter.val=valout;
 s.parameters(n).parameter.size=[it 0 size(x,1) size(x,2) 0];
 s.parameters(n).parameter.quantity='scalar';
 
-if ~isempty(inp.hmaxfile)
-    n=n+1;
-    s.parameters(n).parameter.name='maximum water depth';
-    s.parameters(n).parameter.x=x;
-    s.parameters(n).parameter.y=y;
-    s.parameters(n).parameter.val=hmax;
-    s.parameters(n).parameter.size=[0 0 size(x,1) size(x,2) 0];
-    s.parameters(n).parameter.quantity='scalar';
-end
 
-if ~isempty(inp.vmaxfile)
-    n=n+1;
-    s.parameters(n).parameter.name='maximum current velocity';
-    s.parameters(n).parameter.x=x;
-    s.parameters(n).parameter.y=y;
-    s.parameters(n).parameter.val=vmax;
-    s.parameters(n).parameter.size=[0 0 size(x,1) size(x,2) 0];
-    s.parameters(n).parameter.quantity='scalar';
-end
+hmaxout(1:end-1,1:end-1)=hmax;
+n=n+1;
+s.parameters(n).parameter.name='maximum water depth';
+s.parameters(n).parameter.x=x;
+s.parameters(n).parameter.y=y;
+s.parameters(n).parameter.val=hmaxout;
+s.parameters(n).parameter.size=[0 0 size(x,1) size(x,2) 0];
+s.parameters(n).parameter.quantity='scalar';
+
+zbout(1:end-1,1:end-1)=zb;
+n=n+1;
+s.parameters(n).parameter.name='bed level';
+s.parameters(n).parameter.x=x;
+s.parameters(n).parameter.y=y;
+s.parameters(n).parameter.val=zbout;
+s.parameters(n).parameter.size=[0 0 size(x,1) size(x,2) 0];
+s.parameters(n).parameter.quantity='scalar';
+
+% if exist([folder 'vmaxgeo.dat'],'file')
+vmaxout(1:end-1,1:end-1)=vmax;
+n=n+1;
+s.parameters(n).parameter.name='maximum current velocity';
+s.parameters(n).parameter.x=x;
+s.parameters(n).parameter.y=y;
+s.parameters(n).parameter.val=vmaxout;
+s.parameters(n).parameter.size=[0 0 size(x,1) size(x,2) 0];
+s.parameters(n).parameter.quantity='scalar';
+% end
+
+% if ~isempty(inp.vmaxgeofile)
+%     n=n+1;
+%     s.parameters(n).parameter.name='maximum current velocity';
+%     s.parameters(n).parameter.x=x;
+%     s.parameters(n).parameter.y=y;
+%     s.parameters(n).parameter.val=vmax;
+%     s.parameters(n).parameter.size=[0 0 size(x,1) size(x,2) 0];
+%     s.parameters(n).parameter.quantity='scalar';
+% end
 
 if exist([folder 'cumprcp.dat'],'file')
+    cumprcpout(:,1:end-1,1:end-1)=cumprcp;
     n=n+1;
     s.parameters(n).parameter.name='cumulative precipitation';
     s.parameters(n).parameter.time=tstart+t/86400;
     s.parameters(n).parameter.x=x;
     s.parameters(n).parameter.y=y;
-    s.parameters(n).parameter.val=cumprcp;
+    s.parameters(n).parameter.val=cumprcpout;
     s.parameters(n).parameter.size=[it 0 size(x,1) size(x,2) 0];
     s.parameters(n).parameter.quantity='scalar';
 end
 
-n=n+1;
-s.parameters(n).parameter.name='bed level';
-s.parameters(n).parameter.x=x;
-s.parameters(n).parameter.y=y;
-s.parameters(n).parameter.val=zb;
-s.parameters(n).parameter.size=[0 0 size(x,1) size(x,2) 0];
-s.parameters(n).parameter.quantity='scalar';
-
-save(matfile,'-struct','s');
+if version == 7.3
+	save(matfile,'-struct','s','-v7.3'); %For variables larger than 2GB use MAT-file version 7.3 or later. 
+else
+	save(matfile,'-struct','s');
+end
