9a1d7c1ae2357de1bfda68b25aa94daaaad8946f
groenenb <groenenb@localhost>
2018-08-20 13:35:01 +0000

    replaces getTimeInfoFromMdFile
    svn path=/trunk/matlab/; revision=14520
    
    
    Former-commit-id: 197ef6e86afbd188f0c68e6ff7e8fee81e7b58ea

---
+++ applications/EHY_tools/support_functions/EHY_getTimeInfoFromMdFile.m
@@ -0,0 +1,91 @@
+function [refdate,tunit,tstart,tstop,hisstart,hisstop,mapstart,mapstop]=EHY_getTimeInfoFromMdFile(mdFile,modelType)
+%% EHY_getTimeInfoFromMdFile(mdFile,modelType)
+% refdate       : Reference date in MATLAB's datenum
+% tunit         : Time unit of tstart and tstop (e.g. 'S' , 'M')
+% tstart        : Start time of simulation w.r.t. refdate (in tunit)
+% tstop         : Stop time of simulation w.r.t. refdate (in tunit)
+% hisstart      : Start time of writing history output (in minutes)
+% hisstop       : Stop time of writing history output (in minutes)
+% mapstart      : Start time of writing map output (in minutes)
+% mapstop       : Stop time of writing map output (in minutes)
+% mdFile        : Master definition file (*.mdf, *.mdu, *siminp*)
+% modelType	: Model type ('dfm','d3d','simona')
+%
+% support function of the EHY_tools
+% Julien Groenenboom - E: Julien.Groenenboom@deltares.nl
+
+mdFile=EHY_getMdFile(mdFile);
+if isempty(mdFile)
+    error('No .mdu, .mdf or siminp found in this folder')
+end
+if ~exist('modelType','var')
+    modelType=EHY_getModelType(mdFile);
+    if isempty(modelType)
+        error('Could not determine model type')
+    end
+end
+
+switch modelType
+    case 'dfm'
+        mdu=dflowfm_io_mdu('read',mdFile);
+        refdate=datenum(num2str(mdu.time.RefDate),'yyyymmdd');
+        tunit=mdu.time.Tunit;
+        tstart=mdu.time.TStart;
+        tstop=mdu.time.TStop;
+        if length(mdu.output.HisInterval)==1 % only interval of his file
+            hisstart=tstart;
+            hisstop=tstop;
+        else % his start stop, all in seconds
+            hisstart=mdu.output.HisInterval(2)*timeFactor('S','M');
+            hisstop=mdu.output.HisInterval(3)*timeFactor('S','M');
+        end
+        if length(mdu.output.MapInterval)==1 % only interval of map file
+            mapstart=tstart;
+            mapstop=tstop;
+        else % his start stop, all in seconds
+            mapstart=mdu.output.MapInterval(2)*timeFactor('S','M');
+            mapstop=mdu.output.MapInterval(3)*timeFactor('S','M');
+        end
+    case 'd3d'
+        mdf=delft3d_io_mdf('read',mdFile);
+        refdate=datenum(mdf.keywords.itdate,'yyyy-mm-dd');
+        tunit=mdf.keywords.tunit;
+        tstart=mdf.keywords.tstart;
+        tstop=mdf.keywords.tstop;
+        hisstart=mdf.keywords.flhis(1);
+        hisstop=mdf.keywords.flhis(3);
+        mapstart=mdf.keywords.flmap(1);
+        mapstop=mdf.keywords.flmap(3);
+    case 'simona'
+        [pathstr,name,ext]=fileparts(mdFile);
+        siminp=readsiminp(pathstr,[name ext]);
+        
+        tunit='M';
+        
+        for var={'date','tstart','tstop'}
+            ind1=find(~cellfun(@isempty,strfind(lower(siminp.File),var{1})));
+            ind2=regexp(lower(siminp.File{ind1}),var{1})+length(var{1});
+            dmy=regexp(siminp.File{ind1}(ind2:end),'\s+','split');
+            if strcmp(var{1},'date')
+                refdate=datenum(strtrim(sprintf('%s ',dmy{2:4})));
+            else
+                eval([var{1} '=str2double(dmy(2));'])
+            end
+        end
+        
+        vars={'tfhis'   ,'tlhis'  ,'tfmap'   ,'tlmap';,...
+            'hisstart','hisstop','mapstart','mapstop'};
+        for iVar=1:length(vars)
+            ind1=find(~cellfun(@isempty,strfind(lower(siminp.File),vars{1,iVar})));
+            if ~isempty(ind1)
+                ind2=regexp(lower(siminp.File{ind1}),vars{1,iVar})+length(vars{1,iVar});
+                dmy=regexp(siminp.File{ind1}(ind2:end),'\s+','split');
+                eval([vars{2,iVar} '=str2double(dmy(2));'])
+            else
+                eval([vars{2,iVar} '=t' vars{2,iVar}(4:end) ';'])
+            end
+        end
+end
+
+
+
