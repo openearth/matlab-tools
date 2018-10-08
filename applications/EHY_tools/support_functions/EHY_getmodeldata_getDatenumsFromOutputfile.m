66f380429e3f7527b5d7657002e78ec98ce2faed
groenenb <groenenb@localhost>
2018-10-08 07:13:29 +0000

    svn path=/trunk/matlab/; revision=14667
    Former-commit-id: fcf634b30ebdcaac166f2ebe7b4bd6d92c0b9902

--- applications/EHY_tools/support_functions/EHY_getmodeldata_getDatenumsFromOutputfile.m
+++ applications/EHY_tools/support_functions/EHY_getmodeldata_getDatenumsFromOutputfile.m
@@ -3,12 +3,13 @@ infonc      = ncinfo(outputfile);
 
 % - to enhance speed, reconstruct time array from start time, numel and interval
 ncVarInd    = strmatch('time',{infonc.Variables.Name},'exact');
+ncAttrInd    = strmatch('units',{infonc.Variables(ncVarInd).Attributes.Name},'exact');
 nr_times    = infonc.Variables(ncVarInd).Size;
 seconds_int = ncread(outputfile, 'time', 1, 3);
 interval    = seconds_int(3)-seconds_int(2);
 seconds     = [seconds_int(1) seconds_int(2) + interval*[0:nr_times-2] ]';
 days        = seconds / (24*60*60);
-attri       = infonc.Variables(ncVarInd).Attributes(1).Value;
+attri       = infonc.Variables(ncVarInd).Attributes(ncAttrInd).Value;
 itdate      = attri(15:end);
 datenums    = datenum(itdate, 'yyyy-mm-dd HH:MM:SS')+days;
 
