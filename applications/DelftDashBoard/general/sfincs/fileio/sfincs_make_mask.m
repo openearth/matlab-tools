ee4fc838be433aead8c67c65a748da8c18718713
leijnse <leijnse@localhost>
2018-10-09 15:14:27 +0000

    svn path=/trunk/matlab/; revision=14681
    Former-commit-id: 47d89dea0f4e92d370cf9e487b116a106465745f

--- applications/DelftDashBoard/general/sfincs/fileio/sfincs_make_mask.m
+++ applications/DelftDashBoard/general/sfincs/fileio/sfincs_make_mask.m
@@ -1,6 +1,8 @@
 function msk=sfincs_make_mask(x,y,z,zlev,varargin)
 % Leijnse april 18: included option to exclude points via polygon input
+
 xy=[];
+xy_ex=[];
 
 for ii=1:length(varargin)
     if ischar(varargin{ii})
