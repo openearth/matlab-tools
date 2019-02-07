1f72b8615317dcef1385e2a2151da6aa2a3843be
kaaij <kaaij@localhost>
2019-02-07 13:36:31 +0000

    svn path=/trunk/matlab/; revision=15136
    Former-commit-id: 3c62465eb9ea2b57aaed4784849e3666cec796cb

--- applications/delft3d/nest_matlab/reawri/nesthd_wrihyd.m
+++ applications/delft3d/nest_matlab/reawri/nesthd_wrihyd.m
@@ -1,15 +1,21 @@
-function wrihyd(filename,bnd,nfs_inf,bndval,add_inf)
+function wrihyd(fileOut,bnd,nfs_inf,bndval,add_inf)
 
 % wrihyd : writes hydrodynamic bc to either Delft3D-Flow format or SIMONA format
+[modelType,fileType] = EHY_getModelType(fileOut);
 
-filetype = nesthd_det_filetype(filename);
-
-switch filetype
-   case 'Delft3D'
-      nesthd_wrihyd_bct(filename,bnd,nfs_inf,bndval,add_inf)
-   case 'SIMONA'
-      nesthd_wrihyd_timeser(filename,bnd,nfs_inf,bndval,add_inf)
-   case 'DFLOWFM'
-       % ToDo: write series file
-       nesthd_wrihyd_dflowfmtim (filename,bnd,nfs_inf,bndval,add_inf)
+switch modelType
+   case 'd3d'
+      nesthd_wrihyd_bct(fileOut,bnd,nfs_inf,bndval,add_inf)
+   case 'simona'
+      nesthd_wrihyd_timeser(fileOut,bnd,nfs_inf,bndval,add_inf)
+   case 'dfm'
+       switch fileType
+           
+           % Old (HK) format 
+           case 'tim'      
+               nesthd_wrihyd_dflowfmtim (fileOut,bnd,nfs_inf,bndval,add_inf)
+           % New inifile format
+           case 'bc'
+               nesthd_wrihyd_dflowfmbc  (fileOut,bnd,nfs_inf,bndval,add_inf)
+       end
 end
