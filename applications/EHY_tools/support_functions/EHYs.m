78829f35ca832b04e63b05d62fcd66b977f21b1a
groenenb <groenenb@localhost>
2019-03-25 09:16:23 +0000

    svn path=/trunk/matlab/; revision=15269
    Former-commit-id: f03b164f14c90308a6637d9574687bdf50dccb77

--- applications/EHY_tools/support_functions/EHYs.m
+++
@@ -1,8 +0,0 @@
-function EHY_userStats(mfilename)
-try
-    if ispc
-        filename=['n:\Deltabox\Postbox\Groenenboom, Julien\@LASTMONTH\' getenv('username') '__' mfilename '__' datestr(now,'yyyymmddHHMMSS') '.ehy'];
-        fid=fopen(filename,'w+');
-        fclose(fid);
-    end
-end
