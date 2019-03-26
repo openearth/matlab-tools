4904e70f22417ff2ef76150b3361016ca96b89e6
nederhof <nederhof@localhost>
2019-03-26 13:09:14 +0000

    Update to WES and added wind radii based on Nederhoff et al.
    svn path=/trunk/matlab/; revision=15280
    
    
    Former-commit-id: 88a1b0595c92d69badb86618e22c9e44eeebea1e

--- applications/DelftDashBoard/general/tropicalcyclones/wes/wes3.m
+++ applications/DelftDashBoard/general/tropicalcyclones/wes/wes3.m
@@ -174,7 +174,7 @@ for it=1:length(tc.track)
         end
         
         wind_speed(iphi,:) = vr;
-        if strcmpi(tc.cs.type,'geographic')
+        if strcmpi(spw.cs.type,'geographic')
             lat=tc.track(it).y;
         else
             if isfield(tc,'latitude')
@@ -499,9 +499,9 @@ nm2km=1.852;
 nt=length(tc.track);
 
 % Convert wind speeds to m/s
-switch lower(tc.wind_speed_unit)
+switch lower(spw.wind_speed_unit)
     case{'kts','kt','knots'}
-        tc.radius_velocity=tc.radius_velocity*kts2ms*spw.wind_conversion_factor; % Convert to m/s
+        tc.radius_velocity=spw.radius_velocity*kts2ms*spw.wind_conversion_factor; % Convert to m/s
         for it=1:nt
             tc.track(it).vmax=tc.track(it).vmax*kts2ms*spw.wind_conversion_factor; % Convert to m/s
         end
@@ -509,7 +509,7 @@ end
 
 % Convert radii to km
 for it=1:nt
-    switch lower(tc.radius_unit)
+    switch lower(spw.radius_unit)
         case{'nm'}
             for iq=1:length(tc.track(it).quadrant)
                 for irad=1:length(tc.track(it).quadrant(iq).radius)
@@ -529,7 +529,7 @@ nt=length(tc.track);
 for it=1:nt
     geofacx=1;
     geofacy=1;
-    switch lower(tc.cs.type(1:3))
+    switch lower(spw.cs.type(1:3))
         case{'geo','lat'}
             geofacy=111111;
             geofacx=geofacy*cos(tc.track(it).y*pi/180);
