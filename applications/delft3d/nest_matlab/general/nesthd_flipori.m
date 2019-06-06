c53c8ecb1ed72d2acdb4ec6a84c1633d5a70776d
kaaij <kaaij@localhost>
2019-06-06 15:39:13 +0000

    svn path=/trunk/matlab/; revision=15482
    Former-commit-id: a63b84f96378e11189e4b0f68e5c6045c02af46b

--- applications/delft3d/nest_matlab/general/nesthd_flipori.m
+++ applications/delft3d/nest_matlab/general/nesthd_flipori.m
@@ -3,16 +3,20 @@ function [bndval,thick] = nesthd_flipori(bndval,thick)
 %% Flips verical orientation
 no_times = length(bndval);
 no_pts   = size(bndval(1).value,1);
+kmax     = size(bndval(1).value,2)/2;
+
 lstci    = -1; if length(size(bndval(1).value)) == 3 lstci = nfs_inf.lstci; end
 
 for i_time = 1: no_times
     for i_point = 1: no_pts
         if lstci >= 1
             for l = 1: lstci
-                bndval(i_time).value(i_point,:,l) = flip(bndval(i_time).value(i_point,:,l));
+                bndval(i_time).value(i_point,1     :kmax  ,l) = flip(bndval(i_time).value(i_point,1     :kmax  ,l));
+                bndval(i_time).value(i_point,kmax+1:2*kmax,l) = flip(bndval(i_time).value(i_point,kmax+1:2*kmax,l));
             end
         else
-            bndval(i_time).value(i_point,:) = flip(bndval(i_time).value(i_point,:));
+            bndval(i_time).value(i_point,1     :kmax  ) = flip(bndval(i_time).value(i_point,1     :kmax  ));
+            bndval(i_time).value(i_point,kmax+1:2*kmax) = flip(bndval(i_time).value(i_point,kmax+1:2*kmax));
         end
     end
 end
