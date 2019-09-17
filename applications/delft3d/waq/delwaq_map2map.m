3670d69d8e8bc23c777dfcc7d5bbbed52e6f5faa
gaytan_sa <gaytan_sa@localhost>
2019-09-17 13:50:31 +0000

    This also interpolates the map in the vertical layers
    svn path=/trunk/matlab/; revision=15731
    
    
    Former-commit-id: 1eb9d8a968bbd00012c9e9be67b182a7a2a4e241

--- applications/delft3d/waq/delwaq_map2map.m
+++ applications/delft3d/waq/delwaq_map2map.m
@@ -5,18 +5,18 @@
 %   the interpolated data is saved as FILE2.
 %
 %   STRUCTOUT = DELWAQ_MAP2MAP(...,SUBSTANCESNAME,...) specifies substances to
-%   be used. SUBSTANCESNAME = 0 for all substances.      
+%   be used. SUBSTANCESNAME = 0 for all substances.
 %
-%   STRUCTOUT = DELWAQ_DIFF(...,TYPE) specifies alternate methods.  
+%   STRUCTOUT = DELWAQ_DIFF(...,TYPE) specifies alternate methods.
 %   The default is 'linear'.  Available methods are:
-% %  
+% %
 %      'nearest'  - nearest neighbor interpolation
 %       'linear'  - bilinear interpolation
 %       'spline'  - spline interpolation
 %       'cubic'   - bicubic interpolation as long as the data is
 %                   uniformly spaced, otherwise the same as 'spline'
 %
-%   See also: DELWAQ, DELWAQ_CONC, DELWAQ_RES, DELWAQ_TIME, DELWAQ_STAT, 
+%   See also: DELWAQ, DELWAQ_CONC, DELWAQ_RES, DELWAQ_TIME, DELWAQ_STAT,
 %             DELWAQ_INTERSECT
 
 %   Copyright 2015 Deltares, the Netherlands
@@ -38,22 +38,34 @@ Header = struct1.Header;
 T0 = struct1.T0;
 refTime =  [T0 1];
 
+% [0] if all substances 
 if nargin<5
     subs = 0;
 end
+
+% Type of interpolation 
 if nargin<6
     Type = 'nearest';
 end
 
+% Check if a vertical interpolation is nedeed
+z1 = size(gridStruct1.Index,3);
+z2 = size(gridStruct2.Index,3);
+if z1~=z2
+    interpLayers = 1;
+else
+    interpLayers = 0;
+end
+
 % Find substance
 if subs==0
-   substances = 1:length(struct1.SubsName);
+    substances = 1:length(struct1.SubsName);
 else
-   substances = find(strcmpi(struct1.SubsName,subs));
-end    
+    substances = find(strcmpi(struct1.SubsName,subs));
+end
+
+% Dimensions for grid1 and grid1 (x,y,z)
 NoSegPerLayer = gridStruct1.NoSegPerLayer;
-
-
 x1 = gridStruct1.cen.x;
 y1 = gridStruct1.cen.y;
 
@@ -61,20 +73,30 @@ x2 = gridStruct2.cen.x;
 y2 = gridStruct2.cen.y;
 
 %%
-
 for it = 1:struct1.NTimes
     
     disp([it struct1.NTimes])
     data = nan(length(substances),gridStruct2.NoSeg);
-    for isub = 1:length(substances)        
+    
+    for isub = 1:length(substances)
         [time, local1] = delwaq('read',struct1,substances(isub),0,it);
+        local1 = reshape(local1,NoSegPerLayer,[]);
         localdata = [];
-        for i=1:size(gridStruct1.Index,3)
-            j1 = (NoSegPerLayer*(i-1))+1;
-            j2 = NoSegPerLayer*i;
-            layer = local1(j1:j2)';
+       
+       % Interpolation to more layers
+       if interpLayers           
+            ns1 = NoSegPerLayer;
+            nl1  = size(gridStruct1.Index,3);
+            nl2 = size(gridStruct2.Index,3);
+            [l1,s1] = meshgrid(1:nl1,1:ns1);
+            [l2,s2] = meshgrid(linspace(1,nl1,nl2),1:ns1);            
+            local1 = naninterp(s1,l1,local1,s2,l2,'linear');
+        end
+        
+        for i=1:size(local1,2) 
+            layer = local1(:,i);
             local2  = naninterp(x1,y1,layer,x2,y2,Type);
-            localdata = [localdata(:)' local2(:)'];
+            localdata = [localdata(:)', local2(:)'];
         end
         data(isub,:) = localdata;
     end
