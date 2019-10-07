d71b2184702f11e7b3a51be6c9f66008721f11f9
ibnubarqy3 <ibnubarqy3@localhost>
2019-10-07 12:01:00 +0000

    svn path=/trunk/matlab/; revision=15821
    Former-commit-id: 58fc575195606b22deb3ba7cb4dc51ea0be2415d

--- maintenance/2_tutorial_Generation/evalinemptyworkspace.m
+++
@@ -1,9 +0,0 @@
-function evalinemptyworkspace(str)
-setappdata(0,'emptyworkspaceevaluation',str);
-eval_fun();
-rmappdata(0,'emptyworkspaceevaluation');
-end
-
-function eval_fun()
-eval(getappdata(0,'emptyworkspaceevaluation'));
-end
--- maintenance/2_tutorial_Generation/template/html/script/js/matlabhelp.js
+++
@@ -1,7 +0,0 @@
-function matlabpreparehelprefs() 
-  {
-  	if (document.location.protocol.indexOf('jar') == -1)
-  	{
-  		$(".matlabhref").attr('href',$(".matlabhref").attr('browserhref')).attr('target','new');
-  	}
-  }
