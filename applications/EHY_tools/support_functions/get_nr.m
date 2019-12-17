1bd1ce5da0535afb25db214991a213fb81fcf9db
kaaij <kaaij@localhost>
2019-12-17 17:08:49 +0000

    Ajusted to avoid station Antw1 or Antwn to be used when Antw is requested (don't know who defines these stupid station names).
    svn path=/trunk/matlab/; revision=16140
    
    
    Former-commit-id: a241bb8b058549319f0325cdb85c2eed6797942b

--- applications/EHY_tools/support_functions/get_nr.m
+++ applications/EHY_tools/support_functions/get_nr.m
@@ -1,9 +1,16 @@
 function nr = get_nr(cell_str,str)
 
 %% Restrict to length of str
-for i_cell = 1: length(cell_str)
-    cell_str{i_cell} = cell_str{i_cell}(1:min(length(cell_str{i_cell}), max(length(str),1)));
-end
+%  Cannot remeber why I did this
+% for i_cell = 1: length(cell_str)
+%    cell_str{i_cell} = cell_str{i_cell}(1:min(length(cell_str{i_cell}), max(length(str),1)));
+% end
 
 %% Get the index of str in the cell array of strings cell_str
-nr = find(strcmpi(str,cell_str) == 1);
+% not very elegant. Must do for now
+nr = [];
+for i_name = 1: length(cell_str)
+    if strcmpi(cell_str{i_name},str)
+        nr(end + 1) = i_name;
+    end
+end
