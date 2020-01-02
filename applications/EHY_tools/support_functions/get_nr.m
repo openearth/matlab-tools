08be6125c03ab69b143c57ecee2cd7bc5bda73e7
kaaij <kaaij@localhost>
2020-01-02 17:38:01 +0000

    svn path=/trunk/matlab/; revision=16162
    Former-commit-id: a91e3fa7d6ff1688ab96bb3466c0c582183c7180

--- applications/EHY_tools/support_functions/get_nr.m
+++ applications/EHY_tools/support_functions/get_nr.m
@@ -1,16 +1,11 @@
 function nr = get_nr(cell_str,str)
 
-%% Restrict to length of str
-%  Cannot remeber why I did this
-% for i_cell = 1: length(cell_str)
-%    cell_str{i_cell} = cell_str{i_cell}(1:min(length(cell_str{i_cell}), max(length(str),1)));
-% end
+%% Simple, might be more elegant methods but at least it always works
 
-%% Get the index of str in the cell array of strings cell_str
-% not very elegant. Must do for now
 nr = [];
-for i_name = 1: length(cell_str)
-    if strcmpi(cell_str{i_name},str)
-        nr(end + 1) = i_name;
+for i_cell = 1: length(cell_str)
+    if strcmpi(cell_str{i_cell}, str)
+        nr = i_cell;
+        break
     end
 end
