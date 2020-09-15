208deaca06d9415c70b50339329d80f2608c0417
verbr_wt <verbr_wt@localhost>
2020-09-15 15:18:44 +0000

    dflowfm_oldExt2newExt update:  - now also other boundary types (velocitybnd, salinitybnd, temperaturebnd, neumannbnd)  - both .cmp and .tim files
    svn path=/trunk/matlab/; revision=16587
    
    
    Former-commit-id: e511ec44d5408c7591cdb27466bf2ea5732f732a

--- applications/delft3d/dflowfm/dflowfm_oldExt2newExt.m
+++ applications/delft3d/dflowfm/dflowfm_oldExt2newExt.m
@@ -8,13 +8,25 @@ function dflowfm_oldExt2newExt(oldExtFile,varargin)
 %
 % Example:  dflowfm_oldExt2newExt('D:\model\externalforcing.ext')
 %
-% May2018	Julien Groenenboom      Only works for 'waterlevelbnd' and '.cmp'
+% May2018	Julien Groenenboom      
+% Sep2020   Wilbert Verbruggen
 
-OPT.outputDir=[fileparts(oldExtFile) filesep 'dflowfm_oldExt2newExt_OUTPUT'];
+OPT.outputDir=[fileparts(oldExtFile) filesep 'dflowfm_oldExt2newExt_OUTPUT_',datestr(now,'yyyymmdd_HHMMSS')];
 
 %% OPT
+OPT.bcFilePerItem = 0;
 if nargin>1
     OPT = setproperty(OPT,varargin);
+end
+
+% find mduFile
+
+mduFile = dir([fileparts(oldExtFile) filesep '*.mdu']);
+if ~isempty(mduFile)
+    mdu = dflowfm_io_mdu('read',[fileparts(oldExtFile),filesep,mduFile(1).name]);
+    refDate = datenum(num2str(mdu.time.RefDate),'yyyymmdd');
+else
+    warning('No .mdu file was found in folder where .ext file is located --> .tim files cannot be converted to .bc (refDate unknown)')
 end
 
 if ~exist(OPT.outputDir); mkdir(OPT.outputDir); end
@@ -22,51 +34,141 @@ if ~exist(OPT.outputDir); mkdir(OPT.outputDir); end
 oldExt=dflowfm_io_extfile('read',oldExtFile);
 
 for iE=1:length(oldExt)
+    disp([datestr(now),': Converting item ',num2str(iE),' of ',num2str(length(oldExt)),': ', oldExt(iE).quantity])
     if ~isempty(oldExt(iE).quantity)
-        if ~ismember(oldExt(iE).quantity,{'waterlevelbnd'})
-            disp(['Quantity ''' oldExt(iE).quantity ''' is not yet implemented in this conversion'])
+        if ~strcmpi(oldExt(iE).quantity(end-2:end),'bnd')
+            convertData = 0;
+            disp([datestr(now),': Quantity ''' oldExt(iE).quantity ''' is not a boundary condition, so cannot be converted to .bc'])
+        elseif ~ismember(oldExt(iE).quantity,{'waterlevelbnd','neumannbnd','dischargebnd','velocitybnd','salinitybnd','temperaturebnd'})
+            convertData = 0;
+            disp([datestr(now),': Quantity ''' oldExt(iE).quantity ''' is not yet implemented in this conversion'])
+        else
+            convertData = 1;
         end
-        switch oldExt(iE).quantity
-            case 'waterlevelbnd'
-                % pli file
-                pliFile=EHY_getFullWinPath(oldExt(iE).filename);
-                copyfile(pliFile,OPT.outputDir);
-                
-                [pathstr,name,ext]=fileparts(pliFile);
-                WlBcFile=[OPT.outputDir filesep name '_waterlevelbnd.bc'];
-                fidWlBc=fopen(WlBcFile,'a'); % create new or open existing
-                
-                pli=dflowfm_io_xydata('read',pliFile);
-                if exist([pathstr filesep name '_0001.cmp']) % astro components
-                    for iP=1:size(pli.DATA,1)
-                        % read cmp file
-                        cmpFile=[pathstr filesep name '_' sprintf('%04d',iP) '.cmp'];
+        
+        if convertData
+            % find and copy and open pli file
+            pliFile=EHY_getFullWinPath([fileparts(oldExtFile) filesep oldExt(iE).filename]);
+            copyfile(pliFile,OPT.outputDir);
+            [pathstr,name,ext]=fileparts(pliFile);
+            pli = landboundary('read',pliFile);
+            
+            % define bc name
+            if OPT.bcFilePerItem == 0
+                BcFile=[OPT.outputDir filesep oldExt(iE).quantity '.bc'];
+            else
+                BcFile=[OPT.outputDir filesep oldExt(iE).quantity '_',name,'_item_',num2str(iE),'.bc'];                
+            end
+            
+            % define unit
+            if strcmpi(oldExt(iE).quantity,'waterlevelbnd')
+                info.unit = 'm';
+            elseif strcmpi(oldExt(iE).quantity,'neumannbnd')
+                info.unit = '-';
+            elseif strcmpi(oldExt(iE).quantity,'velocitybnd')
+                info.unit = 'm/s';
+            elseif strcmpi(oldExt(iE).quantity,'dischargebnd')
+                info.unit = 'm3/s';
+            elseif strcmpi(oldExt(iE).quantity,'salinitybnd')
+                info.unit = 'ppt';
+            end
+            
+            % define info structure
+            info.bndType = oldExt(iE).quantity;
+            info.refDate = refDate;
+            
+            % check if forcing is based on astronomical cmp or timeseries
+            if exist([pathstr filesep name '_0001.cmp']) % astro components
+                for iP=1:size(pli,1)
+                    info.Name = [name '_' sprintf('%04d',iP)];
+                    
+                    cmpFile=[pathstr filesep info.Name '.cmp'];
+                    if exist(cmpFile,'file')
                         fidCmp=fopen(cmpFile,'r');
-                        cmp=textscan(fidCmp,'%s%s%s');
-                        cmp=[cmp{:}];
-                        cmp(find(~cellfun(@isempty,strfind(cmp(:,1),'*'))),:)=[]; % delete header/commented lines
                         
-                        % write cmp data to bc file
-                        fprintf(fidWlBc,'[forcing]\n');
-                        fprintf(fidWlBc,'Name                            = %s_%04d\n',name,iP);
-                        fprintf(fidWlBc,'Function                        = astronomic\n');
-                        fprintf(fidWlBc,'Quantity                        = astronomic component\n');
-                        fprintf(fidWlBc,'Unit                            = -\n');
-                        fprintf(fidWlBc,'Quantity                        = waterlevelbnd amplitude\n');
-                        fprintf(fidWlBc,'Unit                            = m\n');
-                        fprintf(fidWlBc,'Quantity                        = waterlevelbnd phase\n');
-                        fprintf(fidWlBc,'Unit                            = deg\n');
-                        for iC = 1:size(cmp,1)
-                            fprintf(fidWlBc,'%-9s',cmp{iC,1});
-                            fprintf(fidWlBc,'%20s  %20s\n',cmp{iC,2},cmp{iC,3});
-                        end
-                        fprintf(fidWlBc,'\n');
-                        fclose(fidCmp);
+                        cmpAll = textread(cmpFile,'%s','delimiter',char(13),'whitespace','');
+                        IDheaderlines = find(~cellfun(@isempty,strfind(cmpAll(:,1),'*')));
+                        
+                        fidCmp=fopen(cmpFile,'r');
+                        cmp=textscan(fidCmp,'%s%s%s','Headerlines',length(IDheaderlines));                        
+                        cmp=[cmp{:}];
+                        
+                        % write .bc file
+                        writeBCFile(BcFile,info,cmp,'cmp')
                     end
-                elseif exist([pathstr filesep name '_0001.tim']) % time series
-                    % to be implemented
                 end
-                fclose(fidWlBc);
+            elseif exist([pathstr filesep name '_0001.tim']) % time series
+                for iP=1:size(pli,1)
+                    info.Name = [name '_' sprintf('%04d',iP)];
+                    % read cmp file
+                    timFile=[pathstr filesep info.Name '.tim'];
+                    if exist(timFile,'file')
+                        tim = importdata(timFile);
+                        if isstruct(tim)
+                            tim = tim.data;
+                        end
+                        
+                        % write .bc file
+                        if ismember(oldExt(iE).quantity,{'salinitybnd','temperaturebnd'})
+                            if size(tim,2)==2
+                                info.vertPosType = 'single';
+                                info.vertInterpolation = 'linear';
+                            else
+                                error([datestr(now),': Conversion of item ',num2str(iE),' of ',num2str(length(oldExt)),': ', oldExt(iE).quantity,' failed. Writing of 3D salinity input is not implemented yet.'])
+                                info.vertPosType = 'zdatum';
+                            end
+                        end
+                        writeBCFile(BcFile,info,tim,'time')
+                        
+                    end
+                end
+                
+            else
+                disp([datestr(now),': No .cmp or .tim file found'])
+            end
         end
     end
+    clear info
 end
+
+
+function writeBCFile(BcFile,info,data,fileType)
+
+fidBc=fopen(BcFile,'a'); % create new or open existing
+
+% start writing forcing block
+fprintf(fidBc,'[forcing]\n');
+fprintf(fidBc,['Name                            = ',info.Name,'\n']);
+
+if strcmpi(fileType,'cmp')
+    % write cmp data to bc file
+    fprintf(fidBc,'Function                        = astronomic\n');
+    fprintf(fidBc,'Quantity                        = astronomic component\n');
+    fprintf(fidBc,'Unit                            = -\n');
+    fprintf(fidBc,['Quantity                        = ',info.bndType,' amplitude\n']);
+    fprintf(fidBc,['Unit                            = ',info.unit,'\n']);
+    fprintf(fidBc,['Quantity                        = ',info.bndType,' phase\n']);
+    fprintf(fidBc,'Unit                            = deg\n');
+    for iC = 1:size(data,1)
+        fprintf(fidBc,'%-9s',data{iC,1});
+        fprintf(fidBc,'%20s  %20s\n',data{iC,2},data{iC,3});
+    end
+elseif strcmpi(fileType,'time')
+    fprintf(fidBc,'Function                        = timeseries\n');
+    fprintf(fidBc,'Time-interpolation              = linear\n');
+    if isfield(info,'vertPosType')
+        fprintf(fidBc,['Vertical position type          = ',info.vertPosType,'\n']);
+        fprintf(fidBc,['Vertical interpolation          = ',info.vertInterpolation,'\n']);
+        
+    end
+    fprintf(fidBc,'Quantity                        = time\n');
+    fprintf(fidBc,['Unit                            = minutes since ',datestr(info.refDate,'yyyy-mm-dd HH:MM:SS'),'\n']);
+    fprintf(fidBc,['Quantity                        = ',info.bndType,'\n']);
+    fprintf(fidBc,['Unit                            = ',info.unit,'\n']);
+    fprintf(fidBc,'%u %3.7f \n',data');
+    fprintf(fidBc,'%s\n','');
+elseif strcmpi(fileType,'time_3D')
+    
+end
+fprintf(fidBc,'\n');
+fclose(fidBc);
