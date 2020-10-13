91a789991e98bf8cd2a86d99283fb046fafe9f94
ormondt <ormondt@localhost>
2020-10-13 05:24:17 +0000

    mex files for subgrid tables added (new format!)
    svn path=/trunk/matlab/; revision=16638
    
    
    Former-commit-id: 0d51f005de42adfca2c8274f3b18e7342986cb85

---
+++ applications/DelftDashBoard/general/sfincs/grid/sfincs_make_subgrid_file_v7.m
@@ -0,0 +1,579 @@
+function sfincs_make_subgrid_file_v7(dr,subgridfile,bathy,cs,nbin,refi,refj,uopt,maxdzdv,usemex)
+% Makes SFINCS subgrid file
+%
+% E.g.:
+%
+% dr='d:\sfincstest\run01\';         % output folder
+% subgridfile='sfincs.sbg';          % name of subgrid file
+% bathy(1).name='ncei_new_river_nc'; % first bathy dataset
+% bathy(2).name='usgs_ned_coastal';  % second bathy dataset
+% bathy(3).name='ngdc_crm';          % third bathy dataset
+% cs.name='WGS 84 / UTM zone 18N';   % cs name of model
+% cs.type='projected';               % cs type of model
+% nbin=5;                            % Number of bins in subgrid table
+% refi=20;                           % Subgrid refinement factor w.r.t. SFINCS grid in n direction
+% refj=20;                           % Subgrid refinement factor w.r.t. SFINCS grid in m direction
+%
+% sfincs_make_subgrid_file(dr,subgridfile,bathy,cs,nbin,refi,refj)
+%%
+
+
+if ~isempty(cs)
+    
+    global bathymetry
+    % Initialize bathymetry datasets (must run oetsettings for this to work, and also have delftdashboard directory)
+    if isempty(bathymetry)
+        bathymetry.dir='d:\delftdashboard\data\bathymetry\';
+        bathymetry=ddb_findBathymetryDatabases(bathymetry);
+    end
+    
+    subgridfile=[dr filesep subgridfile];
+    
+    for ib=1:length(bathy)
+        model.bathymetry(ib).name=bathy(ib).name;
+        ii=strmatch(lower(bathy(ib).name),lower(bathymetry.datasets),'exact');
+        model.bathymetry(ib).csname=bathymetry.dataset(ii).horizontalCoordinateSystem.name;
+        model.bathymetry(ib).cstype=bathymetry.dataset(ii).horizontalCoordinateSystem.type;
+    end
+    
+    model.cs.name=cs.name;
+    model.cs.type=cs.type;
+
+end
+
+% Read sfincs inputs
+inp=sfincs_read_input([dr 'sfincs.inp'],[]);
+
+mmax=inp.mmax;
+nmax=inp.nmax;
+dx=inp.dx;
+dy=inp.dy;
+x0=inp.x0;
+y0=inp.y0;
+rotation=inp.rotation;
+
+indexfile=[dr inp.indexfile];
+bindepfile=[dr inp.depfile];
+binmskfile=[dr inp.mskfile];
+[z,msk]=sfincs_read_binary_inputs(mmax,nmax,indexfile,bindepfile,binmskfile);
+
+di=dy;       % cell size
+dj=dx;       % cell size
+dif=dy/refi; % size of subgrid pixel
+djf=dx/refj; % size of subgrid pixel
+imax=nmax+1; % add extra cell to compute u and v in the last row/column
+jmax=mmax+1; % 
+
+cosrot=cos(rotation*pi/180);
+sinrot=sin(rotation*pi/180);
+
+%
+% % Fine grid
+% sout.zgf=zeros(nmax*refi,mmax*refj);
+% sout.zgf(sout.zgf==0)=NaN;
+
+% nrmax=min(round(2000*dif/di),round(2000*djf/dj)); % maximum number of subgrid cells in a block (limit size of subgrid block to 2000x2000)
+nrmax=2000;
+
+nib=floor(nrmax/(di/dif)); % nr of regular cells in a block
+njb=floor(nrmax/(dj/djf)); % nr of regular cells in a block
+
+ni=ceil(imax/nib); % nr of blocks
+nj=ceil(jmax/njb); % nr of blocks
+
+% ni=ni+1;
+% nj=nj+1;
+
+% % Initialize arrays
+% subgrd.z_zmin=zeros(ni*nib,nj*njb);
+% subgrd.z_zmax=zeros(ni*nib,nj*njb);
+% subgrd.z_volmax=zeros(ni*nib,nj*njb);
+% subgrd.z_dep=zeros(ni*nib,nj*njb,nbin);
+% %subgrd.z_vol=zeros(ni*nib,nj*njb,nbin);
+% 
+% subgrd.u_zmin=zeros(ni*nib,nj*njb);
+% subgrd.u_zmax=zeros(ni*nib,nj*njb);
+% subgrd.u_area=zeros(ni*nib,nj*njb,nbin);
+% subgrd.u_width=zeros(ni*nib,nj*njb,nbin);
+% subgrd.u_depth=zeros(ni*nib,nj*njb,nbin);
+% 
+% subgrd.v_zmin=zeros(ni*nib,nj*njb);
+% subgrd.v_zmax=zeros(ni*nib,nj*njb);
+% subgrd.v_area=zeros(ni*nib,nj*njb,nbin);
+% subgrd.v_width=zeros(ni*nib,nj*njb,nbin);
+% subgrd.v_depth=zeros(ni*nib,nj*njb,nbin);
+
+
+% Initialize temporary arrays
+subgrd.z_zmin=zeros(imax,jmax);
+subgrd.z_zmax=zeros(imax,jmax);
+subgrd.z_volmax=zeros(imax,jmax);
+subgrd.z_depth=zeros(imax,jmax,nbin);
+%subgrd.z_repdepth=zeros(imax,jmax,nbin);
+subgrd.z_hrep=zeros(imax,jmax,nbin);
+%subgrd.z_area=zeros(imax,jmax,nbin);
+%subgrd.z_width=zeros(imax,jmax,nbin);
+
+subgrd.u_zmin=zeros(nmax,mmax);
+subgrd.u_zmax=zeros(nmax,mmax);
+subgrd.u_hrep=zeros(nmax,mmax,nbin);
+%subgrd.u_area=zeros(nmax,mmax,nbin);
+%subgrd.u_width=zeros(nmax,mmax,nbin);
+%subgrd.u_depth=zeros(nmax,mmax,nbin);
+
+subgrd.v_zmin=zeros(nmax,mmax);
+subgrd.v_zmax=zeros(nmax,mmax);
+subgrd.v_hrep=zeros(nmax,mmax,nbin);
+%subgrd.v_area=zeros(nmax,mmax,nbin);
+%subgrd.v_width=zeros(nmax,mmax,nbin);
+%subgrd.v_depth=zeros(nmax,mmax,nbin);
+
+
+% subgrd1=subgrd;
+
+% % Polygons
+% flist=dir('*.pli');
+% npol=length(flist);
+% for ii=1:npol
+%     fname=flist(ii).name;
+%     [x,y]=landboundary('read',fname);
+%     [x,y]=convertCoordinates(x,y,'persistent','CS1.name',model.cs.name,'CS1.type',model.cs.type,'CS2.name','WGS 84','CS2.type','geographic');
+%     pol(ii).x=x;
+%     pol(ii).y=y;
+%     pol(ii).zmin=-4;
+% end
+
+ib=0;
+for ii=1:ni
+    for jj=1:nj
+        
+        %% Loop through blocks
+        
+        ib=ib+1;
+        
+        disp(['Processing block ' num2str(ib) ' of ' num2str(ni*nj)]);
+        
+        % cell indices
+        ic1=(ii-1)*nib+1;
+        jc1=(jj-1)*njb+1;
+        ic2=(ii  )*nib;
+        jc2=(jj  )*njb;
+        
+        ic2=min(ic2,imax);
+        jc2=min(jc2,jmax);
+        
+        % Actual number of grid cells in this block (after cutting off irrelevant edges)
+        nib1=ic2-ic1+1;
+        njb1=jc2-jc1+1;
+        
+        % Make subgrid
+        xx0=(jj-1)*njb*dj;
+        yy0=(ii-1)*nib*di;
+%        xx1=jj*njb*dj-djf;
+%        yy1=ii*nib*di-dif;
+        xx1=xx0 + njb1*dj - djf;
+        yy1=yy0 + nib1*di - dif;
+%         xx1=xx1+dj; % add extra row cuz we need data in u points
+%         yy1=yy1+di; % add extra row cuz we need data in v points
+        xx=xx0:djf:xx1;
+        yy=yy0:djf:yy1;
+        xx=xx+0.5*djf;
+        yy=yy+0.5*dif;
+        [xx,yy]=meshgrid(xx,yy);
+        xg0 = x0 + cosrot*xx - sinrot*yy;
+        yg0 = y0 + sinrot*xx + cosrot*yy;
+        clear xx yy
+        
+        % x=inp.x0+cos(rot)*xg-sin(rot)*yg;
+        % y=inp.y0+sin(rot)*xg+cos(rot)*yg;
+        
+        % Initialize depth of subgrid at NaN
+        zg=zeros(size(xg0));
+        zg(zg==0)=NaN;
+        
+        %         if jj==62
+        %             shite=5;
+        %         end
+        if ~isempty(cs)
+            % Loop through bathymetry datasets
+            for ibat=1:length(model.bathymetry)
+                
+                % Convert subgrid to WGS 84 (should it not be converted to the coordinate system of the bathymetry dataset? Yes, but let's assume for now that the data are in WGS 84!)
+                %        [xg,yg]=convertCoordinates(xg,yg,'persistent','CS1.name',model.cs.name,'CS1.type',model.cs.type,'CS2.name','WGS 84','CS2.type','geographic');
+                
+                %             ddxx=(bboxx(2)-bboxx(1))/20;
+                %             ddyy=(bboxy(2)-bboxy(1))/20;
+                %             bboxx(1)=bboxx(1)-ddxx;
+                %             bboxx(2)=bboxx(2)+ddxx;
+                %             bboxy(1)=bboxy(1)-ddyy;
+                %             bboxy(2)=bboxy(2)+ddyy;
+                %             [bboxx,bboxy]=convertCoordinates(bboxx,bboxy,'persistent','CS1.name',model.cs.name,'CS1.type',model.cs.type,'CS2.name',model.bathymetry(ibat).csname,'CS2.type',model.bathymetry(ibat).cstype);
+                [xg,yg]=convertCoordinates(xg0,yg0,'persistent','CS1.name',model.cs.name,'CS1.type',model.cs.type,'CS2.name',model.bathymetry(ibat).csname,'CS2.type',model.bathymetry(ibat).cstype);
+                
+                bboxx=[nanmin(nanmin(xg)) nanmax(nanmax(xg))];
+                bboxy=[nanmin(nanmin(yg)) nanmax(nanmax(yg))];
+                
+                % Now get the bathy data for this block
+                
+                % Determine bounding box
+                %             xmin=bboxx(1);
+                %             xmax=bboxx(2);
+                %             ymin=bboxy(1);
+                %             ymax=bboxy(2);
+                
+                if ~isempty(find(isnan(zg)))
+                    
+                    %                [xx,yy,zz,ok]=ddb_getBathymetry(bathymetry,[xmin xmax],[ymin ymax],'bathymetry',model.bathymetry(ibat).name,'maxcellsize',min(dif,djf),'quiet');
+                    [xx,yy,zz,ok]=ddb_getBathymetry(bathymetry,bboxx,bboxy,'bathymetry',model.bathymetry(ibat).name,'maxcellsize',min(dif,djf),'quiet');
+                    
+                    if ~isempty(find(~isnan(zz)))
+                        zg1=interp2(xx,yy,zz,xg,yg);
+                        zg(isnan(zg))=zg1(isnan(zg));
+                    end
+                end
+                
+            end
+            
+        else
+            
+            xx=bathy.x;
+            yy=bathy.y;
+            zz=bathy.z;
+            zg=interp2(xx,yy,zz,xg0,yg0);
+            zg(isnan(zg))=0;
+            
+        end
+        
+        
+        if ~isempty(find(isnan(zg)))
+            error(['NaNs found in bathymetry!!! Block ii = ' num2str(ii) ', jj = ' num2str(jj)]);
+        end
+        
+        
+        %         % Adjust the bathymetries in polygons (commented out for now)
+        %         for ipol=1:npol
+        %             xminp=min(pol(ipol).x);
+        %             xmaxp=max(pol(ipol).x);
+        %             yminp=min(pol(ipol).y);
+        %             ymaxp=max(pol(ipol).y);
+        %             if (xminp<xmin && xmaxp<xmin) || (xminp>xmax && xmaxp>xmax) || (yminp<ymin && ymaxp<ymin) || (yminp>ymax && ymaxp>ymax)
+        %                 break
+        %             end
+        %             inpol=inpolygon(xg,yg,pol(ipol).x,pol(ipol).y);
+        %             zg(inpol)=min(zg(inpol),pol(ipol).zmin);
+        %         end
+        
+        %         % Fill the fine grid ()
+        %         if1=(ic1-1)*refi+1;
+        %         if2=(ic2  )*refi  ;
+        %         jf1=(jc1-1)*refj+1;
+        %         jf2=(jc2  )*refj  ;
+        %         sout.zgf(if1:if2,jf1:jf2)=zg(1:end-refi,1:end-refj); % Don't need the row for velocity points here
+        
+        clear xg yg zg1 xx yy zz
+        
+        %% Now compute subgrid properties
+        
+        % Volumes
+        np=0;
+        d=zeros(nib1,njb1,refi*refj); % First create 3D matrix (nmax,mmax,refi*refj)
+        d(d==0)=NaN;
+        for iref=1:refi
+            for jref=1:refj
+                np=np+1;
+                i1=iref;
+                i2=i1+(nib1-1)*refi;
+                j1=jref;
+                j2=j1+(njb1-1)*refj;
+                zg1=zg(i1:refi:i2,j1:refj:j2);
+                d(:,:,np)=zg1;
+            end
+        end
+        
+        if usemex
+            [zmin,zmax,volmax,ddd]=mx_subgrid_volumes(d,nbin,dx,dy,maxdzdv);
+        else
+            % Should get rid of this option
+            [zmin,zmax,volmax,ddd]=sfincs_subgrid_volumes_ddd(d,nbin,dx,dy);
+        end
+        subgrd.z_zmin(ic1:ic2,jc1:jc2)=zmin;
+        subgrd.z_zmax(ic1:ic2,jc1:jc2)=zmax;
+        subgrd.z_volmax(ic1:ic2,jc1:jc2)=volmax;
+        subgrd.z_depth(ic1:ic2,jc1:jc2,:)=ddd;
+         
+
+        if usemex
+            [zmin,zmax,ddd]=mx_subgrid_depth(d,nbin,dx);
+        else
+            % Should get rid of this option
+            [zmin,zmax,ddd]=sfincs_subgrid_area_and_depth_v5(d,nbin,dy);
+        end
+
+        subgrd.z_hrep(ic1:ic2,jc1:jc2,:)=ddd;
+                
+    end
+end
+
+subgrd1.z_zmin   = subgrd.z_zmin(1:nmax,1:mmax,:);
+subgrd1.z_zmax   = subgrd.z_zmax(1:nmax,1:mmax,:);
+subgrd1.z_volmax = subgrd.z_volmax(1:nmax,1:mmax,:);
+subgrd1.z_depth  = subgrd.z_depth(1:nmax,1:mmax,:);
+
+if ~usemex
+    % Should get rid of this option
+    % Smooth very steep slopes
+    for n=1:nmax
+        for m=1:mmax
+            [volmax,dep,iok]=adjust_zvol_slope(subgrd.z_zmin(n,m),subgrd.z_volmax(n,m),squeeze(subgrd.z_depth(n,m,:)),dx,maxdzdv);
+            if ~iok
+                subgrd1.z_volmax(n,m)=volmax;
+                subgrd1.z_depth(n,m,:)=dep;
+            end
+        end
+    end
+end
+
+switch uopt
+    
+    case{'mean'}
+        
+        subgrd1.u_zmin=0.5*(subgrd.z_zmin(1:nmax,1:mmax)+subgrd.z_zmin(1:nmax,2:mmax+1));
+        subgrd1.u_zmax=0.5*(subgrd.z_zmax(1:nmax,1:mmax)+subgrd.z_zmax(1:nmax,2:mmax+1));
+        
+        for ibin=1:nbin
+            subgrd1.u_hrep(1:nmax,1:mmax,ibin)=0.5*(subgrd.z_hrep(1:nmax,1:mmax,ibin)+subgrd.z_hrep(1:nmax,2:mmax+1,ibin));
+        end
+        
+        subgrd1.v_zmin=0.5*(subgrd.z_zmin(1:nmax,1:mmax)+subgrd.z_zmin(2:nmax+1,1:mmax));
+        subgrd1.v_zmax=0.5*(subgrd.z_zmax(1:nmax,1:mmax)+subgrd.z_zmax(2:nmax+1,1:mmax));
+        for ibin=1:nbin
+            subgrd1.v_hrep(1:nmax,1:mmax,ibin)=0.5*(subgrd.z_hrep(1:nmax,1:mmax,ibin)+subgrd.z_hrep(2:nmax+1,1:mmax,ibin));
+        end
+        
+    case{'min'}
+
+        % Should get rid of this option ???
+        
+        hh=max(max(subgrd.z_zmax))+1; % Maximum elevation in the model
+        
+        % U points
+        
+        subgrd1.u_zmin=max(subgrd.z_zmin(1:nmax,1:mmax),subgrd.z_zmin(1:nmax,2:mmax+1));
+        subgrd1.u_zmax=max(subgrd.z_zmax(1:nmax,1:mmax),subgrd.z_zmax(1:nmax,2:mmax+1));
+        
+        zu=zeros(nmax,mmax,nbin);
+        for ibin=1:nbin
+            zu(:,:,ibin)=subgrd1.u_zmin+ibin*(subgrd1.u_zmax-subgrd1.u_zmin)/nbin;
+        end
+        
+        % Left
+        z_zmin=subgrd.z_zmin(1:nmax,1:mmax);
+        z_zmax=subgrd.z_zmax(1:nmax,1:mmax);
+        zmaxmin=z_zmax-z_zmin;
+        zmaxmin=max(zmaxmin,1e-4);
+        z_left=zeros(nmax,mmax,nbin+1);
+        for ibin=1:nbin+1
+            z_left(:,:,ibin)=z_zmin+(ibin-1)*zmaxmin/nbin;
+        end
+        h_left=zeros(nmax,mmax,nbin+1);
+        h_left(:,:,2:nbin+1)=subgrd.z_hrep(1:nmax,1:mmax,:);
+        % Add extra point
+        z_left(:,:,nbin+2)=zeros(nmax,mmax) + hh;
+        h_left(:,:,nbin+2)=h_left(:,:,nbin+1) + hh - z_zmax;
+        
+        % Right
+        z_zmin=subgrd.z_zmin(1:nmax,2:mmax+1);
+        z_zmax=subgrd.z_zmax(1:nmax,2:mmax+1);
+        zmaxmin=z_zmax-z_zmin;
+        zmaxmin=max(zmaxmin,1e-4);
+        z_right=zeros(nmax,mmax,nbin+1);
+        for ibin=1:nbin+1
+            z_right(:,:,ibin)=z_zmin+(ibin-1)*zmaxmin/nbin;
+        end
+        h_right=zeros(nmax,mmax,nbin+1);
+        h_right(:,:,2:nbin+1)=subgrd.z_hrep(1:nmax,2:mmax+1,:);        
+        % Add extra point
+        z_right(:,:,nbin+2)=zeros(nmax,mmax) + hh;
+        h_right(:,:,nbin+2)=h_right(:,:,nbin+1) + hh - z_zmax;
+        
+        for n=1:nmax
+            for m=1:mmax
+                
+                % Pick smallest width
+                
+                h1=interp1(squeeze(z_left(n,m,:)),squeeze(h_left(n,m,:)),squeeze(zu(n,m,:)));
+                h2=interp1(squeeze(z_right(n,m,:)),squeeze(h_right(n,m,:)),squeeze(zu(n,m,:)));
+                i1=find(h1<h2);
+                i2=find(h1>=h2);
+                subgrd1.u_hrep(n,m,i1)=h1(i1);
+                subgrd1.u_hrep(n,m,i2)=h2(i2);
+            end
+        end
+        
+        
+        % V points
+        
+        subgrd1.v_zmin=max(subgrd.z_zmin(1:nmax,1:mmax),subgrd.z_zmin(2:nmax+1,1:mmax));
+        subgrd1.v_zmax=max(subgrd.z_zmax(1:nmax,1:mmax),subgrd.z_zmax(2:nmax+1,1:mmax));
+        
+        zu=zeros(nmax,mmax,nbin);
+        for ibin=1:nbin
+            zu(:,:,ibin)=subgrd1.v_zmin+ibin*(subgrd1.v_zmax-subgrd1.v_zmin)/nbin;
+        end
+        
+        % Left
+        z_zmin=subgrd.z_zmin(1:nmax,1:mmax);
+        z_zmax=subgrd.z_zmax(1:nmax,1:mmax);
+        zmaxmin=z_zmax-z_zmin;
+        zmaxmin=max(zmaxmin,1e-4);
+        z_left=zeros(nmax,mmax,nbin+1);
+        for ibin=1:nbin+1
+            z_left(:,:,ibin)=z_zmin+(ibin-1)*zmaxmin/nbin;
+        end
+        h_left=zeros(nmax,mmax,nbin+1);
+        h_left(:,:,2:nbin+1)=subgrd.z_hrep(1:nmax,1:mmax,:);
+        % Add extra point
+        z_left(:,:,nbin+2)=zeros(nmax,mmax) + hh;
+        h_left(:,:,nbin+2)=h_left(:,:,nbin+1) + hh - z_zmax;
+        
+        % Right
+        z_zmin=subgrd.z_zmin(2:nmax+1,1:mmax);
+        z_zmax=subgrd.z_zmax(2:nmax+1,1:mmax);
+        zmaxmin=z_zmax-z_zmin;
+        zmaxmin=max(zmaxmin,1e-4);
+        z_right=zeros(nmax,mmax,nbin+1);
+        for ibin=1:nbin+1
+            z_right(:,:,ibin)=z_zmin+(ibin-1)*zmaxmin/nbin;
+        end
+        h_right=zeros(nmax,mmax,nbin+1);
+        h_right(:,:,2:nbin+1)=subgrd.z_hrep(2:nmax+1,1:mmax,:);        
+        % Add extra point
+        z_right(:,:,nbin+2)=zeros(nmax,mmax) + hh;
+        h_right(:,:,nbin+2)=h_right(:,:,nbin+1) + hh - z_zmax;
+        
+        for n=1:nmax
+            for m=1:mmax
+                
+                % Pick smallest width
+                
+                h1=interp1(squeeze(z_left(n,m,:)),squeeze(h_left(n,m,:)),squeeze(zu(n,m,:)));
+                h2=interp1(squeeze(z_right(n,m,:)),squeeze(h_right(n,m,:)),squeeze(zu(n,m,:)));
+                i1=find(h1<h2);
+                i2=find(h1>=h2);
+                subgrd1.v_hrep(n,m,i1)=h1(i1);
+                subgrd1.v_hrep(n,m,i2)=h2(i2);
+
+            end
+        end
+end
+
+sfincs_write_binary_subgrid_tables_v7(subgrd1,msk,nbin,subgridfile);
+
+%%
+function [dmin,dmax,volmax,ddd]=sfincs_subgrid_volumes_ddd(d,nbin,dx,dy)
+
+nmax=size(d,1);
+mmax=size(d,2);
+nrd=size(d,3);
+
+d=sort(d,3);
+
+dmin=min(d,[],3);
+dmax=max(d,[],3);
+a=dx*dy/nrd; % pixel area
+
+vol=zeros(nmax,mmax,nrd);
+
+% Loop through depths
+for j=2:nrd
+    lev=squeeze(d(:,:,j));
+    lev=repmat(lev,[1 1 nrd]);
+    vvv=sum(max(lev-d,0)*a,3);
+    vvv=max(vvv,squeeze(vol(:,:,j-1))+1e-9);
+    vol(:,:,j)=vvv;
+end
+ddd=zeros(nmax,mmax,nbin);
+
+volmax=squeeze(vol(:,:,end));
+volbin=volmax/nbin;
+
+for ibin=1:nbin-1
+    vvv=ibin*volbin;
+    for n=1:nmax
+        for m=1:mmax
+            ddd(n,m,ibin)=interp1(squeeze(vol(n,m,:)),squeeze(d(n,m,:)),vvv(n,m));
+        end
+    end
+end
+ddd(:,:,nbin)=dmax;
+
+%%
+function [dmin,dmax,ddd]=sfincs_subgrid_area_and_depth_v5(d,nbin,dx)
+
+nmax=size(d,1);
+mmax=size(d,2);
+nrd=size(d,3);
+
+dmin=min(d,[],3);
+dmax=max(d,[],3);
+
+dmax(dmax<dmin+0.01)=dmax(dmax<dmin+0.01)+0.01;
+% if dmax<dmin+0.01
+%     dmax=dmax+0.01;
+% end
+
+dbin=(dmax-dmin)/nbin;
+dbin=max(dbin,1e-9);
+
+ddd=zeros(nmax,mmax,nbin);
+
+dw=dx/nrd;
+
+% Next bins
+for ibin=1:nbin    
+    zb=dmin+ibin*dbin;
+    zb=repmat(zb,[1 1 nrd]);
+    h=zb-d;
+    h=max(h,0);
+    q=h.^(5/3)*dw;
+    qtot=sum(q,3);
+    hrep=(qtot./dx).^(3/5);
+    ddd(:,:,ibin)=hrep;    
+end
+
+%%
+function [volmax,dep,iok]=adjust_zvol_slope(zmin,volmax,dep,dx,mxsteep)
+
+iok=1;
+
+nbin=length(dep);
+volmax=volmax/dx^2;
+
+dep=[zmin;dep];
+dz=[dep(2:end)-dep(1:end-1)];
+dv=volmax/nbin;
+dzdv=dz./dv;
+vvr=0:dv:volmax;
+
+if max(dzdv)>mxsteep
+    
+    iok=0;
+    
+    % need to adjust
+    for ibin=1:nbin
+        % Set vvru in this bin to be higher than in bin below
+        vvru=max(vvr(ibin+1),vvr(ibin)+1e-9);
+        dzdv1=dz(ibin)/(vvru - vvr(ibin)); % slope
+        if dzdv1>mxsteep
+            vvr(ibin+1)=vvr(ibin)+dz(ibin)/mxsteep;
+        end
+    end
+    
+    % Now re-interpolate
+    vvr=vvr*dx^2;
+    volmax=vvr(end);
+    dv=volmax/nbin;
+    vv=dv:dv:volmax;
+    dep=interp1(vvr,dep,vv);
+    
+end
