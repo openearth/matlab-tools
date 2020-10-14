6f65c5c3730372699b372c59be2cdf2326596af5
ormondt <ormondt@localhost>
2020-10-14 15:21:39 +0000

    sfincs some functions added to compute sfincs output on high-res dem
    svn path=/trunk/matlab/; revision=16647
    
    
    Former-commit-id: c2bc70e615d0a067812b1d7f056793357c9ccb2c

---
+++ applications/DelftDashBoard/general/sfincs/dem/sfincs_make_floodmap.m
@@ -0,0 +1,49 @@
+clear variables;close all;
+
+dr='d:\PhD\sfincs_tests\subgrid\charleston\run01\';
+topofile='charleston_ncei_14m.mat';
+
+matfile=[dr 'sfincs.mat'];
+
+s2=load(topofile);
+
+demx=s2.parameters(1).parameter.x(1,:);  % must be an 1xn vector
+demy=s2.parameters(1).parameter.y(:,1)'; % must be an 1xn vector
+demz=s2.parameters(1).parameter.val;
+
+clear s2
+
+inpfile='sfincs.inp';
+outfile='charleston_ncei_14m_indices.dat';
+cs_dem.name='WGS 84';
+cs_dem.type='geographic';
+cs_sfincs.name='WGS 84 / UTM zone 18N';
+cs_sfincs.type='projected';
+
+% % Write indices file
+% sfincs_write_indices_for_dem(inpfile,outfile,demx,demy,cs_dem,cs_sfincs);
+
+% Read indices file
+[indices,ndem,mdem]=sfincs_read_indices_for_dem(outfile);
+
+% Load sfincs output
+s=load(matfile);
+zs=s.parameters(1).parameter.val(:,1:end-1,1:end-1);
+zsmax=squeeze(max(zs,[],1));
+
+zsmax_dem=sfincs_get_values_for_dem(zsmax,indices,ndem,mdem);
+
+[xx,yy]=meshgrid(demx,demy);
+
+inan=find(demz<1.0 | zsmax_dem-demz<0.2); % Don't plot data with bed level below 1 or with water depth below 0.2
+hh=zsmax_dem-demz;
+hh(inan)=NaN; % Don't plot data with bed level below 1
+zsmax_dem(inan)=NaN; % Don't plot data with bed level below 1
+
+figure(1)
+pcolor(xx,yy,zsmax_dem);shading flat;axis equal;colorbar;title('max water level');
+colormap('jet');caxis([0 6]);
+
+figure(2)
+pcolor(xx,yy,hh);shading flat;axis equal;colorbar;title('max water depth');
+colormap('jet');caxis([0 6]);
---
+++ applications/DelftDashBoard/general/sfincs/dem/sfincs_write_indices_for_dem.m
@@ -0,0 +1,69 @@
+function sfincs_write_indices_for_dem(inpfile,outfile,demx,demy,cs_dem,cs_sfincs)
+%
+% E.g.
+%
+% dr='d:\PhD\sfincs_tests\subgrid\charleston\run01\';
+% topofile='charleston_ncei_14m.mat';
+% 
+% matfile=[dr 'sfincs.mat'];
+% 
+% s2=load(topofile);
+% 
+% demx=s2.parameters(1).parameter.x(1,:);  % must be an 1xn vector
+% demy=s2.parameters(1).parameter.y(:,1)'; % must be an 1xn vector
+% demz=s2.parameters(1).parameter.val;
+% 
+% clear s2
+% 
+% inpfile='sfincs.inp';
+% outfile='charleston_ncei_14m_indices.dat';
+% cs_dem.name='WGS 84';
+% cs_dem.type='geographic';
+% cs_sfincs.name='WGS 84 / UTM zone 18N';
+% cs_sfincs.type='projected';
+% 
+% sfincs_write_indices_for_dem(inpfile,outfile,demx,demy,cs_dem,cs_sfincs);
+
+inp=sfincs_initialize_input;
+inp=sfincs_read_input(inpfile,inp);
+
+% sfincs grid
+[xg0,yg0]=meshgrid(0:inp.dx:(inp.mmax)*inp.dx,0:inp.dy:(inp.nmax)*inp.dy);
+rot=inp.rotation*pi/180;
+xg=inp.x0+cos(rot)*xg0-sin(rot)*yg0;
+yg=inp.y0+sin(rot)*xg0+cos(rot)*yg0;
+clear xg0 yg0
+
+% Convert sfincs grid to dem coordinate system
+[xg,yg]=convertCoordinates(xg,yg,'persistent','CS1.name',cs_sfincs.name,'CS1.type',cs_sfincs.type,'CS2.name',cs_dem.name,'CS2.type',cs_dem.type);        
+
+% size of dem
+ntopo=size(demy,2); 
+mtopo=size(demx,2);
+
+% Get indices. Do this in blocks as the mex file can't handle large arrays
+nn1=ceil(ntopo/500);
+mm1=ceil(mtopo/500);
+indices=zeros(ntopo,mtopo);
+for in=1:nn1
+    for im=1:mm1
+        i1=(in-1)*500+1;
+        j1=(im-1)*500+1;
+        i2=i1+499;
+        j2=j1+499;
+        i2=min(i2,ntopo);
+        j2=min(j2,mtopo);
+        ind=mx_find_grid_indices(demx(j1:j2),demy(i1:i2),xg,yg);
+        ind=double(ind);
+        indices(i1:i2,j1:j2)=ind;
+    end
+end
+
+% Now write output file
+%indices=reshape(indices,[1 ntopo*mtopo]);
+
+fid=fopen(outfile,'w');
+fwrite(fid,ntopo,'integer*4');
+fwrite(fid,mtopo,'integer*4');
+fwrite(fid,indices,'integer*4');
+fclose(fid);
