d4cca373a730e379611fa07983924a2d3441453c
chavarri <chavarri@localhost>
2020-10-19 14:40:26 +0000

    header
    svn path=/trunk/matlab/; revision=16658
    
    
    Former-commit-id: dfcb84d8c424c105e75e4f449c0ef5045d24c670

--- applications/vtools/general/messageOut.m
+++ applications/vtools/general/messageOut.m
@@ -4,11 +4,11 @@
 % 
 %Victor Chavarrias (victor.chavarrias@deltares.nl)
 %
-%$Revision: 16571 $
-%$Date: 2020-09-08 14:39:17 +0200 (Tue, 08 Sep 2020) $
-%$Author: chavarri $
-%$Id: accents2latex.m 16571 2020-09-08 12:39:17Z chavarri $
-%$HeadURL: https://svn.oss.deltares.nl/repos/openearthtools/trunk/matlab/applications/vtools/general/accents2latex.m $
+%$Revision$
+%$Date$
+%$Author$
+%$Id$
+%$HeadURL$
 %
 %writes message to log file and screen with time stamp
 
