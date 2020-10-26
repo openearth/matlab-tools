e8d119f7305f76b55f707b94f336c7e282ad2236
ormondt <ormondt@localhost>
2020-10-26 14:23:02 +0000

    sfincs_write_binary_subgrid_tables_v7.m update
    svn path=/trunk/matlab/; revision=16676
    
    
    Former-commit-id: a49364555ffc1e698ec493c2c54a9beb39b9b57e

--- applications/DelftDashBoard/general/sfincs/fileio/sfincs_write_binary_subgrid_tables_v7.m
+++ applications/DelftDashBoard/general/sfincs/fileio/sfincs_write_binary_subgrid_tables_v7.m
@@ -4,7 +4,10 @@ function sfincs_write_binary_subgrid_tables_v7(subgrd,msk,nbin,subgridfile,uopt)
 % nmax=size(msk,1);
 % mmax=size(msk,2);
 
-indices=find(msk>0);
+iincl=0;  % include only msk=1 and msk=2
+%iincl=-1; % include all points
+
+indices=find(msk>iincl);
 
 fid=fopen(subgridfile,'w');
 
@@ -22,30 +25,30 @@ end
 fwrite(fid,nbin,'integer*4');
 
 % Volumes
-val=subgrd.z_zmin(msk>0);
+val=subgrd.z_zmin(msk>iincl);
 fwrite(fid,val,'real*4');
-val=subgrd.z_zmax(msk>0);
+val=subgrd.z_zmax(msk>iincl);
 fwrite(fid,val,'real*4');
 volmax=squeeze(subgrd.z_volmax);
-val=volmax(msk>0);
+val=volmax(msk>iincl);
 fwrite(fid,val,'real*4');
 for ibin=1:nbin
 %    v=squeeze(subgrd.z_vol(:,:,ibin));
     v=squeeze(subgrd.z_depth(:,:,ibin));
-    val=v(msk>0);
+    val=v(msk>iincl);
     fwrite(fid,val,'real*4');
 end
 
 % U points
-val=subgrd.u_zmin(msk>0);
+val=subgrd.u_zmin(msk>iincl);
 fwrite(fid,val,'real*4');
-val=subgrd.u_zmax(msk>0);
+val=subgrd.u_zmax(msk>iincl);
 fwrite(fid,val,'real*4');
-val=subgrd.u_dhdz(msk>0);
+val=subgrd.u_dhdz(msk>iincl);
 fwrite(fid,val,'real*4');
 for ibin=1:nbin
     v=squeeze(subgrd.u_hrep(:,:,ibin));
-    val=v(msk>0);
+    val=v(msk>iincl);
     fwrite(fid,val,'real*4');
 end
 % for ibin=1:nbin
@@ -56,15 +59,15 @@ end
 % end
 
 % V points
-val=subgrd.v_zmin(msk>0);
+val=subgrd.v_zmin(msk>iincl);
 fwrite(fid,val,'real*4');
-val=subgrd.v_zmax(msk>0);
+val=subgrd.v_zmax(msk>iincl);
 fwrite(fid,val,'real*4');
-val=subgrd.v_dhdz(msk>0);
+val=subgrd.v_dhdz(msk>iincl);
 fwrite(fid,val,'real*4');
 for ibin=1:nbin
     v=squeeze(subgrd.v_hrep(:,:,ibin));
-    val=v(msk>0);
+    val=v(msk>iincl);
     fwrite(fid,val,'real*4');
 end
 % for ibin=1:nbin
