6b05915c89918daef0fdaa32ba19de67afd4c6a7
ormondt <ormondt@localhost>
2020-10-26 14:21:05 +0000

    sfincs_write_binary_inputs.m update
    svn path=/trunk/matlab/; revision=16674
    
    
    Former-commit-id: b48c09a4789596882f08e8d9ef771383d88bdfe0

--- applications/DelftDashBoard/general/sfincs/fileio/sfincs_write_binary_inputs.m
+++ applications/DelftDashBoard/general/sfincs/fileio/sfincs_write_binary_inputs.m
@@ -1,18 +1,24 @@
 function sfincs_write_binary_inputs(z,msk,indexfile,bindepfile,binmskfile)
 
 % Writes binary input files for SFINCS
-    
+
+iincl=0;  % include only msk=1 and msk=2
+%iincl=-1; % include all points
+
 % Index file
-indices=find(msk>0);
-mskv=msk(msk>0);
+indices=find(msk>iincl);
+
+mskv=msk(msk>iincl);
+
 fid=fopen(indexfile,'w');
 fwrite(fid,length(indices),'integer*4');
 fwrite(fid,indices,'integer*4');
 fclose(fid);
 
 % Depth file
-zv=z(msk>0);
+zv=z(msk>iincl);
 % zv=max(zv,-5);
+
 fid=fopen(bindepfile,'w');
 fwrite(fid,zv,'real*4');
 fclose(fid);
