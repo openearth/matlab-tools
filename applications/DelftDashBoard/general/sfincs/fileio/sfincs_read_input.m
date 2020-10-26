e5cc98e3e04f4ba17a36f370837391e53e296f41
ormondt <ormondt@localhost>
2020-10-26 14:26:13 +0000

    sfincs_read_input.m update
    svn path=/trunk/matlab/; revision=16678
    
    
    Former-commit-id: 13d5bc81dc7bec1c71cf425005f6b9985be1f670

--- applications/DelftDashBoard/general/sfincs/fileio/sfincs_read_input.m
+++ applications/DelftDashBoard/general/sfincs/fileio/sfincs_read_input.m
@@ -21,3 +21,15 @@ fclose(fid);
 inp.tref=datenum(inp.tref,'yyyymmdd HHMMSS');
 inp.tstart=datenum(inp.tstart,'yyyymmdd HHMMSS');
 inp.tstop=datenum(inp.tstop,'yyyymmdd HHMMSS');
+
+keywords=fieldnames(inp);
+
+for ii=1:length(keywords)
+    keyw=keywords{ii};
+    switch lower(keyw)
+        case{'cdwnd','cdval'}
+            if ischar(inp.(keyw))
+                inp.(keyw)=str2num(inp.(keyw));
+            end
+    end
+end
