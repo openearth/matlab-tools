8a22c6cdfe5069d340475a2592761478419b7ecc
chavarri <chavarri@localhost>
2020-10-30 08:23:41 +0000

    svn path=/trunk/matlab/; revision=16746
    Former-commit-id: 5bb7ac69bf9c0b39b0a0492cc291a54e3e08eca8

--- applications/vtools/D3D/fcn/D3D_remove_duplicate_crosssections.m
+++ applications/vtools/D3D/fcn/D3D_remove_duplicate_crosssections.m
@@ -4,11 +4,11 @@
 % 
 %Victor Chavarrias (victor.chavarrias@deltares.nl)
 %
-%$Revision: 16571 $
-%$Date: 2020-09-08 14:39:17 +0200 (Tue, 08 Sep 2020) $
-%$Author: chavarri $
-%$Id: D3D_crosssectionlocation.m 16571 2020-09-08 12:39:17Z chavarri $
-%$HeadURL: https://svn.oss.deltares.nl/repos/openearthtools/trunk/matlab/applications/vtools/D3D/input_generation/D3D_crosssectionlocation.m $
+%$Revision$
+%$Date$
+%$Author$
+%$Id$
+%$HeadURL$
 %
 %finds locations with shared cross-section definitions and duplicates the
 %cross-section definition. 
