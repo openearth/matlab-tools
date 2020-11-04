3142dfb9adc9e909d1e9caf58f9e0b446859ec18
mourits <mourits@localhost>
2020-11-04 17:18:50 +0000

    Unintended commit
    svn path=/trunk/matlab/; revision=16766
    
    
    Former-commit-id: 8a1acc106fd1b98e21bac9c0f85fd4bf78b26fb6

--- Toolbox/general/oet_template/oetlogourl.m
+++
@@ -1,8 +0,0 @@
-function im = oetlogourl
-%oetlogourl  returns image filename of logo
-%
-%See also: oetsettings, oetroot
-
-im = 'http://publicwiki.deltares.nl/download/attachments/2392772/OpenEarth-logo-blurred-white-background.png?';
-
-
