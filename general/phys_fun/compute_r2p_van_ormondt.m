c4ea35fe6eb50e7874f9d815e17bdee885e9c580
ormondt <ormondt@localhost>
2020-11-09 14:59:27 +0000

    runup function compute_tm01_ig added
    svn path=/trunk/matlab/; revision=16777
    
    
    Former-commit-id: 5900fda1a026ae4ab4b87e700002c432c88d3f48

--- general/phys_fun/compute_r2p_van_ormondt.m
+++ general/phys_fun/compute_r2p_van_ormondt.m
@@ -207,3 +207,20 @@ fh=1/gambr;
 surfslope=(fh*hm0)./(fh*hm0./sl1).^1.5;
 ksis=surfslope./(sqrt(hm0./l1));
 fac=exp(-max(beta(1).*dirspread.^beta(2) + beta(3).*ksis.^beta(4),0));
+
+function v=compute_tm01_ig(hm0,tp,sl1,sl2,ztide,phi,drspr)
+%
+beta(1)=4.3231843e+00;
+beta(2)=1.2159635e+00;
+beta(3)=-4.7189959e-01;
+beta(4)=2.1100015e+02;
+beta(5)=0.0000000e+00;
+%
+[c cg nnn k] = wavevelocity(tp, 1000);
+l1=2*pi./k;
+gambr=0.70;
+fh=1/gambr;
+surfslope=(fh*hm0)./(fh*hm0./sl1).^1.5;
+steepness=hm0./l1;
+v=beta(1)+beta(2).*surfslope.^beta(3).*steepness.^beta(4);
+v=v.*tp;
