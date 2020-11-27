43bfe5f3d3b8c104b8c0d7516d5604ff83902952
ormondt <ormondt@localhost>
2020-11-27 14:29:41 +0000

    sfincs_write_boundary_conditions.m now written with 3 decimal points
    svn path=/trunk/matlab/; revision=16845
    
    
    Former-commit-id: a40a93d62f976eadad9b715be64c8c017c429147

--- applications/DelftDashBoard/general/sfincs/fileio/sfincs_write_boundary_conditions.m
+++ applications/DelftDashBoard/general/sfincs/fileio/sfincs_write_boundary_conditions.m
@@ -1,7 +1,7 @@
 function sfincs_write_boundary_conditions(filename,t,v)
 
 np=size(v,2);
-fmt=['%10.1f' repmat('%8.2f',[1 np]) '\n'];
+fmt=['%10.1f' repmat('%8.3f',[1 np]) '\n'];
 
 fid=fopen(filename,'wt');
 for it=1:length(t)
