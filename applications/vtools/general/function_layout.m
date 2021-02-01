1f59437e483a4d92c757748348f8a5ab5fc2a592
chavarri <chavarri@localhost>
2021-02-01 12:23:23 +0000

    svn path=/trunk/matlab/; revision=17029
    Former-commit-id: a77227bd647e67d2c1b9124b28b544fa1ec9b942

--- applications/vtools/general/function_layout.m
+++ applications/vtools/general/function_layout.m
@@ -4,11 +4,11 @@
 % 
 %Victor Chavarrias (victor.chavarrias@deltares.nl)
 %
-%$Revision: 16571 $
-%$Date: 2020-09-08 14:39:17 +0200 (Tue, 08 Sep 2020) $
-%$Author: chavarri $
-%$Id: modify_defstr_4.m 16571 2020-09-08 12:39:17Z chavarri $
-%$HeadURL: https://svn.oss.deltares.nl/repos/openearthtools/trunk/matlab/applications/vtools/rmm_setup/modify_defstr_4.m $
+%$Revision$
+%$Date$
+%$Author$
+%$Id$
+%$HeadURL$
 %
 %This does A and B
 %
