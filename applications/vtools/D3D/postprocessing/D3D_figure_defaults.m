b6d37e4b0bf4148c7a10219452a6f2b4ab8d8375
chavarri <chavarri@localhost>
2021-02-19 05:31:44 +0000

    svn path=/trunk/matlab/; revision=17078
    Former-commit-id: 8429aa3e1083ab3e40377621781476bd88a24ec9

--- applications/vtools/D3D/postprocessing/D3D_figure_defaults.m
+++ applications/vtools/D3D/postprocessing/D3D_figure_defaults.m
@@ -4,11 +4,11 @@
 % 
 %Victor Chavarrias (victor.chavarrias@deltares.nl)
 %
-%$Revision: 16571 $
-%$Date: 2020-09-08 14:39:17 +0200 (Tue, 08 Sep 2020) $
-%$Author: chavarri $
-%$Id: D3D_figure_2D.m 16571 2020-09-08 12:39:17Z chavarri $
-%$HeadURL: https://svn.oss.deltares.nl/repos/openearthtools/trunk/matlab/applications/vtools/D3D/postprocessing/D3D_figure_2D.m $
+%$Revision$
+%$Date$
+%$Author$
+%$Id$
+%$HeadURL$
 %
 %plot of 1D volume fraction 2
 
--- applications/vtools/rmm_setup/add_bc_2_mat.m
+++ applications/vtools/rmm_setup/add_bc_2_mat.m
@@ -4,11 +4,11 @@
 % 
 %Victor Chavarrias (victor.chavarrias@deltares.nl)
 %
-%$Revision: 16571 $
-%$Date: 2020-09-08 14:39:17 +0200 (Tue, 08 Sep 2020) $
-%$Author: chavarri $
-%$Id: modify_rtctimeseries.m 16571 2020-09-08 12:39:17Z chavarri $
-%$HeadURL: https://svn.oss.deltares.nl/repos/openearthtools/trunk/matlab/applications/vtools/rmm_setup/modify_rtctimeseries.m $
+%$Revision$
+%$Date$
+%$Author$
+%$Id$
+%$HeadURL$
 %
 
 function add_bc_2_mat(path_mat,path_dat)
