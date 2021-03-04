88d8e013445b21f187d8756912de8a2ec66cedb7
chavarri <chavarri@localhost>
2021-03-04 07:16:52 +0000

    svn path=/trunk/matlab/; revision=17107
    Former-commit-id: 74fa6aeb867ffda8f407296a9827c7561e5f0345

--- applications/vtools/general/interpolate_xy.m
+++ applications/vtools/general/interpolate_xy.m
@@ -4,11 +4,11 @@
 % 
 %Victor Chavarrias (victor.chavarrias@deltares.nl)
 %
-%$Revision: 16571 $
-%$Date: 2020-09-08 14:39:17 +0200 (Tue, 08 Sep 2020) $
-%$Author: chavarri $
-%$Id: cm2ax.m 16571 2020-09-08 12:39:17Z chavarri $
-%$HeadURL: https://svn.oss.deltares.nl/repos/openearthtools/trunk/matlab/applications/vtools/general/cm2ax.m $
+%$Revision$
+%$Date$
+%$Author$
+%$Id$
+%$HeadURL$
 %
 %interpolates based on the closest data points of semi-structured
 %data.
