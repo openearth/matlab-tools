edc9d9d2292f5deb8c7f72037e43c85375874921
leijnse <leijnse@localhost>
2021-03-18 14:16:06 +0000

    TCWiSE is not meant to be added to OET here! In case you want have access to the closed developers repository contact tim.leijnse@deltares.nl Thanks
    svn path=/trunk/matlab/; revision=17118
    
    
    Former-commit-id: 46ba144c8e1ae94c9b9af1f2ac2fdef84a404855

--- 01_scripts/additional/WAFO/wstats/wgumbafit.m
+++
@@ -1,5 +0,0 @@
-function l=wgumbafit(a,data)
-%WGUMBAFIT Is an internal routine for wgumbfit
-%
-
-l=a-mean(data)+mean(data.*exp(-data/a))/mean(exp(-data/a));
--- 01_scripts/tcwise_other/randomsample.m
+++
@@ -1,4 +0,0 @@
-function [y,ix]=randomsample(x,k)
-n=length(x);
-ix=floor(n*rand(1,k))+1;
-y=x(ix);
