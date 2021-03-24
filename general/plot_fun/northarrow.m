cd5fb70f100246b0e2b75edd8893b12fd41e6b7c
l.w.m.roest.x <l.w.m.roest.x@localhost>
2021-03-24 09:39:12 +0000

    axisExponent.m Easy way to set the power of an axis. errorshade.m Indicate uncertainty area around a curve as semi-transparent patch. northarrow.m Plot a north arrow on a map, small update.
    svn path=/trunk/matlab/; revision=17123
    
    
    Former-commit-id: 7bdbd5df3c6996d04718a68d06aa32a3a06eb6c8

--- general/plot_fun/northarrow.m
+++ general/plot_fun/northarrow.m
@@ -1,10 +1,11 @@
 function [x_ds3,y_ds3]=northarrow(d,x0,y0,theta0,varargin)
 %NORTHARROW Plots a 'North arrow' for a map on axes.
 %
-%   More detailed description goes here.
+%   Puts an arrow indicating the North on axes, using patch. Works best on
+%   cartesian grids (eg. RD).
 %
 %   Syntax:
-%   [x_ds3,y_ds3]=nemo_northarrow(d,x0,y0,theta0,c)
+%   [x_ds3,y_ds3]=northarrow(d,x0,y0,theta0,c)
 %
 %   Input: For <keyword,value> pairs call Untitled() without arguments.
 %       d: diameter in axis coordinates
@@ -12,6 +13,7 @@ function [x_ds3,y_ds3]=northarrow(d,x0,y0,theta0,varargin)
 %       y0: y-coordinate centre
 %       theta0: orientation of the arrow in current axis (0=up, 90=right);
 %       varargin:
+%           ax: axis to plot on
 %           c: color of arrow (string or triplet)
 %           fontsize (font size of N)
 %       OR
@@ -23,16 +25,18 @@ function [x_ds3,y_ds3]=northarrow(d,x0,y0,theta0,varargin)
 %       y_ds3= arrow y-coordinates
 %
 %   Example
+%       figure;
 %       x0=600;      
 %       y0=400;
 %       theta0 =-48;
 %       d=100;
 %       c='k';
-%       northarrow(d,x0,y0,theta0,c);
+%       northarrow(d,x0,y0,theta0,'c',c);
+%       figure;
 %       northarrow('auto');
 %
-%   See also:
-%
+%   See also: plot, patch, text
+
 % Bart Roest 2017
 
 %% Copyright notice
@@ -76,6 +80,7 @@ function [x_ds3,y_ds3]=northarrow(d,x0,y0,theta0,varargin)
 % $Keywords: $
 
 %%
+OPT.ax=gca;
 OPT.c='k';
 OPT.fontsize=14;
 % return defaults (aka introspection)
@@ -87,9 +92,9 @@ end
 OPT = setproperty(OPT, varargin);
 %% code
 if nargin==1 %'auto' Automatically scale and place.
-    d=800;
     xl=get(gca,'XLim');
     yl=get(gca,'YLim');
+    d=min([abs(diff(xl)),abs(diff(yl))]).*0.1;
     x0=xl(2)-0.7*d;
     y0=yl(2)-0.8*d;
     theta0=48;
@@ -106,11 +111,11 @@ end
     x_ds3=x_ds2+x0;
     y_ds3=y_ds2+y0;
 
-    patch('XData',x_ds3([1:3,5]),'YData',y_ds3([1:3,5]),'FaceColor','k','EdgeColor',OPT.c);
-    patch('XData',x_ds3([3:6])  ,'YData',y_ds3([3:6])  ,'FaceColor','w','EdgeColor',OPT.c);
+    patch(OPT.ax,'XData',x_ds3([1:3,5]),'YData',y_ds3([1:3,5]),'FaceColor','k','EdgeColor',OPT.c);
+    patch(OPT.ax,'XData',x_ds3([3:6])  ,'YData',y_ds3([3:6])  ,'FaceColor','w','EdgeColor',OPT.c);
 
     %plot(x_ds3,y_ds3,c,'linewidth',2);
-    t1=text(x0,y0-0.5*d,'N');
+    t1=text(OPT.ax,x0,y0-0.5*d,'N');
     set(t1,'Fontsize',OPT.fontsize,'color',OPT.c);
 end
 %EOF
