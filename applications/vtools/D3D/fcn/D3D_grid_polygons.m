c31f370caf79cd425b982060844c025574f454d1
chavarri <chavarri@localhost>
2021-04-08 10:10:29 +0000

    svn path=/trunk/matlab/; revision=17168
    Former-commit-id: 1c1cdf35da3dce0f60e1d2a81e15039171d774c2

--- applications/vtools/D3D/fcn/D3D_grid_polygons.m
+++ applications/vtools/D3D/fcn/D3D_grid_polygons.m
@@ -16,12 +16,14 @@
 
 function polygons=D3D_grid_polygons(ncFile)
 
-gridInfo = EHY_getGridInfo(ncFile,'face_nodes_xy');
+gridInfo=EHY_getGridInfo(ncFile,'face_nodes_xy');
 
-for iC = 1:size(gridInfo.face_nodes_x,2)
-    x = gridInfo.face_nodes_x(:,iC);
-    y = gridInfo.face_nodes_y(:,iC);
-    nonan = ~isnan(x);
+nC=size(gridInfo.face_nodes_x,2);
+polygons=cell(nC,1);
+for iC=1:nC
+    x=gridInfo.face_nodes_x(:,iC);
+    y=gridInfo.face_nodes_y(:,iC);
+    nonan=~isnan(x);
     
     polygons{iC,1} = [x(nonan) y(nonan)];
 end
