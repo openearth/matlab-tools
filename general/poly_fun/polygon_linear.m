302f206c0fffcfcf4362f00a75ad83a22e017476
nederhof <nederhof@localhost>
2021-07-08 17:08:44 +0000

    svn path=/trunk/matlab/; revision=17395
    Former-commit-id: f8ca4bd16df757e379e751e39c31beac4cf2cda7

--- general/poly_fun/polygon_linear.m
+++ general/poly_fun/polygon_linear.m
@@ -1,9 +1,8 @@
-function [Xnew,Ynew ] = polygon_linear(X,Y,distancereq)
+function [Xnew,Ynew] = polygon_linear(X,Y,distancereq)
 % Simple function to refine polylines
 Xnew = X(1); Ynew = Y(1);
 for ii = 1:length(X)-1
     distance(ii) = ((X(ii+1) - X(ii)).^2 + (Y(ii+1) - Y(ii)).^2).^0.5;
-    
     if distance(ii) > 0
         if distance(ii) < distancereq
             Xnew = [Xnew X(ii)];
@@ -17,7 +16,6 @@ for ii = 1:length(X)-1
             Ynew = [Ynew Ynews(2:end)];
         end
     else
-        disp(['same point']);
     end
 end
 end
