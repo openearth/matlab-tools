fe515c12cbed4cc772ab696712b5e63e2c230aa1
nederhof <nederhof@localhost>
2021-07-08 14:23:42 +0000

    svn path=/trunk/matlab/; revision=17394
    Former-commit-id: 214d43f611396f95ae69353c5d6c3c263816015f

--- applications/DelftDashBoard/general/sfincs/fileio/sfincs_write_uniform_wind.m
+++ applications/DelftDashBoard/general/sfincs/fileio/sfincs_write_uniform_wind.m
@@ -5,10 +5,7 @@ function sfincs_write_uniform_wind(filename,vt,vmag,vdir)
 % t0 vmag0 vdir0 
 % t1 vmag1 vdir1
 %%%%%
-
 fid = fopen(filename,'wt');
-
 A = [vt; vmag; vdir]; 
 fprintf(fid, '%f %f %f\n', A);
-
-fclose(fid)
+fclose(fid);
