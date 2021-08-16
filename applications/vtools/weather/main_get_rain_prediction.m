03846b9c03dc12e196ca8d89daab498311a4f376
chavarri <chavarri@localhost>
2021-08-16 05:02:15 +0000

    svn path=/trunk/matlab/; revision=17457
    Former-commit-id: 3a5155da64282d0de21507ba313ac94911bc4187

--- applications/vtools/weather/main_get_rain_prediction.m
+++ applications/vtools/weather/main_get_rain_prediction.m
@@ -30,25 +30,28 @@ T_data=3600;
 %% CALC
 
 t_last=datetime(2000,01,01);
-fpath_html=fullfile(fdir_rain,'file.html');
+
 fname_rain=fullfile(fdir_rain,'rain.mat');
-errstatus_dw=0;
+errstatus_dw=1;
 errstatus_rd=1;
 rain_one=struct();
 while 1
     t_now=datetime('now');
     if seconds(t_now-t_last)>T_data
         while errstatus_rd
-        errstatus_dw=download_web(url_br,fpath_html);
             while errstatus_dw
                 pause(15)
+                messageOut(NaN,'Trying to download.')
+                t_now=datetime('now');
+                fpath_html=fullfile(fdir_rain,sprintf('file_%f.html',datenum(t_now)));
                 errstatus_dw=download_web(url_br,fpath_html);
             end
-        [rain_one,errstatus_rd,errmessage]=read_web_buienradar(fpath_html);
+            messageOut(NaN,'Trying to read.')
+            [rain_one,errstatus_rd,errmessage]=read_web_buienradar(fpath_html);
         end
 %         fclose all; %this should not be necessary, but for some reason sometimes the file cannot be deleted
-        pause(5); %maybe this helps
-        delete(fpath_html);
+%         pause(5); %maybe this helps
+%         delete(fpath_html);
         rain_one.tim_anl=datetime('now');
         if exist(fname_rain,'file')==2
             load(fname_rain,'rain');
@@ -59,7 +62,7 @@ while 1
         end
         save(fname_rain,'rain')
         t_last=t_now;
-        messageOut(NaN,'Read data.')
+        messageOut(NaN,'Data read.')
     else
         messageOut(NaN,'In pause.')
         pause(T_data/6);
