171ca229dbc3633806c4652a9a316792a480a347
chavarri <chavarri@localhost>
2021-09-10 14:31:14 +0000

    svn path=/trunk/matlab/; revision=17483
    Former-commit-id: b52c40298146b4b1c558b29df066cf369a4e1686

--- applications/vtools/general/datenum_tzone.m
+++ applications/vtools/general/datenum_tzone.m
@@ -15,16 +15,16 @@
 function tim_dnum=datenum_tzone(tim_dtime)
 
 tzone=tim_dtime.TimeZone;
-tok=regexp(tzone,'(?\w)(\d{2})(\d{2})','tokens');
+tok=regexp(tzone,'([+-])(\d{2}):(\d{2})','tokens');
 if isempty(tok)
     error('improve the string')
 end
-op=tok{1,1};
-hs=str2double(tok{1,2});
-ms=str2double(tok{1,3});
+op=tok{1,1}{1,1};
+hs=str2double(tok{1,1}{1,2});
+ms=str2double(tok{1,1}{1,3});
 switch op
     case '+'
-        tim_dnum=datenum(tim)+(hs/24+ms/(24/60));
+        tim_dnum=datenum(tim_dtime)+(hs/24+ms/(24/60));
     case '-'
-        tim_dnum=datenum(tim)-(hs/24+ms/(24/60));
+        tim_dnum=datenum(tim_dtime)-(hs/24+ms/(24/60));
 end
