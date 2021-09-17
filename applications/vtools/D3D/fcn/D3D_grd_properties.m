13f273d519276d020f498af0c69d0e577c1586e3
ottevan <ottevan@localhost>
2021-09-17 08:44:05 +0000

    svn path=/trunk/matlab/; revision=17486
    Former-commit-id: 0c25bb1a68806c7558ea2df7d0466e26158d28fb

--- applications/vtools/D3D/fcn/D3D_grd_properties.m
+++ applications/vtools/D3D/fcn/D3D_grd_properties.m
@@ -7,10 +7,12 @@ clear
 %5011 = n cell edges
 %2426 = n cells
 
-path_grd='p:\11205950-001-pumarejo\05_data\05_grd\UGg04_03_full_net.nc';
+path_grd='';
 nci=ncinfo(path_grd);
-edg_node=ncread(path_grd,'mesh2d_edge_nodes'); %{'Start and end nodes of mesh edges'}
+edge_node=ncread(path_grd,'mesh2d_edge_nodes'); %{'Start and end nodes of mesh edges'}
+edge_type=ncread(path_grd,'mesh2d_edge_type');
 node_x=ncread(path_grd,'mesh2d_node_x'); 
+node_y=ncread(path_grd,'mesh2d_node_y'); 
 edge_x=ncread(path_grd,'mesh2d_edge_x'); 
 edge_y=ncread(path_grd,'mesh2d_edge_y'); 
 face_x=ncread(path_grd,'mesh2d_face_x'); 
@@ -23,50 +25,37 @@ NetLinkContour_y=ncread(path_grd,'mesh2d_NetLinkContour_y');
 % The orthogonality is defined as the cosine of the angle ? between a flowlink and a netlink.
 % The smoothness of a mesh is defined as the ratio of the areas of two adjacent cells
 %%
-p0=[node_x(edg_node(1,:)),node_y(edg_node(1,:))];
-pf=[node_x(edg_node(2,:)),node_y(edg_node(2,:))];
+p0=[node_x(edge_node(1,:)),node_y(edge_node(1,:))];
+pf=[node_x(edge_node(2,:)),node_y(edge_node(2,:))];
 
 %%
-% CosTheta = max(min(dot(u,v)/(norm(u)*norm(v)),1),-1);
+internal_edges = find(edge_type==2); 
+for e = internal_edges(:).'; 
+    [ii,jj] = find(net_elem_link == e);
+    if length(jj) == 2; 
+        A.x1 = face_x(jj(1));
+        A.x2 = face_x(jj(2));
+        A.y1 = face_y(jj(1));
+        A.y2 = face_y(jj(2));
+        B.x1 = node_x(edge_node(1,e)); 
+        B.x2 = node_x(edge_node(2,e)); 
+        B.y1 = node_y(edge_node(1,e)); 
+        B.y2 = node_y(edge_node(2,e)); 
+        C = dflowfm.intersect_lines(A, B); 
+        v=[A.x2-A.x1;A.y2-A.y1;];
+        u=[B.x2-B.x1;B.y2-B.y1;];
+        CosTheta(e) = dot(u,v)/(norm(u)*norm(v));
+        Smoothness(e) = max(C.alpha/(1-C.alpha),(1-C.alpha)/C.alpha); 
+        edge_mid_x(e) = C.x; 
+        edge_mid_y(e) = C.y; 
+    else
+        CosTheta(e) = NaN; 
+        Smoothness(e) = NaN; 
+        edge_mid_x(e) = NaN; 
+        edge_mid_y(e) = NaN; 
+    end 
+end
 
 %%
-figure
-hold on
-plot([p0(:,1),pf(:,1)]',[p0(:,2),pf(:,2)]')
+scatter(edge_mid_x,edge_mid_y,100, abs(Smoothness),'.')
 axis equal
-%%
-x1=NetLinkContour_x([1,3],:); %initial and final x of v
-x2=NetLinkContour_x([2,4],:); %initial and final x of u
-y1=NetLinkContour_y([1,3],:);
-y2=NetLinkContour_y([2,4],:);
-
-v=[x1(2,:)-x1(1,:);y1(2,:)-y1(1,:)];
-u=[x2(2,:)-x2(1,:);y2(2,:)-y2(1,:)];
-
-CosTheta = max(min(dot(u,v)/(norm(u)*norm(v)),1),-1);
-CosTheta = dot(u,v)/(norm(u)*norm(v));
-
-%%
-close all
-figure
-hold on
-plot(NetLinkContour_x(:,1),NetLinkContour_y(:,1))
-scatter(face_x,face_y,10,'g','filled')
-% plot(NetLinkContour_x([1,2],1),NetLinkContour_y([1,2],1),'b')
-% plot(NetLinkContour_x([3,4],1),NetLinkContour_y([3,4],1),'r')
-scatter(edge_x,edge_y,10,'b','filled')
-scatter(node_x,node_y,10,'r','filled')
-edg_node
-axis equal
-%%
-figure
-hold on
-% plot([x1,x2],[y1,y2])
-plot(x1,y1,'r');
-plot(x2,y2,'b');
-%%
-figure
-scatter(edge_x,edge_y,10,CosTheta,'filled')
-axis equal
-colorbar
-
