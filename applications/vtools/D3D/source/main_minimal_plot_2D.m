a419741474c24d4a713bff7bd89fb0f1ae2dcafc
chavarri <chavarri@localhost>
2021-11-30 11:06:26 +0000

    svn path=/trunk/matlab/; revision=17615
    Former-commit-id: 64e5ff8253bc23b701cec34792482d891f4e910f

--- applications/vtools/D3D/source/main_minimal_plot_2D.m
+++ applications/vtools/D3D/source/main_minimal_plot_2D.m
@@ -1,11 +1,11 @@
 % 
 %Victor Chavarrias (victor.chavarrias@deltares.nl)
 %
-%$Revision: 17208 $
-%$Date: 2021-04-23 08:52:13 +0200 (Fri, 23 Apr 2021) $
-%$Author: chavarri $
-%$Id: fig_mean_diff.m 17208 2021-04-23 06:52:13Z chavarri $
-%$HeadURL: https://svn.oss.deltares.nl/repos/openearthtools/trunk/matlab/applications/vtools/q_analysis/fig_mean_diff.m $
+%$Revision$
+%$Date$
+%$Author$
+%$Id$
+%$HeadURL$
 %
 %Minimal example to plot 2D results with a map in the background. 
 
