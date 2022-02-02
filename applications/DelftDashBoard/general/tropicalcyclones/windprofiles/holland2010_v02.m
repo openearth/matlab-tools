cc435fe7fe3028a4f998771d46e229fe7b32e6e7
ormondt <ormondt@localhost>
2022-02-02 10:39:02 +0000

    Major bug fix in holland2010_v02. Had forgotten to include the exponent for vt in the calculation of bs ...
    svn path=/trunk/matlab/; revision=17707
    
    
    Former-commit-id: 6d7d2eadaeaa45d1dcd857eaaedc03d78478834e

--- applications/DelftDashBoard/general/tropicalcyclones/windprofiles/holland2010_v02.m
+++ applications/DelftDashBoard/general/tropicalcyclones/windprofiles/holland2010_v02.m
@@ -1,6 +1,6 @@
 function [vr,pr]=holland2010_v02(r,vms,pc,pn,rmax,dpdt,lat,vt,xn)
 % Holland et al. (2010) formula to determine wind profile
-% r    = radius (km)
+% r    = radius (km) for output
 % vms  = axi-symmetric max wind speed (m/s)
 % pc   = central pressure (mbar)
 % pn   = ambient pressure (mbar)
@@ -10,17 +10,17 @@ function [vr,pr]=holland2010_v02(r,vms,pc,pn,rmax,dpdt,lat,vt,xn)
 % vt   = forward speed (m/s)
 % xn   = Holland (2010) size exponent (use 0.5 to revert to Holland (1980))
 
-lat  = abs(lat);
-
 %% 0. Reading parameters
 % Calculate Holland B parameter based on Holland (2008)
 % Assume Dvorak method
 dp  = max(pn - pc,1);
-bs  = -4.4 * 10^-5 * dp.^2 + 0.01 * dp + 0.03 * dpdt - 0.014 * lat + 0.15* vt + 1;
-bs  = min(max(bs, 1.0),3);  % numerical limits
+x   = 0.6*(1-dp/215);
+bs  = -4.4 * 10^-5 * dp.^2 + 0.01 * dp + 0.03 * dpdt - 0.014 * abs(lat) + 0.15*vt^x + 1;
+bs  = min(max(bs, 0.5),2);  % numerical limits
 
 %% 2. Determine wind structure
 x=zeros(size(r))+xn;
 x(r<=rmax)=0.5;
-pr=pc+dp*exp(-(rmax./r).^bs);
+b=bs;
+pr=pc+dp*exp(-(rmax./r).^b);
 vr=vms*(((rmax./r).^bs).*exp(1.0 - (rmax./r).^bs)).^x;
