ddd65dddf0f33fe2fdebaeb8de78389ddd83c1d8
chavarri <chavarri@localhost>
2022-03-02 08:37:25 +0000

    svn path=/trunk/matlab/; revision=17804
    Former-commit-id: de07895441f34272a2cca7aabf4b30303d4de19f

--- applications/vtools/D3D/fcn/D3D_find_nm_line.m
+++ applications/vtools/D3D/fcn/D3D_find_nm_line.m
@@ -4,11 +4,11 @@
 % 
 %Victor Chavarrias (victor.chavarrias@deltares.nl)
 %
-%$Revision: 17754 $
-%$Date: 2022-02-11 06:38:51 +0100 (Fri, 11 Feb 2022) $
-%$Author: chavarri $
-%$Id: angle_polyline.m 17754 2022-02-11 05:38:51Z chavarri $
-%$HeadURL: https://svn.oss.deltares.nl/repos/openearthtools/trunk/matlab/applications/vtools/polyline/angle_polyline.m $
+%$Revision$
+%$Date$
+%$Author$
+%$Id$
+%$HeadURL$
 %
 
 function [nodes_xy_mline,node_all]=D3D_find_nm_line(fpath_grd,node_0,node_1,varargin)
--- applications/vtools/polyline/angle_difference.m
+++ applications/vtools/polyline/angle_difference.m
@@ -4,11 +4,11 @@
 % 
 %Victor Chavarrias (victor.chavarrias@deltares.nl)
 %
-%$Revision: 17754 $
-%$Date: 2022-02-11 06:38:51 +0100 (Fri, 11 Feb 2022) $
-%$Author: chavarri $
-%$Id: angle_polyline.m 17754 2022-02-11 05:38:51Z chavarri $
-%$HeadURL: https://svn.oss.deltares.nl/repos/openearthtools/trunk/matlab/applications/vtools/polyline/angle_polyline.m $
+%$Revision$
+%$Date$
+%$Author$
+%$Id$
+%$HeadURL$
 %
 
 function angle_diff=angle_difference(angle_a,angle_b)
