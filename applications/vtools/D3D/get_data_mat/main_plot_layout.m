0183e906012deaaec49bc1b63f7bd927e9fc7143
chavarri <chavarri@localhost>
2022-04-05 11:46:40 +0000

    svn path=/trunk/matlab/; revision=17939
    Former-commit-id: f1e56c7d46907aae24fa0197137b95bbdab62d50

--- applications/vtools/D3D/get_data_mat/main_plot_layout.m
+++ applications/vtools/D3D/get_data_mat/main_plot_layout.m
@@ -1,13 +1,16 @@
-%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%                 VTOOLS                 %%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 
 %Victor Chavarrias (victor.chavarrias@deltares.nl)
 %
 %$Revision: 17937 $
 %$Date: 2022-04-05 13:43:41 +0200 (Tue, 05 Apr 2022) $
 %$Author: chavarri $
-%$Id: main_plot.m 17937 2022-04-05 11:43:41Z chavarri $
-%$HeadURL: https://svn.oss.deltares.nl/repos/openearthtools/trunk/matlab/applications/vtools/D3D/get_data_mat/main_plot.m $
+%$Id: create_mat_map_sal_mass_01.m 17937 2022-04-05 11:43:41Z chavarri $
+%$HeadURL: https://svn.oss.deltares.nl/repos/openearthtools/trunk/matlab/applications/vtools/D3D/get_data_mat/create_mat_map_sal_mass_01.m $
 %
-%Plot results IJsselmeer 3D
+%
 
 %% PREAMBLE
 
