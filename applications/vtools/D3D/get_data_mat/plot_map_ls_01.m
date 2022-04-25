99f3c69c192c99466d85adc7387dada0ea5ce852
chavarri <chavarri@localhost>
2022-04-25 10:19:44 +0000

    svn path=/trunk/matlab/; revision=17983
    Former-commit-id: 4317b1f7310d9d9341a6d0f661b0b370ad3027df

--- applications/vtools/D3D/get_data_mat/plot_map_ls_01.m
+++ applications/vtools/D3D/get_data_mat/plot_map_ls_01.m
@@ -12,7 +12,7 @@
 %
 %
 
-function plot_map_ls_01(fid_log,in_plot_loc,simdef)
+function plot_map_ls_01(fid_log,flg_loc,simdef)
 
 tag=flg_loc.tag;
 
@@ -32,17 +32,17 @@ ret=gdm_do_mat(fid_log,flg_loc,tag); if ret; return; end
 load(simdef.file.mat.map_ls_01,'data_map_ls_01');
 load(simdef.file.mat.map_ls_01_tim,'time_dnum');
 
-nclim=size(in_plot_loc.clims,1);
+nclim=size(flg_loc.clims,1);
 npli=numel(data_map_ls_01);
 nt=size(data_map_ls_01(1).sal,1);
 
 %figures
-in_p=in_plot_loc; %attention with unexpected input
+in_p=flg_loc; %attention with unexpected input
 in_p.fig_print=1; %0=NO; 1=png; 2=fig; 3=eps; 4=jpg; (accepts vector)
 in_p.fig_visible=0;
 
-if isfield(in_plot_loc,'fig_overwrite')
-    in_p.fig_overwrite=in_plot_loc.fig_overwrite;
+if isfield(flg_loc,'fig_overwrite')
+    in_p.fig_overwrite=flg_loc.fig_overwrite;
 end
 
 switch in_p.fig_print
@@ -55,7 +55,7 @@ end
 fpath_file=cell(nt,nclim,npli);
 for kpli=1:npli
     in_p.xlims=[0,data_map_ls_01(kpli).Scor(end)];
-    in_p.ylims=in_plot_loc.ylims(kpli,:); %move to input
+    in_p.ylims=flg_loc.ylims(kpli,:); %move to input
     in_p.data_ls=data_map_ls_01(kpli);
     for kt=1:nt
         for kclim=1:nclim
@@ -66,7 +66,7 @@ for kpli=1:npli
             in_p.kt=kt;
             in_p.tim=time_dnum(kt);
 
-            clims=in_plot_loc.clims(kclim,:);
+            clims=flg_loc.clims(kclim,:);
             if all(isnan(clims)==[0,1]) %[0,NaN]
                 error('do')
 %                 in_p.clims=[clims(1),max_tot];
@@ -83,10 +83,10 @@ end %kpli
 
 dt_aux=diff(time_dnum);
 dt=dt_aux(1)*24*3600; %[s] we have 1 frame every <dt> seconds 
-rat=in_plot_loc.rat; %[s] we want <rat> model seconds in each movie second
+rat=flg_loc.rat; %[s] we want <rat> model seconds in each movie second
 for kpli=1:npli
 for kclim=1:nclim
-   make_video(fpath_file(:,kclim,kpli),'frame_rate',1/dt*rat);
+   make_video(fpath_file(:,kclim,kpli),'frame_rate',1/dt*rat,'overwrite',flg_loc.fig_overwrite);
 end
 end
 
