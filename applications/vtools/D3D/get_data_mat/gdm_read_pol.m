b0b3dd852a88b7672d1d2cfb0f72740c53e21ce1
chavarri <chavarri@localhost>
2022-06-09 10:37:26 +0000

    svn path=/trunk/matlab/; revision=18126
    Former-commit-id: c19e91a5dc3e76de14aea2af8ffc867bed500f61

--- applications/vtools/D3D/get_data_mat/gdm_read_pol.m
+++ applications/vtools/D3D/get_data_mat/gdm_read_pol.m
@@ -14,5 +14,6 @@
 
 function [pol,pol_name]=gdm_read_pol(fpath_pol)
 pol=D3D_io_input('read',fpath_pol);
-pol_name=strrep(pol.name{1,1},' ','');
+% pol_name=strrep(pol.name{1,1},' ','');
+pol_name=strrep(pol(1).name,' ','');
 end %funtion
