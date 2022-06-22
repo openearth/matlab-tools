b92d7f17b9068ca5e77b5dc724b5667ed8829b1d
ormondt <ormondt@localhost>
2022-06-22 18:11:37 +0000

    ddb model maker: quadtree functionality added
    svn path=/trunk/matlab/; revision=18195
    
    
    Former-commit-id: df67b50aa48ff180b8deb5e616e2ee3d59e3976f

--- applications/DelftDashBoard/models/sfincs/main/ddb_plotsfincs.m
+++ applications/DelftDashBoard/models/sfincs/main/ddb_plotsfincs.m
@@ -46,7 +46,9 @@ for id=id1:id2
             handles=ddb_sfincs_plot_wave_boundary_points(handles,option,'domain',id,'visible',1);
             handles=ddb_sfincs_plot_mask(handles,option,'domain',id,'visible',1);
             handles=ddb_sfincs_plot_coastline_points(handles,option,'domain',id,'visible',1);
-            handles=ddb_sfincs_plot_buq_blocks(handles,option,'domain',id,'visible',1);
+            if ~isempty(handles.model.sfincs.domain(id).buq)
+                handles=ddb_sfincs_plot_buq_blocks(handles,option,'domain',id,'visible',1);
+            end
             
         case{'delete'}
             
@@ -56,7 +58,9 @@ for id=id1:id2
             handles=ddb_sfincs_plot_wave_boundary_points(handles,option,'domain',id);
             handles=ddb_sfincs_plot_mask(handles,option,'domain',id);
             handles=ddb_sfincs_plot_coastline_points(handles,option,'domain',id);
-            handles=ddb_sfincs_plot_buq_blocks(handles,option,'domain',id);
+            if ~isempty(handles.model.sfincs.domain(id).buq)
+                handles=ddb_sfincs_plot_buq_blocks(handles,option,'domain',id);
+            end
 
             try
                 delete(handles.model.sfincs.boundaryspline.handle);                
@@ -141,8 +145,9 @@ for id=id1:id2
 %             else
 %                 ivis=0;
 %             end
-            
-            handles=ddb_sfincs_plot_buq_blocks(handles,option,'domain',id,'visible',1);
+            if ~isempty(handles.model.sfincs.domain(id).buq)
+                handles=ddb_sfincs_plot_buq_blocks(handles,option,'domain',id,'visible',1);
+            end
             
             
     end
