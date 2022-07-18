3c234f10775fc7aa0510bb6a2173570975727498
kaaij <kaaij@localhost>
2022-07-18 15:33:45 +0000

    svn path=/trunk/matlab/; revision=18247
    Former-commit-id: 7b9d3e7db4151d17bd9a56dbfc1b43f84e43a567

---
+++ applications/EHY_tools/EHY_sleep.m
@@ -0,0 +1,26 @@
+function [varargout] = EHY_sleep (varargin)
+
+%% initialise
+fileName      = mfilename('fullpath');
+[dirName,~,~] = fileparts(fileName);
+
+%% Irfanview installed?
+if ~exist('c:\Program Files\IrfanView\i_view64.exe')
+    txt = sprintf(['This program requires irfanview for displaying images (to avoid using the matlab image toolbox) \n' ...
+             'Please install from: https://www.fosshub.com/IrfanView.html']);
+    display(txt);
+end
+
+%% Check!
+nr_times = input('How many times do you feed your baby (natural or by bottle) a day? ');
+
+%% Assign
+if nr_times >=10
+    fileName = [dirName filesep 'sb.gif'];
+else
+    fileName = [dirName filesep 'cb.gif'];
+end
+
+%% Display
+system (['"c:\Program Files\IrfanView\i_view64.exe" /file=' fileName]);
+
