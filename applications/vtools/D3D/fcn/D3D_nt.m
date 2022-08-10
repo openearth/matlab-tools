f9c5ebbe7a560216ad76669a359bfec532592e25
chavarri <chavarri@localhost>
2022-08-10 09:18:06 +0000

    svn path=/trunk/matlab/; revision=18290
    Former-commit-id: 97587c156197d50b3d9989d722da4852b648e6be

--- applications/vtools/D3D/fcn/D3D_nt.m
+++ applications/vtools/D3D/fcn/D3D_nt.m
@@ -12,15 +12,47 @@
 %
 %
 
-function nt=D3D_nt(fpath_res)
+function nt=D3D_nt(fpath_res,varargin)
 
-[~,~,ext]=fileparts(fpath_res);
-switch ext
-    case '.nc'
-        nt=NC_nt(fpath_res);
-    case '.dat'
-        NFStruct=vs_use(fpath_res,'quiet');
-        ITMAPC=vs_let(NFStruct,'map-info-series','ITMAPC','quiet'); %results time vector
-        nt=numel(ITMAPC); %there must be a better way... ask Bert!
-        
-end 
+%% PARSE
+
+parin=inputParser;
+
+addOptional(parin,'res_type','map');
+
+parse(parin,varargin{:});
+
+res_type=parin.Results.res_type;
+
+%% 
+
+if isfolder(fpath_res)
+        fdir_output=fullfile(fpath_res,'output');
+        dire=dir(fdir_output);
+        nf=numel(dire)-2-1; %already the number of the files, which start at 0
+        nt=0;
+        for kf=0:1:nf
+            fdir_loc=fullfile(fdir_output,num2str(kf));
+            simdef.D3D.dire_sim=fdir_loc;
+            simdef=D3D_simpath(simdef);
+            fpath_nc=simdef.file.(res_type);
+            nt=nt+NC_nt(fpath_nc);
+                        
+%             messageOut(NaN,sprintf('Joined time %4.2f %%',kf/nf*100));
+        end
+else
+    
+    [~,~,ext]=fileparts(fpath_res);
+    switch ext
+        case '.nc'
+            nt=NC_nt(fpath_res);
+        case '.dat'
+            NFStruct=vs_use(fpath_res,'quiet');
+            ITMAPC=vs_let(NFStruct,'map-info-series','ITMAPC','quiet'); %results time vector
+            nt=numel(ITMAPC); %there must be a better way... ask Bert!
+
+    end %ext
+    
+end %is
+
+end %function
