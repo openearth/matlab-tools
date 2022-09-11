a2b45472d6921dcbbf7832998417c3cfba0691d4
kaaij <kaaij@localhost>
2022-09-11 09:23:06 +0000

    svn path=/trunk/matlab/; revision=18359
    Former-commit-id: fbf9caa153e0f6e92b615e58ffbeb6149757c53f

--- applications/tide/lpGodin.m
+++ applications/tide/lpGodin.m
@@ -1,4 +1,8 @@
 function data = lpGodin(data,times,varargin)
 
 %% Bridge to call the oetsettings function godin_filter
-data = godin_filter(data,times,'full',true)';
+if length(data(isnan(data))) == length(times)
+    data(1:length(times)) = NaN;
+else
+    data = godin_filter(data,times,'full',true)';
+end
