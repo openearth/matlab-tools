43bc3bb0a01ee66774b8842c79da0ab605f28839
ormondt <ormondt@localhost>
2022-10-12 16:49:56 +0000

    calcrefrac.m update (now allows vector input)
    svn path=/trunk/matlab/; revision=18442
    
    
    Former-commit-id: 976888f760f36e3621b5edb3ef525c0fb69c8ede

--- applications/waves/calcrefrac.m
+++ applications/waves/calcrefrac.m
@@ -18,19 +18,29 @@ function [Kr,alfa]=calcrefrac(d,T,alfa0)
 
 % Based on script by James Hu
 
+
 [L,e,c] = wavelength(T,d,0,0,0,1e-6);
 alfa0=alfa0*pi/180;
-k=2*pi/L;
-if alfa0>=0 && alfa0<=pi/2
-    alfa=asin(tanh(k*d)*sin(alfa0));
-    Kr=sqrt(cos(alfa0)/cos(alfa));
-elseif alfa0>pi/2 && alfa0<pi
-    alfa0=pi-alfa0;
-    alfa=asin(tanh(k*d)*sin(alfa0));
-    Kr=sqrt(cos(alfa0)/cos(alfa));
-    alfa=pi-alfa;
-else
-    Kr=0;
-    alfa=0;
+k=2*pi./L;
+for ii=1:length(alfa0)
+    if length(d)>1
+        dind=ii;
+    else
+        dind=1;
+    end
+    if alfa0(ii)>=0 && alfa0(ii)<=pi/2
+%         alfa(ii)=asin(tanh(k(ii)*d(dind)))*sin(alfa0(ii));
+        alfa(ii)=asin(tanh(k(ii)*d(dind))*sin(alfa0(ii)));
+        Kr(ii)=sqrt(cos(alfa0(ii))/cos(alfa(ii)));
+    elseif alfa0(ii)>pi/2 && alfa0(ii)<pi
+        alfa0(ii)=pi-alfa0(ii);
+%         alfa(ii)=asin(tanh(k(ii)*d(dind)))*sin(alfa0(ii));
+        alfa(ii)=asin(tanh(k(ii)*d(dind))*sin(alfa0(ii)));
+        Kr(ii)=sqrt(cos(alfa0(ii))/cos(alfa(ii)));
+        alfa(ii)=pi-alfa(ii);
+    else
+        Kr(ii)=0;
+        alfa(ii)=0;
+    end
+    alfa(ii)=alfa(ii)*180/pi;
 end
-alfa=alfa*180/pi;
