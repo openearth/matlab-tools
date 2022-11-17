51be9481a15ff4946963ff0a1b158d2c2fcf2da2
chavarri <chavarri@localhost>
2022-11-17 05:25:08 +0000

    svn path=/trunk/matlab/; revision=18553
    Former-commit-id: 38cff14943bf55523b95ea6bc7a025468dca72b4

--- applications/vtools/D3D/get_data_mat/plot_map_2DH_ls_diff_01.m
+++ applications/vtools/D3D/get_data_mat/plot_map_2DH_ls_diff_01.m
@@ -109,7 +109,7 @@ for kt=kt_v %time
             fpath_mat_tmp=mat_tmp_name(fdir_mat_ref,tag,'tim',time_dnum_ref(kt),'var',var_str,'pli',pliname);
             data_ref=load(fpath_mat_tmp,'data');
             
-            if size(data_ref.val,3)>1
+            if size(data_ref.data.val,3)>1
                 messageOut(fid,sprintf('Cannot do differences of 2DV plot for variable %s',var_str));
                 continue
             end
