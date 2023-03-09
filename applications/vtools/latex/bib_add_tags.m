76dbd6028f9e4cb822239683c6c812b7600f4758
chavarri <chavarri@localhost>
2023-03-09 14:28:10 +0000

    svn path=/trunk/matlab/; revision=18779
    Former-commit-id: 4a853826eb60d120fa1a51fdcbbc1b399868ece0

---
+++ applications/vtools/latex/bib_add_tags.m
@@ -0,0 +1,94 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%                 VTOOLS                 %%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 
+%Victor Chavarrias (victor.chavarrias@deltares.nl)
+%
+%$Revision: 17514 $
+%$Date: 2021-10-04 09:15:38 +0200 (Mon, 04 Oct 2021) $
+%$Author: chavarri $
+%$Id: find_str_in_cell.m 17514 2021-10-04 07:15:38Z chavarri $
+%$HeadURL: https://svn.oss.deltares.nl/repos/openearthtools/trunk/matlab/applications/vtools/general/find_str_in_cell.m $
+%
+%Adds 'year' to a bib-file copying from 'date' if it is not present.
+
+function bib_add_tags(fpath_r)
+
+fpath_w=strrep(fpath_r,'.bib','_tmp.bib');
+
+fid_w=fopen(fpath_w,'w');
+fid_r=fopen(fpath_r,'r');
+kb=0;
+while ~feof(fid_r)
+    lin=fgets(fid_r); %with newline character
+    bol_begin=strcmp(lin(1),'@') && ~contains(lin,'@Comment{'); %condition to find a begin of the block. Could be improved if it is not always the first character. We have to exclude the last line
+    if  bol_begin
+        process_block(fid_w,fid_r,lin);
+        kb=kb+1;
+        fprintf('Entrances processes: %d \n',kb);
+    else
+        fprintf(fid_w,lin);
+    end
+end %while
+fclose(fid_w);
+fclose(fid_r);
+
+%rewrite and delete
+copyfile(fpath_w,fpath_r);
+delete(fpath_w);
+
+end %function
+
+%% 
+%% FUNCTIONS
+%%
+
+%%
+
+function process_block(fid_w,fid_r,lin)
+
+%input
+str_set={'year','journal'     };
+str_get={'date','journaltitle'};
+
+%calc
+fprintf(fid_w,lin); %first line with @
+bol_end=false; %condition to finish reading block
+
+ns=numel(str_set);
+bol_set=false(1,ns);
+bol_get=false(1,ns);
+str_save=cell(1,ns);
+while ~bol_end
+    lin=fgets(fid_r);
+    bol_end=strcmp(lin(1),'}'); %it should be the first character. It is always after saving with jabref I think.
+    if bol_end; continue; end
+    lin=strrep(lin,'\','\\');
+    lin=strrep(lin,'%','%%'); 
+    fprintf(fid_w,lin);
+    if ~contains(lin,'='); continue; end
+    tok=regexp(lin,'=','split');
+    for ks=1:ns
+        %check if it has `year`
+        if strcmp(strtrim(tok{1,1}),str_set{ks})
+            bol_set(ks)=true;
+        end
+        %save `date`
+        if contains(tok{1,1},str_get{ks})
+            bol_get(ks)=true;
+            str_save{ks}=tok{1,2};
+        end
+    end
+end %while
+
+%up to this point we have not written the end of the block `}`
+
+for ks=1:ns
+    if ~bol_set(ks) && bol_get(ks)
+        fprintf(fid_w,'%s = %s',str_set{ks},str_save{ks});
+    end
+end
+
+fprintf(fid_w,'}\r');
+    
+end %process_block
---
+++ applications/vtools/latex/latex_toc.m
@@ -0,0 +1,55 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%                 VTOOLS                 %%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 
+%Victor Chavarrias (victor.chavarrias@deltares.nl)
+%
+%$Revision: 17514 $
+%$Date: 2021-10-04 09:15:38 +0200 (Mon, 04 Oct 2021) $
+%$Author: chavarri $
+%$Id: find_str_in_cell.m 17514 2021-10-04 07:15:38Z chavarri $
+%$HeadURL: https://svn.oss.deltares.nl/repos/openearthtools/trunk/matlab/applications/vtools/general/find_str_in_cell.m $
+%
+%Write the TOC of a latex file
+%
+
+function toc=latex_toc(fpath_tex)
+
+%% read
+
+fid=fopen(fpath_tex,'r');
+toc={}; %better to preallocate
+kc=0;
+while ~feof(fid)
+    lin=fgetl(fid);
+    bol_sec=contains(lin,'\gensection');
+    if bol_sec
+        tok=regexp(lin,'{(.*?)}','tokens');
+        kc=kc+1;
+        toc{kc,1}=tok{1,3}{1,1};
+        toc{kc,2}=str2double(tok{1,2}{1,1});
+        lin=fgetl(fid);
+        bol_lab=contains(lin,'\label');
+        if bol_lab
+            tok=regexp(lin,'{(.*?)}','tokens');
+            toc{kc,3}=tok{1,1}{1,1};
+        end %bol_lab
+    end %bol_sec
+end %feof
+
+fclose(fid);
+
+%% write
+
+[fdir,fname,~]=fileparts(fpath_tex);
+fpath_w=fullfile(fdir,sprintf('%s.mtoc',fname));
+fid=fopen(fpath_w,'w');
+nl=size(toc,1);
+for kl=1:nl
+    ntab=toc{kl,2}-1;
+    str_tab=repmat('      ',ntab,1);
+    fprintf(fid,'%s %s (%s) \r\n',str_tab,toc{kl,1},toc{kl,3});
+end %kl
+fclose(fid);
+
+end %function
