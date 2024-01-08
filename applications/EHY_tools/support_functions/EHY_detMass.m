dc00f296666b936414dcb38f76a32cc76a9a6b51
kaaij <kaaij@localhost>
2024-01-08 07:59:23 +0000

    Added reading only relevant partition numbers
    svn path=/trunk/matlab/; revision=19336
    
    
    Former-commit-id: a6ad6235adc87e1312a1ddf86347f584e39ea1d3

--- applications/EHY_tools/support_functions/EHY_detMass.m
+++ applications/EHY_tools/support_functions/EHY_detMass.m
@@ -3,11 +3,25 @@ function [times,mass] = EHY_detMass(fileInp,varargin)
 %% Determine time series of mass inside a polygon 
 %  Initialise
 OPT.varName = 'sal';
-OPT.filePol = [];
+OPT.filePol = '';
 OPT         = setproperty(OPT,varargin);
 
+%% load polygon
+if ~isempty(OPT.filePol)
+    % Load the polygon
+    pol      = readldb(OPT.filePol);
+    Xpol     = pol.x;
+    Ypol     = pol.y;
+else
+    Xpol = [];
+    Ypol = [];
+end
+
+%% Partition numbers
+partitionNrs = EHY_findPartitionNumbers(fileInp,'xrange',[min(Xpol) max(Xpol)],'yrange',[min(Ypol) max(Ypol)]);
+
 %% load grid data
-Info     = EHY_getGridInfo(fileInp,{'XYcen','area','no_layers'},'disp',0);
+Info     = EHY_getGridInfo(fileInp,{'XYcen','area','no_layers'},'disp',0,'mergePartitionNrs',partitionNrs);
 Xcen     = Info.Xcen;
 Ycen     = Info.Ycen;
 area     = Info.area;
@@ -15,11 +29,7 @@ kmax     = Info.no_layers;
 
 %% Inside polygon ?
 inside(1:length(Xcen)) = true;
-if ~isempty(OPT.filePol)
-    % Load the polygon
-    pol      = readldb(OPT.filePol);
-    Xpol     = pol.x;
-    Ypol     = pol.y;
+if ~isempty(Xpol) && ~isempty(Ypol)
     inside   = inpolygon(Xcen,Ycen,Xpol,Ypol);
 end
 
@@ -28,8 +38,8 @@ times                  = EHY_getmodeldata_getDatenumsFromOutputfile(fileInp);
 mass(1: length(times)) = 0.;
 
 %% Interface heights and values
-Info_z = EHY_getMapModelData(fileInp,'varName','Zcen_int');
-Info   = EHY_getMapModelData(fileInp,'varName','sal');
+Info_z = EHY_getMapModelData(fileInp,'varName','Zcen_int','mergePartitionNrs',partitionNrs);
+Info   = EHY_getMapModelData(fileInp,'varName','sal'     ,'mergePartitionNrs',partitionNrs);
 
 %% for each time
 for i_time = 1: length(times)
