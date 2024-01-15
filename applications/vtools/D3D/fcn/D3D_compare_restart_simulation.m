3103f22b6447a7171aa68839f3df24011ed29d27
chavarri <chavarri@localhost>
2024-01-15 14:45:18 +0000

    svn path=/trunk/matlab/; revision=19355
    Former-commit-id: 5a26cee315a621fded6a9049f6b2afc82494ad02

--- applications/vtools/D3D/fcn/D3D_compare_restart_simulation.m
+++ applications/vtools/D3D/fcn/D3D_compare_restart_simulation.m
@@ -10,8 +10,36 @@
 %$Id: gdb_read_variables_file.m 19340 2024-01-08 14:33:47Z chavarri $
 %$HeadURL: https://svn.oss.deltares.nl/repos/openearthtools/trunk/matlab/applications/vtools/D3D/fcn/gdb_read_variables_file.m $
 %
-
-%%
+%Starting from a reference simulation, it creates a 'main' and 'restart' simulation to
+%compare them running GDB. 
+%The 'main' simulation is as the reference one but creating restart files when
+%specified. 
+%The 'restart' simulation is as the main simulation but starting from one restart
+%file as created by the 'main' simulation. 
+%This function also creates the scripts necessary to run GDB (in Linux) for printing 
+%the desired variables to file, for later comparing them using `gdb_compare_files`.
+%The main output files are:
+%   - <fpath_main/logs/print.log>
+%   - <fpath_rst/logs/print.log>
+%where, <fpath_main> is the full path to the main simulation and <fpath_rst> the 
+%full path to the restarted simulation. Both are at the same level as <fpath_ref>. 
+%The main file to run is <./run_main_and_restart.sh>, at the same level as <fpath_ref>.
+%
+%INPUT:
+%   -fpath_ref = full path to the simulation to test. 
+%   -fpath_cmd = full path to the file with debug commands. If NaN, default debug commands. 
+%
+%INPUT (pair input)
+%   -DTUser_fact     = `DtUser` times every which a restart file is created. 
+%   -fpath_exe_gdb   = full path to executables with debug symbols. 
+%   -TStart_rst_fact = `DtUser` times after which the 'restart' simulation starts. If NaN, 
+%                       this is equal to the first time at which a restart file is created. 
+%                       As such, one can stop in debug mode after the first restart file is
+%                       created.
+%   -overwrite       = Overwrite 'main' and 'restart' folders if exists: 0 = NO; 1=YES.
+%
+%OUTPUT:
+%
 %E.G.:
 % fpath_cmd='p:\dflowfm\users\chavarri\240111_rst\commands.txt';
 % fpath_exe_gdb='p:\dflowfm\users\ottevan\delft3d\delft3d\build_dflowfm_debug\install\bin\dflowfm'    ;
@@ -26,15 +54,15 @@ parin=inputParser;
 
 addOptional(parin,'DTUser_fact',10);
 % addOptional(parin,'fpath_exe','p:\d-hydro\dimrset\latest\');
-addOptional(parin,'fpath_exe_gdb','p:\d-hydro\dimrset\latest\');
-addOptional(parin,'TStart_rst_fact',10);
+addOptional(parin,'fpath_exe','p:\d-hydro\dimrset\latest\');
+addOptional(parin,'TStart_rst_fact',NaN);
 addOptional(parin,'overwrite',1);
 
 parse(parin,varargin{:});
 
 DTUser_fact=parin.Results.DTUser_fact;
-% fpath_exe=parin.Results.fpath_exe;
-fpath_exe_gdb=parin.Results.fpath_exe_gdb;
+% fpath_exe=parin.Results.fpath_exe; 
+fpath_exe=parin.Results.fpath_exe_gdb;
 TStart_rst_fact=parin.Results.TStart_rst_fact;
 overwrite=parin.Results.overwrite;
 
@@ -55,7 +83,7 @@ end
 simdef_main=D3D_simpath(fpath_main);
 mdf_main=D3D_io_input('read',simdef_main.file.mdf);
 
-mdf_main.output.RstInterval=mdf_main.time.DtUser*DTUser_fact;
+mdf_main.output.RstInterval=mdf_main.time.DtUser*DTUser_fact; %[s]
 mdf_main.output.Wrirst_bnd=1;
 
 D3D_io_input('write',simdef_main.file.mdf,mdf_main);
@@ -72,10 +100,17 @@ end
 simdef_rst=D3D_simpath(fpath_rst); 
 mdf_rst=D3D_io_input('read',simdef_rst.file.mdf);
 
-TStart_rst=TStart_rst_fact*mdf_rst.time.DtUser; %[TUnit]
+%`TStart_rst` is the time at which the restarted simulation starts. 
+TFact=time_factor(mdf_rst.time.Tunit); %TUnit->s
+if isnan(TStart_rst_fact)
+    %Restart after the first restart file is created. This is useful to set the breakpoint after the first restart file.
+    TStart_rst=mdf_main.output.RstInterval/TFact; %[TUnit] 
+else
+    %Restart `TStart_rst_fact` times `DtUser`. The user is responsible from setting a break point after the right restart file is created. 
+    TStart_rst=TStart_rst_fact*mdf_rst.time.DtUser/TFact; %[TUnit]
+end
 mdf_rst.time.TStart=TStart_rst; %[TUnit]
 
-TFact=time_factor(mdf_rst.time.Tunit);
 mdf_rst.numerics.Tlfsmo=max([0,(mdf_main.time.TStart-TStart_rst)*TFact]); %`Tlfsmo` in [s]
 
 rst_time_dtime=datetime(num2str(mdf_rst.time.RefDate),'InputFormat','yyyyMMdd')+seconds(TStart_rst*TFact); 
@@ -107,17 +142,19 @@ fpath_rel_rst=strrep(strrep(fullfile(simdef_main.file.output,mdf_rst.restart.Res
 mdu_filename=sprintf('%s%s',fname,fext);
 
 create_run_main_and_restart(fdir_up);
-create_run_main_full(fdir_up,fdirname_main,fpath_exe_gdb,mdu_filename)
 create_run_main(fdir_up,fdirname_main,'main');
 create_run_main(fdir_up,fdirname_rst,'rst',fpath_rel_rst);
-create_create_core(fdir_up,fpath_exe_gdb);
-create_list_core(fdir_up,fpath_exe_gdb);
-create_print_core(fdir_up,fpath_exe_gdb);
+create_create_core(fdir_up,fpath_exe);
+create_list_core(fdir_up,fpath_exe);
+create_print_core(fdir_up,fpath_exe);
 create_test_create(fdir_up,fpath_cmd,mdu_filename);
 create_test_list(fdir_up);
 create_postprocess_main_and_restart(fdir_up,fdirname_main,fdirname_rst);
 
-%%
+%not necessary, because it only makes sense to set the breakpoint after a point in which the restart file to which we start has been created.
+% create_run_main_full(fdir_up,fdirname_main,fpath_exe_gdb,mdu_filename); 
+
+%% structure
 
 %run_main_and_restart
     %run_main
@@ -160,7 +197,7 @@ fprintf(fid,'#Change all to Unix and run: \n');
 fprintf(fid,'#  dos2unix *.sh *.gdb \n');
 fprintf(fid,'#  ./run_main_and_restart.sh \n');
 fprintf(fid,' \n');
-fprintf(fid,'./run_main_full.sh \n');
+% fprintf(fid,'./run_main_full.sh \n');
 fprintf(fid,'./run_main.sh \n');
 fprintf(fid,'./run_rst.sh \n');
 
@@ -243,18 +280,31 @@ function create_print_core(fdir,fpath_exe)
 fpath=fullfile(fdir,'print_core.sh');
 fid=fopen(fpath,'w');
 
+fprintf(fid,'rm logs/type.log \n');
 fprintf(fid,'rm logs/print.log \n');
 fprintf(fid,'mkdir logs \n');
-fprintf(fid,'echo set print elements 0 > test_print.gdb \n'); %`>` for opening
-fprintf(fid,'echo set print repeats 0 >> test_print.gdb \n'); %`>>` to append
+fprintf(fid,'echo set print elements 0 > test_type.gdb \n');
+fprintf(fid,'echo set print repeats 0 >> test_type.gdb \n');
+fprintf(fid,'echo set pagination off >> test_type.gdb \n');
+fprintf(fid,'echo set logging file logs/type.log >> test_type.gdb \n');
+fprintf(fid,'echo set logging on >> test_type.gdb \n');
+fprintf(fid,'sed ''s/0x0.*\\bm_\\([a-z0-9_]*\\)_mp_\\([a-z0-9_]*\\)_\\($*.*\\)/echo m_\\1::\\2\\\\n\\nwhatis m_\\1::\\2/g'' logs/list.log >> test_type.gdb \n');
+fprintf(fid,'sed -i ''s/All variables matching regular expression.*//g'' test_type.gdb \n');
+fprintf(fid,'sed -i ''s/Non-debugging symbols://g'' test_type.gdb \n');
+%Remove empty line. Repeated 3 times for safety.
+fprintf(fid,'sed -i -z ''s/\\n\\n/\\n/g'' test_type.gdb \n');
+fprintf(fid,'sed -i -z ''s/\\n\\n/\\n/g'' test_type.gdb \n');
+fprintf(fid,'sed -i -z ''s/\\n\\n/\\n/g'' test_type.gdb \n');
+fprintf(fid,'gdb --batch --command=test_type.gdb %s core.* > tmp \n',linuxify(fpath_exe));
+fprintf(fid,'echo set print elements 0 > test_print.gdb \n');
+fprintf(fid,'echo set print repeats 0 >> test_print.gdb \n');
 fprintf(fid,'echo set pagination off >> test_print.gdb \n');
 fprintf(fid,'echo set logging file logs/print.log >> test_print.gdb \n');
 fprintf(fid,'echo set logging on >> test_print.gdb \n');
-fprintf(fid,'sed ''s/.*m_\\([a-z0-9_]*\\)_mp_\\([a-z0-9_]*\\)_\\($*.*\\)/echo m_\\1::\\2\\\\n\\nprint m_\\1::\\2/g'' logs/list.log >> test_print.gdb \n');
-fprintf(fid,'sed -i -z ''s/All variables matching regular expression "m_flow_mp_":\\n\\nNon-debugging symbols:\\n//g'' test_print.gdb \n');
-fprintf(fid,'sed -i -z ''s/All variables matching regular expression "m_flowtimes_mp_":\\n\\nNon-debugging symbols:\\n//g'' test_print.gdb \n');
-fprintf(fid,'sed -i -z ''s/All variables matching regular expression "m_flowgeom_mp_":\\n\\nNon-debugging symbols:\\n//g'' test_print.gdb \n');
-fprintf(fid,'gdb --batch --command=test_print.gdb %s core.*  \n',linuxify(fpath_exe));
+fprintf(fid,'sed -z ''s/\\([a-z0-9_:]*\\)\\ntype = PTR TO -> [\\*:()a-zA-Z0-9 ,]*/echo \\1\\\\n\\nprint *\\1/g'' logs/type.log >> test_print.gdb \n');
+fprintf(fid,'sed -i -z ''s/\\([a-z0-9_:]*\\)\\ntype = <object is not a[a-z]*ated>\\n//g'' test_print.gdb \n');
+fprintf(fid,'sed -i -z ''s/\\([a-z0-9_:]*\\)\\ntype = [\\*:()a-zA-Z0-9 ,]*/echo \\1\\\\n\\nprint \\1/g'' test_print.gdb \n');
+fprintf(fid,'gdb --batch --command=test_print.gdb %s core.* > tmp \n',linuxify(fpath_exe));
 
 fclose(fid);
 
@@ -273,16 +323,20 @@ fprintf(fid,'set pagination off \n');
 fprintf(fid,'set logging file logs/create.log \n');
 fprintf(fid,'set logging on \n');
 
-% fprintf(fid,'break step_reduce.f90:95 \n');
-% fprintf(fid,'condition 1 time1.gt.ti_rst \n');
 %read
-fid_r=fopen(fpath_cmd,'r');
-while ~feof(fid_r)
-    lin=fgets(fid_r);
-    fprintf(fid,lin);
+if isempty(fpath_cmd) || isnan(fpath_cmd)
+    fprintf(fid,'break step_reduce.f90:95 \n');
+    fprintf(fid,'condition 1 time1.gt.ti_rst \n');
+elseif ~isfile(fpath_cmd)
+    error('File with commands not found: %s',fpath_cmd);
+else
+    fid_r=fopen(fpath_cmd,'r');
+    while ~feof(fid_r)
+        lin=fgets(fid_r);
+        fprintf(fid,lin);
+    end
+    fclose(fid_r);
 end
-fclose(fid_r);
-
 fprintf(fid,'r --autostartstop %s \n',mdu_filename);
 fprintf(fid,'print m_flowtimes::time1 \n');
 fprintf(fid,'generate-core-file \n');
@@ -315,7 +369,7 @@ end
 
 function create_postprocess_main_and_restart(fdir,fdirname_main,fdirname_rst)
 
-fpath=fullfile(fdir,'test_list.gdb');
+fpath=fullfile(fdir,'post_process_main_and_restart.sh');
 fid=fopen(fpath,'w');
 
 fprintf(fid,'cd %s \n',fdirname_main);
