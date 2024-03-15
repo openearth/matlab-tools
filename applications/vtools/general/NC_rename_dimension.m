ff791b4b935d2f96056f71dfbb62c09ae13cf131
chavarri <chavarri@localhost>
2024-03-15 10:57:28 +0000

    svn path=/trunk/matlab/; revision=19489
    Former-commit-id: 4972406f1f2b7756c04c985bdae3529e25dbc5e8

---
+++ applications/vtools/general/NC_rename_dimension.m
@@ -0,0 +1,23 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%                 VTOOLS                 %%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 
+%Victor Chavarrias (victor.chavarrias@deltares.nl)
+%
+%$Revision: 19243 $
+%$Date: 2023-11-20 11:49:45 +0100 (Mon, 20 Nov 2023) $
+%$Author: chavarri $
+%$Id: branch_rijntakken.m 19243 2023-11-20 10:49:45Z chavarri $
+%$HeadURL: https://svn.oss.deltares.nl/repos/openearthtools/trunk/matlab/applications/vtools/general/branch_rijntakken.m $
+%
+%
+
+function NC_rename_dimension(fpath_nc,dimname_in,dimname_out)
+
+nci=netcdf.open(fpath_nc,'NC_WRITE');
+netcdf.reDef(nci)
+dimid=netcdf.inqDimID(nci,dimname_in);
+netcdf.renameDim(nci,dimid,dimname_out)
+netcdf.close(nci);
+
+end %function
