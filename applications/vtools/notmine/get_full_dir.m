7c1c261e43a37e5528b6a335ffb12c9eec67746f
ottevan <ottevan@localhost>
2024-05-08 08:30:46 +0000

    svn path=/trunk/matlab/; revision=19592
    Former-commit-id: 2294dbe9e43b9ed5525e3af1c70d79204515f5cf

--- applications/vtools/notmine/get_full_dir.m
+++ applications/vtools/notmine/get_full_dir.m
@@ -1,13 +1,23 @@
-function fdir_out = get_full_dir(fdir)
+function fdir_out = get_full_dir(fdir,level)
 % get_full_dir - returns dir without intermediate level ups ..
+if nargin == 1;
+    level = 0; 
+    %unit test
+    assert(strcmp(get_full_dir('../../../../a/b/../c/d/e/../../f',0),fullfile('..\..\..\..\a\c\f')))
+end
+
 p = split(fullfile(fdir),filesep);
-idx= find(strcmp(p,'..'));
+ups = strcmp(p,'..');
 fdir_out = fdir;
+minidx = 2; 
+if length(ups)> 0; 
+    minidx = find(ups(minidx:end)==0,1); 
+end
+idx= find(ups(minidx+1:end));
 if length(idx)> 0;
-    if idx(1) > 2;
-        fdir_out = fullfile(p{[1:idx(1)-2,idx(1)+1:end]});
-        fdir_out = get_full_dir(fdir_out);
-    end
+    fdir_out = fullfile(p{[1:minidx+idx(1)-2,minidx+idx(1)+1:end]});
+    level = level + 1; 
+    fdir_out = get_full_dir(fdir_out,level);
 end
 end
 
