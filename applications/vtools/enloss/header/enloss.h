846389f33c4378dc00e1b57d37d6f9664e9f681c
chavarri <chavarri@localhost>
2024-08-19 07:37:06 +0000

    svn path=/trunk/matlab/; revision=19732
    Former-commit-id: c4078a62aa7c70e761cfca0ad388be9102b17a2a

---
+++ applications/vtools/enloss/header/enloss.h
@@ -0,0 +1,6 @@
+// function fcn_enloss(d1,bup,vbov,hunoweir,wsben,wsbov,iflagweir,crestl,rmpben,rmpbov,veg,VillemonteCD1,VillemonteCD2,iflagcriteriumvol,iflaglossvol,testfixedweirs) result(dte)
+double fcn_enloss(double * d1,double * bup,double * vbov,double * hunoweir,double * wsben,double * wsbov, int * iflagweir,double * crestl,double * rmpben,double * rmpbov,double * veg,double * VillemonteCD1,double * VillemonteCD2,int * iflagcriteriumvol,int * iflaglossvol,int * testfixedweirs,double * VillemonteCD3);
+
+// function fcn_volkomen(d1,bup,vbov,hunoweir,wsben,wsbov,iflagweir,crestl,rmpben,rmpbov,veg,VillemonteCD1,VillemonteCD2,iflagcriteriumvol,iflaglossvol,testfixedweirs) result(volk)
+int fcn_volkomen(double * d1,double * bup,double * vbov,double * hunoweir,double * wsben,double * wsbov, int * iflagweir,double * crestl,double * rmpben,double * rmpbov,double * veg,double * VillemonteCD1,double * VillemonteCD2,int * iflagcriteriumvol,int * iflaglossvol,int * testfixedweirs,double * VillemonteCD3);
+
