37a37351758c9e1093735780e08c4dbe71e06448
kovacs <kovacs@localhost>
2024-09-16 12:54:30 +0000

    Debug and improvements
    svn path=/trunk/matlab/; revision=19784
    
    
    Former-commit-id: c178f30fddae5635f8bf9ac2781bdfecc17e7f17

--- applications/EHY_tools/support_functions/EHY_getMapModelData_z.m
+++ applications/EHY_tools/support_functions/EHY_getMapModelData_z.m
@@ -33,9 +33,10 @@ OPT.varName = varName0; % change wanted variabele back to original value
 DataAll = EHY_getMapModelData(inputFile,OPT);
 
 %% check
-GI = EHY_getGridInfo(inputFile,'no_layers');
-if GI.no_layers < 2
-    warning('Data at certain z-level requested, but this is a 2D variabele (and/or model). The 2D-data will be used as horizontal slice');
+[~,dimsInd] = EHY_getDimsInfo(inputFile,OPT,modelType);
+
+if isempty(dimsInd.layers)
+    warning(['Data at certain z-level requested, but this is a 2D variabele (' OPT.varName '). The 2D-data will be used as horizontal slice.']);
     Data = DataAll;
     return
 end
