7c703d80ac2b5f122506772932437230432142ed
kaaij <kaaij@localhost>
2024-11-06 13:45:42 +0000

    svn path=/trunk/matlab/; revision=19874
    Former-commit-id: 059d69d537f674b2758fccec2dd9bf6ae300f569

--- applications/EHY_tools/EHY_plotContourPhase.m
+++ applications/EHY_tools/EHY_plotContourPhase.m
@@ -22,32 +22,13 @@ DATA     = qpread(FI,FI.NumDomains + 2,longName,'griddata');
 DATA_org = DATA.Val;
 
 %% Plot Contour lines, QUICKPLOT style, from 0 until 360 degrees. To avoid thick 0 degrees phase line file with NAN vulue in between 345 and 15 degrees
-Ops                      = struct('version'   , 1.4      , 'axestype','Lon-Lat','presentationtype','contour lines', ...
-                                  'LineParams',{{'linestyle',OPT.linestyle,'color',OPT.Color}},                     ...
-                                  'Thresholds',[0:30:360]                                                         );
-Ops                      = qp_state_version(Ops);
 DATA.Val(DATA.Val > 345) = NaN;
 DATA.Val(DATA.Val <  15) = NaN;
+EHY_plotContour(DATA,'varContours','phs','valContours',30:30:330,'colContours',OPT.Color,'styleContours',{OPT.linestyle});
 
-qp_scalarfield(gca,[],'contour lines','UGRID',DATA,Ops);
-
-%% Now the 0/360 degrees line (add 180 degrees and plot the 180 degrees line)
-DATA.val                 = DATA_org;
-                         DATA.Val                 = DATA.Val - 180.;
-                         DATA.Val(DATA.Val < 0  ) = DATA.Val(DATA.Val < 0  ) + 360.;
-                         DATA.Val(DATA.Val > 345) = NaN;
-                         DATA.Val(DATA.Val <  15) = NaN;
-Ops                      = struct('version'   , 1.4      , 'axestype','Lon-Lat','presentationtype','contour lines', ...
-                                  'LineParams',{{'linestyle',OPT.linestyle,'color',OPT.Color}},                     ...
-                                  'Thresholds',[180.]                                                               );
-Ops                      = qp_state_version(Ops);
-DATA.Val(DATA.Val > 345) = NaN;
-DATA.Val(DATA.Val <  15) = NaN;
-
-qp_scalarfield(gca,[],'contour lines','UGRID',DATA,Ops);
-
-
-
-
+%% Now the 0/360 degrees line 
+DATA.Val                   = DATA_org;
+DATA.Val(DATA.Val < 180  ) = DATA.Val(DATA.Val < 180  ) + 360.;
+EHY_plotContour(DATA,'varContours','phs','valContours',360,'colContours',OPT.Color,'styleContours',{OPT.linestyle});
     
 
