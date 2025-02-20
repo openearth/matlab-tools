02976874c2a68c9a1e66f8a6c0c6616225dc0d21
chavarri <chavarri@localhost>
2025-02-20 14:58:00 +0000

    svn path=/trunk/matlab/; revision=20064
    Former-commit-id: b3b9581609f32a88e7d310728dfa48fc29c238f4

--- applications/vtools/morpho_setup/create_subdomain_BC.m
+++ applications/vtools/morpho_setup/create_subdomain_BC.m
@@ -25,23 +25,33 @@
 %This function contains all steps stems from script `main04_derive_boundary_files.m` in
 %<28_get_partition_pli_grave_lith>.
 
-function create_subdomain_BC(fdir_in,fdir_out,fpath_crs)
+function create_subdomain_BC(fdir_in,fdir_name_1dt,fpath_bc_crs,fdir_out_BC,fpath_enc,bc_type,fdir_hydro_sim,fname_h,fname_q,fname_ext,fpathrel_bc,fpathrel_pli,time_start)
 
 %%
 
-create_mdf_with_crs(fdir_in,fdir_out_1dt,fpath_enc_crs)
+messageOut(NaN,'Start creating mdf-file with additional cross-section.',2)
+fdir_out_1dt=create_mdf_with_crs(fdir_in,fdir_name_1dt,fpath_bc_crs);
 
 %%
 
+messageOut(NaN,'Start running 1 timestep simulation.',2)
 run_simulation_get_shp(fdir_out_1dt)
 
 %%
 
-create_observation_locations(fdir_out_1dt,fpath_obs,fpath_crs,fpath_enc_crs);
+messageOut(NaN,'Start getting observations stations and cross-sections.',2)
+
+mkdir_check(fdir_out_BC);
+fpath_obs=fullfile(fdir_out_BC,'obs.xyn');
+fpath_crs_h=fullfile(fdir_out_BC,'crs_h.pli');
+fpath_crs_q=fullfile(fdir_out_BC,'crs_q.pli');
+
+boundaries=create_observation_locations(fdir_out_1dt,fpath_obs,fpath_crs_h,fpath_crs_q,fpath_enc,bc_type);
 
 %%
 
-write_subdomain_bc_dir(fdir_hydro_sim,fname_h,fname_q,fname_ext)
+messageOut(NaN,'Start getting observations stations and cross-sections.',2)
+write_subdomain_bc_dir(fdir_hydro_sim,fname_h,fname_q,fname_ext,fpath_crs_h,fpath_crs_q,fpath_obs,fdir_out_BC,fpathrel_bc,fpathrel_pli,time_start,boundaries,fpath_enc);
 
 end %function
 
