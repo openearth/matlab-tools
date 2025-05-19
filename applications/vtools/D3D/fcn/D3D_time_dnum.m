5bebd55cee8941c024e5099440c8ff34b1c09132
chavarri <chavarri@localhost>
2025-05-19 05:44:22 +0000

    svn path=/trunk/matlab/; revision=20154
    Former-commit-id: aaf54a708f7c8aa2a60b0779c768caaad03f12dc

--- applications/vtools/D3D/fcn/D3D_time_dnum.m
+++ applications/vtools/D3D/fcn/D3D_time_dnum.m
@@ -54,113 +54,7 @@ fpath_tim_all=fullfile(fdir_mat,sprintf('tim%s.mat',str_tim));
 %%
 
 if isa(in_dtime(1),'double') 
-    %get all results time
-    idx_g=NaN; %not needed, but we need to output it
-    %if it is NaN we read it anyhow because we do not reach this point in case it is NaN and it is the same size as the one we have already.
-    if isempty(fdir_mat) || exist(fpath_tim_all,'file')~=2 || any(isnan(in_dtime)) 
-        messageOut(NaN,sprintf('Mat-file with all times not available or outdated. Reading.'))
-        if isfolder(fpath_map) %SMT
-            [time_r,time_mor_r,time_dnum,time_dtime,time_mor_dnum,time_mor_dtime,sim_idx,time_idx]=D3D_results_time_wrap(fpath_map,results_type);
-        else
-            is_mor=D3D_is(fpath_map);
-            [time_r,time_mor_r,time_dnum,time_dtime,time_mor_dnum,time_mor_dtime]=D3D_results_time(fpath_map,is_mor,[1,Inf]);
-            sim_idx=NaN(size(time_r));
-            time_idx=(1:1:numel(time_r))';
-        end
-        data=v2struct(time_r,time_mor_r,time_dnum,time_dtime,time_mor_dnum,time_mor_dtime,sim_idx,time_idx);
-        save_check(fpath_tim_all,'data')
-    else
-        messageOut(NaN,sprintf('Mat-file with all times available. Loading: %s',fpath_tim_all))
-        load(fpath_tim_all,'data')
-        v2struct(data);
-        
-        %check all fields exist
-        fn=fieldnames(data);
-        fn_check={'time_dnum','time_dtime','time_mor_dnum','time_mor_dtime','sim_idx','time_idx'}; %fieldnames that must be present
-        [~,bol_f]=find_str_in_cell(fn_check,fn);
-
-        %There is a file with all result times, but as we request the last one, we have to check that the simulation has not continued.
-        last_changed=false;
-        if any(isinf(in_dtime)) 
-            if isfolder(fpath_map) %smt
-                [~,~,time_dnum_f,~,~,~,~,~]=D3D_results_time_wrap(fpath_map,results_type);
-            else
-                is_mor=D3D_is(fpath_map);
-                [~,~,time_dnum_f,~,~,~]=D3D_results_time(fpath_map,is_mor,NaN);
-            end
-            if abs(time_dnum_f-time_dnum)>1/3600/24 %1 s threshold
-                last_changed=true;
-            end
-        end
-
-        %It can happen that it has saved a file with no output. Then it
-        %crashes below because of size differences. If there is nothing
-        %inside, we erase. 
-        ntt=numel(data.(fn{1})); 
-
-        if ~all(bol_f) || last_changed || ntt==0 %old time file, data is missing. 
-            messageOut(NaN,'Old time file. Erasing and computing again.')
-            delete(fpath_tim_all)
-            [time_dnum,time_dtime,time_mor_dnum,time_mor_dtime,sim_idx,idx_g,time_idx]=D3D_time_dnum(fpath_map,in_dtime,varargin{:});
-            return
-        end
-
-    end
-    
-    %get the requested ones
-
-        %all
-    if any(isnan(in_dtime))  
-        return
-    end
-    
-        %match each one
-    ntt=numel(time_dnum);
-    nt=numel(in_dtime);
-    time_dnum_s=NaN(nt,1);
-    time_dtime_s=NaT(nt,1);
-    time_dtime_s.TimeZone='+00:00';
-    time_mor_dnum_s=NaN(nt,1);
-    time_mor_dtime_s=NaT(nt,1);
-    time_mor_dtime_s.TimeZone='+00:00';
-    sim_idx_s=NaN(nt,1);
-    time_idx_s=NaN(nt,1);
-    for kt=1:nt
-        if isinf(in_dtime(kt)) %last
-            idx_g=ntt;
-        elseif mod(in_dtime(kt),1)==0 && in_dtime(kt)<=ntt %if integer and smaller than total number of results, you are specifying index
-            idx_g=in_dtime(kt);
-            if ntt>datenum(1687,07,05) %if there are more than 
-                messageOut(NaN,'I supposed the input was an index but the number of results is huge, so maybe you want datenum?') %create a flag to force datenum
-            end
-        else %datenum
-            if tim_type==1
-                tim_cmp=time_dnum;
-            elseif tim_type==2
-                tim_cmp=time_mor_dnum;
-            else
-                error('not sure what you want')
-            end
-            if isnan(tim_cmp)
-                error('Problem with time') %wanted morpho time?
-            end
-            idx_g=absmintol(tim_cmp,in_dtime(kt),'tol',tol,'dnum',1);
-        end
-
-        time_dnum_s(kt,1)=time_dnum(idx_g);
-        time_dtime_s(kt,1)=time_dtime(idx_g);
-        time_mor_dnum_s(kt,1)=time_mor_dnum(idx_g);
-        time_mor_dtime_s(kt,1)=time_mor_dtime(idx_g);
-        sim_idx_s(kt,1)=sim_idx(idx_g);
-        time_idx_s(kt,1)=time_idx(idx_g);
-    end
-    time_dnum=time_dnum_s;
-    time_dtime=time_dtime_s;
-    time_mor_dnum=time_mor_dnum_s;
-    time_mor_dtime=time_mor_dtime_s;
-    sim_idx=sim_idx_s;
-    time_idx=time_idx_s;
-%     end
+    [time_dnum,time_dtime,time_mor_dnum,time_mor_dtime,sim_idx,idx_g,time_idx]=D3D_time_double(fdir_mat,fpath_tim_all,in_dtime,fpath_map,results_type,tim_type,tol);
 elseif isa(in_dtime(1),'datetime') %datetime
     tim_cmp=datenum_tzone(in_dtime);
     [time_dnum,time_dtime,time_mor_dnum,time_mor_dtime,sim_idx,idx_g,time_idx]=D3D_time_dnum(fpath_map,tim_cmp,varargin{:});
@@ -168,3 +62,169 @@ elseif isa(in_dtime(1),'datetime') %datetime
 else
     error('ups...')
 end
+
+end %function
+
+%%
+%% FUNCTIONS
+%%
+
+function [time_dnum,time_dtime,time_mor_dnum,time_mor_dtime,sim_idx,idx_g,time_idx]=D3D_time_double(fdir_mat,fpath_tim_all,in_dtime,fpath_map,results_type,tim_type,tol)
+
+idx_g=NaN; %not needed, but we need to output it
+
+%% get all results time
+[~,~,time_dnum_all,time_dtime_all,time_mor_dnum_all,time_mor_dtime_all,sim_idx_all,time_idx_all]=D3D_time_all(fdir_mat,fpath_tim_all,in_dtime,fpath_map,results_type,tim_type);
+
+%% get the requested ones
+
+%all
+if any(isnan(in_dtime))  
+    %We already have what we want. 
+    return
+end
+
+%match each one
+ntt=numel(time_dnum_all);
+nt=numel(in_dtime);
+time_dnum=NaN(nt,1);
+time_dtime=NaT(nt,1);
+time_dtime.TimeZone='+00:00';
+time_mor_dnum=NaN(nt,1);
+time_mor_dtime=NaT(nt,1);
+time_mor_dtime.TimeZone='+00:00';
+sim_idx=NaN(nt,1);
+time_idx=NaN(nt,1);
+for kt=1:nt
+    if isinf(in_dtime(kt)) %last
+        idx_g=ntt;
+    elseif mod(in_dtime(kt),1)==0 && in_dtime(kt)<=ntt %if integer and smaller than total number of results, you are specifying index
+        idx_g=in_dtime(kt);
+        if ntt>datenum(1687,07,05) %if there are more than 
+            messageOut(NaN,'I supposed the input was an index but the number of results is huge, so maybe you want datenum?') %create a flag to force datenum
+        end
+    else %datenum
+        if tim_type==1 %hydro time
+            tim_cmp=time_dnum_all;
+        elseif tim_type==2 %morpho time
+            tim_cmp=time_mor_dnum_all;
+        else
+            error('You should not reach this point.')
+        end
+        if isnan(tim_cmp)
+            error('Problem with time') %wanted morpho time?
+        end
+        idx_g=absmintol(tim_cmp,in_dtime(kt),'tol',tol,'dnum',1);
+    end
+
+    time_dnum(kt,1)=time_dnum_all(idx_g);
+    time_dtime(kt,1)=time_dtime_all(idx_g);
+    time_mor_dnum(kt,1)=time_mor_dnum_all(idx_g);
+    time_mor_dtime(kt,1)=time_mor_dtime_all(idx_g);
+    sim_idx(kt,1)=sim_idx_all(idx_g);
+    time_idx(kt,1)=time_idx_all(idx_g);
+end %kt
+
+end %function
+
+%%
+
+function [time_r,time_mor_r,time_dnum,time_dtime,time_mor_dnum,time_mor_dtime,sim_idx,time_idx]=D3D_time_get_all_results(fpath_tim_all,fpath_map,results_type)
+
+if isfolder(fpath_map) %SMT
+    [time_r,time_mor_r,time_dnum,time_dtime,time_mor_dnum,time_mor_dtime,sim_idx,time_idx]=D3D_results_time_wrap(fpath_map,results_type);
+else
+    is_mor=D3D_is(fpath_map);
+    [time_r,time_mor_r,time_dnum,time_dtime,time_mor_dnum,time_mor_dtime]=D3D_results_time(fpath_map,is_mor,[1,Inf]);
+    sim_idx=NaN(size(time_r));
+    time_idx=(1:1:numel(time_r))';
+end
+data=v2struct(time_r,time_mor_r,time_dnum,time_dtime,time_mor_dnum,time_mor_dtime,sim_idx,time_idx);
+save_check(fpath_tim_all,'data')
+
+end %function
+
+%% 
+
+function new_all_time_needed=D3D_time_check_if_new_all_time_needed_data(data,in_dtime,fpath_map,results_type,tim_type)
+
+new_all_time_needed=false;
+
+%check all fields exist
+fn=fieldnames(data);
+fn_check={'time_dnum','time_dtime','time_mor_dnum','time_mor_dtime','sim_idx','time_idx'}; %fieldnames that must be present
+[~,bol_f]=find_str_in_cell(fn_check,fn);
+
+if ~all(bol_f)
+    new_all_time_needed=true;
+    return
+end
+
+%There is a file with all result times, but as we request the last one, we have to check that the simulation has not continued.
+last_changed=false;
+if any(isinf(in_dtime)) 
+    if isfolder(fpath_map) %smt
+        [~,~,time_dnum_f,~,~,~,~,~]=D3D_results_time_wrap(fpath_map,results_type);
+    else
+        is_mor=D3D_is(fpath_map);
+        [~,~,time_dnum_f,~,~,~]=D3D_results_time(fpath_map,is_mor,NaN);
+    end
+    if abs(time_dnum_f-data.time_dnum)>1/3600/24 %1 s threshold
+        last_changed=true;
+    end
+end
+
+if last_changed
+    new_all_time_needed=true;
+    return
+end
+
+%It can happen that it has saved a file with no output. Then it
+%crashes below because of size differences. If there is nothing
+%inside, we erase. 
+ntt=numel(data.(fn{1})); 
+
+if ntt==0
+    new_all_time_needed=true;
+    return
+end
+
+%We request an index, and the number of times is smaller than the index.
+if tim_type==3 && ntt<in_dtime
+    new_all_time_needed=true;
+    return    
+end
+
+end %function
+
+%%
+
+function [time_r,time_mor_r,time_dnum,time_dtime,time_mor_dnum,time_mor_dtime,sim_idx,time_idx]=D3D_time_all(fdir_mat,fpath_tim_all,in_dtime,fpath_map,results_type,tim_type)
+
+if isempty(fdir_mat) || exist(fpath_tim_all,'file')~=2
+    messageOut(NaN,sprintf('Mat-file with all times not available. Reading.'))
+    new_all_time_needed=true;
+elseif any(isnan(in_dtime)) 
+    %if it is NaN we read it anyhow because we do not reach this point in case it is NaN and it is the same size as the one we have already.
+    messageOut(NaN,sprintf('Mat-file with all times available but you want all times and we need to check it is updated. Reading.'))
+    new_all_time_needed=true;
+else
+    messageOut(NaN,sprintf('Mat-file with all times available. Loading: %s',fpath_tim_all))
+    load(fpath_tim_all,'data')
+    
+    new_all_time_needed=D3D_time_check_if_new_all_time_needed_data(data,in_dtime,fpath_map,results_type,tim_type);
+
+    if new_all_time_needed  %old time file, data is missing. 
+        messageOut(NaN,'Mat-file with all times is outdated. Erasing and computing again.')
+        delete(fpath_tim_all)
+    end
+end
+
+if new_all_time_needed
+    [time_r,time_mor_r,time_dnum,time_dtime,time_mor_dnum,time_mor_dtime,sim_idx,time_idx]=D3D_time_get_all_results(fpath_tim_all,fpath_map,results_type);
+else
+    messageOut(NaN,'Mat-file with all times is usable.')
+    v2struct(data);
+end
+
+end %function
