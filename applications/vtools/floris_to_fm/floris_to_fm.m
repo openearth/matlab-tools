933b5130e02de835bf9e8964839912188eac0ad5
chavarri <chavarri@localhost>
2025-07-09 09:01:33 +0000

    svn path=/trunk/matlab/; revision=20243
    Former-commit-id: 00a343265d36de3547112086250c5b775cf91ede

---
+++ applications/vtools/floris_to_fm/floris_to_fm.m
@@ -0,0 +1,21 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%                 VTOOLS                 %%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 
+%Victor Chavarrias (victor.chavarrias@deltares.nl)
+%
+%$Revision: 20235 $
+%$Date: 2025-07-07 14:32:25 +0200 (Mon, 07 Jul 2025) $
+%$Author: chavarri $
+%$Id: D3D_gdm.m 20235 2025-07-07 12:32:25Z chavarri $
+%$HeadURL: https://svn.oss.deltares.nl/repos/openearthtools/trunk/matlab/applications/vtools/D3D/get_data_mat/D3D_gdm.m $
+%
+%Convert Floris to Delft3D FM model. 
+
+function floris_to_fm(fpath_file)
+
+[fidr,fname,fext]=fileparts(fpath_file);
+
+D3D_writ
+
+end %function
