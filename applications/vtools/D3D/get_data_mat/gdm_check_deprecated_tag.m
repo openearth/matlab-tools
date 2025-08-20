542a5a9ca254d947c5fcece9768464f6ab60790f
chavarri <chavarri@localhost>
2025-08-20 08:19:00 +0000

    svn path=/trunk/matlab/; revision=20289
    Former-commit-id: b4ea938dccef57f974eeca213e1a75372a87b24b

--- applications/vtools/D3D/get_data_mat/gdm_check_deprecated_tag.m
+++ applications/vtools/D3D/get_data_mat/gdm_check_deprecated_tag.m
@@ -4,11 +4,11 @@
 % 
 %Victor Chavarrias (victor.chavarrias@deltares.nl)
 %
-%$Revision: 20194 $
-%$Date: 2025-06-13 09:30:18 +0200 (Fri, 13 Jun 2025) $
-%$Author: chavarri $
-%$Id: plot_all_runs_one_figure.m 20194 2025-06-13 07:30:18Z chavarri $
-%$HeadURL: https://svn.oss.deltares.nl/repos/openearthtools/trunk/matlab/applications/vtools/D3D/get_data_mat/plot_all_runs_one_figure.m $
+%$Revision$
+%$Date$
+%$Author$
+%$Id$
+%$HeadURL$
 %
 %
 
--- applications/vtools/D3D/get_data_mat/gdm_check_tag.m
+++ applications/vtools/D3D/get_data_mat/gdm_check_tag.m
@@ -4,11 +4,11 @@
 % 
 %Victor Chavarrias (victor.chavarrias@deltares.nl)
 %
-%$Revision: 20194 $
-%$Date: 2025-06-13 09:30:18 +0200 (Fri, 13 Jun 2025) $
-%$Author: chavarri $
-%$Id: create_mat_map_2DH_ls_01.m 20194 2025-06-13 07:30:18Z chavarri $
-%$HeadURL: https://svn.oss.deltares.nl/repos/openearthtools/trunk/matlab/applications/vtools/D3D/get_data_mat/create_mat_map_2DH_ls_01.m $
+%$Revision$
+%$Date$
+%$Author$
+%$Id$
+%$HeadURL$
 %
 %
 
