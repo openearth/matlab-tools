62d816a8cdf26283c93511d9345a4b88a1658f6c
chavarri <chavarri@localhost>
2025-09-16 15:25:41 +0000

    svn path=/trunk/matlab/; revision=20330
    Former-commit-id: 77a28cd25225914a222268c7045326eea1550455

--- applications/vtools/general/standup_round.m
+++ applications/vtools/general/standup_round.m
@@ -1,5 +1,6 @@
 function standup_round()
 
+rng(datenum(datetime('now'))*5000) %changes every 10 or 15 s more or less
 Names = {'Johan Reyns','Victor Chavarrias','Willem Ottevanger','Bert Jagers','Marcio Boechat','Qilong Bi','Joao Dobrochinski','Zeta Tam','Adri Mourits'};
 [~,isort] = sort(rand(1,numel(Names)));
 Names(isort)'
