dffe6475918a57ef2804f7d22da6dea3377899d9
Willem Ottevanger <willem.ottevanger@deltares.nl>
2025-10-08 15:48:10 +0200

    ignore .asv and update to D3D_grd2map

--- applications/vtools/D3D/fcn/D3D_grd2map.m
+++ applications/vtools/D3D/fcn/D3D_grd2map.m
@@ -19,20 +19,49 @@ function D3D_grd2map(fpath_grd,varargin)
 [~,fname_grd]=fileparts(fpath_grd);
 
 parin=inputParser;
+mdu_struct=struct()
+
+mdu_struct.general.program = 'D-Flow FM'; 
+mdu_struct.general.version = '';
+mdu_struct.general.fileversion = 1.09;
+mdu_struct.general.filetype = 'modelDef'; 
+mdu_struct.general.autostart = 0;
+mdu_struct.geometry.netfile = 'tmp_net.nc';  
+mdu_struct.geometry.removesmalllinkstrsh = 0;  %do not remove small flow links. The quality of the grid depends on you.
+mdu_struct.geometry.cosphiutrsh = 1; %do not remove small flow links. The quality of the grid depends on you.
+mdu_struct.time.refdate = '20000101';
+mdu_struct.time.tunit = 'S';
+mdu_struct.time.tstart = 0;
+mdu_struct.time.tstop = 1;
+mdu_struct.time.dtinit = 1;
+mdu_struct.time.dtuser = 1;
+mdu_struct.time.dtmax = 1;
 
 addOptional(parin,'fdir_work',fullfile(pwd,'tmp_grd2map'));
 addOptional(parin,'fpath_exe','c:\Program Files\Deltares\Delft3D FM Suite 2024.03 HM\plugins\DeltaShell.Dimr\kernels\x64\bin\run_dimr.bat');
 addOptional(parin,'fpath_map',fullfile(pwd,sprintf('%s_map.nc',fname_grd)));
-addOptional(parin,'circumcenter_method','internalNetlinksEdge');
-addOptional(parin,'circumcenter_tolerance',1e-3);
+addOptional(parin,'fdir_work_erase',true);
+addOptional(parin,'mdu_struct',mdu_struct);
 
 parse(parin,varargin{:});
 
 fdir_work=parin.Results.fdir_work;
 fpath_exe=parin.Results.fpath_exe;
 fpath_map=parin.Results.fpath_map;
-cc_method=parin.Results.circumcenter_method; 
-cc_tol=parin.Results.circumcenter_tolerance;
+fdir_work_erase=parin.Results.fdir_work_erase;
+
+if isfield(parin.Results, 'mdu_struct'); 
+    chapters = fieldnames(parin.Results.mdu_struct); 
+    for c = 1:length(chapters); 
+        chapter = chapters{c}; 
+        keywords = fieldnames(parin.Results.mdu_struct.(chapter));
+        for k = 1:length(keywords);
+            keyword=keywords{k};
+            mdu_struct.(lower(chapter)).(lower(keyword)) = parin.Results.mdu_struct.(chapter).(keyword);
+        end
+    end
+end 
+
 
 %% CALC
 
@@ -43,9 +72,26 @@ mkdir_check(fdir_work);
 fpath_grd_copy=fullfile(fdir_work,'tmp_net.nc');
 copyfile_check(fpath_grd,fpath_grd_copy,1);
 
+chapters = fieldnames(mdu_struct); 
+for c = 1:length(chapters); 
+    chapter = chapters{c}; 
+    keywords = fieldnames(mdu_struct.(chapter));
+    for k = 1:length(keywords);
+        keyword=keywords{k};
+        keyword_value=mdu_struct.(chapter).(keyword);
+        if ischar(keyword_value); 
+            if exist(keyword_value) == 2; 
+                tmp_file=fullfile(fdir_work, filenameext(keyword_value)); 
+                copyfile_check(keyword_value,tmp_file,1);
+                mdu_struct.(chapter).(keyword)=filenameext(keyword_value);  % rename file
+            end
+        end
+    end
+end 
+
 %create mdu file
 fpath_mdu=fullfile(fdir_work,'tmp.mdu');
-mdufile(fpath_mdu,cc_method,cc_tol);
+mdufile(fpath_mdu,mdu_struct);
 
 %create xml file
 fpath_xml=fullfile(fdir_work,'dimr_config.xml');
@@ -60,8 +106,10 @@ fpath_map_loc=fullfile(fdir_work,'DFM_OUTPUT_tmp','tmp_map.nc');
 copyfile_check(fpath_map_loc,fpath_map,1);
 
 %erase mdu
-if strcmp(fdir_work,pwd)==0
-    erase_directory(fdir_work);
+if fdir_work_erase
+    if strcmp(fdir_work,pwd)==0
+        erase_directory(fdir_work);
+    end
 end
 
 end %function
@@ -70,31 +118,27 @@ end %function
 %% FUNCTIONS
 %%
 
-function mdufile(fpath_mdu,cc_method,cc_tol);
+function mdufile(fpath_mdu,mdu_struct)
 
 fid=fopen(fpath_mdu,'w');
 
-fprintf(fid,'[General]                                                  \r\n');
-fprintf(fid,'Program                           = D-Flow FM              \r\n');
-fprintf(fid,'Version                           =                        \r\n');
-fprintf(fid,'fileVersion                       = 1.09                   \r\n');
-fprintf(fid,'fileType                          = modelDef               \r\n');
-fprintf(fid,'AutoStart                         = 0                      \r\n');
-fprintf(fid,'                                                           \r\n');
-fprintf(fid,'[geometry]                                                 \r\n');
-fprintf(fid,'NetFile                           = tmp_net.nc             \r\n');  
-fprintf(fid,'Removesmalllinkstrsh              = 0                      \r\n');  %do not remove small flow links. The quality of the grid depends on you.
-fprintf(fid,'Cosphiutrsh                       = 1                      \r\n');  %do not remove small flow links. The quality of the grid depends on you.
-fprintf(fid,'circumcenterMethod                = %s \r\n', cc_method);  
-fprintf(fid,'circumcenterTolerance             = %f \r\n', cc_tol);  
-fprintf(fid,'                                                           \r\n');
-fprintf(fid,'[time]                                                     \r\n');
-fprintf(fid,'RefDate                           = 20000101               \r\n');
-fprintf(fid,'Tunit                             = S                      \r\n');
-fprintf(fid,'TStart                            = 0                      \r\n');
-fprintf(fid,'TStop                             = 1                      \r\n');
-fprintf(fid,'DtInit                            = 1                      \r\n');
-fprintf(fid,'DtUser                            = 1                      \r\n');
+chapters = fieldnames(mdu_struct); 
+for c = 1:length(chapters); 
+    chapter = chapters{c}; 
+    keywords = fieldnames(mdu_struct.(chapter));
+    fprintf(fid,'[%s]\r\n', chapter);
+    for k = 1:length(keywords);
+        keyword=keywords{k};
+        mdu_struct.(chapter).(keyword);
+        if ischar(mdu_struct.(chapter).(keyword))
+            fprintf(fid,'%-33s = %-20s \r\n', keyword, mdu_struct.(chapter).(keyword));
+        elseif isinteger(mdu_struct.(chapter).(keyword))
+            fprintf(fid,'%-33s = %i \r\n', keyword, mdu_struct.(chapter).(keyword));
+        elseif isreal(mdu_struct.(chapter).(keyword))
+            fprintf(fid,'%-33s = %f \r\n', keyword, mdu_struct.(chapter).(keyword));
+        end
+    end
+end
 
 fclose(fid);
 
