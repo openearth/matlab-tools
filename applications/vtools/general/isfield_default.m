c6ba55c80a8f37c72b85cecf6f415430160df4ad
Victor Chavarrias <victor.chavarrias@deltares.nl>
2026-01-27 05:35:11 +0100

    Update isfield_default.m

--- applications/vtools/general/isfield_default.m
+++ applications/vtools/general/isfield_default.m
@@ -12,15 +12,28 @@
 %
 %Assing default value to structure if it does not exist.
 
-function struct=isfield_default(struct,var,def,varargin)
+function out=isfield_default(struct,var,def,varargin)
 
 %% PARSE
 
-if nargin<4
-    replace_empty=false;
-else
-    replace_empty=varargin{1,1};
-end
+parin=inputParser;
+
+addOptional(parin,'replace_empty',false);
+addOptional(parin,'output','structure');
+
+parse(parin,varargin{:});
+
+replace_empty=parin.Results.replace_empty;
+output=parin.Results.output;
+
+%%
+
+%I am breaking backward compatibility here. Repare the caller when found where. 
+% if nargin<4
+%     replace_empty=false;
+% else
+%     replace_empty=varargin{1,1};
+% end
 
 %% CALC
 if ~isfield(struct,var)
@@ -29,6 +42,15 @@ end
 
 if replace_empty && isempty(struct.(var))
     struct.(var)=def;
+end
+
+switch output
+    case 'structure'
+        out=struct;
+    case 'array'
+        out=struct.(var);
+    otherwise
+        error('Unknown output type %s',output);
 end
 
 end %function
