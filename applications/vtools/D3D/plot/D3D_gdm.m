a82d0e8a5a4525d297b22569ef05e14f78718cfc
Victor Chavarrias <victor.chavarrias@deltares.nl>
2026-02-20 08:51:50 +0100

    Deprecated `D3D_gdm`

---
+++ applications/vtools/D3D/plot/D3D_gdm.m
@@ -0,0 +1,21 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%                 VTOOLS                 %%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 
+%Victor Chavarrias (victor.chavarrias@deltares.nl)
+%
+%$Revision$
+%$Date$
+%$Author$
+%$Id$
+%$HeadURL$
+%
+%
+
+function D3D_gdm(in_plot)
+
+warning('Deprecated. Call `D3D_plot`.')
+
+D3D_plot(in_plot);
+
+end %function
