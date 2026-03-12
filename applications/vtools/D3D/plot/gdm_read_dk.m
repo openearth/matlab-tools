a783fdc81c336a80a922674bdfced48df0b42df9
Victor Chavarrias <victor.chavarrias@deltares.nl>
2026-03-12 16:04:49 +0100

    Update gdm_read_dk.m

--- applications/vtools/D3D/plot/gdm_read_dk.m
+++ applications/vtools/D3D/plot/gdm_read_dk.m
@@ -24,7 +24,13 @@ end
 if ~isfield(simdef.file,'sed') || isempty(simdef.file.sed)
     return
 end
-dk=D3D_read_sed(simdef.file.sed);
+if iscell(simdef.file.sed)
+    %We assume it is an SMT simulation and the same sediment file is used for all simulations. We take the first one.
+    fpath_sed=simdef.file.sed{1,1};
+else
+    fpath_sed=simdef.file.sed;
+end
+dk=D3D_read_sed(fpath_sed);
 save_check(fpath_mat,'dk');
 
 end %function
