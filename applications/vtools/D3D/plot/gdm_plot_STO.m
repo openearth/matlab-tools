10ccb78e97031fd7c6a1bb0974991c5314db445d
Victor Chavarrias <victor.chavarrias@deltares.nl>
2026-03-19 07:17:41 +0100

    Update gdm_plot_STO.m

--- applications/vtools/D3D/plot/gdm_plot_STO.m
+++ applications/vtools/D3D/plot/gdm_plot_STO.m
@@ -142,16 +142,22 @@ in_p.leg_str=in_plot.(tag).var;
 in_p.val=cell2mat(val_cum_sum_kt);
 in_p.tim=time_dnum_plot(kt);
 in_p.variable='stot_t';
-in_p.fname=fname_noext;
 in_p.s=rkmv.rkm_cen;
 in_p.xlab_str='rkm';
 in_p.xlab_un=1/1000;
-% in_p.leg_loc='northoutside';
-in_p.fig_size=[0,0,14.5,10];
-in_p.leg_loc='eastoutside';
+in_p.leg_loc='northoutside';
+in_p.fig_size=[0,0,14.5,15];
+% in_p.leg_loc='eastoutside';
 % in_p.fig_size=[0,0,27,15];
 in_p.y_scale='log';
 
-fig_1D_01(in_p);
+nxlim=size(flg_loc.xlims,1);
+for kxlim=1:nxlim
+    fname_noext=fullfile(fdir_fig_loc,sprintf('%s_all_%s_%s_%s_%s_kxlim_%d',tag,runid,datestr(time_dnum_plot(kt),'yyyymmdd_HHMMSS'),statis,sb_pol{ksb},kxlim));
+    in_p.fname=fname_noext;
+    in_p.xlims=flg_loc.xlims(kxlim,:);
+
+    fig_1D_01(in_p);
+end 
 
 end %function
