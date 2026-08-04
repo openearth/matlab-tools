c26e6b9f92d0d25f07646af8b2db5a28fd825c06
Victor Chavarrias <victor.chavarrias@deltares.nl>
2026-08-04 10:30:05 +0200

    Change how nt is computed in SMT

--- applications/vtools/D3D/fcn/D3D_nt.m
+++ applications/vtools/D3D/fcn/D3D_nt.m
@@ -31,17 +31,29 @@ if isfolder(fpath_res)
         % fprintf('DEBUG: fpath_res %s \n',fpath_res)
         % fprintf('DEBUG: fdir_output %s \n',fdir_output)
     nf=D3D_SMT_nf(fdir_output);
-    nt=0;
-    for kf=0:1:nf
-        fdir_loc=D3D_SMT_dir_output_loc(fdir_output,kf);       
-            % fprintf('DEBUG: fdir_loc %s \n',fdir_loc)
-        simdef.D3D.dire_sim=fdir_loc;
-        simdef=D3D_simpath(simdef,'overwrite',0); 
-        fpath_nc=simdef.file.(res_type);
-            % fprintf('DEBUG: Processing %s \n',fpath_nc)
-        nt=nt+D3D_nt_single(fpath_nc,res_type);
-        % fprintf('%d\n',kf)
-    end
+
+    %Original exact implementation. We loop through all the files and count the number of time steps in each file. This is slow, but it is the only way to get the exact number of time steps.
+    %
+    % nt=0;
+    % for kf=0:1:nf
+    %     fdir_loc=D3D_SMT_dir_output_loc(fdir_output,kf);       
+    %         % fprintf('DEBUG: fdir_loc %s \n',fdir_loc)
+    %     simdef.D3D.dire_sim=fdir_loc;
+    %     simdef=D3D_simpath(simdef,'overwrite',0); 
+    %     fpath_nc=simdef.file.(res_type);
+    %         % fprintf('DEBUG: Processing %s \n',fpath_nc)
+    %     nt=nt+D3D_nt_single(fpath_nc,res_type);
+    %         % fprintf('%d\n',kf)
+    % end
+
+    %Smart speedup implementation. We assume that all the files have the same number of time steps, and we multiply the number of time steps in the first file by the number of files. This is much faster, but it is not guaranteed to be correct if the number of time steps is not the same in all files.
+    kf=0;
+    fdir_loc=D3D_SMT_dir_output_loc(fdir_output,kf);       
+    simdef.D3D.dire_sim=fdir_loc;
+    simdef=D3D_simpath(simdef,'overwrite',0); 
+    fpath_nc=simdef.file.(res_type);
+    nt=D3D_nt_single(fpath_nc,res_type);
+    nt=(nf+1)*nt;
 else
     nt=D3D_nt_single(fpath_res,res_type);    
 end %is
