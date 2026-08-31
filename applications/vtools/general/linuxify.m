49ea172765a429b81cf8b8402223f17756366a76
Victor Chavarrias <victor.chavarrias@deltares.nl>
2026-08-31 13:31:27 +0200

    make it linux proof

--- applications/vtools/general/linuxify.m
+++ applications/vtools/general/linuxify.m
@@ -23,8 +23,10 @@ else
     path_lin=path_win;
 end
 
+%copying to the clipboard is meant for interactive use, so it is skipped
+%when the result is assigned to a variable
 is_gui_mode = usejava('desktop') && usejava('awt');
-if is_gui_mode
+if is_gui_mode && nargout==0
    clipboard("copy",path_lin);
 end
 
